96f5837eca1495798955d91a1914e32ab25431f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:22 +0000

    Add package repeated-test/1.0/repeated_test-1.0-py2.py3-none-any.whl

---
+++ packages/repeated-test/repeated_test-1.0-py2.py3-none-any.whl/repeated_test/core.py
@@ -0,0 +1,150 @@
+import sys
+import re
+import traceback
+import unittest
+
+import six
+import collections
+
+
+try:
+    abc = collections.abc
+except AttributeError:
+    abc = collections
+
+
+__unittest = True # hides frames from this file from unittest output
+
+
+class FixturesDict(abc.MutableMapping):
+    def __init__(self, *args, **kwargs):
+        self.d = {}
+        self.lines = {}
+        super(FixturesDict, self).__init__(*args, **kwargs)
+
+    def __iter__(self):
+        return iter(self.d)
+
+    def __len__(self):
+        return len(self.d)
+
+    def __setitem__(self, key, value):
+        if key in self.d and (not key.startswith('_') or key == '_test'):
+            raise ValueError("Fixture already present: " + key)
+        else:
+            if key.startswith('test_'):
+                if key[5:] in self.d:
+                    raise ValueError(
+                        "Plain test conflicts with fixture: " + key)
+            elif not key.startswith('_'):
+                if 'test_' + key in self.d:
+                    raise ValueError(
+                        "Fixture conflicts with plain test: " + key)
+        self.lines[key] = traceback.extract_stack(sys._getframe(1), 1)[0][:3]
+        self.d[key] = value
+
+    def __getitem__(self, key):
+        return self.d[key]
+
+    def __delitem__(self, key):
+        del self.d[key]
+
+
+class FixturesMeta(type):
+    @classmethod
+    def __prepare__(cls, name, bases, TestCase=None):
+        ret = FixturesDict()
+        if TestCase is not None:
+            ret['_TestCase'] = TestCase
+        return ret
+
+    def __new__(meta, name, bases, d, TestCase=None):
+        container_loc = traceback.extract_stack(sys._getframe(1), 2)[0][:3]
+        TestCase = d.get('_TestCase', TestCase or unittest.TestCase)
+        try: # py3
+            orig = d.d
+            lines = d.lines
+        except AttributeError: # py2: No custom class dict
+            orig = d
+            lines = {}
+        members = dict(orig)
+        for key, value in orig.items():
+            if not key.startswith('test_') and not key.startswith('_'):
+                members['test_' + key] = _make_testfunc_runner(
+                    value, lines.get(key), container_loc, name, key)
+        bases = tuple(b for b in bases if b is not object)
+        if '_test' not in members:
+            raise ValueError("'_test' function missing from Fixtures class")
+        if members['_test'] is not None:
+            bases = bases + (TestCase,)
+        members['_repeated_test__lines'] = lines
+        members['_TestCase'] = TestCase
+        return super(FixturesMeta, meta).__new__(meta, name, bases, members)
+
+    def __init__(self, *args, **kwargs):
+        kwargs.pop('TestCase', None)
+        super(FixturesMeta, self).__init__(*args, **kwargs)
+
+    def with_test(cls, func):
+        meta = type(cls)
+        tc_cls = (cls._TestCase,) if cls.__dict__['_test'] is None else ()
+        bases = tuple(b for b in cls.__bases__ if b is not object) + tc_cls
+        members = dict(cls.__dict__)
+        members['_test'] = func
+        return super(FixturesMeta, meta).__new__(
+            meta, func.__name__, bases, members)
+
+
+Fixtures = six.with_metaclass(FixturesMeta)
+
+
+def WithTestClass(cls):
+    class metaclass(FixturesMeta):
+        def __new__(cls_, name, bases, d):
+            return FixturesMeta(name, (), d, TestCase=cls)
+    return type.__new__(metaclass, "WithTestClass_"+cls.__name__, (), {})
+
+
+def _make_testfunc_runner(value, fake_loc,
+                          container_loc, cls_name, member_name):
+    def _run_test(self):
+        try:
+            return self._test(*value)
+        except Exception as exc:
+            typ, exc, tb = sys.exc_info()
+            _raise_at_custom_line(
+                *get_loc(fake_loc, container_loc, cls_name, member_name)
+                )(typ, exc, tb.tb_next)
+    return _run_test
+
+
+def get_loc(fake_loc, cls_loc, cls_name, member_name):
+    if fake_loc is not None:
+        return fake_loc
+    cls_filename, cls_lineno, cls_funcname = cls_loc
+    lines = list(open(cls_filename))
+    esc = re.escape(member_name)
+    pat = re.compile(r"^\s*(" + esc + r"\s*=|def\s+" + esc + r"\s*\()")
+    for i, line in enumerate(lines[cls_lineno:], cls_lineno+1):
+        if pat.match(line):
+            return cls_filename, i, cls_name
+    else:
+        return cls_loc
+
+
+def _raise_at_custom_line(filename, lineno, funcname):
+    funcname = funcname.strip('<>')
+    d = {}
+    six.exec_(compile(_make_raiser(lineno, funcname), filename, 'exec'), d)
+    ret = d[funcname]
+    return ret
+
+
+def _make_raiser(lineno, funcname):
+    padding = '\n'*(lineno-1) + 'def '+funcname+'(typ, exc, tb):'
+    if sys.version_info < (3,):
+        return (
+            padding + 'other(typ, exc, tb)\n'
+            'def other(typ, exc, tb):raise typ, exc, tb')
+    else:
+        return padding+'raise exc.with_traceback(tb)'
---
+++ packages/repeated-test/repeated_test-1.0-py2.py3-none-any.whl/repeated_test/tests/test_rt.py
@@ -0,0 +1,300 @@
+import sys
+import inspect
+import unittest
+
+import unittest2
+
+from repeated_test import Fixtures, WithTestClass, tup, core
+
+
+skip_noprepare = unittest2.skipIf(
+    sys.version_info < (3,),
+    "Py version lacks custom class dict support")
+
+
+class RepeatedTestTests(unittest2.TestCase):
+    def setUp(self):
+        class sum_tests(Fixtures):
+            def _test(self, total, *terms):
+                self.assertEqual(total, sum(terms))
+            a = 3, 2, 1
+            b = 6, 3, 2, 1
+            c = 15, 5, 3
+            d = 4, 2, 2
+        self.sum_tests = sum_tests
+
+    def run_test(self, fixture, name):
+        tc = fixture(methodName=name)
+        getattr(tc, name)()
+
+    def test_success(self):
+        self.run_test(self.sum_tests, 'test_a')
+        self.run_test(self.sum_tests, 'test_b')
+        self.run_test(self.sum_tests, 'test_d')
+
+    def test_failure(self):
+        with self.assertRaises(AssertionError):
+            self.run_test(self.sum_tests, 'test_c')
+
+    def test_alt_test(self):
+        @self.sum_tests.with_test
+        def mul_test(self, mul, *terms):
+            result = 1
+            for t in terms:
+                result *= t
+            self.assertEqual(result, mul)
+
+        self.run_test(mul_test, 'test_b')
+        self.run_test(mul_test, 'test_c')
+        self.run_test(mul_test, 'test_d')
+
+        with self.assertRaises(AssertionError):
+            self.run_test(mul_test, 'test_a')
+
+    def test_unspecified_test(self):
+        class fixtures(Fixtures):
+            _test = None
+
+            a = 3, 6, 2
+            b = 6, 24, 4
+            c = 31, 64, 33
+            d = 2, 4, 2
+
+        self.assertFalse(issubclass(fixtures, unittest.TestCase))
+
+        @fixtures.with_test
+        def div_tests(self, r, a, b):
+            self.assertEqual(r, a / b)
+        self.run_test(div_tests, 'test_a')
+        self.run_test(div_tests, 'test_b')
+        self.run_test(div_tests, 'test_d')
+        with self.assertRaises(AssertionError):
+            self.run_test(div_tests, 'test_c')
+
+        @fixtures.with_test
+        def sub_tests(self, r, a, b):
+            self.assertEqual(r, a - b)
+        self.run_test(sub_tests, 'test_c')
+        self.run_test(sub_tests, 'test_d')
+        with self.assertRaises(AssertionError):
+            self.run_test(sub_tests, 'test_a')
+        with self.assertRaises(AssertionError):
+            self.run_test(sub_tests, 'test_b')
+
+    def test_missing_test(self):
+        with self.assertRaises(ValueError,
+                        msg="'_test' function missing from Fixtures class"):
+            class my_tests(Fixtures):
+                a = 1, 2, 3
+                b = 4, 5, 6
+
+    def test_line(self):
+        try:
+            self.run_test(self.sum_tests, "test_c")
+        except AssertionError:
+            _, _, tb = sys.exc_info()
+        else:
+            raise AssertionError("Expected AssertionError")
+        tb_funcs = []
+        tb_lines = []
+        while tb is not None:
+            fi = inspect.getframeinfo(tb)
+            tb_funcs.append(fi.function)
+            tb_lines.append(fi.code_context)
+            tb = tb.tb_next
+        self.assertIn("sum_tests", tb_funcs)
+        self.assertIn(["            c = 15, 5, 3\n"], tb_lines)
+
+    def test_func_location(self):
+        class func_tests(Fixtures):
+            def _test(self, func, a, b):
+                raise AssertionError
+
+            @tup("one", "params")
+            def one():
+                pass
+        try:
+            self.run_test(func_tests, "test_one")
+        except AssertionError:
+            _, _, tb = sys.exc_info()
+        else:
+            raise AssertionError("Expected AssertionError")
+        tb_funcs = []
+        tb_lines = []
+        while tb is not None:
+            fi = inspect.getframeinfo(tb)
+            tb_funcs.append(fi.function)
+            tb_lines.append(fi.code_context)
+            tb = tb.tb_next
+        self.assertIn("func_tests", tb_funcs)
+        try:
+            self.assertIn(['            @tup("one", "params")\n'], tb_lines)
+        except AssertionError:
+            self.assertIn(['            def one():\n'], tb_lines)
+
+    def test_relocate_frame_chevrons(self):
+        f = core._raise_at_custom_line("mymodule.py", 123, "<module>")
+        self.assertEqual(f.__name__, 'module')
+
+    @skip_noprepare
+    def test_dup(self):
+        with self.assertRaises(ValueError):
+            class fail_tests(Fixtures):
+                a = 1
+                a = 2
+
+    @skip_noprepare
+    def test_dup_with(self):
+        with self.assertRaises(ValueError):
+            class fail_tests(WithTestClass(unittest.TestCase)):
+                a = 1
+                a = 2
+
+    @skip_noprepare
+    def test_dup_ok(self):
+        class fail_tests(Fixtures):
+            def _test(self):
+                raise NotImplementedError
+            _private = 1
+            _private = 2
+
+    @skip_noprepare
+    def test_dup_testfunc(self):
+        with self.assertRaises(ValueError):
+            class fail_tests(Fixtures):
+                def _test(self):
+                    raise NotImplementedError
+                _test # silences pyflakes redef warning
+                def _test(self):
+                    raise NotImplementedError
+
+    @skip_noprepare
+    def test_dup_regular(self):
+        with self.assertRaises(ValueError):
+            class fail_tests(Fixtures):
+                def test_1(self):
+                    raise NotImplementedError
+                test_1 # silences pyflakes redef warning
+                def test_1(self):
+                    raise NotImplementedError
+
+    def test_copied_func(self):
+        trap = []
+        class fixtures(Fixtures):
+            def _test(self):
+                raise NotImplementedError
+            def test_a(self):
+                trap.append(0)
+        self.assertEqual(trap, [])
+        self.run_test(fixtures, 'test_a')
+        self.assertEqual(trap, [0])
+
+    @skip_noprepare
+    def test_dup_copied_func(self):
+        with self.assertRaises(ValueError):
+            class fail_test_1(Fixtures):
+                def _test(self):
+                    raise NotImplementedError
+                def test_a(self):
+                    raise NotImplementedError
+                a = ()
+        with self.assertRaises(ValueError):
+            class fail_test_2(Fixtures):
+                def _test(self):
+                    raise NotImplementedError
+                a = ()
+                def test_a(self):
+                    raise NotImplementedError
+
+    def test_tup(self):
+        def func():
+            raise NotImplementedError
+        self.assertEqual(tup(1, 2, 3)(func), (func, 1, 2, 3))
+
+    def test_unittest(self):
+        self.assertTrue(issubclass(self.sum_tests, unittest.TestCase))
+
+
+class CustomTestClassTests(unittest2.TestCase):
+    def setUp(self):
+        class MyTestCase(object):
+            pass
+        self.ctc = MyTestCase
+
+    def test_missing_test(self):
+        m = WithTestClass(self.ctc)
+        with self.assertRaises(ValueError,
+                        msg="'_test' function missing from Fixtures class"):
+            class tests(m):
+                t1 = 1, 2, 3
+
+    def test_par(self):
+        m = WithTestClass(self.ctc)
+        class tests(m):
+            def _test(self):
+                raise NotImplementedError
+        self.assertTrue(issubclass(tests, self.ctc))
+
+        @tests.with_test
+        def other_test():
+            raise NotImplementedError
+        self.assertTrue(issubclass(other_test, self.ctc))
+
+    def test_par_sep(self):
+        m = WithTestClass(self.ctc)
+        class fixtures(m):
+            _test = None
+        @fixtures.with_test
+        def sep_test():
+            raise NotImplementedError
+        self.assertTrue(issubclass(sep_test, self.ctc))
+
+    def test_body(self):
+        class tests(Fixtures):
+            _TestCase = self.ctc
+            def _test(self):
+                raise NotImplementedError
+        self.assertTrue(issubclass(tests, self.ctc))
+
+        @tests.with_test
+        def other_test():
+            raise NotImplementedError
+        self.assertTrue(issubclass(other_test, self.ctc))
+
+    def test_body_sep(self):
+        class fixtures(Fixtures):
+            _TestCase = self.ctc
+            _test = None
+        @fixtures.with_test
+        def sep_test():
+            raise NotImplementedError
+        self.assertTrue(issubclass(sep_test, self.ctc))
+
+    def test_keyword(self):
+        meta = core.FixturesMeta
+        name = 'fixtures'
+        bases = object,
+        d = meta.__prepare__(name, bases, TestCase=self.ctc)
+        def _testfunc(self):
+            raise NotImplementedError
+        d['_test'] = _testfunc
+        fixtures = meta(name, bases, d)
+        self.assertTrue(issubclass(fixtures, self.ctc))
+
+        @fixtures.with_test
+        def other_test():
+            raise NotImplementedError
+        self.assertTrue(issubclass(other_test, self.ctc))
+
+    def test_keyword_sep(self):
+        meta = core.FixturesMeta
+        name = 'fixtures'
+        bases = object,
+        d = meta.__prepare__(name, bases, TestCase=self.ctc)
+        d['_test'] = None
+        fixtures = meta(name, bases, d)
+
+        @fixtures.with_test
+        def other_test():
+            raise NotImplementedError
+        self.assertTrue(issubclass(other_test, self.ctc))
