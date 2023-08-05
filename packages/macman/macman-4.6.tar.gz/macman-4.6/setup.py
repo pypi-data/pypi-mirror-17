6cf51c0f66687821d08ad1db5fbc49b3d7a95e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:25 +0000

    Add package macman/4.6/macman-4.6.tar.gz

---
+++ packages/macman/macman-4.6.tar.gz/macman-4.6/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# Filename: setup.py
+
+from setuptools import setup
+
+setup(name='macman',
+      version='4.6',
+      description='OSX Management tools',
+      license='MIT',
+      packages=['macman'],
+      zip_safe=False)
