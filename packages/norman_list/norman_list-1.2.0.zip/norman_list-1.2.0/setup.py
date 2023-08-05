eebd0eb8efc4a558f266cf26b5992ac547f53535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:51 +0000

    Add package norman_list/1.2.0/norman_list-1.2.0.zip

---
+++ packages/norman_list/norman_list-1.2.0.zip/norman_list-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name          = 'norman_list',
+    version       = '1.2.0',
+    py_modules    = ['多層陣列清單取出'],
+    author        = 'hfpython',
+    author_email = 'hfpython@headfirstlabs.com',
+    url           = 'http://www.headfirstlabs.com',
+    description   = '多層陣列清單取出的範例程式',
+    
+)
