ef04defcea80d012cf9635084bf686219e954b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:47 +0000

    Add package nester_cloud1980_cn/1.0.0/nester_cloud1980_cn-1.0.0.zip

---
+++ packages/nester_cloud1980_cn/nester_cloud1980_cn-1.0.0.zip/nester_cloud1980_cn-1.0.0/nester_cloud1980_cn.py
@@ -0,0 +1,11 @@
+﻿"""这是nester.py模块，提供了一个print_lol函数。
+这个函数的作用是打印列表，其中有可能包含（也可能不包含）嵌套列表。
+"""
+def print_lol(the_list):
+        """这个函数取一个位置参数，名为the_list，它可以是任何Python列表
+        所指定的列表中的每项数据都会递归显示，各数据项各占一行。"""
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_lol(each_item)
+                else:
+                        print(each_item)
---
+++ packages/nester_cloud1980_cn/nester_cloud1980_cn-1.0.0.zip/nester_cloud1980_cn-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+        name='nester_cloud1980_cn',
+        version='1.0.0',
+        py_modules=['nester_cloud1980_cn'],
+        author='cloud1980_cn',
+        author_email='cloud1980_cn@163.com',
+        url='http://www.headfirstlabs.com',
+        description='A simple printer of nested lists', 
+   )
+
+
