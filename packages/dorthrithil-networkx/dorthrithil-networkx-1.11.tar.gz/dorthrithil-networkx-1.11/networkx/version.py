49fd5120329235cc75df0104b6d3e4ef827a2fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:03 +0000

    Add package dorthrithil-networkx/1.11/dorthrithil-networkx-1.11.tar.gz

---
+++ packages/dorthrithil-networkx/dorthrithil-networkx-1.11.tar.gz/dorthrithil-networkx-1.11/networkx/version.py
@@ -0,0 +1,25 @@
+"""
+Version information for NetworkX, created during installation.
+
+Do not add this file to the repository.
+
+"""
+
+import datetime
+
+version = '1.11'
+date = 'Thu Sep  1 14:59:00 2016'
+
+# Was NetworkX built from a development version? If so, remember that the major
+# and minor versions reference the "target" (rather than "current") release.
+dev = False
+
+# Format: (name, major, min, revision)
+version_info = ('networkx', '1', '11', None)
+
+# Format: a 'datetime.datetime' instance
+date_info = datetime.datetime(2016, 9, 1, 14, 59, 0, 851872)
+
+# Format: (vcs, vcs_tuple)
+vcs_info = (None, (None, None))
+
