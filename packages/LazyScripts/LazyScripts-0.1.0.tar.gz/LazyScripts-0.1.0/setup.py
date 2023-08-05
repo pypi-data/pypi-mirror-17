49332ed905a4eaa58abb196f67340293d18ae86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:25 +0000

    Add package LazyScripts/0.1.0/LazyScripts-0.1.0.tar.gz

---
+++ packages/LazyScripts/LazyScripts-0.1.0.tar.gz/LazyScripts-0.1.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('LazyScripts/version.py') as fid:
+    for line in fid:
+        if line.startswith('__version__'):
+            version = line.strip().split()[-1][1:-1]
+            break
+
+setup(name='LazyScripts',
+      packagez=['LazyScripts'],
+      version=version,
+      description='Useful python modules for lazy scripting.',
+      author='James Wenzel',
+      author_email='wenzel.james.r@gmail.com',
+      url='https://github.com/jameswenzel/LazyScripts',
+      download_url='https://github.com/jameswenzel/LazyScripts/tarball/0.1.0',
+      license='Apache License, Version 2.0',
+      keywords=['lazy', 'csv', 'tor', 'multithread', 'beautifulsoup', 'json'],
+      classifiers=[],
+      install_requires=['beautifulsoup4 >= 4.4.1', 'requests >= 2.2.1',
+                        'stem >= 1.4']
+      )
