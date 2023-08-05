a53bac5f14ca8857d6d7daed1235957f680bbb09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:56 +0000

    Add package netjsonconfig/0.4.5/netjsonconfig-0.4.5.tar.gz

---
+++ packages/netjsonconfig/netjsonconfig-0.4.5.tar.gz/netjsonconfig-0.4.5/netjsonconfig/backends/base.py
@@ -0,0 +1,60 @@
+class BaseRenderer(object):
+    """
+    Renderers are used to generate specific configuration blocks.
+    """
+    block_name = None
+
+    def __init__(self, backend):
+        self.config = backend.config
+        self.env = backend.env
+        self.backend = backend
+
+    @classmethod
+    def get_package(cls):
+        return str(cls.__name__).replace('Renderer', '').lower()
+
+    def cleanup(self, output):
+        """
+        Performs cleanup of output (indentation, new lines)
+        """
+        # correct indentation
+        output = output.replace('    ', '')\
+                       .replace('\noption', '\n\toption')\
+                       .replace('\nlist', '\n\tlist')
+        # convert True to 1 and False to 0
+        output = output.replace('True', '1')\
+                       .replace('False', '0')
+        # max 2 consecutive \n delimiters
+        output = output.replace('\n\n\n', '\n\n')
+        # if output is present
+        # ensure it always ends with 1 new line
+        if output.endswith('\n\n'):
+            return output[0:-1]
+        return output
+
+    def render(self):
+        """
+        Renders config block with jinja2 templating engine
+        """
+        # get jinja2 template
+        template_name = '{0}.uci'.format(self.get_package())
+        template = self.env.get_template(template_name)
+        # render template and cleanup
+        context = self.get_context()
+        output = template.render(**context)
+        return self.cleanup(output)
+
+    def get_context(self):
+        """
+        Builds context that is passed to jinja2 templates
+        """
+        # get list of private methods that start with "_get_"
+        methods = [method for method in dir(self) if method.startswith('_get_')]
+        context = {}
+        # build context
+        for method in methods:
+            key = method.replace('_get_', '')
+            context[key] = getattr(self, method)()
+        # determine if all context values are empty
+        context['is_empty'] = not any(context.values())
+        return context
---
+++ packages/netjsonconfig/netjsonconfig-0.4.5.tar.gz/netjsonconfig-0.4.5/netjsonconfig/utils.py
@@ -0,0 +1,74 @@
+import re
+from collections import OrderedDict
+from copy import deepcopy
+
+import six
+
+
+def merge_config(template, config):
+    """
+    Merges ``config`` on top of ``template``.
+
+    Conflicting keys are handled in the following way:
+
+    * simple values (eg: ``str``, ``int``, ``float``, ecc) in ``config`` will
+      overwrite the ones in ``template``
+    * values of type ``list`` in both ``config`` and ``template`` will be summed
+      in order to create a list which contains elements of both
+    * values of type ``dict`` will be merged recursively
+
+    :param template: template ``dict``
+    :param config: config ``dict``
+    :returns: merged ``dict``
+    """
+    result = template.copy()
+    for key, value in config.items():
+        if isinstance(value, dict):
+            node = result.get(key, {})
+            result[key] = merge_config(node, value)
+        elif isinstance(value, list) and isinstance(result.get(key), list):
+            result[key] = deepcopy(result[key]) + deepcopy(value)
+        else:
+            result[key] = value
+    return result
+
+
+def sorted_dict(dictionary):
+    return OrderedDict(sorted(dictionary.items()))
+
+
+var_pattern = re.compile(r'\{\{\s(\w*)\s\}\}')
+
+
+def evaluate_vars(data, context={}):
+    """
+    Evaluates variables in ``data``
+
+    :param data: data structure containing variables, may be
+                 ``str``, ``dict`` or ``list``
+    :param context: ``dict`` containing variables
+    :returns: modified data structure
+    """
+    if isinstance(data, (dict, list)):
+        if isinstance(data, dict):
+            loop_items = data.items()
+        elif isinstance(data, list):
+            loop_items = enumerate(data)
+        for key, value in loop_items:
+            data[key] = evaluate_vars(value, context)
+    elif isinstance(data, six.string_types):
+        for var in var_pattern.findall(data):
+            if var in context:
+                data = data.replace('{{ %s }}' % var, context[var])
+    return data
+
+
+class _TabsMixin(object):  # pragma: nocover
+    """
+    mixin that adds _tabs method to test classes
+    """
+    def _tabs(self, string):
+        """
+        replace 4 spaces with 1 tab
+        """
+        return string.replace('    ', '\t')
