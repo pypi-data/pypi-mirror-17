21993d3efe71255537f4d724b3569bf94b2baaa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:43 +0000

    Add package name_cgf/1.0.0/name_cgf-1.0.0.tar.gz

---
+++ packages/name_cgf/name_cgf-1.0.0.tar.gz/name_cgf-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+         name = "name_cgf",
+         version = "1.0.0",
+         py_modules = ["name_cgf"],
+         author = "cgf",
+         author_email = "247858638@qq.com",
+         url = "http://www/headfirstlabs.com",
+         description = "heheda",
+     )
+
+
