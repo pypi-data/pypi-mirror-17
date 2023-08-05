fa8bd023812cfdfea58e09ffd1da9ec264e53d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:11 +0000

    Add package nester_condoor/1.0.0/nester_condoor-1.0.0.zip

---
+++ packages/nester_condoor/nester_condoor-1.0.0.zip/nester_condoor-1.0.0/nester.py
@@ -0,0 +1,8 @@
+"""这是我写的递归函数print_lol()"""
+
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item)
+        else:
+            print(each_item)
---
+++ packages/nester_condoor/nester_condoor-1.0.0.zip/nester_condoor-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='nester_condoor',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='Condoor',
+    author_email='shengliukang@hotmail.com',
+    )
