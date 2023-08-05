ace524f49d9d016350e89e25ac21739f3409408d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:48 +0000

    Add package alex_nester/1.3.0/alex_nester-1.3.0.tar.gz

---
+++ packages/alex_nester/alex_nester-1.3.0.tar.gz/alex_nester-1.3.0/setup.py
@@ -0,0 +1,10 @@
+﻿from distutils.core import setup
+setup(
+		name		='alex_nester',
+		version		='1.3.0',
+		py_modules	=['alex_nester'],
+		author		='alexshen',
+		author_email='alex719614@gmail.com',
+		url			='http://weibo.com/1672977657/profile?topnav=1&wvr=6&is_all=1',
+		description	='A simple printer of nested lists',
+	 )
