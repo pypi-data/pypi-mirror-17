881f88fcdcaf07f9bfc82c9f3b75c36c7460f148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:39 +0000

    Add package coala-bears/0.9.0.dev20160901195823/coala_bears-0.9.0.dev20160901195823-py3-none-any.whl

---
+++ packages/coala-bears/coala_bears-0.9.0.dev20160901195823-py3-none-any.whl/bears/js/JSComplexityBear.py
@@ -0,0 +1,39 @@
+import json
+
+from coalib.bearlib.abstractions.Linter import linter
+from coalib.bears.requirements.NpmRequirement import NpmRequirement
+from coalib.results.Result import Result
+
+
+@linter(executable='cr')
+class JSComplexityBear:
+    """
+    Calculates cyclomatic complexity using ``cr``.
+    """
+    LANGUAGES = {"JavaScript"}
+    REQUIREMENTS = {NpmRequirement('complexity-report', '2.0.0-alpha')}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+    ASCIINEMA_URL = 'https://asciinema.org/a/39250'
+    CAN_DETECT = {'Complexity'}
+
+    @staticmethod
+    def create_arguments(filename, file, config_file):
+        return '--format', 'json', filename
+
+    def process_output(self, output, filename, file, cc_threshold: int=10):
+        """
+        :param cc_threshold: Threshold value for cyclomatic complexity
+        """
+        message = "{} has a cyclomatic complexity of {}."
+        if output:
+            output = json.loads(output)
+            for function in output["reports"][0]["functions"]:
+                if function["cyclomatic"] >= cc_threshold:
+                    yield Result.from_values(
+                        origin=self,
+                        message=message.format(function["name"],
+                                               function["cyclomatic"]),
+                        file=filename,
+                        line=function["line"])
