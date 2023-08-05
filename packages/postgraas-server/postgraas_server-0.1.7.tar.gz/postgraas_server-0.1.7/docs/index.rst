2b3f35f4a8aec7eb1b75f204995ce4a2d2020edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:18 +0000

    Add package postgraas-server/0.1.7/postgraas_server-0.1.7.tar.gz

---
+++ packages/postgraas-server/postgraas_server-0.1.7.tar.gz/postgraas_server-0.1.7/docs/index.rst
@@ -0,0 +1,26 @@
+================
+postgraas_server
+================
+
+This is the documentation of **postgraas_server**.
+
+Contents
+========
+
+.. toctree::
+   :maxdepth: 2
+
+   Installation <installation>
+   Usage <usage>
+   License <license>
+   Authors <authors>
+   Changelog <changes>
+   Module Reference <api/modules>
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/postgraas-server/postgraas_server-0.1.7.tar.gz/postgraas_server-0.1.7/postgraas_server/create_db.py
@@ -0,0 +1,30 @@
+from postgraas_server.management_database import init_db
+import postgres_instance_driver as pg
+from postgraas_server.configuration import get_config
+
+def create_db_container():
+    config = get_config()
+    db_credentials = {
+            "db_name": config.get('metadb', 'db_name'),
+            "db_username": config.get('metadb', 'db_username'),
+            "db_pwd": config.get('metadb', 'db_pwd'),
+            "host": config.get('metadb', 'host'),
+            "port": config.get('metadb', 'port')
+        }
+    try:
+        db_credentials['container_id'] = pg.create_postgres_instance('postgraas_master_db', db_credentials)
+    except ValueError as e:
+        print "warning container already exists"
+        postrgaas_db = pg.get_container_by_name('postgraas_master_db')
+        db_credentials['container_id'] = postrgaas_db['Id']
+    return db_credentials
+
+
+def main():
+    from postgraas_server import postgraas_api
+    db_credentials = create_db_container()
+    init_db(db_credentials, postgraas_api.app)
+
+
+if __name__ == '__main__':
+    main()
