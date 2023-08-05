93183424ff8c871a7bf51d8a91a406e6a63c8c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:50 +0000

    Add package prance/0.1.3/prance-0.1.3.tar.gz

---
+++ packages/prance/prance-0.1.3.tar.gz/prance-0.1.3/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Dist utils for Prance.
+
+This should work within tox, and install all required dependencies for testing.
+"""
+
+if __name__ == '__main__':
+  # Get setup function
+  try:
+    from setuptools import setup, find_packages
+  except ImportError:
+    from distutils.core import setup, find_packages
+
+  # Run setup
+  setup(
+      name = 'prance',
+      version = '0.1.3',
+      description = 'Swagger/OpenAPI 2.0 Parser',
+      long_description = open('README.rst').read(),
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      keywords = 'swagger openapi parsing',
+      author = 'Jens Finkhaeuser',
+      author_email = 'jens@finkhaeuser.de',
+      url = 'https://github.com/jfinkhaeuser/prance',
+      license = 'MITNFA',
+      packages = find_packages(exclude = ['ez_setup', 'examples', 'tests']),
+      include_package_data = True,
+      install_requires = [
+        'chardet~=2.3',
+        'PyYAML~=3.11',
+        'swagger-spec-validator~=2.0',
+        'dpath~=1.4',
+        'requests~=2.11',
+      ],
+      zip_safe = True,
+      test_suite = 'tests',
+      setup_requires = ['pytest-runner'],
+      tests_require = [
+        'tox>=2.3',
+        'bumpversion>=0.5',
+        'pytest>=3.0',
+        'pytest-cov>=2.3',
+        'flake8>=3.0',
+        'pep8-naming>=0.4',
+        'flake8-quotes>=0.8',
+        'flake8_docstrings>=1.0',
+      ],
+  )
