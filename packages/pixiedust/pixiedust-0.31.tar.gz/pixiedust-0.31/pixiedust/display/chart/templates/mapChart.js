cebb2fada93fad7418000cb43d7aef3e56444528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:49 +0000

    Add package pixiedust/0.31/pixiedust-0.31.tar.gz

---
+++ packages/pixiedust/pixiedust-0.31.tar.gz/pixiedust-0.31/pixiedust/display/chart/templates/mapChart.js
@@ -0,0 +1,9 @@
+function draw() {
+	var drawOptions = {
+		region: '{{this.options.get("mapRegion")}}',
+        displayMode: '{{this.options.get("mapDisplayMode")}}'
+	};
+	var chart = new google.visualization.GeoChart(document.getElementById('map{{prefix}}'));
+	chart.draw(google.visualization.arrayToDataTable({{this.options.get("mapData")}}), drawOptions);
+}
+google.load('visualization', '1.0', {'callback': draw, 'packages':['geochart']});
