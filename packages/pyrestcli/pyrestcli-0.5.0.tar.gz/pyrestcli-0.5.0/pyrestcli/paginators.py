9dc358a3a807193d73bb80b442d2002039c645b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:20 +0000

    Add package pyrestcli/0.5.0/pyrestcli-0.5.0.tar.gz

---
+++ packages/pyrestcli/pyrestcli-0.5.0.tar.gz/pyrestcli-0.5.0/pyrestcli/paginators.py
@@ -0,0 +1,36 @@
+class Paginator:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def get_urls(self, initial_url):
+        raise NotImplemented
+
+    def process_response(self):
+        raise NotImplemented
+
+
+class DummyPaginator(Paginator):
+    def get_urls(self, initial_url):
+        url = initial_url
+        while url is not None:
+            yield url
+            url = None
+
+    def process_response(self, response):
+        return response
+
+
+class NextWithUrlPaginator(Paginator):
+    def get_urls(self, initial_url):
+        self.url = initial_url
+        while self.url is not None:
+            yield self.url
+
+    def process_response(self, response):
+        response_json = response.json()
+        try:
+            self.url = response_json["next"].replace(self.base_url, "")
+        except AttributeError:
+            self.url = None
+
+        return response
