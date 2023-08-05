b34cdbf71c762aee2d547d8cfbebcd1d50396b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:00 +0000

    Add package chinese_nester/1.3.0/chinese_nester-1.3.0.zip

---
+++ packages/chinese_nester/chinese_nester-1.3.0.zip/chinese_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         =  'chinese_nester',
+    version      =  '1.3.0',
+    py_modules   = ['nester'],
+    author       = 'lionel-ldr',
+    author_email = 'lionel-ldr@163.com',
+    url          = 'http://www.headfirstlabs.com',
+    description  = 'A simple printer of nested lists',
+    )
