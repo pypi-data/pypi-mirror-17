1e4b8afdbbd4288ae482acfbf8e362ed769ddc03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:11 +0000

    Add package fredpy/2.0.2/fredpy-2.0.2.tar.gz

---
+++ packages/fredpy/fredpy-2.0.2.tar.gz/fredpy-2.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'fredpy',
+  packages = ['fredpy'],
+  version = '2.0.2',
+  description = 'A package for downloading and working with data from Federal Reserve Economic Data',
+  author = 'Brian C. Jenkins',
+  author_email = 'bcjenkin@uci.edu',
+  url = 'https://github.com/letsgoexploring/fredpy-package',
+  download_url = 'https://github.com/letsgoexploring/fredpy-package/raw/master/dist/fredpy-2.0.2.tar.gz',
+  keywords = ['fred', 'economics','data','federal reserve'],
+  classifiers = [],
+)
