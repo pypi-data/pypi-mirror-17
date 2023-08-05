8692014a6a2822c85cb7ba0a43bb91cba7a1886b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:09 +0000

    Add package dfparaplot/0.1/dfparaplot-0.1.tar.gz

---
+++ packages/dfparaplot/dfparaplot-0.1.tar.gz/dfparaplot-0.1/dfparaplot/__init__.py
@@ -0,0 +1,2 @@
+from pplot import pplot
+
---
+++ packages/dfparaplot/dfparaplot-0.1.tar.gz/dfparaplot-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dfparaplot',
+  packages = ['dfparaplot'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A wrapper around matplotlib plot for parametric pandas dataframe experiment data',
+  author = 'Ferdinand Leinbach',
+  author_email = 'ferid.leinbach@googlemail.com',
+  url = 'https://github.com/effi/dfparaplot', # use the URL to the github repo
+  download_url = 'https://github.com/effi/dfparaplot/tarball/0.1', # I'll explain this in a second
+  keywords = ['ploting', 'parametric', 'dataframe'], # arbitrary keywords
+  classifiers = [],
+)
