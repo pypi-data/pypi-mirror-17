08013dc89b2e0d646192471c70859ae98f4520bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:16 +0000

    Add package squery-lite/2.0.dev5/squery-lite-2.0.dev5.tar.gz

---
+++ packages/squery-lite/squery-lite-2.0.dev5.tar.gz/squery-lite-2.0.dev5/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+
+VERSION = '2.0.dev5'
+
+
+def read(fname):
+    """ Return content of specified file """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='squery-lite',
+    version=VERSION,
+    author='Outernet Inc',
+    author_email='apps@outernet.is',
+    url='https://github.com/Outernet-Project/squery-lite',
+    license='BSD',
+    packages=['squery_lite'],
+    include_package_data=True,
+    long_description=read('README.rst'),
+    install_requires=[
+        'pytz',
+        'sqlize',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
