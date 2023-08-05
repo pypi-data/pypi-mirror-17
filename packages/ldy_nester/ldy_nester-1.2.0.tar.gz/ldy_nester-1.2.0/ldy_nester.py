09b5f354f28d0f022ab908e2a79c4696b9fe8a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:30 +0000

    Add package ldy_nester/1.2.0/ldy_nester-1.2.0.tar.gz

---
+++ packages/ldy_nester/ldy_nester-1.2.0.tar.gz/ldy_nester-1.2.0/ldy_nester.py
@@ -0,0 +1,13 @@
+"""This is the "nester.py" moudle and it provides one function called print_lol(), which print 	    lists that may or may not include nested lists:"""
+
+def print_lol(the_list,level=0):
+	"""This function takes one positional argument called "the_list", which is any python 		   list. Each data item in the provided list is printed to the screen on it's own line.
+	"""
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item,level+1)
+		else:
+			for tab_num in range(level):
+				print("\t",end='')
+			
+			print(each_item)
