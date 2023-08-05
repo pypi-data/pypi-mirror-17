a1d32855ddbdee588c28b193bbc8d88639be68b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:08 +0000

    Add package dropman/1.1.1/dropman-1.1.1.tar.gz

---
+++ packages/dropman/dropman-1.1.1.tar.gz/dropman-1.1.1/dropman/commands/poweroff.py
@@ -0,0 +1,45 @@
+"""The power off command."""
+
+import time
+
+import digitalocean
+
+from .base import Base
+
+
+class PowerOff(Base):
+    @staticmethod
+    def power_off(droplet_id, droplet, manager):
+        if droplet.status != 'active':
+            droplet.power_off()
+
+            print("Droplet id/name: " + str(droplet.id) + "/" + droplet.name + " is now being power off...")
+
+            is_off = False
+            retries = 10
+            while is_off is False or retries > 0:
+                time.sleep(10)
+                droplet = manager.get_droplet(droplet_id)
+
+                if droplet.status == 'off':
+                    is_off = True
+                    print("Droplet id/name: " + str(droplet.id) + "/" + droplet.name + " is now power off.")
+
+                retries -= 1
+
+            return
+
+        print("Droplet id/name: " + str(droplet.id) + "/" + droplet.name + " is already power off.")
+
+    def run(self, api_token):
+        droplet_id = self.options['<id>']
+        manager = digitalocean.Manager(token=api_token)
+        droplet = None
+
+        try:
+            droplet = manager.get_droplet(droplet_id)
+        except digitalocean.baseapi.DataReadError as err:
+            print(err)
+
+        if droplet is not None:
+            PowerOff.power_off(droplet_id, droplet, manager)
