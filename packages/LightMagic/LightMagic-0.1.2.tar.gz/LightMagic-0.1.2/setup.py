85dc341c620cae656361c24210c5c29850a41d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:08 +0000

    Add package LightMagic/0.1.2/LightMagic-0.1.2.tar.gz

---
+++ packages/LightMagic/LightMagic-0.1.2.tar.gz/LightMagic-0.1.2/setup.py
@@ -0,0 +1,42 @@
+import os
+
+from setuptools import (setup, find_packages)
+
+PACKAGE = "LightMagic"
+NAME = "LightMagic"
+DESCRIPTION = __import__(PACKAGE).__doc__
+AUTHOR = "Maxim Radyukov"
+AUTHOR_EMAIL = "maxim.radyukov@gmail.com"
+URL = "https://github.com/MaxRV/LightMagic"
+VERSION = __import__(PACKAGE).__version__
+
+work_dir = os.path.abspath(os.path.dirname(__file__))
+
+with open("%s/README.rst" % work_dir) as f:
+    long_description = f.read()
+
+with open("%s/LightMagic/requirements.txt" % work_dir) as f:
+    install_requires = f.read()
+
+setup(
+        name=NAME,
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=long_description,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        license="MIT",
+        url=URL,
+        packages=find_packages(exclude=["tests.*", "tests"]),
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Topic :: Database :: Front-Ends",
+            "Operating System :: OS Independent",
+        ],
+        install_requires=install_requires
+)
