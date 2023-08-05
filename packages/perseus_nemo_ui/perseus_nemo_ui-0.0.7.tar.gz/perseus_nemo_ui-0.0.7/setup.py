ccef4fe4dd5d69777af4a038429d9e014f707e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:21 +0000

    Add package perseus_nemo_ui/0.0.7/perseus_nemo_ui-0.0.7.tar.gz

---
+++ packages/perseus_nemo_ui/perseus_nemo_ui-0.0.7.tar.gz/perseus_nemo_ui-0.0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='perseus_nemo_ui',
+    version="0.0.7",
+    packages=find_packages(exclude=["examples", "tests"]),
+    url='https://github.com/PerseusDL/perseus_nemo_ui',
+    license='GNU GPL',
+    author='Bridget Almas',
+    author_email='balmas@gmail.com',
+    description='Plugin for Perseus UI for Nemo',
+    test_suite="tests",
+    install_requires=[
+        "flask_nemo>=1.0.0b1"
+    ],
+    tests_require=[
+        "capitains_nautilus>=0.0.5"
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
