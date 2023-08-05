826a3c35e3580aaef3ee20907e649fa3c23eb454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:34 +0000

    Add package nesterqwe/1.4.1/nesterqwe-1.4.1.tar.gz

---
+++ packages/nesterqwe/nesterqwe-1.4.1.tar.gz/nesterqwe-1.4.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='nesterqwe',
+    version='1.4.1',
+    py_modules=['nesterqwe'],
+    author='CoolSearcher',
+    author_email='dolber@gmail.com',
+    description='A simple printer of nested lists',
+
+)
