5a7010ecbf8e26fa372496013d6fde9ef38513dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:06 +0000

    Add package zc-common/0.2.3/zc_common-0.2.3-py2-none-any.whl

---
+++ packages/zc-common/zc_common-0.2.3-py2-none-any.whl/zc_common/remote_resource/models.py
@@ -0,0 +1,53 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class RemoteResource(object):
+
+    def __init__(self, type_name, pk):
+
+        self.type = str(type_name) if type_name else None
+        self.id = str(pk) if pk else None
+
+
+class RemoteForeignKey(models.CharField):
+    description = "A foreign key pointing to an external resource"
+
+    def __init__(self, type_name, *args, **kwargs):
+        if 'max_length' not in kwargs:
+            kwargs['max_length'] = 50
+
+        if 'db_index' not in kwargs:
+            kwargs['db_index'] = True
+
+        if 'db_column' not in kwargs:
+            kwargs['db_column'] = "%s_id" % type_name.lower()
+
+        self.type = type_name
+
+        super(RemoteForeignKey, self).__init__(*args, **kwargs)
+
+    def from_db_value(self, value, expression, connection, context):
+        return RemoteResource(self.type, value)
+
+    def to_python(self, value):
+        if isinstance(value, RemoteResource):
+            return value.id
+
+        if isinstance(value, basestring):
+            return value
+
+        if value is None:
+            return value
+
+        raise ValueError("Can not convert value to a RemoteResource properly")
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(RemoteForeignKey, self).deconstruct()
+
+        args = tuple([self.type] + list(args))
+
+        del kwargs['max_length']
+
+        return name, path, args, kwargs
