15dcf37a9be9694107f36fcffb175c12a4f27854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:07 +0000

    Add package nester-zhy2/1.0.1/nester-zhy2-1.0.1.zip

---
+++ packages/nester-zhy2/nester-zhy2-1.0.1.zip/nester-zhy2-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	      name			= 'nester-zhy2',
+		  version		= '1.0.1',
+		  py_modules	= ['nester'],
+		  author		= 'hfpython',
+		  author_email	= 'hfpython@headfirstlabs.com',
+		  url			= 'http://www.headfirstlabs.com',
+		  description	= 'A simple printer of nested lists',
+	 )
