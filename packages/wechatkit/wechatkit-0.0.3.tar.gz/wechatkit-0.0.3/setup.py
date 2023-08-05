2a34afd0b7cbe48e346d74216fc17afef19cbe55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:23 +0000

    Add package wechatkit/0.0.3/wechatkit-0.0.3.tar.gz

---
+++ packages/wechatkit/wechatkit-0.0.3.tar.gz/wechatkit-0.0.3/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""setup.py."""
+import os
+from setuptools import setup, find_packages
+
+INSTALL_REQUIRES = [
+    'requests>=2.11.1',
+    'xmltodict>=0.10.2'
+]
+
+VERSION = '0.0.3'
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    README = f.read()
+
+
+setup(
+    name='wechatkit',
+    version=VERSION,
+    description='wechatkit is a common wechat api component.',
+    long_description=README,
+    author='silence',
+    author_email='istommao@gmail.com',
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    url='https://github.com/istommao/wechatkit',
+    keywords='wechatkit is a common wechat api component!'
+)
