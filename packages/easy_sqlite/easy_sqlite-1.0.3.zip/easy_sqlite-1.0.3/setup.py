7b8dcbd044c6a8435c4f0e14e2ddd4a38c7635d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:04 +0000

    Add package easy_sqlite/1.0.3/easy_sqlite-1.0.3.zip

---
+++ packages/easy_sqlite/easy_sqlite-1.0.3.zip/easy_sqlite-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'easy_sqlite',
+  packages = ['easy_sqlite'], # this must be the same as the name above
+  version = '1.0.3',
+  description = 'This Library makes using the sqlite3 library much easier and faster.',
+  author = 'Calder White',
+  author_email = 'calderwhite1@gmail.com',
+  url = 'https://github.com/CalderWhite/easy_sqlite', # use the URL to the github repo
+  download_url = 'https://github.com/CalderWhite/easy_sqlite/archive/master.zip', # I'll explain this in a second
+  keywords = [], # arbitrary keywords
+  classifiers = [],
+)
