36f55ac41156b6fdf8f4403ff847fb5933025d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:28 +0000

    Add package django-dynamic-formsets/0.0.3/django-dynamic-formsets-0.0.3.tar.gz

---
+++ packages/django-dynamic-formsets/django-dynamic-formsets-0.0.3.tar.gz/django-dynamic-formsets-0.0.3/README.md
@@ -0,0 +1,8 @@
+A jQuery plugin for managing Django formsets
+============================================
+
+This [jQuery](http://jquery.com/) plugin helps you create more usable [Django](http://www.djangoproject.com/) formsets by allowing clients to add and remove forms on the client-side.
+
+It was primarily developed by [Stanislaus Madueke](https://github.com/elo80ka), and re-packaged as a static Django app (with a couple shiny new enhancements and more docs) by [The Dallas Morning News](https://github.com/DallasMorningNews/).
+
+Documentation will be added very soon.
---
+++ packages/django-dynamic-formsets/django-dynamic-formsets-0.0.3.tar.gz/django-dynamic-formsets-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup  # NOQA
+
+
+REPO_URL = 'https://github.com/dallasmorningnews/django-dynamic-formsets'
+
+VERSION = '0.0.3'
+
+
+setup(
+    name='django-dynamic-formsets',
+    packages=['dynamic_formsets'],
+    version=VERSION,
+    description='A random test lib',
+    author='Allan James Vestal, The Dallas Morning News',
+    author_email='ajvestal@dallasnews.com',
+    url=REPO_URL,
+    download_url=REPO_URL + '/tarball/' + VERSION,
+    keywords=['django', 'formset', 'javascript'],
+    classifiers=[],
+    license='New BSD License',
+    long_description=open('README.md').read(),
+)
