44bb07038debffbef934d3d1bd1be7c542d9fd90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:24 +0000

    Add package NFL_Draftkings/2/NFL_Draftkings-2.zip

---
+++ packages/NFL_Draftkings/NFL_Draftkings-2.zip/NFL_Draftkings-2/NFL_Draftkings/run.py
@@ -0,0 +1,5 @@
+from GetPlayerDKScores import get_scores, get_weekly_scores, get_top_performers
+from ConvertToDraftKings import convert_to_DK
+
+a = get_top_performers(week=1, season=2015)
+print a
