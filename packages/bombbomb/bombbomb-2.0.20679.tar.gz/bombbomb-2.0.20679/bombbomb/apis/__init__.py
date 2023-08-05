532345a67bcbbbc83bd930823c5128142507a8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:29 +0000

    Add package bombbomb/2.0.20679/bombbomb-2.0.20679.tar.gz

---
+++ packages/bombbomb/bombbomb-2.0.20679.tar.gz/bombbomb-2.0.20679/bombbomb/apis/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .prompts_api import PromptsApi
+from .teams_api import TeamsApi
+from .utilities_api import UtilitiesApi
+from .webhooks_api import WebhooksApi
