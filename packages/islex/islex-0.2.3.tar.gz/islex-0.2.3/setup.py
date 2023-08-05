7adaf666f28944dc2ca48a69d232be6f87e6782d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:22 +0000

    Add package islex/0.2.3/islex-0.2.3.tar.gz

---
+++ packages/islex/islex-0.2.3.tar.gz/islex-0.2.3/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    "attrs", "enum34", "six"
+]
+
+test_requirements = [
+    "pytest", "pytest-cov"
+]
+
+setup(
+    name='islex',
+    version='0.2.3',
+    description="Utility classes to support easy loading and manipulation of dictionary-to-pronunciation.",
+    long_description=readme + '\n\n' + history,
+    author="Jeremy G. Kahn",
+    author_email='jeremy@trochee.net',
+    url='https://github.com/jkahn/islex',
+    packages=[
+        'islex',
+    ],
+    package_dir={'islex':
+                 'islex'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=True,
+    keywords='islex',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    setup_requires=['pytest-runner',],
+    test_suite='tests',
+    tests_require=test_requirements,
+    entry_points={
+        'console_scripts': [
+            'islex-write-package = islex.load:write_package_data'
+        ]
+    },
+)
