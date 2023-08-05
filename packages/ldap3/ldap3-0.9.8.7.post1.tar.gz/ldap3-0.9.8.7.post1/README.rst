61b7657926518186be7aaf9ca8876a4d2c1555d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:16 +0000

    Add package ldap3/0.9.8.7.post1/ldap3-0.9.8.7.post1.tar.gz

---
+++ packages/ldap3/ldap3-0.9.8.7.post1.tar.gz/ldap3-0.9.8.7.post1/README.rst
@@ -0,0 +1,92 @@
+LDAP3
+=====
+
+.. image:: https://img.shields.io/pypi/v/ldap3.svg
+    :target: https://pypi.python.org/pypi/ldap3/
+    :alt: Latest Version
+
+.. image:: https://img.shields.io/travis/cannatag/ldap3/master.svg
+    :target: https://travis-ci.org/cannatag/ldap3
+    :alt: TRAVIS-CI build status for master branch
+
+.. image:: https://img.shields.io/pypi/l/ldap3.svg
+    :target: https://pypi.python.org/pypi/ldap3/
+    :alt: License
+
+ldap3 is a strictly RFC 4511 conforming LDAP V3 pure Python **client**. The same codebase works with Python, Python 3, PyPy and PyPy3.
+
+This project was formerly named **python3-ldap**. The name has been changed to avoid confusion with the python-ldap library.
+
+Home Page
+---------
+
+Project home page is https://github.com/cannatag/ldap3
+
+
+Documentation
+-------------
+
+Documentation is available at http://ldap3.readthedocs.org
+
+
+License
+-------
+
+The ldap3 project is open source software released under the **LGPL v3 license**.
+
+
+PEP8 Compliance
+---------------
+
+ldap3 is PEP8 compliant, except for line length.
+
+
+Download
+--------
+
+Package download is available at https://pypi.python.org/pypi/ldap3.
+
+
+Install
+-------
+
+Install with **pip install ldap3**
+
+
+Git repository
+--------------
+
+You can download the latest source at https://github.com/cannatag/ldap3
+
+
+Continuous integration
+----------------------
+
+Continuous integration for testing is at https://travis-ci.org/cannatag/ldap3
+
+Support
+-------
+
+You can submit support tickets on https://github.com/cannatag/ldap3/issues/new
+
+
+Thanks to
+---------
+
+* **Ilya Etingof**, the author of the *pyasn1* package for his excellent work and support.
+* **Mark Lutz** for his *Learning Python* and *Programming Python* excellent books series and **John Goerzen** and **Brandon Rhodes** for their book *Foundations of Python Network Programming*. These books are wonderful tools for learning Python and this project owes a lot to them.
+* **JetBrains** for donating to this project the Open Source license of *PyCharm 3 Professional*.
+* **GitHub** for providing the *free source repository space and the tools* I use to develop this project.
+* The **Python Software Foundation** for providing support for the test lab infrastructure.
+
+
+Contact me
+----------
+
+For information and suggestions you can contact me at cannatag@gmail.com or you can also a support ticket on https://github.com/cannatag/ldap3/issues/new
+
+
+Changelog
+---------
+
+You can read the current changelog at http://ldap3.readthedocs.org/changelog.html
---
+++ packages/ldap3/ldap3-0.9.8.7.post1.tar.gz/ldap3-0.9.8.7.post1/test/testBindOperation.py
@@ -0,0 +1,74 @@
+# Created on 2013.06.06
+#
+# @author: Giovanni Cannata
+#
+# Copyright 2015 Giovanni Cannata
+#
+# This file is part of ldap3.
+#
+# ldap3 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ldap3 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ldap3 in the COPYING and COPYING.LESSER files.
+# If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from ldap3 import ANONYMOUS, SASL, NTLM
+from test import test_sasl_user, test_sasl_password, random_id, get_connection, drop_connection, test_sasl_realm, test_server_type, \
+    test_ntlm_user, test_ntlm_password
+
+testcase_id = random_id()
+
+
+class Test(unittest.TestCase):
+    def test_bind_clear_text(self):
+        connection = get_connection(bind=False)
+        connection.open()
+        connection.bind()
+        self.assertTrue(connection.bound)
+        drop_connection(connection)
+        self.assertFalse(connection.bound)
+
+    def test_bind_anonymous(self):
+        connection = get_connection(bind=False, lazy_connection=False, authentication=ANONYMOUS)
+        connection.open()
+        connection.bind()
+        self.assertTrue(connection.bound)
+        drop_connection(connection)
+        self.assertFalse(connection.bound)
+
+    def test_bind_sasl_digest_md5(self):
+        connection = get_connection(bind=False, authentication=SASL, sasl_mechanism='DIGEST-MD5', sasl_credentials=(test_sasl_realm, test_sasl_user, test_sasl_password, None))
+        connection.open()
+        connection.bind()
+        self.assertTrue(connection.bound)
+        if test_server_type == 'EDIR':
+            connected_user = connection.extend.novell.get_bind_dn()
+            for component in test_sasl_user.split('.'):
+                self.assertTrue(component in connected_user)
+        else:
+            connected_user = str(connection.extend.standard.who_am_i())
+            self.assertEqual(connected_user, test_sasl_user)
+        drop_connection(connection)
+        self.assertFalse(connection.bound)
+
+    def test_ntlm(self):
+        if test_server_type == 'AD':
+            connection = get_connection(bind=False, authentication=NTLM, ntlm_credentials=(test_ntlm_user, test_ntlm_password))
+            connection.open()
+            connection.bind()
+            self.assertTrue(connection.bound)
+            connected_user = str(connection.extend.standard.who_am_i())[2:]
+            self.assertEqual(connected_user, test_ntlm_user)
+            drop_connection(connection)
+            self.assertFalse(connection.bound)
+
