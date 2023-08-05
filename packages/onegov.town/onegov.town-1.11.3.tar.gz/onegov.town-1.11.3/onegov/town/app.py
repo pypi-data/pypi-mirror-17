558c75e7bc93d51dd7cfc30211d8fdaa5bd958c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:25 +0000

    Add package onegov.town/1.11.3/onegov.town-1.11.3.tar.gz

---
+++ packages/onegov.town/onegov.town-1.11.3.tar.gz/onegov.town-1.11.3/onegov/town/app.py
@@ -0,0 +1,48 @@
+""" Contains the application which builds on onegov.core and uses
+more.chameleon.
+
+It's possible that the chameleon integration is moved to onegov.core in the
+future, but for now it is assumed that different applications may want to
+use different templating languages.
+
+"""
+
+from onegov.core import utils
+from onegov.org import OrgApp
+from onegov.org.app import get_i18n_localedirs as get_org_i18n_localedirs
+from onegov.town.theme import TownTheme
+
+
+class TownApp(OrgApp):
+    pass
+
+
+@TownApp.static_directory()
+def get_static_directory():
+    return 'static'
+
+
+@TownApp.template_directory()
+def get_template_directory():
+    return 'templates'
+
+
+@TownApp.setting(section='core', name='theme')
+def get_theme():
+    return TownTheme()
+
+
+@TownApp.setting(section='org', name='enable_user_registration')
+def get_enable_user_registration():
+    return False
+
+
+@TownApp.setting(section='org', name='enable_yubikey')
+def get_enable_yubikey():
+    return True
+
+
+@TownApp.setting(section='i18n', name='localedirs')
+def get_i18n_localedirs():
+    return [utils.module_path('onegov.town', 'locale')] \
+        + get_org_i18n_localedirs()
---
+++ packages/onegov.town/onegov.town-1.11.3.tar.gz/onegov.town-1.11.3/onegov/town/theme/town_theme.py
@@ -0,0 +1,45 @@
+from collections import OrderedDict
+
+from onegov.core.utils import module_path
+from onegov.org.theme import OrgTheme
+
+
+# options editable by the user
+user_options = {
+    'primary-color': '#006fba',
+    'footer-height': '38px'
+}
+
+
+class TownTheme(OrgTheme):
+    name = 'onegov.town.foundation'
+
+    # don't touch this number, it's incremented using bumpversion, so every
+    # release will automatically trigger a rebuild of the theme
+    version = '1.11.3'
+
+    @property
+    def default_options(self):
+        options = super().default_options
+        options.update(OrderedDict((
+            ('tile-image-1', '"../static/homepage-images/tile-1-small.jpg"'),
+            ('tile-image-2', '"../static/homepage-images/tile-2-small.jpg"'),
+            ('tile-image-3', '"../static/homepage-images/tile-3-small.jpg"'),
+            ('tile-image-4', '"../static/homepage-images/tile-4-small.jpg"'),
+            ('tile-image-5', '"../static/homepage-images/tile-5-small.jpg"'),
+            ('tile-image-6', '"../static/homepage-images/tile-6-small.jpg"'),
+        )))
+        options.update(user_options)
+
+        return options
+
+    @property
+    def post_imports(self):
+        return super().post_imports + [
+            'town'
+        ]
+
+    @property
+    def extra_search_paths(self):
+        base_paths = super().extra_search_paths
+        return [module_path('onegov.town.theme', 'styles')] + base_paths
