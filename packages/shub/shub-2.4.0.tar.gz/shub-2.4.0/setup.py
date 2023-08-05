76d8dc285deecba8790ae3deb192f56328784c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:40 +0000

    Add package shub/2.4.0/shub-2.4.0.tar.gz

---
+++ packages/shub/shub-2.4.0.tar.gz/shub-2.4.0/setup.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+from setuptools import setup
+
+
+setup(
+    name='shub',
+    version='2.4.0',
+    packages=['shub'],
+    url='http://doc.scrapinghub.com/shub.html',
+    description='Scrapinghub Command Line Client',
+    long_description=open('README.rst').read(),
+    author='Scrapinghub',
+    author_email='info@scrapinghub.com',
+    maintainer='Scrapinghub',
+    maintainer_email='info@scrapinghub.com',
+    license='BSD',
+    entry_points={
+        'console_scripts': ['shub = shub.tool:cli']
+    },
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['click', 'hubstorage', 'pip', 'requests', 'ruamel.yaml',
+                      'scrapinghub', 'six'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    test_suite='tests',
+    tests_require=['mock']
+)
