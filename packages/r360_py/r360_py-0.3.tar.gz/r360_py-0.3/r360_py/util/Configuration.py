2fa00594b818076e11aa21e1ed2515c427e2cf32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:01 +0000

    Add package r360_py/0.3/r360_py-0.3.tar.gz

---
+++ packages/r360_py/r360_py-0.3.tar.gz/r360_py-0.3/r360_py/util/Configuration.py
@@ -0,0 +1,16 @@
+class Configuration:
+    'This class represents a single configuration, build from traveloptions, for a request'
+
+    @staticmethod
+    def build(travelOptions):
+        
+        return {
+            "sources": travelOptions.getSources(),
+            "polygon": {
+              "values": travelOptions.getTravelTimes(),
+              "intersectionMode": "union",
+              "serializer": travelOptions.getPolygonSerializationType().value,
+              "pointReduction": True,
+              "minPolygonHoleSize": travelOptions.getMinPolygonHoleSize()
+            }
+        };
