a8f7140b382f965f17656767fee73fef745d86ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:11 +0000

    Add package discord_simple/0.0.1.16/discord_simple-0.0.1.16.tar.gz

---
+++ packages/discord_simple/discord_simple-0.0.1.16.tar.gz/discord_simple-0.0.1.16/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+def requirements():
+  """Build the requirements list for this project"""
+  requirements_list = []
+  with open('requirements.txt') as requirements:
+    for install in requirements:
+      requirements_list.append(install.strip())
+  return requirements_list
+setup(name='discord_simple',
+  version='0.0.1.16',
+  license='LGPLv3',
+  description='Discord simple API',
+  author='Alexander N. Skovpen',
+  author_email='a.n.skovpen@gmail.com',
+  url='https://github.com/askovpen/discord_simple/',
+  install_requires=requirements(),
+  packages=find_packages(exclude=['tests*']),
+  include_package_data=True,
+  extras_require={
+    'json': 'ujson',
+  },
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.4',
+  ]
+)
