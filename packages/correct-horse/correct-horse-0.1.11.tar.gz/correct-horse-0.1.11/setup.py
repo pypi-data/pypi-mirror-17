b8eed883c9c6c8c27720e9b12d1a1042471b3ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:23 +0000

    Add package correct-horse/0.1.11/correct-horse-0.1.11.tar.gz

---
+++ packages/correct-horse/correct-horse-0.1.11.tar.gz/correct-horse-0.1.11/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+def slurp(fname):
+    with open(fname,"rt") as fi:
+        return fi.read()
+
+setup(
+    name='correct-horse',
+    version=slurp("VERSION").strip(),
+    description='Print strong, memorable passphrases',
+    url='https://github.com/bbiw/correct-horse',
+    author='Terrel Shumway',
+    long_description=slurp("description.rst"),
+    classifiers=[
+        'Environment :: Console',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Security',
+    ],
+    author_email='algae-ester-1925-riga-seth@shumway.us',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        chbs=correcthorse:cli
+    ''',
+)
