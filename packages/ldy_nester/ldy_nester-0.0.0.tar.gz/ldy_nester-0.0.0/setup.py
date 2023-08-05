9a1ee8a390e24b4f49815cd37401a45b9a6bee76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:51 +0000

    Add package ldy_nester/0.0.0/ldy_nester-0.0.0.tar.gz

---
+++ packages/ldy_nester/ldy_nester-0.0.0.tar.gz/ldy_nester-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name          = 'ldy_nester',
+	vertion       = '1.0.0',
+	py_modules    = ['ldy_nester'],
+	auther        = 'ldy',
+	auther_email  = 'ldy.zzuli.enu.cn',
+	url           = 'http://www.zzuli.edu.cn',
+	description   = 'A simple printer of ldy_nester lists',
+     )
+
