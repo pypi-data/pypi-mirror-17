beff58bdd789ab5b45541872235a2a2d33a1f7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:03 +0000

    Add package rnnlm-python/0.1.2/rnnlm-python-0.1.2.tar.gz

---
+++ packages/rnnlm-python/rnnlm-python-0.1.2.tar.gz/rnnlm-python-0.1.2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup, Extension
+
+try:
+    with open("README.rst") as f:
+        long_description = f.read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="rnnlm-python",
+    version="0.1.2",
+    author="Hiroki Teranishi",
+    author_email="teranishihiroki@gmail.com",
+    description="python wrapper for RNNLM Toolkit (http://rnnlm.org/)",
+    long_description=long_description,
+    url="https://github.com/chantera/rnnlm-python",
+    license="MIT",
+    py_modules=["rnnlm"],
+    ext_modules=[
+        Extension(
+            "_rnnlm",
+            sources=["./rnnlm-python/rnnlm_wrap.cxx", "./rnnlm/rnnlmlib.cpp"]
+        )
+    ],
+    packages=["rnnlm-python"],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+)
