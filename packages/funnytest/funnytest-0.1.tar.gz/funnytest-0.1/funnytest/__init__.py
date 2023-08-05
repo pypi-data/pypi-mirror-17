24035b168864aec30be80ba07b3a08ad87d7822d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package funnytest/0.1/funnytest-0.1.tar.gz

---
+++ packages/funnytest/funnytest-0.1.tar.gz/funnytest-0.1/funnytest/__init__.py
@@ -0,0 +1,2 @@
+def joke():
+    return "It's a joke"
---
+++ packages/funnytest/funnytest-0.1.tar.gz/funnytest-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="funnytest",
+    version="0.1",
+    description="The funnytest is something",
+    author="Yank Lu",
+    author_email="yanklu@gmail.com",
+    license="MIT",
+    packages=["funnytest"],
+    zip_safe=False
+    )
