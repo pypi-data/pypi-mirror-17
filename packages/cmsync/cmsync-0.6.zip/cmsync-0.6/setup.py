fa251eb4d9ff0314b47fe1854af538ea7a18f94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:18 +0000

    Add package cmsync/0.6/cmsync-0.6.zip

---
+++ packages/cmsync/cmsync-0.6.zip/cmsync-0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='cmsync',
+    version='0.6',
+    author='Wilson Santos',
+    author_email='wilson@codeminus.org',
+    description='A simple module to sync local folders that can run commands after the transfer.',
+
+    license='Apache License 2.0',
+    keywords='codeminus sync transfer',
+    url='https://github.com/codeminus/cmsync',
+    packages=find_packages(),
+    scripts=[
+        'bin/cmsync'
+    ],
+    install_requires=[
+        'colorama'
+    ]
+)
