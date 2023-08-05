4af3a72d0fd7036e5df5dc340b18f208c7879de7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:19 +0000

    Add package LightMagic/0.1.4/LightMagic-0.1.4.tar.gz

---
+++ packages/LightMagic/LightMagic-0.1.4.tar.gz/LightMagic-0.1.4/LightMagic/types/Json.py
@@ -0,0 +1,37 @@
+import json
+
+from bson import json_util
+
+from ._Base import _Base
+
+
+class Json(_Base):
+    """
+        Работа с Json
+    """
+
+    def _validate(self, obj, value):
+        """
+            Проверяем корректность входных данных
+        """
+        if isinstance(value, dict):
+            return value
+        elif isinstance(value, list):
+            return value
+        # Разворачиваем
+        elif isinstance(value, str):
+            return json.loads(value, object_hook=json_util.object_hook)
+        else:
+            raise ValueError
+
+    @staticmethod
+    def get_db_type():
+        return 'json'
+
+    @staticmethod
+    def db_serialize(value):
+        """ Трансформирование объекта в БД. Необходимо для преобразования сложных типов данных. """
+        if isinstance(value, (dict, list)):
+            return json.dumps(value, default=json_util.default)
+        else:
+            return value
