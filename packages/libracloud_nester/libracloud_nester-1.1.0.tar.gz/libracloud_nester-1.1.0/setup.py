d48bc456ac5e67635c354945a2b0cf91786cb81a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:34 +0000

    Add package libracloud_nester/1.1.0/libracloud_nester-1.1.0.tar.gz

---
+++ packages/libracloud_nester/libracloud_nester-1.1.0.tar.gz/libracloud_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'libracloud_nester',
+	version		= '1.1.0',
+	py_modules	= ['libracloud_nester'], 
+	author		= 'Max',
+	author_email	= 'max@libracloud.com',
+	url		= 'http://www.libracloud.com',
+	description	= 'A simple printer of nested lists',
+     ) 
