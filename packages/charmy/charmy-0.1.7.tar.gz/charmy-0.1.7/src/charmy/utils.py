ef18e67e915d7062d24f9ecef24cf86fe85ad218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:56 +0000

    Add package charmy/0.1.7/charmy-0.1.7.tar.gz

---
+++ packages/charmy/charmy-0.1.7.tar.gz/charmy-0.1.7/src/charmy/utils.py
@@ -0,0 +1,26 @@
+import platform
+
+from .constants import PLATFORM_LINUX
+from .installers.linux.ubuntu import UID as LINUX_UBUNTU_UID
+from .installers.linux.ubuntu.installer import UbuntuInstaller
+from .installers.linux.other.installer import OtherLinuxInstaller
+
+__title__ = 'charmy.utils'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2015-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('get_installer',)
+
+def get_installer():
+    """Get installer.
+
+    :return:
+    """
+    current_platform = platform.uname()
+
+    if current_platform[0] == PLATFORM_LINUX:
+        dist = platform.dist()
+        if dist[0] == LINUX_UBUNTU_UID:
+            return UbuntuInstaller()
+        else:
+            return OtherLinuxInstaller()
