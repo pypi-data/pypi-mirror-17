cf48d1246a42552bfb56f97f0429cf6ddd3d9dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:15 +0000

    Add package nester_madman/1.1.0/nester_madman-1.1.0.tar.gz

---
+++ packages/nester_madman/nester_madman-1.1.0.tar.gz/nester_madman-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = "nester_madman",
+    version = "1.1.0",
+    py_modules = ['nester'],
+    author = 'hfpython',
+    author_email = 'army650663@gmail.com',
+    url = "http://www.headfirstlabs.com",
+    description = 'A simple printer of nested lists',
+    )
+    
