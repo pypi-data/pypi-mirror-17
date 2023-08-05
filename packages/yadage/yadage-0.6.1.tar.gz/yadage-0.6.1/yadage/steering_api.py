86ad2c04ccf35dd7c6cb68287b07f663a4eb8dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:11 +0000

    Add package yadage/0.6.1/yadage-0.6.1.tar.gz

---
+++ packages/yadage/yadage-0.6.1.tar.gz/yadage-0.6.1/yadage/steering_api.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+import adage
+import adage.backends
+import logging
+import os
+import json
+import workflow_loader
+from yadage.yadagemodels import YadageWorkflow
+import visualize
+import interactive
+import  packtivity.statecontexts.poxisfs_context as statecontext
+
+
+log = logging.getLogger(__name__)
+
+def run_workflow(
+    workdir,
+    workflow,
+    initdata,
+    loadtoplevel,
+    loginterval,
+    schemadir,
+    backend,
+    user_interaction = False,
+    validate = True
+    ):
+    """
+    Main entry point to run a Yadage workflow
+    """
+
+    log.info('running yadage workflow %s',workflow)
+    rootcontext = statecontext.make_new_context(workdir)
+
+    for k,v in initdata.iteritems():
+        candpath = '{}/inputs/{}'.format(workdir,v)
+        if os.path.exists(candpath):
+            initdata[k] = '{}/inputs/{}'.format(rootcontext['readwrite'][0],v)
+
+    workflow_json = workflow_loader.workflow(
+        workflow,
+        toplevel = loadtoplevel,
+        schemadir = schemadir,
+        validate = validate
+    )
+    workflow = YadageWorkflow.createFromJSON(workflow_json,rootcontext)
+    workflow.view().init(initdata)
+
+    yadagedir = '{}/_yadage/'.format(workdir)
+    os.makedirs(yadagedir)
+
+    with open('{}/yadage_instance_before.json'.format(yadagedir),'w') as f:
+        json.dump(workflow.json(),f)
+
+    if user_interaction:
+        extend,submit = interactive.interactive_deciders()
+        interactive_kwargs = {
+            'extend_decider': extend,
+            'submit_decider': submit
+        }
+    else:
+        interactive_kwargs = {}
+
+    adage.rundag(workflow,
+                 track = True,
+                 backend = backend,
+                 update_interval = 0.02,
+                 trackevery = loginterval,
+                 workdir = '{}/_adage'.format(workdir),
+                 **interactive_kwargs
+                )
+
+    with open('{}/yadage_instance.json'.format(yadagedir),'w') as f:
+        json.dump(workflow.json(),f)
+    with open('{}/yadage_template.json'.format(yadagedir),'w') as f:
+        json.dump(workflow_json,f)
+
+    visualize.write_prov_graph(yadagedir,workflow)
+    log.info('finished yadage workflow %s',workflow)
