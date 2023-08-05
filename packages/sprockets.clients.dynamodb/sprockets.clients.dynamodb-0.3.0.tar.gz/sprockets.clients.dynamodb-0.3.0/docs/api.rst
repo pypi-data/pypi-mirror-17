6d4ba7cfeee5c9d007ef9e19e76d829b033f5def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:08 +0000

    Add package sprockets.clients.dynamodb/0.3.0/sprockets.clients.dynamodb-0.3.0.tar.gz

---
+++ packages/sprockets.clients.dynamodb/sprockets.clients.dynamodb-0.3.0.tar.gz/sprockets.clients.dynamodb-0.3.0/docs/api.rst
@@ -0,0 +1,5 @@
+API Documentation
+=================
+
+.. autoclass:: sprockets.clients.dynamodb.DynamoDB
+   :members:
---
+++ packages/sprockets.clients.dynamodb/sprockets.clients.dynamodb-0.3.0.tar.gz/sprockets.clients.dynamodb-0.3.0/sprockets/clients/dynamodb/__init__.py
@@ -0,0 +1,16 @@
+try:
+    from .connector import DynamoDB
+except ImportError as error:
+    def DynamoDB(*args, **kwargs):
+        raise error
+
+version_info = (0, 3, 0)
+__version__ = '.'.join(str(v) for v in version_info)
+
+# Response constants
+TABLE_ACTIVE = 'ACTIVE'
+TABLE_CREATING = 'CREATING'
+TABLE_DELETING = 'DELETING'
+TABLE_DISABLED = 'DISABLED'
+TABLE_UPDATING = 'UPDATING'
+
