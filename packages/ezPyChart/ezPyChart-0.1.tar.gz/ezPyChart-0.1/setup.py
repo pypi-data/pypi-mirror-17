3f12bd28800fb896a4b98100cca667fe5e05c640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package ezPyChart/0.1/ezPyChart-0.1.tar.gz

---
+++ packages/ezPyChart/ezPyChart-0.1.tar.gz/ezPyChart-0.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ezPyChart',
+      version='0.1',
+      description='A command line tool to create pie charts from simple tabular data',
+      author='Cristian Esquivias',
+      url='https://github.com/cesquivias/ezpychart',
+      license='GPLv3+',
+      scripts=['bin/ezpychart'])
