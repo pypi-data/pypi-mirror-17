d00cf3b31a2c335bf1ae740c97b2e4304224a418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:13 +0000

    Add package bitcodin/1.6.7/bitcodin-1.6.7-py3-none-any.whl

---
+++ packages/bitcodin/bitcodin-1.6.7-py3-none-any.whl/bitcodin/__init__.py
@@ -0,0 +1,10 @@
+__author__ = 'dmoser'
+
+# Configuration variables
+
+api_key = None
+api_base = 'http://portal.bitcodin.com/api'
+api_version = None
+
+from bitcodin.resource import *
+from bitcodin.api import *
