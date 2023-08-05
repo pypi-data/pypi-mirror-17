b116fc845bcd4956c7bd41db962400aea581fed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:53 +0000

    Add package nova-powervm/3.0.0.0b3/nova_powervm-3.0.0.0b3-py2-none-any.whl

---
+++ packages/nova-powervm/nova_powervm-3.0.0.0b3-py2-none-any.whl/nova_powervm/tests/virt/powervm/tasks/test_network.py
@@ -0,0 +1,346 @@
+# Copyright 2015, 2016 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import eventlet
+import mock
+
+from nova import exception
+from nova import objects
+from nova import test
+from pypowervm.tests import test_fixtures as pvm_fx
+
+from nova_powervm.tests.virt import powervm
+from nova_powervm.virt.powervm.tasks import network as tf_net
+
+
+def cna(mac):
+    """Builds a mock Client Network Adapter for unit tests."""
+    nic = mock.MagicMock()
+    nic.mac = mac
+    nic.vswitch_uri = 'fake_href'
+    return nic
+
+
+class TestNetwork(test.TestCase):
+    def setUp(self):
+        super(TestNetwork, self).setUp()
+        self.flags(host='host1')
+        self.apt = self.useFixture(pvm_fx.AdapterFx()).adpt
+
+        self.mock_lpar_wrap = mock.MagicMock()
+        self.mock_lpar_wrap.can_modify_io.return_value = True, None
+
+    @mock.patch('nova_powervm.virt.powervm.vif.unplug')
+    @mock.patch('nova_powervm.virt.powervm.vm.get_cnas')
+    def test_unplug_vifs(self, mock_vm_get, mock_unplug):
+        """Tests that a delete of the vif can be done."""
+        inst = objects.Instance(**powervm.TEST_INSTANCE)
+
+        # Mock up the CNA responses.
+        cnas = [cna('AABBCCDDEEFF'), cna('AABBCCDDEE11'), cna('AABBCCDDEE22')]
+        mock_vm_get.return_value = cnas
+
+        # Mock up the network info.  This also validates that they will be
+        # sanitized to upper case.
+        net_info = [
+            {'address': 'aa:bb:cc:dd:ee:ff'}, {'address': 'aa:bb:cc:dd:ee:22'},
+            {'address': 'aa:bb:cc:dd:ee:33'}
+        ]
+
+        # Mock out the vif driver
+        def validate_unplug(adapter, host_uuid, instance, vif,
+                            slot_mgr, cna_w_list=None):
+            self.assertEqual(adapter, self.apt)
+            self.assertEqual('host_uuid', host_uuid)
+            self.assertEqual(instance, inst)
+            self.assertIn(vif, net_info)
+            self.assertEqual('slot_mgr', slot_mgr)
+            self.assertEqual(cna_w_list, cnas)
+
+        mock_unplug.side_effect = validate_unplug
+
+        # Run method
+        p_vifs = tf_net.UnplugVifs(self.apt, inst, net_info, 'host_uuid',
+                                   'slot_mgr')
+        p_vifs.execute(self.mock_lpar_wrap)
+
+        # Make sure the unplug was invoked, so that we know that the validation
+        # code was called
+        self.assertEqual(3, mock_unplug.call_count)
+
+    def test_unplug_vifs_invalid_state(self):
+        """Tests that the delete raises an exception if bad VM state."""
+        inst = objects.Instance(**powervm.TEST_INSTANCE)
+
+        # Mock that the state is incorrect
+        self.mock_lpar_wrap.can_modify_io.return_value = False, 'bad'
+
+        # Run method
+        p_vifs = tf_net.UnplugVifs(self.apt, inst, mock.Mock(), 'host_uuid',
+                                   'slot_mgr')
+        self.assertRaises(tf_net.VirtualInterfaceUnplugException,
+                          p_vifs.execute, self.mock_lpar_wrap)
+
+    @mock.patch('nova_powervm.virt.powervm.vif.plug')
+    @mock.patch('nova_powervm.virt.powervm.vm.get_cnas')
+    def test_plug_vifs_rmc(self, mock_vm_get, mock_plug):
+        """Tests that a crt vif can be done with secure RMC."""
+        inst = objects.Instance(**powervm.TEST_INSTANCE)
+
+        # Mock up the CNA response.  One should already exist, the other
+        # should not.
+        mock_vm_get.return_value = [cna('AABBCCDDEEFF'), cna('AABBCCDDEE11')]
+
+        # Mock up the network info.  This also validates that they will be
+        # sanitized to upper case.
+        net_info = [
+            {'address': 'aa:bb:cc:dd:ee:ff'}, {'address': 'aa:bb:cc:dd:ee:22'},
+            {'address': 'aa:bb:cc:dd:ee:33'}
+        ]
+
+        # Run method
+        p_vifs = tf_net.PlugVifs(mock.MagicMock(), self.apt, inst, net_info,
+                                 'host_uuid', 'slot_mgr')
+
+        p_vifs.execute(self.mock_lpar_wrap)
+
+        # new vif should be created twice.
+        mock_plug.assert_any_call(self.apt, 'host_uuid', inst, net_info[0],
+                                  'slot_mgr', new_vif=False)
+        mock_plug.assert_any_call(self.apt, 'host_uuid', inst, net_info[1],
+                                  'slot_mgr', new_vif=True)
+        mock_plug.assert_any_call(self.apt, 'host_uuid', inst, net_info[2],
+                                  'slot_mgr', new_vif=True)
+
+    @mock.patch('nova_powervm.virt.powervm.vif.plug')
+    @mock.patch('nova_powervm.virt.powervm.vm.get_cnas')
+    def test_plug_vifs_rmc_no_create(self, mock_vm_get, mock_plug):
+        """Verifies if no creates are needed, none are done."""
+        inst = objects.Instance(**powervm.TEST_INSTANCE)
+
+        # Mock up the CNA response.  Both should already exist.
+        mock_vm_get.return_value = [cna('AABBCCDDEEFF'), cna('AABBCCDDEE11')]
+
+        # Mock up the network info.  This also validates that they will be
+        # sanitized to upper case.
+        net_info = [
+            {'address': 'aa:bb:cc:dd:ee:ff'}, {'address': 'aa:bb:cc:dd:ee:11'}
+        ]
+
+        # Run method
+        p_vifs = tf_net.PlugVifs(mock.MagicMock(), self.apt, inst, net_info,
+                                 'host_uuid', 'slot_mgr')
+        p_vifs.execute(self.mock_lpar_wrap)
+
+        # The create should have been called with new_vif as False.
+        mock_plug.assert_called_with(
+            self.apt, 'host_uuid', inst, net_info[1],
+            'slot_mgr', new_vif=False)
+
+    @mock.patch('nova_powervm.virt.powervm.vif.plug')
+    @mock.patch('nova_powervm.virt.powervm.vm.get_cnas')
+    def test_plug_vifs_invalid_state(self, mock_vm_get, mock_plug):
+        """Tests that a crt_vif fails when the LPAR state is bad."""
+        inst = objects.Instance(**powervm.TEST_INSTANCE)
+
+        # Mock up the CNA response.  Only doing one for simplicity
+        mock_vm_get.return_value = []
+        net_info = [{'address': 'aa:bb:cc:dd:ee:ff'}]
+
+        # Mock that the state is incorrect
+        self.mock_lpar_wrap.can_modify_io.return_value = False, 'bad'
+
+        # Run method
+        p_vifs = tf_net.PlugVifs(mock.MagicMock(), self.apt, inst, net_info,
+                                 'host_uuid', 'slot_mgr')
+        self.assertRaises(exception.VirtualInterfaceCreateException,
+                          p_vifs.execute, self.mock_lpar_wrap)
+
+        # The create should not have been invoked
+        self.assertEqual(0, mock_plug.call_count)
+
+    @mock.patch('nova_powervm.virt.powervm.vif.plug')
+    @mock.patch('nova_powervm.virt.powervm.vm.get_cnas')
+    def test_plug_vifs_timeout(self, mock_vm_get, mock_plug):
+        """Tests that crt vif failure via loss of neutron callback."""
+        inst = objects.Instance(**powervm.TEST_INSTANCE)
+
+        # Mock up the CNA response.  Only doing one for simplicity
+        mock_vm_get.return_value = [cna('AABBCCDDEE11')]
+
+        # Mock up the network info.
+        net_info = [{'address': 'aa:bb:cc:dd:ee:ff'}]
+
+        # Ensure that an exception is raised by a timeout.
+        mock_plug.side_effect = eventlet.timeout.Timeout()
+
+        # Run method
+        p_vifs = tf_net.PlugVifs(mock.MagicMock(), self.apt, inst, net_info,
+                                 'host_uuid', 'slot_mgr')
+        self.assertRaises(exception.VirtualInterfaceCreateException,
+                          p_vifs.execute, self.mock_lpar_wrap)
+
+        # The create should have only been called once.
+        self.assertEqual(1, mock_plug.call_count)
+
+    @mock.patch('nova_powervm.virt.powervm.vif.plug')
+    @mock.patch('nova_powervm.virt.powervm.vm.get_cnas')
+    def test_plug_vifs_diff_host(self, mock_vm_get, mock_plug):
+        """Tests that crt vif handles bad inst.host value."""
+        inst = powervm.TEST_INST1
+
+        # Set this up as a different host from the inst.host
+        self.flags(host='host2')
+
+        # Mock up the CNA response.  Only doing one for simplicity
+        mock_vm_get.return_value = [cna('AABBCCDDEE11')]
+
+        # Mock up the network info.
+        net_info = [{'address': 'aa:bb:cc:dd:ee:ff'}]
+
+        # Run method
+        p_vifs = tf_net.PlugVifs(mock.MagicMock(), self.apt, inst, net_info,
+                                 'host_uuid', 'slot_mgr')
+        with mock.patch.object(inst, 'save') as mock_inst_save:
+            p_vifs.execute(self.mock_lpar_wrap)
+
+        # The create should have only been called once.
+        self.assertEqual(1, mock_plug.call_count)
+        # Should have called save to save the new host and then changed it back
+        self.assertEqual(2, mock_inst_save.call_count)
+        self.assertEqual('host1', inst.host)
+
+    @mock.patch('nova_powervm.virt.powervm.vif.plug')
+    @mock.patch('nova_powervm.virt.powervm.vm.get_cnas')
+    def test_plug_vifs_diff_host_except(self, mock_vm_get, mock_plug):
+        """Tests that crt vif handles bad inst.host value.
+
+        This test ensures that if we get a timeout exception we still reset
+        the inst.host value back to the original value
+        """
+        inst = powervm.TEST_INST1
+
+        # Set this up as a different host from the inst.host
+        self.flags(host='host2')
+
+        # Mock up the CNA response.  Only doing one for simplicity
+        mock_vm_get.return_value = [cna('AABBCCDDEE11')]
+
+        # Mock up the network info.
+        net_info = [{'address': 'aa:bb:cc:dd:ee:ff'}]
+
+        # Ensure that an exception is raised by a timeout.
+        mock_plug.side_effect = eventlet.timeout.Timeout()
+
+        # Run method
+        p_vifs = tf_net.PlugVifs(mock.MagicMock(), self.apt, inst, net_info,
+                                 'host_uuid', 'slot_mgr')
+        with mock.patch.object(inst, 'save') as mock_inst_save:
+            self.assertRaises(exception.VirtualInterfaceCreateException,
+                              p_vifs.execute, self.mock_lpar_wrap)
+
+        # The create should have only been called once.
+        self.assertEqual(1, mock_plug.call_count)
+        # Should have called save to save the new host and then changed it back
+        self.assertEqual(2, mock_inst_save.call_count)
+        self.assertEqual('host1', inst.host)
+
+    @mock.patch('nova_powervm.virt.powervm.vif.unplug')
+    @mock.patch('nova_powervm.virt.powervm.vif.plug')
+    @mock.patch('nova_powervm.virt.powervm.vm.get_cnas')
+    def test_plug_vifs_revert(self, mock_vm_get, mock_plug, mock_unplug):
+        """Tests that the revert flow works properly."""
+        inst = objects.Instance(**powervm.TEST_INSTANCE)
+
+        # Make a fake CNA list.  No real data needed, as the thing it calls
+        # into is mocked.
+        cna_list = []
+        mock_vm_get.return_value = cna_list
+
+        # Mock up the network info.  Three roll backs.
+        net_info = [
+            {'address': 'aa:bb:cc:dd:ee:ff'}, {'address': 'aa:bb:cc:dd:ee:22'},
+            {'address': 'aa:bb:cc:dd:ee:33'}
+        ]
+
+        # Make sure we test raising an exception
+        mock_unplug.side_effect = [None, exception.NovaException(), None]
+
+        # Run method
+        p_vifs = tf_net.PlugVifs(mock.MagicMock(), self.apt, inst, net_info,
+                                 'host_uuid', 'slot_mgr')
+        p_vifs.execute(self.mock_lpar_wrap)
+        p_vifs.revert(self.mock_lpar_wrap, mock.Mock(), mock.Mock())
+
+        # The unplug should be called three times.  The exception shouldn't
+        # stop the other calls.
+        self.assertEqual(3, mock_unplug.call_count)
+
+        # Make sure each call is invoked correctly.
+        c1 = mock.call(self.apt, 'host_uuid', inst, net_info[0],
+                       'slot_mgr', cna_w_list=cna_list)
+        c2 = mock.call(self.apt, 'host_uuid', inst, net_info[1],
+                       'slot_mgr', cna_w_list=cna_list)
+        c3 = mock.call(self.apt, 'host_uuid', inst, net_info[2],
+                       'slot_mgr', cna_w_list=cna_list)
+        mock_unplug.assert_has_calls([c1, c2, c3])
+
+    @mock.patch('nova_powervm.virt.powervm.vif.plug_secure_rmc_vif')
+    @mock.patch('nova_powervm.virt.powervm.vif.get_secure_rmc_vswitch')
+    @mock.patch('nova_powervm.virt.powervm.vif.plug')
+    @mock.patch('nova_powervm.virt.powervm.vm.get_cnas')
+    def test_plug_mgmt_vif(self, mock_vm_get, mock_plug,
+                           mock_get_rmc_vswitch, mock_plug_rmc_vif):
+        """Tests that a mgmt vif can be created."""
+        inst = objects.Instance(**powervm.TEST_INSTANCE)
+
+        # Mock up the rmc vswitch
+        vswitch_w = mock.MagicMock()
+        vswitch_w.href = 'fake_mgmt_uri'
+        mock_get_rmc_vswitch.return_value = vswitch_w
+
+        # Run method
+        p_vifs = tf_net.PlugMgmtVif(self.apt, inst, 'host_uuid', 'slot_mgr')
+        p_vifs.execute([])
+
+        # The create should have only been called once.
+        self.assertEqual(1, mock_plug_rmc_vif.call_count)
+
+    @mock.patch('nova.utils.is_neutron')
+    def test_get_vif_events(self, mock_is_neutron):
+        # Set up common mocks.
+        inst = objects.Instance(**powervm.TEST_INSTANCE)
+        net_info = [mock.MagicMock(), mock.MagicMock()]
+        net_info[0]['id'] = 'a'
+        net_info[0].get.return_value = False
+        net_info[1]['id'] = 'b'
+        net_info[1].get.return_value = True
+
+        # Set up the runner.
+        p_vifs = tf_net.PlugVifs(mock.MagicMock(), self.apt, inst, net_info,
+                                 'host_uuid', 'slot_mgr')
+        p_vifs.crt_network_infos = net_info
+
+        # Mock that neutron is off.
+        mock_is_neutron.return_value = False
+        self.assertEqual([], p_vifs._get_vif_events())
+
+        # Turn neutron on.
+        mock_is_neutron.return_value = True
+        resp = p_vifs._get_vif_events()
+
+        # Only one should be returned since only one was active.
+        self.assertEqual(1, len(resp))
---
+++ packages/nova-powervm/nova_powervm-3.0.0.0b3-py2-none-any.whl/nova_powervm/tests/virt/powervm/test_live_migration.py
@@ -0,0 +1,266 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+from __future__ import absolute_import
+
+import fixtures
+import mock
+
+from nova import exception
+from nova import objects
+from nova import test
+
+from nova_powervm.objects import migrate_data as mig_obj
+from nova_powervm.tests.virt import powervm
+from nova_powervm.tests.virt.powervm import fixtures as fx
+from nova_powervm.virt.powervm import live_migration as lpm
+
+
+class TestLPM(test.TestCase):
+    def setUp(self):
+        super(TestLPM, self).setUp()
+
+        self.flags(disk_driver='localdisk', group='powervm')
+        self.drv_fix = self.useFixture(fx.PowerVMComputeDriver())
+        self.drv = self.drv_fix.drv
+        self.apt = self.drv.adapter
+
+        self.inst = objects.Instance(**powervm.TEST_INSTANCE)
+
+        self.mig_data = mig_obj.PowerVMLiveMigrateData()
+        self.mig_data.host_mig_data = {}
+        self.mig_data.dest_ip = '1'
+        self.mig_data.dest_user_id = 'neo'
+        self.mig_data.dest_sys_name = 'a'
+        self.mig_data.public_key = 'PublicKey'
+        self.mig_data.dest_proc_compat = 'a,b,c'
+        self.mig_data.vol_data = {}
+
+        self.lpmsrc = lpm.LiveMigrationSrc(self.drv, self.inst, self.mig_data)
+        self.lpmdst = lpm.LiveMigrationDest(self.drv, self.inst)
+
+        self.add_key = self.useFixture(fixtures.MockPatch(
+            'pypowervm.tasks.management_console.add_authorized_key')).mock
+        self.get_key = self.useFixture(fixtures.MockPatch(
+            'pypowervm.tasks.management_console.get_public_key')).mock
+        self.get_key.return_value = 'PublicKey'
+
+    @mock.patch('pypowervm.tasks.storage.ScrubOrphanStorageForLpar')
+    @mock.patch('nova_powervm.virt.powervm.media.ConfigDrivePowerVM')
+    @mock.patch('nova_powervm.virt.powervm.vm.get_instance_wrapper')
+    @mock.patch('pypowervm.tasks.vterm.close_vterm')
+    @mock.patch('pypowervm.wrappers.managed_system.System.migration_data',
+                new_callable=mock.PropertyMock, name='MigDataProp')
+    def test_lpm_source(self, mock_migrdata, mock_vterm_close, mock_get_wrap,
+                        mock_cd, mock_scrub):
+        migr_data = {'active_migrations_supported': 4,
+                     'active_migrations_in_progress': 2}
+        mock_migrdata.return_value = migr_data
+
+        with mock.patch.object(
+            self.lpmsrc, '_check_migration_ready', return_value=None):
+
+            # Test the bad path first, then patch in values to make succeed
+            mock_wrap = mock.Mock(id=123)
+            mock_get_wrap.return_value = mock_wrap
+
+            self.assertRaises(exception.MigrationPreCheckError,
+                              self.lpmsrc.check_source, 'context',
+                              'block_device_info', [])
+
+            # Patch the proc compat fields, to get further
+            pm = mock.PropertyMock(return_value='b')
+            type(mock_wrap).proc_compat_mode = pm
+
+            self.assertRaises(exception.MigrationPreCheckError,
+                              self.lpmsrc.check_source, 'context',
+                              'block_device_info', [])
+
+            pm = mock.PropertyMock(return_value='Not_Migrating')
+            type(mock_wrap).migration_state = pm
+
+            # Get a volume driver.
+            mock_vol_drv = mock.MagicMock()
+
+            # Finally, good path.
+            self.lpmsrc.check_source('context', 'block_device_info',
+                                     [mock_vol_drv])
+            # Ensure we built a scrubber.
+            mock_scrub.assert_called_with(mock.ANY, 123)
+            # Ensure we added the subtasks to remove the vopts.
+            mock_cd.return_value.dlt_vopt.assert_called_once_with(
+                mock.ANY, stg_ftsk=mock_scrub.return_value,
+                remove_mappings=False)
+            # And ensure the scrubber was executed
+            mock_scrub.return_value.execute.assert_called_once_with()
+            mock_vol_drv.pre_live_migration_on_source.assert_called_once_with(
+                {})
+
+            # Ensure migration counts are validated
+            migr_data['active_migrations_in_progress'] = 4
+            self.assertRaises(exception.MigrationPreCheckError,
+                              self.lpmsrc.check_source, 'context',
+                              'block_device_info', [])
+
+            # Ensure the vterm was closed
+            mock_vterm_close.assert_called_once_with(
+                self.apt, mock_wrap.uuid)
+
+    @mock.patch('pypowervm.wrappers.managed_system.System.migration_data',
+                new_callable=mock.PropertyMock, name='MigDataProp')
+    def test_lpm_dest(self, mock_migrdata):
+        src_compute_info = {'stats': {'memory_region_size': 1}}
+        dst_compute_info = {'stats': {'memory_region_size': 1}}
+
+        migr_data = {'active_migrations_supported': 4,
+                     'active_migrations_in_progress': 2}
+        mock_migrdata.return_value = migr_data
+        with mock.patch.object(self.drv.host_wrapper, 'refresh') as mock_rfh:
+
+            self.lpmdst.check_destination(
+                'context', src_compute_info, dst_compute_info)
+            mock_rfh.assert_called_once_with()
+            self.assertEqual(2, mock_migrdata.call_count)
+
+            # Ensure migration counts are validated
+            migr_data['active_migrations_in_progress'] = 4
+            self.assertRaises(exception.MigrationPreCheckError,
+                              self.lpmdst.check_destination, 'context',
+                              src_compute_info, dst_compute_info)
+            # Repair the stat
+            migr_data['active_migrations_in_progress'] = 2
+
+            # Ensure diff memory sizes raises an exception
+            dst_compute_info['stats']['memory_region_size'] = 2
+            self.assertRaises(exception.MigrationPreCheckError,
+                              self.lpmdst.check_destination, 'context',
+                              src_compute_info, dst_compute_info)
+
+    @mock.patch('pypowervm.tasks.storage.ComprehensiveScrub')
+    def test_pre_live_mig(self, mock_scrub):
+        vol_drv = mock.MagicMock()
+        resp = self.lpmdst.pre_live_migration(
+            'context', 'block_device_info', 'network_info', 'disk_info',
+            self.mig_data, [vol_drv])
+
+        # Make sure we get something back, and that the volume driver was
+        # invoked.
+        self.assertIsNotNone(resp)
+        vol_drv.pre_live_migration_on_destination.assert_called_once_with(
+            self.mig_data.vol_data)
+        self.assertEqual(1, mock_scrub.call_count)
+        self.add_key.assert_called_once_with(self.apt, 'PublicKey')
+
+        vol_drv.reset_mock()
+        raising_vol_drv = mock.Mock()
+        raising_vol_drv.pre_live_migration_on_destination.side_effect = (
+            Exception('foo'))
+        self.assertRaises(
+            exception.MigrationPreCheckError, self.lpmdst.pre_live_migration,
+            'context', 'block_device_info', 'network_info', 'disk_info',
+            self.mig_data, [vol_drv, raising_vol_drv])
+        vol_drv.pre_live_migration_on_destination.assert_called_once_with({})
+        (raising_vol_drv.pre_live_migration_on_destination.
+            assert_called_once_with({}))
+
+    def test_src_cleanup(self):
+        vol_drv = mock.Mock()
+        self.lpmdst.pre_live_vol_data = {}
+        self.lpmdst.cleanup_volume(vol_drv)
+        # Ensure the volume driver was called to clean up the volume.
+        vol_drv.cleanup_volume_at_destination.assert_called_once_with({})
+
+    @mock.patch('pypowervm.tasks.migration.migrate_lpar')
+    def test_live_migration(self, mock_migr):
+
+        self.lpmsrc.lpar_w = mock.Mock()
+        self.lpmsrc.live_migration('context', self.mig_data)
+        mock_migr.assert_called_once_with(
+            self.lpmsrc.lpar_w, 'a', sdn_override=True, tgt_mgmt_svr='1',
+            tgt_mgmt_usr='neo', validate_only=False,
+            virtual_fc_mappings=None, virtual_scsi_mappings=None,
+            vlan_check_override=True)
+
+        # Test that we raise errors received during migration
+        mock_migr.side_effect = ValueError()
+        self.assertRaises(ValueError, self.lpmsrc.live_migration, 'context',
+                          self.mig_data)
+        mock_migr.assert_called_with(
+            self.lpmsrc.lpar_w, 'a', sdn_override=True, tgt_mgmt_svr='1',
+            tgt_mgmt_usr='neo', validate_only=False,
+            virtual_fc_mappings=None, virtual_scsi_mappings=None,
+            vlan_check_override=True)
+
+    @mock.patch('nova_powervm.virt.powervm.vif.post_live_migrate_at_source')
+    def test_post_live_mig_src(self, mock_post_migrate_vif):
+        network_infos = [mock.Mock(), mock.Mock()]
+        self.lpmsrc.post_live_migration_at_source(network_infos)
+        self.assertEqual(2, mock_post_migrate_vif.call_count)
+
+    @mock.patch('nova_powervm.virt.powervm.vif.'
+                'post_live_migrate_at_destination')
+    def test_post_live_mig_dest(self, mock_post_migrate_vif):
+        network_infos = [mock.Mock(), mock.Mock()]
+        self.lpmdst.post_live_migration_at_destination(network_infos, [])
+        self.assertEqual(2, mock_post_migrate_vif.call_count)
+
+    @mock.patch('pypowervm.tasks.migration.migrate_recover')
+    def test_rollback(self, mock_migr):
+        self.lpmsrc.lpar_w = mock.Mock()
+
+        # Test no need to rollback
+        self.lpmsrc.lpar_w.migration_state = 'Not_Migrating'
+        self.lpmsrc.rollback_live_migration('context')
+        self.assertTrue(self.lpmsrc.lpar_w.refresh.called)
+        self.assertFalse(mock_migr.called)
+
+        # Test calling the rollback
+        self.lpmsrc.lpar_w.reset_mock()
+        self.lpmsrc.lpar_w.migration_state = 'Pretend its Migrating'
+        self.lpmsrc.rollback_live_migration('context')
+        self.assertTrue(self.lpmsrc.lpar_w.refresh.called)
+        mock_migr.assert_called_once_with(self.lpmsrc.lpar_w, force=True)
+
+        # Test exception from rollback
+        mock_migr.reset_mock()
+        self.lpmsrc.lpar_w.reset_mock()
+        mock_migr.side_effect = ValueError()
+        self.lpmsrc.rollback_live_migration('context')
+        self.assertTrue(self.lpmsrc.lpar_w.refresh.called)
+        mock_migr.assert_called_once_with(self.lpmsrc.lpar_w, force=True)
+
+    def test_check_migration_ready(self):
+        lpar_w, host_w = mock.Mock(), mock.Mock()
+        lpar_w.can_lpm.return_value = (True, None)
+        self.lpmsrc._check_migration_ready(lpar_w, host_w)
+        lpar_w.can_lpm.assert_called_once_with(host_w, migr_data={})
+
+        lpar_w.can_lpm.return_value = (False, 'This is the reason message.')
+        self.assertRaises(exception.MigrationPreCheckError,
+                          self.lpmsrc._check_migration_ready, lpar_w, host_w)
+
+    @mock.patch('pypowervm.tasks.migration.migrate_abort')
+    def test_migration_abort(self, mock_mig_abort):
+        self.lpmsrc.lpar_w = mock.Mock()
+        self.lpmsrc.migration_abort()
+        mock_mig_abort.called_once_with(self.lpmsrc.lpar_w)
+
+    @mock.patch('pypowervm.tasks.migration.migrate_recover')
+    def test_migration_recover(self, mock_mig_recover):
+        self.lpmsrc.lpar_w = mock.Mock()
+        self.lpmsrc.migration_recover()
+        mock_mig_recover.called_once_with(self.lpmsrc.lpar_w, force=True)
