82f6dddc2506dfccfd744ecc4b967eba8ab590a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:28 +0000

    Add package pyrsl/0.6.0/pyrsl-0.6.0.tar.gz

---
+++ packages/pyrsl/pyrsl-0.6.0.tar.gz/pyrsl-0.6.0/rsl/__init__.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+# Copyright (C) 2015 John Törnblom
+'''
+Parsing and evaluation of the rule-specification language (RSL).  
+'''
+from rsl.parse import parse_file
+from rsl.parse import parse_text
+from rsl.eval import evaluate
+from rsl.runtime import Runtime
+from rsl.runtime import bridge
+from rsl.runtime import string_formatter
+from rsl.gen_erate import main
---
+++ packages/pyrsl/pyrsl-0.6.0.tar.gz/pyrsl-0.6.0/rsl/version.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+# Copyright (C) 2016 John Törnblom
+'''
+Keep version info in one single place
+'''
+
+
+name = 'pyrsl'
+date = '2016-09-03'
+release = '0.6.0'
+
+complete_string = '%s v%s (%s)' % (name, release, date)
