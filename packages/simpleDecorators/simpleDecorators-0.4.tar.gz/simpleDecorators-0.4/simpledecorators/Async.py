271c299b3b8985b265419eab97a69432aab7e5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:51 +0000

    Add package simpleDecorators/0.4/simpleDecorators-0.4.tar.gz

---
+++ packages/simpleDecorators/simpleDecorators-0.4.tar.gz/simpleDecorators-0.4/simpledecorators/Async.py
@@ -0,0 +1,49 @@
+from threading import Thread
+
+def Async(executor=None):
+    """
+    @type executor: simpledecorators.ThreadPool
+    """
+    def asyncDecorator (func):
+        def wrapped(*args, **kwargs):
+            def threadWrapper():
+                return func(*args, **kwargs)
+            if not executor:
+                thread = Thread(target=threadWrapper)
+                thread.start()
+            else:
+                executor.add_task(threadWrapper)
+        return wrapped
+    return asyncDecorator
+
+
+
+if __name__ == "__main__":
+    from time import sleep
+    from ThreadPool import *
+    try:
+        xrange
+    except NameError:
+        xrange = range
+    class TestClass():
+        @Async()
+        def testDecorated(self):
+            print (345)
+    testClass = TestClass()
+    testClass.testDecorated()
+    @Async(executor=ThreadPool(5))
+    def func(a, b):
+        print ("func called")
+        sleep(1)
+        print ("func exit:" + str(a))
+
+    @Async()
+    def funcWithoutExecutor(a):
+        print (a)
+
+    for x in xrange(1, 10):
+        funcWithoutExecutor("noExecutor:" + str(x))
+
+
+    for x in xrange(1, 15):
+        func(x, 2)
