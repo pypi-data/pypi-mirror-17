39c66e0e645277b073c0bc747ed6ac1d936cb6ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:23 +0000

    Add package Flask-Arangodb/1.0.3/Flask-Arangodb-1.0.3.tar.gz

---
+++ packages/Flask-Arangodb/Flask-Arangodb-1.0.3.tar.gz/Flask-Arangodb-1.0.3/Tests/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding=utf-8
+#
+# Copyright (C) 2016 Bas van den Broek
+#
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main())
