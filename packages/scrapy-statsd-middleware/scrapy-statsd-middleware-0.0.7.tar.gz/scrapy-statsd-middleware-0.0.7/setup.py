b9ce848acb5671585e3bb37cab9b4e7bcaff6f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:30 +0000

    Add package scrapy-statsd-middleware/0.0.7/scrapy-statsd-middleware-0.0.7.tar.gz

---
+++ packages/scrapy-statsd-middleware/scrapy-statsd-middleware-0.0.7.tar.gz/scrapy-statsd-middleware-0.0.7/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='scrapy-statsd-middleware',
+    version='0.0.7',
+    description='Statsd integration middleware for scrapy',
+    long_description=readme,
+    author='Zach Goldstein',
+    author_email='zachgold@gmail.com',
+    url='https://github.com/zachgoldstein/scrapy-statsd',
+    license='Apache 2.0',
+    packages=['scrapy_statsd_middleware'],
+    install_requires=[
+      'Scrapy>=1.0.5',
+      'statsd==3.2.1'
+    ],
+    extras_require={
+      'test': ['mock==2.0.0'],
+    }
+)
