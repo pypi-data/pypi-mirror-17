f97affd4c4e3236ab1de2b371ad25f4450d85cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:23 +0000

    Add package coala-bears/0.8.1/coala_bears-0.8.1-py3-none-any.whl

---
+++ packages/coala-bears/coala_bears-0.8.1-py3-none-any.whl/bears/r/RLintBear.py
@@ -0,0 +1,34 @@
+import re
+
+from coalib.bearlib.abstractions.Lint import Lint
+from coalib.bears.LocalBear import LocalBear
+from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
+from coalib.bears.requirements.RscriptRequirement import RscriptRequirement
+
+
+class RLintBear(LocalBear, Lint):
+    executable = 'Rscript'
+    arguments = "-e 'library(lintr)' -e 'lintr::lint(\"{filename}\")'"
+    output_regex = re.compile(
+        r'(.*?):(?P<line>\d+):(?P<column>\d+):'
+        r' (?P<severity>\S+): (?P<message>.*)')
+    severity_map = {
+        "style": RESULT_SEVERITY.NORMAL,
+        "warning": RESULT_SEVERITY.NORMAL,
+        "error": RESULT_SEVERITY.MAJOR}
+    LANGUAGES = {"R"}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    REQUIREMENTS = {RscriptRequirement(package='lintr', flag='-e',
+                                       repo='http://cran.rstudio.com')}
+    LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Syntax', 'Formatting'}
+
+    prerequisite_command = ["Rscript", "-e", "library(lintr)"]
+    prerequisite_fail_msg = 'R library "lintr" is not installed.'
+
+    def run(self, filename, file):
+        '''
+        Checks the code with `lintr`.
+        '''
+        return self.lint(filename)
