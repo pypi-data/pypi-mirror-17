d8062222096c3732c6f5423c73a3cd9f890919e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:47 +0000

    Add package onegov.election-day/0.9.0/onegov.election_day-0.9.0.tar.gz

---
+++ packages/onegov.election-day/onegov.election_day-0.9.0.tar.gz/onegov.election_day-0.9.0/docs/formatbeschreibung.md
@@ -0,0 +1,8 @@
+
+[Formatbeschreibung](format_de.md)
+
+[Format description](format_en.md)
+
+[Spécifications de format](format_fr.md)
+
+[Specifica Formato](format_it.md)
---
+++ packages/onegov.election-day/onegov.election_day-0.9.0.tar.gz/onegov.election_day-0.9.0/onegov/election_day/tests/test_views.py
@@ -0,0 +1,378 @@
+import onegov.election_day
+
+from datetime import date
+from freezegun import freeze_time
+from onegov.testing import utils
+from webtest import TestApp as Client
+
+
+COLUMNS = [
+    'Bezirk',
+    'BFS Nummer',
+    'Gemeinde',
+    'Ja Stimmen',
+    'Nein Stimmen',
+    'Stimmberechtigte',
+    'Leere Stimmzettel',
+    'Ungültige Stimmzettel'
+]
+
+
+def login(client):
+    login = client.get('/auth/login')
+    login.form['username'] = 'admin@example.org'
+    login.form['password'] = 'hunter2'
+    login.form.submit()
+
+
+def test_view_permissions():
+    utils.assert_explicit_permissions(
+        onegov.election_day, onegov.election_day.ElectionDayApp)
+
+
+def test_view_login_logout(election_day_app):
+    client = Client(election_day_app)
+    client.get('/locale/de_CH').follow()
+
+    login = client.get('/').click('Anmelden')
+    login.form['username'] = 'admin@example.org'
+    login.form['password'] = 'hunter1'
+
+    assert "Unbekannter Benutzername oder falsches Passwort" \
+        in login.form.submit()
+    assert 'Anmelden' in client.get('/')
+
+    login.form['password'] = 'hunter2'
+    homepage = login.form.submit().follow()
+
+    assert 'Sie sind angemeldet' in homepage
+    assert 'Abmelden' in homepage
+    assert 'Anmelden' not in homepage
+
+    assert 'Anmelden' in client.get('/').click('Abmelden').follow()
+
+
+def test_view_manage(election_day_app):
+    client = Client(election_day_app)
+    client.get('/locale/de_CH').follow()
+
+    assert client.get('/manage/elections',
+                      expect_errors=True).status_code == 403
+    assert client.get('/manage/votes', expect_errors=True).status_code == 403
+
+    login(client)
+
+    manage = client.get('/manage/elections')
+
+    assert "Noch keine Wahlen erfasst" in manage
+
+    new = manage.click('Neue Wahl')
+    new.form['election_de'] = 'Elect a new president'
+    new.form['date'] = date(2016, 1, 1)
+    new.form['election_type'] = 'majorz'
+    new.form['domain'] = 'federation'
+    new.form['mandates'] = 1
+    manage = new.form.submit().follow()
+
+    assert "Elect a new president" in manage
+    edit = manage.click('Bearbeiten')
+    edit.form['election_de'] = 'Elect a new federal councillor'
+    manage = edit.form.submit().follow()
+
+    assert "Elect a new federal councillor" in manage
+
+    delete = manage.click("Löschen")
+    assert "Wahl löschen" in delete
+    assert "Elect a new federal councillor" in delete
+    assert "Bearbeiten" in delete.click("Abbrechen")
+
+    manage = delete.form.submit().follow()
+    assert "Noch keine Wahlen erfasst" in manage
+
+    manage = client.get('/manage/votes')
+
+    assert "Noch keine Abstimmungen erfasst" in manage
+
+    new = manage.click('Neue Abstimmung')
+    new.form['vote_de'] = 'Vote for a better yesterday'
+    new.form['date'] = date(2016, 1, 1)
+    new.form['domain'] = 'federation'
+    manage = new.form.submit().follow()
+
+    assert "Vote for a better yesterday" in manage
+    edit = manage.click('Bearbeiten')
+    edit.form['vote_de'] = 'Vote for a better tomorrow'
+    manage = edit.form.submit().follow()
+
+    assert "Vote for a better tomorrow" in manage
+
+    delete = manage.click("Löschen")
+    assert "Abstimmung löschen" in delete
+    assert "Vote for a better tomorrow" in delete
+    assert "Bearbeiten" in delete.click("Abbrechen")
+
+    manage = delete.form.submit().follow()
+    assert "Noch keine Abstimmungen erfasst" in manage
+
+
+def test_i18n(election_day_app):
+    client = Client(election_day_app)
+    client.get('/locale/de_CH').follow()
+
+    login(client)
+
+    new = client.get('/manage/votes/new-vote')
+    new.form['vote_de'] = 'Foo'
+    new.form['vote_fr'] = 'Bar'
+    new.form['vote_it'] = 'Baz'
+    new.form['vote_rm'] = 'Qux'
+    new.form['date'] = date(2015, 1, 1)
+    new.form['domain'] = 'federation'
+    new.form.submit()
+
+    homepage = client.get('/')
+    assert "Foo" in homepage
+
+    homepage = homepage.click('Français').follow()
+    assert "Bar" in homepage
+
+    homepage = homepage.click('Italiano').follow()
+    assert "Baz" in homepage
+
+    homepage = homepage.click('Rumantsch').follow()
+    assert "Qux" in homepage
+
+    new = client.get('/manage/elections/new-election')
+    new.form['election_de'] = 'Tick'
+    new.form['election_fr'] = 'Trick'
+    new.form['election_it'] = 'Track'
+    new.form['election_rm'] = 'Quack'
+    new.form['date'] = date(2015, 1, 1)
+    new.form['mandates'] = 1
+    new.form['election_type'] = 'majorz'
+    new.form['domain'] = 'federation'
+    new.form.submit()
+
+    homepage = client.get('/')
+    assert "Quack" in homepage
+
+    homepage = homepage.click('Français').follow()
+    assert "Trick" in homepage
+
+    homepage = homepage.click('Italiano').follow()
+    assert "Track" in homepage
+
+    homepage = homepage.click('Deutsch').follow()
+    assert "Tick" in homepage
+
+
+def test_pages_cache(election_day_app):
+    client = Client(election_day_app)
+    client.get('/locale/de_CH')
+
+    # make sure codes != 200 are not cached
+    anonymous = Client(election_day_app)
+    anonymous.get('/vote/0xdeadbeef', status=404)
+    anonymous.get('/election/0xdeafbeef', status=404)
+
+    login(client)
+
+    new = client.get('/manage/votes/new-vote')
+    new.form['vote_de'] = '0xdeadbeef'
+    new.form['date'] = date(2015, 1, 1)
+    new.form['domain'] = 'federation'
+    new.form.submit()
+
+    assert '0xdeadbeef' in anonymous.get('/')
+    assert '0xdeadbeef' in anonymous.get('/vote/0xdeadbeef')
+
+    edit = client.get('/vote/0xdeadbeef/edit')
+    edit.form['vote_de'] = '0xdeadc0de'
+    edit.form.submit()
+
+    assert '0xdeadc0de' in client.get('/')
+    assert '0xdeadbeef' in anonymous.get('/')
+    assert '0xdeadc0de' in anonymous.get('/', headers=[
+        ('Cache-Control', 'no-cache')
+    ])
+
+    new = client.get('/manage/elections/new-election')
+    new.form['election_de'] = '0xdeafbeef'
+    new.form['date'] = date(2015, 1, 1)
+    new.form['mandates'] = 1
+    new.form['election_type'] = 'majorz'
+    new.form['domain'] = 'federation'
+    new.form.submit()
+
+    assert '0xdeafbeef' not in anonymous.get('/')
+    assert '0xdeafbeef' in anonymous.get(
+        '/', headers=[('Cache-Control', 'no-cache')]
+    )
+
+
+def test_view_latest(election_day_app):
+    client = Client(election_day_app)
+    client.get('/locale/de_CH').follow()
+
+    login(client)
+
+    new = client.get('/manage/votes/new-vote')
+    new.form['vote_de'] = "Abstimmung 1. Januar 2013"
+    new.form['date'] = date(2013, 1, 1)
+    new.form['domain'] = 'federation'
+    new.form.submit()
+
+    new = client.get('/manage/elections/new-election')
+    new.form['election_de'] = "Wahl 1. Januar 2013"
+    new.form['date'] = date(2013, 1, 1)
+    new.form['mandates'] = 1
+    new.form['election_type'] = 'majorz'
+    new.form['domain'] = 'federation'
+    new.form.submit()
+
+    latest = client.get('/')
+    assert "Abstimmung 1. Januar 2013" in latest
+    assert "Wahl 1. Januar 2013" in latest
+
+
+def test_view_latest_json(election_day_app):
+    client = Client(election_day_app)
+    client.get('/locale/de_CH').follow()
+
+    assert client.get('/json').json['archive'] == {}
+    assert client.get('/json').json['results'] == []
+
+    login(client)
+
+    new = client.get('/manage/votes/new-vote')
+    new.form['vote_de'] = "Abstimmung 1. Januar 2013"
+    new.form['date'] = date(2013, 1, 1)
+    new.form['domain'] = 'federation'
+    new.form.submit()
+
+    new = client.get('/manage/elections/new-election')
+    new.form['election_de'] = "Wahl 1. Januar 2013"
+    new.form['date'] = date(2013, 1, 1)
+    new.form['mandates'] = 1
+    new.form['election_type'] = 'majorz'
+    new.form['domain'] = 'federation'
+    new.form.submit()
+
+    latest = client.get('/json')
+    assert list(latest.json['archive'].keys()) == ['2013']
+    assert "Abstimmung 1. Januar 2013" in latest
+    assert "Wahl 1. Januar 2013" in latest
+
+
+def test_view_archive(election_day_app):
+    client = Client(election_day_app)
+    client.get('/locale/de_CH').follow()
+
+    login(client)
+
+    new = client.get('/manage/votes/new-vote')
+    new.form['vote_de'] = "Abstimmung 1. Januar 2013"
+    new.form['date'] = date(2013, 1, 1)
+    new.form['domain'] = 'federation'
+    new.form.submit()
+
+    new = client.get('/manage/elections/new-election')
+    new.form['election_de'] = "Wahl 1. Januar 2013"
+    new.form['date'] = date(2013, 1, 1)
+    new.form['mandates'] = 1
+    new.form['election_type'] = 'majorz'
+    new.form['domain'] = 'federation'
+    new.form.submit()
+
+    assert "archive/2013" in client.get('/')
+
+    archive = client.get('/archive/2013')
+    assert "Abstimmung 1. Januar 2013" in archive
+    assert "Wahl 1. Januar 2013" in archive
+
+    archive = client.get('/archive/2013-01-01')
+    assert "Abstimmung 1. Januar 2013" in archive
+    assert "Wahl 1. Januar 2013" in archive
+
+    archive = client.get('/archive/2013-02-02')
+    assert "noch keine Wahlen oder Abstimmungen" in archive
+
+
+def test_view_archive_json(election_day_app):
+    client = Client(election_day_app)
+    client.get('/locale/de_CH').follow()
+
+    login(client)
+
+    new = client.get('/manage/votes/new-vote')
+    new.form['vote_de'] = "Abstimmung 1. Januar 2013"
+    new.form['date'] = date(2013, 1, 1)
+    new.form['domain'] = 'federation'
+    new.form.submit()
+
+    new = client.get('/manage/elections/new-election')
+    new.form['election_de'] = "Wahl 1. Januar 2013"
+    new.form['date'] = date(2013, 1, 1)
+    new.form['mandates'] = 1
+    new.form['election_type'] = 'majorz'
+    new.form['domain'] = 'federation'
+    new.form.submit()
+
+    archive = client.get('/archive/2013/json')
+    assert list(archive.json['archive'].keys()) == ['2013']
+    assert "Abstimmung 1. Januar 2013" in archive
+    assert "Wahl 1. Januar 2013" in archive
+
+    archive = client.get('/archive/2013-01-01/json')
+    assert list(archive.json['archive'].keys()) == ['2013']
+    assert "Abstimmung 1. Januar 2013" in archive
+    assert "Wahl 1. Januar 2013" in archive
+
+    archive = client.get('/archive/2013-02-02/json')
+    assert list(archive.json['archive'].keys()) == ['2013']
+    assert archive.json['results'] == []
+
+
+def test_view_last_modified(election_day_app):
+    with freeze_time("2014-01-01 12:00"):
+        client = Client(election_day_app)
+        client.get('/locale/de_CH').follow()
+
+        login(client)
+
+        new = client.get('/manage/votes/new-vote')
+        new.form['vote_de'] = "Vote"
+        new.form['date'] = date(2013, 1, 1)
+        new.form['domain'] = 'federation'
+        new.form.submit()
+
+        new = client.get('/manage/elections/new-election')
+        new.form['election_de'] = "Election"
+        new.form['date'] = date(2013, 1, 1)
+        new.form['mandates'] = 1
+        new.form['election_type'] = 'majorz'
+        new.form['domain'] = 'federation'
+        new.form.submit()
+
+        client = Client(election_day_app)
+        client.get('/locale/de_CH').follow()
+
+        for path in (
+            '/'
+            '/json',
+            '/archive/2013',
+            '/election/election',
+            '/election/election/summary',
+            '/election/election/json',
+            '/election/election/data-json',
+            '/election/election/data-csv',
+            '/election/election/data-xlsx',
+            '/vote/vote/',
+            '/vote/vote/summary',
+            '/vote/vote/json',
+            '/vote/vote/data-json',
+            '/vote/vote/data-csv',
+            '/vote/vote/data-xlsx',
+        ):
+            assert client.get(path).headers.get('Last-Modified') == \
+                'Wed, 01 Jan 2014 12:00:00 GMT'
