b6bbf43509ec107ae68f2021334922133d2ffb16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:41 +0000

    Add package atdcheck/1.0.1/atdcheck-1.0.1.tar.gz

---
+++ packages/atdcheck/atdcheck-1.0.1.tar.gz/atdcheck-1.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here=path.abspath(path.dirname(__file__))
+with open(path.join(here,'README.md'),encoding='utf-8') as f:
+	long_description=f.read()
+
+setup(
+	name='atdcheck',
+	version='1.0.1',
+	description='A Python wrapper for ATD (After The Dateline)',
+	long_description=long_description,
+	url='https://github.com/kurniawano/atdcheck',
+	author='Oka Kurniawan',
+	author_email='oka@okakurniawan.net',
+	license='MIT',
+	classifiers=[
+	'Development Status :: 4 - Beta',
+	'Intended Audience :: Education',
+	'Topic :: Utilities',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python :: 2.7',
+	],
+	keywords='tools utilities spell grammar check',
+	packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+	entry_points={
+	'console_scripts':[
+	'atdcheck=atdcheck:main',
+	],
+	}
+	)
