0d544857a687ae44957e33a5ed7ec1eb8098d142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:22 +0000

    Add package discosub/0.1.16/discosub-0.1.16-py2.py3-none-any.whl

---
+++ packages/discosub/discosub-0.1.16-py2.py3-none-any.whl/discosub/main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Hervé Beraud'
+__email__ = 'herveberaud.pro@gmail.com'
+__version__ = '0.1.16'
+
+import argparse
+import sys
+import click
+from discosub.commands.discover import analyze
+
+
+@click.group()
+def main():
+    """Fast BruteForce Subdomain Discover"""
+    pass
+
+
+@main.command()
+@click.argument('target')
+def run(**kwargs):
+    '''Run a subdomain scanner on specified target'''
+    analyze(kwargs['target'])
+
+
+@main.command()
+def version():
+    """Display Discosub Version"""
+    print('discosub {version}'.format(version=__version__),)
+
+
+if __name__ == "__main__":
+    main()
