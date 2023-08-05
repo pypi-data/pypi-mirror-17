3145b242689ee6d9dd968369296cc258d2675054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:21 +0000

    Add package python-easyengine/0.1.0/python-easyengine-0.1.0.tar.gz

---
+++ packages/python-easyengine/python-easyengine-0.1.0.tar.gz/python-easyengine-0.1.0/HISTORY.rst
@@ -0,0 +1,6 @@
+.. :changelog:
+
+History
+-------
+0.0.1 (2016-09-4)
+* Initial commit
---
+++ packages/python-easyengine/python-easyengine-0.1.0.tar.gz/python-easyengine-0.1.0/easyengine/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.1.0'
+
+
+from .exceptions import (
+	EasyEngineException
+)
+from .base import (
+	EasyEngine
+)
