54c4c48cfa6aba8312322e199e6456d0e469c41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:25 +0000

    Add package trackmac/0.0.3/trackmac-0.0.3.tar.gz

---
+++ packages/trackmac/trackmac-0.0.3.tar.gz/trackmac-0.0.3/trackmac/config.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import os
+
+TRACK_SCRIPT = 'tm'
+TRACK_DAEMON = 'trackmac_service'
+TRACK_DIR = os.path.expanduser('~/Library/Application Support/com.github.macleek.trackmac/')
+TRACK_DB_FILE = TRACK_DIR + 'track.db'
+TRACK_LOG_FILE = TRACK_DIR + 'track.log'
+TRACK_PLIST_NAME = 'com.github.macleek.trackmac.plist'
+USER_LAUNCHAGENTS_DIR = os.path.expanduser('~/Library/LaunchAgents')
+BROWSERS = {
+    'Google Chrome': {
+        'bundle_id': 'com.google.Chrome',
+        'tab': 'activeTab',
+        'title': 'title',
+        'url': 'URL'
+    },
+    'Safari': {
+        'bundle_id': 'com.apple.Safari',
+        'tab': 'currentTab',
+        'title': 'name',
+        'url': 'URL'
+    }
+}
