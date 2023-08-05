c5c380b7387db7da22e8c7b3a1c6d6ba23f3a96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:35 +0000

    Add package seismograph/0.3.20/seismograph-0.3.20.tar.gz

---
+++ packages/seismograph/seismograph-0.3.20.tar.gz/seismograph-0.3.20/seismograph/utils/common.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+
+from ..exceptions import TimeoutException
+
+
+def waiting_for(func, timeout=None, exc_cls=None, message=None, delay=None, args=None, kwargs=None):
+    args = args or tuple()
+    kwargs = kwargs or dict()
+
+    timeout = timeout or 0
+    message = message or 'Timeout "{}" exceeded'.format(timeout)
+
+    if timeout:
+        t_start = time.time()
+
+        while time.time() <= t_start + timeout:
+            result = func(*args, **kwargs)
+
+            if result:
+                return result
+
+            if delay:
+                time.sleep(delay)
+        else:
+            if exc_cls:
+                raise exc_cls(message)
+            raise TimeoutException(message)
+
+    result = func(*args, **kwargs)
+
+    if result:
+        return result
+    if exc_cls:
+        raise exc_cls(message)
+    raise TimeoutException(message)
+
+
+def call_to_chain(chain, method_name, *args, **kwargs):
+    for obj in chain:
+        if method_name:
+            getattr(obj, method_name)(*args, **kwargs)
+        else:
+            obj(*args, **kwargs)
+
+
+def measure_time():
+    start_time = time.time()
+    return lambda: time.time() - start_time
+
+
+def pythonpaths(*paths):
+    def wrapper(f):
+        for path in paths:
+            if path not in sys.path:
+                sys.path.append(path)
+        return f
+    return wrapper
