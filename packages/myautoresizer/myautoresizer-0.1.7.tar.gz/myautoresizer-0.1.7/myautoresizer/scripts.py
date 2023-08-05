e8377d1dd688a55ccb11b8e573f6394d4fb9c6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:51 +0000

    Add package myautoresizer/0.1.7/myautoresizer-0.1.7.tar.gz

---
+++ packages/myautoresizer/myautoresizer-0.1.7.tar.gz/myautoresizer-0.1.7/myautoresizer/scripts.py
@@ -0,0 +1,11 @@
+from myautoresizer import print_rect, config_logging_default, auto_resize
+
+
+def ma_printrect():
+    config_logging_default()
+    print_rect()
+
+
+def ma_autoresize():
+    config_logging_default()
+    auto_resize()
