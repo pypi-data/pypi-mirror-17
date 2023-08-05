9611c0c59b5824ce2bf7572a7368b430c49fe2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:36 +0000

    Add package versiontracker/1.0.2.20160831/versiontracker-1.0.2.20160831.tar.gz

---
+++ packages/versiontracker/versiontracker-1.0.2.20160831.tar.gz/versiontracker-1.0.2.20160831/setup.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+
+from setuptools import setup
+
+
+setup(
+    name='versiontracker',
+    version='1.0.2.20160831',
+    description=u"Web scrapping software to keep track of the latest stable "
+                u"version of several pieces of software.",
+    url='https://gitlab.com/gallaecio/versiontracker',
+    author=u"Adrián Chaves (Gallaecio)",
+    author_email='adriyetichaves@gmail.com',
+    license='AGPLv3+',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or '
+            'later (AGPLv3+)',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Archiving :: Packaging',
+    ],
+    packages=['versiontracker'],
+    install_requires=[
+        'beautifulsoup4',
+        'python-dateutil',
+        'requests',
+        'termcolor',
+    ],
+    package_data={
+        'versiontracker': ['data.json'],
+    },
+    entry_points={
+          'console_scripts': [
+              'versiontracker = versiontracker.__main__:main'
+          ]
+      },
+)
