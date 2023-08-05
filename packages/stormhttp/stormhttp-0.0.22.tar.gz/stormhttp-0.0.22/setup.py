b90554cfe7464e204d1e5fbf1e72c0b2419c297e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:24 +0000

    Add package stormhttp/0.0.22/stormhttp-0.0.22.tar.gz

---
+++ packages/stormhttp/stormhttp-0.0.22.tar.gz/stormhttp-0.0.22/setup.py
@@ -0,0 +1,52 @@
+from distutils.core import setup
+from setuptools.command.test import test
+from stormhttp import __version__
+
+
+class PyTest(test):
+    def finalize_options(self):
+        test.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        import sys
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+setup(
+    name="stormhttp",
+    packages=[
+        "stormhttp"
+    ],
+    version=__version__,
+    description="Lightning-fast asynchronous web framework for Python 3.5+",
+    license="Apache 2",
+    author="Seth Michael Larson",
+    author_email="sethmichaellarson@protonmail.com",
+    url="https://github.com/SethMichaelLarson/stormhttp",
+    download_url="https://github.com/SethMichaelLarson/stormhttp/tarball/" + __version__,
+    keywords=["web"],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Internet :: WWW/HTTP"
+    ],
+    install_requires=[
+        "brotlipy>=0.5.1",
+        "cchardet>=1.0.0",
+        "httptools>=0.0.9"
+    ],
+    tests_require=[
+        "pytest",
+        "coverage",
+        "coveralls"
+    ],
+    cmdclass={
+        "test": PyTest
+    }
+)
