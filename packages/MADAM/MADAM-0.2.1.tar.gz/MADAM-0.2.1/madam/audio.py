06560511c56d5f39e1b2b1106a7dfae82d506e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:16 +0000

    Add package MADAM/0.2.1/MADAM-0.2.1.tar.gz

---
+++ packages/MADAM/MADAM-0.2.1.tar.gz/MADAM-0.2.1/madam/audio.py
@@ -0,0 +1,64 @@
+import io
+import os
+import shutil
+import tempfile
+import wave
+
+import mutagen.mp3
+
+from madam.core import Asset, Processor
+from madam.ffmpeg import FFmpegProcessor
+
+
+class WaveProcessor(Processor):
+    """
+    Represents a processor that uses the *wave* module of the standard library
+    to read wave data.
+    """
+    def _read(self, wave_file):
+        with wave.open(wave_file) as wave_data:
+            essence_bytes = wave_data.readframes(wave_data.getnframes())
+            essence_stream = io.BytesIO()
+            essence_stream.write(essence_bytes)
+            essence_stream.seek(0)
+            metadata = dict(
+                mime_type='audio/wav',
+                channels=wave_data.getnchannels(),
+                framerate=wave_data.getframerate(),
+            )
+            asset = Asset(essence_stream, **metadata)
+        return asset
+
+    def _can_read(self, file):
+        try:
+            wave.open(file, 'rb')
+            return True
+        except:
+            return False
+
+
+class MutagenProcessor(Processor):
+    """
+    Represents a processor that uses *Mutagen* to read audio data.
+    """
+    def _read(self, mp3_file):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            file_path = mp3_file.name
+            filename = os.path.basename(file_path)
+            copy_path = os.path.join(temp_dir, filename)
+            shutil.copyfile(file_path, copy_path)
+
+            mp3 = mutagen.mp3.MP3(copy_path)
+            metadata = dict(mime_type='audio/mpeg', duration=mp3.info.length)
+            mp3.tags.delete(copy_path)
+
+            with open(copy_path, 'rb') as mp3_file_copy:
+                asset = Asset(essence=mp3_file_copy, **metadata)
+        return asset
+
+    def _can_read(self, file):
+        with tempfile.NamedTemporaryFile() as temp_file:
+            temp_file.write(file.read())
+            if mutagen.File(temp_file.name):
+                return True
+        return False
---
+++ packages/MADAM/MADAM-0.2.1.tar.gz/MADAM-0.2.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+import versioneer
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name='MADAM',
+    description='Digital asset management library',
+    long_description=long_description,
+    url='https://github.com/eseifert/madam',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author='Michael Seifert, Erich Seifert',
+    author_email='mseifert@error-reports.org, dev@erichseifert.de',
+    install_requires=['bidict', 'frozendict', 'mutagen', 'piexif', 'pillow'],
+    setup_requires=['pytest-runner', 'versioneer'],
+    tests_require=['piexif', 'pillow', 'pytest >=3.0'],
+    extras_require={
+        'doc': ['sphinx >=1.3', 'sphinx_rtd_theme']
+    },
+    packages=['madam'],
+    platforms=['POSIX'],
+    license='AGPLv3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+        'Topic :: Multimedia :: Sound/Audio :: Conversion',
+        'Topic :: Multimedia :: Video :: Conversion',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='asset media processing'
+)
