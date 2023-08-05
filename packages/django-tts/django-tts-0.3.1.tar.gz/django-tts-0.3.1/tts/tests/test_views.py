15add2d889528d41a2eceacf9047cdd31d07adbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:20 +0000

    Add package django-tts/0.3.1/django-tts-0.3.1.tar.gz

---
+++ packages/django-tts/django-tts-0.3.1.tar.gz/django-tts-0.3.1/tts/tests/test_views.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+import os
+import json
+import shutil
+
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.core.urlresolvers import reverse
+from django.conf import settings
+
+
+class TestGenerateView(APITestCase):
+    url = reverse('speach_synthesizer:generate')
+
+    def tearDown(self):
+        shutil.rmtree(settings.OUTPUT_DIR)
+
+    def test_generate(self):
+        response = self.client.post(self.url, {'text': 'Awesome Text!'})
+        content = json.loads(response.content.decode('utf-8'))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(content['status'], 0)
+        self.assertTrue(os.path.exists('%s/%s.wav' % (settings.OUTPUT_DIR, content['uuid'])))
+
+
+class TestGetFileView(APITestCase):
+    get_url = reverse('speach_synthesizer:get_file')
+    gen_url = reverse('speach_synthesizer:generate')
+
+    def test_get_file(self):
+        gen_response = self.client.post(self.gen_url, {'text': 'Awesome Text!'})
+        gen_content = json.loads(gen_response.content.decode('utf-8'))
+
+        get_response = self.client.get(self.get_url, {'uuid': gen_content['uuid']})
+
+        self.assertEqual(get_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(get_response['Content-Disposition'], 'attachment; filename=%s.wav' % gen_content['uuid'])
---
+++ packages/django-tts/django-tts-0.3.1.tar.gz/django-tts-0.3.1/tts/views.py
@@ -0,0 +1,79 @@
+# Create your views here.
+import subprocess
+import os
+import uuid
+
+from django.http import JsonResponse
+from django.http import HttpResponse
+from django.core.files import File
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+from . import models
+from . import serializers
+
+@api_view(['post'])
+def generate(request):
+    """
+    ---
+    parameters:
+    - name: text
+      description: Text for generation
+      required: true
+      paramType: form
+    """
+    serializer = serializers.Generation(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    text = serializer.validated_data['text']
+    tmp_dir = '/tmp/voice/'
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+
+    file_uuid = str(uuid.uuid4())
+    tmp_path = '%s/%s.wav' % (tmp_dir, file_uuid)
+
+    command = 'echo %s | RHVoice-test -p elena -o %s' % (text, tmp_path)
+    status_code, output = subprocess.getstatusoutput(command)
+    if status_code != 0:
+        return Response(
+            {'error': 'File generation error', 'output': output},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+    with open(tmp_path, 'rb') as tmp_file:
+        models.SoundFile.objects.create(
+            uuid=file_uuid,
+            text=text,
+            command=command,
+            file=File(tmp_file),
+            type='wav',
+            user=request.user
+        )
+
+    os.unlink(tmp_path)
+    return JsonResponse({'status': status_code, 'uuid': file_uuid})
+
+
+@api_view()
+def get_file(request):
+    """
+    ---
+    parameters:
+    - name: uuid
+      description: uuid of generated file
+      required: true
+      paramType: query
+    """
+    serializer = serializers.GetFile(data=request.query_params)
+    serializer.is_valid(raise_exception=True)
+
+    sound_file = models.SoundFile.objects.get(uuid=serializer.validated_data['uuid'])
+
+    response = HttpResponse()
+    response.write(sound_file.file.read())
+
+    response['Content-Type'] = 'audio/vnd.wave'
+    response['Content-Length'] = sound_file.file.size
+    response['Content-Disposition'] = 'attachment; filename=%s' % sound_file.file.name.split('/')[-1]
+
+    return response
