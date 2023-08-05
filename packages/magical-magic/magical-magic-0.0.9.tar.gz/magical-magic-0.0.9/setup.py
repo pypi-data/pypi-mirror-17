442f00e95c01429ecb9f40bb1139af2e6c6f1b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:21 +0000

    Add package magical-magic/0.0.9/magical-magic-0.0.9.tar.gz

---
+++ packages/magical-magic/magical-magic-0.0.9.tar.gz/magical-magic-0.0.9/setup.py
@@ -0,0 +1,35 @@
+from os.path import join, dirname
+import setuptools
+
+
+def read(fname):
+    with open(join(dirname(__file__), fname)) as f:
+        return f.read()
+
+setuptools.setup(
+    name="magical-magic",
+    version="0.0.9",
+    author="Tony Fast",
+    author_email="tony.fast@gmail.com",
+    description="Simple reusable magics.",
+    license="BSD-3-Clause",
+    keywords="IPython Magic Jupyter",
+    url="http://github.com/tonyfast/magical-magic",
+    packages=setuptools.find_packages(),
+    long_description=read("readme.rst"),
+    classifiers=[
+        "Topic :: Utilities",
+        "Framework :: IPython",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development :: Testing",
+    ],
+    install_requires=[
+        "IPython",
+    ]
+)
