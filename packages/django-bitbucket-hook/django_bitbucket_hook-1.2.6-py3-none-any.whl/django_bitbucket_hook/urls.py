fff72621017b6aaad09cba228b15bc354c854500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:08 +0000

    Add package django-bitbucket-hook/1.2.6/django_bitbucket_hook-1.2.6-py3-none-any.whl

---
+++ packages/django-bitbucket-hook/django_bitbucket_hook-1.2.6-py3-none-any.whl/django_bitbucket_hook/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django_bitbucket_hook import views
+
+urlpatterns = [
+    url(r'^$', views.only_hook),
+    url(r'^(?P<name>[\w-]+)$', views.hook_name),
+    url(r'^(?P<name>[\w-]+)/(?P<branch>[\w-]+)$', views.hook_name_branch),
+]
