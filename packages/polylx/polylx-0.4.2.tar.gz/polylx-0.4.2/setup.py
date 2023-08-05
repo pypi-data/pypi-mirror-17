1e77d0880acd60bee15fb96e72d535e62536bbca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:27 +0000

    Add package polylx/0.4.2/polylx-0.4.2.tar.gz

---
+++ packages/polylx/polylx-0.4.2.tar.gz/polylx-0.4.2/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+    'numpy',
+    'matplotlib',
+    'pandas',
+    'networkx',
+    'scipy',
+    'shapely'
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='polylx',
+    version='0.4.2',
+    description="A Python package to visualize and analyze microstructures.",
+    long_description=readme + '\n\n' + history,
+    author="Ondrej Lexa",
+    author_email='lexa.ondrej@gmail.com',
+    url='https://github.com/ondrolexa/polylx',
+    packages=[
+        'polylx',
+    ],
+    package_dir={'polylx':'polylx'},
+    include_package_data=True,
+    install_requires=requirements,
+    entry_points = {
+        'console_scripts': ['ipolylx=polylx.core:cli'],
+    },
+    license="BSD",
+    zip_safe=False,
+    keywords='polylx',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
