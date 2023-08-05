70b99c2c482442bed64c41ede5ade7855a69e027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:53 +0000

    Add package vy/1.5.0/vy-1.5.0.tar.gz

---
+++ packages/vy/vy-1.5.0.tar.gz/vy-1.5.0/vyapp/plugins/match_sym_pair.py
@@ -0,0 +1,52 @@
+"""
+Overview
+========
+
+This plugin implements a mechanism to highligh pairs of ( ) [ ] { }.
+
+
+"""
+
+def install(area, setup={'background':'pink', 'foreground':'black'}, 
+            max=1500, timeout=500):
+
+    def cave(tag, args):
+        area.after(timeout, cave, tag, args)
+        index = area.case_pair('insert', max, *args)
+
+        if not index: 
+            area.tag_remove(tag, '1.0', 'end')
+        else:
+            area.tag_update(tag, '1.0', 'end', 
+                            ('insert', 'insert +1c'), (index, '%s +1c' % index))
+
+    area.tag_config('_paren_', **setup)
+    area.tag_config('_bracket_', **setup)
+    area.tag_config('_brace_', **setup)
+    cave('_paren_', ('(', ')'))
+    cave('_bracket_', ('[', ']'))
+    cave('_brace_', ('{', '}'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
