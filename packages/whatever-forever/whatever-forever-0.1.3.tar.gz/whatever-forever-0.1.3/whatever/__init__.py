f6a06fbbb7f9b16130e426d39d0a10cf33757871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:02 +0000

    Add package whatever-forever/0.1.3/whatever-forever-0.1.3.tar.gz

---
+++ packages/whatever-forever/whatever-forever-0.1.3.tar.gz/whatever-forever-0.1.3/whatever/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+# In[ ]:
+
+from whatever.chain import Chain, this, _X, _P
+from whatever import callables
+
+__version__ = "0.1.3"
+
+__all__ = [
+    'this', 'callables', '_X', '_P', 'Chain',
+]
