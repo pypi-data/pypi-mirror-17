3b1ebdda8e0877a511b541f3ebd3da457944b333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:20 +0000

    Add package asciify/0.0.5/asciify-0.0.5.tar.gz

---
+++ packages/asciify/asciify-0.0.5.tar.gz/asciify-0.0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="asciify",
+    packages=find_packages(),
+    version="0.0.5",
+    description="Fun little project",
+    long_description="",
+    url="https://github.com/paolopaolopaolo/ascii_text_generator/",
+    license="GNU",
+    author="Dean Mercado",
+    author_email="dpaolomercado@gmail.com",
+    setup_requires=[
+        "click==6.6"
+    ],
+    install_requires=[
+        "click==6.6"
+    ],
+    test_suite="",
+    entry_points={
+        "console_scripts": [
+            "asciify=asciify.asciify:main",
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha"
+    ]
+)
