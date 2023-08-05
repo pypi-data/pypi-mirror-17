1d461bd70c217a7600c9eeb73597ef7d548e444c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:21 +0000

    Add package yux_nester/1.0.0/yux_nester-1.0.0.tar.gz

---
+++ packages/yux_nester/yux_nester-1.0.0.tar.gz/yux_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'yux_nester',
+        version         = '1.0.0',
+        py_modules      = ['yux_nester'],
+        author          = 'hfpython',
+        author_email    = 'hfpython@headfirstlabs.com',
+        url             = 'http://www.headfirstlabs.com',
+        description     = 'A simple printer of nested lists',
+    )
