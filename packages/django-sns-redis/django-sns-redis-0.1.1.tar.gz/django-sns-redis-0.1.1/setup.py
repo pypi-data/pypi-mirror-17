666e7d89450253140d7ba7919d82df6d57872059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:31 +0000

    Add package django-sns-redis/0.1.1/django-sns-redis-0.1.1.tar.gz

---
+++ packages/django-sns-redis/django-sns-redis-0.1.1.tar.gz/django-sns-redis-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='django-sns-redis',
+    version='0.1.1',
+    packages=['snsredis',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
