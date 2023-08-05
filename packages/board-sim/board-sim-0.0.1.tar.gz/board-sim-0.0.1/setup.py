22fd4e34ec1cf4742ea1565c06559e45b7686b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:42 +0000

    Add package board-sim/0.0.1/board-sim-0.0.1.tar.gz

---
+++ packages/board-sim/board-sim-0.0.1.tar.gz/board-sim-0.0.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+#                       MIT License
+#
+# Copyright (c) 2016 Ennis Massey
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="board-sim",
+    version="0.0.1",
+    author="Ennis Massey",
+    author_email="ennisbaradine@gmail.com",
+    description="An extensible system for emulating circuits",
+    license="MIT License",
+    packages=find_packages(),
+    url="https://github.com/MicroTransactionsMatterToo/BoardSim",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only"
+    ]
+)
