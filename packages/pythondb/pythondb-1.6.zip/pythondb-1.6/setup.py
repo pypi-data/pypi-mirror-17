810c4cb1c845f18a6cd85cac642d6eebd838fa90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:20 +0000

    Add package pythondb/1.6/pythondb-1.6.zip

---
+++ packages/pythondb/pythondb-1.6.zip/pythondb-1.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pythondb',
+      version='1.6',
+      description='A fully featured database creation and maintenance tool using raw Python.',
+      url='https://github.com/Jackojc/pydatabase',
+      author='Jack Clarke',
+      author_email='jackojc@gmail.com',
+      license='MIT',
+      packages=['pydatabase'],
+      zip_safe=False)
