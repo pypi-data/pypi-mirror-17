ad57eda478681baeb3ae19d3b8e1c42766fa78cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:35 +0000

    Add package talkzoho/0.5.3/talkzoho-0.5.3.tar.gz

---
+++ packages/talkzoho/talkzoho-0.5.3.tar.gz/talkzoho-0.5.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+from pip.req import parse_requirements as parse
+
+
+setup(
+    name='talkzoho',
+    version='0.5.3',
+    url='https://github.com/A2Z-Cloud/Talk-Zoho',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    setup_requires=['setuptools-markdown'],
+    install_requires=[str(r.req) for r in parse('requirements/production.txt', session=False)],  # noqa
+    long_description_markdown_filename='README.md')
