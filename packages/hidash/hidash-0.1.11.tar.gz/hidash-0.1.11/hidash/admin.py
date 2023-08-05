64a6e410b681f293b347e5930d68c07e51075149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:23 +0000

    Add package hidash/0.1.11/hidash-0.1.11.tar.gz

---
+++ packages/hidash/hidash-0.1.11.tar.gz/hidash-0.1.11/hidash/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+import models
+
+
+admin.site.register(models.Chart)
+admin.site.register(models.ChartAuthGroup)
+admin.site.register(models.ChartAuthPermission)
+admin.site.register(models.ChartGroup)
+admin.site.register(models.ChartMetric)
