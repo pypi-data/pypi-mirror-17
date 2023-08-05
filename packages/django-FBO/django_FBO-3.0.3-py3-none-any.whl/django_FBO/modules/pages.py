c9819e46fa9d78d115139bff7a961cc6b87a3a64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:46 +0000

    Add package django-FBO/3.0.3/django_FBO-3.0.3-py3-none-any.whl

---
+++ packages/django-FBO/django_FBO-3.0.3-py3-none-any.whl/django_FBO/modules/pages.py
@@ -0,0 +1,50 @@
+import os.path
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.views.generic import DetailView
+
+from .. import FileObject, FBO, Q, Bakeable
+
+
+class PageFile(FileObject):
+
+    def get_absolute_url(self):
+        return reverse(
+            'page',
+            kwargs={
+                'slug': self.slug,
+            }
+        )
+
+
+class Page(FBO):
+    path = os.path.join(settings.BASE_DIR, 'pages')
+    _filters = [
+        ~Q(name__glob='*~'),
+    ]
+    metadata = FileObject.MetadataInFileHead
+    model = PageFile
+    slug_suffices = getattr(settings, 'FBO_DEFAULT_SLUG_SUFFICES', None)
+    slug_strip_index = True
+
+
+class PageView(Bakeable, DetailView):
+    template_name = 'page.html'
+    queryset = Page()
+    slug = None
+    
+    def get_object(self, queryset=None):
+        if queryset is None:
+            queryset = self.get_queryset()
+        if self.slug is not None:
+            return queryset.get(slug=self.slug)
+        else:
+            return super(PageView, self).get_object(queryset)
+
+    def get_paths(self):
+        # Remember that get_absolute_url() doesn't return
+        # an absolute URL, just netloc-relative.
+        return [
+            p.get_absolute_url()
+            for p in self.get_queryset().all()
+        ]
