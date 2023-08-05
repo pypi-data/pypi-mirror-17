f40fb54d58c7d52f25f25b6a0061ef19c01fef70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:27 +0000

    Add package colortools/0.1.2/colortools-0.1.2.tar.gz

---
+++ packages/colortools/colortools-0.1.2.tar.gz/colortools-0.1.2/setup.py
@@ -0,0 +1,60 @@
+# -*- coding: utf8 -*-
+#
+# This file were created by Python Boilerplate. Use boilerplate to start simple
+# usable and best-practices compliant Python projects.
+#
+# Learn more about it at: http://github.com/fabiommendes/boilerplate/
+#
+
+import os
+from setuptools import setup, find_packages
+
+
+# Meta information
+name = 'colortools'
+project = 'colortools'
+author = 'Fábio Macêdo Mendes'
+version = open('VERSION').read().strip()
+dirname = os.path.dirname(__file__)
+
+# Save version and author to __meta__.py
+file_name = os.path.join(dirname, 'src', project, '__meta__.py')
+with open(file_name, 'w', encoding='utf-8') as F:
+    F.write('__version__ = %r\n'
+            '__author__ = %r\n' % (version, author))
+
+setup(
+    # Basic info
+    name=name,
+    version=version,
+    author=author,
+    author_email='fabiomacedomendes@gmail.com',
+    url='',
+    description='A short description for your project.',
+    long_description=open('README.rst').read(),
+
+    # Classifiers (see https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+    ],
+
+    # Packages and depencies
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=[],
+    extras_require={
+        'dev': [
+            'pytest',
+            'python-boilerplate',
+        ],
+    },
+
+    # Other configurations
+    zip_safe=False,
+    platforms='any',
+)
