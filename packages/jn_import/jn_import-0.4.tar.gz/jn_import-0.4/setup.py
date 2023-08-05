9589e301ab0368aad1131ad98b357a1719b6afd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:25 +0000

    Add package jn_import/0.4/jn_import-0.4.tar.gz

---
+++ packages/jn_import/jn_import-0.4.tar.gz/jn_import-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='jn_import',
+      version='0.4',
+      description='Jupyter Notebooks Importing Tools',
+      url='https://github.com/maoxin/jn_import',
+      download_url='https://github.com/maoxin/jn_import/tarball/0.4',
+      author='maoxin',
+      author_email='maoxin.horizon@gmail.com',
+      license='MIT',
+      packages=['jn_import'],
+      zip_safe=False)
