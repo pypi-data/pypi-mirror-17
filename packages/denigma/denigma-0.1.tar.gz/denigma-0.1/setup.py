ccbe0b7efad4e2cf6530565d67a2a3acf05d1ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:25 +0000

    Add package denigma/0.1/denigma-0.1.tar.gz

---
+++ packages/denigma/denigma-0.1.tar.gz/denigma-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name='denigma',
+    packages=['denigma'],
+    version='0.1',
+    description='Digital Decipher Machine',
+    author='Hevok',
+    author_email='hevok@denigma.de',
+    url='https://github.com/hevok/denigma',
+    download_url='https://github.com/hevok/denigma/tarball/0.1',
+    keywords=['knowledge', 'semantics', 'reasoning'],
+    classifiers=[])
