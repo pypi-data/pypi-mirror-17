fe3c368570975182bc1ec5e1fff433acf32ab606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:57 +0000

    Add package searchandreco/1.23.dev1/searchandreco-1.23.dev1.tar.gz

---
+++ packages/searchandreco/searchandreco-1.23.dev1.tar.gz/searchandreco-1.23.dev1/searchandreco/tests/base.py
@@ -0,0 +1,28 @@
+'''
+__author__ : 'chitraangi'
+
+Base file
+'''
+
+import unittest
+
+
+class SearchDbTestCase(unittest.TestCase):
+    '''
+
+    unittest base class
+    '''
+
+    def setUp(self):
+        '''
+
+        :return:
+        '''
+        print ""
+
+    def tearDown(self):
+        '''
+
+        :return:
+        '''
+        print ""
---
+++ packages/searchandreco/searchandreco-1.23.dev1.tar.gz/searchandreco-1.23.dev1/searchandreco/urls.py
@@ -0,0 +1,50 @@
+'''
+__author__ = 'monica'
+__author__ = 'monica'
+
+The end points
+'''
+
+URLS = (
+    '/',
+    'Home',
+    ## we need to ensure that the reuqest come only respective IP// Not open
+    '/create_index/',
+    'CreateIndex',
+    ## we need to ensure that the reuqest come only respective IP// Not open
+    '/exists/',
+    'IndexExists',
+    '/delete_doc/',
+    'DeleteDocument',
+    '/index_doc/',
+    'IndexSingle',
+    '/update_doc/',
+    'UpdateDocument',
+
+    ###added urls
+    '/removeunpublisheddrafts',
+    'RemoveUnpublishedArticles',
+    '/indexpublisheddraftsprop',
+    'IndexPublishedDraftsArticlesProp',
+    '/updatedocumentprop',
+    'UpdateArticlesDocumentProp',
+    '/searchuserfacingsw',
+    'SearchSWUserFacing',
+    '/searchuserfacingvb',
+    'SearchVBUserFacing',
+    '/searchuserfacinggp',
+    'SearchGPUserFacing',
+    '/mltdocsprop',
+    'MltDocProp',
+
+    ####originals
+    '/indexoriginals',
+    'IndexOriginals',
+    '/mltoriginals',
+    'MltOriginals',
+    '/updateoriginals',
+    'UpdateOriginals',
+    '/deleteoriginals',
+    'DeleteOriginals',
+    '/searchuserfacingoriginals',
+    'SearchORIGINALSUserFacing')
