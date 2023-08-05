17ddee6dcba29a74341ecb1521a35b953720a974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:37 +0000

    Add package github-email-explorer/0.2.7/github-email-explorer-0.2.7.tar.gz

---
+++ packages/github-email-explorer/github-email-explorer-0.2.7.tar.gz/github-email-explorer-0.2.7/github_email_explorer/api_url.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from urllib import urlencode
+from urlparse import parse_qs, urlsplit, urlunsplit
+
+
+class GitHubEndPoint(object):
+    api_url = 'https://api.github.com/'
+
+    @staticmethod
+    def user_profile(user_id):
+        return '{}users/{}'.format(GitHubEndPoint.api_url, user_id)
+
+    @staticmethod
+    def repository(user_id, repo):
+        return '{}repos/{}/{}'.format(GitHubEndPoint.api_url, user_id, repo)
+
+    @staticmethod
+    def stargazers(user_id, repo):
+        return '{}repos/{}/{}/stargazers'.format(GitHubEndPoint.api_url, user_id, repo)
+
+    @staticmethod
+    def forks(user_id, repo):
+        return '{}repos/{}/{}/forks'.format(GitHubEndPoint.api_url, user_id, repo)
+
+    @staticmethod
+    def watchers(user_id, repo):
+        return '{}repos/{}/{}/subscribers'.format(GitHubEndPoint.api_url, user_id, repo)
+
+    @staticmethod
+    def rate_limit():
+        return '{}rate_limit'.format(GitHubEndPoint.api_url)
+
+    @staticmethod
+    def add_auth_info(url, github_api_auth):
+        if github_api_auth is None:
+            github_api_auth = ('', '')
+        url = set_url_parameter(url, 'client_id', github_api_auth[0])
+        url = set_url_parameter(url, 'client_secret', github_api_auth[1])
+        return url
+
+    @staticmethod
+    def pagination(url, page=1, per_page=100):
+        url = set_url_parameter(url, 'page', page)
+        url = set_url_parameter(url, 'per_page', per_page)
+        return url
+
+
+def set_url_parameter(url, param_name, param_value):
+    """Set or replace a query parameter and return the modified URL.
+
+    >>> set_url_parameter('http://example.com?foo=bar&biz=baz', 'foo', 'stuff')
+    'http://example.com?foo=stuff&biz=baz'
+
+    """
+    scheme, netloc, path, query_string, fragment = urlsplit(url)
+    query_params = parse_qs(query_string)
+
+    query_params[param_name] = [param_value]
+    new_query_string = urlencode(query_params, doseq=True)
+
+    return urlunsplit((scheme, netloc, path, new_query_string, fragment))
---
+++ packages/github-email-explorer/github-email-explorer-0.2.7.tar.gz/github-email-explorer-0.2.7/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import re
+from setuptools import setup, find_packages
+
+pkg_file = open("github_email_explorer/__init__.py").read()
+metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", pkg_file))
+description = open('README.md').read()
+requirements = [i.strip() for i in open("requirements.txt").readlines()]
+
+setup(
+    name='github-email-explorer',
+    description='A tool to get email addresses by action types such as starred, watching or fork on GitHub repositories; Send email content to those addresses with a template.',
+    version=metadata['version'],
+
+    # Author details
+    author='yuecen',
+    author_email='yuecendev+pypi@gmail.com',
+    url='https://github.com/yuecen/github-email-explorer',
+    long_description=description,
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Email',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='github, email, sendgrid, marketing',
+    install_requires=requirements,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'ge-explore = github_email_explorer.cli_explore:get_github_email_by_repo',
+            'ge-sendgrid = github_email_explorer.cli_sendgrid:send_email_by_sendgrid'
+        ]
+    }
+)
