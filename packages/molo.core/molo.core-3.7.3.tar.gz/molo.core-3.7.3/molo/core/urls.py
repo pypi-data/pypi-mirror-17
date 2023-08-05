254bb4be2d1054d249373af411337708050b73b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:43 +0000

    Add package molo.core/3.7.3/molo.core-3.7.3.tar.gz

---
+++ packages/molo.core/molo.core-3.7.3.tar.gz/molo.core-3.7.3/molo/core/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import patterns, include, url
+
+
+urlpatterns = patterns(
+    '',
+    url(
+        r'^locale/(?P<locale>[\w\-\_]+)/$',
+        'molo.core.views.locale_set',
+        name='locale_set'
+    ),
+
+    url(
+        r'^health/$',
+        'molo.core.views.health',
+        name='health'
+    ),
+
+    url(r'^import/', include(
+        'molo.core.content_import.urls', namespace='content_import')),
+    url(
+        r'^versions/$',
+        'molo.core.views.versions',
+        name='versions'),
+    url(r'^djga/', include('google_analytics.urls')),
+)
