ddb15aeccb81114648e2aef42157416c6bd7e84f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:00 +0000

    Add package zeep/0.15.0/zeep-0.15.0.tar.gz

---
+++ packages/zeep/zeep-0.15.0.tar.gz/zeep-0.15.0/tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from lxml.etree import _Element
+
+
+pytest.register_assert_rewrite('tests.utils')
+
+
+@pytest.fixture(autouse=True)
+def no_requests(request, monkeypatch):
+    if request.node.get_marker('requests'):
+        return
+
+    def func(*args, **kwargs):
+        pytest.fail("External connections not allowed during tests.")
+
+    monkeypatch.setattr("socket.socket", func)
