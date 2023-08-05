8167d73f7e295a6ad7c8a3b5fa487721dd5d4a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:20 +0000

    Add package Novelty/1.0.1/Novelty-1.0.1.tar.gz

---
+++ packages/Novelty/Novelty-1.0.1.tar.gz/Novelty-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='Novelty',
+    version='1.0.1',
+    packages=['Novelty'],
+    url='https://github.com/Fuzen-py/Novelty',
+    license='MIT',
+    author='Fuzen.py',
+    author_email='',
+    description='Python 3 Aiohttp Novelupdates Scraper',
+    long_description='Based on Raitonoberu, Novelty is a Novelupdates scraper to provide a convenient access',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries'
+    ],
+    keywords="NovelUpdates asyncio aiohttp scraping Novelty",
+    install_requires=['aiohttp', 'bs4', 'lxml'])
