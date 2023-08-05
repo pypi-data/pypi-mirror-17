5925e60879d8ed726ff9834148e98b1922bee60d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:39 +0000

    Add package zict/0.0.3/zict-0.0.3.tar.gz

---
+++ packages/zict/zict-0.0.3.tar.gz/zict-0.0.3/zict/tests/test_buffer.py
@@ -0,0 +1,45 @@
+from zict import Buffer
+
+
+def test_simple():
+    a = dict()
+    b = dict()
+    buff = Buffer(a, b, n=10, weight=lambda k, v: v)
+
+    buff['x'] = 1
+    buff['y'] = 2
+
+    assert buff['x'] == 1
+    assert buff['y'] == 2
+    assert a == {'x': 1, 'y': 2}
+    assert buff.fast.total_weight == 3
+
+    buff['z'] = 8
+    assert a == {'y': 2, 'z': 8}
+    assert b == {'x': 1}
+
+    assert buff['x'] == 1
+    assert a == {'x': 1, 'z': 8}
+    assert b == {'y': 2}
+
+    assert 'x' in buff
+    assert 'y' in buff
+    assert 'missing' not in buff
+
+    del buff['z']
+    assert a == {'x': 1}
+    assert b == {'y': 2}
+
+    del buff['y']
+    assert a == {'x': 1}
+    assert b == {}
+
+    assert 'y' not in buff
+
+    buff['a'] = 5
+    assert set(buff) == set(buff.keys()) == {'a', 'x'}
+
+    fast_keys = set(buff.fast)
+    buff['b'] = 1000
+    assert 'b' in buff.slow
+    assert set(buff.fast) == fast_keys
