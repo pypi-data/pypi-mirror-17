434c3e6af66c711dc2414a26dd557f6506c98746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:47 +0000

    Add package daemail/0.3.0/daemail-0.3.0.tar.gz

---
+++ packages/daemail/daemail-0.3.0.tar.gz/daemail-0.3.0/daemail/__init__.py
@@ -0,0 +1,5 @@
+__version__      = '0.3.0'
+__author__       = 'John Thorvald Wodder II'
+__author_email__ = 'daemail@varonathe.org'
+__license__      = 'MIT'
+__url__          = 'https://github.com/jwodder/daemail'
