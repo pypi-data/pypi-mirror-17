bb324b68d547aff418381e64bae55b3b5362b770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:05 +0000

    Add package docl/0.21/docl-0.21.tar.gz

---
+++ packages/docl/docl-0.21.tar.gz/docl-0.21/docl/subprocess.py
@@ -0,0 +1,44 @@
+########
+# Copyright (c) 2016 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+
+
+import subprocess
+import sys
+
+import proxy_tools
+import sh
+
+from docl.configuration import configuration
+
+
+def bake(cmd):
+    return cmd.bake(_err_to_out=True,
+                    _out=lambda l: sys.stdout.write(l),
+                    _tee=True)
+
+
+def docker_proxy():
+    return bake(sh.docker).bake('-H', configuration.docker_host)
+
+
+docker = proxy_tools.Proxy(docker_proxy)
+ssh_keygen = bake(sh.Command('ssh-keygen'))
+cfy = bake(sh.cfy)
+
+
+def ssh(ip, keypath):
+    subprocess.call(['ssh', '-i', keypath, 'root@{}'.format(ip)])
