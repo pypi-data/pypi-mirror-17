8d547a9d67b55bf1b69c48c3ebcafa663dc0055c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:50 +0000

    Add package geometalab.drf-utm-zone-info/0.1.0/geometalab.drf_utm_zone_info-0.1.0-py2.py3-none-any.whl

---
+++ packages/geometalab.drf-utm-zone-info/geometalab.drf_utm_zone_info-0.1.0-py2.py3-none-any.whl/utm_zone_info/__init__.py
@@ -0,0 +1,14 @@
+from . import coordinate_reference_system
+from . import serializers
+from . import urls
+from . import views
+
+__version__ = '0.1.0'
+
+__all__ = [
+    'coordinate_reference_system',
+    'serializers',
+    'urls',
+    'views',
+    '__version__',
+]
