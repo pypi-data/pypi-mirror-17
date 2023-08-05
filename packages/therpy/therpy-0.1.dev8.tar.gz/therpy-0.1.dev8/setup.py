51b17e9cbfdf12ff90060d9e3a31b271857fb6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:55 +0000

    Add package therpy/0.1.dev8/therpy-0.1.dev8.tar.gz

---
+++ packages/therpy/therpy-0.1.dev8.tar.gz/therpy-0.1.dev8/setup.py
@@ -0,0 +1,20 @@
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name='therpy',
+    version='0.1dev8',
+    #packages=['therpy',],
+    packages = find_packages(),
+    package_data = {'therpy.Data':['*']},
+    include_package_data = True,
+    license='Open Source',
+    author='Parth Patel',
+    author_email='pbpatel@mit.edu',
+    install_requires=['pyfits','tabulate','tqdm'],
+)
+
+# Run following commands on terminal
+# python setup.py sdist
+# python setup.py register (ONLY FIRST TIME)
+# python setup.py sdist upload
