0e6ff71af18c7e2bceebe10a1c4b83d2f7572548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:19 +0000

    Add package Products.CMFDiffTool/2.2.1/Products.CMFDiffTool-2.2.1.tar.gz

---
+++ packages/Products.CMFDiffTool/Products.CMFDiffTool-2.2.1.tar.gz/Products.CMFDiffTool-2.2.1/Products/CMFDiffTool/ListDiff.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from App.class_init import InitializeClass
+from Products.CMFDiffTool.FieldDiff import FieldDiff
+
+
+class ListDiff(FieldDiff):
+    """Text difference"""
+
+    meta_type = "List Diff"
+
+    def _parseField(self, value, filename=None):
+        """Parse a field value in preparation for diffing"""
+        # Return the list as is for diffing
+        if type(value) is set:
+            # A set cannot be indexed, so return a list of a set
+            return list(value)
+        else:
+            return value
+
+InitializeClass(ListDiff)
---
+++ packages/Products.CMFDiffTool/Products.CMFDiffTool-2.2.1.tar.gz/Products.CMFDiffTool-2.2.1/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+version = '2.2.1'
+long_description = open("README.rst").read()
+long_description += "\n"
+long_description += open("CHANGES.rst").read()
+
+setup(name='Products.CMFDiffTool',
+      version=version,
+      description="Diff tool for Plone",
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Framework :: Plone',
+          'Framework :: Plone :: 4.3',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          ],
+      keywords='Diff Plone',
+      author='Brent Hendricks',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/Products.CMFDiffTool',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+          test=[
+              'zope.component',
+              'Products.CMFTestCase',
+              'collective.testcaselayer',
+              'plone.namedfile',
+              'plone.app.dexterity',
+          ]
+      ),
+      install_requires=[
+          'setuptools',
+          'zope.interface',
+          'Products.CMFCore',
+          'Products.GenericSetup',
+          'Acquisition',
+          'Zope2',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
