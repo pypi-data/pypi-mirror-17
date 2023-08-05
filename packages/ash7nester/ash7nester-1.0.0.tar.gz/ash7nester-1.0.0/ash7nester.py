90dd92ac9c53c41a9af801ca0d8536a8a275f6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:21 +0000

    Add package ash7nester/1.0.0/ash7nester-1.0.0.tar.gz

---
+++ packages/ash7nester/ash7nester-1.0.0.tar.gz/ash7nester-1.0.0/ash7nester.py
@@ -0,0 +1,12 @@
+""""This is a nester.py module and it provides a function print_lol which prints lists that may or may not include nested lists"""
+def print_lol(the_list,level):
+        """This Function takes the_list and level as positional arguments which can be any python list and a number.
+        Each data item in the list is printed (recursively) to its own line and the nested list are indented based upon the
+        level of indentation"""
+        for each_item in the_list:
+                if isinstance(each_item, list):
+                    print_lol(each_item,level+1)
+                else:
+                    for num in range(level):
+                        print("\t", end='')
+                    print(each_item)
---
+++ packages/ash7nester/ash7nester-1.0.0.tar.gz/ash7nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='ash7nester',
+    version='1.0.0',
+    py_modules=['ash7nester'],
+    author='Ashish Singh',
+    author_email='singhashish_7@yahoo.com',
+    description='A simple printer of nested lists',
+    )
+    
