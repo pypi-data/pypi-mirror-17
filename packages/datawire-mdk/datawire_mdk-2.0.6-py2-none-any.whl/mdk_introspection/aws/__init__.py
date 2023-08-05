be11dc72dc5a547fd19e9ca542df9fce472e2e17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:22 +0000

    Add package datawire-mdk/2.0.6/datawire_mdk-2.0.6-py2-none-any.whl

---
+++ packages/datawire-mdk/datawire_mdk-2.0.6-py2-none-any.whl/mdk_introspection/aws/__init__.py
@@ -0,0 +1,58 @@
+# Quark 1.0.406 run at 2016-08-31 18:17:52.891343
+from quark_runtime import *
+
+import quark_runtime
+import quark_threaded_runtime
+import quark_runtime_logging
+import quark_ws4py_fixup
+import mdk_runtime_files
+import quark.reflect
+import mdk_introspection
+import mdk_runtime
+import datawire_mdk_md
+
+
+class Ec2Host(_QObject):
+    def _init(self):
+        self.scope = None
+        self.env = None
+
+    def __init__(self, env, scope):
+        self._init()
+        (self).scope = (scope).upper()
+        (self).env = env
+
+    @staticmethod
+    def metadataHost(env):
+        return ((env).var(u"DATAWIRE_METADATA_HOST_OVERRIDE")).orElseGet(u"169.254.169.254")
+
+    def get(self):
+        if ((self.scope) == (u"INTERNAL")):
+            return _url_get(((u"http://") + (Ec2Host.metadataHost(self.env))) + (u"/latest/meta-data/local-hostname"))
+
+        if ((self.scope) == (u"PUBLIC")):
+            return _url_get(((u"http://") + (Ec2Host.metadataHost(self.env))) + (u"/latest/meta-data/public-hostname"))
+
+        return _cast(None, lambda: unicode)
+
+    def _getClass(self):
+        return u"mdk_introspection.aws.Ec2Host"
+
+    def _getField(self, name):
+        if ((name) == (u"scope")):
+            return (self).scope
+
+        if ((name) == (u"env")):
+            return (self).env
+
+        return None
+
+    def _setField(self, name, value):
+        if ((name) == (u"scope")):
+            (self).scope = _cast(value, lambda: unicode)
+
+        if ((name) == (u"env")):
+            (self).env = _cast(value, lambda: mdk_runtime.EnvironmentVariables)
+
+
+Ec2Host.mdk_introspection_aws_Ec2Host_ref = None
---
+++ packages/datawire-mdk/datawire_mdk-2.0.6-py2-none-any.whl/quark/spi/__init__.py
@@ -0,0 +1,68 @@
+# Quark 1.0.406 run at 2016-08-31 18:17:52.891343
+from quark_runtime import *
+
+import quark_runtime
+import quark_threaded_runtime
+import quark_runtime_logging
+import quark_ws4py_fixup
+import mdk_runtime_files
+import quark.reflect
+import quark
+import datawire_mdk_md
+import quark.logging
+import quark.spi_api_tracing
+import quark.spi_api
+
+
+class RuntimeSpi(object):
+    pass
+RuntimeSpi.quark_spi_RuntimeSpi_ref = None
+class RuntimeFactory(_QObject):
+    def _init(self):
+        pass
+    def __init__(self): self._init()
+
+    def makeRuntime(self):
+        spi = _RuntimeFactory.create();
+        api = None;
+        if (not (RuntimeFactory.env_checked)):
+            RuntimeFactory.enable_tracing = (quark.logging.Config._getOverrideIfExists()) != (None)
+            RuntimeFactory.env_checked = True
+
+        if (RuntimeFactory.enable_tracing):
+            api = quark.spi_api_tracing.RuntimeProxy(spi)
+        else:
+            api = quark.spi_api.RuntimeProxy(spi)
+
+        return api
+
+    def _getClass(self):
+        return u"quark.spi.RuntimeFactory"
+
+    def _getField(self, name):
+        if ((name) == (u"factory")):
+            return RuntimeFactory.factory
+
+        if ((name) == (u"enable_tracing")):
+            return RuntimeFactory.enable_tracing
+
+        if ((name) == (u"env_checked")):
+            return RuntimeFactory.env_checked
+
+        return None
+
+    def _setField(self, name, value):
+        if ((name) == (u"factory")):
+            RuntimeFactory.factory = _cast(value, lambda: RuntimeFactory)
+
+        if ((name) == (u"enable_tracing")):
+            RuntimeFactory.enable_tracing = _cast(value, lambda: bool)
+
+        if ((name) == (u"env_checked")):
+            RuntimeFactory.env_checked = _cast(value, lambda: bool)
+
+
+RuntimeFactory.factory = RuntimeFactory()
+RuntimeFactory.enable_tracing = True
+RuntimeFactory.env_checked = False
+RuntimeFactory.quark_spi_RuntimeFactory_ref = None
