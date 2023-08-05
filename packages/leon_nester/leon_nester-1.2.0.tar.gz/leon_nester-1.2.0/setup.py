76b1f6452395473465768a54c57834d614de068a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:11 +0000

    Add package leon_nester/1.2.0/leon_nester-1.2.0.tar.gz

---
+++ packages/leon_nester/leon_nester-1.2.0.tar.gz/leon_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name          = 'leon_nester',
+    version       = '1.2.0',
+    py_modules    = ['nester'],
+    author        = 'Leon',
+    author_email  = 'wangliang0210@gmail.com',
+    url           = 'http://www.headfirstlabs.com',
+    description   = 'A Nester',
+    )
