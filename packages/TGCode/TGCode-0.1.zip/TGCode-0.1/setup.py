81c9d9e48f2498835775d2290c0807dfa381dd38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:53 +0000

    Add package TGCode/0.1/TGCode-0.1.zip

---
+++ packages/TGCode/TGCode-0.1.zip/TGCode-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name = "TGCode",
+    version = "0.1",
+    packages = find_packages(),
+    author = "thegamerbr1",
+    author_email = "xthegamerbr1x@gmail.com",
+    description = "TGCode is a simple library that let's you convert binary data into a string and vice-versa.",
+    license = "MIT",
+    long_description = open('readme.txt', 'r').read(),
+    keywords = "encoding binary bytes string data irc file bin convert ",
+)
