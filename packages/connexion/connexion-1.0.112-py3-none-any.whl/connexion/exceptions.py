9cc21093db30ac1c968ff68a541ccf364e36595b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:58 +0000

    Add package connexion/1.0.112/connexion-1.0.112-py3-none-any.whl

---
+++ packages/connexion/connexion-1.0.112-py3-none-any.whl/connexion/exceptions.py
@@ -0,0 +1,57 @@
+"""
+Copyright 2015 Zalando SE
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+ language governing permissions and limitations under the License.
+"""
+
+
+class ConnexionException(BaseException):
+    pass
+
+
+class InvalidSpecification(ConnexionException):
+    def __init__(self, reason='Unknown Reason'):
+        """
+        :param reason: Reason why the specification is invalid
+        :type reason: str
+        """
+        self.reason = reason
+
+    def __str__(self):  # pragma: no cover
+        return '<InvalidSpecification: {}>'.format(self.reason)
+
+    def __repr__(self):  # pragma: no cover
+        return '<InvalidSpecification: {}>'.format(self.reason)
+
+
+class NonConformingResponse(ConnexionException):
+    def __init__(self, reason='Unknown Reason', message=None):
+        """
+        :param reason: Reason why the response did not conform to the specification
+        :type reason: str
+        """
+        self.reason = reason
+        self.message = message
+
+    def __str__(self):  # pragma: no cover
+        return '<NonConformingResponse: {}>'.format(self.reason)
+
+    def __repr__(self):  # pragma: no cover
+        return '<NonConformingResponse: {}>'.format(self.reason)
+
+
+class NonConformingResponseBody(NonConformingResponse):
+    def __init__(self, message, reason="Response body does not conform to specification"):
+        super(NonConformingResponseBody, self).__init__(reason=reason, message=message)
+
+
+class NonConformingResponseHeaders(NonConformingResponse):
+    def __init__(self, message, reason="Response headers do not conform to specification"):
+        super(NonConformingResponseHeaders, self).__init__(reason=reason, message=message)
