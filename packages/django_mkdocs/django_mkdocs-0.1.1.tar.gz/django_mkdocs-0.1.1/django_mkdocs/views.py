e695dea842b7500c8ef7f95b283c881ff5472b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:08 +0000

    Add package django_mkdocs/0.1.1/django_mkdocs-0.1.1.tar.gz

---
+++ packages/django_mkdocs/django_mkdocs-0.1.1.tar.gz/django_mkdocs-0.1.1/django_mkdocs/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse, Http404
+import app_settings
+from django.views.static import serve
+import mimetypes
+
+DOCUMENTATION_ROOT = 'index.html'
+
+
+def documentation(request, path):
+    if path == '':
+        path = DOCUMENTATION_ROOT
+    if not app_settings.DOCUMENTATION_ACCESS_FUNCTION(request.user):
+        raise Http404
+    if not app_settings.DOCUMENTATION_XSENDFILE:
+        return serve(
+            request,
+            path,
+            app_settings.DOCUMENTATION_HTML_ROOT)
+    mimetype, encoding = mimetypes.guess_type(path)
+    response = HttpResponse(content_type=mimetype)
+
+    response['Content-Encoding'] = encoding
+    response['Content-Disposition'] = ''
+    response['X-Sendfile'] = "".join([app_settings.DOCUMENTATION_HTML_ROOT,
+                                      path])
+    return response
