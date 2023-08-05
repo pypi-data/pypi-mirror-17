653ffcb5e97a28c1ccccad0507458cc963d247b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:18 +0000

    Add package booleanOperations/0.5.2/booleanOperations-0.5.2-py2.py3-none-any.whl

---
+++ packages/booleanOperations/booleanOperations-0.5.2-py2.py3-none-any.whl/booleanOperations/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import print_function, division, absolute_import
+from .booleanOperationManager import BooleanOperationManager
+
+# export BooleanOperationManager static methods
+union = BooleanOperationManager.union
+difference = BooleanOperationManager.difference
+intersection = BooleanOperationManager.intersection
+xor = BooleanOperationManager.xor
+getIntersections = BooleanOperationManager.getIntersections
