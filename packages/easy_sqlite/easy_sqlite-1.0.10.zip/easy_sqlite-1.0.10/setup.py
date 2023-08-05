2d9b7d937875bd35932b8261e952cdc53ce27fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:09 +0000

    Add package easy_sqlite/1.0.10/easy_sqlite-1.0.10.zip

---
+++ packages/easy_sqlite/easy_sqlite-1.0.10.zip/easy_sqlite-1.0.10/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from setuptools.command.install import install
+import os
+
+class CustomInstallCommand(install):
+    """Customized setuptools install command - prints a friendly greeting."""
+    def run(self):
+        install.run(self)
+        print("RUNNING CUSTOM SCRIPT")
+        print(os.getcwd())
+
+setup(
+  name = 'easy_sqlite',
+  packages = ['easy_sqlite'], # this must be the same as the name above
+  version = '1.0.10',
+  description = 'This Library makes using the sqlite3 library much easier and faster.',
+  author = 'Calder White',
+  author_email = 'calderwhite1@gmail.com',
+  url = 'https://github.com/CalderWhite/easy_sqlite', # use the URL to the github repo
+  download_url = 'https://github.com/CalderWhite/easy_sqlite/archive/master.zip', # I'll explain this in a second
+  keywords = [], # arbitrary keywords
+  classifiers = [],
+  cmdclass={
+        'install': CustomInstallCommand,
+    }
+)
