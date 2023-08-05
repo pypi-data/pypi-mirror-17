d75e647054dc1e9f0f6c2bebd8835fcc959505bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:45 +0000

    Add package geomeppy/0.1.0/geomeppy-0.1.0.zip

---
+++ packages/geomeppy/geomeppy-0.1.0.zip/geomeppy-0.1.0/geomeppy/__init__.py
@@ -0,0 +1,11 @@
+"""version number"""
+from geomeppy.intersect_match import intersect_idf_surfaces
+from geomeppy.intersect_match import match_idf_surfaces
+from geomeppy.recipes import set_wwr
+from geomeppy.view_geometry import view_idf
+
+__all__ = ['intersect_idf_surfaces',
+           'match_idf_surfaces',
+           'set_wwr',
+           'view_idf',
+           ]
---
+++ packages/geomeppy/geomeppy-0.1.0.zip/geomeppy-0.1.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+setup(
+    name='geomeppy',
+    packages=['geomeppy',
+              'tests',
+              ],
+    version='0.1.0',
+    description='Geometry editing for E+ idf files',
+    author='Jamie Bull',
+    author_email='jamie.bull@oco-carbon.com',
+    url='https://github.com/jamiebull1/geomeppy',
+    download_url='https://github.com/jamiebull1/geomeppy/tarball/v0.1.0',
+    license='MIT License',
+    keywords=['EnergyPlus', 
+              'geometry',
+              'building performance simulation',
+              ],
+    platforms='any',
+    install_requires = [
+        "eppy>=0.5.2",
+        "numpy>=1.11.1",
+        "six>=1.10.0",  # python2/3 compatibility
+        "pyclipper>=1.0.2",  # geometry intersection
+        "transforms3d>=0.3",  # geometry transformations
+        "matplotlib>=1.5.1",  # simple geometry viewer
+        ],
+    classifiers = [
+        'Programming Language :: Python :: 2',
+#        'Programming Language :: Python :: 3',  # on hold until Eppy updates
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering',
+        ],
+)
