7633b65a19dd823555893ceee9f1f82ed0588751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:53 +0000

    Add package curlify/1.2.1/curlify-1.2.1.tar.gz

---
+++ packages/curlify/curlify-1.2.1.tar.gz/curlify-1.2.1/curlify.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+
+def to_curl(request):
+    headers = ["'{0}: {1}'".format(k, v) for k, v in request.headers.items()]
+    headers = " -H ".join(sorted(headers))
+
+    command = "curl -X {method} -H {headers} -d '{data}' '{uri}'".format(
+        data=request.body or "",
+        headers=headers,
+        method=request.method,
+        uri=request.url,
+    )
+    return command
