445e6a0d31546f8db9fd7c6fe510d4815b5c2ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:25 +0000

    Add package num-solver/0.1.3/num_solver-0.1.3.tar.gz

---
+++ packages/num-solver/num_solver-0.1.3.tar.gz/num_solver-0.1.3/num_solver/solver.py
@@ -0,0 +1,28 @@
+import scipy.optimize
+import numpy as np
+
+def solve_num_problem(utility, R, capacity, maxiter=1000):
+    num_resources,num_flows = R.shape
+
+    # the scipy solver has problems if the capacity of any link is zero
+    capacity = capacity.clip(min=0.00001)
+
+    f = lambda x: -1*utility(x)
+    x0 = np.array([1. for _ in range(num_flows)])
+
+    res = scipy.optimize.minimize(f, x0,
+                                  bounds=[(0., None) for _ in x0],
+                                  constraints=[{'type': 'ineq',
+                                                'fun': lambda x: np.asarray(capacity - np.dot(R, x)).flatten()}],
+                                  options={
+                                      'maxiter': maxiter
+                                  })
+
+    if not res.success:
+        print res
+        raise Exception("Couldn't solve NUM problem")
+
+    return res.x
+
+def solve(*args):
+    return solve_num_problem(*args)
---
+++ packages/num-solver/num_solver-0.1.3.tar.gz/num_solver-0.1.3/test/test_num.py
@@ -0,0 +1,72 @@
+import unittest
+import numpy as np
+
+from num_solver import *
+
+DELTA = 0.01
+
+class TestNUM(unittest.TestCase):
+    def test_equal_shared_link(self):
+        utility = lambda x: np.sum(np.ma.log(x))
+        R = np.array([[1., 1.]])
+        capacity = np.array([10.])
+
+        res = solve_num_problem(utility, R, capacity)
+        self.assertTrue(abs(5. - res[0]) < DELTA)
+        self.assertTrue(abs(5. - res[1]) < DELTA)
+
+    def test_solve_alias(self):
+        utility = lambda x: np.sum(np.ma.log(x))
+        R = np.array([[1., 1.]])
+        capacity = np.array([10.])
+
+        res = solve(utility, R, capacity)
+        self.assertTrue(abs(5. - res[0]) < DELTA)
+        self.assertTrue(abs(5. - res[1]) < DELTA)
+
+    def test_unequal_shared_link(self):
+        w = np.array([1., 2.])
+        utility = lambda x: np.dot(w, np.ma.log(x))
+        R = np.array([[1., 1.]])
+        capacity = np.array([9.])
+
+        res = solve_num_problem(utility, R, capacity)
+        self.assertTrue(abs(3. - res[0]) < DELTA)
+        self.assertTrue(abs(6. - res[1]) < DELTA)
+
+    def test_unshared_link(self):
+        w = np.array([1., 2.])
+        utility = lambda x: np.dot(w, np.ma.log(x))
+        R = np.array([[1., 0.], [0., 1.]])
+        capacity = np.array([10., 10.])
+
+        res = solve_num_problem(utility, R, capacity)
+        self.assertTrue(abs(10. - res[0]) < DELTA)
+        self.assertTrue(abs(10. - res[1]) < DELTA)
+
+    def test_zero_capacity(self):
+        w = np.array([1., 2.])
+        utility = lambda x: np.dot(w, np.ma.log(x))
+        R = np.array([[1., 0.], [0., 1.]])
+        capacity = np.array([10., 0.])
+
+        res = solve_num_problem(utility, R, capacity)
+        self.assertTrue(abs(10. - res[0]) < DELTA)
+        self.assertTrue(abs(0. - res[1]) < DELTA)
+
+    def test_many_resources(self):
+        R = np.matrix([[1., 0., 0.],
+                       [0., 1., 0.],
+                       [0., 0., 1.],
+                       [1., 1., 0.],
+                       [0., 1., 1.],
+                       [1., 0., 1.],
+                       [1., 1., 1.]])
+        capacity = np.array([30., 5., 30., 20., 10., 10., 20.])
+        utility = lambda x: np.sum(np.ma.log(x))
+
+        solve_num_problem(utility, R, capacity)
+
+
+if __name__ == '__main__':
+    unittest.main()
