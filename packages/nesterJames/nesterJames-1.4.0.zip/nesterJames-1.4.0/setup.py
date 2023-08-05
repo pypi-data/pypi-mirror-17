558c030f300bfc4519c5646064fe9d8d54b113bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:04 +0000

    Add package nesterJames/1.4.0/nesterJames-1.4.0.zip

---
+++ packages/nesterJames/nesterJames-1.4.0.zip/nesterJames-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='nesterJames',
+        version='1.4.0',
+        py_modules=['nester'],
+        author='JamesLiu',
+        autho_email='hfpython@headfirstlabs.com',
+        url='http://www.headfirstlabs.com',
+        description='A simple printer of hested lists',
+        )
