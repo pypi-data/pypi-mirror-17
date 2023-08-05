a90c6beff6a22c59ee45540d74fc5a658d773d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:40 +0000

    Add package frogp1228_nester/1.0.10/frogp1228_nester-1.0.10.tar.gz

---
+++ packages/frogp1228_nester/frogp1228_nester-1.0.10.tar.gz/frogp1228_nester-1.0.10/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'frogp1228_nester',
+	version = '1.0.10',
+	py_modules = ['frogp1228_nester'],
+	author = 'hfpython',
+	author_email = 'hfpython',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of nester lists',
+	)
