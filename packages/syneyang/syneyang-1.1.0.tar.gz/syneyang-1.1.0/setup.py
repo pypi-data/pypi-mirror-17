c1f3e27c7669ff5ae8f9b36aa0e4eb0da3fe7113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:40 +0000

    Add package syneyang/1.1.0/syneyang-1.1.0.tar.gz

---
+++ packages/syneyang/syneyang-1.1.0.tar.gz/syneyang-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='syneyang',
+    version='1.1.0',
+    py_modules=['nester'],
+    author='syneyang',
+    author_email='syne.yang@gmail.com',
+    url='http://www.jiangbingguozhi.com',
+    description='This just a test!',
+
+)
