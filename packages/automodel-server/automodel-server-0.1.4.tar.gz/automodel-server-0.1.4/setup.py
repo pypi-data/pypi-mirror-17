d9263854ecaa1b1ebb18d2ef7109eeb0fc9f65e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:15 +0000

    Add package automodel-server/0.1.4/automodel-server-0.1.4.tar.gz

---
+++ packages/automodel-server/automodel-server-0.1.4.tar.gz/automodel-server-0.1.4/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='automodel-server',
+    version='0.1.4',
+    url='https://bitbucket.org/jlalmeidaf/automodel-server-with-hmmer',
+    license='',
+    author='João Luiz de Almeida Filho, Jorge Hernandez Fernandez',
+    author_email='joaoluiz.af@gmail.com',
+    keywords='protein modeling modeller automodel',
+    description=u'Server of service for structure protein prediction',
+    packages=['AutomodelServerModules', 'modellerstep', 'modellingfile', 'pdb', 'tests'],
+    install_requires=['argparse', 'biopython', 'rpyc', 'matplotlib'],
+    scripts=['server']
+)
