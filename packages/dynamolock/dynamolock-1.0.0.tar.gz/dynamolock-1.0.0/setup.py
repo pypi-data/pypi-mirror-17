aa0aba49bc7405f10d35489c05b6ef783fe74080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:50 +0000

    Add package dynamolock/1.0.0/dynamolock-1.0.0.tar.gz

---
+++ packages/dynamolock/dynamolock-1.0.0.tar.gz/dynamolock-1.0.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name  = 'dynamolock',
+    version = '1.0.0',
+    description = 'A distributed lock system build on top of DynamoDB',
+    download_url = 'https://github.com/alphagov/dynamolock/tarball/1.0.0',
+    long_description='A distributed lock system build on top of DynamoDB',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: System :: Networking',
+        'Topic :: Utilities'
+    ],
+    keywords = 'python',
+    license = 'BSD',
+    packages = find_packages(),
+    platforms = ['Linux', 'Mac OS X', 'Win'],
+    include_package_data = True,
+    zip_safe = True,
+    install_requires = [ 'boto >= 2.32.0' ],
+    extras_require = {
+        'quality'   : [ 'coverage >= 3.5.3', 'nose >= 1.3.1', 'mock >= 1.0.0', 'pep8 >= 1.3.3' ],
+        'documents' : [ 'Sphinx >= 1.2.2' ],
+    },
+    test_suite = 'nose.collector'
+)
