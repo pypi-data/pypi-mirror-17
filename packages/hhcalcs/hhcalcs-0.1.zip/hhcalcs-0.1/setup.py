066f874cfea065b57c40429c6d83d8f685573961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:25 +0000

    Add package hhcalcs/0.1/hhcalcs-0.1.zip

---
+++ packages/hhcalcs/hhcalcs-0.1.zip/hhcalcs-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hhcalcs',
+      version='0.1',
+      description='Tools for common hydraulic and hydrologic engineering calculations',
+      url='https://github.com/aerispaha/hhcalcs',
+      author='Adam Erispaha',
+      author_email='aerispaha@gmail.com',
+      license='MIT',
+      packages=['hhcalcs'],
+      zip_safe=False)
