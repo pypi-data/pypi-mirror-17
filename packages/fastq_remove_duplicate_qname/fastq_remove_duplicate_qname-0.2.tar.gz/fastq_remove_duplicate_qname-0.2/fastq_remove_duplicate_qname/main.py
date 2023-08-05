7d8b8c5bed3e480fa7b06a1141bf59a9b992afe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:07 +0000

    Add package fastq_remove_duplicate_qname/0.2/fastq_remove_duplicate_qname-0.2.tar.gz

---
+++ packages/fastq_remove_duplicate_qname/fastq_remove_duplicate_qname-0.2.tar.gz/fastq_remove_duplicate_qname-0.2/fastq_remove_duplicate_qname/main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+import sys
+
+from Bio import SeqIO
+
+def main():
+    parser = argparse.ArgumentParser('remove duplicate reads from fastq')
+    parser.add_argument('infile',
+                        nargs='?',
+                        type=argparse.FileType('r'),
+                        default=sys.stdin
+    )
+    parser.add_argument('outfile',
+                        nargs='?',
+                        type=argparse.FileType('w'),
+                        default=sys.stdout
+    )
+    args = parser.parse_args()
+
+    infile = args.infile
+    outfile = args.outfile
+
+    log_file_path = 'fastq_dup_rm.log'
+    
+    logging.basicConfig(
+        filename=log_file_path,
+        filemode='a',
+        level = logging.INFO,
+        #format='%(asctime)s %(levelname)s %(message)s',
+        #datefmt='%Y-%m-%d_%H:%M:%S_%Z',
+    )
+    logger = logging.getLogger(__name__)
+
+    singleton_set = set()
+    readcounter = 0
+    duplicatecounter = 0
+    for record in SeqIO.parse(infile,'fastq'):
+        if record.id in singleton_set:
+            duplicatecounter += 1
+            logger.info('duplicate:\t%s' % record.id)
+        else:
+            readcounter += 1
+            singleton_set.add(record.id)
+            SeqIO.write(record, outfile, 'fastq')
+    logger.info('readcounter:\t%s' % str(readcounter))
+    logger.info('duplicatecounter:\t%s' % str(duplicatecounter))
+    return
+
+if __name__ == '__main__':
+    main()
---
+++ packages/fastq_remove_duplicate_qname/fastq_remove_duplicate_qname-0.2.tar.gz/fastq_remove_duplicate_qname-0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name = 'fastq_remove_duplicate_qname',
+      author = 'Jeremiah H. Savage',
+      author_email = 'jeremiahsavage@gmail.com',
+      version = 0.2,
+      description = 'remove duplicate QNAME from a fastq',
+      url = 'https://github.com/jeremiahsavage/fastq_remove_duplicate_qname/',
+      license = 'Apache 2.0',
+      packages = find_packages(),
+      install_requires = [
+          'biopython == 1.68'
+      ],
+      classifiers = [
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ],
+      entry_points={
+          'console_scripts': ['fastq_remove_duplicate_qname=fastq_remove_duplicate_qname.__main__:main']
+      }
+)
