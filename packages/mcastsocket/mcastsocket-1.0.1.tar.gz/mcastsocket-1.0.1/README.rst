20b846ccd5158510da3f262edd1fa89d583f3356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:13 +0000

    Add package mcastsocket/1.0.1/mcastsocket-1.0.1.tar.gz

---
+++ packages/mcastsocket/mcastsocket-1.0.1.tar.gz/mcastsocket-1.0.1/README.rst
@@ -0,0 +1,49 @@
+===============================
+Multicast Socket
+===============================
+
+.. image:: https://img.shields.io/pypi/v/mcastsocket.svg
+        :target: https://pypi.python.org/pypi/mcastsocket
+
+.. image:: https://img.shields.io/travis/mcfletch/mcastsocket.svg
+        :target: https://travis-ci.org/mcfletch/mcastsocket
+
+
+Provides a simple interface for configuring multicast socket services
+
+* Free software: LGPL 2.1 license 
+
+Sample Usage
+------------
+
+See `docs/sample.py <docs/sample.py>`_
+
+.. literalinclude:: docs/sample.py
+    :language: python 
+    :linenos:
+
+Migrating from `zeroconf.mcastsocket`
+--------------------------------------
+
+If you've been using the `forked zeroconf mcastsocket module <https://github.com/mcfletch/pyzeroconf>`_, just update your 
+imports from::
+
+    from zeroconf import mcastsocket 
+
+to::
+
+    from mcastsocket import mcastsocket 
+
+and your code should continue to work.
+
+Credits
+---------
+
+The original "how to work with multicast" code comes from PyZeroconf. The mcastsocket
+module is largely new-code, but it does derive from the original PyZeroconf, so retains
+its license (LGPL).
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ packages/mcastsocket/mcastsocket-1.0.1.tar.gz/mcastsocket-1.0.1/tests/test_mcastsocket.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_mcastsocket
+----------------------------------
+
+Tests for `mcastsocket` module.
+"""
+import select
+import socket
+import unittest
+from mcastsocket import mcastsocket
+
+
+class TestMcastsocket(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def send_to_group(self, group, message, family=socket.AF_INET):
+        iface_ip = '127.0.0.1' if family == socket.AF_INET else '::1'
+        sock = mcastsocket.create_socket(
+            (iface_ip, 8000),
+            TTL=5,
+            family=family
+        )
+        mcastsocket.join_group(
+            sock,
+            group=group,
+            iface=iface_ip,
+        )
+        sock.sendto(message, (group, 8000))
+        _, writable, _ = select.select([], [sock], [], .5)
+        sock.close()
+
+    def test_create_socket_v2(self):
+        group = '224.1.1.2'
+        sock = mcastsocket.create_socket(
+            (group, 8000),
+            TTL=5,
+        )
+        mcastsocket.join_group(
+            sock,
+            group=group,
+            iface='127.0.0.1',
+        )
+        self.send_to_group(group, b'moo')
+        readable, writable, _ = select.select([sock], [], [], .5)
+        assert readable, 'Nothing received'
+        (content, address) = sock.recvfrom(65000)
+        assert content == b'moo', content
+        assert address == ('127.0.0.1', 8000), address
+        mcastsocket.leave_group(
+            sock,
+            group=group,
+            iface='127.0.0.1',
+        )
+        sock.close()
+
+    def test_create_socket_v3(self):
+        group = '224.1.1.2'
+        sock = mcastsocket.create_socket(
+            (group, 8000),
+            TTL=5,
+        )
+        try:
+            ssm = '198.51.100.23'
+            mcastsocket.join_group(
+                sock,
+                group=group,
+                iface='127.0.0.1',
+                # If commented out, we get a failure,
+                # if present, we filter... yay
+                ssm=ssm,
+            )
+            try:
+                self.send_to_group(group, b'moo')
+                readable, writable, _ = select.select([sock], [], [], .5)
+                if readable:
+                    (content, address) = sock.recvfrom(65000)
+                assert not readable, (
+                    'Should not have received the message due to ssm filtering', address)
+            finally:
+                mcastsocket.leave_group(
+                    sock,
+                    group=group,
+                    iface='127.0.0.1',
+                    # If commented out, we get a failure,
+                    # if present, we filter... yay
+                    ssm=ssm,
+                )
+        finally:
+            sock.close()
+
+    def test_ipv6_basic(self):
+        group = 'ff02::2'
+        sock = mcastsocket.create_socket(
+            ('', 8000),
+            TTL=5,
+            family=socket.AF_INET6,
+        )
+        assert sock.family == socket.AF_INET6, sock.family
+        mcastsocket.join_group(
+            sock,
+            group=group,
+            iface = '::1',
+        )
+        self.send_to_group(group, b'moo', family=socket.AF_INET6)
+        readable, writable, _ = select.select([sock], [], [], .5)
+        assert readable, 'Nothing received'
+        (content, address) = sock.recvfrom(65000)
+        assert content == b'moo', content
+        assert address == ('::1', 8000, 0, 0), address
+        mcastsocket.leave_group(
+            sock,
+            group=group,
+        )
+        sock.close()
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
