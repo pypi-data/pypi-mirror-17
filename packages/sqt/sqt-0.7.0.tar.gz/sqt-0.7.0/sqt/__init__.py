90e24c6a5520a2626784a6a5c9bf1b6c783f1f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:22 +0000

    Add package sqt/0.7.0/sqt-0.7.0.tar.gz

---
+++ packages/sqt/sqt-0.7.0.tar.gz/sqt-0.7.0/sqt/__init__.py
@@ -0,0 +1,12 @@
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+from .args import HelpfulArgumentParser
+from .io.fasta import (
+	SequenceReader, FastaReader, FastqReader, FastaWriter, FastqWriter,
+	IndexedFasta, guess_quality_base )
+from .io.gtf import GtfReader
+from cutadapt.xopen import xopen
+from .cigar import Cigar
+
---
+++ packages/sqt/sqt-0.7.0.tar.gz/sqt-0.7.0/sqt/io/xopen.py
@@ -0,0 +1,6 @@
+"""
+Open compressed files transparently.
+"""
+from __future__ import print_function, division, absolute_import
+
+from cutadapt.xopen import xopen
