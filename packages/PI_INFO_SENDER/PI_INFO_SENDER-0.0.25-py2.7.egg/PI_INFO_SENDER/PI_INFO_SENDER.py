0747cf0f47d5b9a92fafd53c64d05b7c50d9b4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:22 +0000

    Add package PI_INFO_SENDER/0.0.25/PI_INFO_SENDER-0.0.25-py2.7.egg

---
+++ packages/PI_INFO_SENDER/PI_INFO_SENDER-0.0.25-py2.7.egg/PI_INFO_SENDER/PI_INFO_SENDER.py
@@ -0,0 +1,48 @@
+import paho.mqtt.client as mqtt
+import time
+import json
+import sys
+import datetime
+import ssl
+import subprocess
+import os
+class testli:
+    IP = "192.168.1.38"
+    PORT = "1883"
+
+    def on_message(client, userdata, msg):
+        data = json.dumps(json.loads(str(msg.payload))["command"]["command"]["name"])
+        
+
+    def on_connect(client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+        client.subscribe("SiteWhere/commands/ccc-cccc-cccccccccc")
+
+    def on_publish(client, userdata, msg):
+        print "Message has been sent."
+    def stop(self):
+        runing=False
+    
+    client = mqtt.Client()
+    client.on_message = on_message
+    client.on_connect = on_connect
+    client.on_publish = on_publish
+    client.connect(IP,PORT,60)
+
+    client.loop_start()
+    time.sleep(5)
+    runing=True
+    while runing:
+    
+        strDate = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.000Z")
+        temperature = json.dumps({"hardwareId" : "ccc-cccc-cccccccccc",
+                              "type": "DeviceMeasurements"
+                              ,"request": {"measurements": {"temperature":"37",
+                                                            "humidity":"50" },
+                                           "updateState": "true",
+                                           "eventDate": strDate }})
+     
+        client.publish("SiteWhere/input/json", temperature, qos=1)
+
+
+        time.sleep(1)
