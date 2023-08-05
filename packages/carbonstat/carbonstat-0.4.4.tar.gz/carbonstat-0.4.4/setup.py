59bdd266604ebcd1c54dea654e03624f6d130257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:15 +0000

    Add package carbonstat/0.4.4/carbonstat-0.4.4.tar.gz

---
+++ packages/carbonstat/carbonstat-0.4.4.tar.gz/carbonstat-0.4.4/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='carbonstat',
+      version='0.4.4',
+      description='Metric collection agent for Carbon',
+      license='MIT',
+      author='Alexander Tikhonov',
+      author_email='random.gauss@gmail.com',
+      url='https://github.com/Fatal1ty/carbonstat',
+      py_modules=['carbonstat'])
