2990bd0a73a4c4dc646b75407c885cfe9123821e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:20 +0000

    Add package pygount/0.4/pygount-0.4-py3-none-any.whl

---
+++ packages/pygount/pygount-0.4-py3-none-any.whl/pygount-0.4.dist-info/DESCRIPTION.rst
@@ -0,0 +1,247 @@
+.. image:: https://travis-ci.org/roskakori/pygount.svg?branch=master
+    :target: https://travis-ci.org/roskakori/pygount
+    :alt: Build Status
+
+.. image:: https://coveralls.io/repos/roskakori/pygount/badge.png?branch=master
+    :target: https://coveralls.io/r/roskakori/pygount?branch=master
+    :alt: Test coverage
+
+.. image:: https://landscape.io/github/roskakori/pygount/master/landscape.svg?style=flat
+    :target: https://landscape.io/github/roskakori/pygount/master
+    :alt: Code Health
+
+
+pygount
+=======
+
+Pygount is a command line tool to scan folders for source code files and
+count the number of source code lines in it. It is similar to tools like
+`sloccount <http://www.dwheeler.com/sloccount/>`_ and
+`cloc <http://cloc.sourceforge.net/>`_ but uses the
+`pygments <http://pygments.org/>`_
+package to analyze the source code and consequently can analyze any
+`programming language supported by pygments <http://pygments.org/languages/>`_.
+
+The name is a combination of pygments and count.
+
+
+Download and installation
+-------------------------
+
+Pygount is available from https://pypi.python.org/pypi/pygount and can be
+installed running::
+
+  $ pip install pygount
+
+
+Usage
+-----
+
+Simply run and specify the folder to analyze recursively, for example::
+
+  $ pygount ~/development/sometool
+
+If you omit the folder, the current folder of your shell is used as starting
+point. Apart from folders you can also specify single files and shell patterns
+(using ``?``, ``*`` and ranges like ``[a-z]``).
+
+Certain files and folders are automatically excluded from the analysis:
+
+* files starting with dot (.) or ending in tilda (~)
+* folders starting with dot (.) or named ``_svn``.
+
+To specify alternative patterns, use ``--folders-to-skip`` and
+``--names-to-skip``. Both take a comma separated list of patterns, see below
+on the pattern syntax. To for example also prevent folders starting with two
+underscores (``_``) from being analyzed, specify
+``--folders-to-skip=[...],__*``.
+
+To limit the analysis on certain file types, you can specify a comma separated
+list of suffixes to take into account, for example ``--suffix=py,sql,xml``.
+
+By default the result of the analysis are written to the standard output in a
+format similar to sloccount. To redirect the output to a file, use e.g.
+``--out=counts.txt``. To change the format to an XML file similar to cloc, use
+``--format=cloc-xml``.
+
+
+Patterns
+--------
+
+Some command line arguments take patterns as values.
+
+By default, patterns are shell patterns using ``*``, ``?`` and ranges like
+``[a-z]`` as placeholders. Depending on your platform, the are case sensitive
+(Unix) or not (Mac OS, Windows).
+
+If a pattern starts with ``[regex]`` you can specifiy a comma separated list
+of regular expressions instead using all the constructs supported by the
+`Python regular expression syntax <https://docs.python.org/3/library/re.html#regular-expression-syntax>`_.
+Regular expressions are case sensitive unless they include a ``(?i)`` flag.
+
+If the first actual pattern is ``[...]`` default patterns are included.
+Without it, defaults are ignored and only the pattern explicitely stated are
+taken into account.
+
+
+Source code encoding
+--------------------
+
+When reading source code, pygount automatically detects the encoding. It uses
+a simple algorithm where it recognizes BOM, XML declaractions such as::
+
+  <?xml encoding='cp1252'?>
+
+and "magic" comments such as::
+
+  # -*- coding: cp1252 -*-
+
+If the file does not have an appropriate heading, pygount attempts to read it
+using UTF-8. If this fails, it reads the file using a fallback encoding (by
+default CP1252) and ignores any encoding errors.
+
+You can change this behavior using the ``--encoding`` option:
+
+* To keep the automatic analysis and use a different fallback encoding specify
+  for example ``--encoding=automatic;iso-8859-15``
+* To use an automatic detection based on heuristic, use
+  ``--encoding=chardet``. For this to work, the
+  `chardet <https://pypi.python.org/pypi/chardet>`_ package must be installed,
+* To use a specific encoding (for all files analyzed), use for example
+  ``--encoding=iso-8859-15``.
+
+
+Other information
+-----------------
+
+To get a description of all the available command line options, run::
+
+  $ pygount --help
+
+To get the version number, run::
+
+  $ pygount --version
+
+
+Continuous integration
+----------------------
+
+Pygount can produce output that can be processed by the
+`SLOCCount plug-in <https://wiki.jenkins-ci.org/display/JENKINS/SLOCCount+Plugin>`_
+for the `Jenkins <https://jenkins.io/>`_ continuous integration server.
+
+For example::
+
+  pygount --format=cloc-xml --out cloc.xml --suffix=py --verbose
+
+Then add a post-build action "Publish SLOCCount analysis results" and set
+"SLOCCount report" to "cloc.xml".
+
+
+How pygount counts code
+-----------------------
+
+Pygount basically counts physical lines of source code.
+
+First, it lexes the code using the lexers ``pygments`` assigned to it. Lines
+that only contain comment tokens and white space count as comments. Lines
+that only contain white space are not taken into account. Everything else
+counts as code.
+
+If a line contains only "white characters" it is not taken into account
+presumably because the code is only formatted that way to make it easier to
+read. Currently white characters are::
+
+    (),:;[]{}
+
+For some languages "no operations" are detected and treated as white space.
+For example Python's ``pass`` or Transact-SQL's ``begin`` and ``end`` .
+
+As example consider this Python code::
+
+    class SomeError(Exception):
+        """
+        Some error caused by some issue.
+        """
+        pass
+
+This counts as 1 line of code and 3 lines of comments. The line with ``pass``
+is considered a "no operation" and thus not taken into account.
+
+
+Comparison with other tools
+---------------------------
+
+Pygount can analyze more languages than other common tools such as sloccount
+or cloc because it builds on ``pygments``, which provides lexers for hundreds
+of languages. This also makes it easy to support another language: simply
+`write your own lexer <http://pygments.org/docs/lexerdevelopment/>`_.
+
+For certain corner cases pygount give more accurate results because it
+actually lexes the code unlike other tools that mostly look for comment
+markers and can get confused when they show up inside strings. In practice
+though this should not make much of a difference.
+
+Pygount is slower than most other tools. Partially this is due to actually
+lexing instead of just scanning the code. Partially other tools can use
+statically compiled languages such as Java or C, which are generally faster
+than dynamic languages. For many applications though pygount should be
+"fast enough", especially when called during a nightly build.
+
+
+API
+---
+
+Pygount provides a simple API to integrate it in other tools. This however is
+currently still a work in progress and subject to change.
+
+Here's an example on how to analyze one of pygount's own source codes::
+
+  >>> import pygount
+  >>> analysis = pygount.source_analysis('pygount/analysis.py', 'pygount')
+  >>> analysis
+  SourceAnalysis(path='pygount/analysis.py', language='Python', group='pygount', code=164, documentation=48, empty=27, string=0)
+
+
+Version history
+---------------
+
+Version 0.4, 2016-09-11
+
+* Fixed ``LookupError`` on broken encoding in magic comment (issue
+  `#4 <https://github.com/roskakori/pygount/issues/4>`_).
+* Added options ``--folders-to-skip`` and ``--names-to-skip`` to specify which
+  files should be excluded from analysis.
+* Added comma (``,``) and colon (``:``) to list of "white characters" that do
+  not count as code if there is nothing else in the line.
+* Improved pattern matching: for all options that according to ``--help``
+  take ``PATTERNS`` you can now specify that the patterns are regular
+  expressions instead of shell patterns (using ``[regex]``) and that they
+  should extend the default patterns (using ``[...]``).
+* Improved documentation: added notes on how code is counted and how pygount
+  compares to other similar tools.
+
+Version 0.3, 2016-08-20
+
+* Fixed ``@rem`` comments in DOS batch files (issue
+  `#3 <https://github.com/roskakori/pygount/issues/3>`_).
+* Cleaned up code.
+
+Version 0.2, 2016-07-10
+
+* Fixed that files starting with underscore (e.g. ``__init__.py``) were
+  excluded from analysis.
+* Changed ``chardet`` package to be optional.
+* Added possibility to specify single files and glob patterns to analyze.
+* Added that lines containing only certain characters are treated as white
+  space instead of code. Currently this concerns brackets (``()[]{}``) and
+  semicolon (``;``).
+* Added that Python's ``pass`` statement is treated as white space instead of
+  code.
+* Cleaned up and (slightly) optimized code.
+
+Version 0.1, 2016-07-05
+
+* Initial public release.
+
+
---
+++ packages/pygount/pygount-0.4-py3-none-any.whl/pygount/analysis.py
@@ -0,0 +1,385 @@
+"""
+Functions to analyze source code and count lines in it.
+"""
+# Copyright (c) 2016, Thomas Aglassinger.
+# All rights reserved. Distributed under the BSD License.
+import codecs
+import collections
+import glob
+import logging
+import os
+import re
+
+import pygount.common
+
+
+# Attempt to import chardet.
+try:
+    import chardet.universaldetector
+    _detector = chardet.universaldetector.UniversalDetector()
+    has_chardet = True
+except ImportError:
+    _detector = None
+    has_chardet = False
+
+from pygments import lexers, token, util
+
+
+#: Default glob patterns for folders not to analyze.
+DEFAULT_FOLDER_PATTERNS_TO_SKIP_TEXT = ', '.join([
+    '.*',
+    '_svn',  # Subversion hack for Windows
+])
+
+#: Default glob patterns for file names not to analyze.
+DEFAULT_NAME_PATTERNS_TO_SKIP_TEXT = ', '.join([
+    '.*',
+    '*~',
+])
+
+_log = logging.getLogger('pygount')
+
+_MARK_TO_NAME_MAP = (
+    ('c', 'code'),
+    ('d', 'documentation'),
+    ('e', 'empty'),
+    ('s', 'string'),
+)
+_BOM_TO_ENCODING_MAP = collections.OrderedDict((
+    # NOTE: We need an ordered dict due the overlap between utf-32-le and utf-16-be.
+    (codecs.BOM_UTF8, 'utf-8-sig'),
+    (codecs.BOM_UTF32_LE, 'utf-32-le'),
+    (codecs.BOM_UTF16_BE, 'utf-16-be'),
+    (codecs.BOM_UTF16_LE, 'utf-16-le'),
+    (codecs.BOM_UTF32_BE, 'utf-32-be'),
+))
+_XML_PROLOG_REGEX = re.compile(r'<\?xml\s+.*encoding="(?P<encoding>[-_.a-zA-Z0-9]+)".*\?>')
+_CODING_MAGIC_REGEX = re.compile(r'.+coding[:=][ \t]*(?P<encoding>[-_.a-zA-Z0-9]+)\b', re.DOTALL)
+
+#: Results of a source analysis derived by :py:func:`source_analysis`.
+SourceAnalysis = collections.namedtuple(
+    'SourceAnalysis', ['path', 'language', 'group', 'code', 'documentation', 'empty', 'string'])
+
+
+class SourceScanner():
+    def __init__(self, source_patterns, suffixes='*'):
+        self._source_patterns = source_patterns
+        self._suffixes = pygount.common.regexes_from(suffixes)
+        self._folder_regexps_to_skip = pygount.common.regexes_from(DEFAULT_FOLDER_PATTERNS_TO_SKIP_TEXT)
+        self._name_regexps_to_skip = pygount.common.regexes_from(DEFAULT_NAME_PATTERNS_TO_SKIP_TEXT)
+
+    @property
+    def source_patterns(self):
+        return self._source_patterns
+
+    @property
+    def suffixes(self):
+        return self._suffixes
+
+    @property
+    def folder_regexps_to_skip(self):
+        return self._folder_regexps_to_skip
+
+    @folder_regexps_to_skip.setter
+    def set_folder_pattern_to_skip(self, regexps_or_pattern_text):
+        self._folder_regexps_to_skip.append = pygount.common.regexes_from(
+            regexps_or_pattern_text,
+            self.folder_regexps_to_skip)
+
+    @property
+    def generated_regexps(self):
+        return self._generated_regexps_to_skip
+
+    @generated_regexps.setter
+    def set_generated_regexps(self, regexps_or_pattern_text):
+        self._generated_regexps_to_skip = pygount.common.regexes_from(
+            regexps_or_pattern_text,
+            self.generated_regexps)
+
+    @property
+    def name_regexps_to_skip(self):
+        return self._name_regexps_to_skip
+
+    @name_regexps_to_skip.setter
+    def set_name_regexps_to_skip(self, regexps_or_pattern_text):
+        self._name_regexp_to_skip = pygount.common.regexes_from(
+            regexps_or_pattern_text,
+            self.name_regexps_to_skip)
+
+    def _is_path_to_skip(self, name, is_folder):
+        assert os.sep not in name, 'name=%r' % name
+        regexps_to_skip = self._folder_regexps_to_skip if is_folder else self._name_regexps_to_skip
+        return any(
+            path_name_to_skip_regex.match(name) is not None
+            for path_name_to_skip_regex in regexps_to_skip
+        )
+
+    def _paths_and_group_to_analyze_in(self, folder, group):
+        assert folder is not None
+        assert group is not None
+
+        for name in os.listdir(folder):
+            path = os.path.join(folder, name)
+            if not os.path.islink(path):
+                is_folder = os.path.isdir(path)
+                if self._is_path_to_skip(os.path.basename(path), is_folder):
+                    _log.debug('skip due matching skip pattern: %s', path)
+                elif is_folder:
+                    yield from self._paths_and_group_to_analyze_in(path, group)
+                else:
+                    yield path, group
+
+    def _paths_and_group_to_analyze(self, path_to_analyse_pattern, group=None):
+        for path_to_analyse in glob.glob(path_to_analyse_pattern):
+            if os.path.islink(path_to_analyse):
+                _log.debug('skip link: %s', path_to_analyse)
+            else:
+                is_folder = os.path.isdir(path_to_analyse)
+                if self._is_path_to_skip(os.path.basename(path_to_analyse), is_folder):
+                    _log.debug('skip due matching skip pattern: %s', path_to_analyse)
+                else:
+                    actual_group = group
+                    if is_folder:
+                        if actual_group is None:
+                            actual_group = os.path.basename(path_to_analyse)
+                            if actual_group == '':
+                                # Compensate for trailing path separator.
+                                actual_group = os.path.basename(os.path.dirname(path_to_analyse))
+                        yield from self._paths_and_group_to_analyze_in(path_to_analyse_pattern, actual_group)
+                    else:
+                        if actual_group is None:
+                            actual_group = os.path.dirname(path_to_analyse)
+                            if actual_group == '':
+                                actual_group = os.path.basename(os.path.dirname(os.path.abspath(path_to_analyse)))
+                        yield path_to_analyse, actual_group
+
+    def _source_paths_and_groups_to_analyze(self, patterns_to_analyze):
+        assert patterns_to_analyze is not None
+        result = []
+        for pattern in patterns_to_analyze:
+            try:
+                result.extend(self._paths_and_group_to_analyze(pattern))
+            except OSError as error:
+                raise OSError('cannot scan "{0}" for source files: {1}'.format(pattern, error))
+        result = sorted(set(result))
+        return result
+
+    def source_paths(self):
+        source_paths_and_groups_to_analyze = self._source_paths_and_groups_to_analyze(self.source_patterns)
+
+        for source_path, group in source_paths_and_groups_to_analyze:
+            suffix = os.path.splitext(source_path)[1].lstrip('.')
+            is_suffix_to_analyze = any(
+                suffix_regexp.match(suffix)
+                for suffix_regexp in self.suffixes
+            )
+            if is_suffix_to_analyze:
+                yield source_path, group
+            else:
+                _log.info('skip due suffix: %s', source_path)
+
+
+_LANGUAGE_TO_WHITE_WORDS_MAP = {
+    'batchfile': ['@'],
+    'python': ['pass'],
+    'sql': ['begin', 'end']
+}
+for language in _LANGUAGE_TO_WHITE_WORDS_MAP.keys():
+    assert language.islower()
+
+
+def white_characters(language_id):
+    """
+    Characters that count as white space if they are the only characters in a
+    line.
+    """
+    assert language_id is not None
+    assert language_id.islower()
+    return '(),:;[]{}'
+
+
+def white_code_words(language_id):
+    """
+    Words that do not count as code if it is the only word in a line.
+    """
+    assert language_id is not None
+    assert language_id.islower()
+    return _LANGUAGE_TO_WHITE_WORDS_MAP.get(language_id, set())
+
+
+def _delined_tokens(tokens):
+    for token_type, token_text in tokens:
+        newline_index = token_text.find('\n')
+        while newline_index != -1:
+            yield token_type, token_text[:newline_index + 1]
+            token_text = token_text[newline_index + 1:]
+            newline_index = token_text.find('\n')
+        if token_text != '':
+            yield token_type, token_text
+
+
+def _pythonized_comments(tokens):
+    """
+    Similar to tokens but converts strings after a colon (:) to comments.
+    """
+    is_after_colon = True
+    for token_type, token_text in tokens:
+        if is_after_colon and (token_type in token.String):
+            token_type = token.Comment
+        elif token_text == ':':
+            is_after_colon = True
+        elif token_type not in token.Comment:
+            is_whitespace = len(token_text.rstrip(' \f\n\r\t')) == 0
+            if not is_whitespace:
+                is_after_colon = False
+        yield token_type, token_text
+
+
+def _line_parts(lexer, text):
+    line_marks = set()
+    tokens = _delined_tokens(lexer.get_tokens(text))
+    if lexer.name == 'Python':
+        tokens = _pythonized_comments(tokens)
+    language_id = lexer.name.lower()
+    white_text = ' \f\n\r\t' + white_characters(language_id)
+    white_words = white_code_words(language_id)
+    for token_type, token_text in tokens:
+        if token_type in token.Comment:
+            line_marks.add('d')  # 'documentation'
+        elif token_type in token.String:
+            line_marks.add('s')  # 'string'
+        else:
+            is_white_text = (token_text.strip() in white_words) or (token_text.rstrip(white_text) == '')
+            if not is_white_text:
+                line_marks.add('c')  # 'code'
+        if token_text.endswith('\n'):
+            yield line_marks
+            line_marks = set()
+    if len(line_marks) >= 1:
+        yield line_marks
+
+
+def encoding_for(source_path, encoding='automatic', fallback_encoding='cp1252'):
+    """
+    The encoding used by the text file stored in ``source_path``.
+
+    The algorithm used is:
+
+    * If ``encoding`` is ``'automatic``, attempt the following:
+      1. Check BOM for UTF-8, UTF-16 and UTF-32.
+      2. Look for XML prolog or magic heading like ``# -*- coding: cp1252 -*-``
+      3. Read the file using UTF-8.
+      4. If all this fails, use assume the ``fallback_encoding``.
+    * If ``encoding`` is ``'chardet`` use :mod:`chardet` to obtain the encoding.
+    * For any other ``encoding`` simply use the specified value.
+    """
+    assert encoding is not None
+    assert fallback_encoding is not None
+
+    if encoding == 'automatic':
+        with open(source_path, 'rb') as source_file:
+            heading = source_file.read(128)
+        result = None
+        if len(heading) == 0:
+            # File is empty, assume a dummy encoding.
+            result = 'utf-8'
+        if result is None:
+            # Check for known BOMs.
+            for bom, encoding in _BOM_TO_ENCODING_MAP.items():
+                if heading[:len(bom)] == bom:
+                    result = encoding
+                    break
+        if result is None:
+            # Look for common headings that indicate the encoding.
+            ascii_heading = heading.decode('ascii', errors='replace')
+            ascii_heading = ascii_heading.replace('\r\n', '\n')
+            ascii_heading = ascii_heading.replace('\r', '\n')
+            ascii_heading = '\n'.join(ascii_heading.split('\n')[:2]) + '\n'
+            coding_magic_match = _CODING_MAGIC_REGEX.match(ascii_heading)
+            if coding_magic_match is not None:
+                result = coding_magic_match.group('encoding')
+            else:
+                first_line = ascii_heading.split('\n')[0]
+                xml_prolog_match = _XML_PROLOG_REGEX.match(first_line)
+                if xml_prolog_match is not None:
+                    result = xml_prolog_match.group('encoding')
+        if result is None:
+            try:
+                # Attempt to read the first 16 KiB of the file as UTF-8.
+                with open(source_path, 'r', encoding='utf-8') as source_file:
+                    source_file.read(16384)
+                result = 'utf-8'
+            except UnicodeDecodeError:
+                # It is not UTF-8, just assume the fallback encoding.
+                result = fallback_encoding
+    elif encoding == 'chardet':
+        assert _detector is not None, \
+            'without chardet installed, encoding="chardet" must be rejected before calling encoding_for()'
+        _detector.reset()
+        with open(source_path, 'rb') as source_file:
+            for line in source_file.readlines():
+                _detector.feed(line)
+                if _detector.done:
+                    break
+        result = _detector.result['encoding']
+    else:
+        # Simply use the specified encoding.
+        result = encoding
+    assert result is not None
+    return result
+
+
+def source_analysis(source_path, group, encoding='automatic', fallback_encoding='cp1252'):
+    """
+    Analysis for line counts in source code stored in ``source_path``.
+
+    :param source_path:
+    :param group: name of a logical group the sourc code belongs to, e.g. a
+      package.
+    :param encoding: encoding according to :func:`encoding_for`
+    :param fallback_encoding: fallback encoding according to
+      :func:`encoding_for`
+    :return: a :class:`SourceAnalysis`; if no matching lexer could be found,
+      `language`` is ``None``.
+    """
+    assert encoding is not None
+    assert fallback_encoding is not None
+
+    result = None
+    try:
+        lexer = lexers.get_lexer_for_filename(source_path)
+    except util.ClassNotFound:
+        lexer = None
+    if lexer is not None:
+        if encoding == 'automatic':
+            encoding = encoding_for(source_path, encoding, fallback_encoding)
+        _log.info('%s: analyze as %s using encoding %s', source_path, lexer.name, encoding)
+        try:
+            with open(source_path, 'r', encoding=encoding) as source_file:
+                text = source_file.read()
+        except (LookupError, OSError, UnicodeDecodeError) as error:
+            _log.warning('cannot read %s: %s', source_path, error)
+            result = SourceAnalysis(source_path, 'error', group, 0, 0, 0, 0)
+        if result is None:
+            mark_to_count_map = {'c': 0, 'd': 0, 'e': 0, 's': 0}
+            for line_parts in _line_parts(lexer, text):
+                mark_to_increment = 'e'
+                for mark_to_check in ('d', 's', 'c'):
+                    if mark_to_check in line_parts:
+                        mark_to_increment = mark_to_check
+                mark_to_count_map[mark_to_increment] += 1
+            result = SourceAnalysis(
+                path=source_path,
+                language=lexer.name,
+                group=group,
+                code=mark_to_count_map['c'],
+                documentation=mark_to_count_map['d'],
+                empty=mark_to_count_map['e'],
+                string=mark_to_count_map['s']
+            )
+    else:
+        _log.info('%s: skip', source_path)
+        result = SourceAnalysis(source_path, None, group, 0, 0, 0, 0)
+
+    assert result is not None
+    return result
