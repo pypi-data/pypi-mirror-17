3c943f30f50bd9d0f0535556ddd3946e08a5e40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:15 +0000

    Add package Gecko/1.0.15/Gecko-1.0.15.tar.gz

---
+++ packages/Gecko/Gecko-1.0.15.tar.gz/Gecko-1.0.15/README.txt
@@ -0,0 +1,6 @@
+=====
+Gecko
+=====
+
+Gecko is a python based framework that gives users the ability to create a simple
+IoT application with minimal development time.
---
+++ packages/Gecko/Gecko-1.0.15.tar.gz/Gecko-1.0.15/setup.py
@@ -0,0 +1,83 @@
+﻿"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+setup(
+    name='Gecko',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.0.15',
+
+    description='A framework for building applications to communicate with Exosite',
+    #long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/exosite/gecko',
+
+    # Author details
+    author='Andy Lee',
+    author_email='andylee@exosite.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3"
+   
+    ],
+
+    # What does your project relate to?
+    keywords='exosite iot gateway framework ',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    package_data={'Gecko': ['templates/*.txt']},
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['requests', 'pyserial','apscheduler'],
+
+    
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'gecko=Gecko.__main__:main',
+        ],
+    },
+   
+)
