367a0649dd6d9d8dd67f6f541a807131b5c48129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:41 +0000

    Add package looo/1.0.0/looo-1.0.0.tar.gz

---
+++ packages/looo/looo-1.0.0.tar.gz/looo-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'looo',
+		version = '1.0.0',
+		py_modules = ['looo'],
+		author = 'louisxielv',
+		author_email = 'louisxielv@gmail.com',
+		url = 'https://www.linkedin.com/in/lyu-xie-69997310b?trk=hp-identity-name',
+		description = 'a printer of nested lists',
+	)
