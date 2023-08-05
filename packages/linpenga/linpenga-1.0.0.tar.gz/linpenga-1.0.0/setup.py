f150bb60e9125ca8aba0c4bd9a4fa62534d262c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:33 +0000

    Add package linpenga/1.0.0/linpenga-1.0.0.tar.gz

---
+++ packages/linpenga/linpenga-1.0.0.tar.gz/linpenga-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name        =       'linpenga',
+    version     =       '1.0.0',
+    py_modules  =       ['linpenga'],
+    author      =       'lin',
+    author_email=       'hfpython@headfirstlabs.com',
+    url         =       'http://www.headfirstlabs.com',
+    description =       'A simple printer of nested lists',
+    )
