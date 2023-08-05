9b148de10e36529b01b3d542f58e35e6e71c0bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:54 +0000

    Add package pyuarm/1.3.31/pyuarm-1.3.31.tar.gz

---
+++ packages/pyuarm/pyuarm-1.3.31.tar.gz/pyuarm-1.3.31/pyuarm/version.py
@@ -0,0 +1,3 @@
+__version__ = '1.3.31'
+
+support_versions = ['1.7']
---
+++ packages/pyuarm/pyuarm-1.3.31.tar.gz/pyuarm-1.3.31/setup.py
@@ -0,0 +1,36 @@
+from distutils.util import convert_path
+from setuptools import setup, find_packages
+
+main_ns = {}
+ver_path = convert_path('pyuarm/version.py')
+with open(ver_path) as ver_file:
+        exec(ver_file.read(), main_ns)
+
+version = main_ns['__version__']
+
+long_description = open('README.rst').read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name='pyuarm',
+    version=version,
+    author='Joey Song/Alex Tan',
+    packages=find_packages(),
+    entry_points={
+            'console_scripts': [
+                'uarm-firmware = pyuarm.tools.firmware_helper:main',
+                'uarm-calibrate = pyuarm.tools.calibrate:main',
+                'uarm-listport = pyuarm.tools.list_uarms:main',
+                'uarm-miniterm = pyuarm.tools.miniterm:main',
+            ]
+    },
+    package_data={'pyuarm.tools': ['avrdude/*']},
+    include_package_data=True,
+    author_email='developer@ufactory.cc',
+    url="https://github.com/uarm-developer/pyuarm",
+    keywords="pyuarm uarm4py uarmForPython uarm ufactory",
+    install_requires=requirements,
+    long_description=long_description,
+    description='A python library for uArm',
+)
