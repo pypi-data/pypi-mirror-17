58c4981bb7ce836bcef99a53fd2fba793a48b431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:33 +0000

    Add package gviewer/3.0.3/gviewer-3.0.3-py2-none-any.whl

---
+++ packages/gviewer/gviewer-3.0.3-py2-none-any.whl/gviewer/tests/view/test_summary.py
@@ -0,0 +1,332 @@
+import unittest
+import urwid
+import mock
+
+from ..util import render_to_content, render_widgets_to_content
+from gviewer.view.summary import (
+    SummaryItemWidget, SummaryListWalker,
+    FilterSummaryListWalker, SummaryListWidget)
+from gviewer.view.summary import _verify_keys
+from gviewer.view.detail import DetailWidget
+from gviewer.view.element import View
+from gviewer.action import Actions
+from gviewer.store import StaticDataStore
+from gviewer.context import DisplayerContext
+from gviewer.config import Config
+from gviewer.displayer import BaseDisplayer
+
+
+class TestSummaryItemWidget(unittest.TestCase):
+    def setUp(self):
+        self.controller = mock.Mock()
+        self.controller.open_view = mock.Mock(side_effect=self._open_view)
+
+        self.context = mock.Mock()
+        self.context.config.keys = dict()
+
+        self.action_a = mock.Mock()
+        self.displayer_context = DisplayerContext(
+            None, self, Actions([("a", "aaaa", self.action_a)]))
+
+        self.widget = SummaryItemWidget(
+            "message", "summary", self.displayer_context, controller=self.controller,
+            context=self.context)
+
+        self.new_widget = None
+
+    def get_views(self):
+        return [("view", self.view1)]
+
+    def view1(self, message):
+        return View([])
+
+    def _open_view(self, widget, **kwargs):
+        self.new_widget = widget
+
+    def test_render(self):
+        self.assertEqual(
+            render_to_content(self.widget, (7,)),
+            render_widgets_to_content(
+                [urwid.AttrMap(urwid.Text("summary"), "summary")],
+                (7, 1))
+        )
+
+    def test_keypress_enter(self):
+        self.assertIsNone(self.widget.keypress(None, "enter"))
+        self.assertIsNotNone(self.new_widget)
+        self.assertIsInstance(self.new_widget, DetailWidget)
+
+    def test_keypress_custom_action(self):
+        self.assertIsNone(self.widget.keypress(None, "a"))
+        self.action_a.assert_called_with(self.controller, "message", self.widget)
+
+    def test_set_title(self):
+        self.widget.set_title("hahaha")
+        self.assertEqual(self.widget.get_title_as_plain_text(), "hahaha")
+
+
+class TestSummaryListWalker(unittest.TestCase):
+    def setUp(self):
+        self.controller = mock.Mock()
+        self.controller.open_error = mock.Mock(
+            side_effect=self._open_error)
+
+        self.context = mock.Mock()
+        self.context.config.keys = dict()
+
+        self.displayer_context = mock.Mock()
+        self.displayer_context.displayer.summary = mock.Mock(
+            side_effect=lambda m: m)
+        self.displayer_context.displayer.match = mock.Mock(
+            side_effect=lambda k, m, s: k in s)
+        self.on_receive = mock.Mock()
+
+        self.error = False
+
+        self.walker = SummaryListWalker(
+            controller=self.controller, context=self.context,
+            displayer_context=self.displayer_context,
+            on_receive=self.on_receive)
+
+    def _open_error(self, *args, **kwargs):
+        self.error = True
+
+    def test_msg_listener_register(self):
+        self.displayer_context.store.register.assert_called_once_with(
+            self.walker)
+
+    def test_recv(self):
+        self.displayer_context.displayer.summary = mock.Mock(return_value="message")
+
+        self.walker.recv("new message")
+        self.assertEqual(len(self.walker), 1)
+        self.assertFalse(self.error)
+
+        self.displayer_context.displayer.summary.assert_called_once_with("new message")
+
+    def test_recv_failed(self):
+        self.displayer_context.displayer.summary = mock.Mock(
+            side_effect=ValueError("failed"))
+
+        self.walker.recv("new message")
+        self.assertEqual(len(self.walker), 0)
+        self.assertTrue(self.error)
+
+
+class TestFilterSummaryListWalker(unittest.TestCase):
+    def setUp(self):
+        self.controller = mock.Mock()
+        self.controller.open_error = mock.Mock(
+            side_effect=self._open_error)
+
+        self.context = mock.Mock()
+        self.context.config.keys = dict()
+
+        self.displayer_context = mock.Mock()
+        self.displayer_context.displayer = BaseDisplayer()
+        self.on_receive = mock.Mock()
+        self.error = False
+
+        self.original_walker = SummaryListWalker(
+            content=[
+                SummaryItemWidget(
+                    "message 1", "summary 1",
+                    self.displayer_context,
+                    controller=self.controller,
+                    context=self.context),
+                SummaryItemWidget(
+                    "message 2", "summary 2",
+                    self.displayer_context,
+                    controller=self.controller,
+                    context=self.context)
+            ],
+            controller=self.controller,
+            context=self.context,
+            displayer_context=self.displayer_context,
+            on_receive=self.on_receive
+        )
+
+    def _open_error(self, *args, **kwargs):
+        self.error = True
+
+    def test_construct(self):
+        walker = FilterSummaryListWalker(
+            self.original_walker, "summary 1")
+        self.assertEqual(len(walker), 1)
+
+    def test_recv_with_match(self):
+        walker = FilterSummaryListWalker(
+            self.original_walker, "summary 1")
+        walker.recv("summary 1111")
+        self.assertEqual(len(walker), 2)
+
+    def test_recv_with_not_match(self):
+        walker = FilterSummaryListWalker(
+            self.original_walker, "summary 1")
+        walker.recv("summary")
+        self.assertEqual(len(walker), 1)
+
+    def test_recv_failed(self):
+        self.displayer_context.displayer.summary = mock.Mock(side_effect=ValueError("failed"))
+
+        walker = FilterSummaryListWalker(
+            self.original_walker, "summary 1")
+
+        walker.recv("summary 1")
+        self.assertEqual(len(walker), 1)
+        self.assertTrue(self.error)
+
+    def test_close(self):
+        walker = FilterSummaryListWalker(
+            self.original_walker, "summary 1")
+        walker.close()
+        self.displayer_context.store.unregister.assert_called_once_with(walker)
+
+    def test_del_one_item(self):
+        walker = FilterSummaryListWalker(
+            self.original_walker, "")
+        del walker[0]
+        self.assertEqual(len(walker), 1)
+        self.assertEqual(len(self.original_walker), 1)
+        self.assertIs(walker[0], self.original_walker[0])
+
+    def test_del_all_items(self):
+        walker = FilterSummaryListWalker(
+            self.original_walker, "summary 1")
+        del walker[:]
+        self.assertEqual(len(walker), 0)
+        self.assertEqual(len(self.original_walker), 1)
+
+
+class TestSummaryListWidget(unittest.TestCase):
+    def setUp(self):
+        self.controller = mock.Mock()
+
+        self.displayer_context = DisplayerContext(
+            StaticDataStore(["summary 1", "summary 2"]),
+            BaseDisplayer())
+
+        self.context = mock.Mock()
+        self.context.config = Config()
+
+        self.widget = SummaryListWidget(
+            self.displayer_context,
+            controller=self.controller,
+            context=self.context)
+
+        self.displayer_context.store.setup()
+
+    def test_render(self):
+        self.assertEqual(
+            render_to_content(self.widget, (9, 2)),
+            render_widgets_to_content([
+                urwid.AttrMap(urwid.Text("summary 1"), "summary"),
+                urwid.AttrMap(urwid.Text("summary 2"), "summary")
+            ], (9, 2))
+        )
+
+    def test_open_search(self):
+        self.widget._open_search()
+        self.controller.open_edit.assert_called_with(self.widget.search_widget)
+
+    def test_filter(self):
+        self.widget._filter("summary 1")
+        self.assertIsInstance(self.widget.current_walker, FilterSummaryListWalker)
+        self.assertEqual(len(self.widget.current_walker), 1)
+
+    def test_filter_with_empty(self):
+        self.widget._filter("")
+        self.assertIs(
+            self.widget.current_walker,
+            self.widget.base_walker)
+
+    def test_keypress_open_search(self):
+        self.widget.keypress(None, "/")
+        self.controller.open_edit.assert_called_with(self.widget.search_widget)
+
+    def test_keypress_clear_search(self):
+        self.widget._filter("test")
+        self.assertEqual(len(self.widget.current_walker), 0)
+
+        self.widget.keypress((0,), "q")
+        self.assertIs(
+            self.widget.current_walker,
+            self.widget.base_walker)
+        self.assertEqual(len(self.widget.current_walker), 2)
+
+    def test_keypress_back(self):
+        self.widget.keypress((0,), "q")
+        self.controller.back.assert_called_with()
+
+    def test_keypress_open_help(self):
+        self.assertIsNone(self.widget.keypress((0, 0), "?"))
+        self.controller.open_view.assert_called_with(self.widget.help_widget)
+
+    def test_keypress_bottom_and_top(self):
+        self.widget.keypress((10, 10), "G")
+        self.assertEqual(self.widget._w.focus_position, 1)
+        self.controller._update_info.assert_called_with(
+            self.widget, "[2/2]")
+
+        self.widget.keypress((10, 10), "g")
+        self.assertEqual(self.widget._w.focus_position, 0)
+        self.controller._update_info.assert_called_with(
+            self.widget, "[1/2]")
+
+    def test_keypress_bottom_and_top_when_search(self):
+        self.widget._filter("summary")
+        self.widget.keypress((10, 10), "G")
+        self.assertEqual(self.widget._w.focus_position, 1)
+        self.widget.keypress((10, 10), "g")
+        self.assertEqual(self.widget._w.focus_position, 0)
+
+    def test_keypress_clear_item(self):
+        self.widget.keypress((10, 10), "x")
+        self.assertEqual(len(self.widget.current_walker), 1)
+        self.controller._update_info.assert_called_with(
+            self.widget, "[1/1]")
+
+    def test_keypress_clear_items(self):
+        self.widget.keypress((10, 10), "X")
+        self.assertEqual(len(self.widget.current_walker), 0)
+        self.controller._update_info.assert_called_with(
+            self.widget, "[0/0]")
+
+    def test_keypress_up_and_down(self):
+        self.widget.keypress((10, 10), "down")
+        self.assertEqual(self.widget._w.focus_position, 1)
+        self.controller._update_info.assert_called_with(
+            self.widget, "[2/2]")
+
+        self.widget.keypress((10, 10), "up")
+        self.assertEqual(self.widget._w.focus_position, 0)
+        self.controller._update_info.assert_called_with(
+            self.widget, "[1/2]")
+
+    def test_auto_scroll(self):
+        displayer_context = DisplayerContext(
+            StaticDataStore(["summary 1", "summary 2"]),
+            BaseDisplayer())
+        self.context.config = Config(auto_scroll=True)
+        widget = SummaryListWidget(
+            displayer_context,
+            controller=self.controller,
+            context=self.context)
+        displayer_context.store.setup()
+
+        self.assertEqual(widget._w.focus_position, 1)
+        widget.current_walker.recv("summary 3")
+        self.assertEqual(widget._w.focus_position, 2)
+
+        widget._w.set_focus(0)
+        widget.current_walker.recv("summary 4")
+        self.assertEqual(widget._w.focus_position, 0)
+
+
+class TestSummary(unittest.TestCase):
+    def test_verify_keys(self):
+        _verify_keys(Actions([("p", "pppp", None)]))
+
+    def test_verify_keys_failed(self):
+        with self.assertRaises(ValueError):
+            _verify_keys(Actions([("/", "search", None)]))
---
+++ packages/gviewer/gviewer-3.0.3-py2-none-any.whl/gviewer/view/summary.py
@@ -0,0 +1,265 @@
+import urwid
+from collections import OrderedDict
+
+from ..basic_widget import BasicWidget, FocusableText, SearchWidget
+from .helper import (
+    HelpWidget, HelpContent, HelpCategory,
+    make_category_with_actions)
+from .detail import DetailWidget
+
+
+_ADVANCED_KEYS = OrderedDict([
+    ("/", "search"),
+    ("g", "top"),
+    ("G", "bottom"),
+    ("x", "clear current item"),
+    ("X", "clear all items"),
+    ("q", "quit")
+])
+
+
+def _verify_keys(actions):
+    for key, _, _ in actions:
+        if key in _ADVANCED_KEYS:
+            raise ValueError("key '{0}' had defined by GViewer for {1}".format(key, _ADVANCED_KEYS[key]))
+
+
+class SummaryItemWidget(BasicWidget):
+    """ Summary item widget
+
+    Summary item widget that display summary and defined action to open other view
+
+    Attributes:
+        message: Original message genrate by BaseDataStore
+        summary: Format message by displayer
+        displayer_context: DisplayerContext instance
+    """
+    def __init__(self, message, title, displayer_context, **kwargs):
+        super(SummaryItemWidget, self).__init__(
+            widget=self._widget(title),
+            **kwargs)
+
+        self.displayer_context = displayer_context
+        self.message = message
+
+    def _widget(self, title):
+        return FocusableText(title, attr_map="summary", focus_map="summary focus")
+
+    def get_title_as_plain_text(self):
+        """ Get title in plain text
+
+        Returns:
+            title in plain text
+        """
+        return self._w.get_plain_text()
+
+    def set_title(self, title):
+        self.display(self._widget(title))
+
+    def keypress(self, size, key):
+        if key == "enter":
+            try:
+                self.controller.open_view(DetailWidget(
+                    self.message, self.displayer_context, controller=self.controller,
+                    context=self.context))
+            except:  # pragma: no cover
+                self.controller.open_error()
+            return None
+        if key in self.displayer_context.actions:
+            try:
+                self.displayer_context.actions[key].__call__(
+                    self.controller, self.message, self)
+            except:  # pragma: no cover
+                self.controller.open_error()
+            return None
+
+        return super(SummaryItemWidget, self).keypress(size, key)  # pragma: no cover
+
+
+class SummaryListWalker(urwid.SimpleFocusListWalker):
+    """ Summary item widgets wrapper
+
+    Contains the SummaryItemWidget,
+    and used to receive message from data store
+
+    Attributes:
+        content: list of SummaryItemWidget
+        displayer_context: DisplayerContext instance
+    """
+    def __init__(self, content=None, displayer_context=None,
+                 base_walker=None, controller=None, context=None,
+                 on_receive=None):
+        super(SummaryListWalker, self).__init__(content or [])
+        self.controller = controller or base_walker.controller
+        self.context = context or base_walker.context
+        self.displayer_context = displayer_context or base_walker.displayer_context
+        self.on_receive = on_receive or base_walker.on_receive
+        self.base_walker = base_walker
+        self.displayer_context.store.register(self)
+
+    def recv(self, message):
+        """ Action when received message from data store
+
+        Will transform message into summary,
+        and generate a SummaryItemWidget into its content
+        """
+        try:
+            self.append(self._create_widget(message))
+        except:
+            self.controller.open_error()
+        else:
+            self.on_receive()
+
+    def _create_widget(self, message):
+            summary = self.displayer_context.displayer.summary(message)
+            return SummaryItemWidget(
+                message, summary, self.displayer_context, controller=self.controller, context=self.context)
+
+
+class FilterSummaryListWalker(SummaryListWalker):
+    """ Summary item widgets wrapper that filter by keyword
+
+    Optional display SummaryItemWidget depend on summary is match by keyword or not
+
+    Attributes:
+        base_walker: Original SummaryListWalker
+        keyword: Filter keyword
+    """
+    def __init__(self, base_walker, keyword):
+        content = [m for m in base_walker if base_walker.displayer_context.displayer.match(keyword, m.message, m.get_title_as_plain_text())]
+        super(FilterSummaryListWalker, self).__init__(
+            content=content, base_walker=base_walker)
+        self.keyword = keyword
+
+    def recv(self, message):
+        """ Action when received message from data store
+
+        Will transform message into summary,
+        and check message or summary is match by keyword or not,
+        generate a SummaryItemWidget and display it if match
+        """
+        try:
+            widget = self._create_widget(message)
+            if self.displayer_context.displayer.match(self.keyword, message, widget.get_title_as_plain_text()):
+                self.append(widget)
+        except:
+            self.controller.open_error()
+        else:
+            self.on_receive()
+
+    def close(self):
+        """ Unregister listener if quit search mode """
+        self.displayer_context.store.unregister(self)
+
+    def __delitem__(self, index):
+        if isinstance(index, slice):
+            for i in range(index.start, min(index.stop, len(self))):
+                base_walker_index = self.base_walker.index(self[i])
+                del self.base_walker[base_walker_index]
+        else:
+            base_walker_index = self.base_walker.index(self[index])
+            del self.base_walker[base_walker_index]
+        super(FilterSummaryListWalker, self).__delitem__(index)
+
+
+class SummaryListWidget(BasicWidget):
+    """ ListBox widget to contains the content of SummaryItemWidget
+
+    Attributes:
+        displayer_context: DisplayerContext
+    """
+    def __init__(self, displayer_context, **kwargs):
+        super(SummaryListWidget, self).__init__(**kwargs)
+        _verify_keys(displayer_context.actions)
+
+        self.base_walker = SummaryListWalker(
+            displayer_context=displayer_context,
+            on_receive=self._on_receive, **kwargs)
+        self.current_walker = self.base_walker
+
+        self.search_widget = SearchWidget(self._filter, self._clear_search)
+        self.help_widget = HelpWidget(
+            HelpContent(
+                [HelpCategory("Basic", self.context.config.keys),
+                 HelpCategory("Advanced", _ADVANCED_KEYS),
+                 make_category_with_actions("Custom", displayer_context.actions)]),
+            **kwargs
+        )
+
+        self.display(urwid.ListBox(self.base_walker))
+
+    def _filter(self, keyword):
+        if keyword:
+            new_walker = FilterSummaryListWalker(self.base_walker, keyword)
+        else:
+            new_walker = self.base_walker
+        if new_walker is not self.current_walker:
+            self._update_content(new_walker)
+        self.controller._focus_body()
+
+    def _update_content(self, walker):
+        if self.current_walker is not self.base_walker:
+            self.current_walker.close()
+
+        self.current_walker = walker
+        self.display(urwid.ListBox(walker))
+        self.update_info()
+
+    def _open_search(self):
+        self.search_widget.clear()
+        self.controller.open_edit(self.search_widget)
+
+    def _clear_search(self):
+        self._filter(None)
+        self.controller.close_edit()
+
+    def update_info(self):
+        if len(self.current_walker):
+            curr_index = self._w.focus_position + 1
+            total_index = len(self.current_walker)
+            self.controller._update_info(
+                self, "[{0}/{1}]".format(curr_index, total_index))
+        else:
+            self.controller._update_info(self, "[0/0]")
+
+    def _on_receive(self):
+        if self.context.config.auto_scroll:
+            curr_index = self._w.focus_position + 1
+            total_index = len(self.current_walker)
+            if curr_index == total_index - 1:
+                self._w.set_focus(curr_index)
+        self.update_info()
+
+    def keypress(self, size, key):
+        if key == "/":
+            self._open_search()
+            return None
+        if key == "q" and isinstance(self.current_walker, FilterSummaryListWalker):
+            self._clear_search()
+            return None
+        if key == "q":
+            self.controller.back()
+            return None
+        if key == "g":
+            self._w.set_focus(0)
+            self.update_info()
+            return super(SummaryListWidget, self).keypress(size, key)
+        if key == "G":
+            self._w.set_focus(len(self.current_walker) - 1)
+            self.update_info()
+            return super(SummaryListWidget, self).keypress(size, key)
+        if key == "x":
+            del self.current_walker[self._w.focus_position]
+            self.update_info()
+            return super(SummaryListWidget, self).keypress(size, key)
+        if key == "X":
+            del self.current_walker[:]
+            self.update_info()
+            return super(SummaryListWidget, self).keypress(size, key)
+        if key == "?":
+            self.controller.open_view(self.help_widget)
+            return None
+
+        keypress_result = super(SummaryListWidget, self).keypress(size, key)  # pragma: no cover
+        self.update_info()
+        return keypress_result
