2fbc7c297e8379dc348b4983db965b214f7739a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:46 +0000

    Add package talkzoho/0.5.8/talkzoho-0.5.8.tar.gz

---
+++ packages/talkzoho/talkzoho-0.5.8.tar.gz/talkzoho-0.5.8/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='talkzoho',
+    version='0.5.8',
+    url='https://github.com/A2Z-Cloud/Talk-Zoho',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    install_requires=[
+        'fuzzywuzzy',
+        'python-Levenshtein',
+        'requests',
+        'tornado'])
