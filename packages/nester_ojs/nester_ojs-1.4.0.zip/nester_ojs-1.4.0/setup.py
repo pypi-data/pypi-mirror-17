dd7e7556543aeb23c4c5fe1d5074ccfb16697742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:46 +0000

    Add package nester_ojs/1.4.0/nester_ojs-1.4.0.zip

---
+++ packages/nester_ojs/nester_ojs-1.4.0.zip/nester_ojs-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nester_ojs',
+	version = '1.4.0',
+	py_modules = ['nester_ojs'],
+	author = 'jisoko',
+	author_email = 'jisoko@korea.kr',
+	url = 'http://www.edunavi.kr',
+	description = 'A simple printer of nested lists',
+)
