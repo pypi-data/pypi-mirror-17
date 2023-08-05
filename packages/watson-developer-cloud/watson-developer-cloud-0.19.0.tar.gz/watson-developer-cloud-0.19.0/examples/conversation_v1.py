999e8a5b942494fd1427ef9959c0410f63f9a7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:33 +0000

    Add package watson-developer-cloud/0.19.0/watson-developer-cloud-0.19.0.tar.gz

---
+++ packages/watson-developer-cloud/watson-developer-cloud-0.19.0.tar.gz/watson-developer-cloud-0.19.0/examples/conversation_v1.py
@@ -0,0 +1,13 @@
+import json
+from watson_developer_cloud import ConversationV1
+
+conversation = ConversationV1(
+    username='YOUR SERVICE USERNAME',
+    password='YOUR SERVICE PASSWORD',
+    version='2016-07-11')
+
+# replace with your own workspace_id
+workspace_id = '293b58fc-3c5b-4ac5-a8f4-8d52c393d875'
+
+response = conversation.message(workspace_id=workspace_id, message_input={'text': 'What\'s the weather like?'})
+print(json.dumps(response, indent=2))
