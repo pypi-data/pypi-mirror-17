dd3c6b0eb0fae8e7c9a0f4dbaba86d3b7f642621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:17 +0000

    Add package epyc/0.7.1/epyc-0.7.1.tar.gz

---
+++ packages/epyc/epyc-0.7.1.tar.gz/epyc-0.7.1/epyc/__init__.py
@@ -0,0 +1,20 @@
+# Initialisation for epyc package
+#
+# Copyright (C) 2016 Simon Dobson
+#
+# Licensed under the GNU General Public Licence v.2.0
+#
+
+from .experiment import Experiment
+from .experimentcombinator import ExperimentCombinator
+from .repeatedexperiment import RepeatedExperiment
+from .summaryexperiment import SummaryExperiment
+
+from .lab import Lab
+from .clusterlab import ClusterLab
+
+from .labnotebook import LabNotebook
+from .jsonlabnotebook import JSONLabNotebook
+from .sqlitelabnotebook import SqliteLabNotebook
+
+
