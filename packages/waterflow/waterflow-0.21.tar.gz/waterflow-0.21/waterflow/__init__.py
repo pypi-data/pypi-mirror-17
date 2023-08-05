cdf4168b373dac140135b7188bb390ea39f97058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:28 +0000

    Add package waterflow/0.21/waterflow-0.21.tar.gz

---
+++ packages/waterflow/waterflow-0.21.tar.gz/waterflow-0.21/waterflow/__init__.py
@@ -0,0 +1,11 @@
+"""dataflow package provides a framework to build data analysis pipeline
+
+class:
+    Flow -- main class for the dataflow package ;
+        provides functionnal tools to transform a dataset
+        and do machine learning with it
+"""
+
+from flow import Flow, Action
+
+__all__ = [Flow, Action]
