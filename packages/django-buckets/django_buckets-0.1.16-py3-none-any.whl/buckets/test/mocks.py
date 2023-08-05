9f9f37adf453dcc5f2c894300533aa7066621e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:11 +0000

    Add package django-buckets/0.1.16/django_buckets-0.1.16-py3-none-any.whl

---
+++ packages/django-buckets/django_buckets-0.1.16-py3-none-any.whl/buckets/test/mocks.py
@@ -0,0 +1,25 @@
+import pytest
+import shutil
+import os
+
+from django.conf import settings
+
+from buckets.utils import ensure_dirs
+
+
+@pytest.fixture(scope='function')
+def make_dirs(request):
+    ensure_dirs('uploads', 'downloads', 'uploads/files')
+
+    def teardown():
+        shutil.rmtree(os.path.join(settings.MEDIA_ROOT, 's3'))
+    request.addfinalizer(teardown)
+
+
+def create_file():
+    path = os.path.join(settings.MEDIA_ROOT, 's3', 'text.txt')
+    file = open(path, 'wb')
+    file.write('Some content'.encode('utf-8'))
+    file.close()
+
+    return file
