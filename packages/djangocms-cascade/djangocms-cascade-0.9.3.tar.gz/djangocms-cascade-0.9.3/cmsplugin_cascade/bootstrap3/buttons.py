301cac18ec7f64270cdd990cad9a61ec17054b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:52 +0000

    Add package djangocms-cascade/0.9.3/djangocms-cascade-0.9.3.tar.gz

---
+++ packages/djangocms-cascade/djangocms-cascade-0.9.3.tar.gz/djangocms-cascade-0.9.3/cmsplugin_cascade/bootstrap3/buttons.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from collections import OrderedDict
+from django.forms import widgets
+from django.forms.fields import CharField
+from django.forms.widgets import RadioFieldRenderer
+from django.utils.html import format_html, format_html_join
+from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import force_text
+from django.utils.safestring import mark_safe
+from cms.plugin_pool import plugin_pool
+from cmsplugin_cascade.fields import PartialFormField
+from cmsplugin_cascade.link.config import LinkPluginBase, LinkElementMixin, LinkForm
+from cmsplugin_cascade.link.forms import TextLinkFormMixin
+from .glyphicons import GlyphiconRenderer
+
+
+class ButtonTypeRenderer(RadioFieldRenderer):
+    """
+    Render sample buttons in different colors in the button's backend editor.
+    """
+    BUTTON_TYPES = OrderedDict((('btn-default', _("Default")), ('btn-primary', _("Primary")),
+        ('btn-success', _("Success")), ('btn-info', _("Info")), ('btn-warning', _("Warning")),
+        ('btn-danger', _("Danger")), ('btn-link', _("Link")),))
+
+    @classmethod
+    def get_widget(cls):
+        choices = tuple((k, v) for k, v in cls.BUTTON_TYPES.items())
+        return widgets.RadioSelect(choices=choices, renderer=cls)
+
+    def render(self):
+        return format_html('<div class="form-row">{}</div>',
+            format_html_join('\n', '<div class="field-box">'
+                             '<span class="btn {1}">{2}</span>'
+                             '<div class="label">{0}</div></div>',
+                ((force_text(w), w.choice_value, force_text(self.BUTTON_TYPES[w.choice_value])) for w in self)
+            ))
+
+
+class ButtonSizeRenderer(RadioFieldRenderer):
+    """
+    Render sample buttons in different sizes in the button's backend editor.
+    """
+    BUTTON_SIZES = OrderedDict((('btn-lg', _("Large")), ('', _("Default")), ('btn-sm', _("Small")),
+        ('btn-xs', _("Extra small")),))
+
+    @classmethod
+    def get_widget(cls):
+        choices = tuple((k, v) for k, v in cls.BUTTON_SIZES.items())
+        return widgets.RadioSelect(choices=choices, renderer=cls)
+
+    def render(self):
+        return format_html('<div class="form-row">{}</div>',
+            format_html_join('\n',
+                '<div class="field-box"><div class="button-samples">'
+                    '<span class="btn btn-primary {1}">{2}</span>'
+                    '<span class="btn btn-default {1}">{2}</span></div>'
+                    '<div class="label">{0}</div>'
+                '</div>',
+                ((force_text(w), w.choice_value, force_text(self.BUTTON_SIZES[w.choice_value])) for w in self)
+            ))
+
+
+class BootstrapButtonMixin(object):
+    require_parent = True
+    parent_classes = ('BootstrapColumnPlugin', 'SimpleWrapperPlugin',)
+    render_template = 'cascade/bootstrap3/button.html'
+    allow_children = False
+    text_enabled = True
+    default_css_class = 'btn'
+    default_css_attributes = ('button-type', 'button-size', 'button-options', 'quick-float',)
+
+    glossary_fields = (
+        PartialFormField('button-type',
+            ButtonTypeRenderer.get_widget(),
+            label=_("Button Type"),
+            initial='btn-default',
+            help_text=_("Display Link using this Button Style")
+        ),
+        PartialFormField('button-size',
+            ButtonSizeRenderer.get_widget(),
+            label=_("Button Size"),
+            initial='',
+            help_text=_("Display Link using this Button Size")
+        ),
+        PartialFormField('button-options',
+            widgets.CheckboxSelectMultiple(choices=(('btn-block', _('Block level')), ('disabled', _('Disabled')),)),
+            label=_("Button Options"),
+        ),
+        PartialFormField('quick-float',
+            widgets.RadioSelect(choices=(('', _("Do not float")), ('pull-left', _("Pull left")), ('pull-right', _("Pull right")),)),
+            label=_("Quick Float"),
+            initial='',
+            help_text=_("Float the button to the left or right.")
+        ),
+        PartialFormField('icon-left',
+            GlyphiconRenderer.get_widget(),
+            label=_("Prepend icon"),
+            initial='',
+            help_text=_("Prepend a Glyphicon before the content.")
+        ),
+        PartialFormField('icon-right',
+            GlyphiconRenderer.get_widget(),
+            label=_("Append icon"),
+            initial='',
+            help_text=_("Append a Glyphicon after the content.")
+        ),
+    )
+
+    def render(self, context, instance, placeholder):
+        context = super(BootstrapButtonMixin, self).render(context, instance, placeholder)
+        mini_template = '{0}<span class="glyphicon glyphicon-{1} {2}" aria-hidden="true"></span>{3}'
+        icon_left = instance.glossary.get('icon-left')
+        if icon_left:
+            context['icon_left'] = format_html(mini_template, '', icon_left, 'cascade-icon-left', ' ')
+        icon_right = instance.glossary.get('icon-right')
+        if icon_right:
+            context['icon_right'] = format_html(mini_template, ' ', icon_right, 'cascade-icon-right', '')
+        return context
+
+
+class BootstrapButtonPlugin(BootstrapButtonMixin, LinkPluginBase):
+    module = 'Bootstrap'
+    name = _("Button")
+    model_mixins = (LinkElementMixin,)
+    fields = ('link_content',) + LinkPluginBase.fields  # @UndefinedVariable
+
+    class Media:
+        css = {'all': ('cascade/css/admin/bootstrap.min.css', 'cascade/css/admin/bootstrap-theme.min.css',)}
+
+    @classmethod
+    def get_identifier(cls, obj):
+        identifier = super(BootstrapButtonPlugin, cls).get_identifier(obj)
+        content = obj.glossary.get('link_content')
+        if not content:
+            try:
+                content = force_text(ButtonTypeRenderer.BUTTON_TYPES[obj.glossary['button-type']])
+            except KeyError:
+                content = _("Empty")
+        return format_html('{}{}', identifier, content)
+
+    def get_form(self, request, obj=None, **kwargs):
+        link_content = CharField(required=False, label=_("Button Content"),
+            widget=widgets.TextInput(attrs={'id': 'id_name'}))
+        Form = type(str('ButtonForm'), (TextLinkFormMixin, getattr(LinkForm, 'get_form_class')(),),
+                    {'link_content': link_content})
+        kwargs.update(form=Form)
+        return super(BootstrapButtonPlugin, self).get_form(request, obj, **kwargs)
+
+plugin_pool.register_plugin(BootstrapButtonPlugin)
---
+++ packages/djangocms-cascade/djangocms-cascade-0.9.3.tar.gz/djangocms-cascade-0.9.3/cmsplugin_cascade/settings.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from collections import OrderedDict
+import warnings
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+from cmsplugin_cascade.widgets import MultipleCascadingSizeWidget, ColorPickerWidget, SelectOverflowWidget
+
+
+CASCADE_PLUGINS = getattr(settings, 'CMSPLUGIN_CASCADE_PLUGINS', ('cmsplugin_cascade.generic', 'cmsplugin_cascade.link',))
+
+# all other configuration settings are replaced by a dictionary
+if hasattr(settings, 'CMSPLUGIN_CASCADE_ALIEN_PLUGINS'):
+    warnings.warn("CMSPLUGIN_CASCADE_ALIEN_PLUGINS is deprecated. "
+                  "Use CMSPLUGIN_CASCADE['alien_plugins'] instead.")
+
+if hasattr(settings, 'CMSPLUGIN_CASCADE_DEPENDENCIES'):
+    warnings.warn("CMSPLUGIN_CASCADE_DEPENDENCIES is deprecated. "
+                  "Use CMSPLUGIN_CASCADE['dependencies'] instead.")
+
+if hasattr(settings, 'CMSPLUGIN_CASCADE_WITH_EXTRAFIELDS'):
+    warnings.warn("CMSPLUGIN_CASCADE_WITH_EXTRAFIELDS is deprecated. "
+                  "Use CMSPLUGIN_CASCADE['plugins_with_extra_fields'] instead.")
+
+if hasattr(settings, 'CMSPLUGIN_CASCADE_WITH_SHARABLES'):
+    warnings.warn("CMSPLUGIN_CASCADE_WITH_SHARABLES is deprecated. "
+                  "Use CMSPLUGIN_CASCADE['plugins_with_sharables'] instead.")
+
+if hasattr(settings, 'CMSPLUGIN_CASCADE_LINKPLUGIN_CLASSES'):
+    warnings.warn("CMSPLUGIN_CASCADE_LINKPLUGIN_CLASSES is deprecated. "
+                  "Use CMSPLUGIN_CASCADE['link_plugin_classes'] instead.")
+
+if hasattr(settings, 'CMSPLUGIN_CASCADE_EXTRA_INLINE_STYLES'):
+    warnings.warn("CMSPLUGIN_CASCADE_EXTRA_INLINE_STYLES is deprecated. "
+                  "Use CMSPLUGIN_CASCADE['extra_inline_styles'] instead.")
+
+if hasattr(settings, 'CMSPLUGIN_CASCADE_SEGMENTATION_MIXINS'):
+    warnings.warn("CMSPLUGIN_CASCADE_SEGMENTATION_MIXINS is deprecated. "
+                  "Use CMSPLUGIN_CASCADE['segmentation_mixins'] instead.")
+
+if hasattr(settings, 'CMSPLUGIN_CASCADE_PLUGINS_WITH_EXTRA_RENDER_TEMPLATES'):
+    warnings.warn("CMSPLUGIN_CASCADE_PLUGINS_WITH_EXTRA_RENDER_TEMPLATES is deprecated. "
+                  "Use CMSPLUGIN_CASCADE['plugins_with_extra_render_templates'] instead.")
+
+CMSPLUGIN_CASCADE = getattr(settings, 'CMSPLUGIN_CASCADE', {})
+orig_config = dict(CMSPLUGIN_CASCADE)
+
+CMSPLUGIN_CASCADE.setdefault('alien_plugins', ['TextPlugin'])
+
+# Use a prefix to symbolize a Cascade plugin in case there are ambiguous names.
+CMSPLUGIN_CASCADE.setdefault('plugin_prefix', None)
+
+CMSPLUGIN_CASCADE['dependencies'] = {
+    'cascade/js/ring.js': 'cascade/js/underscore.js',
+    'cascade/js/admin/sharableglossary.js': 'cascade/js/ring.js',
+    'cascade/js/admin/segmentplugin.js': 'cascade/js/ring.js',
+    'cascade/js/admin/linkpluginbase.js': ('cascade/js/admin/sharableglossary.js',),
+    'cascade/js/admin/defaultlinkplugin.js': ('cascade/js/admin/linkpluginbase.js',),
+    'cascade/js/admin/imageplugin.js': ('cascade/js/admin/linkpluginbase.js',),
+    'cascade/js/admin/pictureplugin.js': ('cascade/js/admin/linkpluginbase.js',),
+}
+CMSPLUGIN_CASCADE['dependencies'].update(orig_config.get('dependencies', {}))
+
+if 'cmsplugin_cascade.extra_fields' in settings.INSTALLED_APPS:
+    CMSPLUGIN_CASCADE['plugins_with_extra_fields'] = [
+        'BootstrapButtonPlugin', 'BootstrapContainerPlugin', 'BootstrapRowPlugin',
+        'SimpleWrapperPlugin', 'HeadingPlugin', 'HorizontalRulePlugin',
+    ]
+    CMSPLUGIN_CASCADE['plugins_with_extra_fields'].extend(orig_config.get('plugins_with_extra_fields', []))
+else:
+    CMSPLUGIN_CASCADE['plugins_with_extra_fields'] = []
+
+if 'cmsplugin_cascade.sharable' in settings.INSTALLED_APPS:
+    CMSPLUGIN_CASCADE.setdefault('plugins_with_sharables', {})
+else:
+    CMSPLUGIN_CASCADE['plugins_with_sharables'] = {}
+
+CMSPLUGIN_CASCADE.setdefault('link_plugin_classes', (
+    'cmsplugin_cascade.link.plugin_base.DefaultLinkPluginBase',
+    'cmsplugin_cascade.link.plugin_base.LinkElementMixin',
+    'cmsplugin_cascade.link.forms.LinkForm',
+))
+"""
+3-Tuple containing shared base classes for classes wishing to link onto something:
+1. Base class for a CMSPlugin, 2. A Model Mixin, 3. The base class used to build the form.
+"""
+
+CMSPLUGIN_CASCADE.setdefault('plugins_with_bookmark', [
+    'SimpleWrapperPlugin', 'HeadingPlugin'])
+
+CMSPLUGIN_CASCADE.setdefault('bookmark_prefix', '')
+
+CMSPLUGIN_CASCADE['extra_inline_styles'] = OrderedDict((
+    ('Margins', (('margin-top', 'margin-right', 'margin-bottom', 'margin-left',), MultipleCascadingSizeWidget)),
+    ('Paddings', (('padding-top', 'padding-right', 'padding-bottom', 'padding-left',), MultipleCascadingSizeWidget)),
+    ('Widths', (('min-width', 'width', 'max-width',), MultipleCascadingSizeWidget)),
+    ('Heights', (('min-height', 'height', 'max-height',), MultipleCascadingSizeWidget)),
+    ('Colors', (('color', 'background-color',), ColorPickerWidget)),
+    ('Overflow', (('overflow', 'overflow-x', 'overflow-y',), SelectOverflowWidget)),
+))
+CMSPLUGIN_CASCADE['extra_inline_styles'].update(orig_config.get('extra_inline_styles', {}))
+
+CMSPLUGIN_CASCADE.setdefault('segmentation_mixins', [
+    ('cmsplugin_cascade.segmentation.mixins.EmulateUserModelMixin', 'cmsplugin_cascade.segmentation.mixins.EmulateUserAdminMixin')
+])
+
+CMSPLUGIN_CASCADE.setdefault('plugins_with_extra_render_templates', {
+    'TextLinkPlugin': (
+        ('cascade/link/text-link.html', _("default")),
+        ('cascade/link/text-link-linebreak.html', _("with line break")),
+    )
+})
