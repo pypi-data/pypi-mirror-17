4831554a8676dfa4e7fbbd8ca80b2a5d7a273bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:54 +0000

    Add package norman_list/1.3.0/norman_list-1.3.0.zip

---
+++ packages/norman_list/norman_list-1.3.0.zip/norman_list-1.3.0/多層陣列清單取出.py
@@ -0,0 +1,9 @@
+def alist(b,c=0):
+    '''這是一個多層陣列清單執行條列取出迴圈程式'''
+    for a in b:
+        if isinstance(a,list):
+            alist(a,c+1)
+        else:
+            for d in range(c):
+                print("\t", end="")
+            print(a)
