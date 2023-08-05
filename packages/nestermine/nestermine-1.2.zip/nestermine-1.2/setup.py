438728720de6ed2c2489b4d06084733577516406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:50 +0000

    Add package nestermine/1.2/nestermine-1.2.zip

---
+++ packages/nestermine/nestermine-1.2.zip/nestermine-1.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+            
+setup(
+    name='nestermine',
+    version='1.2',
+    py_modules=['nestermine'],
+    author='L21',
+    author_email='191433230@qq.com',
+    url='http://www.cnaicheng.com',
+    description='A simple printer of nested list',
+    )
