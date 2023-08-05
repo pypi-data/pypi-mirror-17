e30cfbb6684cc761c192dc64dfeb786bfb95c62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:53 +0000

    Add package project-generator/0.9.10/project_generator-0.9.10.zip

---
+++ packages/project-generator/project_generator-0.9.10.zip/project_generator-0.9.10/setup.py
@@ -0,0 +1,53 @@
+# Copyright 2014-2015 0xc0170
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import pip
+
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+requirements = [str(requirement.req) for requirement in parse_requirements('requirements.txt', session=pip.download.PipSession())]
+
+setup(
+    name='project_generator',
+    version='0.9.10',
+    description='Project generators for various embedded tools (IDE). IAR, uVision, Makefile and many more in the roadmap!',
+    author='Martin Kojtal',
+    author_email='c0170@rocketmail.com',
+    keywords="c cpp project generator embedded",
+    url="https://github.com/project-generator/project_generator",
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development"
+    ],
+    long_description=read('pypi_readme.rst'),
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            "project_generator=project_generator.main:main",
+            "progen=project_generator.main:main",
+        ]
+    },
+
+    install_requires = requirements,
+    include_package_data = True,
+)
