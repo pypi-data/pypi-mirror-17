fa66878c9ddcca1aa1fe744f8fec3a196d1e582e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:22 +0000

    Add package PI_INFO_SENDER/0.0.2/PI_INFO_SENDER-0.0.2.tar.gz

---
+++ packages/PI_INFO_SENDER/PI_INFO_SENDER-0.0.2.tar.gz/PI_INFO_SENDER-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "PI_INFO_SENDER",
+    version = "0.0.2",
+    author = "Thepnimit&Wijakapong",
+    author_email = "wwilaik@celestica.com",
+    description = ("An demonstration of how to create, document, and publish "
+                                   "to the cheese shop a5 pypi.org."),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    url = "http://packages.python.org/PI_INFO_SENDER",
+    packages=['PI_INFO_SENDER', 'tests'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
