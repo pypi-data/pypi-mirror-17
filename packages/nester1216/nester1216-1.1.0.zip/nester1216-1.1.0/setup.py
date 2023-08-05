02959f8a863378dd7dd6dc1a06dd29cbdd950d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:41 +0000

    Add package nester1216/1.1.0/nester1216-1.1.0.zip

---
+++ packages/nester1216/nester1216-1.1.0.zip/nester1216-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+       name         = 'nester1216',
+       version      = '1.1.0',
+       py_module    = ['nester1216'],
+       author       = 'imeun0',
+       author_email = 'imeun0@gmail.com',
+       url          = 'http://toboost.me/moodle',
+       description  = 'A simple printer of nested list',
+       )
+
