ad0832f9cf775d824a4c20f678e6cc5bc5749b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:10 +0000

    Add package remote-docker/0.3/remote-docker-0.3.tar.gz

---
+++ packages/remote-docker/remote-docker-0.3.tar.gz/remote-docker-0.3/src/errors.py
@@ -0,0 +1,17 @@
+class RemoteDockerError(Exception): pass
+
+class ArgumentError(RemoteDockerError): pass
+
+class TagNotFound(RemoteDockerError): pass
+
+class HostNotFound(RemoteDockerError): pass
+
+class LatestHostNotFound(RemoteDockerError): pass
+
+class JobDuplicate(RemoteDockerError): pass
+
+class JobNotStarted(RemoteDockerError): pass
+
+class WrongDockerExitcode(RemoteDockerError): pass
+
+class WrongExitCode(RemoteDockerError): pass
