e69acd8752716dcce138a3abced39ecf68480aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:31 +0000

    Add package gdapi-python/0.5.3/gdapi-python-0.5.3.tar.gz

---
+++ packages/gdapi-python/gdapi-python-0.5.3.tar.gz/gdapi-python-0.5.3/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+# https://caremad.io/2013/07/setup-vs-requirement/
+
+with open('./requirements.txt') as r:
+    # strip fixed version info from requirements file
+    requirements = [line.split('=', 1)[0] for line in r]
+
+setup(
+    name='gdapi-python',
+    version='0.5.3',
+    py_modules=['gdapi'],
+    url='https://github.com/godaddy/gdapi-python',
+    license='MIT Style',
+    author='Darren Shepherd',
+    author_email='darren.s.shepherd@gmail.com',
+    description='Python API and CLI for GDAPI standard',
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'gdapi = gdapi:_main'
+        ]
+    }
+)
