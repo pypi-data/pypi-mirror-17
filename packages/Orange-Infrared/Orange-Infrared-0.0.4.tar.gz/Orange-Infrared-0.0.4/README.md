92adef4b2726ce619d03450d55bd85c384eb9070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:55 +0000

    Add package Orange-Infrared/0.0.4/Orange-Infrared-0.0.4.tar.gz

---
+++ packages/Orange-Infrared/Orange-Infrared-0.0.4.tar.gz/Orange-Infrared-0.0.4/README.md
@@ -0,0 +1,27 @@
+Orange for spectral data
+========================
+
+This is an add-on for [Orange3](http://orange.biolab.si) for the analysis
+of spectral data.
+
+Installation
+------------
+
+To install the add-on, run
+
+    python setup.py install
+
+To register this add-on with Orange, but keep the code in the development
+directory (do not copy it to Python's site-packages directory), run
+
+    python setup.py develop
+
+Usage
+-----
+
+After the installation, the widgets from this add-on are registered with
+Orange. To run Orange from the terminal, use
+
+    python -m Orange.canvas
+
+The new widget appears in the toolbox bar under the section Infrared.
---
+++ packages/Orange-Infrared/Orange-Infrared-0.0.4.tar.gz/Orange-Infrared-0.0.4/orangecontrib/infrared/data.py
@@ -0,0 +1,133 @@
+from Orange.data.io import FileFormat
+import numpy as np
+import Orange
+from .pymca5 import OmnicMap
+
+
+class DptReader(FileFormat):
+    """ Reader for files with two columns of numbers (X and Y)"""
+    EXTENSIONS = ('.dpt',)
+    DESCRIPTION = 'X-Y pairs'
+
+    def read(self):
+        tbl = np.loadtxt(self.filename)
+        domvals = tbl.T[0]  # first column is attribute name
+        domain = Orange.data.Domain([Orange.data.ContinuousVariable.make("%f" % f) for f in domvals], None)
+        datavals = tbl.T[1:]
+        return Orange.data.Table(domain, datavals)
+
+    @staticmethod
+    def write_file(filename, data):
+        pass #not implemented
+
+
+class OmnicMapReader(FileFormat):
+    """ Reader for files with two columns of numbers (X and Y)"""
+    EXTENSIONS = ('.map',)
+    DESCRIPTION = 'Omnic map'
+
+    def read(self):
+        om = OmnicMap.OmnicMap(self.filename)
+        info = om.info
+        X = om.data
+
+        try:
+            lv = info['OmnicInfo']['Last X value']
+            fv = info['OmnicInfo']['First X value']
+            features = np.linspace(fv, lv, num=X.shape[-1])
+        except KeyError:
+            #just start counting from 0 when nothing is known
+            features = np.arange(X.shape[-1])
+
+        try:
+            loc_first = info['OmnicInfo']["First map location"]
+            loc_last = info['OmnicInfo']["Last map location"]
+            x_locs = np.linspace(min(loc_first[0], loc_last[0]),
+                                 max(loc_first[0], loc_last[0]), X.shape[1])
+            y_locs = np.linspace(min(loc_first[1], loc_last[1]),
+                                 max(loc_first[1], loc_last[1]), X.shape[0])
+        except KeyError:
+            x_locs = None
+            y_locs = None
+
+        spectra = []
+        metadata = []
+
+        for ir, row in enumerate(X):
+            for ic, column in enumerate(row):
+                print(ir, ic, column)
+                spectra.append(column)
+                if x_locs is not None and y_locs is not None:
+                    x = x_locs[ic]
+                    y = y_locs[ir]
+                    metadata.append({"map_x": x, "map_y": y})
+                else:
+                    metadata.append({})
+
+        import itertools
+
+        metakeys = sorted(set(itertools.chain.from_iterable(metadata)))
+        metas = []
+        for mk in metakeys:
+            if mk in ["map_x", "map_y"]:
+                metas.append(Orange.data.ContinuousVariable.make(mk))
+            else:
+                metas.append(Orange.data.StringVariable.make(mk))
+
+        domain = Orange.data.Domain(
+            [Orange.data.ContinuousVariable.make("%f" % f) for f in features],
+            None, metas=metas)
+        data = Orange.data.Table(domain, spectra)
+        for ex, exmeta in zip(data, metadata):
+            for k,v in exmeta.items():
+                ex[k] = v
+
+        return data
+
+    @staticmethod
+    def write_file(filename, data):
+        pass #not implemented
+
+
+def build_spec_table(wavenumbers, intensities):
+    """
+    Converts numpy arrays of wavenumber and intensity into an
+    Orange.data.Table spectra object.
+
+    Args:
+        wavenumbers (np.array): 1D array of wavenumbers
+        intensities (np.array): 2D array of (multi-spectra) intensities
+
+    Returns:
+        table: Orange.data.Table object, spectra format
+    """
+
+    # Add dimension to 1D array if necessary
+    if intensities.ndim == 1:
+        intensities = intensities[None,:]
+
+    # Convert the wavenumbers array into a list of ContinousVariables
+    wn_vars = [Orange.data.ContinuousVariable.make(repr(wavenumbers[i]))
+                for i in range(wavenumbers.shape[0])]
+
+    # Build an Orange.data.Domain object with wn_vars as
+    # independant variables (or "attributes" as Orange calls them)
+    domain = Orange.data.Domain(wn_vars)
+
+    # Finally, build the table using the damain and intensity arrays:
+    table = Orange.data.Table.from_numpy(domain, intensities)
+    return table
+
+
+
+def getx(data):
+    """
+    Return x of the data. If all attribute names are numbers,
+    return their values. If not, return indices.
+    """
+    x = np.arange(len(data.domain.attributes))
+    try:
+        x = np.array([float(a.name) for a in data.domain.attributes])
+    except:
+        pass
+    return x
