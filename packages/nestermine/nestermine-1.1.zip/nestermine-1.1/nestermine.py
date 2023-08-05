479c43bcf3f0e2bb55c8cc753fff5eec19ab3fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:50 +0000

    Add package nestermine/1.1/nestermine-1.1.zip

---
+++ packages/nestermine/nestermine-1.1.zip/nestermine-1.1/nestermine.py
@@ -0,0 +1,12 @@
+''' this is the netser module'''
+def print_lol(the_list,level):
+    '''this is teh nester function'''
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print('\t',end='')
+            print(each_item)
+
+            
