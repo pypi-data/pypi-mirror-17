f7b98329e5f3c202daf19d1cf263379ad06cd6e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:47 +0000

    Add package koala2/0.0.14/koala2-0.0.14.tar.gz

---
+++ packages/koala2/koala2-0.0.14.tar.gz/koala2-0.0.14/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from setuptools import Extension
+from setuptools import find_packages
+from Cython.Build import cythonize
+
+setup(
+    name = "koala2",
+
+    version = "0.0.14",
+
+    author = "Ants, open innovation lab",
+    author_email = "contact@ants.builders",
+
+    packages = find_packages(),
+
+    include_package_data = True,
+
+    url = "https://github.com/anthill/koala",
+
+    license = "MIT",
+    description = "A python module to extract all the content of an Excel document and enable calculation without Excel",
+
+    long_description = open("README.md").read(),
+
+    install_requires = [
+        "lxml"
+    ],
+
+    # ext_modules = cythonize(["koala/*.py", "koala/ast/*.py"]),
+
+
+)
