1231653e88b6569ded864899aedfad7c4c491ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:40 +0000

    Add package alerta-server/4.7.33/alerta-server-4.7.33.tar.gz

---
+++ packages/alerta-server/alerta-server-4.7.33.tar.gz/alerta-server-4.7.33/alerta/app/__init__.py
@@ -0,0 +1,97 @@
+
+import os
+import sys
+import logging
+
+from flask import Flask
+from flask_cors import CORS
+
+LOG_FORMAT = '%(asctime)s - %(name)s[%(process)d]: %(levelname)s - %(message)s'
+
+app = Flask(__name__, static_url_path='')
+
+app.config.from_object('alerta.settings')
+app.config.from_pyfile('/etc/alertad.conf', silent=True)
+app.config.from_envvar('ALERTA_SVR_CONF_FILE', silent=True)
+
+if 'DEBUG' in os.environ:
+    app.debug = True
+
+if 'BASE_URL' in os.environ:
+    app.config['BASE_URL'] = os.environ['BASE_URL']
+
+if 'SECRET_KEY' in os.environ:
+    app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+
+if 'AUTH_REQUIRED' in os.environ:
+    app.config['AUTH_REQUIRED'] = True if os.environ['AUTH_REQUIRED'] == 'True' else False
+
+if 'ADMIN_USERS' in os.environ:
+    app.config['ADMIN_USERS'] = os.environ['ADMIN_USERS'].split(',')
+
+if 'CUSTOMER_VIEWS' in os.environ:
+    app.config['CUSTOMER_VIEWS'] = True if os.environ['CUSTOMER_VIEWS'] == 'True' else False
+
+if 'OAUTH2_CLIENT_ID' in os.environ:
+    app.config['OAUTH2_CLIENT_ID'] = os.environ['OAUTH2_CLIENT_ID']
+
+if 'OAUTH2_CLIENT_SECRET' in os.environ:
+    app.config['OAUTH2_CLIENT_SECRET'] = os.environ['OAUTH2_CLIENT_SECRET']
+
+if 'ALLOWED_EMAIL_DOMAINS' in os.environ:
+    app.config['ALLOWED_EMAIL_DOMAINS'] = os.environ['ALLOWED_EMAIL_DOMAINS'].split(',')
+
+if 'ALLOWED_GITHUB_ORGS' in os.environ:
+    app.config['ALLOWED_GITHUB_ORGS'] = os.environ['ALLOWED_GITHUB_ORGS'].split(',')
+
+if 'GITLAB_URL' in os.environ:
+    app.config['GITLAB_URL'] = os.environ['GITLAB_URL']
+
+if 'ALLOWED_GITLAB_GROUPS' in os.environ:
+    app.config['ALLOWED_GITLAB_GROUPS'] = os.environ['ALLOWED_GITLAB_GROUPS'].split(',')
+
+if 'CORS_ORIGINS' in os.environ:
+    app.config['CORS_ORIGINS'] = os.environ['CORS_ORIGINS'].split(',')
+
+if 'MAIL_FROM' in os.environ:
+    app.config['MAIL_FROM'] = os.environ['MAIL_FROM']
+
+if 'SMTP_PASSWORD' in os.environ:
+    app.config['SMTP_PASSWORD'] = os.environ['SMTP_PASSWORD']
+
+if 'PLUGINS' in os.environ:
+    app.config['PLUGINS'] = os.environ['PLUGINS'].split(',')
+
+if app.debug:
+    app.debug_log_format = LOG_FORMAT
+    app.logger.setLevel(logging.DEBUG)
+else:
+    stderr_handler = logging.StreamHandler()
+    stderr_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    app.logger.addHandler(stderr_handler)
+    app.logger.setLevel(logging.INFO)
+
+# Runtime config check
+if app.config['CUSTOMER_VIEWS'] and not app.config['AUTH_REQUIRED']:
+    raise RuntimeError('Must enable authentication to use customer views')
+
+if app.config['CUSTOMER_VIEWS'] and not app.config['ADMIN_USERS']:
+    raise RuntimeError('Customer views is enabled but there are no admin users')
+
+cors = CORS(app)
+
+from alerta.app.database import Mongo
+db = Mongo()
+
+if sys.version_info[0] == 2:
+    import views
+    import webhooks.views
+    import oembed.views
+    import management.views
+    import auth
+else:
+    from .views import *
+    from .webhooks.views import *
+    from .oembed.views import *
+    from .management.views import *
+    from .auth import *
---
+++ packages/alerta-server/alerta-server-4.7.33.tar.gz/alerta-server-4.7.33/alerta/plugins/__init__.py
@@ -0,0 +1,52 @@
+
+import abc
+import pkg_resources
+
+from six import add_metaclass
+from alerta.app import app
+
+LOG = app.logger
+
+
+class RejectException(Exception):
+    """The alert was rejected because the format did not meet the required policy."""
+
+
+@add_metaclass(abc.ABCMeta)
+class PluginBase(object):
+
+    def __init__(self, name=None):
+        self.name = name or self.__module__
+
+    @abc.abstractmethod
+    def pre_receive(self, alert):
+        """Pre-process an alert based on alert properties or reject it by raising RejectException."""
+        return alert
+
+    @abc.abstractmethod
+    def post_receive(self, alert):
+        """Send an alert to another service or notify users."""
+        return None
+
+    @abc.abstractmethod
+    def status_change(self, alert, status, text):
+        """Trigger integrations based on status changes."""
+        return None
+
+
+def load_plugins(namespace='alerta.plugins'):
+
+    plugins = []
+    for ep in list(pkg_resources.iter_entry_points(namespace)):
+        LOG.debug("Server plug-in '%s' found.", ep.name)
+        try:
+            if ep.name in app.config['PLUGINS']:
+                plugin = ep.load()
+                if plugin:
+                    plugins.append(plugin(ep.name))
+                    LOG.info("Server plug-in '%s' enabled.", ep.name)
+            else:
+                LOG.debug("Server plug-in '%s' not enabled in 'PLUGINS'.", ep.name)
+        except Exception as e:
+            LOG.error("Server plug-in '%s' could not be loaded: %s", ep.name, e)
+    return plugins
