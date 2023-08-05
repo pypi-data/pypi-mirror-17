d83151ee1e69b2b9fd5c315ebb5b1bf14c5ccbb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:20 +0000

    Add package cvx/0.1/cvx-0.1.tar.gz

---
+++ packages/cvx/cvx-0.1.tar.gz/cvx-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="cvx",
+    version="0.1",
+    description="OpenCV 'extension' with routinely used methods",
+    url="https://github.com/erikperillo/cvx",
+    author="Erik Perillo",
+    author_email="erik.perillo@gmail.com",
+    license="GPL3",
+    packages=["cvx"],
+    classifiers=["Programming Language :: Python :: 2.7"],
+    zip_safe=False,
+    install_requires=["numpy"]
+) 
