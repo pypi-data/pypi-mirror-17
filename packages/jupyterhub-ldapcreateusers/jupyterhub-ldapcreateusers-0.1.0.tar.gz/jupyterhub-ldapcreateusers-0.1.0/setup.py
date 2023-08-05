972bab7fc2ad85a78d03377439457edcbb2f01aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:34 +0000

    Add package jupyterhub-ldapcreateusers/0.0.1/jupyterhub-ldapcreateusers-0.1.0.tar.gz

---
+++ packages/jupyterhub-ldapcreateusers/jupyterhub-ldapcreateusers-0.1.0.tar.gz/jupyterhub-ldapcreateusers-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='jupyterhub-ldapcreateusers',
+    version='0.1.0',
+    description='Add on for the jupyterhub-ldapauthenticator to create system users',
+    long_description='This module requires the jupyterhub-ldapauthenticator module and does nothing\
+                       by itself. jupyterhub-ldapcreateusers allows jupyterhub to create system\
+                       accounts for LDAP authenticated users.',
+    url='https://github.com/benhosmer/jupyterhub-ldapcreateusers',
+    author='Ben Hosmer',
+    author_email='ben@radarearth.com',
+    license='Apache 2',
+    packages=['ldapcreateusers'],
+    install_requires=[
+        'jupyterhub-ldapauthenticator',
+    ]
+)
