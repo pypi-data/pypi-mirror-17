9c169bd24f257fa1fbd4d4e8a054acd252918bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:29 +0000

    Add package fwpt_apatcher/0.6.3/fwpt_apatcher-0.6.3.zip

---
+++ packages/fwpt_apatcher/fwpt_apatcher-0.6.3.zip/fwpt_apatcher-0.6.3/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+import apather_do as ad
+
+setup(
+    name='fwpt_apatcher',
+    version=ad.__version__,
+    packages=[''],
+    install_requires=[
+        "pymorphy2",
+        "docx"
+    ],
+    url='',
+    license='MIT',
+    author='alex1ops',
+    author_email='ale51098223@yandex.ru',
+    description='Программа формирования template и сопроводительной документации для патчей ',
+    data_files=[('config', ['cfg/config.ini', 'cfg/_temp.sql'])]
+)
