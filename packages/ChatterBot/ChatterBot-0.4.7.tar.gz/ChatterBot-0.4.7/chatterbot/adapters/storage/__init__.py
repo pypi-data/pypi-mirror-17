1662f4f14dcb4f3065500c0c0ec17ac206751b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:13 +0000

    Add package ChatterBot/0.4.7/ChatterBot-0.4.7.tar.gz

---
+++ packages/ChatterBot/ChatterBot-0.4.7.tar.gz/ChatterBot-0.4.7/chatterbot/adapters/storage/__init__.py
@@ -0,0 +1,4 @@
+from .storage_adapter import StorageAdapter
+from .jsonfile import JsonFileStorageAdapter, JsonDatabaseAdapter
+from .mongodb import MongoDatabaseAdapter
+from .twitter_storage import TwitterAdapter
