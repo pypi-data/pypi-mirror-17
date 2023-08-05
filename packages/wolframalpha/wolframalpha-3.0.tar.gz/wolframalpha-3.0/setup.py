9194dd9f7684c98cd6a5f268145c6cc7cef5d0e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:10 +0000

    Add package wolframalpha/3.0/wolframalpha-3.0.tar.gz

---
+++ packages/wolframalpha/wolframalpha-3.0.tar.gz/wolframalpha-3.0/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+# Project skeleton maintained at https://github.com/jaraco/skeleton
+
+import io
+import sys
+
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+	long_description = readme.read()
+
+needs_pytest = {'pytest', 'test'}.intersection(sys.argv)
+pytest_runner = ['pytest_runner'] if needs_pytest else []
+needs_sphinx = {'release', 'build_sphinx', 'upload_docs'}.intersection(sys.argv)
+sphinx = ['sphinx', 'rst.linker'] if needs_sphinx else []
+needs_wheel = {'release', 'bdist_wheel'}.intersection(sys.argv)
+wheel = ['wheel'] if needs_wheel else []
+
+name = 'wolframalpha'
+description = 'Wolfram|Alpha 2.0 API client'
+
+setup_params = dict(
+	name=name,
+	use_scm_version=True,
+	author="Jason R. Coombs",
+	author_email="jaraco@jaraco.com",
+	description=description or name,
+	long_description=long_description,
+	url="https://github.com/jaraco/" + name,
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+	namespace_packages=name.split('.')[:-1],
+	install_requires=[
+		'six',
+		'xmltodict',
+		'jaraco.itertools>=2.0',
+	],
+	extras_require={
+	},
+	setup_requires=[
+		'setuptools_scm>=1.9',
+	] + pytest_runner + sphinx + wheel,
+	tests_require=[
+		'pytest>=2.8',
+		'pmxbot',
+	],
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+	],
+	entry_points={
+		'pmxbot_handlers': [
+		    'Wolfram|Alpha = wolframalpha.pmxbot',
+        	],
+	},
+)
+if __name__ == '__main__':
+	setuptools.setup(**setup_params)
