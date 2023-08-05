946440f4ce137f68259018fcff95c29c4b7c0b7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:40 +0000

    Add package PLambda/1.0.9/PLambda-1.0.9.tar.gz

---
+++ packages/PLambda/PLambda-1.0.9.tar.gz/PLambda-1.0.9/plambda/util/Util.py
@@ -0,0 +1,12 @@
+import sys
+
+def isString(data):
+    if (sys.version_info[0] == 3):
+        return isinstance(data, str)
+    return isinstance(data, basestring)
+
+
+def isInteger(data):
+    if (sys.version_info[0] == 3):
+        return isinstance(data, int)
+    return isinstance(data, (int, long))
