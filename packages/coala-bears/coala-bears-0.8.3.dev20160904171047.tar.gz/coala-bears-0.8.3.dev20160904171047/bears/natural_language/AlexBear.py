fbf0d9f5fb6b48403b502e014ab50f8427ab0b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:12 +0000

    Add package coala-bears/0.8.3.dev20160904171047/coala-bears-0.8.3.dev20160904171047.tar.gz

---
+++ packages/coala-bears/coala-bears-0.8.3.dev20160904171047.tar.gz/coala-bears-0.8.3.dev20160904171047/bears/natural_language/AlexBear.py
@@ -0,0 +1,26 @@
+import re
+
+from coalib.bearlib.abstractions.Lint import Lint
+from coalib.bears.requirements.NpmRequirement import NpmRequirement
+from coalib.bears.LocalBear import LocalBear
+
+
+class AlexBear(LocalBear, Lint):
+    executable = 'alex'
+    output_regex = re.compile(
+        r'\s+(?P<line>\d+):(?P<column>\d+)\-'
+        r'(?P<end_line>\d+):(?P<end_column>\d+)'
+        r'\s+(?:(warning))\s+(?P<message>.+)')
+    arguments = "{filename}"
+    LANGUAGES = {"Natural Language"}
+    REQUIREMENTS = {NpmRequirement('alex', '2')}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+
+    def run(self, filename, file):
+        '''
+        Checks the markdown file with Alex - Catch insensitive,
+        inconsiderate writing.
+        '''
+        return self.lint(filename)
