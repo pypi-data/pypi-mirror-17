668a5f67533a7f704ba48e2a53d351800caf526c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:10 +0000

    Add package 908dist/1.2.1/908dist-1.2.1.zip

---
+++ packages/908dist/908dist-1.2.1.zip/908dist-1.2.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='908dist',
+    version='1.2.1',
+    py_modules=['nester'],
+    author='labixiaoke',
+    author_email='shaokelv@163.com',
+    url='http://www.shaokelv.163.com',
+    description='A simple printer ',
+
+
+
+    )
