9e3274ba3d14287fce7d9b7e2015c34be0cd1fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:04 +0000

    Add package juno-magic/0.1.10/juno-magic-0.1.10.tar.gz

---
+++ packages/juno-magic/juno-magic-0.1.10.tar.gz/juno-magic-0.1.10/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(name='juno-magic',
+      version='0.1.10',
+      description='IPython magics and utilities to work with bridged kernels',
+      url='https://github.com/timbr-io/juno-magic',
+      author='Pramukta Kumar',
+      author_email='pramukta.kumar@timbr.io',
+      license='MIT',
+      packages=['juno_magic', 'juno_magic.extensions'],
+      zip_safe=False,
+      data_files=[
+        ('share/jupyter/nbextensions/juno_magic', [
+            'juno_magic/static/index.js',
+            'juno_magic/static/juno.css'
+        ]),
+      ],
+      entry_points={
+          "console_scripts": [
+              "wampify = juno_magic.bridge:main"
+          ]
+      },
+      install_requires=[
+          "ipython",
+          "autobahn",
+          "twisted",
+          "txzmq",
+          "sh",
+          "pyopenssl",
+          "service_identity",
+          "requests",
+          "zope.interface"
+        ]
+      )
