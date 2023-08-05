b43edc4cc27dc935c0b4880b0f7f846e4e69dd06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:47 +0000

    Add package pyoctree/0.1.2/pyoctree-0.1.2.tar.gz

---
+++ packages/pyoctree/pyoctree-0.1.2.tar.gz/pyoctree-0.1.2/pyoctree/version.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2015 Michael Hogg
+
+# This file is part of pyoctree - See LICENSE.txt for information on usage and redistribution
+
+# Version
+__version__ = '0.1.2'
