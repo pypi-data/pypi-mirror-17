83aef04c2917ce157866c4d82109dafdd5a9f5d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package peony-twitter/0.2.2/peony-twitter-0.2.2.tar.gz

---
+++ packages/peony-twitter/peony-twitter-0.2.2.tar.gz/peony-twitter-0.2.2/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import re
+import inspect
+
+from setuptools import setup, find_packages
+
+
+def get_metadata(metadatafile):
+    with open(metadatafile) as stream:
+        text = stream.read()
+
+        ex = "__(\w*?)__ *?= *?[\"\']([^\"\']*)"
+        items = re.findall(ex, text)
+        metadata = {key: value for key, value in items}
+
+        metadata['keywords'] = metadata.get('keywords', '').split(', ')
+
+        ex = '"{3}[ \n]*(?P<name>.*)[ \t\n]+(?P<description>.*)'
+        metadata.update(re.search(ex, text).groupdict())
+
+    return metadata
+
+
+def get_requirements(requirementsfile):
+    with open(requirementsfile) as stream:
+        prog = re.compile("# (\S+)\n([^#]*)")
+
+        lines = (line.strip(" \n\t") for line in stream)
+        text = '\n'.join(line for line in lines if line)
+
+        matches = prog.findall(text)
+        requires = {key: value.splitlines()
+                    for key, value in matches}
+
+    return requires
+
+
+def main():
+    if sys.version_info < (3, 5):
+        raise RuntimeError("Peony requires Python 3.5+")
+
+    dirname = os.path.dirname(inspect.getfile(inspect.currentframe()))
+
+    # get metadata and keywords from peony/__init__.py
+    metadata = get_metadata(os.path.join(dirname, 'peony', '__init__.py'))
+
+    # get requirements from requirements.txt
+    requires = get_requirements(os.path.join(dirname, 'requirements.txt'))
+
+    # get long description from README.md
+    with open('README.md') as stream:
+        long_description = stream.read()
+
+    setup(
+        long_description=long_description,
+        packages=find_packages(include=["peony*"]),
+        **metadata,
+        **requires
+    )
+
+
+if __name__ == '__main__':
+    main()
