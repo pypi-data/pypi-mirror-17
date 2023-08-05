8f2d25316ca020eb29113f7ce9b8b9ba2d19d919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:32 +0000

    Add package dingtalk/0.0.2/dingtalk-0.0.2.zip

---
+++ packages/dingtalk/dingtalk-0.0.2.zip/dingtalk-0.0.2/dingtalk/dingtalk.py
@@ -0,0 +1,15 @@
+import urllib2
+import urllib
+import json
+
+
+def dingtalk_bot(token,post_string):
+    data = {
+        'msgtype': 'text', 'text': {'content': '%s' % post_string}
+    }
+    json_str = json.dumps(data)
+    req = urllib2.Request(
+        "https://oapi.dingtalk.com/robot/send?access_token=%s" % token, json_str)
+    req.add_header('Content-type', 'application/json')
+    response = urllib2.urlopen(req, timeout=120)
+    return response.read()
