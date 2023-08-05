8d9b37812fcebafb50800fc2f8bf43472ed278d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:47 +0000

    Add package onegov.election-day/0.9.0/onegov.election_day-0.9.0-py3-none-any.whl

---
+++ packages/onegov.election-day/onegov.election_day-0.9.0-py3-none-any.whl/onegov/election_day/forms/__init__.py
@@ -0,0 +1,15 @@
+from onegov.election_day.forms.delete import DeleteForm
+from onegov.election_day.forms.election import ElectionForm
+from onegov.election_day.forms.upload import (
+    UploadElectionForm,
+    UploadVoteForm
+)
+from onegov.election_day.forms.vote import VoteForm
+
+__all__ = [
+    'DeleteForm',
+    'ElectionForm',
+    'UploadElectionForm',
+    'UploadVoteForm',
+    'VoteForm'
+]
