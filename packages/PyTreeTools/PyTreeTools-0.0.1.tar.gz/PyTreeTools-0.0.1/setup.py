0332f498620eb796e766b7499e9e143935703dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:28 +0000

    Add package PyTreeTools/0.0.1/PyTreeTools-0.0.1.tar.gz

---
+++ packages/PyTreeTools/PyTreeTools-0.0.1.tar.gz/PyTreeTools-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="PyTreeTools",
+    version="0.0.1",
+    author="Steve Oliveira",
+    author_email="steve@overlee.com",
+    packages=["pytreetools", "pytreetools.test"],
+    url="http://pypi.python.org/pypi/PyTreeTools",
+    license="LICENSE.txt",
+    long_description=open("README.md").read(),
+)
