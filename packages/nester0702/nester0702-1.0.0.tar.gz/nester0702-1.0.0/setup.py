0ab6f985563e1b90034efce355b7ab2f5140f8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:25 +0000

    Add package nester0702/1.0.0/nester0702-1.0.0.tar.gz

---
+++ packages/nester0702/nester0702-1.0.0.tar.gz/nester0702-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+name='nester0702',
+version='1.0.0',
+py_modules=['nester'],
+author='harry',
+author_email="690096673@qq.com",
+url='http://www.headfirstlabs.com',
+description='for fun',
+
+	
+	)
