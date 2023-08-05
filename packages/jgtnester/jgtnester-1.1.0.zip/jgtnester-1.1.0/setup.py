94cb4bce007900cf16e90e19c07b3b5d0e832564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:11 +0000

    Add package jgtnester/1.1.0/jgtnester-1.1.0.zip

---
+++ packages/jgtnester/jgtnester-1.1.0.zip/jgtnester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+name = 'jgtnester',
+version = '1.1.0',
+py_modules = ['jgtnester'],
+author = 'jgtnav',
+author_email = '107115058@nitt.edu',
+url = 'http://www.nitt.edu',
+description = 'a printer of nested lists',
+)
+
