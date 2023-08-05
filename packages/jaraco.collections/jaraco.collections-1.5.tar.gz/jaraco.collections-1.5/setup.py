3456239f323965e7b22ef3053c4564ace5281546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:11 +0000

    Add package jaraco.collections/1.5/jaraco.collections-1.5.tar.gz

---
+++ packages/jaraco.collections/jaraco.collections-1.5.tar.gz/jaraco.collections-1.5/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+# Project skeleton maintained at https://github.com/jaraco/skeleton
+
+import io
+import sys
+
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+	long_description = readme.read()
+
+needs_pytest = {'pytest', 'test'}.intersection(sys.argv)
+pytest_runner = ['pytest_runner'] if needs_pytest else []
+needs_sphinx = {'release', 'build_sphinx', 'upload_docs'}.intersection(sys.argv)
+sphinx = ['sphinx', 'rst.linker'] if needs_sphinx else []
+needs_wheel = {'release', 'bdist_wheel'}.intersection(sys.argv)
+wheel = ['wheel'] if needs_wheel else []
+
+tests_require = [
+	'pytest>=2.8',
+]
+
+setup_requires = [
+	'setuptools_scm>=1.9',
+] + pytest_runner + sphinx + wheel
+
+name = 'jaraco.collections'
+description = ''
+
+setup_params = dict(
+	name=name,
+	use_scm_version=True,
+	author="Jason R. Coombs",
+	author_email="jaraco@jaraco.com",
+	description=description or name,
+	long_description=long_description,
+	url="https://github.com/jaraco/" + name,
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+	namespace_packages=name.split('.')[:-1],
+	install_requires=[
+		'jaraco.text',
+		'jaraco.classes',
+		'six>=1.7.0',
+	],
+	extras_require={
+		'tests': tests_require,
+		'setup': setup_requires,
+	},
+	setup_requires=setup_requires,
+	tests_require=tests_require,
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+	],
+	entry_points={
+	},
+)
+if __name__ == '__main__':
+	setuptools.setup(**setup_params)
