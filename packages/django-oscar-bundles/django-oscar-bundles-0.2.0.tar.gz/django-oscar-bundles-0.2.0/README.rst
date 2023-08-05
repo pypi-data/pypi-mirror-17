d4c81f07ca918a22ac0f94ae49a74a6b2788d1ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:53 +0000

    Add package django-oscar-bundles/0.2.0/django-oscar-bundles-0.2.0.tar.gz

---
+++ packages/django-oscar-bundles/django-oscar-bundles-0.2.0.tar.gz/django-oscar-bundles-0.2.0/README.rst
@@ -0,0 +1,18 @@
+=========================
+django-oscar-bundles
+=========================
+
+An extension on top of django-oscar providing product bundle functionality.
+
+
+Changelog
+=========
+
+0.2.0
+------------------
+- Add optional bundle groupings
+
+0.1.0
+------------------
+- Initial release.
+- Renamed package to `oscarbundles` for consistency.
---
+++ packages/django-oscar-bundles/django-oscar-bundles-0.2.0.tar.gz/django-oscar-bundles-0.2.0/oscarbundles/dashboard/forms.py
@@ -0,0 +1,54 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from oscar.core.loading import get_model, get_class
+
+Category = get_model('catalogue', 'Category')
+Product = get_model('catalogue', 'Product')
+Bundle = get_model('oscarbundles', 'Bundle')
+BundleGroup = get_model('oscarbundles', 'BundleGroup')
+
+GroupedModelMultipleChoiceField = get_class('oscarbundles.dashboard.fields', 'GroupedModelMultipleChoiceField')
+
+
+class CategoryFilterForm(forms.Form):
+    category = forms.ModelChoiceField(
+        label=_('Product Category'),
+        help_text=_('Select a category to filter the products below.'),
+        queryset=Category.objects,
+        required=False)
+
+
+class MetadataForm(forms.ModelForm):
+    class Meta:
+        model = Bundle
+        fields = ('name', 'description', 'image', 'bundle_group')
+
+
+class TriggeringProductsForm(forms.ModelForm):
+    triggering_products = GroupedModelMultipleChoiceField(
+        group_by='parent',
+        label=_('Triggering Products'),
+        help_text=_('What products should trigger this bundle?'),
+        queryset=Product.objects)
+
+    class Meta:
+        model = Bundle
+        fields = ('triggering_products', )
+
+
+class SuggestedProductsForm(forms.ModelForm):
+    suggested_products = GroupedModelMultipleChoiceField(
+        group_by='parent',
+        label=_('Suggested Products'),
+        help_text=_('What products should be suggested by this bundle?'),
+        queryset=Product.objects)
+
+    class Meta:
+        model = Bundle
+        fields = ('suggested_products', )
+
+
+class BundleGroupMetadataForm(forms.ModelForm):
+    class Meta:
+        model = BundleGroup
+        fields = ('name', 'description', 'image')
