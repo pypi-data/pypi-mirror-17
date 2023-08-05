3892b521b4abfa8707536a979c4fb81b2c563bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:59 +0000

    Add package newspider/0.9.3/newspider-0.9.3.tar.gz

---
+++ packages/newspider/newspider-0.9.3.tar.gz/newspider-0.9.3/newspider/comm/utils.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+class Utils:
+
+    @classmethod
+    def tag_of_object(cls, obj):
+        tag = str(obj.__class__).split('.')[-1]
+        return tag
+
+    @classmethod
+    def md5(cls,str):
+        import md5
+        m1 = md5.new()
+        m1.update(str)
+        return m1.hexdigest()
+
+    @classmethod
+    def get_attr_in_class(cls,className, attr,default):
+        try:
+            res = eval("%s.%s" %(className, attr))
+            return res
+        except Exception,e:
+            return default
+
+    @classmethod
+    def conv2utf8(cls,content,from_encodeing):
+        return content.decode(from_encodeing).encode('utf-8')
