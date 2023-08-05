47c086b8bd94f346cd68294143b961885288be4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:33 +0000

    Add package jsonipy/0.1/jsonipy-0.1.zip

---
+++ packages/jsonipy/jsonipy-0.1.zip/jsonipy-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='jsonipy',
+      version='0.1',
+      description='converts your objects into JSON ',
+      url='http://github.com/rahulrd/jsonipy',
+      keywords='JSON object convert serialize',
+      author='Rahul Dwivedi',
+      author_email='rahulrd005@gmail.com',
+      license='MIT',
+      packages=['jsonipy','tests'],
+      )
