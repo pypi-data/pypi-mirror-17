27ff6e48cb0c86eeb358b83c9b87dbbd239df4f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:20 +0000

    Add package SimpleSQLite/0.4.8/SimpleSQLite-0.4.8.tar.gz

---
+++ packages/SimpleSQLite/SimpleSQLite-0.4.8.tar.gz/SimpleSQLite-0.4.8/simplesqlite/loader/json/core.py
@@ -0,0 +1,141 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+
+from __future__ import absolute_import
+import json
+
+import path
+
+from ..interface import TableLoader
+from .formatter import JsonTableFormatter
+
+
+class JsonTableFileLoader(TableLoader):
+    """
+    Concrete class of JSON file loader.
+    """
+
+    def __init__(self, file_path=None):
+        super(JsonTableFileLoader, self).__init__(file_path)
+        self.table_name = "%(default)s"
+
+    def make_table_name(self):
+        self._validate()
+
+        return self.table_name.replace(
+            "%(filename)s", path.Path(self.source).namebase)
+
+    def load(self):
+        """
+        Load a JSON file from :py:attr:`.source` that includes table data.
+        This method can be loading two types of JSON formats:
+        **(1)** single table data in a file,
+        acceptable JSON schema is as follows:
+
+        .. code-block:: json
+
+            {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "additionalProperties": {
+                        "anyOf": [
+                            {"type": "string"},
+                            {"type": "number"},
+                            {"type": "null"},
+                        ],
+                    },
+                },
+            }
+
+        :Examples:
+
+            :ref:`example-convert-single-json-table`
+
+        **(2)** multiple table data in a file,
+        acceptable JSON schema is as follows:
+
+        .. code-block:: json
+
+            {
+                "type": "object",
+                "additionalProperties": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "additionalProperties": {
+                            "anyOf": [
+                                {"type": "string"},
+                                {"type": "number"},
+                                {"type": "null"}
+                            ]
+                        }
+                    }
+                }
+            }
+
+        :Examples:
+
+            :ref:`example-convert-multi-json-table`
+
+        The table name string is making from
+        :py:attr:`~simplesqlite.loader.interface.TableLoader.table_name`.
+        Following format specifiers are replaced with specific string.
+
+            +----------------+------------------------------------------------+
+            |format specifier|value after the replacement                     |
+            +================+================================================+
+            |``%(filename)s``|Filename. Defaults to single JSON table.        |
+            +----------------+------------------------------------------------+
+            |``%(key)s``     |Key of the table data                           |
+            |                |(only for multiple JSON table).                 |
+            |                |Defaults to multiple JSON table.                |
+            +----------------+------------------------------------------------+
+
+        :return: Loaded table data.
+        :rtype: iterator of |TableData|
+        :raises simplesqlite.loader.InvalidDataError:
+            If the data is invalid JSON.
+        :raises simplesqlite.loader.ValidationError:
+            If the data is not acceptable JSON format.
+        """
+
+        self._validate()
+
+        with open(self.source, "r") as fp:
+            json_buffer = json.load(fp)
+
+        formatter = JsonTableFormatter(json_buffer)
+        formatter.accept(self)
+
+        return formatter.to_table_data()
+
+
+class JsonTableTextLoader(TableLoader):
+
+    def __init__(self, text):
+        super(JsonTableTextLoader, self).__init__(text)
+        self.table_name = "%(default)s"
+
+    def load(self):
+        """
+        Load a JSON text from :py:attr:`.source` that includes table data.
+
+        :return: Loaded table data.
+        :rtype: iterator of |TableData|
+
+        .. seealso:: :py:meth:`.JsonTableFileLoader.load`
+        """
+
+        self._validate()
+
+        json_buffer = json.loads(self.source)
+
+        formatter = JsonTableFormatter(json_buffer)
+        formatter.accept(self)
+
+        return formatter.to_table_data()
---
+++ packages/SimpleSQLite/SimpleSQLite-0.4.8.tar.gz/SimpleSQLite-0.4.8/simplesqlite/loader/spreadsheet/gsloader.py
@@ -0,0 +1,171 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+
+from __future__ import absolute_import
+
+import dataproperty
+from six.moves import zip
+
+from ..error import InvalidDataError
+from ..error import OpenError
+from ..data import TableData
+from .core import SpreadSheetLoader
+from ..._func import connect_sqlite_db_mem
+
+
+class GoogleSheetsTableLoader(SpreadSheetLoader):
+    """
+    Concrete class of Google Spreadsheet loader.
+
+    Requirements:
+
+        - `gspread <https://github.com/burnash/gspread>`_
+        - `oauth2client <https://pypi.python.org/pypi/oauth2client>`_
+        - `pyOpenSSL <https://pypi.python.org/pypi/pyOpenSSL>`_
+    """
+
+    @property
+    def _sheet_name(self):
+        return self._worksheet.title
+
+    @property
+    def _row_count(self):
+        return self._worksheet.row_count
+
+    @property
+    def _col_count(self):
+        return self._worksheet.col_count
+
+    def __init__(self, file_path=None):
+        super(GoogleSheetsTableLoader, self).__init__(file_path)
+
+        self.title = None
+        self.start_row = 0
+
+        self.__all_values = None
+
+    def make_table_name(self):
+        """
+        |make_table_name|
+
+            ================  ===========================
+            format specifier  value after the replacement
+            ================  ===========================
+            ``%(filename)s``  Filename of the workbook
+            ``%(title)s``     Name of the spreadsheet
+            ================  ===========================
+
+        :return: Table name.
+        :rtype: str
+        """
+
+        self._validate_title()
+        table_name = super(
+            GoogleSheetsTableLoader, self).make_table_name()
+
+        return table_name.replace("%(title)s", self.title)
+
+    def load(self):
+        """
+        Load table data from a Google Spreadsheet.
+        |load_desc|
+
+        :return:
+            |load_return|
+            :py:meth:`~.GoogleSheetsTableLoader.make_table_name`.
+        :rtype: iterator of |TableData|
+        :raises simplesqlite.loader.InvalidDataError:
+            If the header row is not found.
+        :raises simplesqlite.loader.OpenError:
+            If the spread sheet not found.
+        """
+
+        import gspread
+        from oauth2client.service_account import ServiceAccountCredentials
+
+        self._validate_table_name()
+        self._validate_title()
+
+        scope = ['https://spreadsheets.google.com/feeds']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            self.source, scope)
+
+        gc = gspread.authorize(credentials)
+        try:
+            for worksheet in gc.open(self.title).worksheets():
+                self._worksheet = worksheet
+                self.__all_values = [row for row in worksheet.get_all_values()]
+
+                if self._is_empty_sheet():
+                    continue
+
+                try:
+                    self.__strip_empty_col()
+                except ValueError:
+                    continue
+
+                value_matrix = self.__all_values[self._get_start_row_idx():]
+                try:
+                    header_list = value_matrix[0]
+                    record_list = value_matrix[1:]
+                except IndexError:
+                    continue
+
+                yield TableData(
+                    self.make_table_name(), header_list, record_list)
+        except gspread.exceptions.SpreadsheetNotFound:
+            raise OpenError("spreadsheet '{}' not found".format(self.title))
+
+    def _is_empty_sheet(self):
+        return len(self.__all_values) <= 1
+
+    def _get_start_row_idx(self):
+        row_idx = 0
+        for row_value_list in self.__all_values:
+            if all([
+                dataproperty.is_not_empty_string(value)
+                for value in row_value_list
+            ]):
+                break
+
+            row_idx += 1
+
+        return self.start_row + row_idx
+
+    def _validate_title(self):
+        if dataproperty.is_empty_string(self.title):
+            raise ValueError("spreadsheet title is empty")
+
+    def __strip_empty_col(self):
+        from ...sqlquery import SqlQuery
+
+        con = connect_sqlite_db_mem()
+
+        tmp_table_name = "tmp"
+        header_list = [
+            "a{:d}".format(i)
+            for i in range(len(self.__all_values[0]))
+        ]
+        con.create_table_with_data(
+            tmp_table_name, header_list, self.__all_values)
+        for col_idx, header in enumerate(header_list):
+            result = con.select(
+                select=SqlQuery.to_attr_str(header), table_name=tmp_table_name)
+            if any([
+                dataproperty.is_not_empty_string(record[0])
+                for record in result.fetchall()
+            ]):
+                break
+
+        strip_header_list = header_list[col_idx:]
+        if dataproperty.is_empty_sequence(strip_header_list):
+            raise ValueError()
+
+        result = con.select(
+            select=",".join(SqlQuery.to_attr_str_list(strip_header_list)),
+            table_name=tmp_table_name)
+        self.__all_values = result.fetchall()
