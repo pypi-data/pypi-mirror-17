81f12ee467cd17a1f5c6cb79b6145f8694c5ff4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:43 +0000

    Add package lutin/1.2.6/lutin-1.2.6.tar.gz

---
+++ packages/lutin/lutin-1.2.6.tar.gz/lutin-1.2.6/lutin/z_system/lutinSystem_IOs_CoreAudio.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##
+## @author Edouard DUPIN
+##
+## @copyright 2012, Edouard DUPIN, all right reserved
+##
+## @license APACHE v2.0 (see license file)
+##
+
+from lutin import debug
+from lutin import system
+from lutin import tools
+from lutin import env
+import os
+
+class System(system.System):
+	def __init__(self, target):
+		system.System.__init__(self)
+		# create some HELP:
+		self.help="CoreAudio : Ios interface for audio (all time present, just system interface)"
+		self.valid = True
+		# todo : create a searcher of the presence of the library:
+		self.add_export_flag("link", "-framework CoreAudio")
+		self.add_export_flag("link", "-framework AudioToolbox")
+
+
