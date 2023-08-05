9e51ccd6045917bc1698ea1e4a05c9e43371c4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:40 +0000

    Add package nester1001/1.3.0/nester1001-1.3.0.tar.gz

---
+++ packages/nester1001/nester1001-1.3.0.tar.gz/nester1001-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester1001',
+        version = '1.3.0',
+        py_modules = ['nester1001'],
+        author = 'ruby',
+        author_email = '298102113@qq.com',
+        url = 'http://www.baidu.com',
+        description = 'A simple printer of nested lists',
+    )
