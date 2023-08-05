8e688ed07f4e10d15cb1faf1354190cba1ed432b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:37 +0000

    Add package gym_demonstration/0.0.6/gym_demonstration-0.0.6.tar.gz

---
+++ packages/gym_demonstration/gym_demonstration-0.0.6.tar.gz/gym_demonstration-0.0.6/setup.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+from setuptools import setup
+
+setup(name='gym_demonstration',
+      version='0.0.6',
+      install_requires=['gym[atari]>=0.2.6',
+                        'ujson',
+                        'gym-vnc>=0.0.13'],
+)
