ed800aff6892189defcee8d55aae48a077fb5a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:19 +0000

    Add package ct/0.8.6.1/ct-0.8.6.1-py2.7.egg

---
+++ packages/ct/ct-0.8.6.1-py2.7.egg/cantools/__init__.py
@@ -0,0 +1,17 @@
+import util
+import config as cfgmod
+config = cfgmod.config
+
+if config.web.server == "gae":
+	util.init_gae()
+import geo
+from scripts import builder, deploy, init, pubsub, start, index, migrate
+
+ctstart = start.go
+ctdeploy = deploy.run
+ctpubsub = pubsub.get_addr_and_start
+ctinit = init.parse_and_make
+ctindex = index.go
+ctmigrate = migrate.go
+
+__version__ = "0.8.6.1"
