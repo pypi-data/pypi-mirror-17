28fe3fe213e5e69f5ef71534d0c1dbaa45c618c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:41 +0000

    Add package larch/3.3.6/larch-3.3.6-cp35-cp35m-win_amd64.whl

---
+++ packages/larch/larch-3.3.6-cp35-cp35m-win_amd64.whl/larch/built.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# This file automatically created while building Larch. Do not edit manually.
+configuration='DEFAULT'
+time='02:59:19 PM Central Daylight Time'
+date='10 Sep 2016'
+day='Saturday'
+from .version import version
+
+build='%s (%s, %s %s)'%(version,day,date,time)
+from .apsw import apswversion, sqlitelibversion
+from .util import dicta
+versions = dicta({
+'larch':version,
+'apsw':apswversion(),
+'sqlite':sqlitelibversion(),
+})
+
+try:
+	import numpy
+	versions['numpy'] = numpy.version.version
+except:
+	versions['numpy'] = 'failed'
+
+try:
+	import scipy
+	versions['scipy'] = scipy.version.version
+except:
+	versions['scipy'] = 'failed'
+
+try:
+	import pandas
+	versions['pandas'] = pandas.__version__
+except:
+	versions['pandas'] = 'failed'
+
+import sys
+versions['python'] = "{0}.{1}.{2} {3}".format(*(sys.version_info))
+build_config='Larch %s built on %s, %s %s'%(configuration,day,date,time)
