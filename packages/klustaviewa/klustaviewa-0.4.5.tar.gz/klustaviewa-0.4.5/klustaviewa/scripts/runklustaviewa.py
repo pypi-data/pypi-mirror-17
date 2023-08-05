d135158f54925bdc9c271dc538d3950a71cb2f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:47 +0000

    Add package klustaviewa/0.4.5/klustaviewa-0.4.5.tar.gz

---
+++ packages/klustaviewa/klustaviewa-0.4.5.tar.gz/klustaviewa-0.4.5/klustaviewa/scripts/runklustaviewa.py
@@ -0,0 +1,30 @@
+# -----------------------------------------------------------------------------
+# Imports
+# -----------------------------------------------------------------------------
+import sys
+import re
+
+import numpy as np
+
+from qtools import show_window
+from klustaviewa.gui.mainwindow import MainWindow
+
+
+# -----------------------------------------------------------------------------
+# Main function
+# -----------------------------------------------------------------------------
+def main():
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        if len(sys.argv) > 2:
+            shank = int(sys.argv[2])
+        else:
+            shank = None
+    else:
+        filename = None
+    window = show_window(MainWindow, filename=filename, shank=shank)
+    return window
+
+if __name__ == '__main__':
+    main()
+
