3af7b76410e171b609397f292542f3e051af2d65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:42 +0000

    Add package hudsucker/0.1.2/hudsucker-0.1.2-py2.py3-none-any.whl

---
+++ packages/hudsucker/hudsucker-0.1.2-py2.py3-none-any.whl/hudsucker/__main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+import hudsucker
+import sys
+
+
+def main(argv=None):
+    hudsucker.main(argv)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
