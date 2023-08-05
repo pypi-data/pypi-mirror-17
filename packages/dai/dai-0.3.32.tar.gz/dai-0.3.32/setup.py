1a1fa82c54cc5786bdd95147a30ab1203b3b6a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:49 +0000

    Add package dai/0.3.32/dai-0.3.32.tar.gz

---
+++ packages/dai/dai-0.3.32.tar.gz/dai-0.3.32/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='dai',
+      version='0.3.32',
+      description='python worker for distributedAI',
+      url='http://github.com/oeway/distributedAI',
+      author='Wei OUYANG',
+      author_email='wei.ouyang@cri-paris.org',
+      license='MIT',
+      packages=['dai'],
+      install_requires=[
+          'python-meteor',
+      ],
+      zip_safe=False)
