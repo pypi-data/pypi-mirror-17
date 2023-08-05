d3e78b1d2b440dbbd91f0937100e0b0d5f256c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:25 +0000

    Add package scriptler/0.1.0/scriptler-0.1.0.tar.gz

---
+++ packages/scriptler/scriptler-0.1.0.tar.gz/scriptler-0.1.0/scriptler/config.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+.. module:: TODO
+   :platform: Unix
+   :synopsis: TODO.
+
+.. moduleauthor:: Aljosha Friemann aljosha.friemann@gmail.com
+
+----------------------------------------------------------------------------
+"THE BEER-WARE LICENSE" (Revision 42):
+<aljosha.friemann@gmail.com> wrote this file.  As long as you retain this
+notice you can do whatever you want with this stuff. If we meet some day,
+and you think this stuff is worth it, you can buy me a beer in return.
+----------------------------------------------------------------------------
+
+"""
+
+import logging
+
+# F*** configparser and everybody who wrote it..
+
+try:
+    from ConfigParser import ConfigParser
+except ImportError:
+    from configparser import ConfigParser
+
+from simple_model import Model, Attribute, AttributeList
+
+logging.getLogger('simple_model').setLevel(logging.WARNING)
+
+class Source(Model):
+    name   = Attribute(str)
+    url = Attribute(str)
+    user = Attribute(str, optional=True)
+    password = Attribute(str, optional=True)
+    branch = Attribute(str, optional=True)
+
+class Script(Model):
+    name   = Attribute(str)
+    path   = Attribute(str)
+    source = Attribute(Source, optional=True)
+
+class Config(Model):
+    path       = Attribute(str)
+    script_dir = Attribute(str)
+    scripts    = AttributeList(Script, fallback=[])
+
+def parse_config(config_path, defaults = {}):
+    config = ConfigParser()
+    config.read([ config_path ])
+
+    scriptler = defaults
+    scripts = []
+
+    if config.has_section('scripts'):
+        for key in config.options('scripts'):
+            value = config.get('scripts', key)
+            if ':' in value:
+                source_name, path = value.split(':', 1)
+
+                assert config.has_section(source_name), "source %s is not defined" % source_name
+
+                source = Source(name = source_name, **dict(config.items(source_name)))
+            else:
+                path = value
+                source = None
+
+            scripts.append(Script(name = key, path = path, source = source))
+
+    if config.has_section('scriptler'):
+        scriptler.update(config.items('scriptler'))
+
+    del config
+
+    return Config(path = config_path, scripts = scripts, **scriptler)
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
---
+++ packages/scriptler/scriptler-0.1.0.tar.gz/scriptler-0.1.0/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+.. module:: TODO
+   :platform: Unix
+   :synopsis: TODO.
+
+.. moduleauthor:: Aljosha Friemann aljosha.friemann@gmail.com
+
+"""
+
+import os, pip
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname), 'r').read()
+
+install_reqs = pip.req.parse_requirements('requirements.txt', session=pip.download.PipSession())
+
+requirements = [str(ir.req) for ir in install_reqs if ir is not None]
+
+from scriptler import __version__
+
+setup(name             = "scriptler",
+      author           = "Aljosha Friemann",
+      author_email     = "aljosha.friemann@gmail.com",
+      description      = "manage scripts from different sources",
+      license          = read('LICENSE.txt'),
+      long_description = read('README.rst'),
+      url              = "https://www.github.com/afriemann/scriptler",
+      keywords         = ['scripts'],
+      version          = __version__,
+      install_requires = requirements,
+      classifiers      = [],
+      packages         = ["scriptler"],
+      package_data     = { 'scriptler': ['VERSION'] },
+      entry_points     = { 'console_scripts': ['scriptler=scriptler.cli:run'] },
+      platforms        = 'linux'
+)
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
