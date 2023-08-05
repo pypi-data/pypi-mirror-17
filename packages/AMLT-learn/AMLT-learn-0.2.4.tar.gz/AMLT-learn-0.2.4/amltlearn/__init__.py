6929987a8d30404df9864753512774f2ca7b6222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:18 +0000

    Add package AMLT-learn/0.2.4/AMLT-learn-0.2.4.tar.gz

---
+++ packages/AMLT-learn/AMLT-learn-0.2.4.tar.gz/AMLT-learn-0.2.4/amltlearn/__init__.py
@@ -0,0 +1,20 @@
+"""
+.. module:: __init__.py
+
+__init__.py
+*************
+
+:Description: __init__.py
+
+    
+
+:Authors: bejar
+    
+
+:Version: 
+
+:Created on: 01/09/2015 9:53 
+
+"""
+
+__author__ = 'bejar'
