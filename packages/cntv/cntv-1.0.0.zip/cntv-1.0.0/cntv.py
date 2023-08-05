0c99cfe6887259e3166282ff8c2aa603d65498a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:57 +0000

    Add package cntv/1.0.0/cntv-1.0.0.zip

---
+++ packages/cntv/cntv-1.0.0.zip/cntv-1.0.0/cntv.py
@@ -0,0 +1,17 @@
+﻿"""
+这是cntv.py模块，提供了一个名为cntv()的函数，这个
+函数的作用是打印列表，其中有可能 包含（也可能不包含）嵌套列表
+"""
+
+def cntv2(the_list):
+"""
+这个函数取一个位置参数，名为“the_list”，这可以是任何python列表
+（也可以是包含嵌套列表的列表）。所指定的列表中的每个数据项会（递归
+地）输出到屏幕上，各数据项各占一行。
+"""
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			cntv2(each_item)
+		else:
+			print(each_item)
+
---
+++ packages/cntv/cntv-1.0.0.zip/cntv-1.0.0/setup.py
@@ -0,0 +1,11 @@
+﻿from distutils.core import setup
+
+setup(
+		name = 'cntv',
+		version = '1.0.0',
+		py_modules = ['cntv'],
+		author = 'hfpython',
+		author_email = 'vip@vip.com',
+		url = 'http://www.python.com',
+		description = 'test',
+	)
