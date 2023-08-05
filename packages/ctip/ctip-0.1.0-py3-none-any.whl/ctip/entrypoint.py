71b00cc9c881f92a01633ca2e57bba30e9576643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:23 +0000

    Add package ctip/0.1.0/ctip-0.1.0-py3-none-any.whl

---
+++ packages/ctip/ctip-0.1.0-py3-none-any.whl/ctip/entrypoint.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Provides entry point main(argv) when running from the command line.
+
+Created on Sat Jul  9 12:45:39 2016
+
+@author: Aaron Beckett
+"""
+
+
+__version__ = "0.1.0"
+
+
+def main(argv):
+    print(argv)
