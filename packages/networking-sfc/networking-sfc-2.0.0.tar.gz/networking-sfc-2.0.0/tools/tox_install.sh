571ffd7d64477d691991a7b770ca9529329c4a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:01 +0000

    Add package networking-sfc/2.0.0/networking-sfc-2.0.0.tar.gz

---
+++ packages/networking-sfc/networking-sfc-2.0.0.tar.gz/networking-sfc-2.0.0/tools/tox_install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Many of neutron's repos suffer from the problem of depending on neutron,
+# but it not existing on pypi.
+
+# This wrapper for tox's package installer will use the existing package
+# if it exists, else use zuul-cloner if that program exists, else grab it
+# from neutron master via a hard-coded URL. That last case should only
+# happen with devs running unit tests locally.
+
+# From the tox.ini config page:
+# install_command=ARGV
+# default:
+# pip install {opts} {packages}
+
+set -e
+
+echo "PIP HARDCODE" > /tmp/tox_install.txt
+pip install -U -egit+https://git.openstack.org/openstack/neutron#egg=neutron
+
+pip install -U $*
+exit $?
