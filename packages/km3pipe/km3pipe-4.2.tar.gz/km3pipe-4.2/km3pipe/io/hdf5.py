884be7d9a7c08de10acafa4ac65a5fffa9d3851b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:29 +0000

    Add package km3pipe/4.2/km3pipe-4.2.tar.gz

---
+++ packages/km3pipe/km3pipe-4.2.tar.gz/km3pipe-4.2/km3pipe/io/hdf5.py
@@ -0,0 +1,289 @@
+# coding=utf-8
+# Filename: hdf5.py
+# pylint: disable=C0103,R0903
+# vim:set ts=4 sts=4 sw=4 et:
+"""
+Pumps for the EVT simulation dataformat.
+
+"""
+from __future__ import division, absolute_import, print_function
+
+from collections import defaultdict
+import os.path
+from six import string_types
+
+import pandas as pd
+import tables as tb
+
+import km3pipe as kp
+from km3pipe import Pump, Module
+from km3pipe.dataclasses import HitSeries, TrackSeries, EventInfo
+from km3pipe.logger import logging
+from km3pipe.tools import camelise, decamelise, insert_prefix_to_dtype
+
+log = logging.getLogger(__name__)  # pylint: disable=C0103
+
+__author__ = "Tamas Gal and Moritz Lotze"
+__copyright__ = "Copyright 2016, Tamas Gal and the KM3NeT collaboration."
+__credits__ = []
+__license__ = "MIT"
+__maintainer__ = "Tamas Gal and Moritz Lotze"
+__email__ = "tgal@km3net.de"
+__status__ = "Development"
+
+
+class HDF5Sink(Module):
+    def __init__(self, **context):
+        """A Module to dump blob data to HDF5.
+
+        Each item in the blob which has a dtype or a `serialise()` method
+        will be dumped in the HDF5 file.
+        The table name is either the key (if the value is a plain value with
+        a dtype) or the `loc` attribute of the object.
+
+        The target table name is a decamelised version of the blob-key,
+        so for example values which are stored in the blob under `FooBar`
+        will be written to `/foo_bar` in the HDF5 file.
+        """
+        super(self.__class__, self).__init__(**context)
+        self.filename = self.get('filename') or 'dump.h5'
+        self.index = 1
+        self.h5file = tb.open_file(self.filename, mode="w", title="KM3NeT")
+        self.filters = tb.Filters(complevel=5, shuffle=True,
+                                  fletcher32=True)
+        self._tables = {}
+
+    def _write_table(self, where, data, title=''):
+        if len(data) <= 0:
+            return
+        try:
+            data = data.to_records()
+        except AttributeError:
+            pass
+        try:
+            data = data.serialise()
+        except AttributeError:
+            pass
+        self._write_array(where, data, title)
+
+    def _write_array(self, where, arr, title=''):
+        if where not in self._tables:
+            dtype = arr.dtype
+            loc, tabname = os.path.split(where)
+            self._tables[where] = self.h5file.create_table(
+                loc, tabname, description=dtype, title=title,
+                filters=self.filters, createparents=True)
+        tab = self._tables[where]
+        tab.append(arr)
+
+    def process(self, blob):
+        for key, entry in blob.items():
+            if hasattr(entry, 'dtype') or hasattr(entry, 'serialise') or \
+                    hasattr(entry, 'to_records'):
+                try:
+                    h5loc = entry.h5loc
+                except AttributeError:
+                    h5loc = ''
+                where = h5loc + '/' + decamelise(key)
+                self._write_table(where, entry, title=key)
+
+        if not self.index % 1000:
+            for tab in self._tables.values():
+                tab.flush()
+
+        self.index += 1
+        return blob
+
+    def finish(self):
+        for tab in self._tables.values():
+            tab.flush()
+            tab.cols.event_id.create_index()
+        self.h5file.root._v_attrs.km3pipe = str(kp.__version__)
+        self.h5file.root._v_attrs.pytables = str(tb.__version__)
+        self.h5file.close()
+
+
+class HDF5Pump(Pump):
+    """Provides a pump for KM3NeT HDF5 files"""
+    def __init__(self, filename, **context):
+        super(self.__class__, self).__init__(**context)
+        self.filename = filename
+        if os.path.isfile(self.filename):
+            self.h5_file = tb.File(self.filename)
+        else:
+            raise IOError("No such file or directory: '{0}'"
+                          .format(self.filename))
+        self.index = None
+        self._reset_index()
+
+        try:
+            event_info = self.h5_file.get_node('/', 'event_info')
+            self.event_ids = event_info.cols.event_id[:]
+        except tb.NoSuchNodeError:
+            log.critical("No /event_info table found.")
+            raise SystemExit
+
+        self._n_events = len(self.event_ids)
+
+    def process(self, blob):
+        try:
+            blob = self.get_blob(self.index)
+        except KeyError:
+            self._reset_index()
+            raise StopIteration
+        self.index += 1
+        return blob
+
+    def _get_event(self, event_id, where):
+        if not where.startswith('/'):
+            where = '/' + where
+        table = self.h5_file.get_node(where)
+        return table.read_where('event_id == %d' % event_id)
+
+    def get_blob(self, index):
+        event_id = self.event_ids[index]
+        blob = {}
+        hits = self._get_event(event_id, where='hits')
+        if len(hits) > 0:
+            blob['Hits'] = HitSeries.from_table(hits, event_id=event_id)
+        mc_hits = self._get_event(event_id, where='mc_hits')
+        if len(mc_hits) > 0:
+            blob['MCHits'] = HitSeries.from_table(mc_hits, event_id=event_id)
+        mc_tracks = self._get_event(event_id, where='mc_tracks')
+        if len(mc_tracks) > 0:
+            blob['MCTracks'] = TrackSeries.from_table(mc_tracks,
+                                                      event_id=event_id)
+        blob['EventInfo'] = EventInfo.from_table(
+            self._get_event(event_id, where='event_info')[0])
+
+        reco_path = '/reco'
+        for tab in self.h5_file.iter_nodes(reco_path, classname='Table'):
+            tabname = tab.name
+            blob[camelise(tabname)] = tab.read_where(
+                'event_id == %d' % event_id)
+        return blob
+
+    def finish(self):
+        """Clean everything up"""
+        self.h5_file.close()
+
+    def _reset_index(self):
+        """Reset index to default value"""
+        self.index = 0
+
+    def __len__(self):
+        return self._n_events
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        """Python 2/3 compatibility for iterators"""
+        return self.__next__()
+
+    def __next__(self):
+        if self.index >= self._n_events:
+            self._reset_index()
+            raise StopIteration
+        blob = self.get_blob(self.index)
+        self.index += 1
+        return blob
+
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            return self.get_blob(index)
+        elif isinstance(index, slice):
+            return self._slice_generator(index)
+        else:
+            raise TypeError("index must be int or slice")
+
+    def _slice_generator(self, index):
+        """A simple slice generator for iterations"""
+        start, stop, step = index.indices(len(self))
+        for i in range(start, stop, step):
+            yield self.get_blob(i)
+
+
+class H5Chain(object):
+    """Read/write Dataframes to multiple H5 files.
+
+    Example
+    -------
+    >>> h5files = ['numu_cc.h5', 'anue_nc.h5']
+    >>> c = H5Chain(h5files)
+
+    # specify n_events per file, or their event ids
+    >>> which = {'numu_cc.h5': None, 'anue_nc.h5': 100,
+                  'numu_cc.h5': [1, 2, 3],}
+    >>> c = H5Chain(which)
+
+    # these are pandas Dataframes
+    >>> X = c.reco
+    >>> wgt = c.event_info.weights_w2
+    >>> Y_ene = c.mc_tracks[0].energy
+
+    store = defaultdict(list)
+    for file, cond in which:
+        for tab in file.walk_nodes('/', classname='Table'):
+            arr = read_table(tab, cond)
+            arr = pd.DataFrame(arr)
+            store[tab.name].append(arr)
+
+    for key, ds in store.items():
+        store[key] = pd.concat(ds)
+
+    store.key -> store[key]
+
+    """
+
+    def __init__(self, which):
+        self._which = which
+        self._store = defaultdict(list)
+
+        for fil, cond in self._which.items():
+            h5fil = tb.open_file(fil, 'r')
+
+            # tables under '/', e.g. mc_tracks
+            for tab in h5fil.iter_nodes('/', classname='Table'):
+                arr = self._read_table(tab, cond)
+                arr = pd.DataFrame.from_records(arr)
+                self._store[tab.name].append(arr)
+
+            # groups under '/', e.g. '/reco'
+            for gr in h5fil.iter_nodes('/', classname='Group'):
+                arr = self._read_group(gr, cond)
+                self._store[gr._v_name].append(arr)
+
+            h5fil.close()
+
+        for key, dfs in self._store.items():
+            self._store[key] = pd.concat(dfs)
+
+    def __getattr__(self, name):
+        try:
+            return self._store[name]
+        except KeyError:
+            raise AttributeError("The table {} does not exist".format(name))
+
+    def __getitem__(self, name):
+        return self._store[name]
+
+    def _read_group(cls, group, cond):
+        # Store through groupname, insert tablename into dtype
+        tabs = []
+        for tab in group._f_iter_nodes(classname='Table'):
+            tabname = tab.name
+            arr = cls._read_table(tab, cond)
+            arr = insert_prefix_to_dtype(arr, tabname)
+            arr = pd.DataFrame.from_records(arr)
+            tabs.append(arr)
+        tabs = pd.concat(tabs, axis=1)
+        return tabs
+
+    @classmethod
+    def _read_table(cls, table, cond=None):
+        if cond is None:
+            return table[:]
+        if isinstance(cond, string_types):
+            return table.read_where(cond)
+        return table.read(cond)
---
+++ packages/km3pipe/km3pipe-4.2.tar.gz/km3pipe-4.2/km3pipe/io/tests/test_h5.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+import os
+
+import numpy as np
+import tables as tb
+
+from km3pipe.io import _read_group
+from km3pipe.io.hdf5 import H5Chain
+from km3pipe.tools import insert_prefix_to_dtype
+from km3pipe.testing import TestCase
+
+
+class TestMultiTable(TestCase):
+    def setUp(self):
+        self.foo = np.array([
+            (1.0, 2.0, 3.0),
+            (4.0, 5.0, 6.0),
+        ], dtype=[('a', '<f8'), ('b', '<f8'), ('c', '<f8'), ])
+        self.bar = np.array([
+            (10.0, 20.0, 30.0),
+            (40.0, 50.0, 60.0),
+        ], dtype=[('aa', '<f8'), ('bb', '<f8'), ('cc', '<f8'), ])
+        self.tabs = {'foo': self.foo, 'bar': self.bar}
+        self.where = '/lala'
+        self.h5name = './test.h5'
+        self.h5file = tb.open_file(
+            # create the file in memory only
+            self.h5name, 'w', driver="H5FD_CORE", driver_core_backing_store=0)
+        for name, tab in self.tabs.items():
+            self.h5file.create_table(self.where, name=name, obj=tab,
+                                     createparents=True)
+
+    def tearDown(self):
+        self.h5file.close()
+
+    def test_name_insert(self):
+        exp_foo = ('foo_a', 'foo_b', 'foo_c')
+        exp_bar = ('bar_aa', 'bar_bb', 'bar_cc')
+        pref_foo = insert_prefix_to_dtype(self.tabs['foo'], 'foo')
+        pref_bar = insert_prefix_to_dtype(self.tabs['bar'], 'bar')
+        self.assertEqual(exp_foo, pref_foo.dtype.names)
+        self.assertEqual(exp_bar, pref_bar.dtype.names)
+
+    def test_group_read(self):
+        tabs = _read_group(self.h5file, self.where)
+        exp_cols = (
+            'bar_aa', 'bar_bb', 'bar_cc',
+            'foo_a', 'foo_b', 'foo_c',
+        )
+        exp_shape = (2, 6)
+        res_shape = tabs.shape
+        res_cols = tuple(tabs.columns)
+        print(exp_cols)
+        print(res_cols)
+        self.assertEqual(exp_shape, res_shape)
+        self.assertEqual(exp_cols, res_cols)
+
+
+class TestH5Chain(TestCase):
+    def setUp(self):
+        self.foo = np.array([
+            (1.0, 2.0, 1.0, 1),
+            (4.0, 5.0, 0.0, 1),
+            (1.2, 2.2, 1.2, 2),
+            (4.2, 5.2, 0.2, 2),
+            (1.5, 2.5, 1.5, 3),
+            (4.5, 5.5, 0.5, 3),
+            (1.5, 2.5, 1.5, 4),
+            (4.5, 5.5, 0.5, 4),
+        ], dtype=[('a', '<f8'), ('b', '<f8'),
+                  ('c', '<f8'), ('event_id', int)])
+        self.bar = np.array([
+            (10.5, 20.5, 30.5, 1),
+            (40.5, 50.5, 60.5, 2),
+            (11.5, 21.5, 31.5, 3),
+            (41.5, 51.5, 61.5, 4),
+        ], dtype=[('aa', '<f8'), ('bb', '<f8'),
+                  ('cc', '<f8'), ('event_id', int)])
+        self.yay = np.array([
+            (10.0, 20.0, 30.0, 1),
+            (40.0, 50.0, 60.0, 2),
+            (12.0, 22.0, 32.0, 3),
+            (42.0, 52.0, 62.0, 4),
+        ], dtype=[('aaa', '<f8'), ('bbb', '<f8'),
+                  ('ccc', '<f8'), ('event_id', int)])
+        self.tabs = {'foo': self.foo, 'bar': self.bar, 'yay': self.yay}
+        self.where = {'foo': '/', 'bar': '/lala', 'yay': '/lala'}
+        self.h5name = './test.h5'
+        self.h5name2 = './test2.h5'
+        self.h5file = tb.open_file(self.h5name, 'a')
+        self.h5file2 = tb.open_file(self.h5name2, 'a')
+        for name, tab in self.tabs.items():
+            self.h5file.create_table(self.where[name], name=name, obj=tab,
+                                     createparents=True)
+            self.h5file2.create_table(self.where[name], name=name, obj=tab,
+                                      createparents=True)
+        self.h5file.close()
+        self.h5file2.close()
+
+    def tearDown(self):
+        os.remove(self.h5name)
+        os.remove(self.h5name2)
+
+    def test_two_files(self):
+        files = {self.h5name: None, self.h5name2: None}
+        c = H5Chain(files)
+        print(c['foo'][::2])
+        print(c['lala'])
+        self.assertTrue(c['foo'].equals(c.foo))
+
+    def test_event_id(self):
+        files = {self.h5name: 'event_id == 1', self.h5name2: 'event_id == 1'}
+        c = H5Chain(files)
+        print(c['foo'][::2])
+        print(c['lala'])
+        self.assertTrue(c['foo'].equals(c.foo))
+
+    # def test_slice(self):
+    #     files = {self.h5name: slice(None, None, 2)}
+    #     c = H5Chain(files)
+    #     self.assertTrue(c['foo'].equals(c.foo))
+    #     files = {self.h5name: slice(1, 3, 2)}
+    #     c = H5Chain(files)
+    #     self.assertTrue(c['foo'].equals(c.foo))
