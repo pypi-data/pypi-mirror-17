74601323730cfa9224ae007c559d3b06bd8b74ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:46 +0000

    Add package mibig-tools/0.1.10/mibig-tools-0.1.10.tar.gz

---
+++ packages/mibig-tools/mibig-tools-0.1.10.tar.gz/mibig-tools-0.1.10/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mibig-tools',
+    version='0.1.10',
+    install_requires=[
+        'Click >= 4.0',
+        'Biopython >=1.6.5',
+        'toolz >= 0.7.2',
+        'pandas >=0.16'
+    ],
+    packages=find_packages(),
+    entry_points='''
+        [console_scripts]
+        mibig_process_folder = mibig_tools.process_all_mibig:process_mibig_cluster_folder
+    ''',
+)
