6f7beea2d9265a02688f8e45afcf1e1553e4b3d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:20 +0000

    Add package weixin-python/0.2.3/weixin-python-0.2.3.tar.gz

---
+++ packages/weixin-python/weixin-python-0.2.3.tar.gz/weixin-python-0.2.3/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='weixin-python',
+    description='Weixin for python',
+    license = 'BSD',
+    packages = find_packages(),
+    version='0.2.3',
+    author='zwczou',
+    author_email='zwczou@gmail.com',
+    url='https://github.com/zwczou/weixin-python',
+    keywords=['weixin', 'weixin pay', 'weixin login'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask',
+    ],
+    classifiers=[],
+)
