6c68f1288468be5b69891075870f905d7b4aee71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:02 +0000

    Add package kryptal/0.1.9/kryptal-0.1.9.tar.gz

---
+++ packages/kryptal/kryptal-0.1.9.tar.gz/kryptal-0.1.9/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup, find_packages
+
+setup(name='kryptal',
+      version='0.1.9',
+      description='',
+      author='Sebastian Messmer',
+      author_email='messmer@cryfs.org',
+      license='GPLv3',
+      url='https://github.com/cryfs/kryptal',
+      packages=find_packages(),
+      package_data={
+          'kryptal.gui': [
+              '*.ui'
+          ],
+          'kryptal.plugins.filesystems': [
+              '*.kryptal-plugin'
+          ],
+          'kryptal.plugins.storageproviders': [
+              '*.kryptal-plugin'
+          ]
+      },
+      entry_points = {
+        'gui_scripts': [
+          'kryptal = kryptal.gui.__main__:main'
+        ]
+      },
+      install_requires=[
+          'PyQT5 >= 5.7',
+          'yapsy >= 1.11.223',
+          'appdirs >= 1.4.0'
+      ],
+      #keywords = ...,
+      classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Environment :: X11 Applications :: Qt",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Communications :: File Sharing",
+        "Topic :: Security",
+        "Topic :: Security :: Cryptography",
+        "Topic :: System :: Filesystems",
+        "Topic :: Utilities"
+      ]
+)
