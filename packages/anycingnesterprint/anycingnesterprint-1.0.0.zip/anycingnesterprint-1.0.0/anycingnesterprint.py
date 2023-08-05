5f8f80b87dc76d4fc7291f34564e79491f893785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:58 +0000

    Add package anycingnesterprint/1.0.0/anycingnesterprint-1.0.0.zip

---
+++ packages/anycingnesterprint/anycingnesterprint-1.0.0.zip/anycingnesterprint-1.0.0/anycingnesterprint.py
@@ -0,0 +1,7 @@
+'''This is "nesterprint"module, provide a function "print_iol", it can print each item of a list(even if it contain a nester list)'''
+def print_iol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_iol(each_item)
+        else:
+            print(each_item)
