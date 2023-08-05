bd16a94352d82f8b6f5da7b063610b1c508b0e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:57 +0000

    Add package exp10it/2.3.20/exp10it-2.3.20.tar.gz

---
+++ packages/exp10it/exp10it-2.3.20.tar.gz/exp10it-2.3.20/1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+#from exp10it import *
+#print get_yanzhengma_from_pic("/root/桌面/1.png")
+from exp10it import *
+#tab_complete_file_path()
+raw_input()
---
+++ packages/exp10it/exp10it-2.3.20.tar.gz/exp10it-2.3.20/2.py
@@ -0,0 +1,8 @@
+from exp10it import *
+crawl_uri("http://www.freebuf.com/")
+#crawl_uri("https://www.qq.com")
+#crawl_uri("https://3xp10it.github.io")
+#crawl_uri("https://www.seebug.org")
+#print get_http_domain_pattern_from_uri("https://www.baiud.com/1./2")
+#crawl_uri("http://sz.tedu.cn")
+#crawl_uri("http://tools.kali.org")
