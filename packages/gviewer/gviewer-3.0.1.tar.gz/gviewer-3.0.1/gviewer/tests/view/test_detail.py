c9f35f79555f0fed635ad44210a43b37a39cc172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:12 +0000

    Add package gviewer/3.0.1/gviewer-3.0.1.tar.gz

---
+++ packages/gviewer/gviewer-3.0.1.tar.gz/gviewer-3.0.1/gviewer/tests/view/test_detail.py
@@ -0,0 +1,182 @@
+import unittest
+import urwid
+import mock
+
+from gviewer.tests.util import render_to_content, render_widgets_to_content
+from gviewer.view.detail import DetailWidget, Tabs
+from gviewer.view.detail import _verify_keys
+from gviewer.view.element import View, Text, Group
+from gviewer.action import Actions
+
+
+class TestDetailWidget(unittest.TestCase):
+    def setUp(self):
+        self.displayer_context = mock.Mock()
+
+        self.displayer_context.displayer.get_views = mock.Mock(return_value=[
+            ("View 1", self._view1),
+            ("View 2", self._view2),
+            ("View 3", self._view3)
+        ])
+
+        self.context = mock.Mock()
+        self.context.config.keys = dict()
+        self.controller = mock.Mock()
+        self.controller.open_view = mock.Mock(side_effect=self._open_view)
+
+        self.test_message = dict(view1="view1", view2="view2", view3="view3")
+
+        self.widget = DetailWidget(
+            self.test_message, self.displayer_context,
+            index=0, controller=self.controller, context=self.context)
+        self.new_widget = None
+
+    def _view1(self, message):
+        return self._display(message["view1"])
+
+    def _view2(self, message):
+        return self._display(message["view2"])
+
+    def _view3(self, message):
+        return self._display(message["view3"])
+
+    def _display(self, message):
+        return View([Group("Title", [Text(message)])])
+
+    def _open_view(self, widget, push_prev):
+        self.new_widget = widget
+
+    def test_render(self):
+        self.assertEqual(
+            render_to_content(self.widget._w.contents["body"][0], (7, 2)),
+            render_widgets_to_content([
+                urwid.AttrMap(urwid.Text("Title"), "view-title"),
+                urwid.AttrMap(urwid.Text("view1"), "view-item")
+            ], (7, 2))
+        )
+
+    def test_next_view(self):
+        self.widget.keypress((0,), "tab")
+        self.assertIsNotNone(self.new_widget)
+        self.assertEqual(self.new_widget.index, 1)
+
+        self.widget = self.new_widget
+        self.widget.keypress((0,), "tab")
+        self.assertIsNot(self.new_widget, self.widget)
+        self.assertEqual(self.new_widget.index, 2)
+
+        self.widget = self.new_widget
+        self.widget.keypress((0,), "tab")
+        self.assertIsNot(self.new_widget, self.widget)
+        self.assertEqual(self.new_widget.index, 0)
+
+    def test_prev_view(self):
+        self.widget.keypress((0,), "shift tab")
+        self.assertIsNotNone(self.new_widget)
+        self.assertEqual(self.new_widget.index, 2)
+
+        self.widget = self.new_widget
+        self.widget.keypress((0,), "shift tab")
+        self.assertIsNot(self.new_widget, self.widget)
+        self.assertEqual(self.new_widget.index, 1)
+
+        self.widget = self.new_widget
+        self.widget.keypress((0,), "shift tab")
+        self.assertIsNot(self.new_widget, self.widget)
+        self.assertEqual(self.new_widget.index, 0)
+
+    def test_no_tab(self):
+        self.displayer_context.displayer.get_views = mock.Mock(return_value=[
+            ("View 1", lambda m: self._display(m["view1"]))
+        ])
+        self.widget = DetailWidget(
+            self.test_message, self.displayer_context,
+            index=0, controller=self.controller, context=self.context)
+
+        with self.assertRaises(KeyError):
+            self.widget._w.contents["header"]
+
+    def test_swtich_tab_when_no_tab(self):
+        self.displayer_context.displayer.get_views = mock.Mock(return_value=[
+            ("View 1", lambda m: self._display(m["view1"]))
+        ])
+        self.widget = DetailWidget(
+            self.test_message, self.displayer_context,
+            index=0, controller=self.controller, context=self.context)
+
+        self.widget.keypress((0,), "tab")
+        self.assertIsNone(self.new_widget)
+
+        self.widget.keypress((0,), "shift tab")
+        self.assertIsNone(self.new_widget)
+
+    def test_open_search(self):
+        self.widget.keypress((0,), "/")
+        self.controller.open_edit.assert_called_with(self.widget.search_widget)
+
+    def test_clear_search(self):
+        self.widget._clear_search()
+        self.controller.close_edit.assert_called_with()
+
+    def test_open_summary(self):
+        self.widget.keypress((0,), "q")
+        self.controller.back.assert_called_with()
+
+    def test_search(self):
+        self.widget.search_widget.keypress((4,), "v")
+        self.widget.search_widget.keypress((4,), "i")
+        self.widget.search_widget.keypress((4,), "e")
+        self.widget.search_widget.keypress((4,), "w")
+        self.widget.search_widget.keypress((4,), "enter")
+
+        match = render_widgets_to_content(
+            [urwid.Text(("view-title", "Title")),
+             urwid.Text([("match", "view"), ("view-item", "1")])],
+            (5, 3)
+        )
+        no_match = render_widgets_to_content(
+            [urwid.Text(("view-title", "Title")),
+             urwid.Text(("view-item", "view1"))],
+            (5, 3)
+        )
+
+        self.assertEqual(
+            render_to_content(self.widget.content_widget, (5, 3)),
+            match)
+
+        self.widget.keypress((0,), "n")
+        self.assertEqual(
+            render_to_content(self.widget.content_widget, (5, 3)),
+            no_match)
+
+        self.widget.keypress((0,), "N")
+        self.assertEqual(
+            render_to_content(self.widget.content_widget, (5, 3)),
+            match)
+
+        self.widget.keypress((0,), "N")
+        self.assertEqual(
+            render_to_content(self.widget.content_widget, (5, 3)),
+            no_match)
+
+
+class TestTabs(unittest.TestCase):
+    def test_render(self):
+        widget = Tabs(["view1", "view2", "view3"], 0)
+        self.assertEqual(
+            render_to_content(widget, (15,)),
+            render_widgets_to_content([
+                urwid.AttrMap(urwid.Text("view1"), "tabs focus"),
+                urwid.AttrMap(urwid.Text("view2"), "tabs"),
+                urwid.AttrMap(urwid.Text("view3"), "tabs")
+            ], (15,), inline=True)
+        )
+
+
+class TestDetail(unittest.TestCase):
+    def test_verify_keys(self):
+        _verify_keys(Actions([("p", "pppp", None)]))
+
+    def test_verify_keys_failed(self):
+        with self.assertRaises(ValueError):
+            _verify_keys(Actions([("/", "search", None)]))
---
+++ packages/gviewer/gviewer-3.0.1.tar.gz/gviewer-3.0.1/gviewer/tests/view/test_element.py
@@ -0,0 +1,331 @@
+import unittest
+import urwid
+import mock
+
+from ..util import render_to_content, render_widgets_to_content
+from gviewer.basic_widget import SearchableText
+from gviewer.view.element import (
+    Text, Prop, Group, PropsGroup, View,
+    TitleWidget, ContentWidget, EmptyLine)
+
+
+class TestText(unittest.TestCase):
+    def test_widget(self):
+        widget = Text("content").widget(None)
+        self.assertTrue(isinstance(widget, SearchableText))
+        self.assertEqual(
+            widget.get_plain_text(),
+            u"content")
+
+    def test_to_text(self):
+        self.assertEqual(
+            str(Text(b"content")), "content")
+        self.assertEqual(
+            str(Text(u"content")), "content")
+
+    def test_markup_to_text(self):
+        self.assertEqual(
+            str(Text([("aaa", "bbb"), "ccc"])), "bbbccc")
+
+
+class TestProp(unittest.TestCase):
+    def test_widget(self):
+        widget = Prop("key", "value").widget(None)
+        self.assertIsInstance(widget, SearchableText)
+        self.assertEqual(
+            widget.text,
+            [('view-item key', u'key: '),
+             ('view-item value', u'value')])
+
+    def test_widget_with_padding(self):
+        prop = Prop("key", "value")
+        prop.max_key_length = 5
+        self.assertEqual(
+            prop.widget(None).text,
+            [('view-item key', u'key   : '),
+             ('view-item value', u'value')])
+
+    def test_to_text(self):
+        self.assertEqual(
+            str(Prop("key", "value")),
+            "key: value")
+
+    def test_to_text_with_padding(self):
+        prop = Prop("key", "value")
+        prop.max_key_length = 5
+        self.assertEqual(
+            str(prop),
+            "key   : value")
+
+
+class TestGroup(unittest.TestCase):
+    def test_widget_with_title(self):
+        widgets = Group(
+            "title",
+            items=[
+                Text("first line"),
+                Text("second line")]
+        ).widgets(None)
+
+        self.assertEqual(len(widgets), 3)
+        self.assertTrue(isinstance(widgets[0], TitleWidget))
+        self.assertTrue(isinstance(widgets[1], SearchableText))
+        self.assertTrue(isinstance(widgets[2], SearchableText))
+
+    def test_widget_without_title(self):
+        widgets = Group(
+            "title",
+            items=[
+                Text("first line"),
+                Text("second line")],
+            show_title=False).widgets(None)
+
+        self.assertEqual(len(widgets), 2)
+        self.assertTrue(isinstance(widgets[0], SearchableText))
+        self.assertTrue(isinstance(widgets[1], SearchableText))
+
+    def test_to_text(self):
+        self.assertEqual(
+            str(Group("title", items=[Text("first line"), Text("second line")],
+                show_title=True)),
+            u"title\nfirst line\nsecond line")
+
+        self.assertEqual(
+            str(Group("title", items=[Text("first line"), Text("second line")],
+                show_title=False)),
+            u"first line\nsecond line")
+
+
+class TestPropsGroup(unittest.TestCase):
+    def test_calc_key_length(self):
+        props_group = PropsGroup(
+            "title",
+            items=[
+                Prop("key", "value"),
+                Prop("longkey", "value"),
+                Prop("k", "value")]
+        )
+        self.assertEqual(props_group.items[0].max_key_length, 7)
+        self.assertEqual(props_group.items[1].max_key_length, 7)
+        self.assertEqual(props_group.items[2].max_key_length, 7)
+
+
+class TestView(unittest.TestCase):
+    def test_widget(self):
+        view = View([
+            Group("group1", [Text("content1")]),
+            Group("group2", [Text("content2")])]
+        )
+        widget = view.widget(None)
+        self.assertTrue(isinstance(widget, ContentWidget))
+
+        contents = widget._w.body
+        self.assertEqual(len(contents), 6)
+        self.assertIsInstance(contents[0], TitleWidget)
+        self.assertIsInstance(contents[1], SearchableText)
+        self.assertIsInstance(contents[2], EmptyLine)
+        self.assertIsInstance(contents[3], TitleWidget)
+        self.assertIsInstance(contents[4], SearchableText)
+        self.assertIsInstance(contents[5], EmptyLine)
+
+    def test_empty_widget(self):
+        view = View([])
+        contents = view.widget(None)._w.body
+        self.assertEqual(len(contents), 1)
+        self.assertIsInstance(contents[0], EmptyLine)
+
+    def test_actions(self):
+        action = mock.Mock()
+        controller = mock.Mock()
+        context = mock.Mock()
+        context.config.keys = dict()
+
+        view = View([
+            Group("group1", [Text("content1")]),
+            Group("group2", [Text("content2")])],
+            actions=dict(a=action)
+        )
+        widget = view.widget("message", controller=controller, context=context)
+        widget.keypress((0, 0), "a")
+        action.assert_called_with(controller, "message")
+
+        self.assertEqual(
+            widget.keypress((0, 0), "b"),
+            "b")
+
+    def test_to_text(self):
+        view = View([
+            Group("group1", [Text("content1")]),
+            Group("group2", [Text("content2")])]
+        )
+        self.assertEqual(
+            str(view),
+            u"group1\ncontent1\n\ngroup2\ncontent2\n")
+
+
+class TestTitleWidget(unittest.TestCase):
+    def test_render(self):
+        widget = TitleWidget("content")
+        contents = [c for c in widget.render((7,)).content()]
+        self.assertEqual(len(contents), 1)
+        self.assertEqual(len(contents[0]), 1)
+        self.assertEqual(contents[0][0], ("view-title", None, "content"))
+
+
+class TestContentWidget(unittest.TestCase):
+    def test_search(self):
+        widget = ContentWidget([
+            SearchableText("this is aaa bbb"),
+            SearchableText("this is ccc ddd"),
+            SearchableText("this is eee aaa")
+        ], None)
+
+        no_match = render_widgets_to_content([
+            urwid.Text("this is aaa bbb"),
+            urwid.Text("this is ccc ddd"),
+            urwid.Text("this is eee aaa")],
+            (15, 3)
+        )
+        first_match = render_widgets_to_content([
+            urwid.Text(["this is ", ("match", "aaa"), " bbb"]),
+            urwid.Text("this is ccc ddd"),
+            urwid.Text("this is eee aaa")],
+            (15, 3)
+        )
+        second_match = render_widgets_to_content([
+            urwid.Text("this is aaa bbb"),
+            urwid.Text("this is ccc ddd"),
+            urwid.Text(["this is eee ", ("match", "aaa")])],
+            (15, 3)
+        )
+
+        self.assertEqual(
+            render_to_content(widget, (15, 3)),
+            no_match
+        )
+
+        widget.search_next("aaa")
+        self.assertEqual(
+            render_to_content(widget, (15, 3)),
+            first_match
+        )
+
+        widget.search_next("aaa")
+        self.assertEqual(
+            render_to_content(widget, (15, 3)),
+            second_match
+        )
+
+        widget.search_next("aaa")
+        self.assertEqual(
+            render_to_content(widget, (15, 3)),
+            no_match
+        )
+
+        widget.search_prev("aaa")
+        self.assertEqual(
+            render_to_content(widget, (15, 3)),
+            second_match
+        )
+
+        widget.search_prev("aaa")
+        self.assertEqual(
+            render_to_content(widget, (15, 3)),
+            first_match
+        )
+
+        widget.search_prev("aaa")
+        self.assertEqual(
+            render_to_content(widget, (15, 3)),
+            no_match
+        )
+
+    def test_search_next_before_move(self):
+        widget = ContentWidget([
+            SearchableText("this is aaa bbb"),
+            SearchableText("this is ccc ddd"),
+            SearchableText("this is eee aaa")
+        ], None)
+
+        no_match = render_widgets_to_content([
+            urwid.Text("this is aaa bbb"),
+            urwid.Text("this is ccc ddd"),
+            urwid.Text("this is eee aaa")],
+            (15, 3)
+        )
+        first_match = render_widgets_to_content([
+            urwid.Text(["this is ", ("match", "aaa"), " bbb"]),
+            urwid.Text("this is ccc ddd"),
+            urwid.Text("this is eee aaa")],
+            (15, 3)
+        )
+        second_match = render_widgets_to_content([
+            urwid.Text("this is aaa bbb"),
+            urwid.Text("this is ccc ddd"),
+            urwid.Text(["this is eee ", ("match", "aaa")])],
+            (15, 3)
+        )
+
+        self.assertEqual(
+            render_to_content(widget, (15, 3)),
+            no_match
+        )
+
+        widget.search_next("aaa")
+        self.assertEqual(
+            render_to_content(widget, (15, 3)),
+            first_match
+        )
+
+        widget._w.focus_position = 2
+
+        widget.search_next("aaa")
+        self.assertEqual(
+            render_to_content(widget, (15, 3)),
+            second_match
+        )
+
+    def test_search_prev_before_move(self):
+        widget = ContentWidget([
+            SearchableText("this is aaa bbb"),
+            SearchableText("this is ccc ddd"),
+            SearchableText("this is eee aaa")
+        ], None)
+
+        no_match = render_widgets_to_content([
+            urwid.Text("this is aaa bbb"),
+            urwid.Text("this is ccc ddd"),
+            urwid.Text("this is eee aaa")],
+            (15, 3)
+        )
+        first_match = render_widgets_to_content([
+            urwid.Text(["this is ", ("match", "aaa"), " bbb"]),
+            urwid.Text("this is ccc ddd"),
+            urwid.Text("this is eee aaa")],
+            (15, 3)
+        )
+        second_match = render_widgets_to_content([
+            urwid.Text("this is aaa bbb"),
+            urwid.Text("this is ccc ddd"),
+            urwid.Text(["this is eee ", ("match", "aaa")])],
+            (15, 3)
+        )
+
+        self.assertEqual(
+            render_to_content(widget, (15, 3)),
+            no_match
+        )
+
+        widget.search_next("aaa")
+        self.assertEqual(
+            render_to_content(widget, (15, 3)),
+            first_match
+        )
+
+        widget._w.focus_position = 2
+
+        widget.search_prev("aaa")
+        self.assertEqual(
+            render_to_content(widget, (15, 3)),
+            second_match
+        )
