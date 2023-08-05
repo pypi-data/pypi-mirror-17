3408c390def4796ebdeb0d7c32b26c26e1ca8ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:22 +0000

    Add package wechatkit/0.0.2/wechatkit-0.0.2.tar.gz

---
+++ packages/wechatkit/wechatkit-0.0.2.tar.gz/wechatkit-0.0.2/wechatkit/utils.py
@@ -0,0 +1,66 @@
+# coding: utf-8
+"""Wechatlib utils."""
+import requests
+
+from wechatkit import consts
+from wechatkit.exceptions import WechatException
+
+
+class RequestUtil(object):
+    """Wechat api request util."""
+
+    @staticmethod
+    def get(url):
+        """Get method."""
+        try:
+            resp = requests.get(url)
+            resp.encoding = 'utf-8'
+            retdata = resp.json()
+        except Exception as error:
+            raise WechatException(''.join(error.args))
+        else:
+            return retdata
+
+    @staticmethod
+    def post(url, data, headers=None):
+        """Post method."""
+        if headers:
+            result = requests.post(url, data, headers)
+        else:
+            result = requests.post(url, data)
+        result.encoding = 'utf-8'
+
+        return result.json()
+
+    @staticmethod
+    def get_retcode_msg(retcode):
+        """Get wechat retcode msg."""
+        return consts.RETCODE_DICT.get(str(retcode), '未知状态码')
+
+
+class WechatBasicAPI(object):
+    """Wechat basic api."""
+
+    @staticmethod
+    def get_access_token(appid, appsecret):
+        """Get access token from wechat."""
+        url = consts.WECHAT_ACCESS_TOKEN_URL.format(appid, appsecret)
+
+        result = RequestUtil.get(url)
+
+        if result.get('errmsg'):
+            raise WechatException(result.get('errmsg'))
+
+        return result
+
+    @staticmethod
+    def get_callbackip(access_token):
+        """Get wecaht callbackip."""
+        url = consts.WECHAT_CALLBACKIP_URL.format(access_token)
+
+        result = RequestUtil.get(url)
+
+        if result.get('errmsg'):
+            raise WechatException(result.get('errmsg'))
+
+        return result
