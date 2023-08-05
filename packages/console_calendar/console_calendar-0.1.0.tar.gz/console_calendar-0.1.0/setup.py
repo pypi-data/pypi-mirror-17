c3598b4cf71dc9990e8a17af778652d047677963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:59 +0000

    Add package console_calendar/0.1.0/console_calendar-0.1.0.tar.gz

---
+++ packages/console_calendar/console_calendar-0.1.0.tar.gz/console_calendar-0.1.0/setup.py
@@ -0,0 +1,31 @@
+import imp
+import os
+from setuptools import setup, find_packages
+
+version = imp.load_source(
+    'version', os.path.join('console_calendar', 'version.py'))
+
+setup(
+    name='console_calendar',
+    description=("A calendar working in console."),
+    version=version.VERSION_STRING,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    scripts=['console_calendar/bin/cc'],
+    install_requires=[
+        'termcolor',
+    ],
+    tests_require=[
+        'ipython',
+        'pytest',
+    ],
+    author='Kazuya Gosho',
+    author_email='ketsume0211@gmail.com',
+    url='https://github.com/acro5piano/console_calendar',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
