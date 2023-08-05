51a6ca029ec4c7e134d5aac407ee339bdc39fd1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:10 +0000

    Add package cmsync/0.8.1/cmsync-0.8.1.zip

---
+++ packages/cmsync/cmsync-0.8.1.zip/cmsync-0.8.1/cmsync/__init__.py
@@ -0,0 +1,1 @@
+from .cmsync import CMSync
---
+++ packages/cmsync/cmsync-0.8.1.zip/cmsync-0.8.1/cmsync/color.py
@@ -0,0 +1,10 @@
+import colorama
+
+colorama.init()
+
+
+class Color:
+    ERROR = colorama.Fore.LIGHTRED_EX
+    WARNING = colorama.Fore.LIGHTYELLOW_EX
+    INFO = colorama.Fore.LIGHTCYAN_EX
+    COMMAND = colorama.Fore.LIGHTWHITE_EX
