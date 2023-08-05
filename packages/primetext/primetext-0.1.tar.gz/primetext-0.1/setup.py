8116063a6af723f1c4ef61628135530b97ae42f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:45 +0000

    Add package primetext/0.1/primetext-0.1.tar.gz

---
+++ packages/primetext/primetext-0.1.tar.gz/primetext-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'primetext',
+  packages = ['primetext'], # this must be the same as the name above
+  version = '0.1',
+  description = 'package for indexing text datasets using prime number factorisation for fast word frequency analysis',
+  author = 'Zack Akil',
+  author_email = 'zackakil94@gmail.com',
+  url = 'https://github.com/ZackAkil/primetext', # use the URL to the github repo
+  download_url = 'https://github.com/ZackAkil/primetext/tarball/0.1', # I'll explain this in a second
+  keywords = ['prime','factor','text','word','frequency','search', 'indexing'], # arbitrary keywords
+  classifiers = [],
+)
