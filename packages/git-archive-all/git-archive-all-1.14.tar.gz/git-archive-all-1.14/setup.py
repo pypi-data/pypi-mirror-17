51ef20b8f5f9fc839d268941704c6abfed0fdc93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:16 +0000

    Add package git-archive-all/1.14/git-archive-all-1.14.tar.gz

---
+++ packages/git-archive-all/git-archive-all-1.14.tar.gz/git-archive-all-1.14/setup.py
@@ -0,0 +1,43 @@
+import re
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+# Parse the version from the file.
+verstrline = open('git_archive_all.py', "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in git_archive_all.py")
+
+setup(
+    name='git-archive-all',
+    version=verstr,
+    description='Archive git repository with its submodules.',
+    author='Ilya Kulakov',
+    author_email="kulakov.ilya@gmail.com",
+    url='https://github.com/Kentzo/git-archive-all',
+    py_modules=['git_archive_all'],
+    entry_points="""
+    [console_scripts]
+    git-archive-all=git_archive_all:main
+    """,
+    license="MIT License",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Version Control',
+        'Topic :: System :: Archiving'
+    ]
+)
