47075e74e4c06a00608b7b5527516df7ad7cb6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:13 +0000

    Add package nester023/1.1.0/nester023-1.1.0.tar.gz

---
+++ packages/nester023/nester023-1.1.0.tar.gz/nester023-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'nester023',
+    version     = '1.1.0',
+    py_modules  = ['nester'],
+    author      = 'lehoainam',
+    author_email= 'hoainamle023@gmail.com',
+    url         = 'http://www.tottenhamhotspur.com',
+    description = 'A simple tester of nested list',
+    )
