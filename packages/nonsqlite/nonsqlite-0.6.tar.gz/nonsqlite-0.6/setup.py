579b4fe436ca28560c3900185f28b3ed5e3d60fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:20 +0000

    Add package nonsqlite/0.6/nonsqlite-0.6.tar.gz

---
+++ packages/nonsqlite/nonsqlite-0.6.tar.gz/nonsqlite-0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nonsqlite',
+      version='0.6',
+      description='Non SQL Database',
+      url='https://github.com/emilianobilli/nonsqlite',
+      author='Emiliano Billi',
+      author_email='emiliano.billi@gmail.com',
+      license='GPL',
+      packages=['nonsqlite'],
+      zip_safe=False)
