6591e15962555fcee54d5c1a70d5576c84ebec27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:12 +0000

    Add package django-reversion-compare/0.7.1/django-reversion-compare-0.7.1.tar.gz

---
+++ packages/django-reversion-compare/django-reversion-compare-0.7.1.tar.gz/django-reversion-compare-0.7.1/tests/manage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from .test_utils import cleanup_temp
+
+
+def cli(arguments):
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+    print("\nUse DJANGO_SETTINGS_MODULE=%r" % os.environ["DJANGO_SETTINGS_MODULE"])
+    from django.core.management import execute_from_command_line
+
+    old_cwd = os.getcwd()
+    try:
+        execute_from_command_line(arguments)
+    finally:
+        os.chdir(old_cwd)
+        from django.conf import settings
+        cleanup_temp(settings.UNITTEST_TEMP_PATH)
+
+
+def start_test_server():
+    cli(arguments=[sys.argv[0], "run_testserver"])
+
+
+if __name__ == "__main__":
+    cli(sys.argv)
---
+++ packages/django-reversion-compare/django-reversion-compare-0.7.1.tar.gz/django-reversion-compare-0.7.1/tests/settings.py
@@ -0,0 +1,62 @@
+
+import os
+import tempfile
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.request",
+    "django.contrib.auth.context_processors.auth",
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+
+    "reversion.middleware.RevisionMiddleware",
+)
+
+LANGUAGE_CODE = "en"
+LANGUAGES = (
+    ("en","de"),
+)
+SECRET_KEY = 'unittests-fake-key'
+
+PASSWORD_HASHERS = [ # Speeding up the tests
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+SITE_ID = 1
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'django.contrib.sessions',
+
+    'reversion',
+
+    'reversion_compare',
+    'tests',
+]
+
+ROOT_URLCONF="tests.urls"
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+UNITTEST_TEMP_PATH = tempfile.mkdtemp(prefix="reversion_compare_unittest_")
+print("Use temp dir: %r" % UNITTEST_TEMP_PATH)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(UNITTEST_TEMP_PATH, 'cmsplugin_markup_unittest_database'),
+    }
+}
+
+DEBUG = True
+
+# add reversion models to django admin:
+ADD_REVERSION_ADMIN = True
