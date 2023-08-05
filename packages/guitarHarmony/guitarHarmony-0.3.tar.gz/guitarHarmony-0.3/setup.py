9a38550fe22d9784190e3700fb9bfa1127caa41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:14 +0000

    Add package guitarHarmony/0.3/guitarHarmony-0.3.tar.gz

---
+++ packages/guitarHarmony/guitarHarmony-0.3.tar.gz/guitarHarmony-0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'guitarHarmony',
+  packages = ['guitarHarmony'], # this must be the same as the name above
+  version = '0.3',
+  description = 'A python wrapper to learn music theory',
+  author = 'Xi He',
+  author_email = 'heeryerate@gmail.com',
+  url = 'https://bitbucket.org/Xi_He/music-theory', # use the URL to the github repo
+  download_url = 'https://bitbucket.org/Xi_He/music-theory/get/0.3.zip', # I'll explain this in a second
+  keywords = ['music', 'theory', 'guitar', 'harmony'], # arbitrary keywords
+  classifiers = [],
+)
