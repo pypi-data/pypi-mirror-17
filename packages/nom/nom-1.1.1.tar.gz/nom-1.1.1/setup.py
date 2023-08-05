b1a78b8af407f4d1a1e2a62c9d3df711797bb363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:46 +0000

    Add package nom/1.1.1/nom-1.1.1.tar.gz

---
+++ packages/nom/nom-1.1.1.tar.gz/nom-1.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='nom',
+    version='1.1.1',
+    description='tool for managing markdown notes',
+    url='https://github.com/frnsys/nom',
+    author='Francis Tseng',
+    author_email='f@frnsys.com',
+    license='GPLv3',
+
+    packages=['nom'],
+    install_requires=[
+        'Jinja2==2.8',
+        'Markdown==2.6.5',
+        'click==6.2',
+        'gfm==0.0.3',
+        'html2text==2015.11.4',
+        'lxml==3.5.0',
+        'py-gfm==0.1.1',
+        'watchdog==0.8.3'
+    ],
+    entry_points='''
+        [console_scripts]
+        nom=nom.cli:cli
+    ''',
+)
