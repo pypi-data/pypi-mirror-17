559453d3b4fc722b2d676400fa37ded9eec49aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:25 +0000

    Add package imongo/0.0.1a9/imongo-0.0.1a9.zip

---
+++ packages/imongo/imongo-0.0.1a9.zip/imongo-0.0.1a9/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from orange import *
+requires=['pymongo',
+          'mongoengine',
+          'orange',]
+setup(
+        name='imongo',
+        platforms='any',
+        description='None',
+        long_description='None',
+        install_requires=requires,
+        )
