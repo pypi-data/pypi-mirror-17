c058649ba3aec9fafd81a0c6e211220079d1d0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:07 +0000

    Add package compmech/0.7.0/compmech-0.7.0-cp35-cp35m-win_amd64.whl

---
+++ packages/compmech/compmech-0.7.0-cp35-cp35m-win_amd64.whl/compmech/panel/assembly/__init__.py
@@ -0,0 +1,18 @@
+"""
+===================================================================
+Assembly of Plate and Panel Models (:mod:`compmech.panel.assembly`)
+===================================================================
+
+.. currentmodule:: compmech.panel.assembly
+
+.. autoclass:: PanelAssembly
+    :members:
+
+
+"""
+from __future__ import absolute_import
+
+from . assembly import PanelAssembly
+from . tstiff2d_1stiff_freq import tstiff2d_1stiff_freq
+from . tstiff2d_1stiff_compression import tstiff2d_1stiff_compression
+from . tstiff2d_1stiff_flutter import tstiff2d_1stiff_flutter
---
+++ packages/compmech/compmech-0.7.0-cp35-cp35m-win_amd64.whl/compmech/version.py
@@ -0,0 +1,14 @@
+
+# THIS FILE IS GENERATED FROM CompMech setup.py
+short_version = '0.7.0'
+version = '0.7.0'
+full_version = '0.7.0'
+git_revision = '231af41cda72bd37ad3b14ceff637c9db2b4c446'
+isreleased = True
+if isreleased:
+    __version__ = version
+else:
+    __version__ = full_version
+
+if not isreleased:
+    version = full_version
