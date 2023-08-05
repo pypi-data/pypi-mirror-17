35cea5271c1fa4c91d15d3c1bb7136b332ea153e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:10 +0000

    Add package tankbuster/0.1.4/tankbuster-0.1.4.tar.gz

---
+++ packages/tankbuster/tankbuster-0.1.4.tar.gz/tankbuster-0.1.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from setuptools import setup
+from setuptools.command.install import install
+
+setup(name='tankbuster',
+      version='0.1.4',
+      description='An image classifier trained to detect Soviet/Russian T-72 tanks',
+      url='https://github.com/thiippal/tankbuster',
+      author='Tuomo Hiippala',
+      packages=['tankbuster'],
+      package_dir={'tankbuster': 'tankbuster'},
+      package_data={'tankbuster': ['*.py', 'classifier/*.json', 'classifier/*.hdf5', 'cnn/*.py', 'engine/*.py']},
+      author_email='tuomo.hiippala@iki.fi',
+      license='MIT',
+      keywords=['osint', 'imaging', 'classifier'],
+      download_url='https://github.com/thiippal/tankbuster/tarball/0.1.4',
+      install_requires=["Pillow>=3.3.1",
+                        "numpy>=1.11.1",
+                        "Keras>=1.0.8",
+                        "colorama>=0.3.7",
+                        "h5py>=2.6.0"],
+      classifiers=['Programming Language :: Python :: 2.7'])
