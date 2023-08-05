3804c942bce73e840f538b67bf33dfd4b9ed5644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:10 +0000

    Add package pyfil/0.6/pyfil-0.6.tar.gz

---
+++ packages/pyfil/pyfil-0.6.tar.gz/pyfil-0.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pyfil',
+    version='0.6',
+    author='Aaron Christianson',
+    license='BSD',
+    author_email='ninjaaron@gmail.com',
+    url='https://github.com/ninjaaron/pyfil',
+    description='print the return value of python expression -- also in loops',
+    long_description=open('README.rst').read(),
+    keywords='evaluate',
+    packages=['pyfil'],
+    entry_points={'console_scripts': ['rep=pyfil.pyfil:main']},
+)
