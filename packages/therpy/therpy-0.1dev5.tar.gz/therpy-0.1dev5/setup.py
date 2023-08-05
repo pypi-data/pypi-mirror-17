9c0aa015e3528e00c1bca6c5e29535e962d3c94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:36 +0000

    Add package therpy/0.1dev5/therpy-0.1dev5.tar.gz

---
+++ packages/therpy/therpy-0.1dev5.tar.gz/therpy-0.1dev5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='therpy',
+    version='0.1dev5',
+    packages=['therpy',],
+    license='Open Source',
+)
+
+# Run following commands on terminal
+# python setup.py sdist
+# python setup.py register (ONLY FIRST TIME)
+# python setup.py sdist upload
