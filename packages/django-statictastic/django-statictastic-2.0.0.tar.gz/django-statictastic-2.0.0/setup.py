584f1b5661f25947ab282efe338fe31237b24a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:55 +0000

    Add package django-statictastic/2.0.0/django-statictastic-2.0.0.tar.gz

---
+++ packages/django-statictastic/django-statictastic-2.0.0.tar.gz/django-statictastic-2.0.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env/python
+
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+metadata = {}
+execfile("statictastic/metadata.py", metadata)
+
+setup(
+    name='django-statictastic',
+    version=metadata['__version__'],
+    license=metadata['__license__'],
+    description='A fantastic way to sync your files to S3.',
+    author=metadata['__author__'],
+    author_email=metadata['__email__'],
+    packages=[
+        'statictastic',
+        'statictastic.management',
+        'statictastic.management.commands',
+        'statictastic.templatetags',
+    ],
+    install_requires=["requests>=2.0.0", "cssmin>=0.2.0", "lxml>=3.0.0"],
+)
+
