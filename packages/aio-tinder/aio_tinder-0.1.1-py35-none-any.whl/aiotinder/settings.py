f9f42445017528852e39b1a11e3d87528ca296f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:39 +0000

    Add package aio-tinder/0.1.1/aio_tinder-0.1.1-py35-none-any.whl

---
+++ packages/aio-tinder/aio_tinder-0.1.1-py35-none-any.whl/aiotinder/settings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+HEADERS = {
+    "User-Agent": "Tinder/5.4.1 (iPhone; iOS 9.3.5; Scale/2.00)",
+    "Content-Type": "application/json",
+    "os_version": "90000300005",
+    "platform": "ios",
+    "app-version": "1738",
+    "x-client-version": "54103"
+}
+
+API_URL = "https://api.gotinder.com/"
