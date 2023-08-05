c48968f7cfb32499e5b838b5c2c5306e10f2114e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:29 +0000

    Add package phriky-units/0.1.0a23/phriky_units-0.1.0a23.tar.gz

---
+++ packages/phriky-units/phriky_units-0.1.0a23.tar.gz/phriky_units-0.1.0a23/phriky_units/phriky_units_tests.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""  COPYRIGHT (c) 2016 UNIVERSITY OF NEBRASKA NIMBUS LAB - JOHN-PAUL ORE
+        PHRIKY-UNITS, THE PHYSICAL UNITS INCONSISTENCY DETECTION TOOL
+"""
+
+from cps_units_checker import CPSUnitsChecker
+from distutils import spawn
+from subprocess import Popen
+import click
+import os
+import pkg_resources
+from pkg_resources import resource_string
+import re
+import sys
+
+
+
+@click.command()
+@click.argument('target_file') 
+@click.option('--include_dir', default='', help='include directory for cppcheck')
+def main(target_file, include_dir):
+    """Console script for phriky_units"""
+    print pkg_resources.resource_listdir('phriky_units.resources', '')
+    with open('delete_me.txt', 'w') as fp:
+        fp.write(resource_string('phriky_units.resources.cppcheck', 'std.cfg'))
+    with open(os.path.join(__file__, 'd2.txt'), 'w') as fp2:
+        fp2.write(resource_string('phriky_units.resources.cppcheck', 'std.cfg'))
+    
+
+
+
+if __name__ == "__main__":
+    main()
