1030df0377c75bcf7b9bb870c8e1e2c5a1c7b324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:04 +0000

    Add package trial_nester/1.0.0/trial_nester-1.0.0.zip

---
+++ packages/trial_nester/trial_nester-1.0.0.zip/trial_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'trial_nester',
+    version = '1.0.0',
+    py_modules = ['trial_nester'],
+    author = 'hfpython',
+    author_email = 'hfpython@headfirstlabs.com',
+    url = 'http://headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+)
