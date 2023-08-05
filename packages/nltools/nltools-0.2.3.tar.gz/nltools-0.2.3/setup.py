8c7b6314ab84913629166b70bbd1e347ebbb4f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:29 +0000

    Add package nltools/0.2.3/nltools-0.2.3.tar.gz

---
+++ packages/nltools/nltools-0.2.3.tar.gz/nltools-0.2.3/setup.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+__version__ = '0.2.3'
+
+setup(
+    name='nltools',
+    version='0.2.3',
+    author='Luke Chang',
+    author_email='luke.j.chang@dartmouth.edu',
+    packages=['nltools'],
+    package_data={'nltools': ['resources/*']},
+    license='LICENSE.txt',
+    description='A Python package to analyze neuroimaging data',
+    long_description='nltools is a collection of python tools to perform preprocessing, univariate GLMs, and predictive multivariate modeling of neuroimaging data. It is the analysis engine powering www.neuro-learn.org.',
+    url='http://neurolearn.readthedocs.org/en/latest/',
+    keywords = ['neuroimaging', 'preprocessing', 'analysis','machine-learning'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        ]
+)
+
