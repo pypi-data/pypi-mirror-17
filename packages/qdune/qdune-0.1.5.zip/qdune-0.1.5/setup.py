9338a7e5354c21f12b64b9bdeffcfb46f1fa0539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:04 +0000

    Add package qdune/0.1.5/qdune-0.1.5.zip

---
+++ packages/qdune/qdune-0.1.5.zip/qdune-0.1.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='qdune',
+      version='0.1.5',
+      description='dune point cloud processing tools',
+      author='Thomas Ashley',
+      author_email='tashley22@gmail.com',
+      url='https://github.com/tashley/qDune',
+      license='MIT',
+      packages=['qdune'],
+      include_package_data=True,
+      install_requires = ['numpy', 'pandas', 'matplotlib']
+)
