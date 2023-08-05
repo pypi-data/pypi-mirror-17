25e630836ec70d206cbe8994fe009b21325ed1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:05 +0000

    Add package nester-ttram/1.1.0/nester-ttram-1.1.0.zip

---
+++ packages/nester-ttram/nester-ttram-1.1.0.zip/nester-ttram-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+	name         = 'nester-ttram',
+	version      = '1.1.0',
+	py_modules   = ['nester'],
+	author       = 'ttram-pypi',
+	author_email = 'tritram@yahoo.com',
+	url          = 'http://www.headfirstlabs.com',
+	description  = 'A simple printer of nested lists',
+	)
