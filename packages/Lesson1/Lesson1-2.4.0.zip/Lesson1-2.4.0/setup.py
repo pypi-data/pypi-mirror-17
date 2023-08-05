964742de2b8c5b3c55d85c917f6d20c54fde54ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:44 +0000

    Add package Lesson1/2.4.0/Lesson1-2.4.0.zip

---
+++ packages/Lesson1/Lesson1-2.4.0.zip/Lesson1-2.4.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name          = 'Lesson1',
+    version       = '2.4.0',
+    py_modules    = ['print_all'],
+    author        = 'Andy Gao',
+    author_email  = 'andy_gao@wistron.com',
+    url           = 'http://www.test.com',
+    description   = 'Just a test',
+    )
+
