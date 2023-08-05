cd2eda208059315443fb6c16882fc01518ce49ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:15 +0000

    Add package bennu/0.4.1a9/bennu-0.4.1a9.zip

---
+++ packages/bennu/bennu-0.4.1a9.zip/bennu-0.4.1a9/bennu/emailutility.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2016, MuChu Hsu
+Contributed by Muchu Hsu (muchu1983@gmail.com)
+This file is part of BSD license
+
+<https://opensource.org/licenses/BSD-3-Clause>
+"""
+import logging
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+class EmailUtility:
+    
+    #建構子
+    def __init__(self):
+        #default email setting
+        self.DEFAULT_SMTP = "smtp.gmail.com:587"
+        self.DEFAULT_ACCOUNT = "public.muchu1983@gmail.com"
+        self.DEFAULT_PASSWORD = "bee520BEE520"
+        
+    #寄送 email
+    def sendEmail(self, strSubject=None, strFrom=None, strTo=None, strMsg=None, lstStrTarget=None, strSmtp=None, strAccount=None, strPassword=None):
+        if (strSmtp and strAccount and strPassword):
+            strSmtp = self.DEFAULT_SMTP
+            strAccount = self.DEFAULT_ACCOUNT
+            strPassword = self.DEFAULT_PASSWORD
+        #郵件內容
+        msg = MIMEText(strMsg, "html", "utf-8")
+        msg["Subject"] = Header(strSubject, "utf-8")
+        msg["From"] = Header(strFrom, "utf-8")
+        msg["To"] = Header(strTo, "utf-8")
+        #傳送
+        server = smtplib.SMTP(strSmtp)
+        server.ehlo()
+        server.starttls()
+        server.login(strAccount, strPassword)
+        server.sendmail(strAccount, lstStrTarget, msg.as_string())
+        server.quit()
---
+++ packages/bennu/bennu-0.4.1a9.zip/bennu-0.4.1a9/setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2015, MuChu Hsu
+Contributed by Muchu Hsu (muchu1983@gmail.com)
+This file is part of BSD license
+
+<https://opensource.org/licenses/BSD-3-Clause>
+"""
+from setuptools import setup,find_packages
+
+with open("README.txt") as file:
+    long_description = file.read()
+
+setup(
+    name = "bennu",
+    version = "0.4.1a9",
+    author = "MuChu Hsu",
+    author_email = "muchu1983@gmail.com",
+    maintainer = "MuChu Hsu",
+    maintainer_email = "muchu1983@gmail.com",
+    url="https://github.com/muchu1983/bennu",
+    description = "muchu's utility module",
+    long_description=long_description,
+    download_url="https://pypi.python.org/pypi/bennu",
+    platforms = "python 3.4",
+    license = "BSD 3-Clause License",
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = ["Pillow>=3.0.0", "Flask>=0.10.1", "pymongo>=3.2.2", "selenium>=2.53.2"],
+    entry_points = {"console_scripts":["bennu=bennu.launcher:entry_point"]},
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Win32 (MS Windows)",
+        "Environment :: X11 Applications :: GTK",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: Chinese (Traditional)",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Internet :: WWW/HTTP",
+        ],
+)
+
+
+
+
