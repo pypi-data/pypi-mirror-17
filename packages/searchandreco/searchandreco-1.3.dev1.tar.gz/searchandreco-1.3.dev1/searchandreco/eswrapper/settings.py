d0d2f10c4dc5486cb25cfd8d68e12e8e3ee543a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:49 +0000

    Add package searchandreco/1.3.dev1/searchandreco-1.3.dev1.tar.gz

---
+++ packages/searchandreco/searchandreco-1.3.dev1.tar.gz/searchandreco-1.3.dev1/searchandreco/eswrapper/settings.py
@@ -0,0 +1,71 @@
+__author__ = 'monica'
+
+import redis
+
+## Import for instantiating elastic search via elasticsearch-py client library
+## (elasticsearch==1.4.0)
+
+from elasticsearch import Elasticsearch
+
+# Different nodes of Elastic Search
+
+
+ES_NODE1 = '10.2.3.xx'
+
+ES_NODE2 = '10.2.3.xxx'
+
+ES_NODE3 = '10.2.3.xxx'
+
+ES_NODE_LOCAL = 'localhost'
+
+ES_NODE1_PORT = '8080'
+
+ES_NODE2_PORT = '8080'
+
+ES_NODE3_PORT = '8080'
+
+ES_PORT_LOCAL = '9200'
+
+
+## This code for working on the queue at production
+REDIS_HOST = 'xx.x.x.xxx'
+REDIS_HOST_GP = 'xx.x.x.xxx'
+
+USE_LOCAL = True
+
+UWSGI_SETUP = False
+
+
+GP_STOPWORDS_FILE_NAME = "/mnt/apps/repo/searchandreco/searchandreco/hindi_stpwords.txt"
+
+SUB_LOG_FILE_NAME = '/mnt/logs/esapi/vbsub.log'
+GP_SUB_LOG_FILE_NAME = '/mnt/logs/esapi/gpsub.log'
+
+try:
+    from local_settings import *
+
+except:
+    pass
+
+redis_client = redis.client.StrictRedis(host=REDIS_HOST, port=6379)
+redis_client_gp = redis.client.StrictRedis(host=REDIS_HOST_GP, port=6379)
+
+
+
+
+ES_LOCAL_HOST = [{'node': ES_NODE_LOCAL, 'port': ES_PORT_LOCAL}]
+
+ES_HOSTS = [{'host': ES_NODE1, 'port': ES_NODE1_PORT}, {'host': ES_NODE2, 'port': ES_NODE2_PORT}, \
+            {'host': ES_NODE3, 'port': ES_NODE3_PORT}]
+
+
+    ## A little code which instantiates ElasticSearch
+
+if(USE_LOCAL == True):
+
+    es_client = Elasticsearch('http://localhost:9200/', sniff_on_start=True, sniff_on_connection_fail=True, sniffer_timeout=300)
+else:
+    es_client = Elasticsearch(ES_HOSTS, sniff_on_start=True, sniff_on_connection_fail=True, sniffer_timeout=300)
+
+
+
---
+++ packages/searchandreco/searchandreco-1.3.dev1.tar.gz/searchandreco-1.3.dev1/searchandreco/script/aliastoindex.py
@@ -0,0 +1,119 @@
+from eswrapper.defines import *
+from eswrapper.originals_defines import *
+from eswrapper.settings import *
+import elasticsearch
+from elasticsearch import Elasticsearch, helpers
+import json
+import pprint
+
+### creating new index for gp and adding it to gp alias, data will populated through mongo.
+
+##connect to es server
+es = es_client
+
+
+## function to create index
+def create_index(prop, mapping = ES_INDEX_MAPPING, settings = None):
+
+		result = {'status': 0, 'index_created': '', 'error': ''}
+
+		try:
+
+			if(check_index_exists(index_name_new) == True):
+				result['status'] = 2
+				result['index_created'] = index_name_new
+				result['error'] = 'index exists'
+				return result
+
+
+			if(prop == '25rt') or (prop == '679a'):
+
+				settings = ES_INDEX_SETTINGS
+
+			if (prop == 's57v') :
+
+				settings = ES_INDEX_SETTINGS_GP
+
+
+			if (prop == 'or05') :
+
+				mapping = ES_INDEX_MAPPING_ORIGINALS
+				settings = ES_INDEX_SETTINGS_ORIGINALS
+
+
+			index_parameters =  { 'settings': settings, 'mappings': mapping }
+
+			#print index_parameters
+
+			cr_status = es.indices.create(index = index_name_new, body = index_parameters)
+
+			if(cr_status.has_key('acknowledged') and cr_status['acknowledged'] == True):
+				result['status'] = 1
+				result['index_created'] = index_name_new
+
+
+		except Exception as (e):
+			# TODO: add a logger here
+			result['status'] = 0
+			result['error'] = str(e)
+
+			print "any exception", str(e)
+
+		return result
+
+
+
+###function to check if index exists
+def check_index_exists(index):
+
+		status = es.indices.exists(index)
+		return status
+
+
+
+
+
+
+
+print "*****"
+print "Alias ['vb_realtimeposts_alias , gp_realtimeposts_alias , swp_realtimeposts_alias , swp_originalsposts_alias']"
+print "*****\n"
+
+all_alias = es.indices.get_aliases(name = ['vb_realtimeposts_alias','gp_realtimeposts_alias','swp_realtimeposts_alias','swp_originalsposts_alias'])
+pprint.pprint(all_alias)
+
+print "\nEnter the name of alias"
+alias_name = raw_input()
+
+print "\n"
+
+
+
+print "Enter the name of new index created\n"
+index_name_new = raw_input()
+
+print "\nEnter the property code of alias for which index is to be created, [ VB='679a' , SW='25rt' , GP='s57v' , SWPORIG='or05' ] "
+prop_name = raw_input()
+
+
+###create a new index
+nindex = create_index(prop_name)
+print "\n"
+print nindex
+
+
+a_body = {"actions":
+			[
+				{ "add":    { "index": nindex, "alias": alias_name }}
+			]
+		}
+
+### changing the alias point to index
+#es.indices.update_aliases(body = a_body)
+es.indices.put_alias(name = alias_name,index = index_name_new)
+
+###check for alias point to index after updation
+check_updated_alias = es.indices.get_alias(alias_name)
+print check_updated_alias
+
+print "\n Your alias is now pointing to :" + index_name_new
