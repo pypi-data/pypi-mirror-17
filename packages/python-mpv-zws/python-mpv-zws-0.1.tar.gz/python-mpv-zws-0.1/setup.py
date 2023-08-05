5bc003ca0d4d2b03e87f640a2d84780febab5a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:09 +0000

    Add package python-mpv-zws/0.1/python-mpv-zws-0.1.tar.gz

---
+++ packages/python-mpv-zws/python-mpv-zws-0.1.tar.gz/python-mpv-zws-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+setup(
+    name = 'python-mpv-zws',
+    version = '0.1',
+    py_modules = ['python_mpv_zws'],
+    description = ('A python interface to the mpv media player'),
+    url = 'https://github.com/ZWS2014/python-mpv-zws',
+    author = 'jaseg, forked by Dan Bright',
+    author_email = 'productions@zaziork.com',
+    license = 'AGPLv2'
+)
