b8f90975de5d898d82e15269ce35a8d367c469f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:53 +0000

    Add package ensembles/0.1.2/ensembles-0.1.2.tar.gz

---
+++ packages/ensembles/ensembles-0.1.2.tar.gz/ensembles-0.1.2/setup.py
@@ -0,0 +1,17 @@
+
+import os
+from setuptools import setup
+
+setup(
+    name = "ensembles",
+    version = "0.1.2",
+    author = "Prajwal Kailas",
+    author_email = "prajwal967@gmail.com",
+    description = ("A package for ensembling of machine learning models"),
+    license = "MIT",
+    keywords = "Ensemble",
+    url = "https://github.com/unnati-xyz/ensemble-package",
+    packages=['ensembles'],
+    classifiers=[]
+)
+
