cfc020fba08ea3320c88944d7f848ee99b9b978a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:32 +0000

    Add package PepipostAPIV10Lib/0.1/PepipostAPIV10Lib-0.1.tar.gz

---
+++ packages/PepipostAPIV10Lib/PepipostAPIV10Lib-0.1.tar.gz/PepipostAPIV10Lib-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='PepipostAPIV10Lib',
+      version='0.1',
+      description='Official repository of the Pepipost SDK for Python',
+      url='https://github.com/pepipost/pepipost-sdk-python',
+      author='Tabrez Shaikh',
+      author_email='tabrez@pepipost.com',
+      license='MIT',
+      packages=['PepipostAPIV10Lib'],
+      zip_safe=False)
