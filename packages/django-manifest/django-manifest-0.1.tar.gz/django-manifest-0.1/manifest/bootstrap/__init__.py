b06c40bd7dc8df7789e6b8cc33af107dd788fd10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:24 +0000

    Add package django-manifest/0.1/django-manifest-0.1.tar.gz

---
+++ packages/django-manifest/django-manifest-0.1.tar.gz/django-manifest-0.1/manifest/bootstrap/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from django import template
+template.add_to_builtins('django.templatetags.i18n')
+template.add_to_builtins('manifest.bootstrap.templatetags.bootstrap_tags')
