9a46e9e6e8a05168d5693c898457a3dda6c0a64b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:30 +0000

    Add package pypif/1.0.12/pypif-1.0.12.tar.gz

---
+++ packages/pypif/pypif-1.0.12.tar.gz/pypif-1.0.12/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='pypif',
+      version='1.0.12',
+      url='http://github.com/CitrineInformatics/pypif',
+      description='Python tools for working with the Physical Information File (PIF)',
+      author='Kyle Michel',
+      author_email='kyle@citrine.io',
+      packages=find_packages())
