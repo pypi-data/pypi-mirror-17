32d61b46a3bee1a5e66ef56961a944dcf1f57be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:12 +0000

    Add package bubbletools/0.1.4/bubbletools-0.1.4.tar.gz

---
+++ packages/bubbletools/bubbletools-0.1.4.tar.gz/bubbletools-0.1.4/bubbletools/__main__.py
@@ -0,0 +1,28 @@
+"""Bubble format related tools
+
+usage:
+    bubble-tool.py validate <bblfile> [--profiling]
+    bubble-tool.py dot <bblfile> [<dotfile>]
+
+"""
+
+
+import docopt
+
+from bubbletools import validator
+from bubbletools import converter
+from bubbletools import utils
+
+
+if __name__ == "__main__":
+    args = docopt.docopt(__doc__)
+
+    if args['validate']:
+        logs = validator.validate(utils.file_lines(args['<bblfile>']),
+                                  profiling=args['--profiling'])
+        for log in logs:
+            print(log)
+
+    if args['dot']:
+        lines = utils.file_lines(args['<bblfile>']),
+        converter.to_dot(lines, args['<dotfile>'])
