f61c58a9f1cc3420605ae405270314f3e5bfc500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:27 +0000

    Add package orochi/0.2.1/orochi-0.2.1.tar.gz

---
+++ packages/orochi/orochi-0.2.1.tar.gz/orochi-0.2.1/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+from setuptools import setup, find_packages
+from orochi import meta
+
+readme = io.open('README.rst', mode='r', encoding='utf8').read()
+
+setup(name='orochi',
+      version=meta.version,
+      description=meta.description,
+      author=meta.author,
+      author_email=meta.author_email,
+      url='https://github.com/dbrgn/orochi',
+      packages=find_packages(),
+      zip_safe=False,
+      include_package_data=True,
+      license=meta.license,
+      keywords='orochi music playlists 8tracks eighttracks mplayer player',
+      long_description=readme,
+      install_requires=['requests>=1.2.0,<3'],
+      entry_points={
+          'console_scripts': [
+              '%s = orochi.client:main' % meta.title,
+          ]
+      },
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Natural Language :: English',
+          'Operating System :: MacOS',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Internet',
+          'Topic :: Multimedia :: Sound/Audio :: Players',
+          'Topic :: Terminals',
+      ],
+)
