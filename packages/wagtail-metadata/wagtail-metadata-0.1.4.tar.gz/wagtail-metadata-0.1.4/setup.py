7164326e9138c89f32924b06634ed6b9bd0ad5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:24 +0000

    Add package wagtail-metadata/0.1.4/wagtail-metadata-0.1.4.tar.gz

---
+++ packages/wagtail-metadata/wagtail-metadata-0.1.4.tar.gz/wagtail-metadata-0.1.4/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""
+Install wagtail-metadata using setuptools
+"""
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+from setuptools import find_packages, setup
+
+setup(
+    name='wagtail-metadata',
+    version='0.1.4',
+    description="A tool to assist with metadata for social media.",
+    long_description=readme,
+    author='Liam Brenner',
+    author_email='liam@takeflight.com.au',
+    url='https://github.com/takeflight/wagtail-metadata',
+
+    install_requires=[
+        'wagtail>=1.0',
+    ],
+    zip_safe=False,
+    license='BSD License',
+
+    packages=find_packages(),
+
+    include_package_data=True,
+    package_data={},
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
