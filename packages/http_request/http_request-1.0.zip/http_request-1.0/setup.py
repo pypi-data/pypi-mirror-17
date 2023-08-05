760471afbe226efd8b35246b772fd4aec568bf05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:58 +0000

    Add package http_request/1.0/http_request-1.0.zip

---
+++ packages/http_request/http_request-1.0.zip/http_request-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='http_request',
+    version='1.0',
+    packages=['http_request'],
+    url='https://github.com/dennisfischer/http_request',
+    license='MIT',
+    author='Dennis Fischer',
+    author_email='chaosking0912@gmail.com',
+    description='A small python library to parse and build HTTP requests'
+)
