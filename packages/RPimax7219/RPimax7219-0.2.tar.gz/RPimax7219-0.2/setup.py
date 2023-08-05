c70f1ad4d7a18fbc1047b4639c7df474a4cd8f14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:06 +0000

    Add package RPimax7219/0.2/RPimax7219-0.2.tar.gz

---
+++ packages/RPimax7219/RPimax7219-0.2.tar.gz/RPimax7219-0.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup,Extension
+
+setup(
+    name = "RPimax7219",
+    version = "0.2",
+    author = "Fernando Manfredi",
+    author_email = "contact@acidhub.click",
+    description = ("A small library to drive a MAX7219 LED serializer using hardware spidev"),
+    license = "MIT",
+    keywords = "raspberry pi rpi led max7219 matrix seven segment",
+    url = "https://github.com/Acidhub/RPimax7219",
+    packages=['RPimax7219'],
+)
