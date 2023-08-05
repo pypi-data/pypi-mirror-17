dabd7a238e6acb6d2dfa8bd3f80ee62681e6d4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:50 +0000

    Add package twindb-infrastructure/0.1.3/twindb-infrastructure-0.1.3.tar.gz

---
+++ packages/twindb-infrastructure/twindb-infrastructure-0.1.3.tar.gz/twindb-infrastructure-0.1.3/tests/test_twindb_infrastructure.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_twindb_infrastructure
+----------------------------------
+
+Tests for `twindb_infrastructure` module.
+"""
+
+
+from click.testing import CliRunner
+
+from twindb_infrastructure import cli
+
+
+def test_command_line_interface():
+    runner = CliRunner()
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert 'twindb_infrastructure.cli.main' in result.output
+    help_result = runner.invoke(cli.main, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help  Show this message and exit.' in help_result.output
