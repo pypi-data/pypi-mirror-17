7cf5bf404701f993534669877ceca9fef481c2f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:35 +0000

    Add package ljq_helloWorld/2.0.0/ljq_helloWorld-2.0.0.tar.gz

---
+++ packages/ljq_helloWorld/ljq_helloWorld-2.0.0.tar.gz/ljq_helloWorld-2.0.0/setup.py
@@ -0,0 +1,15 @@
+#encoding:utf-8
+#发布文件
+
+from distutils.core import setup
+
+setup(
+    name='ljq_helloWorld',
+    version='2.0.0',
+    #py_modules=['helloWorld'],
+    #author='keenleung',
+    #author_email='lsjljq@163.com',
+    #url='http://www.keenleung.com',
+    description='这是我的第一个发布安装文件'
+)
+
