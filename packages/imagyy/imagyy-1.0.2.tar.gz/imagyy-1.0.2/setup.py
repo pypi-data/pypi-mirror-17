6ac8c69a5a3191cabd92bb39aef6c1853e79701f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package imagyy/1.0.2/imagyy-1.0.2.tar.gz

---
+++ packages/imagyy/imagyy-1.0.2.tar.gz/imagyy-1.0.2/setup.py
@@ -0,0 +1,52 @@
+__author__ = 'tusharmakkar08'
+
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+install_requirement = [str(ir.req) for ir in install_reqs]
+
+setup(
+    # Application name:
+    name="imagyy",
+
+    # Version number:
+    version="1.0.2",
+
+    # Application author details:
+    author="Tushar Makkar",
+    author_email="tusharmakkar08@gmail.com",
+
+    # Packages
+    packages=['image_search', 'image_search.search_files'],
+
+    package_data={'': ['*.md']},
+
+    license='MIT',
+    platforms=['any'],
+    # Details
+    url="https://github.com/tusharmakkar08/Imagyy/",
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # license="LICENSE.txt",
+    description="Fetches public photos of different social networking platforms",
+
+    long_description=open("README.rst").read(),
+
+    entry_points={
+        'console_scripts': [
+            'imagyy = image_search.main_search:command_line_runner',
+        ]
+    },
+
+    # Dependent packages (distributions)
+    install_requires=install_requirement,
+
+)
