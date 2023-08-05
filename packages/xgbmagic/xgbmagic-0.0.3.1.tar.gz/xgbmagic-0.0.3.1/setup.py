09858a8b869d6ca651d5a1be0b57649b9278e2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:11 +0000

    Add package xgbmagic/0.0.3.1/xgbmagic-0.0.3.1.tar.gz

---
+++ packages/xgbmagic/xgbmagic-0.0.3.1.tar.gz/xgbmagic-0.0.3.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='xgbmagic',
+      version='0.0.3.1',
+      description='Data preprocessing and analysis using XGBoost',
+      url='http://github.com/mirri66/xgbmagic',
+      author='Grace Tang',
+      author_email='tsmgrace@gmail.com',
+      license='MIT',
+      packages=['xgbmagic'],
+      install_requires=[
+          'pandas',
+          'numpy',
+          'sklearn',
+          'seaborn',
+          'unicodecsv'
+      ],
+      zip_safe=False)
