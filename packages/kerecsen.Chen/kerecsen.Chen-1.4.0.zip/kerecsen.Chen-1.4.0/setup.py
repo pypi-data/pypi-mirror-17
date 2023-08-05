23cc30dcc36c3e73b52b18b6d30985e20bc7c7f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:53 +0000

    Add package kerecsen.Chen/1.4.0/kerecsen.Chen-1.4.0.zip

---
+++ packages/kerecsen.Chen/kerecsen.Chen-1.4.0.zip/kerecsen.Chen-1.4.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='kerecsen.Chen',
+      version='1.4.0',
+      py_modules=['newnester'],
+      author='kerecsen.Chen',
+      author_email='541448862chen@gmail.com',
+      url='http://www.headfirstlabs.com',
+      description='A simple printer of nested lists',
+      )
