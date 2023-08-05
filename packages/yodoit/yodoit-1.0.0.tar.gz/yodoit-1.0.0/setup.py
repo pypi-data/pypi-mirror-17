fe59bd8010aadeabb7d10808272f73baf505e6e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:58 +0000

    Add package yodoit/1.0.0/yodoit-1.0.0.tar.gz

---
+++ packages/yodoit/yodoit-1.0.0.tar.gz/yodoit-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='yodoit',
+    description='Python CLI for using an ' \
+                'Eisenhower Decision Matrix with Trello',
+    version='1.0.0',
+    url='https://github.com/niketanpatel/yodoit-cli',
+    author='Niketan Patel',
+    scripts=['bin/yodoit', 'bin/yodoit_get_existing_board', 'bin/yodoit_get_new_board', 'bin/yodoit_set_authkeys'],
+    packages=['yodoit'],
+    license='MIT',
+    keywords='todo list todolist trello developer ' \
+             'cli eisenhower decision matrix box productivity method'
+)
