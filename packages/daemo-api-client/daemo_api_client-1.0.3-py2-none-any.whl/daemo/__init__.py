686689fbe0e6d9c440f6b780ff442e600c6fd4ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:50 +0000

    Add package daemo-api-client/1.0.3/daemo_api_client-1.0.3-py2-none-any.whl

---
+++ packages/daemo-api-client/daemo_api_client-1.0.3-py2-none-any.whl/daemo/__init__.py
@@ -0,0 +1,12 @@
+import logging
+
+__version__ = "1.0.2"
+
+try:
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
