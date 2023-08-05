cb4f3e16356509916d0187c5d8bf8d015936d08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:25 +0000

    Add package scrapy-eagle/0.0.32/scrapy-eagle-0.0.32.tar.gz

---
+++ packages/scrapy-eagle/scrapy-eagle-0.0.32.tar.gz/scrapy-eagle-0.0.32/scrapy_eagle/dashboard/memory.py
@@ -0,0 +1,30 @@
+import redis
+
+from scrapy_eagle.dashboard.settings import get_config_file
+
+redis_pool = None
+
+
+def init_memory():
+
+    global redis_pool
+
+    config = get_config_file()
+
+    redis_pool = redis.ConnectionPool(
+        host=config['redis']['host'],
+        port=config['redis']['port'],
+        db=config['redis']['db']
+    )
+
+
+def get_redis_pool():
+    return redis_pool
+
+
+def get_connection():
+
+    if not redis_pool:
+        init_memory()
+
+    return redis.Redis(connection_pool=redis_pool)
