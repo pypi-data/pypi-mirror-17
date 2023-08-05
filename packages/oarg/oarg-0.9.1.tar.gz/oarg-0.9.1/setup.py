5530f21743a2c109c40b2171f5904778eb3c0278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:23 +0000

    Add package oarg/0.9.1/oarg-0.9.1.tar.gz

---
+++ packages/oarg/oarg-0.9.1.tar.gz/oarg-0.9.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="oarg",
+    version="0.9.1",
+    description="Command line simple argument parser",
+    url="https://github.com/erikperillo/oarg_py",
+    author="Erik Perillo",
+    author_email="erik.perillo@gmail.com",
+    license="GPL3",
+    packages=["oarg"],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3"
+    ],
+    zip_safe=False,
+) 
