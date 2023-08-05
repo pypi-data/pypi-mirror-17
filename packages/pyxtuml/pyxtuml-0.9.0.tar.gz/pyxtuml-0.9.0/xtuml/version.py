d7a194291d2a517eb485d6c7cd03bf93884d5943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:25 +0000

    Add package pyxtuml/0.9.0/pyxtuml-0.9.0.tar.gz

---
+++ packages/pyxtuml/pyxtuml-0.9.0.tar.gz/pyxtuml-0.9.0/xtuml/version.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+# Copyright (C) 2016 John Törnblom
+
+name = 'pyxtuml'
+date = '2016-09-03'
+version = '0.9'
+release = '0.9.0'
+
+complete_string = '%s v%s (%s)' % (name, release, date)
