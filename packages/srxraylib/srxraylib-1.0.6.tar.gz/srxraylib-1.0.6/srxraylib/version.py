c246f28f9d74fc878c672fd7ab173cb211bdcf2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:52 +0000

    Add package srxraylib/1.0.6/srxraylib-1.0.6.tar.gz

---
+++ packages/srxraylib/srxraylib-1.0.6.tar.gz/srxraylib-1.0.6/srxraylib/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM OASYS SETUP.PY
+short_version = '1.0.6'
+version = '1.0.6'
+full_version = '1.0.6.dev0+9483aab'
+git_revision = '9483aab2b7ff5988db17b20e6b92abd03dc3afee'
+release = False
+
+if not release:
+    version = full_version
+    short_version += ".dev"
