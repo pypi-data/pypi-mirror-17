b36a2944445c524f1d6d86b1b5733952b423deef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:41 +0000

    Add package nesteringu/1.0.0/nesteringu-1.0.0.zip

---
+++ packages/nesteringu/nesteringu-1.0.0.zip/nesteringu-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+       name         = 'nesteringu',
+       version      = '1.0.0',
+       py_module    = ['nesteringu'],
+       author       = 'imeun0',
+       author_email = 'imeun0@gmail.com',
+       url          = 'http://toboost.me/moodle',
+       description  = 'A simple printer of nested list',
+       )
+
