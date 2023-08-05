af5a889ecb2505145156f3e35a2af74e3c3be173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:22 +0000

    Add package mojapi/0.1.0/mojapi-0.1.0.tar.gz

---
+++ packages/mojapi/mojapi-0.1.0.tar.gz/mojapi-0.1.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='UTF-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mojapi',
+    version='0.1.0',
+    url='https://github.com/zugmc/mojapi/',
+    author='gozug',
+    author_email='gozug@zugmc.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    download_url='https://github.com/zugmc/mojapi/tarball/0.1.0',
+    description='Python library for interacting with Mojang APIs',
+    long_description=long_description,
+    packages=find_packages(),
+    keywords=' '.join([
+        'minecraft',
+        'mojang',
+        'api'
+    ]),
+    install_requires=[
+        'requests>=2.11.1'
+    ]
+)
