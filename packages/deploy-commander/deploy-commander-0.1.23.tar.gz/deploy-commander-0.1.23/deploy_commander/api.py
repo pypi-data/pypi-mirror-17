4ed8bc5356763e03997df783dec0cdfefbef33be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:52 +0000

    Add package deploy-commander/0.1.23/deploy-commander-0.1.23.tar.gz

---
+++ packages/deploy-commander/deploy-commander-0.1.23.tar.gz/deploy-commander-0.1.23/deploy_commander/api.py
@@ -0,0 +1,323 @@
+# api.py
+import os
+import falcon
+import time
+import threading
+import subprocess
+import json
+import smtplib
+import subprocess
+import requests
+import re
+
+from os.path import isfile, join
+from os import listdir
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from fabric.api import env
+
+from config import init
+
+
+env.home_path = os.environ['DC_HOME_PATH']
+
+# Init default config
+init()
+
+def output_to_text(output, payload_data):
+    """
+    Format command line output to text
+    """
+    output = output.replace('[0m', '')
+    output = output.replace('[32m', '')
+    output = output.replace('[31m', '')
+    output = output.replace('[33m', '')
+    
+    return output
+
+def output_to_html(output, payload_data):
+    """
+    Format command line output to html
+    """
+    output = output.replace('[0m', '</span>')
+    output = output.replace('[32m', '<span style="color:green;">')
+    output = output.replace('[31m', '<span style="color:red;">')
+    output = output.replace('[33m', '<span style="color:yellow;">')
+        
+    output = output.replace('\n', '<br />')
+    
+    
+    params = {'output': output}
+    params.update(payload_data)
+    
+    html = """
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+           <html xmlns="http://www.w3.org/1999/xhtml">
+           <head>
+           <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+           <title>Deploy Commander</title>
+           <style type="text/css">
+           body{
+               background-color:black;
+               color:white;
+           }
+           </style>
+           <body>
+           <div style="float:right;position:absolute;right:20px;"><img src="%(user_avatar)s" /><br /><a href="%(user_home)s">%(author_name)s</a></div>
+           <div style="font-family: Monospace;white-space:pre;width:98ch;">%(output)s</div>'
+           </body>
+           </html>""" % params
+
+    return html
+
+def run_command(payload_data, project, environment):
+    try:
+        if env.has_key('hook') and 'slack_hook_url' in env['hook']:
+            payload = { "username": "DeployCommander",   "text": "Deployment start for project `%s` and environment `%s`" % (project, environment),   "icon_emoji": ":pray:" }
+            requests.post(env['hook']['slack_hook_url'], json.dumps(payload))
+    except:
+        pass
+
+    go = 'go:%(project)s,%(environment)s' % {'environment':environment,
+                                             'project':project}
+
+    command = os.path.join(os.environ['DC_VIRTUALENV_PATH'], 'bin', 'deploy-commander')
+    #execute = "(cd %s && (%s %s run:deploy-app))" % (os.environ['DC_HOME_PATH'], command, go)
+
+    run = 'run:deploy-app'
+
+
+    os.chdir(os.environ['DC_HOME_PATH'])
+    
+    # Make dir
+    output_dir = os.path.join(os.environ['DC_HOME_PATH'], 'logs', 'build', 'output')
+                     
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    output_path_file = os.path.join(output_dir, "%s.output" % int(time.time()))
+    
+    output_log = open(output_path_file, 'w+')
+
+
+    lockfile_dir = os.path.join(os.environ['DC_HOME_PATH'], 'lockfiles')
+    
+    if not os.path.exists(lockfile_dir):
+        os.makedirs(lockfile_dir)
+
+    lockfile_path_file =  os.path.join(lockfile_dir, ('%s-%s-%s.pid' % (project, environment, 'deploy-app')))
+
+    # For now just kill old process, and start a new one
+    if os.path.isfile(lockfile_path_file):
+        with open(lockfile_path_file) as log_file:
+            pid=log_file.read()
+        
+        print "Lockfile with pid#%s exists, kill process and remove file" % (pid)
+        try:
+            if env.has_key('hook') and 'slack_hook_url' in env['hook']:
+                payload = { "username": "DeployCommander",   "text": "Double deployment start for project `%s` and environment `%s`, process is killed." % (project, environment),   "icon_emoji": ":spock-hand:" }
+                requests.post(env['hook']['slack_hook_url'], json.dumps(payload))
+        except:
+            pass
+
+        kill = subprocess.Popen(['kill', pid], stdout=output_log, stderr=output_log)
+        kill.wait()
+
+
+    process = subprocess.Popen([command, go, run], stdout=output_log, stderr=output_log)
+
+    lock = open(lockfile_path_file, 'w+')
+    lock.write(str(process.pid));
+    lock.close()
+
+    print "PID:", process.pid
+    return_code = process.wait()
+    os.remove(lockfile_path_file)
+
+    # If process is killed, don't mail!
+    if return_code != -15:
+        with open(output_path_file) as raw_output_fp:
+            raw_output = raw_output_fp.read()
+            html_body  = output_to_html(raw_output, payload_data)
+            plain_body = output_to_text(raw_output, payload_data)
+            mail_log(html_body=html_body,
+                     plain_body=plain_body,
+                     error=process.returncode)
+            try:
+                if env.has_key('hook') and 'slack_hook_url' in env['hook']:
+                    if process.returncode:
+                        payload = { "username": "DeployCommander",   "text": "Failed deployment for project `%s` to environment `%s`." % (project, environment),   "icon_emoji": ":bangbang:" }
+                        requests.post(env['hook']['slack_hook_url'], json.dumps(payload))
+                    else:
+                        payload = { "username": "DeployCommander",   "text": "Project `%s` is deployed to environment `%s`." % (project, environment),   "icon_emoji": ":+1:" }
+                        requests.post(env['hook']['slack_hook_url'], json.dumps(payload))
+            except:
+                pass
+
+    # Cleanup log files
+    cleanup_folder(path=output_dir)
+
+def mail_log(html_body, plain_body, error=False):
+    # Make dir file html
+    html_dir = os.path.join(os.environ['DC_HOME_PATH'], 'logs', 'build', 'html')
+                     
+    if not os.path.exists(html_dir):
+        os.makedirs(html_dir)
+
+    html_log = open(os.path.join(html_dir, "%s.html" % int(time.time())), 'w+')
+    html_log.write(html_body)
+    html_log.close()
+    
+    
+    # Make dir file plain
+    text_dir = os.path.join(os.environ['DC_HOME_PATH'], 'logs', 'build', 'txt')
+                     
+    if not os.path.exists(text_dir):
+        os.makedirs(text_dir)
+
+    text_log = open(os.path.join(text_dir, "%s.txt" % int(time.time())), 'w+')
+    text_log.write(plain_body)
+    text_log.close()
+
+    if 'mail' in env and env['mail']:
+        """
+        When mail isset in the config it will use the settings 
+        for the smtp server
+        """
+        
+        mail_config = env.mail
+        
+        email_from = mail_config['from']
+        email_to = mail_config['to'][0]
+        
+        msg = MIMEMultipart('alternative')
+        msg['From'] = email_from
+        msg['To'] = email_to
+        
+        # Set important if there was an error!
+        if error:
+            msg['Subject'] = 'Deploy Commander failed task'
+            msg['X-Priority'] = '2'
+        else:
+            msg['Subject'] = 'Deploy Commander task executed'
+        
+        msg.attach(MIMEText(plain_body, 'text'))
+        msg.attach(MIMEText(html_body, 'html'))
+        
+        # Create sendmail
+        s = smtplib.SMTP(mail_config['host'], str(mail_config['port']))
+        s.login(mail_config['user'], mail_config['password'])
+        
+        s.sendmail(email_from, mail_config['to'], msg.as_string())
+        s.quit()
+    
+def cleanup_folder(path, max_files = 10):
+    """
+    Cleanup old files (logs)
+    """
+    onlyfiles = [ f for f in listdir(path) if isfile(join(path,f)) ]
+        
+    onlyfiles = sorted(onlyfiles, reverse=False)
+        
+    if (len(onlyfiles) > max_files):
+        for remove_file in onlyfiles[0:-max_files]:
+            os.remove(os.path.join(path, remove_file))
+
+class BitbucketWebhookResource:
+    def on_post(self, req, resp):
+        """Handles POST requests"""
+        # Try to read the payload
+        try:
+            raw_json = str(req.stream.read())
+        except Exception as ex:
+            raise falcon.HTTPError(falcon.HTTP_400, 'Error', ex.message)
+        
+
+        # Write log file
+        file = "%s.json" % int(time.time())
+        
+        dir = os.path.join(os.environ['DC_HOME_PATH'], 'logs', 'bitbucket', 'webhook')
+                     
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        
+        log_file = open(os.path.join(dir, file), 'w+')
+        log_file.write(str(raw_json))
+        log_file.close()
+        
+        data = json.loads(raw_json)
+        
+        payload_data = {}
+        if data.has_key('pullrequest'):
+            try:
+                payload_data = {'title':data['pullrequest']['title'],
+                                'description':data['pullrequest']['description'],
+                                'branch':data['pullrequest']['destination']['branch']['name'],
+                                'project':data['pullrequest']['destination']['repository']['name'],
+                                'user_avatar':data['pullrequest']['author']['links']['avatar']['href'],
+                                'user_home':data['pullrequest']['author']['links']['html']['href'],
+                                'author_name':data['pullrequest']['author']['display_name']}
+            except:
+                print("Invalid params?")
+            
+            if env.has_key('hook'):
+                hook_config = env.hook
+            else:
+                hook_config = {
+                    'environment_mapping':{'develop':'testing',
+                                           'release':'staging'}
+                }
+            
+            project = 'unknown'
+            environment = 'unknown'
+
+            if hook_config.has_key('environment_mapping'):
+                
+                for k, v in hook_config['environment_mapping'].iteritems():
+                    match = re.search(k, payload_data['branch'])
+                    
+                    if match:
+                        project     = payload_data['project']
+                        environment = v
+                         #Start deploy commander thread
+                        thread = threading.Thread(name='worker', target=run_command, args=(payload_data, project, environment))
+                        thread.start()
+                        continue
+
+            
+            # Cleanup log files
+            cleanup_folder(path=dir)
+
+        data_body = {
+            'status':'ok',
+            'payload_data':payload_data,
+            'project':project, 
+            'environment':environment
+        }
+        
+        body = json.dumps(data_body)
+        resp.status = falcon.HTTP_200  # This is the default status
+        resp.body = body
+
+
+class RootResource:
+    def on_get(self, req, resp):
+        """Handles root get requests"""
+        resp.status = falcon.HTTP_200  # This is the default status
+        resp.body = '{"status":"ok"}'
+
+# falcon.API instances are callable WSGI apps
+app = falcon.API()
+
+# new version of hook
+bitbucket_webhook =  BitbucketWebhookResource()
+
+root_resource = RootResource()
+
+# Bitbucket pull request post hook
+app.add_route('/api/v1/bitbucket/webhook', bitbucket_webhook)
+
+# Yust add a root responsive
+app.add_route('/', root_resource)
---
+++ packages/deploy-commander/deploy-commander-0.1.23.tar.gz/deploy-commander-0.1.23/deploy_commander/command/system.py
@@ -0,0 +1,269 @@
+""""
+System commands for deploy commander
+"""
+import os
+import utils
+import traceback
+import config
+
+from fabric.api import env
+from fabric.contrib.files import is_link
+from fabric.operations import run
+from fabric.operations import sudo
+from fabric.operations import get
+from fabric.operations import local
+from fabric.operations import put
+
+from fabric.utils import abort
+
+from fabric.colors import yellow
+from fabric.colors import green
+from fabric.colors import red
+from fabric.context_managers import cd
+from fabric.context_managers import settings
+from fabric.context_managers import hide
+
+def cleanup_old_files(params):
+    abort(red("Command `cleanup_old_files` is depricated, use `filesystem_remove_old`"))
+
+
+def filesystem_remove_old(params):
+    """
+    Delete old files and folders
+    """
+    params = utils.format_params(params)
+
+    if not 'minutes' in params:
+        params['minutes'] = 86400
+
+    if not 'path' in params:
+        abort('No path set')
+
+    with settings(warn_only=True):
+        run("find %s/* -maxdepth 0 -cmin +%s -exec rm -Rf {} \;" % (params['path'], params['minutes']))
+
+
+# BUG : command doesn't work!?
+# def filesystem_keep_latest(params):
+#     """
+#     Keep latest files and folders
+#     """
+#     params = utils.format_params(params)
+
+#     if not 'releases' in params:
+#         params['releases'] = 42
+
+#     if not 'path' in params:
+#         abort('No path set')
+
+#     if not 'name' in params:
+#         abort('No name set')
+
+#     with settings(warn_only=True):
+#         run("KEEPCOUNT=%s;find /projects/keeplatest -maxdepth 1 | grep %s | sort -rn | tail -n +$KEEPCOUNT+1 | while read folder; do rm -rf \"$folder\" ; done;" % (params['releases'], params['path'], params['name']))
+
+
+def symlink(params):
+    """
+    Create a symlink command.
+    """
+    params = utils.format_params(params)
+
+    if not 'source' in params:
+        abort('No source set')
+
+    if not 'target' in params:
+        abort('No target set')
+
+    if is_link(params['source']):
+        print(yellow("Symlink `%s` exists and will be removed" % params['source']))
+        run('rm %s' % params['source'])
+
+    command = "ln -s %s %s" % (params['target'], params['source'])
+    run(command)
+
+    print(green("Symlink from `%s` to `%s`." % (params['source'], params['target'])))
+
+
+def command(params):
+    """
+    Run a command
+    """
+    with hide('running'):
+        params = utils.format_params(params)
+
+        if not 'command' in params:
+            abort('No command set')
+
+        if 'use_sudo' in params:
+            sudo(params['command'])
+        else:
+            run(params['command'])
+
+    if 'secure' in params and params['secure'] == "True":
+        print(green("Secure command executed"))
+    else:
+        print(green("Command `%s` executed" % params['command']))
+        
+
+def multi_command(params):
+    """
+    Run a command multiple times, based on config
+    """
+    with hide('running'):
+        #params['command'] = utils.format_params(params, strict=False)
+        if not 'command' in params:
+            abort('No command set')
+
+        #Skip command, we'll format it later on.. but we'll need it for the list_config_file..
+        original_params = params.copy();
+        del params['command']
+
+        if not 'list_config_file' in params:
+            abort('No list_config_file set')
+      
+        utils.format_params(params=params)
+        
+        data_list = config.read_config(params['list_config_file'])
+
+        if data_list:
+            for data in data_list:
+                tmp_params = original_params.copy();
+                tmp_params = utils.format_params(params=tmp_params, merge_extra_params=data)
+                if 'use_sudo' in tmp_params:
+                    sudo(tmp_params['command'])
+                else:
+                    run(tmp_params['command'])
+
+                if 'secure' in params and params['secure'] == "True":
+                    print(green("Secure command %s executed" % original_params['command']))
+                else:
+                    print(green("Command `%s` executed" % tmp_params['command']))
+        else:
+            if 'secure' in params and params['secure'] == "True":
+                print(yellow("Command not executed, data list %s is empty" % (params['list_config_file'])))
+            else:
+                print(yellow("Command `%s` not executed, data list %s is empty" % (tmp_params['command'], params['list_config_file'])))
+                
+def multi_local_command(params):
+    """
+    Run a local command multiple times, based on config
+    """
+    with hide('running'):
+        if not 'command' in params:
+            abort('No command set')
+
+        #Skip command, we'll format it later on.. but we'll need it for the list_config_file..
+        original_params = params.copy();
+        del params['command']
+
+        if not 'list_config_file' in params:
+            abort('No list_config_file set')
+      
+        utils.format_params(params=params)
+        
+        data_list = config.read_config(params['list_config_file'])
+
+        if data_list:
+            for data in data_list:
+                tmp_params = original_params.copy();
+                tmp_params = utils.format_params(params=tmp_params, merge_extra_params=data)
+
+                local(tmp_params['command'])
+
+                if 'secure' in params and params['secure'] == "True":
+                    print(green("Secure command %s executed" % original_params['command']))
+                else:
+                    print(green("Command `%s` executed" % tmp_params['command']))
+        else:
+            if 'secure' in params and params['secure'] == "True":
+                print(yellow("Command not executed, data list %s is empty" % (params['list_config_file'])))
+            else:
+                print(yellow("Command `%s` not executed, data list %s is empty" % (tmp_params['command'], params['list_config_file'])))
+
+def ensure_path(params):
+    """
+    Ensure a certain path
+    """
+    params = utils.format_params(params)
+
+    if not 'path' in params:
+        abort('No path set')
+
+    utils.ensure_path(path=params['path'])
+
+    print(green("Ensure path `%s`." % (params['path'])))
+
+
+def download_from_remote(params):
+    """
+    Download folder to local path
+    """
+    params = utils.format_params(params)
+
+    if not 'remote_path' in params:
+        abort('No remote path set')
+
+    if not 'local_path' in params:
+        abort('No local path set')
+
+    print("Reading from `%s`" % params['remote_path'])
+    print("Target to `%s`" % params['local_path'])
+
+    try:
+        get(**params)
+    except Exception, e:
+        print(str(e))
+
+
+def upload_to_remote(params):
+    """
+    Upload files to remote
+    """
+    params = utils.format_params(params)
+
+    if not 'target_source' in params:
+        abort('No target_source set')
+
+    if not 'local_source' in params:
+        abort('No local_source set')
+
+    print("Reading from `%s`" % params['local_source'])
+    print("Target to `%s`" % params['target_source'])
+
+    try:
+        put(params['local_source'], params['target_source'])
+    except Exception, e:
+        print(str(e))
+
+
+def upload_template(params):
+    """
+    Upload a template and render it with the given params.
+    """
+
+    cwd = os.getcwd()
+
+    params = utils.format_params(params)
+
+    if not 'use_sudo' in params:
+        params['use_sudo'] = False
+
+    if 'use_sudo' in params:
+        use_sudo = params['use_sudo']
+    else:
+        use_sudo = False
+
+    current_path_template = "%s/template/%s" % (cwd, params['source'])
+
+    if not os.path.isfile(current_path_template):
+        print(yellow("No template `%s` found in current path. It will fallback to deploy commander defaults" % (current_path_template)))
+        template_dir = "%s/template" % os.environ['DEPLOY_COMMANDER_ROOT_PATH']
+    else:
+        template_dir = "%s/template" % cwd
+
+    utils.upload_template(params['source'], params['target'],
+                          use_sudo=use_sudo, use_jinja=True,
+                          context=params, template_dir=template_dir)
+
+    print(green("Upload template from `%s/%s` to `%s`." % (template_dir, params['source'], params['target'])))
