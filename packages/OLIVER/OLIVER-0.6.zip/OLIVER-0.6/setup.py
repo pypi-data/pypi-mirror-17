12d32f3756a786f38e1b51c5b9d48cdfc2d62d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package OLIVER/0.6/OLIVER-0.6.zip

---
+++ packages/OLIVER/OLIVER-0.6.zip/OLIVER-0.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+setup(
+    name = "OLIVER",
+    version = "0.6",
+    packages = ['oliver'],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['py4j','numpy','pandas'],
+
+    #package_data = {
+    #    # If any package contains *.txt or *.rst files, include them:
+    #    '': ['*.txt', '*.rst'],
+    #    # And include any *.msg files found in the 'hello' package, too:
+    #    'hello': ['*.msg'],
+    #},
+
+    # metadata for upload to PyPI
+    author = "Oliver Tessmer",
+    author_email = "oliver.tessmer@gmail.com",
+    description = "Convenience functions for interacting with the OLIVER workspace",
+    #license = "PSF",
+    keywords = "oliver",
+    url = "https://msu.edu/~tessmero/",   # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
+)
