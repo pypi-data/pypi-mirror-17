c9e526de7357e1c9ed8dfd369998af70a2e27ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:59 +0000

    Add package nesterino/1.3.0/nesterino-1.3.0.zip

---
+++ packages/nesterino/nesterino-1.3.0.zip/nesterino-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = "nesterino",
+        version = "1.3.0",
+        py_modules = ["nesterino"],
+        author = "Hiperd00",
+        author_email = "hiperd00@hotmail.com",
+        url = "",
+        description = "Improved printer for nested lists"
+)
