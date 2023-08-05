d21bf40223e883f3509618357b8d63db7cb3e0f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:05 +0000

    Add package niworkflows/0.0.3a2/niworkflows-0.0.3a2.tar.gz

---
+++ packages/niworkflows/niworkflows-0.0.3a2.tar.gz/niworkflows-0.0.3a2/niworkflows/anat/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: oesteban
+# @Date:   2016-07-21 10:47:37
+# @Last Modified by:   oesteban
+# @Last Modified time: 2016-07-21 11:53:28
+
+from niworkflows.anat.skullstrip import afni_wf as skullstrip_afni
