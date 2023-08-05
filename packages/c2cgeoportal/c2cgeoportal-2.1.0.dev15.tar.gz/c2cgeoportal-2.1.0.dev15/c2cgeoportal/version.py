5b422b88a0a9d4acfb70832ca21a5fe8c1504659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:18 +0000

    Add package c2cgeoportal/2.1.0.dev15/c2cgeoportal-2.1.0.dev15.tar.gz

---
+++ packages/c2cgeoportal/c2cgeoportal-2.1.0.dev15.tar.gz/c2cgeoportal-2.1.0.dev15/c2cgeoportal/version.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2016, Camptocamp SA
+# All rights reserved.
+
+# Auto-generated file. Do not Edit!
+INFO = {
+    "git_hash": "f7b23e3f3a81128acf0e266ab2c6146c5589bff3",
+    "version": "2.0",
+    "git_tag": "2.1.0.dev15",
+}
