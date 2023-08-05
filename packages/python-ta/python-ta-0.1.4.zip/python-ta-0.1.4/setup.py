fa38f5d575877d612cbaf53e943571a0298f49e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:05 +0000

    Add package python-ta/0.1.4/python-ta-0.1.4.zip

---
+++ packages/python-ta/python-ta-0.1.4.zip/python-ta-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    try:
+        import pypandoc
+    except ImportError:
+        return ''
+
+    return pypandoc.convert('README.md', 'rst')
+
+setup(
+    name='python-ta',
+    version='0.1.4',
+    description='Code checking tool for teaching Python',
+    long_description=readme(),
+    url='http://github.com/pyta-uoft/pyta',
+    author='David Liu',
+    author_email='david@cs.toronto.edu',
+    license='MIT',
+    packages=['pyta', 'pyta.reporters', 'pyta.checkers'],
+    install_requires=[
+        'pylint',
+        'colorama',
+        'six',
+        'jinja2'
+    ],
+    zip_safe=False)
