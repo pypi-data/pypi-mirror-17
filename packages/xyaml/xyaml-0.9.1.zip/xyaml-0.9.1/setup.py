f418f0b67f04519630593f48c05348ee3cec5d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:14 +0000

    Add package xyaml/0.9.1/xyaml-0.9.1.zip

---
+++ packages/xyaml/xyaml-0.9.1.zip/xyaml-0.9.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='xyaml',
+    version='0.9.1',
+    packages=[''],
+    url='https://github.com/bsimpson888/xyaml',
+    license='GPL',
+    author='Marco Bartel',
+    author_email='bsimpson888@gmail.com',
+    description='xyaml is a extension to PyYAML which allows you to include child yaml files.'
+)
