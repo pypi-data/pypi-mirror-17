df1194fa7f58c907dabc8e65bbca6f6ae5994d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:22 +0000

    Add package businesstime/0.2.1/businesstime-0.2.1.tar.gz

---
+++ packages/businesstime/businesstime-0.2.1.tar.gz/businesstime-0.2.1/businesstime/test/holidays/aus_test.py
@@ -0,0 +1,32 @@
+from datetime import date
+import unittest
+
+from businesstime.holidays.aus import QueenslandPublicHolidays, BrisbanePublicHolidays
+
+
+class QueenslandPublicHolidaysTest(unittest.TestCase):
+
+    def test_2016_08(self):
+        holidays_gen = QueenslandPublicHolidays()
+        self.assertEqual(
+            list(holidays_gen(date(2016, 8, 1), end=date(2016, 8, 31))),
+            []
+        )
+
+
+class BrisbanePublicHolidaysTest(unittest.TestCase):
+
+    def test_2016_08(self):
+        holidays_gen = BrisbanePublicHolidays()
+        self.assertEqual(
+            list(holidays_gen(date(2016, 8, 1), end=date(2016, 8, 31))),
+            [
+                date(2016, 8, 10)
+            ]
+        )
+
+    def test_out_of_range(self):
+        holidays_gen = BrisbanePublicHolidays()
+        def test():
+            return list(holidays_gen(date(2017, 1, 1), end=date(2017, 12, 31)))
+        self.assertRaises(NotImplementedError, test)
