52f4429271e3713fdefef3020bfcbec2e139304f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:38 +0000

    Add package boofuzz/0.0.5/boofuzz-0.0.5.tar.gz

---
+++ packages/boofuzz/boofuzz-0.0.5.tar.gz/boofuzz-0.0.5/boofuzz/sex.py
@@ -0,0 +1,25 @@
+# Sulley EXception Class
+
+
+class BoofuzzError(Exception):
+    pass
+
+
+class BoofuzzRestartFailedError(BoofuzzError):
+    pass
+
+
+class BoofuzzTargetConnectionFailedError(BoofuzzError):
+    pass
+
+
+class SullyRuntimeError(Exception):
+    pass
+
+
+class SizerNotUtilizedError(Exception):
+    pass
+
+
+class MustImplementException(Exception):
+    pass
