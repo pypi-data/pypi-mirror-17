a83ca75b6f3fbaed5cdd1a7992ee33d024954a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:54 +0000

    Add package anycingnesterprint/1.2.0/anycingnesterprint-1.2.0.zip

---
+++ packages/anycingnesterprint/anycingnesterprint-1.2.0.zip/anycingnesterprint-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'anycingnesterprint',
+    version = '1.2.0',
+    py_modules = ['anycingnesterprint'],
+    author = 'anycing',
+    author_email = 'anycing@gmail.com',
+    url = 'http://www.anycing.com',
+    description = 'A simple printer of nested list',
+)
