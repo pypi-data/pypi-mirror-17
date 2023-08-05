bc549e17e3c4daa5c93a9929fb5c22e58af0520f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:24 +0000

    Add package yujia_nester/1.0.0/yujia_nester-1.0.0.zip

---
+++ packages/yujia_nester/yujia_nester-1.0.0.zip/yujia_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'yujia_nester',
+    version = '1.0.0',
+    py_modules = ['yujia_nester'],
+    author = 'hfpython',
+    author_email = 'hfpython@headfirstlabs.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple prinster of nested libts',
+    )
