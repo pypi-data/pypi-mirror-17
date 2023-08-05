2f079053e366d0860e5d13c8b619f924ea150e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:06 +0000

    Add package djangocms-vimeo/0.4.0/djangocms-vimeo-0.4.0.tar.gz

---
+++ packages/djangocms-vimeo/djangocms-vimeo-0.4.0.tar.gz/djangocms-vimeo-0.4.0/djangocms_vimeo/tests.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from cms.test_utils.testcases import CMSTestCase
+
+
+class VimeoTestCase(CMSTestCase):
+
+    def setUp(self):
+        pass
