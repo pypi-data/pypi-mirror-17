fea18fb9c2f86f4a27240f341d43ae7989d2ab0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:11 +0000

    Add package pyblog/0.2/pyblog-0.2.tar.gz

---
+++ packages/pyblog/pyblog-0.2.tar.gz/pyblog-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='pyblog',
+      version='0.2',
+      description='Dirt-simple static blog general',
+      url='http://github.com/cesarparent/pyblog',
+      author='Cesar Parent',
+      author_email='cesar@cesarparent.com',
+      license='MIT',
+      packages=['pyblog'],
+      install_requires=[
+          'markdown',
+          'jinja2'
+      ],
+      entry_points = {
+          'console_scripts': [
+              'pyblog-init = pyblog.cli:pyblog_init',
+              'pyblog-build = pyblog.cli:pyblog_build'
+          ],
+      },
+      zip_safe=False)
