369bf8f4b72d0f77365a9068757a5e04cf839eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:44 +0000

    Add package mir.winenv/0.3.0/mir.winenv-0.3.0.tar.gz

---
+++ packages/mir.winenv/mir.winenv-0.3.0.tar.gz/mir.winenv-0.3.0/setup.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2016  Allen Li
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mir.winenv',
+    version='0.3.0',
+    description='Manage Wine environments',
+    long_description='',
+    keywords='',
+    url='https://github.com/darkfeline/mir.winenv',
+    author='Allen Li',
+    author_email='darkfeline@felesatra.moe',
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'winenv = mir.winenv.__main__:main',
+        ],
+    },
+)
