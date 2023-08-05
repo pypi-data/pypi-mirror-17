c4250dfa28a82321030636c3350e2eb6eb25c9ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:46 +0000

    Add package invenio-indexer/1.0.0a7/invenio-indexer-1.0.0a7.tar.gz

---
+++ packages/invenio-indexer/invenio-indexer-1.0.0a7.tar.gz/invenio-indexer-1.0.0a7/RELEASE-NOTES.rst
@@ -0,0 +1,36 @@
+==========================
+ Invenio-Indexer v1.0.0a7
+==========================
+
+Invenio-Indexer v1.0.0a7 was released on September 2, 2016.
+
+About
+-----
+
+Indexer for Invenio.
+
+*This is an experimental developer preview release.*
+
+What's new
+----------
+
+- Initial public release.
+
+Installation
+------------
+
+   $ pip install invenio-indexer==1.0.0a7
+
+Documentation
+-------------
+
+   http://pythonhosted.org/invenio-indexer/
+
+Happy hacking and thanks for flying Invenio-Indexer.
+
+| Invenio Development Team
+|   Email: info@inveniosoftware.org
+|   IRC: #invenio on irc.freenode.net
+|   Twitter: http://twitter.com/inveniosoftware
+|   GitHub: https://github.com/inveniosoftware/invenio-indexer
+|   URL: http://inveniosoftware.org
---
+++ packages/invenio-indexer/invenio-indexer-1.0.0a7.tar.gz/invenio-indexer-1.0.0a7/invenio_indexer/ext.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2016 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Flask exension for Invenio-Indexer."""
+
+from __future__ import absolute_import, print_function
+
+from flask import current_app
+from werkzeug.utils import cached_property, import_string
+
+from . import config
+from .cli import run  # noqa
+
+
+class InvenioIndexer(object):
+    """Invenio-Indexer extension."""
+
+    def __init__(self, app=None):
+        """Extension initialization.
+
+        :param app: The Flask application. (Default: ``None``)
+        """
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Flask application initialization.
+
+        :param app: The Flask application.
+        """
+        self.init_config(app)
+        app.extensions['invenio-indexer'] = self
+
+    def init_config(self, app):
+        """Initialize configuration.
+
+        :param app: The Flask application.
+        """
+        for k in dir(config):
+            if k.startswith('INDEXER_'):
+                app.config.setdefault(k, getattr(config, k))
+
+    @cached_property
+    def record_to_index(self):
+        """Import the configurable 'record_to_index' function."""
+        return import_string(current_app.config.get('INDEXER_RECORD_TO_INDEX'))
