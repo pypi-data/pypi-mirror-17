5c41ed23ef3e738fd66ff51cf821ca323a164de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:51 +0000

    Add package spectral-cube/0.4.0/spectral-cube-0.4.0.tar.gz

---
+++ packages/spectral-cube/spectral-cube-0.4.0.tar.gz/spectral-cube-0.4.0/spectral_cube/cube_utils.py
@@ -0,0 +1,311 @@
+from __future__ import print_function, absolute_import, division
+
+import contextlib
+import warnings
+try:
+    import builtins
+except ImportError:
+    # python2
+    import __builtin__ as builtins
+
+import numpy as np
+from astropy.wcs import (WCSSUB_SPECTRAL, WCSSUB_LONGITUDE, WCSSUB_LATITUDE)
+from . import wcs_utils
+from astropy import log
+from astropy.io.fits import BinTableHDU, Column
+from astropy import units as u
+import itertools
+
+def _fix_spectral(wcs):
+    """
+    Attempt to fix a cube with an invalid spectral axis definition.  Only uses
+    well-known exceptions, e.g. CTYPE = 'VELOCITY'.  For the rest, it will try
+    to raise a helpful error.
+    """
+
+    axtypes = wcs.get_axis_types()
+
+    types = [a['coordinate_type'] for a in axtypes]
+
+    if wcs.naxis not in (3,4):
+        raise TypeError("The WCS has {0} axes of types {1}".format(len(types),
+                                                                   types))
+
+    # sanitize noncompliant headers
+    if 'spectral' not in types:
+        log.warning("No spectral axis found; header may be non-compliant.")
+        for ind,tp in enumerate(types):
+            if tp not in ('celestial','stokes'):
+                if wcs.wcs.ctype[ind] in wcs_utils.bad_spectypes_mapping:
+                    wcs.wcs.ctype[ind] = wcs_utils.bad_spectypes_mapping[wcs.wcs.ctype[ind]]
+
+    return wcs
+
+def _split_stokes(array, wcs):
+    """
+    Given a 4-d data cube with 4-d WCS (spectral cube + stokes) return a
+    dictionary of data and WCS objects for each Stokes component
+
+    Parameters
+    ----------
+    array : `~numpy.ndarray`
+        The input 3-d array with two position dimensions, one spectral
+        dimension, and a Stokes dimension.
+    wcs : `~astropy.wcs.WCS`
+        The input 3-d WCS with two position dimensions, one spectral
+        dimension, and a Stokes dimension.
+    """
+
+    if array.ndim not in (3,4):
+        raise ValueError("Input array must be 3- or 4-dimensional for a"
+                         " STOKES cube")
+
+    if wcs.wcs.naxis != 4:
+        raise ValueError("Input WCS must be 4-dimensional for a STOKES cube")
+
+    wcs = _fix_spectral(wcs)
+
+    # reverse from wcs -> numpy convention
+    axtypes = wcs.get_axis_types()[::-1]
+
+    types = [a['coordinate_type'] for a in axtypes]
+
+    try:
+        # Find stokes dimension
+        stokes_index = types.index('stokes')
+    except ValueError:
+        # stokes not in list, but we are 4d
+        if types.count('celestial') == 2 and types.count('spectral') == 1:
+            if None in types:
+                stokes_index = types.index(None)
+                log.warning("FITS file has no STOKES axis, but it has a blank"
+                         " axis type at index {0} that is assumed to be "
+                         "stokes.".format(4-stokes_index))
+            else:
+                for ii,tp in enumerate(types):
+                    if tp not in ('celestial', 'spectral'):
+                        stokes_index = ii
+                        stokes_type = tp
+
+                log.warning("FITS file has no STOKES axis, but it has an axis"
+                         " of type {1} at index {0} that is assumed to be "
+                         "stokes.".format(4-stokes_index, stokes_type))
+        else:
+            raise IOError("There are 4 axes in the data cube but no STOKES "
+                          "axis could be identified")
+
+    # TODO: make the stokes names more general
+    stokes_names = ["I", "Q", "U", "V"]
+
+    stokes_arrays = {}
+
+    wcs_slice = wcs_utils.drop_axis(wcs, wcs.naxis - 1 - stokes_index)
+
+    if array.ndim == 4:
+        for i_stokes in range(array.shape[stokes_index]):
+
+            array_slice = [i_stokes if idim == stokes_index else slice(None)
+                           for idim in range(array.ndim)]
+
+            stokes_arrays[stokes_names[i_stokes]] = array[array_slice]
+    else:
+        # 3D array with STOKES as a 4th header parameter
+        stokes_arrays['I'] = array
+
+    return stokes_arrays, wcs_slice
+
+
+def _orient(array, wcs):
+    """
+    Given a 3-d spectral cube and WCS, swap around the axes so that the
+    spectral axis cube is the first in Numpy notation, and the last in WCS
+    notation.
+
+    Parameters
+    ----------
+    array : `~numpy.ndarray`
+        The input 3-d array with two position dimensions and one spectral
+        dimension.
+    wcs : `~astropy.wcs.WCS`
+        The input 3-d WCS with two position dimensions and one spectral
+        dimension.
+    """
+
+    if array.ndim != 3:
+        raise ValueError("Input array must be 3-dimensional")
+
+    if wcs.wcs.naxis != 3:
+        raise ValueError("Input WCS must be 3-dimensional")
+
+    wcs = wcs_utils.diagonal_wcs_to_cdelt(_fix_spectral(wcs))
+
+    # reverse from wcs -> numpy convention
+    axtypes = wcs.get_axis_types()[::-1]
+
+    types = [a['coordinate_type'] for a in axtypes]
+
+    nums = [None if a['coordinate_type'] != 'celestial' else a['number']
+            for a in axtypes]
+
+    if 'stokes' in types:
+        raise ValueError("Input WCS should not contain stokes")
+
+    t = [types.index('spectral'), nums.index(1), nums.index(0)]
+    result_array = array.transpose(t)
+
+    result_wcs = wcs.sub([WCSSUB_LONGITUDE, WCSSUB_LATITUDE, WCSSUB_SPECTRAL])
+
+    return result_array, result_wcs
+
+
+def slice_syntax(f):
+    """
+    This decorator wraps a function that accepts a tuple of slices.
+
+    After wrapping, the function acts like a property that accepts
+    bracket syntax (e.g., p[1:3, :, :])
+
+    Parameters
+    ----------
+    f : function
+    """
+
+    def wrapper(self):
+        result = SliceIndexer(f, self)
+        result.__doc__ = f.__doc__
+        return result
+
+    wrapper.__doc__ = slice_doc.format(f.__doc__ or '',
+                                       f.__name__)
+
+    result = property(wrapper)
+    return result
+
+slice_doc = """
+{0}
+
+Notes
+-----
+Supports efficient Numpy slice notation,
+like ``{1}[0:3, :, 2:4]``
+"""
+
+
+class SliceIndexer(object):
+
+    def __init__(self, func, _other):
+        self._func = func
+        self._other = _other
+
+    def __getitem__(self, view):
+        return self._func(self._other, view)
+
+    def __iter__(self):
+        raise Exception("You need to specify a slice (e.g. ``[:]`` or "
+                        "``[0,:,:]`` in order to access this property.")
+
+
+# TODO: make this into a proper configuration item
+# TODO: make threshold depend on memory?
+MEMORY_THRESHOLD=1e8
+
+def is_huge(cube):
+    if cube.size < MEMORY_THRESHOLD:  # smallish
+        return False
+    else:
+        return True
+
+
+def iterator_strategy(cube, axis=None):
+    """
+    Guess the most efficient iteration strategy
+    for iterating over a cube, given its size and layout
+
+    Parameters
+    ----------
+    cube : SpectralCube instance
+        The cube to iterate over
+    axis : [0, 1, 2]
+        For reduction methods, the axis that is
+        being collapsed
+
+    Returns
+    -------
+    strategy : ['cube' | 'ray' | 'slice']
+        The recommended iteration strategy.
+        *cube* recommends working with the entire array in memory
+        *slice* recommends working with one slice at a time
+        *ray*  recommends working with one ray at a time
+    """
+    # pretty simple for now
+    if cube.size < 1e8:  # smallish
+        return 'cube'
+    return 'slice'
+
+
+def beams_to_bintable(beams):
+    """
+    Convert a list of beams to a CASA-style BinTableHDU
+    """
+
+    c1 = Column(name='BMAJ', format='1E', array=[bm.major.to(u.arcsec).value for bm in beams], unit=u.arcsec.to_string('FITS'))
+    c2 = Column(name='BMIN', format='1E', array=[bm.minor.to(u.arcsec).value for bm in beams], unit=u.arcsec.to_string('FITS'))
+    c3 = Column(name='BPA', format='1E', array=[bm.pa.to(u.deg).value for bm in beams], unit=u.deg.to_string('FITS'))
+    c4 = Column(name='CHAN', format='1J', array=[bm.meta['CHAN'] if 'CHAN' in bm.meta else 0 for bm in beams])
+    c5 = Column(name='POL', format='1J', array=[bm.meta['POL'] if 'POL' in bm.meta else 0 for bm in beams])
+
+    bmhdu = BinTableHDU.from_columns([c1, c2, c3, c4, c5])
+    bmhdu.header['EXTNAME'] = 'BEAMS'
+    bmhdu.header['EXTVER'] = 1
+    bmhdu.header['XTENSION'] = 'BINTABLE'
+    bmhdu.header['NCHAN'] = len(beams)
+    bmhdu.header['NPOL'] = len(set([bm.meta['POL'] for bm in beams]))
+    return bmhdu
+
+def average_beams(beams, includemask=None):
+    """
+    Average the beam major, minor, and PA attributes.
+
+    This is usually a dumb thing to do!
+    """
+
+    from radio_beam import Beam
+    from astropy.stats import circmean
+
+    if includemask is None:
+        includemask = itertools.cycle([True])
+
+    major = u.Quantity([bm.major for bm,incl in zip(beams,includemask) if incl], u.deg)
+    minor = u.Quantity([bm.minor for bm,incl in zip(beams,includemask) if incl], u.deg)
+    pa = u.Quantity([bm.pa for bm,incl in zip(beams,includemask) if incl], u.deg)
+    new_beam = Beam(major=major.mean(), minor=minor.mean(),
+                    pa=circmean(pa, weights=major/minor))
+
+    return new_beam
+
+@contextlib.contextmanager
+def _map_context(numcores):
+    """
+    Mapping context manager to allow parallel mapping or regular mapping
+    depending on the number of cores specified.
+    """
+    if numcores is not None and numcores > 1:
+        try:
+            import multiprocessing
+            p = multiprocessing.Pool(processes=numcores)
+            map = p.map
+            parallel = True
+        except ImportError:
+            map = builtins.map
+            warnings.warn("Could not import multiprocessing.  "
+                          "map will be non-parallel.")
+            parallel = False
+    else:
+        parallel = False
+        map = builtins.map
+
+    try:
+        yield map
+    finally:
+        if parallel:
+            p.close()
---
+++ packages/spectral-cube/spectral-cube-0.4.0.tar.gz/spectral-cube-0.4.0/spectral_cube/lower_dimensional_structures.py
@@ -0,0 +1,336 @@
+from __future__ import print_function, absolute_import, division
+
+import warnings
+from astropy import units as u
+from astropy import wcs
+from astropy.io.fits import Header, Card, HDUList, PrimaryHDU
+from .io.core import determine_format
+from . import spectral_axis
+
+import numpy as np
+
+from .base_class import BaseNDClass, SpectralAxisMixinClass
+from .cube_utils import beams_to_bintable
+
+__all__ = ['LowerDimensionalObject', 'Projection', 'Slice', 'OneDSpectrum']
+
+
+class LowerDimensionalObject(u.Quantity, BaseNDClass):
+    """
+    Generic class for 1D and 2D objects.
+    """
+
+    @property
+    def header(self):
+        header = self._header
+        # This inplace update is OK; it's not bad to overwrite WCS in this
+        # header
+        if self.wcs is not None:
+            header.update(self.wcs.to_header())
+        header['BUNIT'] = self.unit.to_string(format='fits')
+        for keyword in header:
+            if 'NAXIS' in keyword:
+                del header[keyword]
+        header.insert(2, Card(keyword='NAXIS', value=self.ndim))
+        for ind,sh in enumerate(self.shape[::-1]):
+            header.insert(3+ind, Card(keyword='NAXIS{0:1d}'.format(ind+1),
+                                      value=sh))
+
+        if 'beam' in self.meta:
+            header.update(self.meta['beam'].to_header_keywords())
+
+        return header
+
+    @property
+    def hdu(self):
+        if self.wcs is None:
+            hdu = PrimaryHDU(self.value)
+        else:
+            hdu = PrimaryHDU(self.value, header=self.wcs.to_header())
+        hdu.header['BUNIT'] = self.unit.to_string(format='fits')
+
+        if 'beam' in self.meta:
+            hdu.header.update(self.meta['beam'].to_header_keywords())
+
+        return hdu
+
+    def write(self, filename, format=None, overwrite=False):
+        """
+        Write the lower dimensional object to a file.
+
+        Parameters
+        ----------
+        filename : str
+            The path to write the file to
+        format : str
+            The kind of file to write. (Currently limited to 'fits')
+        overwrite : bool
+            If True, overwrite ``filename`` if it exists
+        """
+        if format is None:
+            format = determine_format(filename)
+        if format == 'fits':
+            self.hdu.writeto(filename, clobber=overwrite)
+        else:
+            raise ValueError("Unknown format '{0}' - the only available "
+                             "format at this time is 'fits'")
+
+    def to(self, unit, equivalencies=[]):
+        """
+        Return a new `LowerDimensionalObject` of the same class with the
+        specified unit.
+
+        See `astropy.units.Quantity.to` for further details.
+        """
+        converted_array = u.Quantity.to(self, unit,
+                                        equivalencies=equivalencies).value
+
+        # use private versions of variables, not the generated property
+        # versions
+        # Not entirely sure the use of __class__ here is kosher, but we do want
+        # self.__class__, not super()
+        new = self.__class__(value=converted_array, unit=unit, copy=True,
+                             wcs=self._wcs, meta=self._meta, mask=self._mask,
+                             header=self._header)
+
+        return new
+
+    def __getitem__(self, key, **kwargs):
+        """
+        Return a new ``LowerDimensionalObject'' of the same class while keeping
+        other properties fixed.
+        """
+        new_qty = super(LowerDimensionalObject, self).__getitem__(key)
+
+        if new_qty.ndim < 2:
+            # do not return a projection
+            return u.Quantity(new_qty)
+
+        if self._wcs is not None:
+            newwcs = self._wcs[key]
+        else:
+            newwcs = None
+
+        new = self.__class__(value=new_qty.value,
+                             unit=new_qty.unit,
+                             copy=False,
+                             wcs=newwcs,
+                             meta=self._meta,
+                             mask=self._mask,
+                             header=self._header,
+                             **kwargs)
+
+        return new
+
+    def __array_finalize__(self, obj):
+        self._wcs = getattr(obj, '_wcs', None)
+        self._meta = getattr(obj, '_meta', None)
+        self._mask = getattr(obj, '_mask', None)
+        self._header = getattr(obj, '_header', None)
+        super(LowerDimensionalObject, self).__array_finalize__(obj)
+
+    @property
+    def __array_priority__(self):
+        return super(LowerDimensionalObject, self).__array_priority__*2
+
+
+class Projection(LowerDimensionalObject):
+
+    def __new__(cls, value, unit=None, dtype=None, copy=True, wcs=None,
+                meta=None, mask=None, header=None):
+
+        if np.asarray(value).ndim != 2:
+            raise ValueError("value should be a 2-d array")
+
+        if wcs is not None and wcs.wcs.naxis != 2:
+            raise ValueError("wcs should have two dimension")
+
+        self = u.Quantity.__new__(cls, value, unit=unit, dtype=dtype,
+                                  copy=copy).view(cls)
+        self._wcs = wcs
+        self._meta = {} if meta is None else meta
+        self._mask = mask
+        if header is not None:
+            self._header = header
+        else:
+            self._header = Header()
+
+        return self
+
+
+    def quicklook(self, filename=None, use_aplpy=True):
+        """
+        Use `APLpy <https://pypi.python.org/pypi/APLpy>`_ to make a quick-look
+        image of the projection. This will make the ``FITSFigure`` attribute
+        available.
+
+        If there are unmatched celestial axes, this will instead show an image
+        without axis labels.
+
+        Parameters
+        ----------
+        filename : str or Non
+            Optional - the filename to save the quicklook to.
+        """
+        if use_aplpy:
+            try:
+                if not hasattr(self, 'FITSFigure'):
+                    import aplpy
+                    self.FITSFigure = aplpy.FITSFigure(self.hdu)
+
+                self.FITSFigure.show_grayscale()
+                self.FITSFigure.add_colorbar()
+                if filename is not None:
+                    self.FITSFigure.save(filename)
+            except (wcs.InconsistentAxisTypesError, ImportError):
+                self._quicklook_mpl(filename=filename)
+        else:
+            self._quicklook_mpl(filename=filename)
+
+    def _quicklook_mpl(self, filename=None):
+        from matplotlib import pyplot
+        self.figure = pyplot.imshow(self.value)
+        if filename is not None:
+            self.figure.savefig(filename)
+
+# A slice is just like a projection in every way
+class Slice(Projection):
+    pass
+
+
+class OneDSpectrum(LowerDimensionalObject,SpectralAxisMixinClass):
+
+    def __new__(cls, value, unit=None, dtype=None, copy=True, wcs=None,
+                meta=None, mask=None, header=None, spectral_unit=None,
+                beams=None):
+
+        if np.asarray(value).ndim != 1:
+            raise ValueError("value should be a 1-d array")
+
+        if wcs is not None and wcs.wcs.naxis != 1:
+            raise ValueError("wcs should have two dimension")
+
+        self = u.Quantity.__new__(cls, value, unit=unit, dtype=dtype,
+                                  copy=copy).view(cls)
+        self._wcs = wcs
+        self._meta = {} if meta is None else meta
+        self._mask = mask
+        if header is not None:
+            self._header = header
+        else:
+            self._header = Header()
+
+        self._spectral_unit = spectral_unit
+
+        if spectral_unit is None:
+            if 'CUNIT1' in self._header:
+                self._spectral_unit = u.Unit(self._header['CUNIT1'])
+            elif self._wcs is not None:
+                self._spectral_unit = u.Unit(self._wcs.wcs.cunit[0])
+
+        if beams is not None:
+            self.beams = beams
+
+        return self
+
+
+    @property
+    def header(self):
+        header = self._header
+        # This inplace update is OK; it's not bad to overwrite WCS in this
+        # header
+        if self.wcs is not None:
+            header.update(self.wcs.to_header())
+        header['BUNIT'] = self.unit.to_string(format='fits')
+        header.insert(2, Card(keyword='NAXIS', value=self.ndim))
+        for ind,sh in enumerate(self.shape[::-1]):
+            header.insert(3+ind, Card(keyword='NAXIS{0:1d}'.format(ind+1),
+                                      value=sh))
+
+        # Preserve the spectrum's spectral units
+        if self._spectral_unit != u.Unit(header['CUNIT1']):
+            spectral_scale = spectral_axis.wcs_unit_scale(self._spectral_unit)
+            header['CDELT1'] *= spectral_scale
+            header['CRVAL1'] *= spectral_scale
+            header['CUNIT1'] = self.unit.to_string(format='FITS')
+
+        return header
+
+
+    @property
+    def spectral_axis(self):
+        """
+        A `~astropy.units.Quantity` array containing the central values of
+        each channel along the spectral axis.
+        """
+
+        if self._wcs is None:
+            spec_axis = np.arange(self.size) * u.dimensionless_unscaled
+        else:
+            spec_axis = self.wcs.wcs_pix2world(np.arange(self.size), 0)[0] * \
+                u.Unit(self.wcs.wcs.cunit[0])
+            if self._spectral_unit is not None:
+                spec_axis = spec_axis.to(self._spectral_unit)
+
+        return spec_axis
+
+    def quicklook(self, filename=None, drawstyle='steps-mid', **kwargs):
+        """
+        Plot the spectrum with current spectral units in the currently open
+        figure
+
+        kwargs are passed to `matplotlib.pyplot.plot`
+
+        Parameters
+        ----------
+        filename : str or Non
+            Optional - the filename to save the quicklook to.
+        """
+        from matplotlib import pyplot
+        ax = pyplot.gca()
+        ax.plot(self.spectral_axis, self.value, drawstyle=drawstyle, **kwargs)
+        ax.set_xlabel(self.wcs.wcs.cunit[0])
+        ax.set_ylabel(self.unit)
+        if filename is not None:
+            pyplot.gcf().savefig(filename)
+
+
+    def with_spectral_unit(self, unit, velocity_convention=None,
+                           rest_value=None):
+
+        newwcs, newmeta = self._new_spectral_wcs(unit,
+                                                 velocity_convention=velocity_convention,
+                                                 rest_value=rest_value)
+
+        newheader = self._nowcs_header.copy()
+        newheader['CUNIT1'] = unit.to_string(format='FITS')
+
+
+        return OneDSpectrum(value=self.value, unit=self.unit, wcs=newwcs,
+                            header=newheader, meta=newmeta, copy=False,
+                            spectral_unit=unit)
+
+    def __getitem__(self, key, **kwargs):
+        try:
+            beams = self.beams[key]
+        except (AttributeError,TypeError):
+            beams = None
+
+        return super(OneDSpectrum, self).__getitem__(key, beams=beams)
+
+    @property
+    def hdu(self):
+        if hasattr(self, 'beams'):
+            warnings.warn("There are multiple beams for this spectrum that "
+                          "are being ignored when creating the HDU.")
+        return super(OneDSpectrum, self).hdu
+
+    @property
+    def hdulist(self):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            hdu = self.hdu
+
+        beamhdu = beams_to_bintable(self.beams)
+
+        return HDUList([hdu, beamhdu])
