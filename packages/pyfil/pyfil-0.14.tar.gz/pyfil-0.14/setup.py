49e2a92b710926a8b70291ba55060109aa0703ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:00 +0000

    Add package pyfil/0.14/pyfil-0.14.tar.gz

---
+++ packages/pyfil/pyfil-0.14.tar.gz/pyfil-0.14/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+try:
+    from urllib import request
+except ImportError:
+    import urllib2 as request
+
+fastep = request.urlopen('https://raw.githubusercontent.com/ninjaaron/fast-entry_points/master/fastentrypoints.py')
+namespace = {}
+exec(fastep.read(), namespace)
+
+setup(
+    name='pyfil',
+    version='0.14',
+    author='Aaron Christianson',
+    license='BSD',
+    author_email='ninjaaron@gmail.com',
+    url='https://github.com/ninjaaron/pyfil',
+    description='Python one-liners in the shell in the spirit of Perl and AWK',
+    long_description=open('README.rst').read(),
+    keywords='evaluate',
+    packages=['pyfil'],
+    entry_points={'console_scripts': ['rep=pyfil.pyfil:main']},
+)
