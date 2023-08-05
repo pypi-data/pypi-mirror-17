fdf15d3e8a3f71d3abd0d14b39a0b11a12c7760a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:01 +0000

    Add package zhyuoct_nester/1.0.1/zhyuoct_nester-1.0.1.tar.gz

---
+++ packages/zhyuoct_nester/zhyuoct_nester-1.0.1.tar.gz/zhyuoct_nester-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name = 'zhyuoct_nester',
+		version = '1.0.1',
+		py_modules = ['nester'],
+		author = 'zy',
+		author_email = 'zhyu.oct@hotmail.com',
+		url = "",
+		description = 'A simple printer of nested lists',
+	)
