115f262a64807b5d993c8c13fff12e238e7a0a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:57 +0000

    Add package searchandreco/1.7.dev1/searchandreco-1.7.dev1.tar.gz

---
+++ packages/searchandreco/searchandreco-1.7.dev1.tar.gz/searchandreco-1.7.dev1/searchandreco/eswrapper/settings.py
@@ -0,0 +1,50 @@
+__author__ = 'monica'
+
+## Import for instantiating elastic search via elasticsearch-py client library
+## (elasticsearch==1.4.0)
+
+from elasticsearch import Elasticsearch
+
+# Different nodes of Elastic Search
+
+ES_NODE1 = '10.2.3.xx'
+
+ES_NODE2 = '10.2.3.xxx'
+
+ES_NODE3 = '10.2.3.xxx'
+
+ES_NODE1_PORT = '8080'
+
+ES_NODE2_PORT = '8080'
+
+ES_NODE3_PORT = '8080'
+
+
+USE_LOCAL = True
+
+UWSGI_SETUP = False
+
+
+GP_STOPWORDS_FILE_NAME = "/mnt/apps/repo/searchandreco/searchandreco/hindi_stpwords.txt"
+
+SUB_LOG_FILE_NAME = '/mnt/logs/esapi/vbsub.log'
+GP_SUB_LOG_FILE_NAME = '/mnt/logs/esapi/gpsub.log'
+
+try:
+    from local_settings import *
+
+except:
+    pass
+
+
+ES_HOSTS = [{'host': ES_NODE1, 'port': ES_NODE1_PORT}, {'host': ES_NODE2, 'port': ES_NODE2_PORT}, \
+            {'host': ES_NODE3, 'port': ES_NODE3_PORT}]
+
+
+    ## A little code which instantiates ElasticSearch
+
+if(USE_LOCAL == True):
+
+    es_client = Elasticsearch("http://10.2.1.14:9200/", sniff_on_start=True, sniff_on_connection_fail=True, sniffer_timeout=300)
+else:
+    es_client = Elasticsearch(ES_HOSTS, sniff_on_start=True, sniff_on_connection_fail=True, sniffer_timeout=300)
