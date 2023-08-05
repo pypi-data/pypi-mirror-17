6853828b560bd6d372bf846c1aca4d70838d052e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:30 +0000

    Add package InferBear/0.9.0.1472802302/InferBear-0.9.0.1472802302.tar.gz

---
+++ packages/InferBear/InferBear-0.9.0.1472802302.tar.gz/InferBear-0.9.0.1472802302/coalaInferBear/InferBear.py
@@ -0,0 +1,30 @@
+import re
+
+from coalib.bearlib.abstractions.Lint import Lint
+from coalib.bears.LocalBear import LocalBear
+from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
+
+
+class InferBear(LocalBear, Lint):
+    executable = 'infer'
+    arguments = '-npb -- javac {filename}'
+    output_regex = re.compile(
+        r'(.+):'
+        r'(?P<line>.+): '
+        r'(?P<severity>error|warning): '
+        r'(?P<message>.*)')
+    severity_map = {
+        "error": RESULT_SEVERITY.MAJOR,
+        "warning": RESULT_SEVERITY.NORMAL}
+    LANGUAGES = {"Java"}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+    ASCIINEMA_URL = 'https://asciinema.org/a/1g2k0la7xo5az9t8f1v5zy66q'
+    CAN_DETECT = {'Security'}
+
+    def run(self, filename, file):
+        '''
+        Checks the code with ``infer``.
+        '''
+        return self.lint(filename)
