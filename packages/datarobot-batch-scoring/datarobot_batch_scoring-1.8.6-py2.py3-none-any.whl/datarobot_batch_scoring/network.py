d3f9f2857500fcc56ee964d02ec97b34cad584c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:38 +0000

    Add package datarobot-batch-scoring/1.8.6/datarobot_batch_scoring-1.8.6-py2.py3-none-any.whl

---
+++ packages/datarobot-batch-scoring/datarobot_batch_scoring-1.8.6-py2.py3-none-any.whl/datarobot_batch_scoring/network.py
@@ -0,0 +1,62 @@
+import collections
+import logging
+import textwrap
+import gc
+
+import requests
+
+try:
+    from futures import ThreadPoolExecutor
+except ImportError:
+    from concurrent.futures import ThreadPoolExecutor
+
+
+logger = logging.getLogger(__name__)
+
+
+FakeResponse = collections.namedtuple('FakeResponse', 'status_code, text')
+
+
+class Network(object):
+
+    def __init__(self, concurrency, timeout, ui=None):
+        self._executor = ThreadPoolExecutor(concurrency)
+        self._timeout = timeout
+        self.session = requests.Session()
+        self._ui = ui or logger
+
+    def _request(self, request):
+        prepared = self.session.prepare_request(request)
+        try:
+            self.session.send(prepared, timeout=self._timeout)
+        except requests.exceptions.ReadTimeout:
+            self._ui.warning(textwrap.dedent("""The server did not send any data
+in the allotted amount of time.
+You might want to decrease the "--n_concurrent" parameters
+or
+increase "--timeout" parameter.
+"""))
+
+        except Exception as exc:
+            self._ui.debug('Exception {}: {}'.format(type(exc), exc))
+            try:
+                callback = request.kwargs['hooks']['response']
+            except AttributeError:
+                callback = request.hooks['response'][0]
+            response = FakeResponse(400, 'No Response')
+            callback(response)
+        finally:
+            request.data = None
+            prepared.data = None
+            del request
+            del prepared
+            gc.collect()
+
+    def perform_requests(self, requests):
+        return self._executor.map(self._request, requests)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._executor.shutdown(wait=False)
