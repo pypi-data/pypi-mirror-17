ca0a6a80d630bbdeecf1511dac2d2daae1587afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:32 +0000

    Add package whatismymovie/0.1.1/whatismymovie-0.1.1.tar.gz

---
+++ packages/whatismymovie/whatismymovie-0.1.1.tar.gz/whatismymovie-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='whatismymovie',
+    description='Python API wrapper for whatismymovie.com',
+    version='0.1.1',
+    url='https://github.com/Pitasi/py-whatismymovie',
+    author='Antonio Pitasi',
+    author_email='pitasi.antonio@gmail.com',
+    packages=['whatismymovie'],
+    install_requires=['requests'],
+    license='GPL3',
+    keywords='whatisthismovie imdb api movie'
+)
