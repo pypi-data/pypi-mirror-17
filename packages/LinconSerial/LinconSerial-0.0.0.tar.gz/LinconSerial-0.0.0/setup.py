12fd0377854e386fb24da73780be5babe6108359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:02 +0000

    Add package LinconSerial/0.0.0/LinconSerial-0.0.0.tar.gz

---
+++ packages/LinconSerial/LinconSerial-0.0.0.tar.gz/LinconSerial-0.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name =           'LinconSerial',
+    version =        '0.0.0',
+    description =    'Linux Connect Over Serial',
+    author =         'Sean Jennings',
+    author_email =   'seanrjennings@gmail.com',
+    license =        'MIT',
+    url =            'https://bitbucket.org/sjennings/linconserial',
+    download_url =   'https://bitbucket.org/sjennings/linconserial/get/master.tar.gz',
+    classifiers =   ['Development Status :: 3 - Alpha',
+                     'Programming Language :: Python :: 2.7'],
+    keywords =      [],
+    packages =       find_packages(),
+    install_requires = [],
+)
