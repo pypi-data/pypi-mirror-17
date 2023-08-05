eb47b9685f715e3fd7beb2611f0af4e9c41b326a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:23 +0000

    Add package Tom_20160826/1.1.0/Tom_20160826-1.1.0.zip

---
+++ packages/Tom_20160826/Tom_20160826-1.1.0.zip/Tom_20160826-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'Tom_20160826',
+	version = '1.1.0',
+	py_modules = ['Tom_20160826'],
+	author = 'whzengyt',
+	author_email = 'whzengyt@cn.ibm.com',
+	description = 'A simple printer of nested lists',
+	)
