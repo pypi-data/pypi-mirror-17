bd78855bc349e260adcd8b248002bd9a66b2bf2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:35 +0000

    Add package activedetect/0.1/activedetect-0.1.tar.gz

---
+++ packages/activedetect/activedetect-0.1.tar.gz/activedetect-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'activedetect',
+  packages = ['activedetect'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A Library For Error Detection For Predictive Analytics',
+  author = 'Sanjay Krishnan and Eugene Wu',
+  author_email = 'sanjay@eecs.berkeley.edu',
+  url = 'https://github.com/sjyk/activedetect/', # use the URL to the github repo
+  download_url = 'https://github.com/sjyk/activedetect/tarball/0.1', 
+  keywords = ['error', 'detection', 'cleaning'], # arbitrary keywords
+  classifiers = [],
+)
