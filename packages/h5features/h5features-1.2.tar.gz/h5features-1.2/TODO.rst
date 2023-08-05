75034dda9863d30373a152011c1b670fdab0b933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:56 +0000

    Add package h5features/1.2/h5features-1.2.tar.gz

---
+++ packages/h5features/h5features-1.2.tar.gz/h5features-1.2/TODO.rst
@@ -0,0 +1,28 @@
+These document the scheduled and/or requested changes to the h5features package.
+
+For a future release
+--------------------
+
+* Test convertion from h5features old versions
+
+* read/write bigger than RAM -> catch MemoryError when np.concatenate
+  on writing.
+
+* labels can be of arbitrary type (optionally sorted)
+
+* Have a h5features.File class inspired by h5py.File
+
+  * Make Data a dict with the following syntax::
+
+      reader = h5f.Reader(file, group)
+      reader['item'][from_time:to_time]
+      reader['item'].features
+      reader['item'].labels
+      reader.keys()
+
+  * Make an Item class wrapping Labels and Features
+
+* Implement sparse functionalities
+* Handle h5py MPI driver for concurent reading
+* Enable autochunking from h5py (with chunk=None)
+* Allow data compression as an option for the writer
---
+++ packages/h5features/h5features-1.2.tar.gz/h5features-1.2/setup.py
@@ -0,0 +1,46 @@
+# Copyright 2014-2016 Thomas Schatz, Mathieu Bernard, Roland Thiolliere
+#
+# This file is part of h5features.
+#
+# h5features is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# h5features is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with h5features.  If not, see <http://www.gnu.org/licenses/>.
+"""Setup script for the h5features package"""
+
+import os
+from setuptools import setup, find_packages
+
+VERSION = '1.2'
+
+# On Reads The Docs we don't install any package
+ON_RTD = os.environ.get('READTHEDOCS', None) == 'True'
+REQUIREMENTS = [] if ON_RTD else [
+    'h5py >= 2.3.0',
+    'numpy >= 1.8.0',
+    'scipy >= 0.13.0',
+]
+
+setup(
+    name='h5features',
+    version=VERSION,
+    description='efficient storage of large features data',
+    long_description=open('README.rst').read(),
+    keywords='HDF5 h5py features read write',
+    author='Thomas Schatz, Mathieu Bernard, Roland Thiolliere',
+    author_email='mmathieubernardd@gmail.com',
+    url='https://github.com/bootphon/h5features',
+    license='GPLv3',
+    packages=find_packages(exclude=['test']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+)
