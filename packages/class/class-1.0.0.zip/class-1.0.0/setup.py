ab4ecce31e757784f802dbc3d40a9c96ffcf9cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:57 +0000

    Add package class/1.0.0/class-1.0.0.zip

---
+++ packages/class/class-1.0.0.zip/class-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name = 'class',
+		version = '1.0.0',
+		py_modules = ['classPrac'],
+		author = 'Wjq_python',
+		author_email = '614754894@qq.com',
+		url =   'http://user.qzone.qq.com/614754894/main',
+		description =  'A simple class!'
+	 )
