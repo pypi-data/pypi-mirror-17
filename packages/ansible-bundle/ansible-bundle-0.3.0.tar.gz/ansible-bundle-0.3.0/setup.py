f36878214d11609cf3d5afa6fc850599157d5373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:29 +0000

    Add package ansible-bundle/0.3.0/ansible-bundle-0.3.0.tar.gz

---
+++ packages/ansible-bundle/ansible-bundle-0.3.0.tar.gz/ansible-bundle-0.3.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from subprocess import Popen, PIPE
+import sys
+
+p = Popen(['git', 'tag'], stdout=PIPE, stderr=PIPE)
+stdout, stderr = p.communicate()
+version = stdout.split('\n')[-2] # Last line is always empty, so take previous
+
+setup(name='ansible-bundle',
+      version=version,
+      description="Manage ansible role and modules versioned dependencies.",
+      long_description=open('README.md').read(),
+      keywords='ansible roles modules',
+      author='David PG',
+      author_email='david.3pwood@gmail.com',
+      url='https://github.com/devopsysadmin/ansible-bundle',
+      license='GPL v2',
+      packages=find_packages(exclude=['test']),
+      entry_points={
+          'console_scripts': [
+             'ansible-bundle = ansible_bundle.main:main',
+          ],
+      },
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[line for line in open('requirements.txt')],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+          'Operating System :: OS Independent',
+          'Topic :: Utilities'
+      ]
+)
