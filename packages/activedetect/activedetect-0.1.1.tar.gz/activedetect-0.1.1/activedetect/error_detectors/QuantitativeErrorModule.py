972e2550524f8f06c6b91c2624526f1003e58def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:36 +0000

    Add package activedetect/0.1.1/activedetect-0.1.1.tar.gz

---
+++ packages/activedetect/activedetect-0.1.1.tar.gz/activedetect-0.1.1/activedetect/error_detectors/QuantitativeErrorModule.py
@@ -0,0 +1,80 @@
+"""
+Detects Quantitative Errors in a dataset using
+standard deviations
+"""
+import numpy as np
+from ErrorModule import ErrorModule
+
+class QuantitativeErrorModule(ErrorModule):
+
+	def __init__(self, thresh=3.5):
+		self.thresh = thresh
+
+
+	"""
+	Returns the subset of a domain that is potentially
+	erroneous
+	"""
+	def predict(self, vals):
+		
+		vals = filter( lambda x : not np.isinf(x) ,[self.tryParse(v) for v in list(vals)])
+
+		std = np.std(vals)
+		mean = np.mean(vals)
+		
+		#make a copy
+		incorpus = []
+		error = []
+		incorpus.extend(vals)
+
+		for a in vals:
+			if np.abs(a - mean) > self.thresh*std:
+				error.append(a)
+				incorpus.remove(a)
+
+		return error, incorpus
+
+
+	def tryParse(self, num):
+		try:
+			return float(num)
+		except ValueError:
+			return np.inf
+
+
+	"""
+	Turns an error set into a set of records
+	"""
+	def getRecordSet(self, errors, dataset, col):
+
+		indices = []
+		erecords = []
+
+		for i,d in enumerate(dataset):
+			val = self.tryParse(d[col])
+
+			match = False
+
+			for e in errors:
+
+				if e == val:
+					match = True
+
+			#match or can't parse
+			if match or np.isinf(val):
+				indices.append(i)
+				erecords.append(d)
+
+		return erecords, indices
+
+	"""
+	Returns a description
+	"""
+	def desc(self):
+		return "A numerical value was found with a value of greater than > "+ str(self.thresh) + " stds from the mean"
+
+	"""
+	Returns where it is applicable
+	"""
+	def availTypes(self):
+		return ['numerical']
