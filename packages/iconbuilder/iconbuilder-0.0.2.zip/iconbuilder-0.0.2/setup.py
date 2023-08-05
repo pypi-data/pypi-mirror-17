378c114dcf5825f33bc3cf6786c650007cbbbf62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:46 +0000

    Add package iconbuilder/0.0.2/iconbuilder-0.0.2.zip

---
+++ packages/iconbuilder/iconbuilder-0.0.2.zip/iconbuilder-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup,find_packages
+
+with open('README.md') as f:
+    long_description=f.read()
+
+setup(
+    name='iconbuilder',
+    version='0.0.2',
+    keywords=('image','resizer'),
+    description='just a simple image resizer',
+    license='GPL License',
+    py_modules=['iconbuilder'],
+    install_requires=['Image','Click'],
+    long_description=long_description,
+    author='Xiaozhe Yaoi@zhitan',
+    author_email='xiaozhe.yaoi@gmail.com',
+    packages=find_packages(),
+    platforms='any',
+    url='https://github.com/stevefermi/IconBuilder',
+    entry_points='''
+        [console_scripts]
+        iconbuilder=iconbuilder.iconbuilder:main
+    ''',
+)
