8d73c54db2843a7713ffd4dae33961ba866ea1b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:44 +0000

    Add package Kyoukai/1.9.2/Kyoukai-1.9.2.tar.gz

---
+++ packages/Kyoukai/Kyoukai-1.9.2.tar.gz/Kyoukai-1.9.2/setup.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+rootpath = os.path.abspath(os.path.dirname(__file__))
+
+
+# Extract version
+def extract_version(module = 'kyoukai'):
+    version = None
+    fname = os.path.join(rootpath, module, 'util.py')
+    with open(fname) as f:
+        for line in f:
+            if line.startswith('VERSION'):
+                _, version = line.split('=')
+                version = version.strip()[1:-1]  # Remove quotation characters.
+                break
+    return version
+
+
+deps = [
+    "httptools>=0.0.9",
+    "PyYAML==3.11",
+    "typeguard>=1.2.1",
+    "asphalt>=2.0.0",
+    "werkzeug>=0.11.10",
+    "Mako>=1.0.4"
+]
+
+if sys.platform != "win32":
+    # Add python-magic to deps.
+    deps.append("python-magic")
+
+setup(
+    name='Kyoukai',
+    version=extract_version(),
+    packages=find_packages(),
+    url='https://mirai.veriny.tf',
+    license='MIT',
+    author='Isaac Dickinson',
+    author_email='sun@veriny.tf',
+    description='A fast, asynchronous web framework for Python 3.5+',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Application Frameworks"
+    ],
+    install_requires=deps,
+    test_requires=[
+        "pytest",
+        "pytest-asyncio"
+    ]
+)
