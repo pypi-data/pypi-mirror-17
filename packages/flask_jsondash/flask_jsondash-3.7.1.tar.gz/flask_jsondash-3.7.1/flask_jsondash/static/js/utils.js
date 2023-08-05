c573dbf0d02cdb7ed09cee7dd8b54b36844931a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:19 +0000

    Add package flask_jsondash/3.7.1/flask_jsondash-3.7.1.tar.gz

---
+++ packages/flask_jsondash/flask_jsondash-3.7.1.tar.gz/flask_jsondash-3.7.1/flask_jsondash/static/js/utils.js
@@ -0,0 +1,72 @@
+/**
+ * Utility functions.
+ */
+
+function serializeToJSON(arr) {
+    // Convert form data to a proper json value
+    var json = {};
+    $.each(arr, function(_, pair){
+        json[pair.name] = pair.value;
+    });
+    return json;
+}
+
+function isOverride(config) {
+    return config.override && config.override === true;
+}
+
+// Credit: http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+}
+
+function guid() {
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+function normalizeName(name) {
+    if(!name) return 'id_NONAME_' + s4();
+    // Credit: http://stackoverflow.com/questions/4328500/
+    // how-can-i-strip-all-punctuation-from-a-string-in-javascript-using-regex
+    name = name.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    // Reformat it for ID use.
+    return 'id_' + name.replace(/#/gi, '').replace(/ /gi, '_');
+}
+
+function polygon(d) {
+    return "M" + d.join("L") + "Z";
+}
+
+function isD3Subtype(config) {
+    // Handle specific D3 types that aren't necessarily referenced under
+    // the D3 namespace in a select field.
+    if(config.type === 'dendrogram') return true;
+    if(config.type === 'voronoi') return true;
+    if(config.type === 'circlepack') return true;
+    if(config.type === 'treemap') return true;
+    if(config.type === 'radial-dendrogram') return true;
+    return false;
+}
+
+function isSparkline(type) {
+    return type.substr(0, 10) === 'sparklines';
+}
+
+/**
+ * [getDigitSize return a d3 scale for adjusting font size based
+ *     on digits and width of container.]
+ */
+function getDigitSize() {
+    var BOX_PADDING = 20;
+    // scale value is reversed, since we want
+    // the font-size to get smaller as the number gets longer.
+    var scale = d3.scale.linear()
+        .clamp(true)
+        .domain([2, 14]) // min/max digits length: $0 - $999,999,999.00
+        .range([90, 30]); // max/min font-size
+    window.scale = scale;
+    return scale;
+}
---
+++ packages/flask_jsondash/flask_jsondash-3.7.1.tar.gz/flask_jsondash-3.7.1/setup.py
@@ -0,0 +1,56 @@
+"""Setup for Flask Jsondash."""
+
+import os
+from setuptools import setup
+
+SRCDIR = '.'
+folder = os.path.abspath(os.path.dirname(__file__))
+template_start = '{}/flask_jsondash/templates'.format(folder)
+static_start = '{}/flask_jsondash/static'.format(folder)
+
+
+def readme():
+    """Grab the long README file."""
+    try:
+        with open('README.md', 'r') as fobj:
+            return fobj.read()
+    except IOError:
+        try:
+            with open('README.rst', 'r') as fobj:
+                return fobj.read()
+        except IOError:
+            return 'No README specified.'
+
+
+def get_requires():
+    """Extract the requirements from a standard requirements.txt file."""
+    path = '{}/requirements.txt'.format(folder)
+    with open(path) as reqs:
+        return [req for req in reqs.readlines() if req]
+
+
+setup(
+    name='flask_jsondash',
+    version='3.7.1',
+    description=('Easily configurable, chart dashboards from any '
+                 'arbitrary API endpoint. JSON config only. Ready to go.'),
+    long_description=readme(),
+    author='Chris Tabor',
+    author_email='dxdstudio@gmail.com',
+    url='https://github.com/christabor/flask_jsondash',
+    license='MIT',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    install_requires=get_requires(),
+    package_dir={'flask_jsondash': 'flask_jsondash'},
+    packages=['flask_jsondash'],
+    zip_safe=False,
+    include_package_data=True,
+)
