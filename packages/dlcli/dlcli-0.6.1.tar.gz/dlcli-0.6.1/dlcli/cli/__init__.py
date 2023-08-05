960904ea4ccfcecf9b24da8fec1f253e24603b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:06 +0000

    Add package dlcli/0.6.1/dlcli-0.6.1.tar.gz

---
+++ packages/dlcli/dlcli-0.6.1.tar.gz/dlcli-0.6.1/dlcli/cli/__init__.py
@@ -0,0 +1,14 @@
+from .cli import *  # this import needs to be first!
+from .backup import *
+from .install import *
+from .pull import *
+from .push import *
+from .restore import *
+from .rm import *
+from .run import *
+from .set import *
+from .get import *
+from .create import *
+from .search import *
+from .source import *
+from .graph import *
