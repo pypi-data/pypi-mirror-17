3e0d8ae679d135062a57f0cd636678e568aa030f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:49 +0000

    Add package mongodb-audit-uploader/0.0.1/mongodb-audit-uploader-0.0.1.tar.gz

---
+++ packages/mongodb-audit-uploader/mongodb-audit-uploader-0.0.1.tar.gz/mongodb-audit-uploader-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(name='mongodb-audit-uploader',
+      version='0.0.1',
+      description='Upload MongoDB ',
+      url='https://github.com/bookmd/mongodb-audit-uploader',
+      author='Ben Waters',
+      author_email='ben@book-md.com',
+      license='MIT',
+      packages=['mongodb_auditor_uploader'],
+      install_requires=['elasticsearch'],
+      data_files=[('/etc/defaults/mongodb-audit-uploader.conf', ['conf/mongodb-audit-uploader.conf'])],
+      scripts=['bin/mongodb-auditor-uploader'],
+      zip_safe=False)
