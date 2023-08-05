e92e7be9bb87e878eeefa1371aa76f87ae07daef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:05 +0000

    Add package ocapi/0.6.post44/ocapi-0.6.post44.tar.gz

---
+++ packages/ocapi/ocapi-0.6.post44.tar.gz/ocapi-0.6.post44/ocapi/info.py
@@ -0,0 +1,19 @@
+#  !/usr/bin/env python
+#  -*- coding:utf-8 -*-
+#  (c)Ing. Zdenek Dlauhy, Michal Dlauhý, info@dlauhy.cz
+
+"""
+module for access to oc api
+"""
+
+import ocapi.version as v
+
+__version__ = "0.6-{}".format(v.__revision__)
+__author__ = "Zdenek Dlauhy, Michal Dlauhy"
+__email__ = "info@pripravto.cz"
+__url__ = "http://www.pripravto.cz"
+__web__ = __url__
+__desc__ = "API connector to Pripravto service"
+__sys_name__ = "ocapi"
+__license__ = "MIT"
+__doc__ = __desc__
