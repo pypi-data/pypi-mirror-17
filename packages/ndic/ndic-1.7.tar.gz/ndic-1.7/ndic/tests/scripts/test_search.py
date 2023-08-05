3b7dd177df1fe96cfe719d0c46d919dfcb55f677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:21 +0000

    Add package ndic/1.7/ndic-1.7.tar.gz

---
+++ packages/ndic/ndic-1.7.tar.gz/ndic-1.7/ndic/tests/scripts/test_search.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+from unittest import TestCase
+
+from click.testing import CliRunner
+import mock
+import requests
+
+from ndic.scripts.search import cli_search
+from ndic.exceptions import NdicConnectionError
+
+
+class NdicScriptTestCase(TestCase):
+
+    def setUp(self):
+        self.runner = CliRunner()
+
+    def test_search_korean_word(self):
+        result = self.runner.invoke(
+            cli_search,
+            ["사과"],
+        )
+        self.assertEqual(
+            result.exit_code,
+            0,
+        )
+        self.assertEqual(
+            result.output.replace('\n', ''),
+            u"(과일) apple",
+        )
+
+    def test_search_english_word(self):
+        result = self.runner.invoke(
+            cli_search,
+            ["apple"],
+        )
+        self.assertEqual(
+            result.exit_code,
+            0,
+        )
+        self.assertEqual(
+            result.output.replace('\n', ''),
+            u"사과",
+        )
+
+    def test_search_nonexistent_korean_word(self):
+        result = self.runner.invoke(
+            cli_search,
+            ["아갸야라"],
+        )
+        self.assertEqual(
+            result.exit_code,
+            0,
+        )
+        self.assertFalse(
+            result.output.replace('\n', ''),
+        )
+
+    def test_search_nonexistent_english_word(self):
+        result = self.runner.invoke(
+            cli_search,
+            ["asfasdfasdf"],
+        )
+        self.assertEqual(
+            result.exit_code,
+            0,
+        )
+        self.assertFalse(
+            result.output.replace('\n', ''),
+        )
+
+    @mock.patch.object(requests, 'get', side_effect=requests.ConnectionError)
+    def test_search_without_internet_network(self, mock_requests):
+        result = self.runner.invoke(
+            cli_search,
+            ["사과"],
+        )
+        self.assertEqual(
+            result.exit_code,
+            -1,
+        )
+        self.assertEqual(
+            result.exception.__class__,
+            NdicConnectionError,
+        )
