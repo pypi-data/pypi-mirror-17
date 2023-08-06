fef5ba8ca2012e2fa57c0bfb1f0667def1f0e89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:27 +0000

    Add package fer2py/0.1/fer2py-0.1.zip

---
+++ packages/fer2py/fer2py-0.1.zip/fer2py-0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'fer2py',
+  packages = ['fer2py'], 
+  version = '0.1',
+  description = 'Python Fer2',
+  author = 'Ante Novokmet',
+  author_email = 'ante.je.zakon@gmail.com',
+  url = 'https://github.com/ANovokmet/fer2py/', 
+  download_url = 'https://github.com/ANovokmet/fer2py/tarball/0.1'
+)
