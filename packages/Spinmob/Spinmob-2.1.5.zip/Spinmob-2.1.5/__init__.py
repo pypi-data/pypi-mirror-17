b70a07b6c62a0edae209c30c40c754a42e6f42cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:44 +0000

    Add package Spinmob/2.1.5/Spinmob-2.1.5.zip

---
+++ packages/Spinmob/Spinmob-2.1.5.zip/Spinmob-2.1.5/__init__.py
@@ -0,0 +1,39 @@
+import os           as _os
+import matplotlib   as _mpl
+import pylab
+
+# ignore warnings by default
+import warnings
+warnings.simplefilter("ignore")
+
+# Start the QApplication. Not sure why it has to be done this way.
+import PyQt4.QtCore as _qtc
+import PyQt4.Qt     as _qt
+
+# make sure we have a valid qt application for dialogs etc...
+_qtapp = _qtc.QCoreApplication.instance()
+if not _qtapp: _qtapp = _qt.QApplication(_os.sys.argv)
+
+
+#_qtapp = _qt.qApp
+#if _qtapp == None:
+#    print "Creating QApplication"
+#    _qtapp = _qt.QApplication(_os.sys.argv)
+
+import _settings
+settings = _settings.settings()
+
+# some defaults
+_mpl.rcParams['figure.facecolor']='w'
+
+
+
+import _plot             as plot        ; plot._settings    = settings
+import _data             as data        ; data._settings    = settings
+import _dialogs          as dialogs     ; dialogs._settings = settings
+import _pylab_tweaks     as tweaks      ; tweaks._settings  = settings
+import _functions        as fun         ; fun._settings     = settings
+
+plot.tweaks._pylab_colormap._settings = settings
+
+instaprint = tweaks.instaprint
