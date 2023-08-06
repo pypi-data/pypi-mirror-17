dfea71c23ac0260ee42bae9dccd4b606b6b47b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:41 +0000

    Add package chess_py/2.2/chess_py-2.2.tar.gz

---
+++ packages/chess_py/chess_py-2.2.tar.gz/chess_py-2.2/chess_py/__init__.py
@@ -0,0 +1,4 @@
+from .core import Location, Move, converter, notation_const, Board, color
+from .game import Game, game_state
+from .pieces import Bishop, King, Knight, Pawn, Piece, piece_const, Queen, Rook
+from .players import Player, Human
---
+++ packages/chess_py/chess_py-2.2.tar.gz/chess_py-2.2/chess_py/core/__init__.py
@@ -0,0 +1,4 @@
+from .algebraic import Move, Location, converter, notation_const
+from .board import Board
+
+__all__ = ['converter', 'Location', 'Move', 'notation_const', 'Board', 'color']
