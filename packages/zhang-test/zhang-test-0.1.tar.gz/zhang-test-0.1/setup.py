4083b5a4dd2f030ac87cec817ed8c606d2c707db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:26 +0000

    Add package zhang-test/0.1/zhang-test-0.1.tar.gz

---
+++ packages/zhang-test/zhang-test-0.1.tar.gz/zhang-test-0.1/setup.py
@@ -0,0 +1,13 @@
+"""
+just a simple test
+"""
+from setuptools import setup
+
+setup(
+    name='zhang-test',
+    version='0.1',
+    license='MIT',
+    description=__doc__.strip(),
+    author='zhang cabbage',
+    author_email='jiahua_zhang@foxmail.com'
+)
