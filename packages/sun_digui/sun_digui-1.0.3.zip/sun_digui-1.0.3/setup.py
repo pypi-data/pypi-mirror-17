d5ae3ce407cd470ab3978cf597fb8d2e54cf816f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:34 +0000

    Add package sun_digui/1.0.3/sun_digui-1.0.3.zip

---
+++ packages/sun_digui/sun_digui-1.0.3.zip/sun_digui-1.0.3/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='sun_digui',
+    version='1.0.3',
+    py_modules=['digui'],
+    author='sun',
+    url='http://www.meihuaedu.cn',
+    description='aaaaa'
+    )
