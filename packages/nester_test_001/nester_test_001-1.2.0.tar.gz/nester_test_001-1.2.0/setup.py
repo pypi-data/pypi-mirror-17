334b2cfb9e2a6daf23b4ac20ac1d2cf5c4f9fa12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:34 +0000

    Add package nester_test_001/1.2.0/nester_test_001-1.2.0.tar.gz

---
+++ packages/nester_test_001/nester_test_001-1.2.0.tar.gz/nester_test_001-1.2.0/setup.py
@@ -0,0 +1,12 @@
+"""I want to try distutils my code"""
+from distutils.core import setup
+
+setup(
+	name = 'nester_test_001',
+	version = '1.2.0',
+	py_modules = ['nester'],
+	author = 'LYZ',
+	author_email = 'Haha',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of nested lists',
+)
