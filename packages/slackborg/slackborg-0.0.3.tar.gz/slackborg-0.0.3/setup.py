db5cbac10a944b85c31252bb9d0721aa1de5d9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:49 +0000

    Add package slackborg/0.0.3/slackborg-0.0.3.tar.gz

---
+++ packages/slackborg/slackborg-0.0.3.tar.gz/slackborg-0.0.3/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    author='Josef Lange',
+    author_email='josef.d.lange@me.com',
+    name='slackborg',
+    description='A simple semi-conversational, multi-step workflow slack bot framework.',
+    version='0.0.3',
+    packages=['slackborg',],
+    license='MIT',
+    long_description=long_description,
+    install_requires=[
+        'slackclient'
+    ],
+    url='https://github.com/josefdlange/slackborg',
+    test_suite='nose.collector',
+    tests_require=['nose']
+)
+
+# End
+
