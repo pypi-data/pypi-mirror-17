4384dc7d5c061844dd97d4522a9b4aa19e4d6b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:50 +0000

    Add package trustedanalytics/0.7.3.post20161014775/trustedanalytics-0.7.3.post20161014775.tar.gz

---
+++ packages/trustedanalytics/trustedanalytics-0.7.3.post20161014775.tar.gz/trustedanalytics-0.7.3.post20161014775/trustedanalytics/core/aggregation.py
@@ -0,0 +1,89 @@
+# vim: set encoding=utf-8
+
+#
+#  Copyright (c) 2015 Intel Corporation 
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+"""
+trustedanalytics frame aggregation functions
+"""
+import json
+
+from trustedanalytics.core.atktypes import valid_data_types
+
+
+class AggregationUdf(object):
+    """
+    Class for Aggregate-by-key that holds the user given parameters
+    """
+
+    def __init__(self, aggregator, output_schema, init_values=None):
+        self.output_schema = output_schema
+        self.aggregator = aggregator
+        self.init_values = init_values
+
+
+class AggregationFunctions(object):
+
+    """
+    Defines supported aggregation functions, maps them to keyword strings
+    """
+    avg = 'AVG'
+    count = 'COUNT'
+    count_distinct = 'COUNT_DISTINCT'
+    max = 'MAX'
+    min = 'MIN'
+    sum = 'SUM'
+    var = 'VAR'
+    stdev = 'STDEV'
+
+    def histogram(self, cutoffs, include_lowest=True, strict_binning=False):
+        return repr(GroupByHistogram(cutoffs, include_lowest, strict_binning))
+
+    def __repr__(self):
+        return ", ".join([k for k in AggregationFunctions.__dict__.keys()
+                          if isinstance(k, basestring) and not k.startswith("__")]) + ", udf"
+
+    def __contains__(self, item):
+        return (item in AggregationFunctions.__dict__.values()) or isinstance(item, AggregationUdf)
+
+    def udf(self, aggregator, output_schema, init_values=None):
+        return AggregationUdf(aggregator, output_schema, init_values)
+
+agg = AggregationFunctions()
+
+
+class GroupByHistogram:
+    """
+    Class for histogram aggregation function that uses cutoffs to compute histograms
+    """
+    def __init__(self, cutoffs, include_lowest=True, strict_binning=False):
+        for c in cutoffs:
+            if not isinstance(c, (int, long, float, complex)):
+                raise ValueError("Bad value %s in cutoffs, expected a number")
+        self.cutoffs = cutoffs
+        self.include_lowest = include_lowest
+        self.strict_binning = strict_binning
+
+    def __repr__(self):
+        return 'HISTOGRAM=' + json.dumps(self.__dict__)
+
+
+
+
+
+
+
+
