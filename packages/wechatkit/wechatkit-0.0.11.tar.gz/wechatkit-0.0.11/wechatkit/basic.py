9c4aff4bfbbe8f815bcc3375120abf72f2163c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:20 +0000

    Add package wechatkit/0.0.11/wechatkit-0.0.11.tar.gz

---
+++ packages/wechatkit/wechatkit-0.0.11.tar.gz/wechatkit-0.0.11/wechatkit/basic.py
@@ -0,0 +1,48 @@
+"""wechatkit basic"""
+
+from . import consts
+from .utils import RequestUtil
+
+
+class WechatBasicAPI(object):
+    """Wechat basic api."""
+
+    @staticmethod
+    def get_access_token(appid, secret):
+        """Get access token from wechat."""
+        url = consts.WECHAT_ACCESS_TOKEN_URL.format(appid=appid, secret=secret)
+
+        result = RequestUtil.get(url)
+
+        if result.get('errmsg'):
+            errmsg = RequestUtil.get_retcode_msg(result.get('errcode'))
+            result['errmsg'] = errmsg
+
+        return result
+
+    @staticmethod
+    def get_callbackip(access_token):
+        """Get wecaht callbackip."""
+        url = consts.WECHAT_CALLBACKIP_URL.format(access_token=access_token)
+
+        result = RequestUtil.get(url)
+
+        if result.get('errmsg'):
+            errmsg = RequestUtil.get_retcode_msg(result.get('errcode'))
+            result['errmsg'] = errmsg
+
+        return result
+
+    @staticmethod
+    def get_web_access_token(appid, appsecret, code):
+        """Get web access toekn differ from base access token."""
+        urlfmt = '{}appid={}&secret={}&code={}&grant_type=authorization_code'
+        url = urlfmt.format(consts.WECHAT_WEB_AUTH_ACCESS_TOKEN_URI,
+                            appid, appsecret, code)
+        result = RequestUtil.get(url)
+
+        if result.get('errmsg'):
+            errmsg = RequestUtil.get_retcode_msg(result.get('errcode'))
+            result['errmsg'] = errmsg
+
+        return result
