d3887e80fd80d1aa12754a1fbef2dec7b281697a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:19 +0000

    Add package pystan/2.12.0.0/pystan-2.12.0.0-cp27-cp27m-manylinux1_x86_64.whl

---
+++ packages/pystan/pystan-2.12.0.0-cp27-cp27m-manylinux1_x86_64.whl/pystan/stan/lib/stan_math_2.12.0/stan/math/fwd/scal/fun/asinh.hpp
@@ -0,0 +1,21 @@
+#ifndef STAN_MATH_FWD_SCAL_FUN_ASINH_HPP
+#define STAN_MATH_FWD_SCAL_FUN_ASINH_HPP
+
+#include <math.h>
+#include <stan/math/fwd/core.hpp>
+#include <stan/math/prim/scal/fun/square.hpp>
+#include <cmath>
+
+namespace stan {
+  namespace math {
+
+    template <typename T>
+    inline fvar<T> asinh(const fvar<T>& x) {
+      using ::asinh;
+      using std::sqrt;
+      return fvar<T>(asinh(x.val_), x.d_ / sqrt(square(x.val_) + 1));
+    }
+
+  }
+}
+#endif
