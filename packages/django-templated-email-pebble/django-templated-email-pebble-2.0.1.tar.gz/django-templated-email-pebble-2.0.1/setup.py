ad98d3991b789e2d1fa90adddc06bfc4a4ac3bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:05 +0000

    Add package django-templated-email-pebble/2.0.1/django-templated-email-pebble-2.0.1.tar.gz

---
+++ packages/django-templated-email-pebble/django-templated-email-pebble-2.0.1.tar.gz/django-templated-email-pebble-2.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+DESCRIPTION = "Pebble's fork of a Django oriented templated / transaction email abstraction"
+
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.rst').read()
+except:
+    pass
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Framework :: Django',
+]
+
+setup(
+    name='django-templated-email-pebble',
+    version='2.0.1',
+    packages=['templated_email', 'templated_email.backends'],
+    author='SF Software limited t/a Pebble',
+    author_email='sysadmin@talktopebble.co.uk',
+    url='http://github.com/mypebble/django-templated-email/',
+    license='MIT',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    platforms=['any'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'six',
+    ]
+)
