070684a35602a36e0a040d1a06a0f772aa00be16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:55 +0000

    Add package incremental/16.9.0/incremental-16.9.0.tar.gz

---
+++ packages/incremental/incremental-16.9.0.tar.gz/incremental-16.9.0/examplesetup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='exampleproj',
+    package_dir={"": "src"},
+    packages=["exampleproj"],
+    use_incremental=True,
+    zip_safe=False,
+    setup_requires=['incremental'],
+)
