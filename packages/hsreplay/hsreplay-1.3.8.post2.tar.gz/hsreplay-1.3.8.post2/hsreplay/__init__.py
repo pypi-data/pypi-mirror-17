b48c327c8cce2d46fc97011ebcd83fb728d84608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:59 +0000

    Add package hsreplay/1.3.8.post2/hsreplay-1.3.8.post2.tar.gz

---
+++ packages/hsreplay/hsreplay-1.3.8.post2.tar.gz/hsreplay-1.3.8.post2/hsreplay/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+DTD_VERSION = "1.3"
+LIB_VERSION = 8
+__author__ = "Jerome Leclanche"
+__email__ = "jerome@hearthsim.net"
+__version__ = "%s.%s.post2" % (DTD_VERSION, LIB_VERSION)
+
+
+SYSTEM_DTD = "https://hearthsim.info/hsreplay/dtd/hsreplay-%s.dtd" % (DTD_VERSION)
