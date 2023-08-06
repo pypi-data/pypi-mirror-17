828b1b81212c50f809c8d31654dbd84e37ab93ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:57 +0000

    Add package qiniu4blog-mk/1.4.2/qiniu4blog_mk-1.4.2.zip

---
+++ packages/qiniu4blog-mk/qiniu4blog_mk-1.4.2.zip/qiniu4blog_mk-1.4.2/setup.py
@@ -0,0 +1,30 @@
+#encoding:utf-8
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.4.2'
+
+setup(name='qiniu4blog_mk',
+      version=version,
+      description="写博客用的七牛图床",
+      long_description="""The author of the origin version of this package is wzyuliyang, the current version is modified by mirsking""",
+      classifiers=[],
+      keywords='python qiniu',
+      author='wzyuliyang, mirsking',
+      author_email='wzyuliyang911@gmail.com, mirsking@gmail.com',
+      url='https://github.com/wzyuliyang/qiniu4blog, https://github.com/mirsking/qiniu4blog',
+      license='MIT License',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'qiniu',
+		'pyperclip',
+        'watchdog',
+      ],
+      entry_points={
+        'console_scripts':[
+            'qiniu4blog_mk = qiniu4blog.qiniu4blog:main'
+        ]
+      },
+)
