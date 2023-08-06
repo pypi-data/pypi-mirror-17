80d3ea922dc1538597e345485f81a288e9e31b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:53 +0000

    Add package jrri_nester/1.2.0/jrri_nester-1.2.0.zip

---
+++ packages/jrri_nester/jrri_nester-1.2.0.zip/jrri_nester-1.2.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+		name		= 'jrri_nester',
+		version		= '1.2.0',
+		py_modules	= ['nester'],
+		author		= 'jerri',
+		author_email= 'dheigh@163.com',
+		url			= 'http://www.headfirstlabs.com',
+		description	= 'A simple printer of neste lists',
+		)
+
+		
