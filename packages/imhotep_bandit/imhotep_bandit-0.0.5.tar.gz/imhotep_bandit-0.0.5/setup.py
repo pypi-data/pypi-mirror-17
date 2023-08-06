61cf9a3de5f492d02683c7c358aa315915fbcaa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:52 +0000

    Add package imhotep_bandit/0.0.5/imhotep_bandit-0.0.5.tar.gz

---
+++ packages/imhotep_bandit/imhotep_bandit-0.0.5.tar.gz/imhotep_bandit-0.0.5/setup.py
@@ -0,0 +1,25 @@
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name='imhotep_bandit',
+    version='0.0.5',
+    author="Dan Palmer",
+    author_email="dan@danpalmer.me",
+    description="An imhotep plugin for eslint",
+    license='MIT',
+    url='https://github.com/danpalmer/imhotep-bandit',
+    packages=find_packages(),
+    install_requires=[
+        'imhotep>=1.0.1',
+        'bandit>=1.1.0',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+    entry_points={
+        'imhotep_linters': [
+            '.py = imhotep_bandit.plugin:Bandit',
+        ],
+    },
+)
