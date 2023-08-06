463944cae7ae18a745e862725f180d24fbdb62ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:28 +0000

    Add package tabulator/0.7.6/tabulator-0.7.6.tar.gz

---
+++ packages/tabulator/tabulator-0.7.6.tar.gz/tabulator-0.7.6/tabulator/writers/api.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from six import add_metaclass
+from abc import ABCMeta, abstractmethod
+
+
+# Module API
+
+@add_metaclass(ABCMeta)
+class Writer(object):
+    """Writer representation.
+
+    Args:
+        options(dict): writer options
+
+    """
+
+    # Public
+
+    @abstractmethod
+    def write(self, target, encoding, extended_rows):
+        """Write tabular data to target.
+        """
+        pass
