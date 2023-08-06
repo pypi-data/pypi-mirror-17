71a1effe0986e84c861619f0fde50d41cafd020f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:33 +0000

    Add package SPLAT-library/0.2.1/SPLAT-library-0.2.1.tar.gz

---
+++ packages/SPLAT-library/SPLAT-library-0.2.1.tar.gz/SPLAT-library-0.2.1/splat/annotation/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+########################################################################################################################
+##### INFORMATION ######################################################################################################
+### @PROJECT_NAME:		SPLAT: Speech Processing and Linguistic Analysis Tool										 ###
+### @VERSION_NUMBER:																								 ###
+### @PROJECT_SITE:		github.com/meyersbs/SPLAT																     ###
+### @AUTHOR_NAME:		Benjamin S. Meyers																			 ###
+### @CONTACT_EMAIL:		ben@splat-library.org																		 ###
+### @LICENSE_TYPE:		MIT																							 ###
+########################################################################################################################
+########################################################################################################################
+
+"""
+This package contains the following files:
+	[01] Dialog Act Annotation Scheme.md
+			An explanation of the annotation scheme used in the MeyersDialogActAnnotator.
+	[02] MeyersDialogActAnnotator.py
+			Provides functionality to semi-automatically insert dialog acts.
+	[03] QuarteroniDialogActAnnotator.py
+			Provides functionality to semi-automatically insert dialog acts.
+	[04] SpeakerIndicatorAnnotator.py
+			Provides functionality to semi-automatically insert speaker markers.
+"""
