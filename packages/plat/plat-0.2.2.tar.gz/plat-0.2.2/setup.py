3bba6162bd0463b87ab747e6535f9fbba5b22136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:57 +0000

    Add package plat/0.2.2/plat-0.2.2.tar.gz

---
+++ packages/plat/plat-0.2.2.tar.gz/plat-0.2.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from setuptools import find_packages
+
+install_requires = [
+    'numpy',
+    'scipy',
+    'Pillow',
+    'arghandler',
+    'braceexpand',
+    'fuel'
+]
+
+setup(name='plat',
+      version='0.2.2',
+      description='Utilities for working with generative latent spaces',
+      author='Tom White',
+      author_email='tom@sixdozen.com',
+      url='https://github.com/dribnet/plat',
+      download_url='https://github.com/dribnet/plat/archive/0.2.2.tar.gz',
+      license='MIT',
+      entry_points={
+          'console_scripts': ['plat = plat.bin.platcmd:main']
+      },
+      install_requires=install_requires,
+      packages=find_packages())
