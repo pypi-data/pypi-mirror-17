ced2d9340962f227c65cd2384e01d12d047d78f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:56 +0000

    Add package LI-AWS-Deploy/1.0.10/LI-AWS-Deploy-1.0.10.tar.gz

---
+++ packages/LI-AWS-Deploy/LI-AWS-Deploy-1.0.10.tar.gz/LI-AWS-Deploy-1.0.10/deploy/__main__.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python3
+from deploy import run
+
+run.start()
---
+++ packages/LI-AWS-Deploy/LI-AWS-Deploy-1.0.10.tar.gz/LI-AWS-Deploy-1.0.10/deploy/utils.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+import subprocess
+
+
+def run_command(command_list, title=None):
+    if title:
+        print("\n\n>> {}".format(title))
+        print("{:*^{num}}".format('', num=len(title) + 3))
+    # try:
+    for task in command_list:
+        if task['run_stdout']:
+            ret = subprocess.run(
+                [task['command']],
+                shell=True,
+                stdout=subprocess.PIPE,
+                universal_newlines=True)
+
+            if ret.returncode != 0:
+                print('Ocorreu um erro. Processo abortado')
+                return False
+
+            ret = subprocess.run(
+                [ret.stdout],
+                shell=True,
+                universal_newlines=True)
+        else:
+            ret = subprocess.run(
+                [task['command']],
+                shell=True,
+                universal_newlines=True)
+
+        if ret.returncode != 0:
+            print('Ocorreu um erro. Processo abortado')
+            return False
+
+    return True
