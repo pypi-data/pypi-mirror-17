c846e5df8deea902d6f75010358144f5813a8f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:25 +0000

    Add package web-walker/1.0.2/web-walker-1.0.2.tar.gz

---
+++ packages/web-walker/web-walker-1.0.2.tar.gz/web-walker-1.0.2/walker/spiders/constant/item_field.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+from helper import *
+import time
+import random
+
+
+BASE_FIELD = ["success", "domain", "exception", "crawlid", "meta", "response_url", "status_code", "status_msg", "url", "seed"]
+ITEM_FIELD = {}
+
