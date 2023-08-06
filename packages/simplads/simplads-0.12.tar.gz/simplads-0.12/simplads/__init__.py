b8907d4b87749f58f6b8e0dfd065a958d28ef34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:46 +0000

    Add package simplads/0.12/simplads-0.12.tar.gz

---
+++ packages/simplads/simplads-0.12.tar.gz/simplads-0.12/simplads/__init__.py
@@ -0,0 +1,6 @@
+from .simplads.list_simplad import ListSimplad
+from .simplads.log_simplad import LogSimplad, LogDeltaMaker
+from .simplads.maybe_simplad import MaybeSimplad, MaybeDeltaMaker
+from .simplads.reader_simplad import ReaderSimplad, ReaderDeltaMaker
+from .simplads.writer_simplad import WriterSimplad, WriterDeltaMaker
+from .simplad_bundle.simplad_bundle import SimpladBundle as Bundle
