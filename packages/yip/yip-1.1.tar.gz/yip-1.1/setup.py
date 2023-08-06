e0c154c53b5f805a07c9367206d6024b260ec344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:06 +0000

    Add package yip/1.1/yip-1.1.tar.gz

---
+++ packages/yip/yip-1.1.tar.gz/yip-1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='yip',
+    version='1.1',
+    description='Frontend for searching PyPI, a feature rich alternative \
+            to pip search',
+    url='https://github.com/balzss/yip',
+    author='balzss',
+    author_email='balazs.saros@gmail.com',
+    license='GPLv3',
+    keywords='pip frontend user-friendly CLI command-line',
+    scripts=['yip']
+)
