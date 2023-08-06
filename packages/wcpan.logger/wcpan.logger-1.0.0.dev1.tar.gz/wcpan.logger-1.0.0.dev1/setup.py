ffb71fccb43efc20fae565bf8ee15a04ee3db77c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:15 +0000

    Add package wcpan.logger/1.0.0.dev1/wcpan.logger-1.0.0.dev1.tar.gz

---
+++ packages/wcpan.logger/wcpan.logger-1.0.0.dev1.tar.gz/wcpan.logger-1.0.0.dev1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+        name='wcpan.logger',
+        version='1.0.0.dev1',
+        author='Wei-Cheng Pan',
+        author_email='legnaleurc@gmail.com',
+        url='https://github.com/legnaleurc/wcpan.logger',
+        packages=[
+            'wcpan',
+            'wcpan.logger',
+        ],
+        classifiers=[
+            'Programming Language :: Python :: 3 :: Only',
+        ])
