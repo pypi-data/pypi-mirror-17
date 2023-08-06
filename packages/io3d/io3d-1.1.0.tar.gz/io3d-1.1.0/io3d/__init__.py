ac077642ef5477191758eab2e1eedabf7c3329c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:42 +0000

    Add package io3d/1.1.0/io3d-1.1.0.tar.gz

---
+++ packages/io3d/io3d-1.1.0.tar.gz/io3d-1.1.0/io3d/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ['dcmreaddata', 'datareader', 'datawriter', 'rawN']
+__version__ = "1.1.0"
+# from pycut import Model, ImageGraphCut
+# from seed_editor_qt import QTSeedEditor
+from dcmreaddata import DicomReader
+from datawriter import DataWriter
+from datareader import DataReader
+from datareader import read
+from datawriter import write
+
