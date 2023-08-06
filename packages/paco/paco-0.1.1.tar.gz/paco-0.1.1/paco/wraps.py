c085b8a40c09ac8fd81aee2f75585b723347ffbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:53 +0000

    Add package paco/0.1.1/paco-0.1.1.tar.gz

---
+++ packages/paco/paco-0.1.1.tar.gz/paco-0.1.1/paco/wraps.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import asyncio
+
+
+def wraps(fn):
+    """
+    Wraps a given function as coroutine function.
+
+    This is a convenient helper function.
+
+    Arguments:
+        fn (function): function object to wrap.
+
+    Returns:
+        coroutinefunction.
+
+    Usage::
+
+        def mult(num, foo=None):
+            return num * 2
+
+        coro = paco.wrap(mult)
+        await coro(2, foo='bar')
+        => 4
+    """
+    @asyncio.coroutine
+    def wrapper(*args, **kw):
+        return fn(*args, **kw)
+    return wrapper
