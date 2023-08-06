0ba9f5da22c4be97de765508451b2eecfc8fc131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:38 +0000

    Add package Novelty/1.0.3/Novelty-1.0.3.tar.gz

---
+++ packages/Novelty/Novelty-1.0.3.tar.gz/Novelty-1.0.3/Novelty/__init__.py
@@ -0,0 +1,9 @@
+"""
+An Object Based scraper for NovelUpdates to rip information from the page
+"""
+from .novelty import Novelty
+__version__ = "1.0.3"
+__author__ = 'Fuzen.py'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Byron'
+__title__ = 'Novelty'
