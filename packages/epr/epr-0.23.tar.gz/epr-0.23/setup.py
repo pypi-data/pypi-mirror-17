063bd5a25d299a92d0de475b76d8678b3e58f56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:51 +0000

    Add package epr/0.23/epr-0.23.tar.gz

---
+++ packages/epr/epr-0.23.tar.gz/epr-0.23/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+        name='epr',
+        version='0.23',
+        author='Luke Avery',
+        author_email='cogmob@gmail.com',
+        url='https://github.com/Cogmob/epr',
+        description=('Tool for quickly adding debugging output'),
+        packages=[
+            'epr'
+        ],
+        install_requires=[
+            'glob2'
+        ],
+        long_description=open('README.rst', 'r').read()
+)
