bc3d0f1dd77af1c4b9002e04e470451526a47881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:55 +0000

    Add package ddf/0.0.0/ddf-0.0.0.tar.gz

---
+++ packages/ddf/ddf-0.0.0.tar.gz/ddf-0.0.0/src/ddf/shortcuts.py
@@ -0,0 +1,20 @@
+"""Shortcuts, use: `from ddf import shortcuts as ddf` to feel awesome."""
+
+from .action import (
+    Action,
+    Remove,
+    RemoveChoices,
+)
+
+from .condition import (
+    Condition,
+    ValueIs,
+)
+
+from .form import (
+    Field,
+    FormMixin,
+    Widget,
+)
+
+from .rule import Rule
