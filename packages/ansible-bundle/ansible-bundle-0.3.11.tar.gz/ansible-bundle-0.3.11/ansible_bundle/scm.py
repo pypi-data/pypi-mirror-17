a26065c5af999e8b40ba8827862e9425ce003e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:42 +0000

    Add package ansible-bundle/0.3.11/ansible-bundle-0.3.11.tar.gz

---
+++ packages/ansible-bundle/ansible-bundle-0.3.11.tar.gz/ansible-bundle-0.3.11/ansible_bundle/scm.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf8 -*-
+
+from ansible_bundle import shell, defaults
+
+class Git:
+    url = None
+    path = None
+    branch = None
+
+    def __init__(self, url, path='.', branch='master'):
+        self.url = url
+        self.path = path
+        self.branch = branch
+
+    def _is_branch(self):
+        with open(shell.path('.git', 'HEAD')) as fn:
+            ref = fn.read()
+        return self.branch in ref
+
+    def get(self):
+        cmd = ['git', 'clone', '--branch', self.branch,
+               '--depth', '1', self.url, self.path]
+        stdout, rc = shell.run(cmd)
+        shell.cd(shell.WORKDIR)
+        if rc == shell.OK:
+            return True
+        else:
+            return False
+
+    def update(self):
+        shell.cd(self.path)
+        if self._is_branch():
+            cmds = (
+                ['git', 'checkout', '.'],
+                ['git', 'pull', '--rebase', 'origin', self.branch],
+                )
+            for cmd in cmds:
+                stdout, rc = shell.run(cmd)
+                if rc == shell.ERROR:
+                    return False
+        elif shell.config.verbose > defaults.QUIET:
+            shell.echo_info('Current branch mismatches bundle version. Assuming tag and skip.\n')
+        shell.cd(shell.WORKDIR)
+        return True
