4ba6dd0a6f24c2e74f145f1a0ccdfb3d5c114514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:27 +0000

    Add package rattail/0.7.21/rattail-0.7.21.tar.gz

---
+++ packages/rattail/rattail-0.7.21.tar.gz/rattail-0.7.21/rattail/fablib/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#  Rattail -- Retail Software Framework
+#  Copyright © 2010-2015 Lance Edgar
+#
+#  This file is part of Rattail.
+#
+#  Rattail is free software: you can redistribute it and/or modify it under the
+#  terms of the GNU Affero General Public License as published by the Free
+#  Software Foundation, either version 3 of the License, or (at your option)
+#  any later version.
+#
+#  Rattail is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#  FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Rattail.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+"""
+Fabric Library
+
+This subpackage contains various tasks and associated functions for use with
+Fabric deployment and maintenance.
+"""
+
+from .core import put, upload_template, make_deploy, mkdir, get_debian_version, rsync
+from .core import make_system_user, add_mail_alias
+
+from .python import workon, cdvirtualenv
