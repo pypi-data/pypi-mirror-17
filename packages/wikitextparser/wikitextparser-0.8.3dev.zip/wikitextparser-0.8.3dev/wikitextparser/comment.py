9470c6c442440a6d27cdee03862b1177dc370037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:52 +0000

    Add package wikitextparser/0.8.3dev/wikitextparser-0.8.3dev.zip

---
+++ packages/wikitextparser/wikitextparser-0.8.3dev.zip/wikitextparser-0.8.3dev/wikitextparser/comment.py
@@ -0,0 +1,30 @@
+﻿"""Define the Comment class."""
+
+
+from .wikitext import IndexedWikiText
+
+
+class Comment(IndexedWikiText):
+
+    """Create a new <!-- comment --> object."""
+
+    def __init__(self, string, spans=None, index=None):
+        """Run self._common_init."""
+        self._common_init(string, spans)
+        if index is None:
+            self._index = len(self._spans['comments']) - 1
+        else:
+            self._index = index
+
+    def __repr__(self):
+        """Return the string representation of the Comment."""
+        return 'Comment(' + repr(self.string) + ')'
+
+    def _get_span(self):
+        """Return the self-span."""
+        return self._spans['comments'][self._index]
+
+    @property
+    def contents(self):
+        """Return contents of this comment."""
+        return self.string[4:-3]
