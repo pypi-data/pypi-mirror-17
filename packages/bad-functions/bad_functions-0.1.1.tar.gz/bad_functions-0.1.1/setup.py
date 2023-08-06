7e873b62a98756a3df9f41e4e4c1b18599d1625d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:58 +0000

    Add package bad-functions/0.1.1/bad_functions-0.1.1.tar.gz

---
+++ packages/bad-functions/bad_functions-0.1.1.tar.gz/bad_functions-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from distutils.core import Extension
+
+bad_functions_module = Extension('bad_functions', sources=['bad_functions/funcs_module.c', 'bad_functions/funcs.c'])
+setup(name='bad_functions',
+      version='0.1.1',
+      description='Set of bad functions in C that cause bad behavior',
+      py_modules=['bad_functions.dummy'],
+      ext_modules=[bad_functions_module])
