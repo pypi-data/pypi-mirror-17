24813250ba550b6c3e0d8d7c8cc18adb96d67f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:33 +0000

    Add package pyReframe/0.1/pyReframe-0.1.tar.gz

---
+++ packages/pyReframe/pyReframe-0.1.tar.gz/pyReframe-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pyReframe',
+    version='0.1',
+    packages=['', ''],
+    package_dir={'': 'pyReframe'},
+    download_url = 'https://github.com/alchayward/pyReframe/tarball/0.1',
+    url='https://github.com/alchayward/pyReframe.git',
+    license='MIT',
+    author='Andrew L. C. Hayward',
+    author_email='alc.hayward@gmail.com',
+    description='a port of clojures reframe to python', requires=['pyrsistent', 'rx'])
