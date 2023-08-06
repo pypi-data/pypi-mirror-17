60e9e16eb3d4d07d1f5fe266abcb4447152796da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:19 +0000

    Add package cast_convert/0.1.5.4/cast_convert-0.1.5.4.tar.gz

---
+++ packages/cast_convert/cast_convert-0.1.5.4.tar.gz/cast_convert-0.1.5.4/cast_convert/cmd.py
@@ -0,0 +1,46 @@
+import click
+
+from .convert import convert_video, need_to_transcode, get_ffmpeg_cmd
+from .media_info import get_transcode_info
+from .preferences import THREADS
+
+
+@click.group(help="Convert and inspect video for Chromecast compatibility")
+def cmd(debug: bool=False):
+    pass
+
+
+@click.command(help="Generate ffmpeg conversion command.")
+@click.argument("filename")
+def get_cmd(filename: str):
+    print(get_ffmpeg_cmd(filename))
+
+
+@click.command(help="Convert video to Chromecast compatible encodings and container")
+@click.argument("filename")
+@click.option("-t", "--threads", default=THREADS,  type=click.INT,
+              help="Count of threads for ffmpeg to use. Default: %s" % THREADS)
+def convert(filename: str, threads: int):
+    print("%s -> %s" % (filename, convert_video(filename, threads)))
+
+
+@click.command(help="Inspect video for transcoding options")
+@click.argument("filename")
+def inspect(filename: str):
+    info = get_transcode_info(filename)
+
+    if need_to_transcode(info):
+        print("Transcode video to %s" % str(info))
+
+    else:
+        print("No need to transcode %s.")
+
+
+cmd.add_command(get_cmd)
+cmd.add_command(convert)
+cmd.add_command(inspect)
+
+
+if __name__ == "__main__":
+    cmd()
+
