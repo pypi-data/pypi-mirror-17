00a9a55ebca460fc67eefcf96537402696f346a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:50 +0000

    Add package haproxy-herald/0.1.0/haproxy-herald-0.1.0.tar.gz

---
+++ packages/haproxy-herald/haproxy-herald-0.1.0.tar.gz/haproxy-herald-0.1.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='haproxy-herald',
+      version='0.1.0',
+      description='Haproxy load feedback and check agent',
+      url='https://github.com/helpshift/herald',
+      download_url='https://github.com/helpshift/herald/tarball/0.1.0',
+      author='Raghu Udiyar',
+      author_email='raghusiddarth@gmail.com',
+      license='MIT',
+      packages=['herald', 'herald.plugins'],
+      install_requires=['gevent==1.0.2',
+                        'pyyaml==3.11'],
+      package_data={'herald.plugins': ['*.py']},
+      entry_points={
+          'console_scripts': [
+              'herald = herald.herald:main'
+          ]
+      },
+      keywords=['Haproxy']
+      )
