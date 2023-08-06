9808b87b7afa420b78a34548d813c223fd2d48a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:15 +0000

    Add package xiashan/2.0.0/xiashan-2.0.0.zip

---
+++ packages/xiashan/xiashan-2.0.0.zip/xiashan-2.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name           = 'xiashan',
+	version        = '2.0.0',
+	py_modules     = ['xiashan'],
+	author         = 'chenxiaozhong',
+	author_email   = 'chenxiaozhong038@sina.com',
+	url            = 'http://www.headfirstlabs.com',
+	description    = 'A simple printer of nested lists',
+	)
