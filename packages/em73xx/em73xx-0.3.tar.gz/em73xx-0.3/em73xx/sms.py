f7b3a7a01b49cff15473cabbe208ded4f452d050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:33 +0000

    Add package em73xx/0.3/em73xx-0.3.tar.gz

---
+++ packages/em73xx/em73xx-0.3.tar.gz/em73xx-0.3/em73xx/sms.py
@@ -0,0 +1,48 @@
+from .utils import unquote
+
+
+class SMS(object):
+    @classmethod
+    def fromJson(cls, sms_json):
+        return cls(
+            sms_json["sms_id"],
+            sms_json["status"],
+            sms_json["sender"],
+            sms_json["date"],
+            sms_json["time"],
+            sms_json["message"],
+            sms_json["read"]
+        )
+
+    @classmethod
+    def from_AT_response(cls, header, message):
+        (sms_id_txt, status, sender, something, date, time) = header.split(",")
+
+        sms_id = int(sms_id_txt.replace("+CMGL: ", ""))
+        status = unquote(status)
+        sender = unquote(sender)
+        date = unquote(date)
+        time = unquote(time)
+        message = message.strip()
+
+        return cls(sms_id, status, sender, date, time, message, False)
+
+    def __init__(self, sms_id, status, sender, date, time, message, read):
+        self.sms_id = sms_id
+        self.status = status
+        self.sender = sender
+        self.date = date
+        self.time = time
+        self.message = message
+        self.read = read
+
+    def toJson(self):
+        return {
+            "sms_id": self.sms_id,
+            "status": self.status,
+            "sender": self.sender,
+            "date": self.date,
+            "time": self.time,
+            "message": self.message,
+            "read": self.read
+        }
