7738ba341382561ac29a516e7b4fa0e0ad6ef23d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:53 +0000

    Add package bullet-dodger/1.6.4/bullet_dodger-1.6.4.tar.gz

---
+++ packages/bullet-dodger/bullet_dodger-1.6.4.tar.gz/bullet_dodger-1.6.4/bullet_dodger/bonus.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pygame
+from pygame.locals import *
+
+from bullet_dodger.global_constants import RED
+
+class Bonus(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super(Bonus, self).__init__()
+        self.image = pygame.Surface((15, 15))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = x - self.rect.centerx
+        self.rect.y = y - self.rect.centery
---
+++ packages/bullet-dodger/bullet_dodger-1.6.4.tar.gz/bullet_dodger-1.6.4/run_bullet_dodger.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import bullet_dodger.main
+
+bullet_dodger.main.start_screen()
