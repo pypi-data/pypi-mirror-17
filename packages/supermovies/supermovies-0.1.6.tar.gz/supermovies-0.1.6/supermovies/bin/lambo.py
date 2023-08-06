21eeea03a4343ce175cf295412c6cf5a416aef29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:05 +0000

    Add package supermovies/0.1.6/supermovies-0.1.6.tar.gz

---
+++ packages/supermovies/supermovies-0.1.6.tar.gz/supermovies-0.1.6/supermovies/bin/lambo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os, sys
+
+from supermovies import Movie, Movie3D, Playlist, BadMan
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+default_movie_file = os.path.join(BASE_DIR, 'movies.csv')
+
+from_file = sys.argv[1] if len(sys.argv) > 1 else default_movie_file
+
+playlist = Playlist("Fozzie", BadMan)
+playlist.load(from_file)
+movie3d = Movie3D("glee", 5, 20)
+playlist.add_movie(movie3d)
+
+while(True):
+  answer = input("\nHow many viewings? ('quit' to exit) ").lower()
+
+  if(answer.isdigit()):
+    playlist.play(int(answer))
+  elif(answer in ('quit', 'exit')):
+    playlist.print_stats()
+    break
+  else:
+    print("Please enter a number or 'quit'")
+
+playlist.save()
