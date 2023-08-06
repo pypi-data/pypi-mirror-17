129ac48a92761ea12d0ff914ce529701b99e25e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:32 +0000

    Add package azure-cli-webapp/0.1.0b4/azure-cli-webapp-0.1.0b4.tar.gz

---
+++ packages/azure-cli-webapp/azure-cli-webapp-0.1.0b4.tar.gz/azure-cli-webapp-0.1.0b4/azure/cli/command_modules/webapp/_help.py
@@ -0,0 +1,13 @@
+#---------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+#---------------------------------------------------------------------------------------------
+
+from azure.cli.core.help_files import helps #pylint: disable=unused-import
+
+#pylint: disable=line-too-long
+
+helps['webapp'] = """
+    type: group
+    short-summary: Quickly create and deploy mission-critical Web apps at scale
+"""
