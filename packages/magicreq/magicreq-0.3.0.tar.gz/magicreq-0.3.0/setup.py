f6bd3396e34dce072a3f743aa4967bad34893612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:48 +0000

    Add package magicreq/0.3.0/magicreq-0.3.0.tar.gz

---
+++ packages/magicreq/magicreq-0.3.0.tar.gz/magicreq-0.3.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.3.0'
+
+setup(
+    name='magicreq',
+    version=VERSION,
+    author='Justin Patrin',
+    author_email='papercrane@reversefold.com',
+    maintainer='Justin Patrin',
+    maintainer_email='papercrane@reversefold.com',
+    description='magicreq',
+    long_description="""magicreq""",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    scripts=[
+    ],
+    url='https://github.com/reversefold/magicreq',
+)
