757b64ecb0ba6017dbeeba4427a474671d4e136a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:53 +0000

    Add package recombee-api-client/1.2.5/recombee-api-client-1.2.5.tar.gz

---
+++ packages/recombee-api-client/recombee-api-client-1.2.5.tar.gz/recombee-api-client-1.2.5/tests/test_cases/test_add_bookmark.py
@@ -0,0 +1,11 @@
+#
+# This file is auto-generated, do not edit
+#
+
+from tests.test_cases.add_interaction import AddInteractionTest
+from recombee_api_client.api_requests import *
+
+class AddBookmarkTestCase (AddInteractionTest):
+
+    def create_request(self, user_id, item_id, timestamp=None, cascade_create=None):
+        return AddBookmark(user_id, item_id, timestamp=timestamp, cascade_create=cascade_create)
