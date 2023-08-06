33b02ccbdde88bea268204f9d6f576411a4b8946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:22 +0000

    Add package django-cenvars/0.0.0.2/django-cenvars-0.0.0.2.tar.gz

---
+++ packages/django-cenvars/django-cenvars-0.0.0.2.tar.gz/django-cenvars-0.0.0.2/django_cenvars/__info__.py
@@ -0,0 +1,12 @@
+"""
+Project information.
+We use this module as __init__ is a generic module that django requires to be
+specifically set up.
+"""
+VERSION = (0, 0, 0, 2)
+LABELS = dict()
+LABELS['class'] = 'DjangoCenvars'
+LABELS['name'] = 'django_cenvars'
+LABELS['package_name'] = 'django-cenvars'
+LABELS['verbose_name'] = 'Django Centralised Environment Variables Service'
+
