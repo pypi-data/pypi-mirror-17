5ce99518c88cdca43c5fdd1895bd3ab33e33e115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:14 +0000

    Add package freeopcua/0.10.17/freeopcua-0.10.17.tar.gz

---
+++ packages/freeopcua/freeopcua-0.10.17.tar.gz/freeopcua-0.10.17/opcua/common/xmlexporter.py
@@ -0,0 +1,318 @@
+"""
+from a list of nodes in the address space, build an XML file
+format is the one from opc-ua specification
+"""
+import logging
+from collections import OrderedDict
+import xml.etree.ElementTree as Et
+
+from opcua import ua
+from opcua.ua import object_ids as o_ids
+
+
+class XmlExporter(object):
+
+    def __init__(self, server):
+        self.logger = logging.getLogger(__name__)
+        self.server = server
+        self.aliases = {}
+
+        node_set_attributes = OrderedDict()
+        node_set_attributes['xmlns:xsi'] = 'http://www.w3.org/2001/XMLSchema-instance'
+        node_set_attributes['xmlns:uax'] = 'http://opcfoundation.org/UA/2008/02/Types.xsd'
+        node_set_attributes['xmlns:xsd'] = 'http://www.w3.org/2001/XMLSchema'
+        node_set_attributes['xmlns'] = 'http://opcfoundation.org/UA/2011/03/UANodeSet.xsd'
+
+        self.etree = Et.ElementTree(Et.Element('UANodeSet', node_set_attributes))
+
+    def build_etree(self, node_list, uris=None):
+        """
+        Create an XML etree object from a list of nodes; custom namespace uris are optional
+        Args:
+            node_list: list of Node objects for export
+            uris: list of namespace uri strings
+
+        Returns:
+        """
+        self.logger.info('Building XML etree')
+
+        # add all nodes in the list to the XML etree
+        for node in node_list:
+            self.node_to_etree(node)
+
+        # add aliases to the XML etree
+        self._add_alias_els()
+
+        if uris:
+            # add namespace uris to the XML etree
+            self._add_namespace_uri_els(uris)
+
+    def write_xml(self, xmlpath, pretty=True):
+        """
+        Write the XML etree in the exporter object to a file
+        Args:
+            xmlpath: string representing the path/file name
+
+        Returns:
+        """
+        # try to write the XML etree to a file
+        self.logger.info('Exporting XML file to %s', xmlpath)
+        #from IPython import embed
+        #embed()
+        if pretty:
+            indent(self.etree.getroot())
+            self.etree.write(xmlpath, 
+                             short_empty_elements=False, 
+                             encoding='utf-8', 
+                             xml_declaration=True
+                            )
+        else:
+            self.etree.write(xmlpath, 
+                             short_empty_elements=False, 
+                             encoding='utf-8', 
+                             xml_declaration=True)
+
+    def dump_etree(self):
+        """
+        Dump etree to console for debugging
+        Returns:
+        """
+        self.logger.info('Dumping XML etree to console')
+        Et.dump(self.etree)
+
+    def node_to_etree(self, node):
+        """
+        Add the necessary XML sub elements to the etree for exporting the node
+        Args:
+            node: Node object which will be added to XML etree
+
+        Returns:
+        """
+        node_class = node.get_node_class()
+        print("Exporting: ", node)
+
+        if node_class is ua.NodeClass.Object:
+            self.add_etree_object(node)
+        elif node_class is ua.NodeClass.ObjectType:
+            self.add_etree_object_type(node)
+        elif node_class is ua.NodeClass.Variable:
+            self.add_etree_variable(node)
+        elif node_class is ua.NodeClass.VariableType:
+            self.add_etree_variable_type(node)
+        elif node_class is ua.NodeClass.ReferenceType:
+            self.add_etree_reference_type(node)
+        elif node_class is ua.NodeClass.DataType:
+            self.add_etree_datatype(node)
+        elif node_class is ua.NodeClass.Method:
+            self.add_etree_method(node)
+        else:
+            self.logger.info("Exporting node class not implemented: %s ", node_class)
+
+    def _add_sub_el(self, el, name, text):
+        child_el = Et.SubElement(el, name)
+        child_el.text = text
+        return child_el
+
+    def _add_node_common(self, nodetype, node):
+        browsename = node.get_browse_name().to_string()
+        nodeid = node.nodeid.to_string()
+        parent = node.get_parent()
+        displayname = node.get_display_name().Text.decode('utf-8')
+        desc = node.get_description().Text
+        print("NODE COMMON", node)
+        node_el = Et.SubElement(self.etree.getroot(), nodetype)
+        node_el.attrib["NodeId"] = nodeid
+        node_el.attrib["BrowseName"] = browsename
+        if parent is not None:
+            node_class = node.get_node_class()
+            if node_class in (ua.NodeClass.Object, ua.NodeClass.Variable, ua.NodeClass.Method):
+                node_el.attrib["ParentNodeId"] = parent.nodeid.to_string()
+        self._add_sub_el(node_el, 'DisplayName', displayname)
+        if desc not in (None, ""):
+            self._add_sub_el(node_el, 'Description', desc.decode('utf-8'))
+        #FIXME: add WriteMask and UserWriteMask
+        return node_el
+
+    def add_etree_object(self, node):
+        """
+        Add a UA object element to the XML etree
+        """
+        obj_el = self._add_node_common("UAObject", node)
+        var = node.get_attribute(ua.AttributeIds.EventNotifier)
+        if var.Value.Value != 0:
+            obj_el.attrib["EventNotifier"] = str(var.Value.Value)
+        self._add_ref_els(obj_el, node)
+
+    def add_etree_object_type(self, node):
+        """
+        Add a UA object type element to the XML etree
+        """
+        obj_el = self._add_node_common("UAObjectType", node)
+        abstract = node.get_attribute(ua.AttributeIds.IsAbstract).Value.Value
+        if abstract:
+            obj_el.attrib["IsAbstract"] = 'true'
+        self._add_ref_els(obj_el, node)
+
+    def add_variable_common(self, node, el):
+        dtype = node.get_data_type()
+        if dtype.Identifier in o_ids.ObjectIdNames:
+            dtype_name = o_ids.ObjectIdNames[dtype.Identifier]
+            self.aliases[dtype_name] = dtype.to_string()
+        else:
+            dtype_name = dtype.to_string()
+        rank = node.get_value_rank()
+        if rank != -1:
+            el.attrib["ValueRank"] = str(rank)
+        #var = node.get_attribute(ua.AttributeIds.ArrayDimensions())
+        #self._addobj_el.attrib["ArrayDimensions"] = str(var.Value.Value)
+        el.attrib["DataType"] = dtype_name
+        value_to_etree(el, dtype_name, dtype, node)
+
+    def add_etree_variable(self, node):
+        """
+        Add a UA variable element to the XML etree
+        """
+        var_el = self._add_node_common("UAVariable", node)
+        self.add_variable_common(node, var_el)
+
+        accesslevel = node.get_attribute(ua.AttributeIds.AccessLevel).Value.Value
+        useraccesslevel = node.get_attribute(ua.AttributeIds.UserAccessLevel).Value.Value
+
+        # We only write these values if they are different from defaults
+        # Not sure where default is defined....
+        if accesslevel not in (0, ua.AccessLevel.CurrentRead.mask):
+            var_el.attrib["AccessLevel"] = str(accesslevel)
+        if useraccesslevel not in (0, ua.AccessLevel.CurrentRead.mask):
+            var_el.attrib["UserAccessLevel"] = str(useraccesslevel)
+
+        var = node.get_attribute(ua.AttributeIds.MinimumSamplingInterval)
+        if var.Value.Value:
+            var_el.attrib["MinimumSamplingInterval"] = str(var.Value.Value)
+        var = node.get_attribute(ua.AttributeIds.Historizing)
+        if var.Value.Value:
+            var_el.attrib["Historizing"] = 'true'
+        self._add_ref_els(var_el, node)
+
+    def add_etree_variable_type(self, node):
+        """
+        Add a UA variable type element to the XML etree
+        """
+
+        var_el = self._add_node_common("UAVariableType", node)
+        self.add_variable_common(node, var_el)
+
+        abstract = node.get_attribute(ua.AttributeIds.IsAbstract)
+        if abstract.Value.Value:
+            var_el.attrib["IsAbstract"] = "true" 
+
+        self._add_ref_els(var_el, node)
+
+    def add_etree_method(self, node):
+        obj_el = self._add_node_common("UAMethod", node)
+
+        var = node.get_attribute(ua.AttributeIds.Executable)
+        if var.Value.Value is False:
+            obj_el.attrib["Executable"] = "false"
+        var = node.get_attribute(ua.AttributeIds.UserExecutable)
+        if var.Value.Value is False:
+            obj_el.attrib["UserExecutable"] = "false"
+        self._add_ref_els(obj_el, node)
+
+    def add_etree_reference_type(self, obj):
+        obj_el = self._add_node_common("UAReferenceType", obj)
+        var = obj.get_attribute(ua.AttributeIds.InverseName)
+        if var is not None and var.Value.Value is not None:
+            self._add_sub_el(obj_el, 'InverseName', var.Value.Value.Text.decode('utf-8'))
+        self._add_ref_els(obj_el, obj)
+
+    def add_etree_datatype(self, obj):
+        """
+        Add a UA data type element to the XML etree
+        """
+        obj_el = self._add_node_common("UADataType", obj)
+        self._add_ref_els(obj_el, obj)
+
+    def _add_namespace_uri_els(self, uris):
+        nuris_el = Et.Element('NamespaceUris')
+
+        for uri in uris:
+            self._add_sub_el(nuris_el, 'Uri', uri)
+
+        self.etree.getroot().insert(0, nuris_el)
+
+    def _add_alias_els(self):
+        aliases_el = Et.Element('Aliases')
+
+        for k, v in self.aliases.items():
+            ref_el = Et.SubElement(aliases_el, 'Alias', Alias=k)
+            ref_el.text = v
+
+        self.etree.getroot().insert(0, aliases_el)
+
+    def _add_ref_els(self, parent_el, obj):
+        refs = obj.get_references()
+        refs_el = Et.SubElement(parent_el, 'References')
+
+        for ref in refs:
+            if ref.ReferenceTypeId.Identifier in o_ids.ObjectIdNames:
+                ref_name = o_ids.ObjectIdNames[ref.ReferenceTypeId.Identifier]
+            else:
+                ref_name = ref.ReferenceTypeId.to_string()
+            ref_el = Et.SubElement(refs_el, 'Reference')
+            ref_el.attrib['ReferenceType'] = ref_name
+            if not ref.IsForward:
+                ref_el.attrib['IsForward'] = 'false'
+            ref_el.text = ref.NodeId.to_string()
+
+            self.aliases[ref_name] = ref.ReferenceTypeId.to_string()
+
+
+def value_to_etree(el, dtype_name, dtype, node):
+    var = node.get_data_value().Value
+    if var.Value is not None:
+        val_el = Et.SubElement(el, 'Value')
+        _value_to_etree(val_el, dtype_name, dtype, var.Value)
+
+
+def _value_to_etree(el, dtype_name, dtype, val):
+    if isinstance(val, (list, tuple)):
+        list_el = Et.SubElement(el, "uax:ListOf" + dtype_name)
+        for nval in val:
+            _value_to_etree(list_el, dtype_name, dtype, nval)
+    else:
+        if dtype.Identifier is int and dtype.Identifier > 21:  # this is an extentionObject:
+            _extobj_to_etree(el, dtype_name, dtype, val)
+        else:
+            val_el = Et.SubElement(el, "uax:" + dtype_name)
+            val_el.text = str(val)
+
+
+def _extobj_to_etree(val_el, dtype_name, dtype, val):
+    obj_el = Et.SubElement(val_el, "uax:ExtensionObject")
+    type_el = Et.SubElement(obj_el, "uax:TypeId")
+    id_el = Et.SubElement(type_el, "uax:Identifier")
+    id_el.text = val.TypeId.to_string()
+    body_el = Et.SubElement(obj_el, "uax:Body")
+    struct_el = Et.SubElement(body_el, "uax:" + dtype_name)
+    # FIXME: finish
+   
+
+def indent(elem, level=0):
+    '''
+    copy and paste from http://effbot.org/zone/element-lib.htm#prettyprint
+    it basically walks your tree and adds spaces and newlines so the tree is
+    printed in a nice way
+    '''
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
---
+++ packages/freeopcua/freeopcua-0.10.17.tar.gz/freeopcua-0.10.17/opcua/common/xmlimporter.py
@@ -0,0 +1,257 @@
+"""
+add node defined in XML to address space
+format is the one from opc-ua specification
+"""
+import logging
+import sys
+
+
+from opcua import ua
+from opcua.common import xmlparser
+
+
+def ua_type_to_python(val, uatype):
+    if uatype.startswith("Int") or uatype.startswith("UInt"):
+        return int(val)
+    elif uatype in ("String"):
+        return val
+    elif uatype in ("Bytes", "Bytes", "ByteString", "ByteArray"):
+        if sys.version_info.major > 2:
+            return bytes(val, 'utf8')
+        else:
+            return val
+    else:
+        raise Exception("uatype nopt handled", uatype, " for val ", val)
+
+
+def to_python(val, obj, attname):
+    if isinstance(obj, ua.NodeId) and attname == "Identifier":
+        return ua.NodeId.from_string(val)
+    else:
+        return ua_type_to_python(val, obj.ua_types[attname])
+
+
+class XmlImporter(object):
+
+    def __init__(self, server):
+        self.logger = logging.getLogger(__name__)
+        self.parser = None
+        self.server = server
+
+    def import_xml(self, xmlpath, act_server):
+        """
+        import xml and return added nodes
+        """
+        self.logger.info("Importing XML file %s", xmlpath)
+        self.parser = xmlparser.XMLParser(xmlpath, act_server)
+        nodes = []
+        for nodedata in self.parser:
+            if nodedata.nodetype == 'UAObject':
+                node = self.add_object(nodedata)
+            elif nodedata.nodetype == 'UAObjectType':
+                node = self.add_object_type(nodedata)
+            elif nodedata.nodetype == 'UAVariable':
+                node = self.add_variable(nodedata)
+            elif nodedata.nodetype == 'UAVariableType':
+                node = self.add_variable_type(nodedata)
+            elif nodedata.nodetype == 'UAReferenceType':
+                node = self.add_reference_type(nodedata)
+            elif nodedata.nodetype == 'UADataType':
+                node = self.add_datatype(nodedata)
+            elif nodedata.nodetype == 'UAMethod':
+                node = self.add_method(nodedata)
+            else:
+                self.logger.warning("Not implemented node type: %s ", nodedata.nodetype)
+                continue
+            nodes.append(node)
+        return nodes
+
+    def _get_node(self, obj):
+        node = ua.AddNodesItem()
+        node.RequestedNewNodeId = ua.NodeId.from_string(obj.nodeid)
+        node.BrowseName = ua.QualifiedName.from_string(obj.browsename)
+        node.NodeClass = getattr(ua.NodeClass, obj.nodetype[2:])
+        if obj.parent:
+            node.ParentNodeId = ua.NodeId.from_string(obj.parent)
+        if obj.parentlink:
+            node.ReferenceTypeId = self.to_nodeid(obj.parentlink)
+        if obj.typedef:
+            node.TypeDefinition = ua.NodeId.from_string(obj.typedef)
+        return node
+
+    def to_nodeid(self, nodeid):
+        if not nodeid:
+            return ua.NodeId(ua.ObjectIds.String)
+        elif "=" in nodeid:
+            return ua.NodeId.from_string(nodeid)
+        elif hasattr(ua.ObjectIds, nodeid):
+            return ua.NodeId(getattr(ua.ObjectIds, nodeid))
+        else:
+            if nodeid in self.parser.aliases:
+                nodeid = self.parser.aliases[nodeid]
+            else:
+                nodeid = "i={}".format(getattr(ua.ObjectIds, nodeid))
+            return ua.NodeId.from_string(nodeid)
+
+    def add_object(self, obj):
+        node = self._get_node(obj)
+        attrs = ua.ObjectAttributes()
+        if obj.desc:
+            attrs.Description = ua.LocalizedText(obj.desc)
+        attrs.DisplayName = ua.LocalizedText(obj.displayname)
+        attrs.EventNotifier = obj.eventnotifier
+        node.NodeAttributes = attrs
+        res = self.server.add_nodes([node])
+        self._add_refs(obj)
+        return res[0].AddedNodeId
+
+    def add_object_type(self, obj):
+        node = self._get_node(obj)
+        attrs = ua.ObjectTypeAttributes()
+        if obj.desc:
+            attrs.Description = ua.LocalizedText(obj.desc)
+        attrs.DisplayName = ua.LocalizedText(obj.displayname)
+        attrs.IsAbstract = obj.abstract
+        node.NodeAttributes = attrs
+        res = self.server.add_nodes([node])
+        self._add_refs(obj)
+        return res[0].AddedNodeId
+
+    def add_variable(self, obj):
+        node = self._get_node(obj)
+        attrs = ua.VariableAttributes()
+        if obj.desc:
+            attrs.Description = ua.LocalizedText(obj.desc)
+        attrs.DisplayName = ua.LocalizedText(obj.displayname)
+        attrs.DataType = self.to_nodeid(obj.datatype)
+        # if obj.value and len(obj.value) == 1:
+        if obj.value is not None:
+            attrs.Value = self._add_variable_value(obj, )
+        if obj.rank:
+            attrs.ValueRank = obj.rank
+        if obj.accesslevel:
+            attrs.AccessLevel = obj.accesslevel
+        if obj.useraccesslevel:
+            attrs.UserAccessLevel = obj.useraccesslevel
+        if obj.minsample:
+            attrs.MinimumSamplingInterval = obj.minsample
+        if obj.dimensions:
+            attrs.ArrayDimensions = obj.dimensions
+        node.NodeAttributes = attrs
+        res = self.server.add_nodes([node])
+        self._add_refs(obj)
+        return res[0].AddedNodeId
+    
+    def _make_ext_obj(sefl, obj):
+        ext = getattr(ua, obj.objname)()
+        for name, val in obj.body.items():
+            if type(val) is str:
+                raise Exception("Error val should a dict", name, val)
+            else:
+                for attname, v in val.items():
+                    if type(v) is str:
+                        setattr(ext, attname, to_python(v, ext, attname))
+                    else:
+                        for attname2, v2 in v.items():
+                            obj2 = getattr(ext, attname)
+                            setattr(obj2, attname2, to_python(v2, obj2, attname2))
+        return ext
+
+    def _add_variable_value(self, obj):
+        """
+        Returns the value for a Variable based on the objects valuetype. 
+        """
+        if obj.valuetype == 'ListOfExtensionObject':
+            values = []
+            for ext in obj.value:
+                extobj = self._make_ext_obj(ext)
+                values.append(extobj)
+            return values
+        elif obj.valuetype.startswith("ListOf"):
+            vtype = obj.valuetype[6:]
+            return [getattr(ua, vtype)(v) for v in obj.value]
+        else:
+            return ua.Variant(obj.value, getattr(ua.VariantType, obj.valuetype))
+
+    def add_variable_type(self, obj):
+        node = self._get_node(obj)
+        attrs = ua.VariableTypeAttributes()
+        if obj.desc:
+            attrs.Description = ua.LocalizedText(obj.desc)
+        attrs.DisplayName = ua.LocalizedText(obj.displayname)
+        attrs.DataType = self.to_nodeid(obj.datatype)
+        if obj.value and len(obj.value) == 1:
+            attrs.Value = obj.value[0]
+        if obj.rank:
+            attrs.ValueRank = obj.rank
+        if obj.abstract:
+            attrs.IsAbstract = obj.abstract
+        if obj.dimensions:
+            attrs.ArrayDimensions = obj.dimensions
+        node.NodeAttributes = attrs
+        res = self.server.add_nodes([node])
+        self._add_refs(obj)
+        return res[0].AddedNodeId
+
+    def add_method(self, obj):
+        node = self._get_node(obj)
+        attrs = ua.MethodAttributes()
+        if obj.desc:
+            attrs.Description = ua.LocalizedText(obj.desc)
+        attrs.DisplayName = ua.LocalizedText(obj.displayname)
+        if obj.accesslevel:
+            attrs.AccessLevel = obj.accesslevel
+        if obj.useraccesslevel:
+            attrs.UserAccessLevel = obj.useraccesslevel
+        if obj.minsample:
+            attrs.MinimumSamplingInterval = obj.minsample
+        if obj.dimensions:
+            attrs.ArrayDimensions = obj.dimensions
+        node.NodeAttributes = attrs
+        res = self.server.add_nodes([node])
+        self._add_refs(obj)
+        return res[0].AddedNodeId
+
+    def add_reference_type(self, obj):
+        node = self._get_node(obj)
+        attrs = ua.ReferenceTypeAttributes()
+        if obj.desc:
+            attrs.Description = ua.LocalizedText(obj.desc)
+        attrs.DisplayName = ua.LocalizedText(obj.displayname)
+        if obj. inversename:
+            attrs.InverseName = ua.LocalizedText(obj.inversename)
+        if obj.abstract:
+            attrs.IsAbstract = obj.abstract
+        if obj.symmetric:
+            attrs.Symmetric = obj.symmetric
+        node.NodeAttributes = attrs
+        res = self.server.add_nodes([node])
+        self._add_refs(obj)
+        return res[0].AddedNodeId
+
+    def add_datatype(self, obj):
+        node = self._get_node(obj)
+        attrs = ua.DataTypeAttributes()
+        if obj.desc:
+            attrs.Description = ua.LocalizedText(obj.desc)
+        attrs.DisplayName = ua.LocalizedText(obj.displayname)
+        if obj.abstract:
+            attrs.IsAbstract = obj.abstract
+        node.NodeAttributes = attrs
+        res = self.server.add_nodes([node])
+        self._add_refs(obj)
+        return res[0].AddedNodeId
+
+    def _add_refs(self, obj):
+        if not obj.refs:
+            return
+        refs = []
+        for data in obj.refs:
+            ref = ua.AddReferencesItem()
+            ref.IsForward = True
+            ref.ReferenceTypeId = self.to_nodeid(data.reftype)
+            ref.SourceNodeId = ua.NodeId.from_string(obj.nodeid)
+            ref.TargetNodeClass = ua.NodeClass.DataType
+            ref.TargetNodeId = ua.NodeId.from_string(data.target)
+            refs.append(ref)
+        self.server.add_references(refs)
