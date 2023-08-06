af202a72da646e09aa39bb2552321ff3b11253cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:17 +0000

    Add package CHJ/1.0.2/CHJ-1.0.2.tar.gz

---
+++ packages/CHJ/CHJ-1.0.2.tar.gz/CHJ-1.0.2/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2003-2016 Mag. Christian Tanzer. All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+# ****************************************************************************
+# This module is licensed under the terms of the BSD 3-Clause License
+# <http://www.c-tanzer.at/license/bsd_3c.html>.
+# ****************************************************************************
+#
+#++
+# Name
+#    CHJ.__init__
+#
+# Purpose
+#    Package for CSS/HTML/Javascript support
+#
+# Revision Dates
+#    11-Oct-2016 (CT) Creation (factor from `GTW`)
+#    ««revision-date»»···
+#--
+
+from   _TFL.Package_Namespace import Package_Namespace
+
+__version__ = "1.0.2"
+
+CHJ = Package_Namespace ()
+
+del Package_Namespace
+
+### __END__ CHJ.__init__
