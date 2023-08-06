029d95a353b48d0dc7725b142ce5cd2a87013b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:46 +0000

    Add package nesterJackie/1.1.0/nesterJackie-1.1.0.zip

---
+++ packages/nesterJackie/nesterJackie-1.1.0.zip/nesterJackie-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nesterJackie',
+    version = '1.1.0',
+    py_modules = ['nesterJackie'],
+    author = 'Jackie',
+    author_email = 'maoxiaojieh@163.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists'
+    )
