c713218b51c72f06965fa927f89dcf4c751939d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:52 +0000

    Add package ImportFromBitbucket/0.1.5/ImportFromBitbucket-0.1.5.zip

---
+++ packages/ImportFromBitbucket/ImportFromBitbucket-0.1.5.zip/ImportFromBitbucket-0.1.5/ImportFromBitbucket.py
@@ -0,0 +1,11 @@
+import urllib.request
+def loadPyFile(fileName):
+    webFile='https://bitbucket.org/juan_ignacio_garcia_garcia/commutativemonoids/raw/master/'+fileName
+    print('downloading...',webFile)
+    response=urllib.request.urlopen(webFile)
+    content = response.read()
+    pos=len(webFile)-webFile[-1::-1].index('/')
+    file=open(webFile[pos:],'w')
+    file.write(content.decode())
+    file.close()
+
