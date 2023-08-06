e1fa30c209e44d5e7310fd6fe4d138975f7dfe98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:45 +0000

    Add package looplistawy/1.3.0/looplistawy-1.3.0.tar.gz

---
+++ packages/looplistawy/looplistawy-1.3.0.tar.gz/looplistawy-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name 		= 'looplistawy',
+	version 	= '1.3.0',
+	py_modules 	= ['looplistawy'],
+	author 		= 'wesley_viana',
+	author_email 	= 'wesleyviana2201@gmail.com',
+	url 		= 'http://www.headfirstlabs.com',
+	description 	= 'A simple printer of nested lists',
+	)
