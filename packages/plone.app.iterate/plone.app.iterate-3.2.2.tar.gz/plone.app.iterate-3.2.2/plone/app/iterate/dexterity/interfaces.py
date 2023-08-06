e08991c252c5101a0bf23f8f96f47dc75b41f1ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:57 +0000

    Add package plone.app.iterate/3.2.2/plone.app.iterate-3.2.2.tar.gz

---
+++ packages/plone.app.iterate/plone.app.iterate-3.2.2.tar.gz/plone.app.iterate-3.2.2/plone/app/iterate/dexterity/interfaces.py
@@ -0,0 +1,11 @@
+from plone.app.iterate.interfaces import IIterateAware
+from zope.interface import Attribute
+from z3c.relationfield.interfaces import IRelationValue
+
+
+class IStagingRelationValue(IRelationValue):
+    pass
+
+
+class IDexterityIterateAware(IIterateAware):
+    pass
