6245eed94abb0c31bcb3d535eeb4e1a326ab6ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:36 +0000

    Add package range_test/1.0.0/range_test-1.0.0.tar.gz

---
+++ packages/range_test/range_test-1.0.0.tar.gz/range_test-1.0.0/range.py
@@ -0,0 +1,49 @@
+for num in range(2):
+    print(num)
+
+
+movies = [
+    "The Holy Grail",
+    1975,
+    "Terry Jones & Terry Gilliam",
+    911111,
+    ["Graham Chapman",
+     ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle",
+      ["abc", "defg", "highk", "Terry Jones"]
+      ]
+     ]
+]
+
+movies1 =  ["The Holy Grail",1975,
+            ["Terry Jones","Terry Gilliam"],
+            [911111,"trdfc"]]
+
+def print_list2(the_list,level):
+    '''"""This function takes a positional argument called "the_list", which
+is any Python list (of - possibly - nested lists). Each data item in the
+provided list is (recursively) printed to the screen on it's own line.
+a second paramter level is used to insert "tab" when a nested list is encountered'''
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_list2(each_item,level+1)
+            print(level)
+
+        else:
+            for tab_stop in range(level):
+                #print("we will put a tab here")
+                print("\t", end='')
+            print(each_item)
+
+
+
+
+#invoke the function
+print_list2(movies1,1)
+
+
+
+
+
+
+
+
---
+++ packages/range_test/range_test-1.0.0.tar.gz/range_test-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'range_test',
+	version = '1.0.0',
+	py_modules = ['range'],
+	author = 'wenwen',
+	author_email= 'deutschland.gray@gmail.com',
+	url = 'http://www.wenwen.rocks',
+	description='play around range'
+)
