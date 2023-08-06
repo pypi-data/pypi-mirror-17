c493b5d12a228527934396403963b06e3c491aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:20 +0000

    Add package ttooongli-nester/1.0.0/ttooongli_nester-1.0.0.tar.gz

---
+++ packages/ttooongli-nester/ttooongli_nester-1.0.0.tar.gz/ttooongli_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = "ttooongli_nester",
+    version      = "1.0.0",
+    py_modules   = ["ttooongli_nester"],
+    author       = "tongli",
+    author_email = "tonglisir@sina.com",
+    url          = 'http://www.headfirstlabs.com',
+    description  = "tongli dr nester",
+)
