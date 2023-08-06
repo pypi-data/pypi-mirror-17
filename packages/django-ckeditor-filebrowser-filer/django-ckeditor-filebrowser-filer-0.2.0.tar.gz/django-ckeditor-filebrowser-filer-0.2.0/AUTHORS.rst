8fa4a31959e58f7fe44c3c7833265a77c7386d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:51 +0000

    Add package django-ckeditor-filebrowser-filer/0.2.0/django-ckeditor-filebrowser-filer-0.2.0.tar.gz

---
+++ packages/django-ckeditor-filebrowser-filer/django-ckeditor-filebrowser-filer-0.2.0.tar.gz/django-ckeditor-filebrowser-filer-0.2.0/AUTHORS.rst
@@ -0,0 +1,19 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Iacopo Spalletti <i.spalletti@nephila.it>
+* Original code by Enver Bisevac
+
+Contributors
+------------
+
+Adam Chainz
+Andrea Stagi
+bee-keeper
+Gasim Gasimzada
+Paolo Romolini
+Richard Terry
---
+++ packages/django-ckeditor-filebrowser-filer/django-ckeditor-filebrowser-filer-0.2.0.tar.gz/django-ckeditor-filebrowser-filer-0.2.0/ckeditor_filebrowser_filer/urls.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, url
+from .views import *
+
+urlpatterns = patterns('',
+    url(r'version/$', filer_version, name='filer_version'),
+    url(r'setting/(?P<setting>\w+)/$', get_setting, name='get_setting'),
+    url(r'url_reverse/$', url_reverse, name='js_url_reverse'),
+    url(r'url_image/(?P<image_id>\d+)/$', url_image, name='url_image'),
+    url(r'url_image/(?P<image_id>\d+)/(?P<thumb_options>\d+)/$', url_image, name='url_image'),
+    url(r'url_image/(?P<image_id>\d+)/(?P<width>\d+)/(?P<height>\d+)/$', url_image, name='url_image'),
+    url(r'serve/(?P<image_id>\d+)/$', serve_image, name='serve_image'),
+    url(r'serve/(?P<image_id>\d+)/(?P<thumb_options>\d+)/$', serve_image, name='serve_image'),
+    url(r'serve/(?P<image_id>\d+)/(?P<width>\d+)/(?P<height>\d+)/$', serve_image, name='serve_image'),
+    url(r'thumbnail_options/$', thumbnail_options, name='thumbnail_options'),
+) 
