beaf4f7ae48fe3ae4d7c8cd6945c15c5d921e3e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:34 +0000

    Add package mcutool/1.0.0/mcutool-1.0.0.zip

---
+++ packages/mcutool/mcutool-1.0.0.zip/mcutool-1.0.0/mcutool.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _*_ coding=gbk _*_
+
+
+import wx
+from mcutool.gui import MyFrame
+
+if __name__=="__main__":
+    app = wx.App()
+    MyFrame()
+    app.MainLoop()  
+    
