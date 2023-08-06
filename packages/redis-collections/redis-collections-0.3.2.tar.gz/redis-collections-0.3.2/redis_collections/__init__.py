f116b1408c2b6a305369b90bb7238b8a0238c308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:58 +0000

    Add package redis-collections/0.3.2/redis-collections-0.3.2.tar.gz

---
+++ packages/redis-collections/redis-collections-0.3.2.tar.gz/redis-collections-0.3.2/redis_collections/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+
+__title__ = 'redis-collections'
+__version__ = '0.3.2'
+__author__ = 'Honza Javorek'
+__license__ = 'ISC'
+__copyright__ = 'Copyright 2013-? Honza Javorek'
+
+
+from .base import RedisCollection  # NOQA
+from .dicts import DefaultDict, Dict, Counter  # NOQA
+from .lists import Deque, List  # NOQA
+from .sets import Set  # NOQA
+
+__all__ = [
+    'Counter',
+    'DefaultDict',
+    'Deque',
+    'Dict',
+    'List',
+    'RedisCollection',
+    'Set',
+]
