601d93b3d754e2de38ff95280955fd2206ae1685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:07 +0000

    Add package nest_loop/2.1.0/nest_loop-2.1.0.zip

---
+++ packages/nest_loop/nest_loop-2.1.0.zip/nest_loop-2.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+
+    name       =     'nest_loop',
+    version    =     '2.1.0',
+    py_modules =     ['lala'],
+    author     =     'leeway',
+    authoremail=     'lycanleeway@163.com',
+    url        =     'http://user.qzone.qq.com/519586955',
+    description=     'A simple test for python language',
+    )
