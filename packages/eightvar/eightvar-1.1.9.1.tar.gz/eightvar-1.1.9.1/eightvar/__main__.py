1a9565cfeeee21dea4b5d639e5e01df91868c7e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:37 +0000

    Add package eightvar/1.1.9.1/eightvar-1.1.9.1.tar.gz

---
+++ packages/eightvar/eightvar-1.1.9.1.tar.gz/eightvar-1.1.9.1/eightvar/__main__.py
@@ -0,0 +1,3 @@
+import __init__ as ev
+
+ev.prnt('8v.1.1.9v.out"It\ works!"fin')
---
+++ packages/eightvar/eightvar-1.1.9.1.tar.gz/eightvar-1.1.9.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'eightvar',
+  packages = ['eightvar'], # this must be the same as the name above
+  version = '1.1.9.1',
+  description = '8var is an esoteric programming language, and this Python module is an interpreter for it. See http://esolangs.org/wiki/8var',
+  author = 'Sai Raghu Neelothpal Saraswatibhatla',
+  author_email = 'neelu.rsa@gmail.com',
+  url = 'https://github.com/neelusb/8var-pymodule',
+  download_url = 'https://github.com/neelusb/8var-pymodule/tarball/1.1.9.1',
+  keywords = ['8var', 'eightvar', 'esolang'],
+  classifiers = [],
+)
