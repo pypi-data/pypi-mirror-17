78e2ee0067c3e6bfff918f7074d94935a5a575b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:21 +0000

    Add package simplesqltool/1.3/simplesqltool-1.3.tar.gz

---
+++ packages/simplesqltool/simplesqltool-1.3.tar.gz/simplesqltool-1.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='simplesqltool', version='1.3',
+      description='simpleutil tools for sql',
+      author='Yong Wang',
+      packages=['simplesqltool'],
+      author_email='ever0702@gmail.com')
