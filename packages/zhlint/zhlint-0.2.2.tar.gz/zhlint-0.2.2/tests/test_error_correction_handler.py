600a5c79d03829dd558caab5903de5fb5e8b86ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:06 +0000

    Add package zhlint/0.2.2/zhlint-0.2.2.tar.gz

---
+++ packages/zhlint/zhlint-0.2.2.tar.gz/zhlint-0.2.2/tests/test_error_correction_handler.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from __future__ import (
+    division, absolute_import, print_function, unicode_literals,
+)
+from builtins import *                  # noqa
+from future.builtins.disabled import *  # noqa
+
+import os.path
+
+from zhlint.error_detection import (
+    process_errors,
+)
+from zhlint.preprocessor import transform
+from zhlint.error_correction import (
+    ErrorCorrectionHandler,
+    DiffOperation,
+    DiffOperationExecutor,
+)
+
+
+DATA = os.path.join(
+    os.path.dirname(__file__),
+    'data',
+)
+
+
+def load_test_md(name):
+    return open(os.path.join(DATA, name), encoding='utf-8').read()
+
+
+def load_and_check(name):
+    content = load_test_md(name)
+    elements = transform(content)
+    h = ErrorCorrectionHandler(content, elements)
+    process_errors(h, elements[0])
+    executor = DiffOperationExecutor(h.diffs, h.raw_lines)
+    return executor
+
+
+def test_simple():
+    executor = load_and_check('correction_simple.md')
+    assert [
+        DiffOperation.replace(1, 3, val='，'),
+        DiffOperation.delete(1, 4),
+        DiffOperation.replace(1, 7, val='。'),
+    ] == executor.diffs
+
+    assert '中文，标点。\n' == executor.apply_diff_operations()
+
+
+def test_latex():
+    executor = load_and_check('correction_latex.md')
+    assert [
+        DiffOperation.replace(1, 40, val='。'),
+    ] == executor.diffs
+
+    assert (
+        '第 (5) 步的重定向 transitions $ChildrenTrans$。\n' ==
+        executor.apply_diff_operations()
+    )
---
+++ packages/zhlint/zhlint-0.2.2.tar.gz/zhlint-0.2.2/zhlint/metadata.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import (
+    division, absolute_import, print_function, unicode_literals,
+)
+
+
+NAME = 'zhlint'
+VERSION = '0.2.2'
+AUTHORS = [
+    'huntzhan',
+]
+EMAILS = [
+    'programmer.zhx@gmail.com',
+]
+LICENSE = ''
+URL = ''
+DESCRIPTION = 'automated checker of chinese document.'
