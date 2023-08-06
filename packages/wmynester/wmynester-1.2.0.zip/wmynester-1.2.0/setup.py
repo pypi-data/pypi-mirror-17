368d5d8ede6a4e2bd2d4d6ab13c41834f0c5bc8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:57 +0000

    Add package wmynester/1.2.0/wmynester-1.2.0.zip

---
+++ packages/wmynester/wmynester-1.2.0.zip/wmynester-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name 		= "wmynester",
+	version 	= "1.2.0",
+	py_modules	= ['wmynester'],
+	author		= "吴孟尧",
+	author_email= "1780574563@qq.com",
+	description	= "A simple printer of nested list",
+	)
