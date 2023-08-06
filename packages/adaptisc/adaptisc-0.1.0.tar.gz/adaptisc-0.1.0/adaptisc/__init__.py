e5ec46599f4ff3352b524241d87fcf41567dc625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:17 +0000

    Add package adaptisc/0.1.0/adaptisc-0.1.0.tar.gz

---
+++ packages/adaptisc/adaptisc-0.1.0.tar.gz/adaptisc-0.1.0/adaptisc/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    adapt
+    ~~~~~
+
+    Linux USB JTAG controller for Digilent boards.
+
+    :copyright: (c) 2014 by Jessy Diamond Exum.
+    :license: Pending, see LICENSE for more details.
+"""
+
+__version__ = '0.1.0'
