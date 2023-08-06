79ba19d85c70ad0df8d538b649f6beb5ec28aef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:38 +0000

    Add package minpy/0.2.0/minpy-0.2.0.tar.gz

---
+++ packages/minpy/minpy-0.2.0.tar.gz/minpy-0.2.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import setuptools
+import os.path
+
+with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as f:
+    version = f.readline().strip()
+
+setuptools.setup(
+    name='minpy',
+    version=version,
+    description='Pure NumPy practice with third-party operator integration.',
+    maintainer='DMLC',
+    maintainer_email='minerva-support@googlegroups.com',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pillow',
+        'mxnet',
+        'semantic-version',
+        'six',
+        'h5py',
+    ],
+    data_files=[('', ['VERSION'])],
+    url='https://github.com/dmlc/minpy')
