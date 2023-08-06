9f87f20b586ae5b6b23f2d42aaaf51080d9fc241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:44 +0000

    Add package django_universe/0.91/django_universe-0.91.tar.gz

---
+++ packages/django_universe/django_universe-0.91.tar.gz/django_universe-0.91/django_universe/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render,HttpResponse,render_to_response
+from django.conf import settings
+import json,os
+# Create your views here.
+
+def index(request):
+    views_dict = {}
+    apps = settings.APPS_FOR_UNIVERSE
+    for app in apps:
+	
+	#views
+	
+	list1 = []
+	list2 = []
+	views_file = os.path.join(settings.BASE_DIR,app,'views.py')
+	for line in file(views_file):
+	    if line.startswith('def'):
+		list1.append(line[4:line.index('(')])
+	views_dict[app] = list1
+	
+    return render('code_base.html',{})
