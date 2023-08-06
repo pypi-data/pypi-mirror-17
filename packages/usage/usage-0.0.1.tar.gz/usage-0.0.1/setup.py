2516d20ff3706c7ab1a62609706b7fa6f3a00406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:59 +0000

    Add package usage/0.0.1/usage-0.0.1.tar.gz

---
+++ packages/usage/usage-0.0.1.tar.gz/usage-0.0.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+VERSION = '0.0.1'
+LONG_DESCRIPTION = ''
+INSTALL_REQUIRES = []
+ENTRY_POINTS = {
+    'console_scripts': [
+        'usage = usage:main',
+    ]
+}
+
+
+setup(
+    name='usage',
+    version=VERSION,
+    keywords=[],
+    description='Usage',
+    long_description=LONG_DESCRIPTION,
+    author='xlzd',
+    author_email='i@xlzd.me',
+    license='WTFPL',
+    url='https://github.com/xlzd/usage',
+    download_url='https://github.com/xlzd/usage',
+    install_requires=INSTALL_REQUIRES,
+    packages=['usage'],
+    entry_points=ENTRY_POINTS,
+    classifiers=[]
+)
