a928e36370d478d3df24a4a355d2c52cea8c5343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:31 +0000

    Add package django_limiter/1.0.1/django_limiter-1.0.1.tar.gz

---
+++ packages/django_limiter/django_limiter-1.0.1.tar.gz/django_limiter-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='django_limiter',
+    description='Signed Cookie based limiting RPS module for Django',
+    keywords='django rps limit',
+    packages=['django_limiter'],
+    version='1.0.1',
+    author='Roman Kitaev',
+    author_email='t4k.kitaetz@gmail.com',
+    url='https://github.com/deliro/django_limiter',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+    ],
+)
