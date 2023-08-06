15c5207a55c9ff07552bf96f50ed0525fdb40cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:56 +0000

    Add package filemerge/0.0.3/filemerge-0.0.3.tar.gz

---
+++ packages/filemerge/filemerge-0.0.3.tar.gz/filemerge-0.0.3/setup.py
@@ -0,0 +1,43 @@
+# Copyright 2016 Intuit
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup
+
+
+def setup_filemerge():
+    """
+    Actual setup job. Just calls setuptools.setup
+    @rtype: None
+    @return: None
+    """
+
+    setup(
+        name="filemerge",
+        packages=["filemerge"],
+        # package_data={"thrivext": ["templates/*"]},
+        # include_package_data=True,
+        version="0.0.3",
+        # install_requires=reqs,
+        description="Filemerge: Tool to merge small HDFS files",
+        author="Rohan Kekatpure",
+        author_email="rohan_kekatpure@intuit.com",
+        url="https://github.com/intuit/filemerge",
+        download_url="https://github.com/intuit/filemerge/tarball/0.1",
+        keywords=["pypi", "filemerge"],
+        classifiers=[]
+    )
+
+if __name__ == "__main__":
+    setup_filemerge()
+
