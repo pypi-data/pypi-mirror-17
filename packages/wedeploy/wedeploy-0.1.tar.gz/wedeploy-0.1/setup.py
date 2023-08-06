ade27843c38024b5d48b71b504c72ca17fbda4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:10 +0000

    Add package wedeploy/0.1/wedeploy-0.1.tar.gz

---
+++ packages/wedeploy/wedeploy-0.1.tar.gz/wedeploy-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'wedeploy',
+  packages = ['wedeploy'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Wedeploy API',
+  author = 'Ivan Santos',
+  author_email = 'ivan.santos@liferay.com',
+  url = 'https://github.com/wedeploy/api-python', # use the URL to the github repo
+  download_url = 'https://github.com/wedeploy/api-python/tarball/0.1', # I'll explain this in a second
+  keywords = ['wedeploy'], # arbitrary keywords
+  classifiers = [],
+)
