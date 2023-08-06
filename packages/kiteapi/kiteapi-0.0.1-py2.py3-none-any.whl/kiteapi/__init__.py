fd7551e5ab54abeea41885f1e2fc3bb9ebd7e5ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:48 +0000

    Add package kiteapi/0.0.1/kiteapi-0.0.1-py2.py3-none-any.whl

---
+++ packages/kiteapi/kiteapi-0.0.1-py2.py3-none-any.whl/kiteapi/__init__.py
@@ -0,0 +1,10 @@
+"""
+Unofficial Python Api to communicate with your Kite (https://kite.com).
+
+Contribution welcome. 
+"""
+
+# Note to the kite team: If you want to get back the Kite name on PyPI, I would
+# be happy to give it back to you. 
+
+__version__ = '0.0.1'
