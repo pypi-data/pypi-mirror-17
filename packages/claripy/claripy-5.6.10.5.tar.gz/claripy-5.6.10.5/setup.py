ff72eb896a71bb4940f01b9ac1184348448128d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:56 +0000

    Add package claripy/5.6.10.5/claripy-5.6.10.5.tar.gz

---
+++ packages/claripy/claripy-5.6.10.5.tar.gz/claripy-5.6.10.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='claripy',
+    version='5.6.10.5',
+    packages=['claripy', 'claripy.backends', 'claripy.frontends', 'claripy.vsa', 'claripy.ast', 'claripy.frontend_mixins'],
+    install_requires=[
+        'ana',
+        'angr-only-z3-custom',
+    ],
+    description='An abstraction layer for constraint solvers',
+    url='https://github.com/angr/claripy',
+)
