476dcf4c5498ce73ebd7dfd3fe133ed0a42dc036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:57 +0000

    Add package globus-cli/0.3.1.1/globus_cli-0.3.1.1-py2.7.egg

---
+++ packages/globus-cli/globus_cli-0.3.1.1-py2.7.egg/globus_cli/config_command/edit.py
@@ -0,0 +1,15 @@
+import click
+import os
+import subprocess
+
+from globus_cli.helpers import common_options
+
+
+@click.command('edit', help='Edit your Globus Config file')
+@common_options(no_format_option=True)
+def edit_command():
+    """
+    Executor for `globus config edit`
+    """
+    editor = os.environ.get('EDITOR', 'nano')
+    subprocess.call([editor, os.path.expanduser('~/.globus.cfg')])
