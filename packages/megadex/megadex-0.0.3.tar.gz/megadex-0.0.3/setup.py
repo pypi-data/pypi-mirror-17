aec23757be2fba4a3285c79851b1b718cb338cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:08 +0000

    Add package megadex/0.0.3/megadex-0.0.3.tar.gz

---
+++ packages/megadex/megadex-0.0.3.tar.gz/megadex-0.0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = "megadex",
+    description      = "Mega Indexer",
+    url              = "https://github.com/hodgesds/megadex",
+    version          = "0.0.3",
+    author           = "Daniel Hodges",
+    author_email     = "hodges.daniel.scott@gmail.com",
+    scripts          = [ "bin/megadex" ],
+    install_requires = [
+                        "elasticsearch",
+                        "exifread",
+                        "openpyxl",
+                        "pykml",
+                        "dbfread",
+                        "pyshp",
+                        "python-docx",
+			"pyodbc",
+                        "python-magic",
+                        "python-pptx",
+                        "watchdog",
+                        "xlrd",
+                        "xmltodict",
+                       ],
+    test_suite       = "",
+    tests_require    = [ "tox", "nose" ],
+    packages         = find_packages(
+        where        = ".",
+        exclude      = ("tests*", "bin*", "example*"),
+    ),
+)
