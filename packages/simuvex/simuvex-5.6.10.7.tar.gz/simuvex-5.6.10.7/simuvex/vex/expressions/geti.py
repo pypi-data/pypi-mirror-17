9a1982018089555a2906d99f6c994307b3a942a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:58 +0000

    Add package simuvex/5.6.10.7/simuvex-5.6.10.7.tar.gz

---
+++ packages/simuvex/simuvex-5.6.10.7.tar.gz/simuvex-5.6.10.7/simuvex/vex/expressions/geti.py
@@ -0,0 +1,32 @@
+from .base import SimIRExpr
+from .. import size_bytes
+from ... import s_options as o
+from ...s_action import SimActionData
+from ...s_variable import SimRegisterVariable
+
+class SimIRExpr_GetI(SimIRExpr):
+    def _execute(self):
+        self.ix = self._translate_expr(self._expr.ix)
+        size = size_bytes(self._expr.descr.elemTy)
+        self.type = self._expr.descr.elemTy
+        self.array_base = self._expr.descr.base
+        self.array_index = (self.ix.expr + self._expr.bias) % self._expr.descr.nElems
+        self.offset = self.array_base + self.array_index*size
+
+        # FIXME: @fish will this code work with symbolic offset?
+        if o.FRESHNESS_ANALYSIS in self.state.options:
+            var = SimRegisterVariable(self.offset, size)
+            if not self.state.scratch.used_variables.contains_register_variable(var):
+                self.state.scratch.input_variables.add(var)
+
+        # get it!
+        self.expr = self.state.registers.load(self.offset, size)
+
+        if self.type.startswith('Ity_F'):
+            self.expr = self.expr.raw_to_fp()
+
+        # finish it and save the register references
+        self._post_process()
+        if o.TRACK_REGISTER_ACTIONS in self.state.options:
+            r = SimActionData(self.state, self.state.registers.id, SimActionData.READ, addr=self.offset, size=size, data=self.expr)
+            self.actions.append(r)
