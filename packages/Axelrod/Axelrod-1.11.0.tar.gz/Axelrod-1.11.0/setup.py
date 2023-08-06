8e723009ff132eea4fc21d2206a106d5778b5bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:55 +0000

    Add package Axelrod/1.11.0/Axelrod-1.11.0.tar.gz

---
+++ packages/Axelrod/Axelrod-1.11.0.tar.gz/Axelrod-1.11.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+# Read in the requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='Axelrod',
+    version='1.11.0',
+    install_requires=requirements,
+    author='Vince Knight, Owen Campbell, Karol Langner, Marc Harper',
+    author_email=('axelrod-python@googlegroups.com'),
+    packages=['axelrod', 'axelrod.strategies', 'axelrod.tests'],
+    url='http://axelrod.readthedocs.org/',
+    license='The MIT License (MIT)',
+    description='Reproduce the Axelrod iterated prisoners dilemma tournament',
+)
