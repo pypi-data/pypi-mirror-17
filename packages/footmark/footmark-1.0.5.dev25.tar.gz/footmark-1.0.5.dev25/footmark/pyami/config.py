5bf181a985f63d0b1d44eb0a21311fbee9202584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:11 +0000

    Add package footmark/1.0.5.dev25/footmark-1.0.5.dev25.tar.gz

---
+++ packages/footmark/footmark-1.0.5.dev25.tar.gz/footmark-1.0.5.dev25/footmark/pyami/config.py
@@ -0,0 +1,68 @@
+#
+import os
+
+try:
+    os.path.expanduser('~')
+    expanduser = os.path.expanduser
+except (AttributeError, ImportError):
+    # This is probably running on App Engine.
+    expanduser = (lambda x: x)
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+# By default we use two locations for the footmark logging configurations,
+# /etc/footmark/logging.conf and ~/.footmark/logging.conf (which works on Windows and Unix).
+FootmarkConfigPath = '/etc/footmark/logging.conf'
+FootmarkConfigLocations = [FootmarkConfigPath]
+UserConfigPath = os.path.join(expanduser('~'), '.footmark/logging.conf')
+FootmarkConfigLocations.append(UserConfigPath)
+
+# If there's a FOOTMARK_CONFIG variable set, we load ONLY
+# that variable
+if 'FOOTMARK_CONFIG' in os.environ:
+    FootmarkConfigLocations = [expanduser(os.environ['FOOTMARK_CONFIG'])]
+
+# If there's a FOOTMARK_PATH variable set, we use anything there
+# as the current configuration locations, split with os.pathsep.
+elif 'FOOTMARK_PATH' in os.environ:
+    FootmarkConfigLocations = []
+    for path in os.environ['FOOTMARK_PATH'].split(os.pathsep):
+        FootmarkConfigLocations.append(expanduser(path))
+
+# Default logging configurations.
+# By default we use location /var/footmark/ for the footmark logs.
+LoggingDict = '/var/log/footmark/'
+# LoggingDict = './'
+LoggingDictConfig = {
+    'version':1,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s [%(levelname)s] %(filename)s %(funcName)s %(lineno)d: %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+            'level': 'DEBUG',
+            'stream': 'ext://sys.stdout'
+        },
+        'file': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'formatter': 'default',
+            'level': 'DEBUG',
+            'filename': LoggingDict+'footmark.log',
+            'when': 'D',
+            'interval': 1,
+            'backupCount': 7
+        }
+    },
+    "loggers": {
+        'footmark': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file']
+        }
+    }
+}
