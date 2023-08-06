f2e6df9fa7476dec59d934714412671b41388d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:54 +0000

    Add package Fyreside/0.0.4/Fyreside-0.0.4-py3-none-any.whl

---
+++ packages/Fyreside/Fyreside-0.0.4-py3-none-any.whl/fyreside/txt.py
@@ -0,0 +1,38 @@
+SPLASH = """
+Welcome to Fireside!
+built using qtMUD 0.0.8
+
+Fireside is a talker-style MUD where you gain action points for how much you
+talk, and can use those to "play" cards in your "hand".
+
+If you don't like the hand you were dealt, "discard" and "draw" to try and
+get different ones, but note that you can only have up to five cards in your
+hand at a time.
+
+Type "commands" to see a list of commands, and "help" for more information."""
+
+SERVER_RESET = """
+
+        \           OH NO           /
+         \      {player.name}      /
+          \ broke into the server /
+           ]     and deleted     [
+           ]  everyone's hand!   [
+           ]___               ___[
+           ]  ]\             /[  [
+           ]  ] \           / [  [
+           ]  ]  ]         [  [  [
+           ]  ]  ]__     __[  [  [
+           ]  ]  ] ]\   /[ [  [  [
+           ]  ]  ] ]     [ [  [  [
+           ]  ]  ]_]     [_[  [  [
+           ]  ]  ]         [  [  [
+           ]  ] /   `       \ [  [
+           ]__]/             \[__[
+           ]                     [
+           ]                     [
+           ]                     [
+          /                       \\
+         /                         \\
+        /                           \\
+"""
