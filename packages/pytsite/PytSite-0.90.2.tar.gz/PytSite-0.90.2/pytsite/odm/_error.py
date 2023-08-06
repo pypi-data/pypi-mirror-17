af037c0dfb55cbda562838578cffc7b1dfee7357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:02 +0000

    Add package pytsite/0.90.2/PytSite-0.90.2.tar.gz

---
+++ packages/pytsite/PytSite-0.90.2.tar.gz/PytSite-0.90.2/pytsite/odm/_error.py
@@ -0,0 +1,57 @@
+"""ODM errors.
+"""
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+class ModelAlreadyRegistered(Exception):
+    pass
+
+
+class ModelNotRegistered(Exception):
+    pass
+
+
+class EntityNotFound(Exception):
+    pass
+
+
+class EntityNotStored(Exception):
+    pass
+
+
+class ForbidEntityOperation(Exception):
+    pass
+
+
+class ForbidEntityCreate(ForbidEntityOperation):
+    pass
+
+
+class ForbidEntityModify(ForbidEntityOperation):
+    pass
+
+
+class ForbidEntityDelete(ForbidEntityOperation):
+    pass
+
+
+class FieldNotDefined(Exception):
+    pass
+
+
+class EntityDeleted(Exception):
+    pass
+
+
+class EntityNotLocked(Exception):
+    pass
+
+
+class FieldEmpty(Exception):
+    pass
+
+
+class NoCachedData(Exception):
+    pass
