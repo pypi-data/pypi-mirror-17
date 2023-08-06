203cd3d8decbca71592ced50bcbbcdf53ea1e6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:21 +0000

    Add package simulacrum/0.2.1/simulacrum-0.2.1.tar.gz

---
+++ packages/simulacrum/simulacrum-0.2.1.tar.gz/simulacrum-0.2.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name = 'simulacrum',
+      version = '0.2.1',
+      description = 'Create Pandas DataFrames of simulated data with columns following statistical distributions or categorical datatypes',
+      url = 'https://github.com/jbrambleDC/simulacrum',
+      download_url = 'https://github.com/jbrambleDC/simulacrum/tarball/0.2.1',
+      author = 'Jordan Bramble',
+      author_email = 'jordanbramble@gmail.com',
+      license = 'MIT',
+      packages = ['simulacrum'],
+      keywords = ['simulation', 'data', 'data science'],
+      install_requires = ['pandas', 'faker'],
+      zip_safe = False)
