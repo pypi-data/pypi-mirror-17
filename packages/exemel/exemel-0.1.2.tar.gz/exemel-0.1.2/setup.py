89116ee285e3a2341332479c7b10861ebc07dbca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:44 +0000

    Add package exemel/0.1.2/exemel-0.1.2.tar.gz

---
+++ packages/exemel/exemel-0.1.2.tar.gz/exemel-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='exemel',
+    version='0.1.2',
+    description=(
+        'Build XML documents easily and concisely using native Python data '
+        'structures.'
+    ),
+    url='https://github.com/aptbosox/exemel',
+    author='Alexander Thompson',
+    author_email='aptbosox@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    py_modules=[
+        'exemel'
+    ],
+    install_requires=[
+        'lxml>=3.0'
+    ]
+)
