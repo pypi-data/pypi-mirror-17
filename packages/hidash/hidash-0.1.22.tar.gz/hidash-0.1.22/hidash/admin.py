13fcac9281c4a6178668945a4a0f1a2b20f979eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:29 +0000

    Add package hidash/0.1.22/hidash-0.1.22.tar.gz

---
+++ packages/hidash/hidash-0.1.22.tar.gz/hidash-0.1.22/hidash/admin.py
@@ -0,0 +1,59 @@
+from django.contrib import admin
+import models
+
+
+class ChartMetricInlineAdmin(admin.TabularInline):
+    model = models.ChartMetric
+    fk_name = 'chart'
+    extra = 1
+
+
+class ChartAuthPermissionInlineAdmin(admin.TabularInline):
+    model = models.ChartAuthPermission
+    fk_name = 'chart'
+    extra = 1
+
+
+class ChartAuthGroupInlineAdmin(admin.TabularInline):
+    model = models.ChartAuthGroup
+    fk_name = 'chart'
+    extra = 1
+
+
+class ChartGroupInlineAdmin(admin.TabularInline):
+    model = models.ChartGroup.chart.through
+    extra = 1
+
+
+class ChartAdmin(admin.ModelAdmin):
+    list_display = ('name', 'library', 'chart_type', 'priority', 'grid_width',
+                    'height', 'groups')
+    inlines = [
+        ChartMetricInlineAdmin, ChartAuthPermissionInlineAdmin,
+        ChartAuthGroupInlineAdmin, ChartGroupInlineAdmin
+    ]
+
+
+class ChartGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_all_charts')
+
+
+class ReportRecipientsAdmin(admin.TabularInline):
+    model = models.ReportRecipients
+
+
+class ScheduledReportParamAdmin(admin.TabularInline):
+    model = models.ScheduledReportParam
+
+
+class ScheduledReportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_reports', 'get_parameters')
+    inlines = [
+        ScheduledReportParamAdmin,
+        ReportRecipientsAdmin
+    ]
+
+
+admin.site.register(models.ScheduledReport, ScheduledReportAdmin)
+admin.site.register(models.Chart, ChartAdmin)
+admin.site.register(models.ChartGroup, ChartGroupAdmin)
