6bf64bd2d0f6fdd8efc7cb9e44aebc6f0754e179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:37 +0000

    Add package pyspacewar/1.1.0/pyspacewar-1.1.0-py3-none-any.whl

---
+++ packages/pyspacewar/pyspacewar-1.1.0-py3-none-any.whl/pyspacewar-1.1.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,17 @@
+Two ships duel in a gravity field.   Gravity doesn't affect
+the ships themselves (which have spanking new anti-gravity
+devices), but it affects missiles launced by the ships.
+
+You can play against the computer, or two players can play
+with one keyboard.  There is also a Gravity Wars mode, where
+the two ships do not move, and the players repeatedly
+specify the direction and velocity of their missiles.
+
+Latest changes
+--------------
+
+September 21, 2016: Released version 1.1.0:
+
+- Support Python 3.4 and 3.5 in addition to 2.7.
+- Made 'pip install pyspacewar' work.
+
---
+++ packages/pyspacewar/pyspacewar-1.1.0-py3-none-any.whl/pyspacewar/version.py
@@ -0,0 +1,27 @@
+"""
+PySpaceWar version number tracker.
+"""
+
+version = "1.1.0"
+
+
+def get_git_revision(unknown='', format=' (git %s)'):
+    """Return the latest revision number of the files in the package."""
+    import os
+    import subprocess
+    package_root = os.path.dirname(__file__)
+    try:
+        p = subprocess.Popen(['git', 'describe', '--always'],
+                             cwd=package_root,
+                             stdout=subprocess.PIPE)
+    except OSError:  # pragma: nocover
+        return unknown
+    else:
+        ver = p.communicate()[0].strip()
+        if not isinstance(ver, str):
+            ver = ver.decode('UTF-8', 'replace')
+        return format % ver
+
+
+if 'dev' in version:
+    version += get_git_revision(format='+git.%s')
