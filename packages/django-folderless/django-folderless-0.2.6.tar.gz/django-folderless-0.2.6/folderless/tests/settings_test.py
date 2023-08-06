92c2fed22fd990c4f8215b73bc433e6030f0d38d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:41 +0000

    Add package django-folderless/0.2.6/django-folderless-0.2.6.tar.gz

---
+++ packages/django-folderless/django-folderless-0.2.6.tar.gz/django-folderless-0.2.6/folderless/tests/settings_test.py
@@ -0,0 +1,87 @@
+"""Settings that need to be set in order to run the tests."""
+import os
+import sys
+import tempfile
+import logging
+import django.conf.global_settings as DEFAULT_SETTINGS
+
+
+DEBUG = True
+
+logging.getLogger("factory").setLevel(logging.WARN)
+
+SITE_ID = 1
+
+APP_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), ".."))
+
+# only django 1.7 and up ?!
+# sys.path.insert(0, APP_ROOT + "/../")
+
+# NOSE_ARGS = ['--nocapture',
+#              '--nologcapture',
+#              '--exclude-dir=folderless/migrations',
+#              '--exclude-dir=migrations', ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+ROOT_URLCONF = 'folderless.tests.urls'
+
+# media root is overridden when needed in tests
+MEDIA_ROOT = tempfile.mkdtemp(suffix='folderless_media_root')
+MEDIA_URL = "/media/"
+FILE_UPLOAD_TEMP_DIR = tempfile.mkdtemp(suffix='folderless_temp')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(APP_ROOT, '../test_app_static')
+STATICFILES_DIRS = (
+    os.path.join(APP_ROOT, 'static'),
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(APP_ROOT, 'tests/test_app/templates'),
+)
+
+COVERAGE_REPORT_HTML_OUTPUT_DIR = os.path.join(
+    os.path.join(APP_ROOT, 'tests/coverage'))
+COVERAGE_MODULE_EXCLUDES = [
+    'tests$', 'settings$', 'urls$', 'locale$',
+    'migrations', 'fixtures', 'admin$', 'django_extensions',
+]
+
+EXTERNAL_APPS = (
+    'django.contrib.admin',
+    # 'django.contrib.admindocs',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
+    'django_nose',
+    'easy_thumbnails',
+)
+
+INTERNAL_APPS = (
+    'folderless',
+    'folderless.tests.test_app',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+)
+
+INSTALLED_APPS = EXTERNAL_APPS + INTERNAL_APPS
+COVERAGE_MODULE_EXCLUDES += EXTERNAL_APPS
+
+SECRET_KEY = 'foobarXXXXY'
---
+++ packages/django-folderless/django-folderless-0.2.6.tar.gz/django-folderless-0.2.6/folderless/tests/test_admin.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+
+# from django.forms.models import modelform_factory
+import os
+import json
+
+from django.test import TestCase
+# from django.core.files import File as DjangoFile
+from django.core.urlresolvers import reverse
+
+from django.conf import settings
+
+from folderless.models import File
+from folderless.tests.utils import create_superuser, create_image
+
+
+class FolderlessAdminUrlsTests(TestCase):
+
+    def setUp(self):
+        self.superuser = create_superuser()
+        self.client.login(username='admin', password='secret')
+        self.img = create_image()
+        self.image_name = 'test_file.jpg'
+        self.filename = os.path.join(settings.FILE_UPLOAD_TEMP_DIR,
+                                     self.image_name)
+        self.img.save(self.filename, 'JPEG')
+
+    def tearDown(self):
+        self.client.logout()
+
+    def test_app_index_get(self):
+        response = self.client.get(reverse('admin:app_list', args=('folderless',)))
+        self.assertEqual(response.status_code, 200)
+
+    def test_file_change_list(self):
+        # upload file, so we get an item in the list
+        self.client.post(
+            reverse('admin:folderless-ajax_upload'),
+            {'ajax_file': open(self.filename), }
+        )
+        response = self.client.get(reverse('admin:folderless_file_changelist'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_file_change_view(self):
+        self.client.post(
+            reverse('admin:folderless-ajax_upload'),
+            {'ajax_file': open(self.filename), }
+        )
+        file_id = File.objects.all()[0].id
+        response = self.client.get(reverse('admin:folderless_file_change', args=(file_id, )))
+        self.assertEqual(response.status_code, 200)
+
+    def test_empty_file_field(self):
+        response = self.client.get(reverse('admin:test_app_testmodel_add'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_file_upload(self):
+        self.assertEqual(File.objects.count(), 0)
+        self.client.post(
+            reverse('admin:folderless-ajax_upload'),
+            {'ajax_file': open(self.filename), }
+        )
+        self.assertEqual(File.objects.count(), 1)
+
+    def test_bad_file_upload(self):
+        self.assertEqual(File.objects.count(), 0)
+        response = self.client.post(
+            reverse('admin:folderless-ajax_upload'), {}
+        )
+        data = json.loads(response.content)
+        self.assertEqual(data["success"], False)
+
+    def test_non_post_upload_request(self):
+        self.assertEqual(File.objects.count(), 0)
+        response = self.client.get(reverse('admin:folderless-ajax_upload'))
+        data = json.loads(response.content)
+        self.assertEqual(data["success"], False)
+
+    def test_prevent_duplicate_file_name_upload(self):
+        self.assertEqual(File.objects.count(), 0)
+        self.client.post(
+            reverse('admin:folderless-ajax_upload'),
+            {'ajax_file': open(self.filename), }
+        )
+        self.assertEqual(File.objects.count(), 1)
+        response = self.client.post(
+            reverse('admin:folderless-ajax_upload'),
+            {'ajax_file': open(self.filename), }
+        )
+        self.assertEqual(response.status_code, 409)
+        self.assertEqual(File.objects.count(), 1)
+
+    def test_prevent_duplicate_file_content_upload(self):
+        self.assertEqual(File.objects.count(), 0)
+        self.client.post(
+            reverse('admin:folderless-ajax_upload'),
+            {'ajax_file': open(self.filename), 'filename': "first-%s" % self.image_name}
+        )
+        self.assertEqual(File.objects.count(), 1)
+        response = self.client.post(
+            reverse('admin:folderless-ajax_upload'),
+            {'ajax_file': open(self.filename), 'filename': "second-%s" % self.image_name}
+        )
+        self.assertEqual(response.status_code, 409)
+        self.assertEqual(File.objects.count(), 1)
+
+    def test_prevent_duplicate_json_response(self):
+        self.assertEqual(File.objects.count(), 0)
+        self.client.post(
+            reverse('admin:folderless-ajax_upload'),
+            {'ajax_file': open(self.filename), 'filename': "first-%s" % self.image_name}
+        )
+        self.assertEqual(File.objects.count(), 1)
+        response = self.client.post(
+            reverse('admin:folderless-ajax_upload'),
+            {'ajax_file': open(self.filename), 'filename': "second-%s" % self.image_name}
+        )
+        data = json.loads(response.content)
+        self.assertEqual(data["success"], False)
+        self.assertGreaterEqual(data["errors"], 1)
