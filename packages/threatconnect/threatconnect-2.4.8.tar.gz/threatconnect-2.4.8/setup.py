8d96be5f592eca3c33d73c88395f4c8277028630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:06 +0000

    Add package threatconnect/2.4.8/threatconnect-2.4.8.tar.gz

---
+++ packages/threatconnect/threatconnect-2.4.8.tar.gz/threatconnect-2.4.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+#
+# PATCH 1
+#
+
+setup(
+    name='threatconnect',
+    version='2.4.8',
+    description='Python SDK for ThreatConnect API',
+    author='ThreatConnect',
+    author_email='support@threatconnect.com',
+    # package_dir = {'': 'src'},
+    packages=find_packages(),
+    url='https://github.com/ThreatConnect-Inc/threatconnect-python',
+    download_url='https://github.com/ThreatConnect-Inc/threatconnect-python/tarball/2.4',
+    license='ASL',
+    install_requires=['requests', 'python-dateutil'],
+    extras_require={
+        ':python_version=="2.7"': ['enum34']
+    },
+    use_2to3=True,
+    # convert_2to3_doctests = [''],
+    # use_2to3_fixers = [''],
+    use_2to3_exclude_fixers=['lib2to3.fixes.fix_print'],
+    entry_points={
+        'console_scripts': [
+            'stanchion=bin.stanchion:main'
+        ],
+    },
+)
