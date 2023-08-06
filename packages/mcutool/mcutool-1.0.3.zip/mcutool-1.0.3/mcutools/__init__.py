02b836830c265b744da806d3ccc856ec5ab9626d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:41 +0000

    Add package mcutool/1.0.3/mcutool-1.0.3.zip

---
+++ packages/mcutool/mcutool-1.0.3.zip/mcutool-1.0.3/mcutools/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+# _*_ coding=gbk _*_
+
---
+++ packages/mcutool/mcutool-1.0.3.zip/mcutool-1.0.3/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# _*_ coding=gbk _*_
+
+
+from setuptools import setup, find_packages
+
+setup(name='mcutool',
+      version='1.0.3',
+      description='a arm mcu debug tool',
+      author='xwm',
+      author_email='xwmdev@163.com',
+      url='https://blog.xieweiming.cn',
+      packages = find_packages(),
+      #py_modules = ['Upgrade'],
+      scripts = ['mcutool.py'],
+      platforms = 'any',
+      requires = ['wx','serial'],
+     )
