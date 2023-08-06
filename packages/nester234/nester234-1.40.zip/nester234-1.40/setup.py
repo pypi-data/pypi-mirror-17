fa172e0a3cd43084aae5cf9f6b3b251cd6fea06c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:40 +0000

    Add package nester234/1.40/nester234-1.40.zip

---
+++ packages/nester234/nester234-1.40.zip/nester234-1.40/setup.py
@@ -0,0 +1,5 @@
+# setup.py
+# the command includes "python setup.py sdisk" "python setup.py install" and then you will have your own library;
+from distutils.core import setup
+
+setup(name='nester234',version='1.40',py_modules=['nester234'],author='sunTrail',author_email='2329408386@qq.com',url='http://cqu.edu.cn',description='A simple printer of nested lists')
