2dafeb09f2c16a6ae7741cf3bfca7058aa9d540d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:13 +0000

    Add package jerri_nester/1.3.3/jerri_nester-1.3.3.zip

---
+++ packages/jerri_nester/jerri_nester-1.3.3.zip/jerri_nester-1.3.3/jerri_nester.py
@@ -0,0 +1,12 @@
+"""this is the nester.py module and it provide one function called print_lol()
+which prints lists that may or may not include nested listes,secord parameter is level insert tab"""
+def print_lol(the_list, indent=False, level=0):
+	""" 这个函数取一个位置参数，名为“the_list”这可以是任何python列表（也可以包含嵌套列表）所指定列表中的每个数据项会递归的输入到屏幕上，个数据各占用一行"""
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item, indent, level+1)
+		else:
+			if indent:
+				for tab_stop in range(level):
+					print("\t",end='')
+			print(each_item)
---
+++ packages/jerri_nester/jerri_nester-1.3.3.zip/jerri_nester-1.3.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+		name		= 'jerri_nester',
+		version		= '1.3.3',
+		py_modules	= ['jerri_nester'],
+		author		= 'jerri',
+		author_email= 'dheigh@163.com',
+		url			= 'http://www.headfirstlabs.com',
+		description	= 'A simple printer of neste lists',
+		)
+
+		
