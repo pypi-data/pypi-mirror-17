2e4187443da347a3eb50f04c577b007eb2487320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:53 +0000

    Add package aiolirc/0.1.0/aiolirc-0.1.0.tar.gz

---
+++ packages/aiolirc/aiolirc-0.1.0.tar.gz/aiolirc-0.1.0/aiolirc/tests/test_lirc_client.py
@@ -0,0 +1,28 @@
+
+import asyncio
+
+from aiolirc.tests.helpers import AioTestCase, EmulatedClient
+
+
+class TestLIRCClient(AioTestCase):
+
+    async def test_via_emulator(self):
+        async with EmulatedClient(check_interval=.01) as client:
+            for i in range(10):
+                self.assertEqual(await client.__anext__(), 'amp power')
+
+            for i in range(5):
+                self.assertEqual(await client.__anext__(), 'amp source')
+
+            for i in range(5):
+                self.assertEqual(await client.__anext__(), 'off')
+
+            for i in range(2):
+                self.assertEqual(await client.__anext__(), 'amp source')
+
+            async with client.ignore():
+                await asyncio.sleep(.1)
+
+            await self.assertRaises(asyncio.QueueEmpty, client.__anext__)
+
+
