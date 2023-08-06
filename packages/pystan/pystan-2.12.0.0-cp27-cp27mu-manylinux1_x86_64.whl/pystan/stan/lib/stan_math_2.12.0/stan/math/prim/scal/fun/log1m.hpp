b7fbd15c2b6dd5ed81e5cf5f973c2bf8dadc4979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:28 +0000

    Add package pystan/2.12.0.0/pystan-2.12.0.0-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/pystan/pystan-2.12.0.0-cp27-cp27mu-manylinux1_x86_64.whl/pystan/stan/lib/stan_math_2.12.0/stan/math/prim/scal/fun/log1m.hpp
@@ -0,0 +1,44 @@
+#ifndef STAN_MATH_PRIM_SCAL_FUN_LOG1M_HPP
+#define STAN_MATH_PRIM_SCAL_FUN_LOG1M_HPP
+
+#include <stan/math/prim/scal/fun/log1p.hpp>
+
+namespace stan {
+  namespace math {
+
+    /**
+     * Return the natural logarithm of one minus the specified value.
+     *
+     * The main use of this function is to cut down on intermediate
+     * values during algorithmic differentiation.
+     *
+     *
+       \f[
+       \mbox{log1m}(x) =
+       \begin{cases}
+         \ln(1-x) & \mbox{if } x \leq 1 \\
+         \textrm{NaN} & \mbox{if } x > 1\\[6pt]
+         \textrm{NaN} & \mbox{if } x = \textrm{NaN}
+       \end{cases}
+       \f]
+
+       \f[
+       \frac{\partial\, \mbox{log1m}(x)}{\partial x} =
+       \begin{cases}
+         -\frac{1}{1-x} & \mbox{if } x \leq 1 \\
+         \textrm{NaN} & \mbox{if } x > 1\\[6pt]
+         \textrm{NaN} & \mbox{if } x = \textrm{NaN}
+       \end{cases}
+       \f]
+     *
+     * @param x Specified value.
+     * @return Natural log of one minus <code>x</code>.
+     */
+    inline double log1m(const double x) {
+      return log1p(-x);
+    }
+
+  }
+}
+
+#endif
