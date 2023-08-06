240868825983d4b033804af3bdea58b55947831c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:55 +0000

    Add package pa11ycrawler/1.5.2/pa11ycrawler-1.5.2.tar.gz

---
+++ packages/pa11ycrawler/pa11ycrawler-1.5.2.tar.gz/pa11ycrawler-1.5.2/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+def is_requirement(line):
+    line = line.strip()
+    # Skip blank lines, comments, and editable installs
+    return not (
+        line == '' or
+        line.startswith('--') or
+        line.startswith('-r') or
+        line.startswith('#') or
+        line.startswith('-e') or
+        line.startswith('git+')
+    )
+
+
+def get_requirements(path):
+    with open(path) as f:
+        lines = f.readlines()
+    return [l.strip() for l in lines if is_requirement(l)]
+
+
+setup(
+    name='pa11ycrawler',
+    version='1.5.2',
+    package_data={
+        'pa11ycrawler': [
+            'templates/*.*',
+            'templates/assets/js/*.*',
+            'templates/assets/css/*.*',
+        ]
+    },
+    packages=[
+        'pa11ycrawler',
+        'pa11ycrawler.pipelines',
+        'pa11ycrawler.spiders',
+    ],
+    install_requires=get_requirements("requirements.txt"),
+    tests_require=get_requirements("dev-requirements.txt"),
+    license="Apache-2.0",
+    entry_points={
+        'console_scripts': [
+            'pa11ycrawler-html=pa11ycrawler.html:main',
+        ]
+    }
+)
