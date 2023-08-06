44d4adb36195d22ef0707896a9a1d0a7d1e1bbc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:10 +0000

    Add package codedocu/0.1/codedocu-0.1.tar.gz

---
+++ packages/codedocu/codedocu-0.1.tar.gz/codedocu-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'codedocu',
+      version = '0.1',
+      description = 'Documenting python code made easy!',
+      url = 'http://github.com/sumitpathak92/codedocu',
+      author = 'Sumit Pathak',
+      author_email = 'pathaksumit92@gmail.com',
+      license = 'MIT',
+      packages = ['codedocu'],
+      zip_safe = False)
