40e93437d604b106fc21d53438a10214d2282b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:25 +0000

    Add package themis-autoscaler/0.1.2/themis-autoscaler-0.1.2.tar.gz

---
+++ packages/themis-autoscaler/themis-autoscaler-0.1.2.tar.gz/themis-autoscaler-0.1.2/web/views/config.js
@@ -0,0 +1,49 @@
+(function () {
+  'use strict';
+
+  var app = angular.module('app');
+
+  app.controller('configCtrl', function($scope, restClient) {
+
+    var client = restClient;
+
+    var setConfigData = function(config) {
+      $scope.$apply(function(){
+        $scope.config = config.config;
+      });
+    };
+
+    $scope.load = function() {
+      client.then(function(client) {
+        $scope.loading = true;
+        client.default.getConfig({
+          section: 'global'
+        }).then(function(obj) {
+          $scope.loading = false;
+          setConfigData(obj.obj);
+        }, function(err) {
+          $scope.loading = false;
+          console.log(err);
+        });
+      });
+    };
+
+    $scope.save = function() {
+      client.then(function(client) {
+        /* load config */
+        client.default.setConfig({
+          config:$scope.config,
+          section: 'global'
+        }).then(function(obj) {
+          setConfigData(obj.obj);
+        }, function(err) {
+          console.log(err);
+        });
+      });
+    };
+
+    $scope.load();
+
+  });
+
+})();
