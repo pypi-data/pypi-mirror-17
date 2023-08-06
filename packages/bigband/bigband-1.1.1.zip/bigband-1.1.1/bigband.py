56020a8008cdbb7213bf57f40fef6a9227b03764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:02 +0000

    Add package bigband/1.1.1/bigband-1.1.1.zip

---
+++ packages/bigband/bigband-1.1.1.zip/bigband-1.1.1/bigband.py
@@ -0,0 +1,14 @@
+'''这是“form.py”模块，提供了一个名为form()的函数，这个函
+数的作用是打印列表，其中有可能含有（也可能不包含）嵌套列表。'''
+def form(anylist,level):
+    '''这个函数取一个位置参数，名为“anylist”，这可以是任何
+    python列表（也可以是包含嵌套列表的列表）。所指定的列表中每个
+    数据项会（递归地）输出到屏幕上，各数据项各占一行。
+    第二个参数（名为“level”）用来在遇到嵌套列表是插入制表符'''
+    for each in anylist:
+        if isinstance(each,list):
+            form(each,level+1)
+        else:
+            for num in range(level):
+                print("\t", end='')
+            print(each)
