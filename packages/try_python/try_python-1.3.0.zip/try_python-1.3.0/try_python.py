7cc29c6c11142ded6afca10830125f006b9298ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:24 +0000

    Add package try_python/1.3.0/try_python-1.3.0.zip

---
+++ packages/try_python/try_python-1.3.0.zip/try_python-1.3.0/try_python.py
@@ -0,0 +1,25 @@
+# try_python.py
+
+# 함수 print_list(the_list)를 포함
+
+# 리스트의 항목을 차례대로 표시하며 항목 자체가 리스트(하위 리스트)인 경우 하위리스트의 항목도 차례대로 표시
+
+def print_list(the_list,indent=False,level=0):
+
+    for each_item in the_list:
+
+        if isinstance(each_item,list):
+
+            print_list(each_item,indent,level+1)
+
+        else:
+
+            if indent:
+
+                for tab_stop in range(level):
+
+                    print("\t",end='')
+
+            print(each_item)
+
+            
