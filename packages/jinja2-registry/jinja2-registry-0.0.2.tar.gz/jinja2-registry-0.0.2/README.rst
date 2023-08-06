ff28b56ae944df53d6690087eb653e176ca29a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:48 +0000

    Add package jinja2-registry/0.0.2/jinja2-registry-0.0.2.tar.gz

---
+++ packages/jinja2-registry/jinja2-registry-0.0.2.tar.gz/jinja2-registry-0.0.2/README.rst
@@ -0,0 +1,4 @@
+jinja2_registry
+***************
+
+jinja2_registry is a convenience library for managing multiple template namespaces with Jinja2
---
+++ packages/jinja2-registry/jinja2-registry-0.0.2.tar.gz/jinja2-registry-0.0.2/jinja2_registry/__init__.py
@@ -0,0 +1,4 @@
+"""jinja2-registry: Easy handling of multiple loaders and default contexts"""
+
+from __future__ import absolute_import
+from .base import Renderer, register_loader, register_filesystem_loader
