04b7774ad6dc0afe1382fd3fc8ed0194b0421f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:53 +0000

    Add package apize/0.0.1/apize-0.0.1.tar.gz

---
+++ packages/apize/apize-0.0.1.tar.gz/apize-0.0.1/apize/decorators.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+from __future__ import absolute_import
+
+import json
+import requests
+from .exceptions import *
+
+
+def send_request(url, method, data, params, headers, cookies):
+	"""
+	Forge and send HTTP request.
+	"""
+	for p in params:
+		url = url.replace(':'+p, str(params[p]))
+
+	methodLowerCase = method.lower()
+	
+	if methodLowerCase == 'get':
+		r = requests.get(url, headers=headers, cookies=cookies)
+	elif methodLowerCase == 'post':
+		r = requests.post(url, data=data, headers=headers, cookies=cookies)
+	elif methodLowerCase == 'put':
+		r = requests.put(url, data=data, headers=headers, cookies=cookies)
+	elif methodLowerCase == 'delete':
+		r = requests.delete(url, headers=headers, cookies=cookies)
+	else:
+		raise UnknowMethod(str(method))
+	
+	r.close()
+
+	return {
+		'data': r.json(), 
+		'status': r.status_code
+	}
+
+
+def apize(url, method='GET'):
+	"""
+	Convert data and params dict -> json.
+	"""
+	def decorator(func):
+		def wrapper(*args, **kwargs):
+			elem = func(*args, **kwargs)
+			
+			if type(elem) is not dict:
+				raise BadReturnVarType(func.__name__)
+			
+			if not 'data' in elem:
+				elem['data'] = {}
+			if not 'params' in elem:
+				elem['params'] = {}
+			if not 'headers' in elem:
+				elem['headers'] = {}
+			if not 'cookies' in elem:
+				elem['cookies'] = {} 
+			
+			response = send_request(url, method, 
+				json.dumps(elem['data']), 
+				elem['params'],
+				elem['headers'],
+				elem['cookies']
+			)
+			
+			return response
+		return wrapper
+		
+	return decorator
+
