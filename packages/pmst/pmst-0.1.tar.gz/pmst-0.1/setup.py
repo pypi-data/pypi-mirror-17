f7ce65cf2a9fc45b232a4660d6a04b49dd4d54e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:19 +0000

    Add package pmst/0.1/pmst-0.1.tar.gz

---
+++ packages/pmst/pmst-0.1.tar.gz/pmst-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pmst',
+      version='0.1',
+      description='Polarized microscope simulation tool.',
+      url='https://github.com/talonchandler/pmst',
+      author='Talon Chandler',
+      author_email='talonchandler@uchicago.edu',
+      license='MIT',
+      packages=['pmst'],
+      zip_safe=True)
