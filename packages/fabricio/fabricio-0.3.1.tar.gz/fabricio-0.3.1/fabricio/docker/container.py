19a7d99e2592e2d7b576b125e613fdcdcbdc2ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:07 +0000

    Add package fabricio/0.3.1/fabricio-0.3.1.tar.gz

---
+++ packages/fabricio/fabricio-0.3.1.tar.gz/fabricio-0.3.1/fabricio/docker/container.py
@@ -0,0 +1,169 @@
+import json
+
+import fabricio
+
+from . import Image
+
+
+class Container(object):
+
+    image = None  # type: Image
+
+    cmd = None
+
+    user = None
+
+    ports = None
+
+    env = None
+
+    volumes = None
+
+    links = None
+
+    hosts = None
+
+    network = None
+
+    restart_policy = None
+
+    stop_signal = None
+
+    stop_timeout = 10
+
+    def __init__(self, name, options=None):
+        self.name = name
+        self.options = options or {}
+
+    def __str__(self):
+        return str(self.name)
+
+    def fork(self, name=None, options=None):
+        if name is None:
+            name = self.name
+        if options is None:
+            options = self.options.copy()
+        return self.__class__(name=name, options=options)
+
+    def __copy__(self):
+        return self.fork()
+
+    @property
+    def info(self):
+        command = 'docker inspect --type container {container}'
+        info = fabricio.run(command.format(container=self))
+        return json.loads(info)[0]
+
+    def delete(self, force=False, ignore_errors=False):
+        command = 'docker rm {force}{container}'
+        force = force and '--force ' or ''
+        fabricio.run(
+            command.format(container=self, force=force),
+            ignore_errors=ignore_errors,
+        )
+
+    def run(self, tag=None, registry=None):
+        self.__class__.image[registry:tag].run(
+            cmd=self.cmd,
+            temporary=False,
+            name=self.name,
+            user=self.user,
+            ports=self.ports,
+            env=self.env,
+            volumes=self.volumes,
+            links=self.links,
+            hosts=self.hosts,
+            network=self.network,
+            restart_policy=self.restart_policy,
+            stop_signal=self.stop_signal,
+            options=self.options,
+        )
+
+    def execute(self, cmd, ignore_errors=False, quiet=True, use_cache=False):
+        command = 'docker exec --tty --interactive {container} {cmd}'
+        return fabricio.run(
+            command.format(container=self, cmd=cmd),
+            ignore_errors=ignore_errors,
+            quiet=quiet,
+            use_cache=use_cache,
+        )
+
+    def start(self):
+        command = 'docker start {container}'
+        fabricio.run(command.format(container=self))
+
+    def stop(self, timeout=None):
+        if timeout is None:
+            timeout = self.stop_timeout
+        command = 'docker stop --time {timeout} {container}'
+        fabricio.run(command.format(container=self, timeout=timeout))
+
+    def restart(self, timeout=None):
+        if timeout is None:
+            timeout = self.stop_timeout
+        command = 'docker restart --time {timeout} {container}'
+        fabricio.run(command.format(container=self, timeout=timeout))
+
+    def rename(self, new_name):
+        command = 'docker rename {container} {new_name}'
+        fabricio.run(command.format(container=self, new_name=new_name))
+        self.name = new_name
+
+    def signal(self, signal):
+        command = 'docker kill --signal {signal} {container}'
+        fabricio.run(command.format(container=self, signal=signal))
+
+    def update(self, tag=None, registry=None, force=False):
+        if not force:
+            try:
+                current_image_id = self.image.id
+            except RuntimeError:  # current container not found
+                pass
+            else:
+                new_image = self.__class__.image[registry:tag]
+                if current_image_id == new_image.id:
+                    fabricio.log('No change detected, update skipped.')
+                    self.start()  # force starting container
+                    return False
+        new_container = self.fork(name=self.name)
+        obsolete_container = self.get_backup_container()
+        try:
+            obsolete_image = obsolete_container.image
+        except RuntimeError:
+            pass
+        else:
+            obsolete_container.delete()
+            obsolete_image.delete(ignore_errors=True)
+        try:
+            backup_container = self.fork()
+            backup_container.rename(obsolete_container.name)
+        except RuntimeError:
+            pass
+        else:
+            backup_container.stop()
+        new_container.run(tag=tag, registry=registry)
+        return True
+
+    def revert(self):
+        failed_image = self.image
+        self.stop()
+        self.delete()
+        failed_image.delete(ignore_errors=True)
+        backup_container = self.get_backup_container()
+        backup_container.start()
+        backup_container.rename(self.name)
+
+    def get_backup_container(self):
+        return self.fork(name='{container}_backup'.format(container=self))
+
+    def migrate(self, tag=None, registry=None):
+        pass
+
+    def migrate_back(self):
+        pass
+
+    def backup(self):
+        pass
+
+    def restore(self, backup_name=None):
+        pass
---
+++ packages/fabricio/fabricio-0.3.1.tar.gz/fabricio-0.3.1/fabricio/docker/image.py
@@ -0,0 +1,153 @@
+import json
+
+from docker import utils as docker_utils, auth as docker_auth
+
+import fabricio
+
+from fabricio.utils import default_property, Options
+
+
+class Image(object):
+
+    def __init__(self, name, tag=None, registry=None):
+        parsed_registry, parsed_name, parsed_tag = self._parse_image_name(name)
+        self.name = parsed_name
+        self.tag = tag or parsed_tag or 'latest'
+        self.registry = registry or parsed_registry
+
+    def __str__(self):
+        if 'id' in vars(self):
+            return str(self.id)
+        if self.registry:
+            return '{registry}/{name}:{tag}'.format(
+                registry=self.registry,
+                name=self.name,
+                tag=self.tag,
+            )
+        return '{name}:{tag}'.format(name=self.name, tag=self.tag)
+
+    def __get__(self, container, container_cls):
+        if container is None:
+            return self
+        field_name = self._get_field_name(container_cls)
+        image = container.__dict__[field_name] = self.__class__(
+            name=self.name,
+            tag=self.tag,
+        )
+        image.id = container.info['Image']
+        return image
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            registry, tag = item.start, item.stop
+        else:
+            registry, tag = None, item
+        return self.__class__(
+            name=self.name,
+            tag=tag or self.tag,
+            registry=registry or self.registry,
+        )
+
+    def _get_field_name(self, container_cls):
+        for attr in dir(container_cls):
+            if getattr(container_cls, attr) is self:
+                return attr
+
+    @staticmethod
+    def _parse_image_name(image):
+        repository, tag = docker_utils.parse_repository_tag(image)
+        registry, name = docker_auth.resolve_repository_name(repository)
+        if registry == docker_auth.INDEX_NAME:
+            registry = ''
+        return registry, name, tag
+
+    @staticmethod
+    def make_container_options(
+        temporary=None,
+        name=None,
+        user=None,
+        ports=None,
+        env=None,
+        volumes=None,
+        links=None,
+        hosts=None,
+        network=None,
+        restart_policy=None,
+        stop_signal=None,
+        options=None,
+    ):
+        options = Options(options or ())
+        options.update((
+            ('name', name),
+            ('user', user),
+            ('publish', ports),
+            ('env', env),
+            ('volume', volumes),
+            ('link', links),
+            ('add-host', hosts),
+            ('net', network),
+            ('restart', restart_policy),
+            ('stop-signal', stop_signal),
+            ('rm', temporary),
+            ('tty', temporary),
+            ('detach', temporary is not None and not temporary),
+        ))
+        return options
+
+    @property
+    def info(self):
+        command = 'docker inspect --type image {image}'
+        info = fabricio.run(command.format(image=self))
+        return json.loads(str(info))[0]
+
+    @default_property
+    def id(self):
+        return self.info.get('Id')
+
+    def delete(self, force=False, ignore_errors=False):
+        command = 'docker rmi {force}{image}'
+        force = force and '--force ' or ''
+        fabricio.run(
+            command.format(image=self.id, force=force),
+            ignore_errors=ignore_errors,
+        )
+
+    def run(
+        self,
+        cmd=None,
+        temporary=True,
+        quiet=True,
+        name=None,
+        user=None,
+        ports=None,
+        env=None,
+        volumes=None,
+        links=None,
+        hosts=None,
+        network=None,
+        restart_policy=None,
+        stop_signal=None,
+        options=None,
+    ):
+        command = 'docker run {options} {image} {cmd}'
+        return fabricio.run(
+            command.format(
+                image=self,
+                cmd=cmd or '',
+                options=self.make_container_options(
+                    temporary=temporary,
+                    name=name,
+                    user=user,
+                    ports=ports,
+                    env=env,
+                    volumes=volumes,
+                    links=links,
+                    hosts=hosts,
+                    network=network,
+                    restart_policy=restart_policy,
+                    stop_signal=stop_signal,
+                    options=options,
+                ),
+            ),
+            quiet=quiet,
+        )
