d1af4f8ba0a715b8dff70d224f25112145205d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:02 +0000

    Add package pyshtrih/1.1.0/pyshtrih-1.1.0.tar.gz

---
+++ packages/pyshtrih/pyshtrih-1.1.0.tar.gz/pyshtrih-1.1.0/pyshtrih/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+from .protocol import Protocol
+from .device import ShtrihFRK, ShtrihComboFRK, ShtrihComboPTK, ShtrihAllCommands
+from .excepts import ProtocolError, NoConnectionError, UnexpectedResponseError, Error, CheckError, \
+    OpenCheckError, ItemSaleError, CloseCheckError, NoItemsError
+
+
+__version__ = '1.1.0'
+__all__ = (
+    'Protocol',
+    'ShtrihFRK', 'ShtrihComboFRK', 'ShtrihComboPTK', 'ShtrihAllCommands',
+    'ProtocolError', 'NoConnectionError', 'UnexpectedResponseError', 'Error', 'CheckError',
+    'OpenCheckError', 'ItemSaleError', 'CloseCheckError', 'NoItemsError'
+)
