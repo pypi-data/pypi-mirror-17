572ca8d46d5c048dddc5990105df8aa35f59ff3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:19 +0000

    Add package QualiLab-CLI/1.0.3/QualiLab_CLI-1.0.3.zip

---
+++ packages/QualiLab-CLI/QualiLab_CLI-1.0.3.zip/QualiLab_CLI-1.0.3/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='QualiLab_CLI', packages=['QualiLab_CLI'], version='1.0.3',
+      description='Command line library suport Telnet and SSH for use with Quali CloudShell field solutions', author='Gregory Sloan',
+      author_email='gregory.s@quali.com',
+      url='https://github.com/QualiSystemsLab/QualiLab-CLI',
+      download_url='https://github.com/QualiSystemsLab/QualiLab-CLI/tarball/1.0.3',
+      keywords=['telnet', 'console', 'handler', 'ssh','cli','quali', 'cloudshell'], classifiers=[],
+      )
