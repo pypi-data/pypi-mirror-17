12eb7e89edf4ecf55a46f9ebe0ebd6aa1ec95628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:26 +0000

    Add package msmexplorer/0.2.0/msmexplorer-0.2.0.tar.gz

---
+++ packages/msmexplorer/msmexplorer-0.2.0.tar.gz/msmexplorer-0.2.0/msmexplorer/plots/__init__.py
@@ -0,0 +1,6 @@
+from .msm import *
+from .tpt import *
+from .projection import *
+from .cluster import *
+from .chord import *
+from .timeseries import *
---
+++ packages/msmexplorer/msmexplorer-0.2.0.tar.gz/msmexplorer-0.2.0/msmexplorer/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '0.2.0'
+short_version = '0.2.0'
+full_version = '0.2.0'
+git_revision = 'ffdf9abf3df75bdff8eebad91bbe81024ab2ee2d'
+release = True
