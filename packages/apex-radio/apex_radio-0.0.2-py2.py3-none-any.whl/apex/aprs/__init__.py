fdd60f61eed9a052d033ae780ce31051ab4d811e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:25 +0000

    Add package apex-radio/0.0.2/apex_radio-0.0.2-py2.py3-none-any.whl

---
+++ packages/apex-radio/apex_radio-0.0.2-py2.py3-none-any.whl/apex/aprs/__init__.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# APRS Python Module.
+
+"""
+APRS Python Module.
+~~~~
+
+
+:author: Jeffrey Phillips Freeman WI2ARD <freemo@gmail.com>
+:copyright: Copyright 2016 Syncleus, Inc. and contributors
+:license: Apache License, Version 2.0
+:source: <https://github.com/syncleus/apex>
+
+"""
+
+# These imports are for python3 compatability inside python2
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import logging
+
+from .aprs import Aprs  # noqa: F401
+from .igate import IGate  # noqa: F401
+from .igate import ReconnectingPacketBuffer  # noqa: F401
+
+__author__ = 'Jeffrey Phillips Freeman (WI2ARD)'
+__maintainer__ = 'Jeffrey Phillips Freeman (WI2ARD)'
+__email__ = 'jeffrey.freeman@syncleus.com'
+__license__ = 'Apache License, Version 2.0'
+__copyright__ = 'Copyright 2016, Syncleus, Inc. and contributors'
+__credits__ = []
+__version__ = '0.0.2'
+
+# Set default logging handler to avoid "No handler found" warnings.
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        """Default logging handler to avoid "No handler found" warnings."""
+        def emit(self, record):
+            """Default logging handler to avoid "No handler found" warnings."""
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
