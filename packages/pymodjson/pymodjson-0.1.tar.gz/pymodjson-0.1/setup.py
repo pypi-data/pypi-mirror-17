e8a22866e8eaa800c9bf037e70e1d2a6610a8757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:11 +0000

    Add package pymodjson/0.1/pymodjson-0.1.tar.gz

---
+++ packages/pymodjson/pymodjson-0.1.tar.gz/pymodjson-0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='pymodjson',
+    packages=['pymodjson'],
+    version='0.1',
+    description='A simple framework to model JSON objects as Python classes.',
+    author='Saravana N Siddhavarapu',
+    author_email='saravanareddys@gmail.com',
+    url='https://github.com/saravanareddy/pymodjson',
+    download_url='https://github.com/saravanareddy/pymodjson/tarball/0.1',
+    keywords=['JSON', 'objects', 'models', 'python', 'classes'],
+    classifiers=[],
+)
