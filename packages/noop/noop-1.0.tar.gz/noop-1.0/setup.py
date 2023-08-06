ef317d7ccb36558833f50eb2fa43577232d391f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:29 +0000

    Add package noop/1.0/noop-1.0.tar.gz

---
+++ packages/noop/noop-1.0.tar.gz/noop-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='noop',
+    packages=['noop'],  # this must be the same as the name above
+    version='1.0',
+    description='''Simplest package to test pip
+    ''',
+    author='J. Fernando Sanchez',
+    author_email='balkian@gmail.com',
+    url='https://github.com/balkian/noop/',  # use the URL to the github repo
+    download_url='https://github.com/balkian/noop/archive/{}.tar.gz' .format('1.0'),
+    keywords=['noop'],
+    classifiers=[],
+    install_requires=[],
+)
