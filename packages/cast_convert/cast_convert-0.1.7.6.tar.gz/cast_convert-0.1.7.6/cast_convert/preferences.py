ab0f63de93750c783b439973aa72c53be77c181b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:08 +0000

    Add package cast_convert/0.1.7.6/cast_convert-0.1.7.6.tar.gz

---
+++ packages/cast_convert/cast_convert-0.1.7.6.tar.gz/cast_convert-0.1.7.6/cast_convert/preferences.py
@@ -0,0 +1,28 @@
+from multiprocessing import cpu_count
+
+
+ENCODER_OPTIONS = {
+    'mp3': '-c:a libmp3lame '
+           '-q:a 3 ',
+    'h264': '-c:v libx264 '
+            '-preset ultrafast '
+            '-crf 21 ',
+    'mp4': '-f mp4 '
+}
+
+COPY_OPTIONS = {
+    'audio': '-c:a copy',
+    'video': '-c:v copy',
+    'container': ''
+}
+
+__cpus = cpu_count()
+THREADS = (__cpus - 1) if __cpus >= 2 else 1
+
+AUDIO_CODEC = 'mp3'
+VIDEO_CODEC = 'h264'
+CONTAINER_TYPE = 'mp4'
+
+NEW_FILE_FMT = '%s_castconvert.mp4'
+
+FILESIZE_CHECK_WAIT = 2.0
