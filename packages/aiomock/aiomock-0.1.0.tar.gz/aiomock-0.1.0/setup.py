539969edf2fec3eafa0f32e2312e48c7a5276256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:23 +0000

    Add package aiomock/0.1.0/aiomock-0.1.0.tar.gz

---
+++ packages/aiomock/aiomock-0.1.0.tar.gz/aiomock-0.1.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='aiomock',
+    version='0.1.0',
+    requires=[
+    ],
+    packages=['aiomock'],
+    url='https://github.com/nhumrich/aiomock',
+    license='MIT',
+    author='nhumrich',
+    author_email='nick@humrich.us',
+    keywords=['asyncio', 'testing', 'mock'],
+    description='a python mock with async methods'
+)
