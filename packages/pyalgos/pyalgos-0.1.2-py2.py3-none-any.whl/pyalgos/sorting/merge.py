a13ab8b823adcdb5fcaddb8322e5752f338be63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:05 +0000

    Add package pyalgos/0.1.2/pyalgos-0.1.2-py2.py3-none-any.whl

---
+++ packages/pyalgos/pyalgos-0.1.2-py2.py3-none-any.whl/pyalgos/sorting/merge.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+def merge(elements):
+    if len(elements) <= 1:
+        # When the length of the list if less than or equal to 1, just return
+        # the list back.
+        return elements
