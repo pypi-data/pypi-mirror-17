3a4ca5d3e20c241fa6df7b07bddf33911d04ee19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:39 +0000

    Add package wuqiwei/1.1.0/wuqiwei-1.1.0.zip

---
+++ packages/wuqiwei/wuqiwei-1.1.0.zip/wuqiwei-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'wuqiwei',
+	version = '1.1.0',
+	py_modules = ['nester'],
+	author = 'wuqiwei',
+	author_email = 'wuqiwei@dino-info.com',
+	url = 'http://www.dino-tech.com',
+	description = 'A simple printer of nested lists',
+    )
