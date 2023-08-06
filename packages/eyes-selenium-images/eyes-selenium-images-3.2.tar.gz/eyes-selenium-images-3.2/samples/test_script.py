1db6cb76b9590a6fbb854aa6b38669a02aa153e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:57 +0000

    Add package eyes-selenium-images/3.2/eyes-selenium-images-3.2.tar.gz

---
+++ packages/eyes-selenium-images/eyes-selenium-images-3.2.tar.gz/eyes-selenium-images-3.2/samples/test_script.py
@@ -0,0 +1,20 @@
+import os
+
+from applitools import logger
+from applitools.eyes import Eyes
+from applitools.geometry import Region
+from applitools.logger import StdoutLogger
+
+logger.set_logger(StdoutLogger())
+eyes = Eyes("https://localhost.applitools.com")
+eyes.api_key = os.environ['APPLITOOLS_API_KEY']
+
+try:
+    eyes.open(None, "Python Images", "First image test")
+    with open("MY_IMAGE.png", 'rb') as f:
+        image = f.read()
+    eyes.check_image(image, None, "home")
+    eyes.check_image(image, Region(left=1773, top=372, width=180, height=220), "My region")
+    eyes.close()
+finally:
+    eyes.abort_if_not_closed()
