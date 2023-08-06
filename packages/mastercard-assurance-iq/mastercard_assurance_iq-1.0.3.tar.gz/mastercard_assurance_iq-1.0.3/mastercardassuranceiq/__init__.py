9fd6cf014c6a6a95ab9f7984963a73ab4a6f8d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:22 +0000

    Add package mastercard-assurance-iq/1.0.3/mastercard_assurance_iq-1.0.3.tar.gz

---
+++ packages/mastercard-assurance-iq/mastercard_assurance_iq-1.0.3.tar.gz/mastercard_assurance_iq-1.0.3/mastercardassuranceiq/__init__.py
@@ -0,0 +1,5 @@
+from mastercardapicore import *
+from mastercardassuranceiq.ecommercemessage import *
+from mastercardassuranceiq.ecommercemessageconfidence import *
+from mastercardassuranceiq.sdkconfig import *
+from mastercardassuranceiq.ecommercemessagescore import *
