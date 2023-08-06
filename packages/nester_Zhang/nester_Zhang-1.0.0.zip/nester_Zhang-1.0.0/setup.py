1de827e6be955bf4b124424a34f76315ca7d0f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:15 +0000

    Add package nester_Zhang/1.0.0/nester_Zhang-1.0.0.zip

---
+++ packages/nester_Zhang/nester_Zhang-1.0.0.zip/nester_Zhang-1.0.0/setup.py
@@ -0,0 +1,16 @@
+'''
+Created on 2016-10-11
+
+@author: Administrator
+'''
+from distutils.core import setup
+
+setup(
+        name="nester_Zhang",
+        version="1.0.0",
+        py_modules=["nester_Zhang"],
+        author="AAA",
+        author_email="AAA@qq.com",
+        url="http://www.baidu.com",
+        description="",
+      )
