dc95911385233ea17e0ca1056c96818b663c9d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:15 +0000

    Add package flask-tmpl/0.1.0/flask-tmpl-0.1.0.tar.gz

---
+++ packages/flask-tmpl/flask-tmpl-0.1.0.tar.gz/flask-tmpl-0.1.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+from setuptools import setup, find_packages
+version = '0.1.0'
+classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    ("Topic :: Software Development :: "
+     "Libraries :: Python Modules")
+]
+
+setup(name="flask-tmpl",
+      version=version,
+      description="PasteScript templates for the Flask+Celery+SQLAlchemy",
+      classifiers=classifiers,
+      keywords='flask paste templates',
+      author='Tyrael Lau',
+      author_email='liqianglau@outlook.com',
+      url='https://liuliqiang.info/docs/flask-tmpl',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['flask'],
+      include_package_data=True,
+      install_requires=['setuptools', 'PasteScript'],
+      entry_points="""
+        # -*- Entry points: -*-
+         [paste.paster_create_template]
+        flask-tmpl=flask.skels.package:Package
+      """
+)
