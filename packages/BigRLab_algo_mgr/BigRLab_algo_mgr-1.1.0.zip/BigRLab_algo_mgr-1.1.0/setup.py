44eee7f63a89647954d4772396eb757c1013314e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:05 +0000

    Add package BigRLab_algo_mgr/1.1.0/BigRLab_algo_mgr-1.1.0.zip

---
+++ packages/BigRLab_algo_mgr/BigRLab_algo_mgr-1.1.0.zip/BigRLab_algo_mgr-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+      name='BigRLab_algo_mgr',
+      version='1.1.0',
+      py_modules=['BigRLab_algo_mgr'],
+      author='lipd',
+      author_email='pandeng.li@163.com',
+      description='BigRLab apiserver for users',
+
+
+      )
