bd0b6be757abf34275e7c71ac5bc374f08d660bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:49 +0000

    Add package sda/0.8.2/sda-0.8.2-py2.py3-none-any.whl

---
+++ packages/sda/sda-0.8.2-py2.py3-none-any.whl/login/page.py
@@ -0,0 +1,15 @@
+from sda.fixtures import *
+from sda.page import *
+from sda.structures import *
+
+
+class LoginPage(Page)
+
+	def __init__(self, driver):
+
+	# --- Add your page elements below ---
+
+	# An example might be:
+	# self.username = InputText(driver, By.ID, "id_username")
+
+	pass
