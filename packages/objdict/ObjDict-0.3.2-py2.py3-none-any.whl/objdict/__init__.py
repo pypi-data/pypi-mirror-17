6bc43ec10fc1b4e1f5f53518c43081e0b46fd65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:06 +0000

    Add package objdict/0.3.2/ObjDict-0.3.2-py2.py3-none-any.whl

---
+++ packages/objdict/ObjDict-0.3.2-py2.py3-none-any.whl/objdict/__init__.py
@@ -0,0 +1,4 @@
+from objdict.objDict import dumps, loads, from_json, to_json
+from objdict.objDict import ClassRegistry, JsonEncoder, ObjDict
+from objdict.dualUrlJson import combiParse,unParse
+from objdict import objDict
