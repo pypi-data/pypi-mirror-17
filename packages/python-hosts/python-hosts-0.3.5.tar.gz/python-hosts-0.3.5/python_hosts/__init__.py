11580a5bd73fb3886e00209232e55b56ad329d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:14 +0000

    Add package python-hosts/0.3.5/python-hosts-0.3.5.tar.gz

---
+++ packages/python-hosts/python-hosts-0.3.5.tar.gz/python-hosts-0.3.5/python_hosts/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+This package contains all of the modules utilised by the python-hosts library.
+
+hosts: Contains the Hosts and HostsEntry classes that represent instances of a hosts file and it's individual lines/entries
+
+utils: Contains helper functions to check the available operations on a hosts file and the validity of a hosts file entry
+
+exception: Contains the custom exceptions that are raised in the event of an error in processing a hosts file and its entries
+"""
