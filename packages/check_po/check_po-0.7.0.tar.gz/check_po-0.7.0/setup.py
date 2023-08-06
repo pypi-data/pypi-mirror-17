9ac69f10f6b58ba18b99d30cc7749ea32eb8721d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:20 +0000

    Add package check_po/0.7.0/check_po-0.7.0.tar.gz

---
+++ packages/check_po/check_po-0.7.0.tar.gz/check_po-0.7.0/setup.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+from setuptools import setup
+
+setup(
+    # test_suite='nose.collector',
+    setup_requires=['setuptools>=28.0', 'pbr>=1.9'],
+    # tests_require=['nose', 'coverage'],
+    pbr=True,
+)
