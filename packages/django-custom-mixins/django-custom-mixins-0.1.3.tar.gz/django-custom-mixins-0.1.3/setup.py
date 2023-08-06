f4f2f630565647ebda30746803fd3f0f844facd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:37 +0000

    Add package django-custom-mixins/0.1.3/django-custom-mixins-0.1.3.tar.gz

---
+++ packages/django-custom-mixins/django-custom-mixins-0.1.3.tar.gz/django-custom-mixins-0.1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-custom-mixins',
+    version='0.1.3',
+    packages=find_packages(),
+    author='Dmitry Kalinin',
+    author_email='dmitry.kalinin.email@gmail.com',
+    url='https://github.com/null-none/django-custom-mixins',
+)
