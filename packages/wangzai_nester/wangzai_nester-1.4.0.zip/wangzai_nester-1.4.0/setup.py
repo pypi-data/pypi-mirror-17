3e53907e1b184848a8b37c7512e3ec95aa3062ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:28 +0000

    Add package wangzai_nester/1.4.0/wangzai_nester-1.4.0.zip

---
+++ packages/wangzai_nester/wangzai_nester-1.4.0.zip/wangzai_nester-1.4.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='wangzai_nester',
+        version='1.4.0',
+        py_modules=['nester'],
+        author='allen.xu',
+        author_email='nanjing_eryu@outlook.com',
+        url='http://www.headfirstlabs.com',
+        description='A simple printer of nested lists',
+        )
+
+        
