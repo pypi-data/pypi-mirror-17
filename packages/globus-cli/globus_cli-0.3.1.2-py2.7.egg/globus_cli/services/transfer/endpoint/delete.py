d2c0c5f77905d4170e2a5f231b3826955c1497e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:43 +0000

    Add package globus-cli/0.3.1.2/globus_cli-0.3.1.2-py2.7.egg

---
+++ packages/globus-cli/globus_cli-0.3.1.2-py2.7.egg/globus_cli/services/transfer/endpoint/delete.py
@@ -0,0 +1,23 @@
+import click
+
+from globus_cli.safeio import safeprint
+from globus_cli.helpers import (
+    common_options, outformat_is_json, print_json_response)
+from globus_cli.services.transfer.helpers import get_client, endpoint_id_option
+
+
+@click.command('delete', help='Delete a given Endpoint')
+@common_options
+@endpoint_id_option
+def endpoint_delete(endpoint_id):
+    """
+    Executor for `globus transfer endpoint delete`
+    """
+    client = get_client()
+
+    res = client.delete_endpoint(endpoint_id)
+
+    if outformat_is_json():
+        print_json_response(res)
+    else:
+        safeprint(res['message'])
---
+++ packages/globus-cli/globus_cli-0.3.1.2-py2.7.egg/globus_cli/services/transfer/endpoint/server/list.py
@@ -0,0 +1,22 @@
+import click
+
+from globus_cli.helpers import common_options, print_table, outformat_is_json
+from globus_cli.services.transfer.helpers import (
+    get_client, endpoint_id_option, print_json_from_iterator)
+
+
+@click.command('list', help='List all servers belonging to an Endpoint')
+@common_options
+@endpoint_id_option
+def server_list(endpoint_id):
+    """
+    Executor for `globus transfer endpoint server list`
+    """
+    client = get_client()
+
+    server_iterator = client.endpoint_server_list(endpoint_id)
+
+    if outformat_is_json():
+        print_json_from_iterator(server_iterator)
+    else:
+        print_table(server_iterator, [('ID', 'id'), ('URI', 'uri')])
