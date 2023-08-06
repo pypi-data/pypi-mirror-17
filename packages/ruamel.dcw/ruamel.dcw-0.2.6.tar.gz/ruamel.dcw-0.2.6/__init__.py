7f935a6d990d90173f669a89ac56153d9c3c6213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:08 +0000

    Add package ruamel.dcw/0.2.6/ruamel.dcw-0.2.6.tar.gz

---
+++ packages/ruamel.dcw/ruamel.dcw-0.2.6.tar.gz/ruamel.dcw-0.2.6/__init__.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+
+from __future__ import print_function
+from __future__ import absolute_import
+
+_package_data = dict(
+    full_package_name="ruamel.dcw",
+    version_info=(0, 2, 6),
+    author="Anthon van der Neut",
+    author_email="a.van.der.neut@ruamel.eu",
+    description="docker-compose wrapper allowing for user data and env. var defaults",
+    # keywords="",
+    entry_points='dcw=ruamel.dcw.__main__:main',
+    # entry_points=None,
+    license="MIT",
+    since=2016,
+    # status: "α|β|stable",  # the package status on PyPI
+    # data_files="",
+    universal=True,
+    install_requires=dict(
+        any=["docker-compose>=1.8.0,<1.9.0", "ruamel.yaml>=0.11", "ruamel.std.pathlib",
+             "ruamel.showoutput"],
+        # py27=["ruamel.ordereddict"],
+    ),
+)
+
+
+def _convert_version(tup):
+    """Create a PEP 386 pseudo-format conformant string from tuple tup."""
+    ret_val = str(tup[0])  # first is always digit
+    next_sep = "."  # separator for next extension, can be "" or "."
+    for x in tup[1:]:
+        if isinstance(x, int):
+            ret_val += next_sep + str(x)
+            next_sep = '.'
+            continue
+        first_letter = x[0].lower()
+        next_sep = ''
+        if first_letter in 'abcr':
+            ret_val += 'rc' if first_letter == 'r' else first_letter
+        elif first_letter in 'pd':
+            ret_val += '.post' if first_letter == 'p' else '.dev'
+    return ret_val
+
+version_info = _package_data['version_info']
+__version__ = _convert_version(version_info)
+
+del _convert_version
