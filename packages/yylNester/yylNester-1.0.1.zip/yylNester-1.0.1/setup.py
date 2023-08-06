a3732a4adabb8d60dd1ac6114c775be73becf654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:13 +0000

    Add package yylNester/1.0.1/yylNester-1.0.1.zip

---
+++ packages/yylNester/yylNester-1.0.1.zip/yylNester-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'yylNester',
+		version = '1.0.1',
+		py_modules = ['yylNester'],
+		author = 'hfpython',
+		author_email = '136083918@qq.com',
+		url = 'http://www.headfirstlabs.com',
+		description = 'A simple printer of nested lists'
+	)
