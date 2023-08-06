158bd40c58d7b20b5a081df9c8e35b72da516e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:23 +0000

    Add package mosfit/0.1.0/mosfit-0.1.0.tar.gz

---
+++ packages/mosfit/mosfit-0.1.0.tar.gz/mosfit-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+extensions = []
+
+setup(
+    name='mosfit',
+    packages=['mosfit'],
+    version='0.1.0',
+    description=('Package that performs maximum likelihood analysis to fit '
+                 'semi-analytical model predictions to observed '
+                 'astronomical transient data.'),
+    author='James Guillochon',
+    author_email='guillochon@gmail.com',
+    url='https://github.com/guillochon/mosfit',
+    download_url='https://github.com/guillochon/mypackage/tarball/0.1',
+    keywords=['astronomy', 'fitting', 'monte carlo', 'modeling'],
+    classifiers=[]
+)
