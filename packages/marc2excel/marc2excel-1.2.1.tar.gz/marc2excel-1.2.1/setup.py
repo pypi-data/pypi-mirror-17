9052934ea25c00f89b82f1a50671ae3e798fa98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:29 +0000

    Add package marc2excel/1.2.1/marc2excel-1.2.1.tar.gz

---
+++ packages/marc2excel/marc2excel-1.2.1.tar.gz/marc2excel-1.2.1/setup.py
@@ -0,0 +1,53 @@
+import re
+from setuptools import setup
+
+
+version = re.search('^__version__\s*=\s*"(.*)"',
+                    open('marc2excel/__init__.py').read(),
+                    re.M).group(1)
+
+try:
+    long_description = read("README.rst")
+except:
+    long_description = ""
+
+
+requirements = [
+    "openpyxl>=2.3.0, <3.0",
+    "pymarc>=3.1.5, <4.0",
+    "click>=6.6, <7.0",
+    "tqdm>=4.7.0, <5.0"
+]
+
+setup_requirements = [
+    "pytest-runner>=2.9, <3.0",
+]
+
+test_requirements = [
+    "pytest>=3.0, <4.0",
+    "pytest-cov>=2.3.1, <3.0",
+    "pytest-mock>=1.0, <2.0"
+]
+
+setup(
+    name='marc2excel',
+    version=version,
+    author='Alexander Mendes',
+    author_email='alexanderhmendes@gmail.com',
+    url='https://github.com/alexandermendes/marc2excel',
+    license="BSD",
+    description='Convert MARC files to Excel spreadsheets and vice-versa.',
+    long_description=long_description,
+    install_requires=requirements,
+    setup_requires=setup_requirements,
+    tests_require=test_requirements,
+    packages=['marc2excel'],
+    scripts=["bin/marc2excel_cli.py", "bin/excel2marc_cli.py"],
+    test_suite="test",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python"
+    ],
+)
---
+++ packages/marc2excel/marc2excel-1.2.1.tar.gz/marc2excel-1.2.1/test/test_main.py
@@ -0,0 +1,37 @@
+# -*- coding: utf8 -*-
+
+import openpyxl
+import pymarc
+import marc2excel
+from marc2excel.main import Converter
+from pytest_mock import mocker
+
+converter = marc2excel.Converter()
+
+
+class TestConverter():
+
+    def test_excel2marc_convenience_method(self, tmp_mrc, tmp_xlsx, mocker):
+        mocker.patch('marc2excel.main.Converter.excel2marc')
+        marc2excel.excel2marc(tmp_xlsx, tmp_mrc)
+        Converter.excel2marc.assert_called_once_with(tmp_xlsx, tmp_mrc,
+                                                     sheet=0, force_utf8=False)
+
+    def test_marc2excel_convenience_method(self, tmp_mrc, tmp_xlsx, mocker):
+        mocker.patch('marc2excel.main.Converter.marc2excel')
+        marc2excel.marc2excel(tmp_mrc, tmp_xlsx)
+        Converter.marc2excel.assert_called_once_with(tmp_mrc, tmp_xlsx,
+                                                     force_utf8=False)
+
+    def test_marc2excel_converts_all_records(self, marc_path, tmp_xlsx):
+        converter.marc2excel(marc_path, tmp_xlsx)
+        wb = openpyxl.load_workbook(tmp_xlsx)
+        ws = wb.get_active_sheet()
+        assert ws.max_row == 21
+        assert ws.max_column == 80
+
+    def test_excel2marc_converts_all_records(self, xlsx_path, tmp_mrc):
+        converter.excel2marc(xlsx_path, tmp_mrc.name)
+        reader = pymarc.MARCReader(tmp_mrc)
+        count = len([record for record in reader])
+        assert count == 20
