f64be75f77700cde7b8ff4a37dccb6d3da8b602e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:40 +0000

    Add package UpdateSystem/0.0.7/UpdateSystem-0.0.7.tar.gz

---
+++ packages/UpdateSystem/UpdateSystem-0.0.7.tar.gz/UpdateSystem-0.0.7/lib/updatesystem/log.py
@@ -0,0 +1,32 @@
+import logging
+from datetime import datetime
+
+def getlogger():
+
+    logger = logging.getLogger()
+    # hard code
+    now = datetime.now()
+    filename="/var/log/update-system.{}-{:02d}-{:02d}.log".format(now.year, now.month,
+            now.day)
+
+    # set formatter
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s] - [%(module)s %(funcName)s() line:%(lineno)d ]: %(message)s',"%Y-%m-%d %H:%M:%S") 
+
+    # set fh 
+    fh = logging.FileHandler(filename, encoding="UTF-8")
+    fh.setFormatter(formatter)
+    fh.setLevel(logging.DEBUG)
+    # set ch
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    ch.setLevel(logging.INFO)
+    # set logger with ch and fh
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+
+    return logger
+
+
+logger = getlogger()
+
