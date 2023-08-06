de1c63bf1653813cef22c7e46ac8f24e4b7bdc62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:04 +0000

    Add package djl_nester/1.3.0/djl_nester-1.3.0.tar.gz

---
+++ packages/djl_nester/djl_nester-1.3.0.tar.gz/djl_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'djl_nester',
+    version = '1.3.0',
+    py_modules = ['djl_nester'],
+    author = 'hfpython',
+    author_email = 'livewiremktg@gmail.com',
+    url = 'http://www.someurlhere.com',
+    description = 'A simple printer of nested lists. Default is set to no indent feature but can be added buy setting indent to True',
+)
