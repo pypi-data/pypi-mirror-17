9d40eadd6ba5137e5366296205725c6e67f1002c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:45 +0000

    Add package OntoContext/0.12/OntoContext-0.12.tar.gz

---
+++ packages/OntoContext/OntoContext-0.12.tar.gz/OntoContext-0.12/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name = 'OntoContext',
+    packages = ['OntoContext'],
+    version = '0.12',  
+    description = 'Beta Version',
+    author = 'Walid Bedhiafi',
+    author_email = 'ontocontext@gmail.com',
+    url = 'https://github.com/walidbedhiafi/OntoContext1',
+    download_url = 'https://github.com/walidbedhiafi/OntoContext1/archive/0.12.tar.gz',
+    keywords = ['Bioinformatics', 'textmining' , 'Annotating', 'Ontology', 'biomedicaltext'],
+    classifiers = [],
+)
+
+
