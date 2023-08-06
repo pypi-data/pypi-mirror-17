8b39da2662162c1524ef92b6dda5fcf36261f083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:02 +0000

    Add package KegLogin/0.5.0/KegLogin-0.5.0.tar.gz

---
+++ packages/KegLogin/KegLogin-0.5.0.tar.gz/KegLogin-0.5.0/keg_login/ext.py
@@ -0,0 +1,14 @@
+import jinja2
+
+
+class KegLogin(object):
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        loader = jinja2.ChoiceLoader([
+            app.jinja_loader,
+            jinja2.PackageLoader('keg_login', 'templates')
+        ])
+        app.jinja_loader = loader
