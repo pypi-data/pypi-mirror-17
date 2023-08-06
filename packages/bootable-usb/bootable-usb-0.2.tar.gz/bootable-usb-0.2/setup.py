80a29647db45001d5af7965896511310395ae38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:20 +0000

    Add package bootable-usb/0.2/bootable-usb-0.2.tar.gz

---
+++ packages/bootable-usb/bootable-usb-0.2.tar.gz/bootable-usb-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='bootable-usb',
+    version='0.2',
+    scripts=['bootable-usb'],
+    url='https://www.github.com/balzss/bootable-usb',
+    license='GPLv3'
+)
