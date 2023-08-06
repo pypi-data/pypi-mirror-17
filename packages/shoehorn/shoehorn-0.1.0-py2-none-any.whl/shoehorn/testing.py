530d8b2973e5ad4d1de1aed6803dda6f4b4a7722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:51 +0000

    Add package shoehorn/0.1.0/shoehorn-0.1.0-py2-none-any.whl

---
+++ packages/shoehorn/shoehorn-0.1.0-py2-none-any.whl/shoehorn/testing.py
@@ -0,0 +1,12 @@
+from . import logger
+
+class CaptureLogging(object):
+
+    def __init__(self, logger=logger):
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
