7d4d384f2c3a3986e369f76e003855b5e923303b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:19 +0000

    Add package irpy/0.6.2/irpy-0.6.2.tar.gz

---
+++ packages/irpy/irpy-0.6.2.tar.gz/irpy-0.6.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, Extension
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    USE_CYTHON = False
+else:
+    USE_CYTHON = True
+
+ext = '.pyx' if USE_CYTHON else '.c'
+extensions = [Extension("irpy", ["irpy"+ext])]
+
+if USE_CYTHON:
+    extensions = cythonize(extensions)
+
+setup(
+    name="irpy",
+    version="0.6.2",
+    ext_modules = extensions,
+    description = 'IRP for Python',
+    long_description='Look a github README please.',
+    url='https://github.com/TApplencourt/IRPy',
+    download_url = 'https://github.com/TApplencourt/IRPy/archive/master.tar.gz',
+    license='WTFPL',
+    author='Thomas Applencourt',
+    author_email='thomas.applencourt@irsamc.ups-tlse.fr',
+    use_2to3=True,
+)
