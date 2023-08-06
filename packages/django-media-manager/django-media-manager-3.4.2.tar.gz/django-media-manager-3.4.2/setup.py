ac3c0584b70a5cd81813d3d82b0bd8793e1978f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:29 +0000

    Add package django-media-manager/3.4.1/django-media-manager-3.4.2.tar.gz

---
+++ packages/django-media-manager/django-media-manager-3.4.2.tar.gz/django-media-manager-3.4.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-media-manager',
+    version='3.4.2',
+    description='Media-Management with the Django Admin-Interface.',
+    author=['Patrick Kranzlmueller','Six Foot'],
+    author_email='dev@6ft.com',
+    url='https://github.com/oliverseal/django-media-manager',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    install_requires = [
+        "Django>=1.8",
+        "pillow",
+    ],
+)
