b3bb4a36af507744aa2ecead8a03a22f5ac484a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:08 +0000

    Add package funcrec/1.0.0/funcrec-1.0.0.tar.gz

---
+++ packages/funcrec/funcrec-1.0.0.tar.gz/funcrec-1.0.0/funcRec.py
@@ -0,0 +1,13 @@
+"""Este é o módulo "nester.py", e fornece uma função chamada print_lol()
+que imprimem listas que podem ou não conter listas aninhadas"""
+'Este é o módulo "nester.py", e fornece uma função chamada print_lol()\nque imprimem listas que podem ou não conter listas aninhadas'
+def print_lol(the_list):
+	"""Esta função requer um argumento posicional chamado "the_list", que é
+qualquer lista Python (de possíveis listas aninhadas). Cada item de dados na
+lista fornecida é (recursivamente) impresso na tela em sua própria linha"""
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item)
+		else:
+			print(each_item)
+			
---
+++ packages/funcrec/funcrec-1.0.0.tar.gz/funcrec-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'funcrec',
+version = '1.0.0',
+py_modules = ['funcRec'],
+author = 'Odair Soares',
+author_email = 'odairsds@gmail.com',
+url = 'http://www.odairsoares.com',
+description = 'A simple printer of nested liss'
+)
