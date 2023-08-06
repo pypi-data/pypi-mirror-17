0bbd1b26807ecafb1cb97a86b5534655810d7e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:12 +0000

    Add package footmark/1.0.5.dev29/footmark-1.0.5.dev29.tar.gz

---
+++ packages/footmark/footmark-1.0.5.dev29.tar.gz/footmark-1.0.5.dev29/footmark/__init__.py
@@ -0,0 +1,36 @@
+#
+import logging
+import logging.config
+import os
+from footmark.pyami.config import Config, FootmarkLoggingConfig, DefaultLoggingConfig
+
+__version__ = '1.0.5.dev.29'
+Version = __version__  # for backware compatibility
+
+
+def init_logging():
+    try:
+        Config().init_config()
+        try:
+            logging.config.fileConfig(os.path.expanduser(FootmarkLoggingConfig))
+        except:
+            logging.config.dictConfig(DefaultLoggingConfig)
+    except:
+        pass
+
+init_logging()
+log = logging.getLogger('footmark')
+
+def connect_ecs(acs_access_key_id=None, acs_secret_access_key=None, **kwargs):
+    """
+    :type acs_access_key_id: string
+    :param acs_access_key_id: Your AWS Access Key ID
+
+    :type acs_secret_access_key: string
+    :param acs_secret_access_key: Your AWS Secret Access Key
+
+    :rtype: :class:`footmark.ecs.connection.ECSConnection`
+    :return: A connection to Amazon's ECS
+    """
+    from footmark.ecs.connection import ECSConnection
+    return ECSConnection(acs_access_key_id, acs_secret_access_key, **kwargs)
