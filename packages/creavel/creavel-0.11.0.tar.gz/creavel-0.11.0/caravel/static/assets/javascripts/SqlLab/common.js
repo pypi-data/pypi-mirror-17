fc5c62b353f6a9cfb49cff6c6a13c256803f6633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:33 +0000

    Add package creavel/0.11.0/creavel-0.11.0.tar.gz

---
+++ packages/creavel/creavel-0.11.0.tar.gz/creavel-0.11.0/caravel/static/assets/javascripts/SqlLab/common.js
@@ -0,0 +1,8 @@
+export const STATE_BSSTYLE_MAP = {
+  failed: 'danger',
+  pending: 'info',
+  running: 'warning',
+  success: 'success',
+};
+
+export const STATUS_OPTIONS = ['success', 'failed', 'running'];
