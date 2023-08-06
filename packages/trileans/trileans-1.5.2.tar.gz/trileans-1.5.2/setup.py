e062b241e66e0bcd41b31a95c738ba18d54a0e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:48 +0000

    Add package trileans/1.5.2/trileans-1.5.2.tar.gz

---
+++ packages/trileans/trileans-1.5.2.tar.gz/trileans-1.5.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+    name = "trileans",
+    version = "1.5.2",
+    packages = ['trileans'],
+    author = "rittbys",
+    description = "Trileans are three-valued objects.",
+    license = "MIT",
+    keywords = "Trileans objects",
+    url = "https://github.com/rittbys/Trileans",
+    classifiers=[
+	'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4'
+    ]
+)
