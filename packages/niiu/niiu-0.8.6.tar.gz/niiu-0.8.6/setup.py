f3ae46e469865583cc13f8e5f883bbef0a29ad29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:56 +0000

    Add package niiu/0.8.6/niiu-0.8.6.tar.gz

---
+++ packages/niiu/niiu-0.8.6.tar.gz/niiu-0.8.6/setup.py
@@ -0,0 +1,21 @@
+""" this script is design for dagobah"""
+# _*_ coding: utf-8 _*_
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+        name = "niiu",
+        packages = find_packages(),
+        version = "0.8.6",
+        classifiers = [
+            "Programming Language :: Python",
+            ],
+	scripts = ['bin/funnist-joke', '/bin/lala.sh'],
+        entry_points = {
+            "console_scripts": [
+                "niiu = niiu.niiu:main",
+		"nana = niiu.nana:main",
+                ],
+            }
+        )
