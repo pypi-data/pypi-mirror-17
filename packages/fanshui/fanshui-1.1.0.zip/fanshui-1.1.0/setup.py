437d62d75313c6d5edd8af10de2a1b8a3b692dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:00 +0000

    Add package fanshui/1.1.0/fanshui-1.1.0.zip

---
+++ packages/fanshui/fanshui-1.1.0.zip/fanshui-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+			name    ='fanshui',
+			version  ='1.1.0',
+			py_modules=['nester'],
+			author='fanshui',
+			author_email='hfpaython@headfirstlabs.com',
+			url='http://www.headfirstlabs.com',
+			description='a simple printer of nested list',
+			
+)
