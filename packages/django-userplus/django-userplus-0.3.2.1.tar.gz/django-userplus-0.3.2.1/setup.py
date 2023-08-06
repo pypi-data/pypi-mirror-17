675fdfcb7e2d2f04ecdad3417b321d2ed6351917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:33 +0000

    Add package django-userplus/0.3.2.1/django-userplus-0.3.2.1.tar.gz

---
+++ packages/django-userplus/django-userplus-0.3.2.1.tar.gz/django-userplus-0.3.2.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='django-userplus',
+      version='0.3.2.1',
+      description='Extended Auth User module for Django',
+      url='https://github.com/ifedapoolarewaju/django-userplus.git',
+      author='Ifedapo Olarewaju',
+      author_email='ifedapoolarewaju@gmail.com',
+      license='MIT',
+      packages=['userplus', 'userplus.lib'],
+      install_requires=['Django>=1.9', 'facebook-sdk>=2.0.0'],
+      classifiers=[
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Framework :: Django'
+      ],
+      zip_safe=False)
---
+++ packages/django-userplus/django-userplus-0.3.2.1.tar.gz/django-userplus-0.3.2.1/userplus/models.py
@@ -0,0 +1,46 @@
+from __future__ import unicode_literals
+import datetime
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.forms.models import model_to_dict
+from django.conf import settings
+
+from userplus.lib.utils import hash_str
+
+
+class UserPlus(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    activation_key = models.CharField(max_length=400, blank=True, null=True)
+    activation_expiry_date = models.DateTimeField(blank=True, null=True)
+
+    if getattr(settings, 'USERPLUS_ENABLE_FACEBOOK_LOGIN'):
+        is_facebook_user = models.BooleanField(default=False, editable=False)
+
+    class Meta:
+        abstract = True
+        unique_together = ('email',)  # hack to make emails unique
+
+    def save(self, *args, **kwargs):
+        # coerce to lowercase for case insensitivity.
+        if not self.pk:
+            self.username = self.username.lower()
+            self.email = self.email.lower()
+
+        if 'set_activation_key' in kwargs and kwargs.pop('set_activation_key'):
+            self.set_activation_key()
+        super(UserPlus, self).save(*args, **kwargs)
+
+    def set_activation_key(self):
+        self.activation_key = hash_str(self.email, 5)
+        activation_days = datetime.timedelta(days=getattr(settings, 'USERPLUS_ACTIVATION_DAYS', 2))
+        self.activation_expiry_date = datetime.datetime.now() + activation_days
+
+    def to_dict(self, fields=None, exclude=None):
+        if not fields and hasattr(self, 'public_fields'):
+            fields = getattr(self, 'public_fields')
+
+        dict_ = model_to_dict(self, fields=fields, exclude=exclude)
+        dict_['id'] = str(self.id)
+        return dict_
