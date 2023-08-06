2837f517ef5b11aa5696fffb48ded1f9f6d7844d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:23 +0000

    Add package nester_peter/1.1.0/nester_peter-1.1.0.zip

---
+++ packages/nester_peter/nester_peter-1.1.0.zip/nester_peter-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+        name = 'nester_peter',
+        version = '1.1.0',
+        py_modules = ['nester_peter'],
+        author = 'peter',
+        author_email = 'peter@gmail.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists'
+    )
