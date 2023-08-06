44c49c44cbad0032ce82637e7e0a21b803702027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:57 +0000

    Add package bufferapp-logger/0.0.4/bufferapp-logger-0.0.4.tar.gz

---
+++ packages/bufferapp-logger/bufferapp-logger-0.0.4.tar.gz/bufferapp-logger-0.0.4/logger/logger.py
@@ -0,0 +1,46 @@
+import logging
+import socket
+from logging import StreamHandler
+import sys
+
+class ContextFilter(logging.Filter):
+    hostname = socket.gethostname()
+    svc_name = None
+
+    def __init__(self, svc_name):
+        self.svc_name = svc_name
+
+    def filter(self, record):
+        record.hostname = ContextFilter.hostname
+        record.svc_name = self.svc_name
+        return True
+
+def getLogger(svc_name='test', level=logging.INFO):
+    logger = logging.getLogger('k8s_logger_' + svc_name)
+    logger.setLevel(level)
+
+    f = ContextFilter(svc_name)
+    logger.addFilter(f)
+
+    handler = StreamHandler(sys.stdout)
+    formatter = logging.Formatter('{"name": "%(svc_name)s", "msg": "%(message)s", "timestamp": "%(asctime)s.%(msecs)03dZ", "hostname": "%(hostname)s"}', datefmt='%Y-%m-%dT%H:%M:%S')
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
+
+def getTracker(svc_name='test', level=logging.INFO):
+    logger = logging.getLogger('k8s_metrics_tracker_' + svc_name)
+    logger.setLevel(level)
+
+    f = ContextFilter(svc_name)
+    logger.addFilter(f)
+
+    handler = StreamHandler(sys.stdout)
+    formatter = logging.Formatter('{"type": "%(svc_name)s", "value": %(message)s, "date": "%(asctime)s.%(msecs)03dZ", "hostname": "%(hostname)s", "buffermetrics": true}', datefmt='%Y-%m-%dT%H:%M:%S')
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
