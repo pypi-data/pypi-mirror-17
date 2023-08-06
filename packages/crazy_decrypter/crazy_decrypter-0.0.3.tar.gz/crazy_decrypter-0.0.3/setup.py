e538fb5088acef58a006462e7d94664d4caaf982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:03 +0000

    Add package crazy_decrypter/0.0.3/crazy_decrypter-0.0.3.tar.gz

---
+++ packages/crazy_decrypter/crazy_decrypter-0.0.3.tar.gz/crazy_decrypter-0.0.3/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name                = "crazy_decrypter",
+    packages            = ['crazy'],
+    version             = "0.0.3",
+    url                 = 'https://github.com/agusmakmun/Crazy-Decrypter/',
+    download_url        = 'https://github.com/agusmakmun/Crazy-Decrypter/tarball/v0.0.3',
+    description         = 'Python Crazy Decrypter is real crazy tool to decrypt md5, sha1, sha224, sha256, sha384, and sha512 with Brute Force method.',
+    license             = 'MIT',
+    author              = 'Agus Makmun (Summon Agus)',
+    author_email        = 'ags@dracos-linux.id',
+    keywords            = ['Crazy Decrypter', 'Python Crazy Decrypter'],
+)
