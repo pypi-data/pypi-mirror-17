9e16b538ad73eeefd1e1b001d022462cfd1ff692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:43 +0000

    Add package rinobot_plugin/0.0.2/rinobot_plugin-0.0.2.tar.gz

---
+++ packages/rinobot_plugin/rinobot_plugin-0.0.2.tar.gz/rinobot_plugin-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rinobot_plugin',
+    description='Rinobot plugin helpers',
+    version='0.0.2',
+
+    author='Eoin Murray',
+    author_email='eoin@rinocloud.com',
+    url='https://github.com/rinocloud/rinobot-plugin',
+    keywords=['rinocloud', 'rinobot', 'plugin'],
+
+    packages=find_packages(),
+    include_package_data=True,
+
+    install_requires=[
+        'click',
+        'numpy'
+    ],
+
+    entry_points='''
+        [console_scripts]
+        rinobot-plugin=rinobot_plugin.scripts.cli:cli
+    ''',
+
+    test_suite='rinobot_plugin.test.all',
+    tests_require=['mock'],
+)
