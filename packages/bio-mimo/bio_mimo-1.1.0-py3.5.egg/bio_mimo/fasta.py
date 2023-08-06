6aa38819e96cbda71f778d461a6222c86e746b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:12 +0000

    Add package bio-mimo/1.1.0/bio_mimo-1.1.0-py3.5.egg

---
+++ packages/bio-mimo/bio_mimo-1.1.0-py3.5.egg/bio_mimo/fasta.py
@@ -0,0 +1,44 @@
+import gzip
+
+from collections import deque
+from mimo import Stream
+from lhc.io.fasta.wrapper import FastaWrapper
+
+
+class StreamFasta(Stream):
+
+    IN = ['fasta_file']
+    OUT = ['sequence_fragment']
+
+    async def run(self, ins, outs):
+        async for fasta_file in ins.fasta_file:
+            with gzip.open(fasta_file, 'rt', encoding='utf-8') if fasta_file.endswith('.gz') else \
+                    open(fasta_file, encoding='utf-8') as fileobj:
+                iterator = FastaWrapper(fileobj)
+                for item in iterator:
+                    await outs.sequence_fragment.push(item)
+        outs.sequence_fragment.close()
+
+
+class GetChromosomeSequenceByInterval(Stream):
+
+    IN = ['interval', 'sequence_fragment']
+    OUT = ['sequence']
+
+    async def run(self, ins, outs):
+        fragments = deque()
+        while ins.interval.is_open():
+            interval = await ins.interval.pop()
+            while len(fragments) > 0 and fragments[0].stop < interval.start:
+                fragments.popleft()
+
+            while ins.sequence_fragment.is_open() and (len(fragments) == 0 or fragments[-1].start < interval.stop):
+                fragment = await ins.sequence_fragment.pop()
+                if interval.start < fragment.stop:
+                    fragments.append(fragment)
+
+            sequence = ''.join(fragment.seq for fragment in fragments)
+            fr = interval.start - fragments[0].start
+            to = interval.stop - fragments[0].start
+            await outs.sequence.push(sequence[fr:to])
+        outs.sequence.close()
