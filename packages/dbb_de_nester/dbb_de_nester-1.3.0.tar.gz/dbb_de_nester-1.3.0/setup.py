a7cc2299f5a0f1ed4fbf07e62e7ff456d2d5c092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:52 +0000

    Add package dbb_de_nester/1.3.0/dbb_de_nester-1.3.0.tar.gz

---
+++ packages/dbb_de_nester/dbb_de_nester-1.3.0.tar.gz/dbb_de_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'dbb_de_nester',
+    version = '1.3.0',
+    py_modules = ['nester'],
+    author = 'dbb',
+    author_email = '2923555602@qq.com',
+    url = '',
+    description = 'A simple printer of nested lists.'
+)
