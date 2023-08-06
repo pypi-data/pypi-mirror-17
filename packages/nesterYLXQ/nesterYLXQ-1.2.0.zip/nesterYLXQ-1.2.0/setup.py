c2fd38fd1c96020bea4ddccb51b3f85c08046ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:05 +0000

    Add package nesterYLXQ/1.2.0/nesterYLXQ-1.2.0.zip

---
+++ packages/nesterYLXQ/nesterYLXQ-1.2.0.zip/nesterYLXQ-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name        = 'nesterYLXQ',
+        version     ='1.2.0',
+        py_modules  =['nesterYLXQ'],
+        author      ='YLXQpython',
+        author_email='1521894295@qq.com',
+        #url         'http://yjsxt.xidian.edu.cn/student/index.jsp',
+        description ='A simple printer of nested lists',
+    )
