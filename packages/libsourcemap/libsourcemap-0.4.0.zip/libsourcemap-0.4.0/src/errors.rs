0134690077330d0bc38396153590a680c082fa6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:38 +0000

    Add package libsourcemap/0.4.0/libsourcemap-0.4.0.zip

---
+++ packages/libsourcemap/libsourcemap-0.4.0.zip/libsourcemap-0.4.0/src/errors.rs
@@ -0,0 +1,28 @@
+use std::io;
+use std::str::Utf8Error;
+
+use sourcemap;
+
+
+error_chain! {
+    foreign_links {
+        io::Error, IoError;
+        Utf8Error, Utf8Error;
+        sourcemap::Error, SourceMapError;
+    }
+
+    errors {
+        UnsupportedMemDbVersion {
+            description("Unsupported memdb version")
+        }
+        BadMemDb {
+            description("Bad memdb data")
+        }
+        TooManySources {
+            description("Too many sources in the file for memdb")
+        }
+        TooManyNames {
+            description("Too many names in the file for memdb")
+        }
+    }
+}
---
+++ packages/libsourcemap/libsourcemap-0.4.0.zip/libsourcemap-0.4.0/src/lib.rs
@@ -0,0 +1,19 @@
+#![recursion_limit = "1024"]
+
+extern crate sourcemap;
+extern crate memmap;
+
+#[macro_use]
+extern crate error_chain;
+
+mod errors;
+pub mod memdb;
+
+pub use errors::{Error, ErrorKind, ChainErr, Result};
+pub use unified::{View, TokenMatch};
+
+// unified interface
+mod unified;
+
+// cabi for unified interface
+pub mod cabi;
