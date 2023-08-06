974a4392d1c7ff8ae4df8abe8c7592096aa85aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:31 +0000

    Add package nester_jackieluo_tencent/1.1.0/nester_jackieluo_tencent-1.1.0.zip

---
+++ packages/nester_jackieluo_tencent/nester_jackieluo_tencent-1.1.0.zip/nester_jackieluo_tencent-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester_jackieluo_tencent',
+    version='1.1.0',
+    py_modules=['nester'],
+    author='jackieluo',
+    author_email='luojiachen1@qq.com',
+    url='http://www.headfirstlabs.com',
+    description='A Simple printer of nested lists',
+    )
