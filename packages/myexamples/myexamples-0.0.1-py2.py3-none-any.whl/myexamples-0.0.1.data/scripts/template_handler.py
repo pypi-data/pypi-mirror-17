936c9f5267a06d8df98d446382d922ac83280a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:42 +0000

    Add package myexamples/0.0.1/myexamples-0.0.1-py2.py3-none-any.whl

---
+++ packages/myexamples/myexamples-0.0.1-py2.py3-none-any.whl/myexamples-0.0.1.data/scripts/template_handler.py
@@ -0,0 +1,11 @@
+#!/home/xuanluong/Code/Project/.ENV/bin/python
+
+import pkgutil
+
+
+def load_content(package, resource):
+	return pkgutil.get_data(package, resource)
+
+if __name__ == '__main__':
+	content = load_content('myexamples', 'templates/simple.tmpl')
+	print(content)
