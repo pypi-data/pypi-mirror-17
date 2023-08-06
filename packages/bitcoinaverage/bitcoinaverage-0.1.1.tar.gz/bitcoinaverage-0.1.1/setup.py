8784cd74f8b4d8721856d61c3f949f111da1328b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:54 +0000

    Add package bitcoinaverage/0.1.1/bitcoinaverage-0.1.1.tar.gz

---
+++ packages/bitcoinaverage/bitcoinaverage-0.1.1.tar.gz/bitcoinaverage-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name = 'bitcoinaverage',
+  packages = ['bitcoinaverage'], # this must be the same as the name above
+  version = '0.1.1',
+  description = 'Library to integrate with the BitcoinAverage API',
+  author = 'Blockchain Data LTD',
+  author_email = 'info@bitcoinaverage.com',
+  keywords = ['bitcoinaverage', 'bitcoin', 'api'], # arbitrary keywords
+  classifiers = [],
+)
