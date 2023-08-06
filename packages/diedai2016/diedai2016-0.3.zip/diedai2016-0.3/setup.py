6fda72d893d415a31c23790b5a6d36cbd38072c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:44 +0000

    Add package diedai2016/0.3/diedai2016-0.3.zip

---
+++ packages/diedai2016/diedai2016-0.3.zip/diedai2016-0.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name         =   'diedai2016',
+        version      =   '0.3',
+        py_modules   =   ['dd3'],
+        author       =   'liuyuan',
+        author_email =   'liuyuan@motie.com',
+        url          =   'http://www.baidu.com/',
+        description  =   'rubbish',
+    )
