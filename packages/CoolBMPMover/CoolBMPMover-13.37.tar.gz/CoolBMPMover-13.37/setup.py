de01a98788b897a335759d024d642bc850ac3592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:27 +0000

    Add package CoolBMPMover/13.37/CoolBMPMover-13.37.tar.gz

---
+++ packages/CoolBMPMover/CoolBMPMover-13.37.tar.gz/CoolBMPMover-13.37/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='CoolBMPMover',
+    version='13.37',
+    description='A super cool file mover for CS530!',
+    long_description=long_description,
+    # Author details
+    author='Michael Green',
+    author_email='me@michaelgreen.net',
+    # Choose your license
+    license='GPL',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords="CoolBMPMover"
+)
