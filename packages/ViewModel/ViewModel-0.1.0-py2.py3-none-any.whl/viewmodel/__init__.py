f5f6aaf5fd760ba567ca4457cfc660931ba1e5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:37 +0000

    Add package ViewModel/0.1.0/ViewModel-0.1.0-py2.py3-none-any.whl

---
+++ packages/ViewModel/ViewModel-0.1.0-py2.py3-none-any.whl/viewmodel/__init__.py
@@ -0,0 +1,5 @@
+from viewmodel import viewModel,viewFields
+from viewmodel.viewModel import BaseView
+from viewmodel.viewFields import (Case, IdField, TxtField, IntField,
+                DateField, TimeField, EnumField, EnumForeignField,
+                TxtListField, ObjListField )
