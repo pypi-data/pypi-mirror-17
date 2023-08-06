880106bff42a8a5665e266f8615cc79be14f3892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:46 +0000

    Add package django-userplus/0.3.2.6/django-userplus-0.3.2.6.tar.gz

---
+++ packages/django-userplus/django-userplus-0.3.2.6.tar.gz/django-userplus-0.3.2.6/userplus/backends.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from facebook import GraphAPI
+
+
+class UserPlusBackend(ModelBackend):
+
+    def authenticate(self, username=None, password=None, email=None, facebook_token=None, **kwargs):
+        if facebook_token and getattr(settings, 'USERPLUS_ENABLE_FACEBOOK_LOGIN', None):
+            user = self.authenticate_facebook(facebook_token)
+
+        elif email:
+            UserModel = get_user_model()
+            try:
+                user = UserModel._default_manager.get(email=email.lower())
+                if not user.check_password(password):
+                    user = None
+            except UserModel.DoesNotExist:
+                # Run the default password hasher once to reduce the timing
+                # difference between an existing and a non-existing user (#20760).
+                UserModel().set_password(password)
+                return
+        else:
+            user = super(UserPlusBackend, self).authenticate(
+                username=username.lower(), password=password, **kwargs)
+
+        if getattr(user, 'is_active', None):
+            return user
+
+    def authenticate_facebook(self, token):
+        email = GraphAPI(token).get_object('me').get('email')
+        if email:
+            UserModel = get_user_model()
+            try:
+                user = UserModel._default_manager.get(email=email.lower())
+                if not user.is_facebook_user:
+                    user = None
+            except UserModel.DoesNotExist:
+                username = email
+                user = UserModel.objects.create_user(username, email=email, is_facebook_user=True)
+            return user
+        return None
