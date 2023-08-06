137cd1341e52ec3c39a6c457a1bf6bbe4532d40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:05 +0000

    Add package icecherry1/1.3.0/icecherry1-1.3.0.zip

---
+++ packages/icecherry1/icecherry1-1.3.0.zip/icecherry1-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name		='icecherry1',
+	version		='1.3.0',
+	py_modules	=['icecherry1'],
+	author		='hfpython',
+	author_email='hfpython@headfristlabs.com',
+	url			='http://www.headfristlabs.com',
+	description ='A simple printer of nested lists',
+)
