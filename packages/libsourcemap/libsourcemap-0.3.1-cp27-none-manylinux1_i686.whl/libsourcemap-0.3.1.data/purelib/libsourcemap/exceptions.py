038eb5be7113b96796bfba80c2d6d57296628e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:50 +0000

    Add package libsourcemap/0.3.1/libsourcemap-0.3.1-cp27-none-manylinux1_i686.whl

---
+++ packages/libsourcemap/libsourcemap-0.3.1-cp27-none-manylinux1_i686.whl/libsourcemap-0.3.1.data/purelib/libsourcemap/exceptions.py
@@ -0,0 +1,36 @@
+from ._compat import implements_to_string
+
+
+@implements_to_string
+class SourceMapError(Exception):
+    """Raised if something goes wrong with sourcemap processing."""
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message.encode('utf-8')
+
+
+class IndexedSourceMap(SourceMapError):
+    """Raised if a sourcemap is indexed."""
+
+
+class BadJson(SourceMapError):
+    """Raised if bad JSON data was encountered."""
+
+
+class CannotFlatten(SourceMapError):
+    """Raised if an index cannot be flattened into a sourcemap."""
+
+
+class UnsupportedMemDbVersion(SourceMapError):
+    """Raised if an unsupported memdb is loaded."""
+
+
+special_errors = {
+    2: IndexedSourceMap,
+    3: BadJson,
+    4: CannotFlatten,
+    5: UnsupportedMemDbVersion,
+}
