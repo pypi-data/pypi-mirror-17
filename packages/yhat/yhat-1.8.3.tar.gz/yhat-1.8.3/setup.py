2c34f6cdea0df02bffe2fecff273e99fd73fc718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:19 +0000

    Add package yhat/1.8.3/yhat-1.8.3.tar.gz

---
+++ packages/yhat/yhat-1.8.3.tar.gz/yhat-1.8.3/setup.py
@@ -0,0 +1,39 @@
+import os
+
+from distutils.core import setup
+from setuptools import find_packages
+
+# Get version from defined python file
+with open(
+    os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'yhat',
+        'version.py'
+    )
+) as fh:
+    version = fh.read().strip()
+exec(version)
+
+setup(
+    name="yhat",
+    version=__version__,
+    author="Greg Lamp",
+    author_email="greg@yhathq.com",
+    url="https://github.com/yhat/yhat-client",
+    packages=find_packages(),
+    description="Python client for Yhat (http://yhathq.com/)",
+    license="BSD",
+    classifiers=(
+    ),
+    install_requires=[
+        "pip",
+        "Flask==0.10.1",
+        "websocket-client==0.12.0",
+        "dill==0.2b1",
+        "terragon==0.2.1",
+        "progressbar==2.2",
+        "poster==0.8.1"
+    ],
+    long_description=open("README.rst").read(),
+    keywords=['yhat', 'scikits', 'numpy', 'pandas'],
+)
