fe3f1f89a8a78e52b0760761dea06183ddf7b264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:41 +0000

    Add package jaraco.financial/1.12/jaraco.financial-1.12.tar.gz

---
+++ packages/jaraco.financial/jaraco.financial-1.12.tar.gz/jaraco.financial-1.12/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+# Project skeleton maintained at https://github.com/jaraco/skeleton
+
+import io
+import sys
+
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+	long_description = readme.read()
+
+needs_wheel = {'release', 'bdist_wheel', 'dists'}.intersection(sys.argv)
+wheel = ['wheel'] if needs_wheel else []
+
+name = 'jaraco.financial'
+description = ''
+
+setup_params = dict(
+	name=name,
+	use_scm_version=True,
+	author="Jason R. Coombs",
+	author_email="jaraco@jaraco.com",
+	description=description or name,
+	long_description=long_description,
+	url="https://github.com/jaraco/" + name,
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+	namespace_packages=name.split('.')[:-1],
+	install_requires=[
+		'keyring',
+		'path.py',
+		'ofxparse',
+		'requests',
+		'jaraco.itertools',
+		'jaraco.logging',
+		'jaraco.ui',
+		'jaraco.text',
+		'jaraco.collections',
+		'python-dateutil>=2.0',
+	],
+	extras_require={
+	},
+	setup_requires=[
+		'setuptools_scm>=1.9,!=1.13.1,!=1.14.0',
+	] + wheel,
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+	],
+	entry_points={
+		'console_scripts': [
+			'fix-qif-date-format = jaraco.financial.qif:fix_dates_cmd',
+			'launch-in-money = jaraco.financial.msmoney:launch_cmd',
+			'ofx = jaraco.financial.ofx:handle_command_line',
+			'clean-msmoney-temp = jaraco.financial.msmoney:clean_temp',
+			'record-document-hashes = jaraco.financial.records:send_hashes',
+		],
+	},
+)
+if __name__ == '__main__':
+	setuptools.setup(**setup_params)
