7f8fc15f6d6a9c03307a020cfa1230f12e93ce09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:25 +0000

    Add package cast_convert/0.1.5.13/cast_convert-0.1.5.13.tar.gz

---
+++ packages/cast_convert/cast_convert-0.1.5.13.tar.gz/cast_convert-0.1.5.13/cast_convert/__init__.py
@@ -0,0 +1,16 @@
+__version__ = '0.1.5.13'
+
+
+from .cmd import cmd as command
+from .watch import *
+from . import *
+from .convert import *
+from .media_info import *
+
+
+@click.command(help="Print version")
+def version():
+    print(__version__)
+
+
+command.add_command(version)
