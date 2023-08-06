2e4cf2dd793eed6be08266340d52590353dc3f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:15 +0000

    Add package pytrip98/1.1.4/pytrip98-1.1.4-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/pytrip98/pytrip98-1.1.4-cp27-cp27mu-manylinux1_x86_64.whl/pytrip/__init__.py
@@ -0,0 +1,38 @@
+"""
+    This file is part of PyTRiP.
+
+    PyTRiP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PyTRiP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PyTRiP.  If not, see <http://www.gnu.org/licenses/>
+"""
+
+# do not check this file for PEP8 compatibility
+# flake8 complains about "E402 module level import not at top of file"
+# flake8: noqa
+
+from pytrip.ctx import CtxCube
+from pytrip.dos import DosCube
+from pytrip.vdx import VdxCube, Voi
+from pytrip.paths import DensityCube
+from pytrip.rst import Rst
+from pytrip.let import LETCube
+from pytrip.get_dvh import GetDvh
+from pytrip.ctimage import CTImages
+
+# from https://docs.python.org/3/tutorial/modules.html
+# if a package's __init__.py code defines a list named __all__,
+# it is taken to be the list of module names that should be imported when from package import * is encountered.
+__all__ = ['CtxCube', 'VdxCube', 'Voi', 'GetDvh', 'DosCube', 'DensityCube', 'LETCube', 'dicomhelper', 'res',
+           'Rst', 'CTImages']
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
