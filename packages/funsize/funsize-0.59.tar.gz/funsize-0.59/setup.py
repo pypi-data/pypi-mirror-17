1789733b57cfc982cdcbb586ed66b8472916c4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:43 +0000

    Add package funsize/0.59/funsize-0.59.tar.gz

---
+++ packages/funsize/funsize-0.59.tar.gz/funsize-0.59/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+setup(
+    name="funsize",
+    version="0.59",
+    description="Funsize Scheduler",
+    author="Mozilla Release Engineering",
+    packages=["funsize"],
+    include_package_data=True,
+    # Not zip safe because we have data files in the package
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "funsize-scheduler = funsize.scheduler:main",
+        ],
+    },
+    install_requires=[
+        "amqp",
+        "anyjson",
+        "argparse",
+        "cffi",
+        # PGPy depends on this _specific_ version of cryptography
+        "cryptography==0.6",
+        "enum34",
+        "kombu",
+        "more_itertools",
+        "PGPy",
+        "pycparser",
+        "PyHawk-with-a-single-extra-commit",
+        "Jinja2",
+        "python-jose<=0.5.6",
+        "PyYAML",
+        "redo",
+        # Because taskcluster hard pins this version...
+        "requests==2.4.3",
+        "singledispatch",
+        "six",
+        "taskcluster>=0.0.26",
+        "wsgiref",
+    ],
+    tests_require=[
+        'hypothesis',
+        'pytest',
+        'mock',
+    ],
+)
