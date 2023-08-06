bba303e5f85396570a26b24b52b730379b1a0991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:34 +0000

    Add package feelinglucky/0.1.5/feelinglucky-0.1.5.tar.gz

---
+++ packages/feelinglucky/feelinglucky-0.1.5.tar.gz/feelinglucky-0.1.5/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'feelinglucky',
+    version = '0.1.5',
+    description = 'This package gives you the url of the first google search result of a query',
+    author = 'Dinesh Rai',
+    author_email = 'dinesh@techiemd.com',
+    url = 'https://github.com/DineshRai/feelinglucky', 
+    py_modules=['feelinglucky'],
+    install_requires=[
+        "requests",
+        "BeautifulSoup4"
+    ]
+)
