0a4a642ace45568c0b16026fea20a8332902d23d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:12 +0000

    Add package km3pipe/4.8.1/km3pipe-4.8.1.tar.gz

---
+++ packages/km3pipe/km3pipe-4.8.1.tar.gz/km3pipe-4.8.1/km3pipe/style.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+# Filename: style.py
+# pylint: disable=locally-disabled
+"""
+The KM3Pipe style definitions.
+
+"""
+from __future__ import division, absolute_import, print_function
+
+import matplotlib.pyplot as plt
+import km3pipe as kp
+
+__author__ = "Tamas Gal"
+__copyright__ = "Copyright 2016, Tamas Gal and the KM3NeT collaboration."
+__credits__ = []
+__license__ = "MIT"
+__maintainer__ = "Tamas Gal"
+__email__ = "tgal@km3net.de"
+__status__ = "Development"
+
+
+plt.style.use(kp.mplstyle)
