eae3b16962334e03afc4585c2a10d491cf53cde0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:51 +0000

    Add package niiu/0.8.2/niiu-0.8.2.tar.gz

---
+++ packages/niiu/niiu-0.8.2.tar.gz/niiu-0.8.2/niiu/nana.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+
+import subprocess
+
+def main():
+    subprocess.call(['./nana.sh'])
+
+if __name__ == "__main__":
+    main()
---
+++ packages/niiu/niiu-0.8.2.tar.gz/niiu-0.8.2/setup.py
@@ -0,0 +1,20 @@
+""" this script is design for dagobah"""
+# _*_ coding: utf-8 _*_
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+        name = "niiu",
+        packages = find_packages(),
+        version = "0.8.2",
+        classifiers = [
+            "Programming Language :: Python",
+            ],
+        entry_points = {
+            "console_scripts": [
+                "niiu = niiu.niiu:main",
+		"nana.sh = niiu.nana:main",
+                ],
+            }
+        )
