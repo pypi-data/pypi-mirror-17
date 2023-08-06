f36ca3569da8a63bed99d711de3efbbdbe1bb6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:38 +0000

    Add package putio.py/3.2.0/putio.py-3.2.0.tar.gz

---
+++ packages/putio.py/putio.py-3.2.0.tar.gz/putio.py-3.2.0/setup.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from setuptools import setup
+
+setup(
+    name='putio.py',
+    description='Python client for put.io API v2',
+    version='3.2.0',
+    author=u'Cenk Altı',
+    author_email='cenkalti@gmail.com',
+    url='http://github.com/cenk/putio.py',
+    py_modules=['putio'],
+    include_package_data=True,
+    zip_safe=True,
+    platforms='any',
+    install_requires=['requests', 'tus.py'],
+)
