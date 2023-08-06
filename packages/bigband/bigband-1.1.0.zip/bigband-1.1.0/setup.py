3dfba261b3400006ec88d7300d6d32471014d272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:01 +0000

    Add package bigband/1.1.0/bigband-1.1.0.zip

---
+++ packages/bigband/bigband-1.1.0.zip/bigband-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'bigband',
+        version     = '1.1.0',
+        py_modules  = ['bigband'],
+        author      = 'hlq',
+        author_email= 'hfpython@headfirstlabs.com',
+        url         = 'http://wwww.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+     )
