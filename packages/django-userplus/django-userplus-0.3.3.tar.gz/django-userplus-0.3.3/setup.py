12b413f6bf68e36f5e93ecc05d421717762367a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:47 +0000

    Add package django-userplus/0.3.3/django-userplus-0.3.3.tar.gz

---
+++ packages/django-userplus/django-userplus-0.3.3.tar.gz/django-userplus-0.3.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='django-userplus',
+      version='0.3.3',
+      description='Extended Auth User module for Django',
+      url='https://github.com/ifedapoolarewaju/django-userplus.git',
+      author='Ifedapo Olarewaju',
+      author_email='ifedapoolarewaju@gmail.com',
+      license='MIT',
+      packages=['userplus', 'userplus.lib', 'userplus.static'],
+      install_requires=['Django>=1.9', 'facebook-sdk>=2.0.0'],
+      include_package_data=True,
+      classifiers=[
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Framework :: Django'
+      ],
+      zip_safe=False)
