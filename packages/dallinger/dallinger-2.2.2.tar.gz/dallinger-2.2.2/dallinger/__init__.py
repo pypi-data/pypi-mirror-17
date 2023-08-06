f045d2e38be4d60b5c3dc14af9b98731365d4b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:26 +0000

    Add package dallinger/2.2.2/dallinger-2.2.2.tar.gz

---
+++ packages/dallinger/dallinger-2.2.2.tar.gz/dallinger-2.2.2/dallinger/__init__.py
@@ -0,0 +1,21 @@
+"""This is Dallinger, a platform for simulating evolution with people."""
+
+from . import (
+    models,
+    information,
+    nodes,
+    networks,
+    processes,
+    transformations,
+    experiments
+)
+
+__all__ = (
+    "models",
+    "information",
+    "nodes",
+    "networks",
+    "processes",
+    "transformations",
+    "experiments"
+)
