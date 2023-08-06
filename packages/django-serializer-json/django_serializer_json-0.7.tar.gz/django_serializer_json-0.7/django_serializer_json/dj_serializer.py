bba8ec07c57990e7f639b2463139bb1f87bfd9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:23 +0000

    Add package django-serializer-json/0.7/django_serializer_json-0.7.tar.gz

---
+++ packages/django-serializer-json/django_serializer_json-0.7.tar.gz/django_serializer_json-0.7/django_serializer_json/dj_serializer.py
@@ -0,0 +1,16 @@
+import json
+
+
+def dj_serializer(queryset, result_key='result'):
+    dict_results = {}
+    result_list = []
+    for instance in queryset:
+        loop_dict = {}
+        for field in instance._meta.local_fields:
+            field_name = field.name
+            attribute = getattr(instance, field_name)
+            loop_dict[field_name] = str(attribute)
+        result_list.append(loop_dict)
+    dict_results[result_key] = result_list
+    json_data = json.dumps(dict_results)
+    return json.loads(json_data)
---
+++ packages/django-serializer-json/django_serializer_json-0.7.tar.gz/django_serializer_json-0.7/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='django_serializer_json',
+      version='0.7',
+      description='Simple json serializer for Django FW',
+      author='Alex Gonchar',
+      packages=['django_serializer_json'],
+      zip_safe=False)
