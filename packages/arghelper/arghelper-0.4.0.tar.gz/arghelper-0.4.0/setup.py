621bf342559c968b532ecb0f3422939740fe1756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:42 +0000

    Add package arghelper/0.4.0/arghelper-0.4.0.tar.gz

---
+++ packages/arghelper/arghelper-0.4.0.tar.gz/arghelper-0.4.0/setup.py
@@ -0,0 +1,50 @@
+import codecs
+import os
+import re
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """Read parts of a file
+
+    Taken from pip's setup.py
+    intentionally *not* adding an encoding option to open
+    see: https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    """
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='arghelper',
+    version='0.4.0',
+    description='Python argparse helper module',
+    long_description=long_description,
+    author='Matthew Rankin',
+    author_email='matthew@questrail.com',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    url='https://github.com/questrail/arghelper',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    setup_requires=[
+        'flake8'
+    ]
+)
