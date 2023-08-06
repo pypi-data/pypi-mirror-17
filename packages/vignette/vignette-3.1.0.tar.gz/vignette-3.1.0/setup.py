9697227e3986bf71e53b1db82ead0cc48cee5706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:44 +0000

    Add package vignette/3.1.0/vignette-3.1.0.tar.gz

---
+++ packages/vignette/vignette-3.1.0.tar.gz/vignette-3.1.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# this project is licensed under the WTFPLv2, see COPYING.txt for details
+
+from setuptools import setup, find_packages
+
+import glob
+
+with open('VERSION.txt') as fd:
+	version = fd.read().strip()
+
+setup(
+	name='vignette',
+	version=version,
+
+	description='Library to create FreeDesktop-compatible thumbnails',
+	url='https://github.com/hydrargyrum/vignette',
+	author='Hg',
+	author_email='dev+pip@indigo.re',
+	license='WTFPLv2',
+	classifiers=[
+		'Development Status :: 4 - Beta',
+
+		'Intended Audience :: Developers',
+
+		'License :: Public Domain',
+
+		'Topic :: Desktop Environment',
+		'Topic :: Multimedia :: Graphics',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: Utilities',
+
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.2',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+	],
+	keywords='freedesktop xdg image thumbnail',
+
+	extras_require={
+		'PIL': ['PIL'],
+	},
+
+	zip_safe=True,
+	packages=find_packages(),
+	test_suite='test',
+	entry_points={
+		'console_scripts': [
+			'vignette=vignette:main'
+		]
+	}
+)
