79be10e6ae683f59dbbb0d5c865171bf79b2bd0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:57 +0000

    Add package my_python_files/1.1.1/my_python_files-1.1.1.zip

---
+++ packages/my_python_files/my_python_files-1.1.1.zip/my_python_files-1.1.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+name = 'my_python_files',
+version = '1.1.1',
+py_modules = ['nester'],
+author = 'newbie_coder',
+author_email = 'newbie_coder@gmail.com',
+url = '',
+description = 'A simple printer of nested lists',)
