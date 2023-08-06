237d04b24efe5e4545ba6ea51b7736c395297567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:35 +0000

    Add package testutils/0.2.0/testutils-0.2.0.tar.gz

---
+++ packages/testutils/testutils-0.2.0.tar.gz/testutils-0.2.0/testutils/reanimator/default_webdriver.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+default_wd_func = webdriver.Remote
+default_wd_args = []
+default_wd_kwargs = {
+    'command_executor': 'http://127.0.0.1:4444/wd/hub',
+    'desired_capabilities': DesiredCapabilities.CHROME
+}
+
