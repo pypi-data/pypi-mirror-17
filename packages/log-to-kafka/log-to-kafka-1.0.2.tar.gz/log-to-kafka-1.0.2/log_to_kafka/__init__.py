a3cdc942ab88c0bbe909eff7471e5a9a630674bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:35 +0000

    Add package log-to-kafka/1.0.2/log-to-kafka-1.0.2.tar.gz

---
+++ packages/log-to-kafka/log-to-kafka-1.0.2.tar.gz/log-to-kafka-1.0.2/log_to_kafka/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+VERSION = '1.0.2'
+
+AUTHOR = "cn"
+
+AUTHOR_EMAIL = "308299269@qq.com"
+
+HOME_PAGE = "https://www.github.com/ShichaoMa/log_to_kafka"
+
+from .logger import LogFactory, Logger, LogObject, KafkaHandler
