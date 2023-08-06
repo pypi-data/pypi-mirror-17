8e8cba0cea8627e6d32bfd268e179f2f37b066a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:33 +0000

    Add package csv2arff/1.6/csv2arff-1.6.tar.gz

---
+++ packages/csv2arff/csv2arff-1.6.tar.gz/csv2arff-1.6/setup.py
@@ -0,0 +1,24 @@
+#-*-coding:utf-8-*-
+
+import sys
+from setuptools import *
+
+setup(
+    name="csv2arff",
+    version="1.6",
+    author=u"Uğur Soğukpınar",
+    author_email="sogukpinar.ugur@gmail.com",
+    url="https://github.com/ugursogukpinar/csv2arff",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "csv2arff = csv2arff.csv2arff:main",
+        ],
+    },
+    license="LICENSE.txt",
+    description="Csv to Arff converter",
+    long_description=open("README.md").read(),
+    install_requires=list(filter(None, [
+        "numpy",
+    ])),
+)
