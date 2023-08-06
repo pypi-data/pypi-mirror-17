e783148af4411e479b94f4251709b67f90053e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:47 +0000

    Add package fallball/0.1.16/fallball-0.1.16.tar.gz

---
+++ packages/fallball/fallball-0.1.16.tar.gz/fallball-0.1.16/fallball/fallballapp/tests/tests_base.py
@@ -0,0 +1,48 @@
+import json
+
+from django.contrib.auth.models import User
+from django.test import TestCase
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+
+from fallballapp.models import Application
+
+
+def _get_client():
+    """
+    Returns request object with admin token
+    """
+    client = APIClient()
+    # Get admin token and set up credentials
+    admin = User.objects.filter(username='admin').first()
+    if not admin:
+        admin = User.objects.create_superuser('admin', 'admin@fallball.io', '1q2w3e')
+
+    token = Token.objects.create(user=admin)
+    client.credentials(HTTP_AUTHORIZATION='Token {token}'.format(token=token.key))
+    return client
+
+
+class BaseTestCase(TestCase):
+    """
+    Test basic operations: model objects create/delete
+    """
+
+    # Check that resellers, clients, objects can be created and deleted
+    @classmethod
+    def setUpTestData(cls):
+        cls.client_request = _get_client()
+
+    def test_object_creation(self):
+        # create_application
+        self.client_request.post('/v1/applications/',
+                                 json.dumps({'id': 'tricky_chicken'}),
+                                 content_type='application/json')
+
+        # create second application
+        self.client_request.post('/v1/applications/',
+                                 json.dumps({'id': 'tricky_chicken_2'}),
+                                 content_type='application/json')
+
+        self.assertTrue(Application.objects.filter(id='tricky_chicken'))
+        self.assertTrue(Application.objects.filter(id='tricky_chicken_2'))
