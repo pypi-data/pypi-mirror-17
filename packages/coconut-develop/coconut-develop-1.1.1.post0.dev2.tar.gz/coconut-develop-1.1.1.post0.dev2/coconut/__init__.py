aa9b96b03859122fd4d09a29d7f850b928a7b66d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:57 +0000

    Add package coconut-develop/1.1.1.post0.dev2/coconut-develop-1.1.1.post0.dev2.tar.gz

---
+++ packages/coconut-develop/coconut-develop-1.1.1.post0.dev2.tar.gz/coconut-develop-1.1.1.post0.dev2/coconut/__init__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#-----------------------------------------------------------------------------------------------------------------------
+# INFO:
+#-----------------------------------------------------------------------------------------------------------------------
+
+"""
+Author: Evan Hubinger
+License: Apache 2.0
+Description: The Coconut Programming Language.
+"""
+
+#-----------------------------------------------------------------------------------------------------------------------
+# IMPORTS:
+#-----------------------------------------------------------------------------------------------------------------------
+
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+from coconut.root import *  # NOQA
+
+#-----------------------------------------------------------------------------------------------------------------------
+# IPYTHON:
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+def load_ipython_extension(ipython):
+    """Loads Coconut as an IPython extension."""
+    from coconut.convenience import cmd, parse, CoconutException
+    from coconut.logging import logger
+
+    def magic(line, cell=None):
+        """Coconut IPython magic."""
+        try:
+            if cell is None:
+                code = line
+            else:
+                cmd(line)  # first line in block is cmd
+                code = cell
+            compiled = parse(code)
+        except CoconutException:
+            logger.print_exc()
+        else:
+            ipython.run_cell(compiled, shell_futures=False)
+    ipython.register_magic_function(magic, "line_cell", "coconut")
