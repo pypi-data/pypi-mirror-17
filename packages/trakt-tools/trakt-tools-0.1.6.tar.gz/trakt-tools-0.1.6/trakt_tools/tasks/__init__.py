5fa48320341c0572bb2f1f2797e69348f9a63889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:21 +0000

    Add package trakt-tools/0.1.6/trakt-tools-0.1.6.tar.gz

---
+++ packages/trakt-tools/trakt-tools-0.1.6.tar.gz/trakt-tools-0.1.6/trakt_tools/tasks/__init__.py
@@ -0,0 +1,9 @@
+from .history import MergeHistoryDuplicatesTask, ScanHistoryDuplicatesTask
+from .profile import CreateBackupTask
+
+
+__all__ = [
+    'MergeHistoryDuplicatesTask',
+    'ScanHistoryDuplicatesTask',
+    'CreateBackupTask'
+]
