46d0ec0d41d73116e9595e5193e4a551c1fdf6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:27 +0000

    Add package CI_LocalConnector/0.15/CI_LocalConnector-0.15.zip

---
+++ packages/CI_LocalConnector/CI_LocalConnector-0.15.zip/CI_LocalConnector-0.15/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='CI_LocalConnector',
+      version='0.15',
+      py_modules=['CI_LocalConnector' , 'CI_LC_BL'],
+	  description="IOT application that collect data from PLC (ModBus or AnB Ethernet/IP) and send to cloud using https",
+	  author="Ido Peles",
+	  author_email="idop@contel.co.il",
+      )
