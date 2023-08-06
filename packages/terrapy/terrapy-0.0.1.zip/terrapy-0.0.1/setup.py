0e1875b77d2d38d2a8368cf9f4eefbb9a11e7bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:24 +0000

    Add package terrapy/0.0.1/terrapy-0.0.1.zip

---
+++ packages/terrapy/terrapy-0.0.1.zip/terrapy-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='terrapy',
+      version='0.0.1',
+      description='Terra Python - for earth science',
+      url='http://github.com/jingkangy91/terrapy',
+      author='Jingkang Yang',
+      author_email='jingkangy91@gmail.com',
+      license='MIT',
+      packages=['terrapy'],
+      zip_safe=False)
