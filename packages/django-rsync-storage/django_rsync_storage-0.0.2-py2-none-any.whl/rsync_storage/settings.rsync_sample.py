196beeb394e05e4bf0f8f96e6074d228715f5765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:47 +0000

    Add package django-rsync-storage/0.0.2/django_rsync_storage-0.0.2-py2-none-any.whl

---
+++ packages/django-rsync-storage/django_rsync_storage-0.0.2-py2-none-any.whl/rsync_storage/settings.rsync_sample.py
@@ -0,0 +1,12 @@
+RSYNC_HOSTS = [
+    {'host': 'current', 'media_root': '/opt/media/', 'prefix': 'mc1_'},
+    {'host': '172.17.0.2', 'media_root': '/opt/media/', 'prefix': 'mc2_'}
+]
+
+
+
+RSYNC_HOSTS = [
+    {'host':'current',  'media_root':'/home/mis_mm/project_mis_mm/media/', 'prefix':'mc2_'},
+    {'host':'192.168.3.52',  'media_root':'/home/mis_mm/project_mis_mm/media/'},
+    {'host':'192.168.4.52',  'media_root':'/home/mis_mm/project_mis_mm/media/'},
+]
