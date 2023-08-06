7112c0dd97bd65cfd79a38cb5d64bb8bced950c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:26 +0000

    Add package pix-css/0.1.0/pix-css-0.1.0.tar.gz

---
+++ packages/pix-css/pix-css-0.1.0.tar.gz/pix-css-0.1.0/setup.py
@@ -0,0 +1,36 @@
+# @Author: BingWu Yang <detailyang>
+# @Date:   2016-09-20T20:27:36+08:00
+# @Email:  detailyang@gmail.com
+# @Last modified by:   detailyang
+# @Last modified time: 2016-09-20T15:23:48+08:00
+# @License: The MIT License (MIT)
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pix-css',
+    version='0.1.0',
+    keywords=('pixel css art'),
+    description='transform picture to pixel cs',
+    license='MIT License',
+    install_requires=[
+        'Jinja2==2.8',
+        'MarkupSafe==0.23',
+        'Pillow==3.3.1',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pix=pix:main',
+        ],
+    },
+    data_files=[
+        ('tpl', ['pix/tpl/dynamic.tpl', 'pix/tpl/static.tpl']),
+    ],
+    author='detailyang',
+    author_email='detailyang@gmail.com',
+
+    packages=find_packages(),
+    platforms='any',
+)
