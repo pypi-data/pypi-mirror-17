d3daeb740c3458c2570e1c98a6e1ae6eccc78d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:30 +0000

    Add package everypolitician-popolo/0.0.3/everypolitician-popolo-0.0.3.tar.gz

---
+++ packages/everypolitician-popolo/everypolitician-popolo-0.0.3.tar.gz/everypolitician-popolo-0.0.3/popolo_data/importer.py
@@ -0,0 +1,34 @@
+import json
+
+import requests
+
+from .base import (
+    MembershipCollection, PersonCollection, OrganizationCollection)
+
+
+class Popolo(object):
+
+    @classmethod
+    def from_filename(cls, filename):
+        with open(filename) as f:
+            return cls(json.load(f))
+
+    @classmethod
+    def from_url(cls, url):
+        r = requests.get(url)
+        return cls(r.json())
+
+    def __init__(self, json_data):
+        self.json_data = json_data
+
+    @property
+    def persons(self):
+        return PersonCollection(self.json_data.get('persons', []), self)
+
+    @property
+    def organizations(self):
+        return OrganizationCollection(self.json_data.get('organizations', []), self)
+
+    @property
+    def memberships(self):
+        return MembershipCollection(self.json_data.get('memberships', []), self)
