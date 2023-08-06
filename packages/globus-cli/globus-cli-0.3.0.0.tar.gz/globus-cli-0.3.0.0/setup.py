e89d20c068abb8649356bfdb91c040056b63992d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:00 +0000

    Add package globus-cli/0.3.0.0/globus-cli-0.3.0.0.tar.gz

---
+++ packages/globus-cli/globus-cli-0.3.0.0.tar.gz/globus-cli-0.3.0.0/setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup, find_packages
+
+# single source of truth for package version
+version_ns = {}
+with open(os.path.join("globus_cli", "version.py")) as f:
+    exec(f.read(), version_ns)
+version = version_ns['__version__']
+
+setup(
+    name="globus-cli",
+    version=version,
+    packages=find_packages(),
+    install_requires=['click', 'globus-sdk>=0.3.0', 'configobj'],
+
+    entry_points={
+        'console_scripts': ['globus = globus_cli:main']
+    },
+
+    # descriptive info, non-critical
+    description="Globus CLI",
+    long_description=open("README.rst").read(),
+    author="Stephen Rosen",
+    author_email="sirosen@globus.org",
+    url="https://github.com/globusonline/globus-cli",
+    keywords=["globus", "cli", "command line"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5"
+    ],
+)
