88efc9e6d6868f8072fbbdcfc87a4ef40b9c9eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:45 +0000

    Add package uwsgi-cloudwatch/0.0.1.1/uwsgi-cloudwatch-0.0.1.1.tar.gz

---
+++ packages/uwsgi-cloudwatch/uwsgi-cloudwatch-0.0.1.1.tar.gz/uwsgi-cloudwatch-0.0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='uwsgi-cloudwatch',
+    version='0.0.1.1',
+    packages=find_packages(exclude=('etc')),
+    include_package_data=True,
+    author='Justin Stewart',
+    author_email='jstewart@wdtinc.com',
+    description='uwsgi-cloudwatch',
+    url='https://github.com/wdtinc/uwsgi-cloudwatch',
+    install_requires=[
+        'click==6.6',
+        'requests==2.11.1',
+        'boto3==1.4.0',
+        'arrow==0.8.0'
+    ],
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+    ),
+    entry_points={
+        'console_scripts': [
+            'uwsgi-cloudwatch = uwsgi_cloudwatch.main:cli'
+        ]
+    }
+)
