a841ecf036bc126719b3fabc7b459cba25457a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:20 +0000

    Add package pc987_sulib/0.221/pc987_sulib-0.221.tar.gz

---
+++ packages/pc987_sulib/pc987_sulib-0.221.tar.gz/pc987_sulib-0.221/pc987_sulib/pc987_sulib.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# FILE : pc987_sulib.py
+
+def print_hello_world():
+    print('Hello world')
---
+++ packages/pc987_sulib/pc987_sulib-0.221.tar.gz/pc987_sulib-0.221/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pc987_sulib',
+      version='0.221',
+      description='The most useful Python module',
+      url='http://github.com/somewher/out/there',
+      author='Author Name',
+      author_email='authorname@email.com',
+      license='MIT',
+      packages=['pc987_sulib'],
+      zip_safe=False)
