73954184e8f93c212c6e6a1b471fc4ac3f61d5e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:45 +0000

    Add package alt-bucket/0.0.1/alt-bucket-0.0.1.tar.gz

---
+++ packages/alt-bucket/alt-bucket-0.0.1.tar.gz/alt-bucket-0.0.1/setup.py
@@ -0,0 +1,64 @@
+#!env/bin/python
+from setuptools import setup, find_packages
+
+license_type = 'MIT License'
+dev_status = '3 - Alpha'
+# dev_status = '4 - Beta'
+# dev_status = '5 - Production/Stable'
+
+setup(
+
+    # Author
+    author='Astafev Alexey',
+    author_email='efsneiron@gmail.com',
+
+    # Meta
+    name='alt-bucket',
+    version='0.0.1',
+    description='The common package',
+    url='https://github.com/altaid/bucket',
+    keywords=[
+        'python3',
+        'flask',
+        'orm',
+        'sqlalchemy',
+        'webapp'
+    ],
+
+    # classifiers
+    # see: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+
+        # Development status
+        'Development Status :: ' + dev_status,
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+
+        # License
+        'License :: OSI Approved :: ' + license_type,
+
+        # categories
+        'Programming Language :: Python :: 3',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+    ],
+
+    # Project packages
+    packages=find_packages(exclude=['tests']),
+
+    # project dependencies
+    install_requires=[
+    ],
+
+    # entry_points={
+    #     'console_scripts': [
+    #         'bucket=bucket:cli:bucket',
+    #     ],
+    # },
+
+    # License
+    license=license_type,
+)
