12af5d72ffbf8c41115fb17d67bf5d5219d91664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:00 +0000

    Add package qtgui/0.0.1/qtgui-0.0.1.tar.gz

---
+++ packages/qtgui/qtgui-0.0.1.tar.gz/qtgui-0.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from distutils.core import setup
+setup(
+        name='qtgui',
+        version='0.0.1',
+        author='Huang tao',
+        author_email='huangtao.jh@gmail.com',
+        platforms='any',
+        description='python3 pyqt wraper',
+        long_description='python3 pyqt wraper',
+        packages=['qtgui'],
+        license='GPL',
+        requires=['pyqt3_pyqt5']
+        )
