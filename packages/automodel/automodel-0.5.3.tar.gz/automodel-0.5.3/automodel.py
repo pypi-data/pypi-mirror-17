260581517ea5d16b7a9283c393b26b39529f34f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:29 +0000

    Add package automodel/0.5.3/automodel-0.5.3.tar.gz

---
+++ packages/automodel/automodel-0.5.3.tar.gz/automodel-0.5.3/automodel.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#Boa:App:BoaApp
+
+import wx
+
+import automodelclient.window.main_window as main_window
+
+modules ={u'main_window': [1, 'Main frame of Application', u'main_window.py'],
+ 'sequece_window': [0, '', u'sequece_window.py'],
+ u'alinhar_window': [0, '', u'alinhar_window.py'],
+ u'loop_window': [0, '', u'loop_window.py'],
+ u'modelar_window': [0, '', u'modelar_window.py']}
+
+class BoaApp(wx.App):
+    def OnInit(self):
+        self.main = main_window.create(None)
+        self.main.Show()
+        self.SetTopWindow(self.main)
+        return True
+
+def main():
+    application = BoaApp(0)
+    application.MainLoop()
+
+if __name__ == '__main__':
+    main()
