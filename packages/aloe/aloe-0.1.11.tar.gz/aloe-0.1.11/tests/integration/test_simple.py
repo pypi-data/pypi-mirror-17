6d8eeaad6b0e8a8a2a73c525f01d08d8f42e6917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:57 +0000

    Add package aloe/0.1.11/aloe-0.1.11.tar.gz

---
+++ packages/aloe/aloe-0.1.11.tar.gz/aloe-0.1.11/tests/integration/test_simple.py
@@ -0,0 +1,71 @@
+"""
+Test running Aloe in a separate process, as the real users would.
+"""
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+import os
+import subprocess
+import unittest
+
+from aloe.testing import in_directory
+
+
+ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+MAIN = os.path.join(ROOT_PATH, 'aloe', '__init__.py')
+
+
+@in_directory('tests/simple_app')
+class SimpleIntegrationTest(unittest.TestCase):
+    """
+    Test that basic feature running works as expected.
+    """
+
+    def test_success(self):
+        """
+        Test running a simple feature.
+        """
+
+        exitcode, _ = self.run_feature('features/calculator.feature')
+        self.assertEqual(exitcode, 0, "Feature run successfully.")
+
+    def test_success_zh(self):
+        """
+        Test running a simple feature with non-ASCII characters, with verbose
+        output.
+        """
+
+        exitcode, out = self.run_feature('features/calculator_zh.feature',
+                                         '--verbosity=3')
+        print(out)
+        self.assertEqual(exitcode, 0, "Feature run successfully.")
+
+    def test_failure(self):
+        """
+        Test running a failing feature.
+        """
+
+        exitcode, _ = self.run_feature('features/wrong_expectations.feature')
+        self.assertNotEqual(exitcode, 0, "Feature run failed.")
+
+    def run_feature(self, *args):
+        """
+        Run a feature and return the (exitcode, output) tuple.
+        """
+
+        # Ensure Aloe itself is on the path
+        env = os.environ.copy()
+        env['PYTHONPATH'] = ROOT_PATH
+
+        try:
+            output = subprocess.check_output(
+                (MAIN,) + args,
+                stderr=subprocess.STDOUT,
+                env=env,
+            )
+            return 0, output
+        except subprocess.CalledProcessError as ex:
+            return ex.returncode, ex.output
