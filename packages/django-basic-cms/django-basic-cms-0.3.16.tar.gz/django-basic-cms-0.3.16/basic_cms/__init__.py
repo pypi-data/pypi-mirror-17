ef358d3cecd9da6da4afa1bb507ff344d81bb852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:22 +0000

    Add package django-basic-cms/0.3.16/django-basic-cms-0.3.16.tar.gz

---
+++ packages/django-basic-cms/django-basic-cms-0.3.16.tar.gz/django-basic-cms-0.3.16/basic_cms/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Django Basic CMS module."""
+VERSION = (0, 3, 16)
+__version__ = '.'.join(map(str, VERSION))
+__author__ = "ArabellaTech"
+__contact__ = "geeks@arabel.la"
+__homepage__ = "https://github.com/ArabellaTech/django-basic-cms"
+__docformat__ = "restructuredtext"
+__doc__ = 'A tree based Django Basic CMS application'
+__license__ = 'BSD'
+
+default_app_config = 'basic_cms.app_config.BasicCmsConfig'
---
+++ packages/django-basic-cms/django-basic-cms-0.3.16.tar.gz/django-basic-cms-0.3.16/basic_cms/testproj/documents/views.py
@@ -0,0 +1,17 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from basic_cms.testproj.documents.models import Document
+
+
+def document_view(request, *args, **kwargs):
+    context = kwargs
+    if kwargs.get('current_page', False):
+        documents = Document.objects.filter(page=kwargs['current_page'])
+        context['documents'] = documents
+    if 'document_id' in kwargs:
+        document = Document.objects.get(pk=int(kwargs['document_id']))
+        context['document'] = document
+    context['in_document_view'] = True
+    rcontext = RequestContext(request, context)
+
+    return render_to_response('pages/examples/index.html', rcontext)
