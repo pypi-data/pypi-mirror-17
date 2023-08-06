54455270cda7dddd7785de7d628c5216cc9e6310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:54 +0000

    Add package nester_sym/1.0.0/nester_sym-1.0.0.zip

---
+++ packages/nester_sym/nester_sym-1.0.0.zip/nester_sym-1.0.0/nester_sym.py
@@ -0,0 +1,11 @@
+# coding = utf-8
+"""
+这是"nester.py"模块，提供了一个名为print_lol()的函数，这个函数的作用是遍历list,其中
+可能包含嵌套列表
+"""
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item, list):#如果有嵌套list，则调用自己
+            print_lol(each_item)
+        else:
+            print(each_item)
