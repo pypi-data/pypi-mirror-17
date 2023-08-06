8bd912b1cc4b667bbe387a9905e4842ade7c3161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:53 +0000

    Add package ftw.copymovepatches/1.0.0/ftw.copymovepatches-1.0.0.tar.gz

---
+++ packages/ftw.copymovepatches/ftw.copymovepatches-1.0.0.tar.gz/ftw.copymovepatches-1.0.0/ftw/copymovepatches/testing.py
@@ -0,0 +1,36 @@
+from ftw.builder.testing import BUILDER_LAYER
+from ftw.builder.testing import functional_session_factory
+from ftw.builder.testing import set_builder_session_factory
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.testing import z2
+from zope.configuration import xmlconfig
+import ftw.copymovepatches.tests.builders
+
+
+class FtwLayer(PloneSandboxLayer):
+    defaultBases = (PLONE_FIXTURE, BUILDER_LAYER)
+
+    def setUpZope(self, app, configurationContext):
+        xmlconfig.string(
+            '<configure xmlns="http://namespaces.zope.org/zope">'
+            '  <include package="z3c.autoinclude" file="meta.zcml" />'
+            '  <includePlugins package="plone" />'
+            '  <includePluginsOverrides package="plone" />'
+            '  <include package="ftw.copymovepatches.tests" file="profiles.zcml" />'
+            '</configure>',
+            context=configurationContext)
+
+        z2.installProduct(app, 'collective.indexing')
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'ftw.copymovepatches.tests:default')
+
+
+FTW_FIXTURE = FtwLayer()
+FTW_FUNCTIONAL = FunctionalTesting(
+    bases=(FTW_FIXTURE,
+           set_builder_session_factory(functional_session_factory)),
+    name="ftw.copymovepatches:functional")
