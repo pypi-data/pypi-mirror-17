62bb7e5a050109fc70fd82eef545931ade741f29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:18 +0000

    Add package host_profiler/0.7/host_profiler-0.7.tar.gz

---
+++ packages/host_profiler/host_profiler-0.7.tar.gz/host_profiler-0.7/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8  -*-
+
+from setuptools import setup
+
+setup(name='host_profiler',
+      version='0.7',
+      description='get host informations',
+      url='http://github.com/dowan/host_profiler',
+      author='Dowan',
+      author_email='contact@dowan.beer',
+      license='MIT',
+      scripts=['scripts/runbench'],
+      packages=['host_profiler'],
+      keywords = ['profiler', 'bench', 'psutil'],
+      install_requires=['pandas', 'psutil'])
