8875fde41bca9f06772e3c86c60b719f03cc1619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:07 +0000

    Add package CBM_utils/1.0.0/CBM_utils-1.0.0.zip

---
+++ packages/CBM_utils/CBM_utils-1.0.0.zip/CBM_utils-1.0.0/setup.py
@@ -0,0 +1,17 @@
+'''
+    Created by Tracy on 2016.9.30
+    Mail tracyliubai@gmail.com
+    Site http://liu-bai.com
+'''
+from distutils.core import setup
+
+setup(
+    name='CBM_utils',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Tracy',
+    author_email='tracyliubai@gmail.com',
+    description=''
+)
