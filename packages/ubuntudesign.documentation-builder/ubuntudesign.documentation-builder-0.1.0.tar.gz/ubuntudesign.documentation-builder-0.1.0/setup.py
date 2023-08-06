2cd7f9fb11743ea22c57fbcbadd0c49afce93c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:51 +0000

    Add package ubuntudesign.documentation-builder/0.1.0/ubuntudesign.documentation-builder-0.1.0.tar.gz

---
+++ packages/ubuntudesign.documentation-builder/ubuntudesign.documentation-builder-0.1.0.tar.gz/ubuntudesign.documentation-builder-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='ubuntudesign.documentation-builder',
+    version='0.1.0',
+    author='Canonical webteam',
+    author_email='robin+pypi@canonical.com',
+    url='https://github.com/ubuntudesign/documentation-builder',
+    packages=[
+        'ubuntudesign.documentation_builder',
+    ],
+    description=(
+        'A command-line tool for building documentation from repositories '
+        'into HTML files. Initially based on '
+        'https://github.com/juju/docs/blob/master/tools/mdbuild.py.'
+    ),
+    scripts=['bin/documentation-builder'],
+    # long_description=open('README.md').read(),
+    install_requires=[
+        "Markdown>=2.6.6",
+        "GitPython>=2.0.8",
+        "mdx-anchors-away>=1.0.1",
+        "mdx-callouts>=1.0.0",
+        "mdx-foldouts>=1.0.0"
+    ],
+)
+
