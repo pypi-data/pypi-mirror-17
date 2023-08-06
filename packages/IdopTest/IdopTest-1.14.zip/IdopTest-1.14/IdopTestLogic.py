236e38157d84e33ab3255907d2780a6283799946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:34 +0000

    Add package IdopTest/1.14/IdopTest-1.14.zip

---
+++ packages/IdopTest/IdopTest-1.14.zip/IdopTest-1.14/IdopTestLogic.py
@@ -0,0 +1,27 @@
+import logging , sys , time, datetime , os , threading
+
+ver = "1.14"
+print "IdopTestLogic Imported " + ver
+
+def getVersion():
+    print "IdopTest get Version " + ver
+
+def initLog():
+    logging.basicConfig(filename='IdoTest.log',level=logging.INFO , format='%(asctime)s %(message)s')    
+    logging.info('===============================')
+    logging.info('IdoTest Init')
+
+def ci_print(msg , level = ''):
+    try:
+        msg = ver + ' '  + msg
+        if level=='info':
+            logging.info(msg)
+        else:
+            logging.warning(msg)
+            
+        print msg
+    except Exception as inst:
+        logging.warning('Main Exception :: ' + inst)
+
+
+
