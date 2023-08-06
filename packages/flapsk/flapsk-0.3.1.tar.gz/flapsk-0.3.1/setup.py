902503def09c068238f550f73fc7971b4b1676c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:53 +0000

    Add package flapsk/0.3.1/flapsk-0.3.1.tar.gz

---
+++ packages/flapsk/flapsk-0.3.1.tar.gz/flapsk-0.3.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='flapsk',
+      version='0.3.1',
+      description='The funniest joke in the world',
+      license='MIT',
+      packages=['flapsk'],
+      zip_safe=False)
