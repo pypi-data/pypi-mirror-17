05d726510e053d6e0f36d756f4b8cbc1909fa80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:40 +0000

    Add package g3py/0.1/g3py-0.1.tar.gz

---
+++ packages/g3py/g3py-0.1.tar.gz/g3py-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='g3py',
+      version='0.1',
+      description='Generalized Graphical Gaussian Processes',
+      #url='http://github.com/storborg/funniest',
+      author='Gonzalo Rios',
+      author_email='grios@dim.uchile.cl',
+      license='MIT',
+      packages=['g3py'],
+      zip_safe=False)
