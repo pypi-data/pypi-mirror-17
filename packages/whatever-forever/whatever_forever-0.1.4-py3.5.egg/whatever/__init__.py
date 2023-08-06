97f3b086e9ce287ff2889f0c874ff584141cc6a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:17 +0000

    Add package whatever-forever/0.1.4/whatever_forever-0.1.4-py3.5.egg

---
+++ packages/whatever-forever/whatever_forever-0.1.4-py3.5.egg/whatever/__init__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+# In[1]:
+
+from whatever.chain import Chain, chain, _x, __x, __p, _this
+from whatever import callables
+
+__version__ = "0.1.3"
+
+__all__ = [
+    '_x', '__x', '__p', 'chain', '_this', 'callables',
+]
+
+
+# In[ ]:
