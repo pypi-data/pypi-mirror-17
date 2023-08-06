7966311008c4bdd2dcde4bc38302611c5626ce21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:14 +0000

    Add package apize/0.2.3/apize-0.2.3.tar.gz

---
+++ packages/apize/apize-0.2.3.tar.gz/apize-0.2.3/apize/apize.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from __future__ import absolute_import
+
+from apize.exceptions import BadReturnVarType
+from apize.http_request import send_request
+
+
+class Apize():
+	
+	def __init__(self, api_url, headers={}, verify_cert=False):
+		self.api_url = api_url
+		self.headers = headers
+		self.verify_cert = verify_cert
+
+	def call(self, path, method='GET'):
+		def decorator(func):
+			def wrapper(*args, **kwargs):
+				elem = func(*args, **kwargs)
+
+				if type(elem) is not dict:
+					raise BadReturnVarType(func.__name__)
+
+				absolute_url = self.api_url + path
+
+				## Merge global headers and custom headers.
+				fin_headers = self.headers.copy()
+				fin_headers.update(elem.get('headers', {}))
+				
+				response = send_request(absolute_url, method, 
+					elem.get('data', {}),
+					elem.get('args', {}),
+					elem.get('params', {}),
+					fin_headers,
+					elem.get('cookies', {}),
+					elem.get('timeout', 8),
+					elem.get('is_json', False),
+					elem.get('verify_cert', self.verify_cert)
+				)
+
+				return response
+			return wrapper
+
+		return decorator
---
+++ packages/apize/apize-0.2.3.tar.gz/apize-0.2.3/apize/http_request.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+from __future__ import absolute_import
+
+import json
+import requests
+from http.cookiejar import CookieJar
+
+
+def send_request(url, method, data, 
+	args, params, headers, cookies, timeout, is_json, verify_cert):
+	"""
+	Forge and send HTTP request.
+	"""
+	## Parse url args
+	for p in args:
+		url = url.replace(':' + p, str(args[p]))
+
+	try:
+		if data:
+			if is_json:
+				headers['Content-Type'] = 'application/json'
+				data = json.dumps(data)
+
+			request = requests.Request(
+				method.upper(), url, data=data, params=params, 
+				headers=headers, cookies=cookies
+			)
+		else:
+			request = requests.Request(
+				method.upper(), url, params=params, headers=headers, 
+				cookies=cookies
+			)
+
+		## Prepare and send HTTP request.
+		session = requests.Session()
+		session.verify = verify_cert
+		r = session.send(request.prepare(), timeout=timeout)
+
+	except requests.exceptions.Timeout:
+		return {
+			'data': {}, 
+			'cookies': CookieJar(),
+			'content_type': '', 
+			'status': 0, 
+			'is_json': False,
+			'timeout': True
+		}
+
+	try:
+		content_type = r.headers.get('Content-Type', 'application/json')
+		response = r.json()
+		isjson = True
+		
+	except json.decoder.JSONDecodeError:
+		content_type = r.headers.get('Content-Type', 'text/html')
+		response = r.text
+		isjson = False
+
+	return {
+		'data': response,
+		'cookies': r.cookies,
+		'content_type': content_type, 
+		'status': r.status_code,
+		'is_json': isjson,
+		'timeout': False
+	}
