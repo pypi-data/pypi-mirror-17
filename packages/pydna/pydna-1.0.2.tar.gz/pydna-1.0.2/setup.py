4878abc799c7d91a74afb556c595e92c0ae8f9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:51 +0000

    Add package pydna/1.0.2/pydna-1.0.2.tar.gz

---
+++ packages/pydna/pydna-1.0.2.tar.gz/pydna-1.0.2/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import versioneer
+#versioneer.VCS = 'git'
+#versioneer.versionfile_source = 'pydna/_version.py'
+#versioneer.versionfile_build = 'pydna/_version.py'
+#versioneer.tag_prefix = '' # tags are like 1.2.0
+#versioneer.parentdir_prefix = '' # dirname like 'myproject-1.2.0'
+
+# Read author etc..
+for line in open('pydna/__init__.py'):
+    if line.startswith('__') and not line.startswith('__version') and not line.startswith('__long'):
+        exec(line.strip())
+
+from setuptools import setup
+
+setup(  name            = 'pydna',
+        version=versioneer.get_version()[:5],
+        cmdclass=versioneer.get_cmdclass(),
+        author          = __author__,
+        author_email    = __email__,
+        packages=['pydna',
+                  'pydna.py_rstr_max',],
+        url='http://pypi.python.org/pypi/pydna/',
+        license='LICENSE.txt',
+        description='''Contains classes and code for representing double
+                     stranded DNA and functions for simulating homologous
+                     recombination between DNA molecules.''',
+        long_description=open('README.rst').read(),
+        install_requires =[
+        "biopython",
+        "networkx",
+        "appdirs",
+        "prettytable"],
+        zip_safe = False,
+        keywords = "bioinformatics",
+        classifiers = ['Development Status :: 4 - Beta',
+                       'Environment :: Console',
+                       'Intended Audience :: Education',
+                       'Intended Audience :: Developers',
+                       'Intended Audience :: Science/Research',
+                       'License :: OSI Approved :: BSD License',
+                       'Programming Language :: Python :: 2.7',
+                       'Topic :: Education',
+                       'Topic :: Scientific/Engineering :: Bio-Informatics',])
