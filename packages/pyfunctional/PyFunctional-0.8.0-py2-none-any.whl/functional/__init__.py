bed85505a55deaa2606a222ea6e66fcfa8bdaf4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:48 +0000

    Add package pyfunctional/0.8.0/PyFunctional-0.8.0-py2-none-any.whl

---
+++ packages/pyfunctional/PyFunctional-0.8.0-py2-none-any.whl/functional/__init__.py
@@ -0,0 +1,15 @@
+"""
+Package for creating data pipelines, LINQ-style data analysis, and functional programming. Imports
+the primary entrypoint at streams.seq
+"""
+from __future__ import absolute_import
+
+from functional.streams import seq, pseq
+
+__author__ = "Pedro Rodriguez"
+__copyright__ = "Copyright 2015, Pedro Rodriguez"
+__license__ = "MIT"
+__version__ = "0.5.0"
+__maintainer__ = "Pedro Rodriguez"
+__email__ = "ski.rodriguez@gmail.com"
+__status__ = "Development"
