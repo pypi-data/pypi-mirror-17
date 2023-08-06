16b26fb7c9a4d02433298d0fb23df48a37280af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:42 +0000

    Add package niiu/0.4.2/niiu-0.4.2.tar.gz

---
+++ packages/niiu/niiu-0.4.2.tar.gz/niiu-0.4.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+VERSION = '0.4.2'
+
+setup(
+	name = "niiu",
+	packages = find_packages(),
+	version = VERSION,
+	entry_points = {
+		"console_scripts": [
+			"niiu = niiu.niiu:main",
+		],
+	}
+)
