3b288a1c03c377c62977f7c759b569fcd2ba1d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:00 +0000

    Add package PyLog2html/1.0.4/PyLog2html-1.0.4.tar.gz

---
+++ packages/PyLog2html/PyLog2html-1.0.4.tar.gz/PyLog2html-1.0.4/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from PyLog2html import *
+logger=PyLogger(html_filename='test.html',console_log=True)
+
+if __name__=="__main__":
+	for i in range(6):
+		logger.debug('This is debug')
+		logger.info('This is info')
+		logger.warning("This is <hl>warning</hl>")
+		logger.error('This is <hl>error</hl> xxx')
+		logger.info('_____________')
+        logger.table('Add html table:<table><tr><th>...</th></tr></table>')
