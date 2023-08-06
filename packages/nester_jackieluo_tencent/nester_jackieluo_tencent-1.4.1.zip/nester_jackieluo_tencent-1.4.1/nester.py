c81e8cffb5695417fb22dcfee17ba5c6beec4ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:17 +0000

    Add package nester_jackieluo_tencent/1.4.1/nester_jackieluo_tencent-1.4.1.zip

---
+++ packages/nester_jackieluo_tencent/nester_jackieluo_tencent-1.4.1.zip/nester_jackieluo_tencent-1.4.1/nester.py
@@ -0,0 +1,11 @@
+import sys
+def print_lol(the_list,indent=False,level=0,dist=sys.stdout):
+	for item in the_list:
+		if isinstance(item,list):
+			print_lol(item,indent,level+1,dist)
+		else:
+			if indent==True:
+				for tab_stop in range(level):
+					print('\t',end='',file=dist)
+			print(item,file=dist)
+
