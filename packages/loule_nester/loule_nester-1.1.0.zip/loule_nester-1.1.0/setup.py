bb528e191bfb60abf48f5ce80e66299e464021e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:35 +0000

    Add package loule_nester/1.1.0/loule_nester-1.1.0.zip

---
+++ packages/loule_nester/loule_nester-1.1.0.zip/loule_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name			= 'loule_nester',
+		version			= '1.1.0',
+		py_modules		= ['loule_nester'],
+		author			= 'amandoloule',
+		author_email	= 'amandoloule@loulesoft.com.br',
+		url				= 'http://www.loulesoft.com.br',
+		description		= 'A simple printer of nested lists (a Head First Python exercise)'
+)
