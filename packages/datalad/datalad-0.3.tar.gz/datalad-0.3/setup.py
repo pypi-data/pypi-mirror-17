fd5af780b641f8c6efa905663d718ad3ebc337df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:37 +0000

    Add package datalad/0.3/datalad-0.3.tar.gz

---
+++ packages/datalad/datalad-0.3.tar.gz/datalad-0.3/setup.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the DataLad package for the
+#   copyright and license terms.
+#
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+
+import platform
+
+from glob import glob
+from os.path import sep as pathsep
+from os.path import join as opj
+from os.path import splitext
+
+from setuptools import findall
+from setuptools import setup, find_packages
+
+# manpage build imports
+from distutils.command.build_py import build_py
+from setup_support import BuildManPage, BuildRSTExamplesFromScripts
+from setup_support import get_version
+
+
+def findsome(subdir, extensions):
+    """Find files under subdir having specified extensions
+
+    Leading directory (datalad) gets stripped
+    """
+    return [
+        f.split(pathsep, 1)[1] for f in findall(opj('datalad', subdir))
+        if splitext(f)[-1].lstrip('.') in extensions
+    ]
+
+# datalad version to be installed
+version = get_version()
+
+# Only recentish versions of find_packages support include
+# datalad_pkgs = find_packages('.', include=['datalad*'])
+# so we will filter manually for maximal compatibility
+datalad_pkgs = [pkg for pkg in find_packages('.') if pkg.startswith('datalad')]
+
+# keyring is a tricky one since it got split into two as of 8.0 and on older
+# systems there is a problem installing via pip (e.g. on wheezy) so for those we
+# would just ask for keyring
+keyring_requires = ['keyring>=8.0', 'keyrings.alt']
+pbar_requires = ['tqdm']
+
+dist = platform.dist()
+# on oldstable Debian let's ask for lower versions and progressbar instead
+if dist[0] == 'gentoo':
+    pbar_requires = ['progressbar']
+if dist[0] == 'debian' and dist[1].split('.', 1)[0] == '7':
+    keyring_requires = ['keyring<8.0']
+    pbar_requires = ['progressbar']
+
+requires = {
+    'core': [
+        'appdirs',
+        'GitPython>=2.0.8',
+        'iso8601',
+        'humanize',
+        'mock',  # mock is also used for auto.py, not only for testing
+        'patool>=1.7',
+        'six>=1.8.0',
+    ] + pbar_requires,
+    'downloaders': [
+        'boto',
+        'msgpack-python',
+        'requests>=1.2',
+    ] + keyring_requires,
+    'downloaders-extra': [
+        'requests_ftp',
+    ],
+    'crawl': [
+        'scrapy>=1.1.0rc3',  # versioning is primarily for python3 support
+    ],
+    'publish': [
+        'jsmin',
+    ],
+    'tests': [
+        'BeautifulSoup4',  # VERY weak requirement, still used in one of the tests
+        'httpretty>=0.8.14',
+        'mock',
+        'nose>=1.3.4',
+        'vcrpy',
+    ],
+    'metadata': [
+        'simplejson',
+        'pyld',
+        'PyYAML',  # very optional
+    ]
+}
+requires['full'] = sum(list(requires.values()), [])
+
+
+# configure additional command for custom build steps
+class DataladBuild(build_py):
+    def run(self):
+        self.run_command('build_manpage')
+        self.run_command('build_examples')
+        build_py.run(self)
+
+cmdclass = {
+    'build_manpage': BuildManPage,
+    'build_examples': BuildRSTExamplesFromScripts,
+    'build_py': DataladBuild
+}
+
+setup(
+    name="datalad",
+    author="The DataLad Team and Contributors",
+    author_email="team@datalad.org",
+    version=version,
+    description="data distribution geared toward scientific datasets",
+    packages=datalad_pkgs,
+    install_requires=requires['core'] + requires['downloaders'] + requires['publish'],
+    extras_require=requires,
+    entry_points={
+        'console_scripts': [
+            'datalad=datalad.cmdline.main:main',
+            'git-annex-remote-datalad-archives=datalad.customremotes.archives:main',
+            'git-annex-remote-datalad=datalad.customremotes.datalad:main',
+        ],
+    },
+    cmdclass=cmdclass,
+    package_data={
+        'datalad':
+            findsome('resources', {'sh', 'html', 'js', 'css', 'png', 'svg'}) +
+            findsome('downloaders/configs', {'cfg'})
+    }
+)
