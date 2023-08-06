75f078ffb9e788be1ab4320cda9dfa8279baa713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:56 +0000

    Add package list_function_zhou/1.0.0/list_function_zhou-1.0.0.zip

---
+++ packages/list_function_zhou/list_function_zhou-1.0.0.zip/list_function_zhou-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        ='list_function_zhou',
+        version     ='1.0.0',
+        py_modules  =['list_function_zhou'],
+        author      ='zhoushirong',
+        author_email='1769138434@qq.com',
+        url         ='http://www.csdn.net',
+        description ='my first function',
+    )
