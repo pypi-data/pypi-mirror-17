84225d0ef1aedf1732dee915d2fee219bd5cad95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:30 +0000

    Add package SyntheSys/0.1/SyntheSys-0.1-py3-none-any.whl

---
+++ packages/SyntheSys/SyntheSys-0.1-py3-none-any.whl/SyntheSys/Library/SW/Processing/__init__.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python3
+
+import os, sys
+
+__all__=["Finance", "ImageProcessing", "Standard"]
+
+#----------------------------------------------
+from SyntheSys.Library.SW.Processing.TestNode import __BASICBLOCK__TestNode
+TestNode = __BASICBLOCK__TestNode()
+
+
+
+
+
+
+
+
+
+
