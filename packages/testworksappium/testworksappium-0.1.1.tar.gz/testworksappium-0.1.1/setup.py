3d1a6eefe19f8c305e72aca14317262331aa7028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:03 +0000

    Add package testworksappium/0.1.1/testworksappium-0.1.1.tar.gz

---
+++ packages/testworksappium/testworksappium-0.1.1.tar.gz/testworksappium-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+__version__ = '0.1.1'
+__url__ = 'https://github.com/apallin/testworks-appium/tarball/{}'.format(
+    __version__)
+
+setup(
+    name='testworksappium',
+    version=__version__,
+    description="""
+                A repository for Testworks Conf's Mobile Automation workshop
+                presented by Adam Pallin
+                """,
+    author='Adam Pallin',
+    author_email='adamrpallin@gmail.com',
+    url='https://github.com/apallin/testworks-appium',
+    packages=['testworksappium'],
+    keywords=['appium', 'testworks'],
+    download_url=__url__,
+    install_requires=[
+        'Appium-Python-Client >= 0.22, < 1.0',
+        'pytest >= 3.0.0, < 3.1.0',
+    ],
+)
