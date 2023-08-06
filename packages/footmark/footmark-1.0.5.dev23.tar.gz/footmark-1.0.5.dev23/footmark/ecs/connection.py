b11049bb497f8e83e8a9b460fd806caae980319e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:45 +0000

    Add package footmark/1.0.5.dev23/footmark-1.0.5.dev23.tar.gz

---
+++ packages/footmark/footmark-1.0.5.dev23.tar.gz/footmark-1.0.5.dev23/footmark/ecs/connection.py
@@ -0,0 +1,267 @@
+# encoding: utf-8
+"""
+Represents a connection to the ECS service.
+"""
+
+import warnings
+
+import six
+
+from footmark.connection import ACSQueryConnection
+from footmark.ecs.instance import Instance
+from footmark.ecs.volume import Disk
+from footmark.ecs.securitygroup import SecurityGroup
+from footmark.ecs.regioninfo import RegionInfo
+from footmark.exception import ECSResponseError
+
+
+class ECSConnection(ACSQueryConnection):
+    # SDKVersion = footmark.config.get('Footmark', 'ecs_version', '2014-05-26')
+    SDKVersion = '2014-05-26'
+    DefaultRegionId = 'cn-hangzhou'
+    DefaultRegionName = u'杭州'.encode("UTF-8")
+    ResponseError = ECSResponseError
+
+    def __init__(self, acs_access_key_id=None, acs_secret_access_key=None,
+                 region=None, sdk_version=None, security_token=None, ):
+        """
+        Init method to create a new connection to ECS.
+        """
+        if not region:
+            region = RegionInfo(self, self.DefaultRegionName,
+                                self.DefaultRegionId)
+        self.region = region
+        if sdk_version:
+            self.SDKVersion = sdk_version
+
+        self.ECSSDK = 'aliyunsdkecs.request.v' + self.SDKVersion.replace('-', '')
+
+        super(ECSConnection, self).__init__(acs_access_key_id,
+                                            acs_secret_access_key,
+                                            self.region, self.ECSSDK, security_token)
+
+    def build_filter_params(self, params, filters):
+        if not isinstance(filters, dict):
+            return
+
+        flag = 1
+        for key, value in filters.items():
+            acs_key = key
+            if acs_key.startswith('tag:'):
+                while (('set_Tag%dKey' % flag) in params):
+                    flag += 1
+                if flag < 6:
+                    params['set_Tag%dKey' % flag] = acs_key[4:]
+                    params['set_Tag%dValue' % flag] = filters[acs_key]
+                flag += 1
+                continue
+            if key == 'group_id':
+                if not value.startswith('sg-') or len(value) != 12:
+                    warnings.warn(
+                        "The group-id filter now requires a security group "
+                        "identifier (sg-*) instead of a security group ID. "
+                        "The group-id " + value + "may be invalid.",
+                        UserWarning)
+                params['set_SecurityGroupId'] = value
+                continue
+            if not isinstance(value, dict):
+                acs_key = ''.join(s.capitalize() for s in acs_key.split('_'))
+                params['set_' + acs_key] = value
+                continue
+
+            self.build_filters_params(params, value)
+
+    # Instance methods
+
+    def get_all_instances(self, instance_ids=None, filters=None, max_results=None):
+        """
+        Retrieve all the instance associated with your account.
+
+        :rtype: list
+        :return: A list of  :class:`footmark.ecs.instance`
+
+        """
+        warnings.warn(('The current get_all_instances implementation will be '
+                       'replaced with get_all_instances.'),
+                      PendingDeprecationWarning)
+
+        params = {}
+        if instance_ids:
+            self.build_list_params(params, instance_ids, 'InstanceIds')
+        if filters:
+            self.build_filter_params(params, filters)
+        if max_results is not None:
+            params['MaxResults'] = max_results
+        instances = self.get_list('DescribeInstances', params, ['Instances', Instance])
+        for inst in instances:
+            filters = {}
+            filters['instance_id'] = inst.id
+            volumes = self.get_all_volumes(filters=filters)
+            block_device_mapping = {}
+            for vol in volumes:
+                block_device_mapping[vol.id] = vol
+            setattr(inst, 'block_device_mapping', block_device_mapping)
+            filters = {}
+            filters['security_group_id'] = inst.security_group_id
+            security_groups = self.get_all_security_groups(filters=filters)
+            setattr(inst, 'security_groups', security_groups)
+
+        return instances
+
+    def start_instances(self, instance_ids=None):
+        """
+        Start the instances specified
+
+        :type instance_ids: list
+        :param instance_ids: A list of strings of the Instance IDs to start
+
+        :rtype: list
+        :return: A list of the instances started
+        """
+        params = {}
+        results = []
+        if instance_ids:
+            if isinstance(instance_ids, six.string_types):
+                instance_ids = [instance_ids]
+            for instance_id in instance_ids:
+                self.build_list_params(params, instance_id, 'InstanceId')
+                if self.get_status('StartInstance', params):
+                    results.append(instance_id)
+        return results
+
+    def stop_instances(self, instance_ids=None, force=False):
+        """
+        Stop the instances specified
+
+        :type instance_ids: list
+        :param instance_ids: A list of strings of the Instance IDs to stop
+
+        :type force: bool
+        :param force: Forces the instance to stop
+
+        :rtype: list
+        :return: A list of the instances stopped
+        """
+        params = {}
+        results = []
+        if force:
+            self.build_list_params(params, 'true', 'ForceStop')
+        if instance_ids:
+            if isinstance(instance_ids, six.string_types):
+                instance_ids = [instance_ids]
+            for instance_id in instance_ids:
+                self.build_list_params(params, instance_id, 'InstanceId')
+                if self.get_status('StopInstance', params):
+                    results.append(instance_id)
+        return results
+
+    def reboot_instances(self, instance_ids=None, force=False):
+        """
+        Reboot the specified instances.
+
+        :type instance_ids: list
+        :param instance_ids: The instances to terminate and reboot
+
+        :type force: bool
+        :param force: Forces the instance to stop
+
+        """
+        params = {}
+        results = []
+        if force:
+            self.build_list_params(params, 'true', 'ForceStop')
+        if instance_ids:
+            if isinstance(instance_ids, six.string_types):
+                instance_ids = [instance_ids]
+            for instance_id in instance_ids:
+                self.build_list_params(params, instance_id, 'InstanceId')
+                if self.get_status('RebootInstance', params):
+                    results.append(instance_id)
+
+        return results
+
+    def terminate_instances(self, instance_ids=None, force=False):
+        """
+        Terminate the instances specified
+
+        :type instance_ids: list
+        :param instance_ids: A list of strings of the Instance IDs to terminate
+
+        :type force: bool
+        :param force: Forces the instance to stop
+
+        :rtype: list
+        :return: A list of the instance_ids terminated
+        """
+        params = {}
+        result = []
+        if force:
+            self.build_list_params(params, 'true', 'Force')
+        if instance_ids:
+            if isinstance(instance_ids, six.string_types):
+                instance_ids = [instance_ids]
+            for instance_id in instance_ids:
+                self.build_list_params(params, instance_id, 'InstanceId')
+                if self.get_status('DeleteInstance', params):
+                    result.append(instance_id)
+        return result
+
+    def get_all_volumes(self, volume_ids=None, filters=None):
+        """
+        Get all Volumes associated with the current credentials.
+
+        :type volume_ids: list
+        :param volume_ids: Optional list of volume ids.  If this list
+                           is present, only the volumes associated with
+                           these volume ids will be returned.
+
+        :type filters: dict
+        :param filters: Optional filters that can be used to limit
+                        the results returned.  Filters are provided
+                        in the form of a dictionary consisting of
+                        filter names as the key and filter values
+                        as the value.  The set of allowable filter
+                        names/values is dependent on the request
+                        being performed.  Check the ECS API guide
+                        for details.
+
+        :type dry_run: bool
+        :param dry_run: Set to True if the operation should not actually run.
+
+        :rtype: list of :class:`boto.ec2.volume.Volume`
+        :return: The requested Volume objects
+        """
+        params = {}
+        if volume_ids:
+            self.build_list_params(params, volume_ids, 'DiskIds')
+        if filters:
+            self.build_filter_params(params, filters)
+        return self.get_list('DescribeDisks', params, ['Disks', Disk])
+
+    def get_all_security_groups(self, group_ids=None, filters=None):
+        """
+        Get all security groups associated with your account in a region.
+
+        :type group_ids: list
+        :param group_ids: A list of IDs of security groups to retrieve for
+                          security groups within a VPC.
+
+        :type filters: dict
+        :param filters: Optional filters that can be used to limit
+                        the results returned.  Filters are provided
+                        in the form of a dictionary consisting of
+                        filter names as the key and filter values
+                        as the value.  The set of allowable filter
+                        names/values is dependent on the request
+                        being performed.  Check the EC2 API guide
+                        for details.
+
+        :rtype: list
+        :return: A list of :class:`boto.ec2.securitygroup.SecurityGroup`
+        """
+        params = {}
+        if group_ids:
+            self.build_list_params(params, group_ids, 'SecurityGroupId')
+        if filters:
+            self.build_filter_params(params, filters)
+        return self.get_list('DescribeSecurityGroups', params, ['SecurityGroups', SecurityGroup])
