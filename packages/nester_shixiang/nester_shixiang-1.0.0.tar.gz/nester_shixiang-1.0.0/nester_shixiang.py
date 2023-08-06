5c87e0185a1dd1aa6480cc04d53a16802d702276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:42 +0000

    Add package nester_shixiang/1.0.0/nester_shixiang-1.0.0.tar.gz

---
+++ packages/nester_shixiang/nester_shixiang-1.0.0.tar.gz/nester_shixiang-1.0.0/nester_shixiang.py
@@ -0,0 +1,10 @@
+"""这是“wester.py”模块，提供了一个名为print_lol的函数，
+这个函数的作用是打印列表，其中可能包含（也可能不包含）列表"""
+def print_lol(the_list):
+    """这个函数取一个位置参数，名为“the_list”，这可以是任何Python列表
+    所指定的列表中的每个数据项会递归地输出到屏幕上，各数据项各占一行"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
---
+++ packages/nester_shixiang/nester_shixiang-1.0.0.tar.gz/nester_shixiang-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name          =  'nester_shixiang',
+        version       =  '1.0.0',
+        py_modules    =  ['nester_shixiang'],
+        author        =  'shixiangwang',
+        author_email  =  'shixiangwang@foxmail.com',
+        description   =  'A simple printer of nested lists',
+    )
