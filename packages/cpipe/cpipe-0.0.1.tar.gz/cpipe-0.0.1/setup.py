c54c5c9cc66b4ff2310536f3191a947c8e4c70cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:55 +0000

    Add package cpipe/0.0.1/cpipe-0.0.1.tar.gz

---
+++ packages/cpipe/cpipe-0.0.1.tar.gz/cpipe-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'cpipe',
+  packages = ['cpipe'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'pipeline system',
+  author = 'Kwon (Byoungkwon) An',
+  author_email = 'dran@csail.mit.edu',
+  url = 'https://github.com/dran/cpipe', # use the URL to the github repo
+  download_url = 'https://github.com/dran/cpipe@0.0.1', # I'll explain this in a second
+  keywords = ['cloud', 'pipeline', 'computation'], # arbitrary keywords
+  classifiers = [],
+)
