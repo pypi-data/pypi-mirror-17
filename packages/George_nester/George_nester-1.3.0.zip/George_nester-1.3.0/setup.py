90cfc86e653af2c082b7a36484e6538236fd3f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:47 +0000

    Add package George_nester/1.3.0/George_nester-1.3.0.zip

---
+++ packages/George_nester/George_nester-1.3.0.zip/George_nester-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'George_nester',
+	version = '1.3.0',
+	py_modules = ['George_nester'],
+	author = 'George',
+	author_email = 'qk0614@gmail.com',
+	url = 'https://wiki.python.org/moin/CheeseShopTutorial',
+	description = 'A simple printer of nested lists',
+    )
