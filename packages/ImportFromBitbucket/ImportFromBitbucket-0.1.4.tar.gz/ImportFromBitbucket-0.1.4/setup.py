4961de76257048f9884685e16598fd5c797c6b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:15 +0000

    Add package ImportFromBitbucket/0.1.4/ImportFromBitbucket-0.1.4.tar.gz

---
+++ packages/ImportFromBitbucket/ImportFromBitbucket-0.1.4.tar.gz/ImportFromBitbucket-0.1.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name="ImportFromBitbucket",
+ version="0.1.4",
+ description="Import .py files from CommmutativeMonoids project",
+ author="J. I. García-García",
+ author_email="ignacio.garcia@uca.es",
+ url="https://bitbucket.org/juan_ignacio_garcia_garcia/commutativemonoids",
+ license="GPL",
+ py_modules=["ImportFromBitbucket"]
+)
