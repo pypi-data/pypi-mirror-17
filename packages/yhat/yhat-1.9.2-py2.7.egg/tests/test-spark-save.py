0a6315c85abb2853704c7194345496dfea503a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:01 +0000

    Add package yhat/1.9.2/yhat-1.9.2-py2.7.egg

---
+++ packages/yhat/yhat-1.9.2-py2.7.egg/tests/test-spark-save.py
@@ -0,0 +1,19 @@
+from pyspark import SparkContext
+from pyspark.mllib.recommendation import ALS
+import terragon
+
+sc = SparkContext()
+
+r1 = (1, 1, 1.0)
+r2 = (1, 2, 2.0)
+r3 = (2, 1, 2.0)
+ratings = sc.parallelize([r1, r2, r3])
+model = ALS.trainImplicit(ratings, 1, seed=10)
+model.predict(2, 2)
+
+
+stringified_spark_model = terragon.dumps_spark_to_base64(sc, model)
+print(stringified_spark_model)
+
+# with open("/tmp/test.sparkle", "wb") as f:
+    # f.write(stringified_spark_model)
