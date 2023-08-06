b697ba1ebf0177ab00f4aeb364760e454cd703ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:24 +0000

    Add package simple-migrate/0.0.1/simple-migrate-0.0.1.tar.gz

---
+++ packages/simple-migrate/simple-migrate-0.0.1.tar.gz/simple-migrate-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from os.path import join, dirname
+
+with open(join(dirname(__file__), 'requires.txt'), 'r') as f:
+    install_requires = f.read().split("\n")
+
+setup(
+    name="simple-migrate",
+    version="0.0.1",
+    description="A simple database migrate tool",
+    author="ncuhome",
+    author_email="dev@ncuhome.cn",
+    url="https://github.com/ncuhome/simple-migrate",
+    license="MIT",
+    py_modules=['simple_migrate'],
+    install_requires=install_requires,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
