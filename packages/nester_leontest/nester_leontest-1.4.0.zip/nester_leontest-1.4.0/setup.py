ee23f8d9420f56bb4306e16e83bc2da6a7b3e66b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:30 +0000

    Add package nester_leontest/1.4.0/nester_leontest-1.4.0.zip

---
+++ packages/nester_leontest/nester_leontest-1.4.0.zip/nester_leontest-1.4.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_leontest',
+    version = '1.4.0',
+    py_modules = ['nester_leontest'],
+    author = 'Leon',
+    author_email = 'leon_119@yahoo.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
+
+    
