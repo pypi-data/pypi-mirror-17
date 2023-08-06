87900b54e5e1b9616653ed0822ab316e9676cd6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:40 +0000

    Add package django-statictastic/3.0.0/django-statictastic-3.0.0.tar.gz

---
+++ packages/django-statictastic/django-statictastic-3.0.0.tar.gz/django-statictastic-3.0.0/statictastic/metadata.py
@@ -0,0 +1,7 @@
+__version__ = "3.0.0"
+__author__ = "Dan Loewenherz"
+__copyright__ = "Copyright 2012-2016, Dan Loewenherz"
+__maintainer__ = "Dan Loewenherz"
+__email__ = "dan@lionheartsw.com"
+__license__ = "Apache 2.0"
+
