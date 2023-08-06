acaf67cfc3420fd0b0b46c518847a0fcc5a330fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:00 +0000

    Add package django-datawatch/0.1.11/django-datawatch-0.1.11.tar.gz

---
+++ packages/django-datawatch/django-datawatch-0.1.11.tar.gz/django-datawatch-0.1.11/django_datawatch/tests/test_integration.py
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals, print_function
+
+from django.test.testcases import TestCase
+
+from django_datawatch.base import BaseCheck
+from django_datawatch.monitoring import monitor
+
+
+class CheckImplementationTestCase(TestCase):
+    pass
+
+
+def test_generator(check_instance):
+    def test(self):
+        if not hasattr(check_instance, 'slug'):
+            self.fail('{check} has no slug'.format(
+                check=check_instance.__class__.__name__))
+        self.assertIsInstance(
+            check_instance, BaseCheck,
+            '{slug} is not derived from BaseCheck'.format(
+                slug=check_instance.slug))
+
+        check_instance_dict = check_instance.__class__.__dict__
+        if 'generate' not in check_instance_dict:
+            self.fail('{slug} must implement the generate method'.format(
+                slug=check_instance.slug))
+        if 'check' not in check_instance_dict:
+            self.fail('{slug} must implement the check method'.format(
+                slug=check_instance.slug))
+        if 'get_identifier' not in check_instance_dict:
+            self.fail('{slug} must implement the get_identifier method'.format(
+                slug=check_instance.slug))
+    return test
+
+
+monitor.autodiscover_checks()
+for check in monitor.get_all_registered_checks():
+    check_instance = check()
+    test_name = 'test_{module}_{check}'.format(
+        module=check_instance.__module__,
+        check=check_instance.__class__.__name__)
+    test = test_generator(check_instance)
+    setattr(CheckImplementationTestCase, test_name, test)
