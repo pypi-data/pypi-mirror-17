e0f5287d143737df1b0a4d6c6cbaa4a51cc70d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:53 +0000

    Add package django-request/1.5.0/django_request-1.5.0-py2-none-any.whl

---
+++ packages/django-request/django_request-1.5.0-py2-none-any.whl/request/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '1.5.0'
+__copyright__ = 'Copyright (c) 2009-2016, Kyle Fuller'
+__licence__ = 'BSD'
+__author__ = [
+    'Kyle Fuller <kyle@fuller.li>',
+    'Jannis Leidel (jezdez)',
+    'krisje8 <krisje8@gmail.com>',
+]
+__URL__ = 'http://django-request.readthedocs.org/en/latest/'
