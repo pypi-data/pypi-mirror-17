188b714e62ce2dfbe881c62154387a766db35184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:29 +0000

    Add package twentyc.tmpl/0.2.0/twentyc.tmpl-0.2.0.tar.gz

---
+++ packages/twentyc.tmpl/twentyc.tmpl-0.2.0.tar.gz/twentyc.tmpl-0.2.0/setup.py
@@ -0,0 +1,27 @@
+
+from setuptools import setup
+
+version = open('config/VERSION').read().strip()
+
+setup(
+    name='twentyc.tmpl',
+    version=version,
+    author='20C',
+    author_email='code@20c.com',
+    description='template abstraction and helper functions',
+    long_description=open('README.txt').read(),
+    license='LICENSE.txt',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=['twentyc.tmpl'],
+    namespace_packages=['twentyc'],
+    download_url = 'https://github.com/20c/twentyc.tmpl/tarball/%s' % version,
+    url = 'https://github.com/20c/twentyc.tmpl',
+    zip_safe=False
+)
