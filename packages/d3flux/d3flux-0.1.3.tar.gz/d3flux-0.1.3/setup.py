6b6c52439401c54bcfe9c125a37f17633c34a555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:46 +0000

    Add package d3flux/0.1.3/d3flux-0.1.3.tar.gz

---
+++ packages/d3flux/d3flux-0.1.3.tar.gz/d3flux-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='d3flux',
+      version='0.1.3',
+      description='A d3.js-based metabolic visualization tool for cobrapy',
+      url='https://github.com/pstjohn/d3flux',
+      download_url='https://github.com/pstjohn/d3flux/tarball/0.1',
+      author='Peter St. John',
+      author_email='peter.stjohn@nrel.gov',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['pandas', 'cobra', 'jinja2', 'ipython'],
+      )
