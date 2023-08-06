7124eed77c4c6afdc6b1757bffa999cd36df57de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:21 +0000

    Add package talkzoho/0.8.5/talkzoho-0.8.5.tar.gz

---
+++ packages/talkzoho/talkzoho-0.8.5.tar.gz/talkzoho-0.8.5/talkzoho/crm/potentials/filter_potentials.py
@@ -0,0 +1,20 @@
+from talkzoho.regions import US
+from talkzoho.crm.filter_records import filter_records
+from talkzoho.crm.potentials import MODULE
+
+
+async def filter_potentials(*,
+                            auth_token=None,
+                            term=None,
+                            region=US,
+                            columns=None,
+                            offset=0,
+                            limit=None):
+    return await filter_records(
+        MODULE,
+        auth_token=auth_token,
+        term=term,
+        region=region,
+        columns=columns,
+        offset=offset,
+        limit=limit)
---
+++ packages/talkzoho/talkzoho-0.8.5.tar.gz/talkzoho-0.8.5/talkzoho/projects/accounts/get_account.py
@@ -0,0 +1,20 @@
+from talkzoho.regions import US
+from talkzoho.projects.get_record import get_record
+
+from talkzoho.projects.accounts import MODULE
+
+
+async def get_account(*,
+                      id,
+                      module,
+                      portal_id,
+                      auth_token=None,
+                      region=US,
+                      columns=None):
+    return get_record(
+        id=id,
+        moudle=MODULE,
+        portal_id=portal_id,
+        auth_token=auth_token,
+        region=region,
+        columns=columns)
