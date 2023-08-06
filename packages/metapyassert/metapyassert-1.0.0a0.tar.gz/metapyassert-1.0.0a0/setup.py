d7ee81b9b4d24b08b2a8056c634d310f6a88ca1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:04 +0000

    Add package metapyassert/1.0.0a0/metapyassert-1.0.0a0.tar.gz

---
+++ packages/metapyassert/metapyassert-1.0.0a0.tar.gz/metapyassert-1.0.0a0/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+
+setup(
+    name='metapyassert',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.0.0a0',
+
+    description='MetaPyAssert is a package to give better type assertions.',
+
+    # Author details
+    author='PyAssert',
+    author_email='supachatdaemon@gmail.com',
+    url='',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='assertions meta metapyassert',
+)
