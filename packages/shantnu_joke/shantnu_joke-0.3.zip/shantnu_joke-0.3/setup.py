c61af615d384e8602319df85512c7662654ad855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:42 +0000

    Add package shantnu_joke/0.3/shantnu_joke-0.3.zip

---
+++ packages/shantnu_joke/shantnu_joke-0.3.zip/shantnu_joke-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='shantnu_joke',
+      version='0.3',
+      description='Shantnus terrible joke',
+      url='https://github.com/shantnu/shantnu_joke',
+      author='Shantnu',
+      license='GPL',
+      )
