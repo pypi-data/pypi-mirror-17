4875e9f6a4b7652c87f4faafcb267145e5066946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:17 +0000

    Add package json2table/1.1.4/json2table-1.1.4.tar.gz

---
+++ packages/json2table/json2table-1.1.4.tar.gz/json2table-1.1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = "json2table",
+    packages = ["json2table"],
+    version = "1.1.4",
+    description = "Convert JSON to an HTML table",
+    long_description=open("README.rst").read(),
+    author = "Ryan Latture",
+    author_email = "ryan.latture@gmail.com",
+    url = "https://github.com/latture/json2table",
+    download_url = "https://github.com/latture/json2table/tarball/master",
+    keywords = ["json", "HTML", "convert", "table"],
+    license = "MIT",
+    classifiers = (
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ),
+)
