dea4025825a3df166130a9246e52b040ba607451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:31 +0000

    Add package Shiny_SDK/0.1/Shiny_SDK-0.1.zip

---
+++ packages/Shiny_SDK/Shiny_SDK-0.1.zip/Shiny_SDK-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Shiny_SDK',
+    version='0.1',
+    keywords=('Shiny-Project'),
+    description='Python SDK from Shiny.',
+    classifiers=[
+    ],
+    url = 'https://shiny.kotori.moe',
+    license='MIT License',
+    install_requires=['requests'],
+
+    author='Eridanus Sora',
+    author_email='sora@sound.moe',
+
+    packages=find_packages(),
+    platforms='any',
+)
