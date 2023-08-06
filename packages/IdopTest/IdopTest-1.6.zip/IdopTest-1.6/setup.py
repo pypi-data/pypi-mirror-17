f4789e4153bf3bffc974ee23d5570ca9e81a6ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:40 +0000

    Add package IdopTest/1.6/IdopTest-1.6.zip

---
+++ packages/IdopTest/IdopTest-1.6.zip/IdopTest-1.6/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='IdopTest',
+      version='1.6',
+      py_modules=['IdopTest'],
+	  description="Idop Test stuff.",
+	  author="Ido Peles",
+	  author_email="idop@contel.co.il",
+      )
