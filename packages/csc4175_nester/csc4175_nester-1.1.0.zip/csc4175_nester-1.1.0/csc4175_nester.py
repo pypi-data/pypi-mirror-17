431aedd415d847f4738ce200489f8748e2df82b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:51 +0000

    Add package csc4175_nester/1.1.0/csc4175_nester-1.1.0.zip

---
+++ packages/csc4175_nester/csc4175_nester-1.1.0.zip/csc4175_nester-1.1.0/csc4175_nester.py
@@ -0,0 +1,14 @@
+"""hello welcome to python"""
+def print_lol(the_list,level):
+    """this is a jok"""
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t","end=")
+            print(each_item,list)
+
+
+
+
