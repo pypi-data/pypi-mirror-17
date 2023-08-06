a8030b7f11128114d877e8adbc13b6375dbc5b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:47 +0000

    Add package sqlalchemy-media/0.3.0/sqlalchemy-media-0.3.0.tar.gz

---
+++ packages/sqlalchemy-media/sqlalchemy-media-0.3.0.tar.gz/sqlalchemy-media-0.3.0/sqlalchemy_media/__init__.py
@@ -0,0 +1,13 @@
+
+from .typing_ import Attachable
+from .attachments import Attachment, AttachmentCollection, AttachmentList, AttachmentDict, File, FileDict, FileList, \
+    Image
+from .stores import Store, FileSystemStore, StoreManager
+from .descriptors import BaseDescriptor, StreamDescriptor, StreamCloserDescriptor, LocalFileSystemDescriptor, \
+    UrlDescriptor, CgiFieldStorageDescriptor, AttachableDescriptor
+from .analyzers import Analyzer, MagicAnalyzer
+from .validators import Validator, ContentTypeValidator
+
+
+__version__ = '0.3.0'
+
