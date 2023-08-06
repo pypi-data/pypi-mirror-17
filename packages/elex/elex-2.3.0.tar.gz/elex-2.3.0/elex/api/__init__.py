60de3252d9a344d334e4930e0c889097cb64ae60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:31 +0000

    Add package elex/2.3.0/elex-2.3.0.tar.gz

---
+++ packages/elex/elex-2.3.0.tar.gz/elex-2.3.0/elex/api/__init__.py
@@ -0,0 +1,26 @@
+from .models import (
+    APElection,
+    Candidate,
+    BallotMeasure,
+    CandidateReportingUnit,
+    ReportingUnit,
+    Race,
+    Elections,
+    Election
+)
+from .delegates import (
+    CandidateDelegateReport,
+    DelegateReport
+)
+__all__ = [
+    'APElection',
+    'BallotMeasure',
+    'Candidate',
+    'CandidateDelegateReport',
+    'CandidateReportingUnit',
+    'DelegateReport',
+    'Election',
+    'Elections',
+    'Race',
+    'ReportingUnit',
+]
