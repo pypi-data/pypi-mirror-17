ef447c43f6b3c4c4948dd250e24d894c7cc4609f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:15 +0000

    Add package microschema/0.4/microschema-0.4.tar.gz

---
+++ packages/microschema/microschema-0.4.tar.gz/microschema-0.4/microschema/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+__version__ = '0.4'
+
+from .core import (
+    validate,
+    convert,
+    ValidationError,
+    ConversionError,
+)
