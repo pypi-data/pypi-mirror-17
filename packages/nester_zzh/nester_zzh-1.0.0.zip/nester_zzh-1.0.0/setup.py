28875dff5bed8bd45e36aff026db2a6f83b5a9f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:50 +0000

    Add package nester_zzh/1.0.0/nester_zzh-1.0.0.zip

---
+++ packages/nester_zzh/nester_zzh-1.0.0.zip/nester_zzh-1.0.0/setup.py
@@ -0,0 +1,12 @@
+''' 模块安装文件  '''
+from distutils.core import setup
+
+setup(
+    name = 'nester_zzh', #这个是最终打包的文件名
+    version = '1.0.0',
+    py_modules = ['nester.py'], #要打包哪些，.py文件
+    author = 'zhengzhihan',
+    author_email = '21414474@qq.com',
+    url = 'http://localhost',
+    description = '这是hfpyhon第二章中的发布文件',
+    )
