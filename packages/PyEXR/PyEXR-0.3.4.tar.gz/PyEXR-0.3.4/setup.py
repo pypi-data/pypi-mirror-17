35e76e7948c48797673c7a288c247b32515f7c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:38 +0000

    Add package PyEXR/0.3.4/PyEXR-0.3.4.tar.gz

---
+++ packages/PyEXR/PyEXR-0.3.4.tar.gz/PyEXR-0.3.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name             = "PyEXR",
+  version          = "0.3.4",
+  description      = "One line EXR manipulation library",
+  author           = "Thijs Vogels",
+  author_email     = "t.vogels@me.com",
+  url              = "https://github.com/tvogels/pyexr",
+  install_requires = ["OpenEXR","numpy","future"],
+  packages         = find_packages(),
+)
