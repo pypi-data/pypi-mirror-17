c12cada564bc124e7433eb7b7bfd4d479e036ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:14 +0000

    Add package openwebif.py/0.9/openwebif.py-0.9.tar.gz

---
+++ packages/openwebif.py/openwebif.py-0.9.tar.gz/openwebif.py-0.9/openwebif/constants.py
@@ -0,0 +1,11 @@
+"""
+openwebif.constants
+~~~~~~~~~~~~~~~~~~~~
+
+List of constants
+
+Copyright (c) 2015 Finbarr Brady <https://github.com/fbradyirl>
+Licensed under the MIT license.
+"""
+
+DEFAULT_PORT = 80
---
+++ packages/openwebif.py/openwebif.py-0.9.tar.gz/openwebif.py-0.9/setup.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2015 Finbarr Brady <https://github.com/fbradyirl>
+# Licensed under the MIT license.
+
+# Used this guide to create module
+# http://peterdowns.com/posts/first-time-with-pypi.html
+
+from distutils.core import setup
+
+setup(
+    name='openwebif.py',
+    version='0.9',
+    description='Provides a python interface to interact with a device running OpenWebIf',
+    author='Finbarr Brady',
+    author_email='fbradyirl@users.noreply.github.com',
+    url='https://github.com/fbradyirl/openwebif.py',
+    download_url = 'https://github.com/fbradyirl/openwebif.py/tarball/0.9',
+    keywords='enigma2 openwebif python cgi interface',
+    packages=['openwebif'],
+    install_requires=['requests'],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet'
+        ],
+    )
