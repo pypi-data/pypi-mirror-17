63499481b848a67a1a66473b7a8e2aea49b872cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:40 +0000

    Add package hydrofunctions/0.1.4/hydrofunctions-0.1.4-py2.py3-none-any.whl

---
+++ packages/hydrofunctions/hydrofunctions-0.1.4-py2.py3-none-any.whl/hydrofunctions-0.1.4.dist-info/DESCRIPTION.rst
@@ -0,0 +1,67 @@
+===============================
+HydroFunctions
+===============================
+
+
+.. image:: https://img.shields.io/pypi/v/hydrofunctions.svg
+        :target: https://pypi.python.org/pypi/hydrofunctions
+
+.. image:: https://img.shields.io/travis/mroberge/hydrofunctions.svg
+        :target: https://travis-ci.org/mroberge/hydrofunctions
+
+.. image:: https://readthedocs.org/projects/hydrofunctions/badge/?version=latest
+        :target: https://hydrofunctions.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+
+A suite of convenience functions for exploring water data in IPython.
+
+
+* Free software: MIT license
+
+
+Features
+--------
+
+* Retrieves stream gauge data from the USGS NWIS service
+* Extracts into a Pandas dataframe or dict
+* Still under development! More features to come!
+
+Credits
+---------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+
+
+
+=======
+History
+=======
+
+0.1.0 (2016-08-11)
+------------------
+
+* Project set up.
+
+0.1.1 (2016-08-11)
+------------------
+
+* Customized project boilerplate.
+
+0.1.2 (2016-08-26)
+------------------
+
+* Set up template for custom exceptions
+* Added get_nwis function
+
+0.1.3 (2016-09-09)
+-----------------------
+
+* Check user inputs & raise explanatory exceptions
+* Extract data from response into a dataframe.
+* Stations object for managing data.
+
+
---
+++ packages/hydrofunctions/hydrofunctions-0.1.4-py2.py3-none-any.whl/tests/test_hydrofunctions.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_hydrofunctions
+----------------------------------
+
+Tests for `hydrofunctions` module.
+"""
+from __future__ import absolute_import, print_function
+from unittest import mock
+import unittest
+
+import pandas as pd
+
+import hydrofunctions as hf
+
+
+class TestHydrofunctions(unittest.TestCase):
+
+    @mock.patch('requests.get')
+    def test_hf_get_nwis_calls_correct_url(self, mock_get):
+
+        """
+        Thanks to
+        http://engineroom.trackmaven.com/blog/making-a-mockery-of-python/
+        """
+
+        site = 'A'
+        service = 'B'
+        start = 'C'
+        end = 'D'
+
+        expected_url = 'http://waterservices.usgs.gov/nwis/B/?'
+        expected_headers = {'max-age': '120', 'Accept-encoding': 'gzip'}
+        expected_params = {'format': 'json,1.1', 'sites': 'A', 'endDT': 'D',
+                           'startDT': 'C', 'parameterCd': '00060'}
+        expected = 'mock data'
+
+        mock_get.return_value = expected
+        actual = hf.get_nwis(site, service, start, end)
+        mock_get.assert_called_once_with(expected_url, params=expected_params,
+                                         headers=expected_headers)
+        self.assertEqual(actual, expected)
+
+    def test_hf_extract_nwis_dict(self):
+        # I need to make a response fixture to test this out!!
+        test = hf.get_nwis("01589440", "dv", "2013-01-01", "2013-01-05")
+        actual = hf.extract_nwis_dict(test)
+        self.assertIs(type(actual), dict, msg="Did not return a dict")
+
+    def test_hf_extract_nwis_df(self):
+        # I need to make a response fixture to test this out!!
+        test = hf.get_nwis("01589440", "dv", "2013-01-01", "2013-01-05")
+        actual = hf.extract_nwis_df(test)
+        self.assertIs(type(actual), pd.core.frame.DataFrame,
+                      msg="Did not return a df")
+
+    @unittest.skip("What happens if NWIS returns valid response with no data?")
+    def test_hf_extract_nwis_raises_exception_when_df_is_empty(self):
+        # See line 78 in hydrofunctions
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
