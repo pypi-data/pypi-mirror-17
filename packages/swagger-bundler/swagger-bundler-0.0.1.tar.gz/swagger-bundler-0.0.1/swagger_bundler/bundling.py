3dc290cba590121bc23d73a8c8e431ebe5c57173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:40 +0000

    Add package swagger-bundler/0.0.1/swagger-bundler-0.0.1.tar.gz

---
+++ packages/swagger-bundler/swagger-bundler-0.0.1.tar.gz/swagger-bundler-0.0.1/swagger_bundler/bundling.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+import logging
+from . import loading
+from .ordering import ordering
+from . import composing
+from . import prefixing
+
+logger = logging.getLogger(__name__)
+
+
+def transform(ctx, data):
+    subfiles = ctx.detector.detect_compose()
+    if subfiles:
+        data = composing.transform(ctx, data, subfiles)
+
+    namespace = ctx.detector.detect_namespace()
+    if namespace:
+        data = prefixing.transform(ctx, data, namespace=namespace)
+    return data
+
+
+def run(ctx, inp, outp):
+    subcontext = ctx.make_subcontext_from_port(inp)
+    result = transform(subcontext, subcontext.data)
+    ordered = ordering(result)
+    loading.dump(ordered, outp, allow_unicode=True, default_flow_style=False)
