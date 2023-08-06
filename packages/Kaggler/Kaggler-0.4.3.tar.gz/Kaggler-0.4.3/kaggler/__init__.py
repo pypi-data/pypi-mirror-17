7f97daa6b3fbfc48c7c73d8348ea3d71db25d8f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:20 +0000

    Add package Kaggler/0.4.3/Kaggler-0.4.3.tar.gz

---
+++ packages/Kaggler/Kaggler-0.4.3.tar.gz/Kaggler-0.4.3/kaggler/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.4.3'
+__all__ = ['const',
+           'data_io',
+           'feature_selection',
+           'metrics',
+           'model',
+           'online_model',
+           'preprocessing',
+           'util']
