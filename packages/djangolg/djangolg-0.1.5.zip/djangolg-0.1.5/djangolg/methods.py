748b23c02bf547dd8e9a435e26581f4c1360962b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:22 +0000

    Add package djangolg/0.1.5/djangolg-0.1.5.zip

---
+++ packages/djangolg/djangolg-0.1.5.zip/djangolg-0.1.5/djangolg/methods.py
@@ -0,0 +1,93 @@
+from djangolg import settings
+
+
+def methods():
+    M = sorted(settings.METHODS.items(), key=lambda m: m[1]['index'])
+    return [m[0] for m in M]
+
+
+def dialects():
+    D = sorted(settings.DIALECTS.items(), key=lambda d: d[1]['index'])
+    return [d[0] for d in D]
+
+
+def dialect_choices():
+    return [ (d, Dialect(d).name) for d in dialects() ]
+
+
+class Method(object):
+    def __init__(self, method):
+        if method in methods():
+            self.method = settings.METHODS[method]
+        else:
+            raise KeyError
+        self._dialect = None
+
+    @property
+    def dialect(self):
+        return self._dialect
+
+    @dialect.setter
+    def dialect(self, dialect=None):
+        if self.name in dialect.cmds:
+            self._dialect = dialect
+        else:
+            raise NotImplementedError
+        return self
+
+    @property
+    def name(self):
+        return self.method['name']
+
+    @property
+    def title(self):
+        return self.method['title']
+
+    @property
+    def description(self):
+        return self.method['description']
+
+    @property
+    def target(self):
+        return self.method['target']
+
+    @property
+    def options(self):
+        if self.dialect:
+            if 'options' in self.dialect.cmds[self.name]:
+                return self.dialect.cmds[self.name]['options']
+        else:
+            if 'options' in self.method:
+                return self.method['options']
+        return []
+
+    @property
+    def option_choices(self):
+        return ( (self.options.index(option), option['label']) for option in self.options )
+
+    @property
+    def cmd(self):
+        if self.dialect:
+            return self.dialect.cmds[self.name]['cmd']
+        else:
+            return self.method['cmd']
+
+
+class Dialect(object):
+    def __init__(self, dialect):
+        if dialect in dialects():
+            self.dialect = settings.DIALECTS[dialect]
+        else:
+            raise KeyError
+
+    @property
+    def index(self):
+        return self.dialect['index']
+
+    @property
+    def name(self):
+        return self.dialect['name']
+
+    @property
+    def cmds(self):
+        return self.dialect['cmds']
