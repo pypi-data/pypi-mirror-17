a8afb135ee01ae7ed54f10a8dec52f852c9af26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:35 +0000

    Add package dcos-shakedown/1.1.6/dcos-shakedown-1.1.6.tar.gz

---
+++ packages/dcos-shakedown/dcos-shakedown-1.1.6.tar.gz/dcos-shakedown-1.1.6/shakedown/__init__.py
@@ -0,0 +1,13 @@
+from shakedown.cli import *
+from shakedown.cli.helpers import *
+from shakedown.dcos import *
+from shakedown.dcos.command import *
+from shakedown.dcos.file import *
+from shakedown.dcos.package import *
+from shakedown.dcos.service import *
+from shakedown.dcos.task import *
+from shakedown.dcos.zookeeper import *
+from shakedown.dcos.agent import *
+from shakedown.dcos.master import *
+
+VERSION='1.1.6'
