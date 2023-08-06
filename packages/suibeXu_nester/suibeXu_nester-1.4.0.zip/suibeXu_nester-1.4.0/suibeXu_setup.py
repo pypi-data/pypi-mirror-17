7175fe01bb477cf28f2d4afd3cf87ee378879c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:53 +0000

    Add package suibeXu_nester/1.4.0/suibeXu_nester-1.4.0.zip

---
+++ packages/suibeXu_nester/suibeXu_nester-1.4.0.zip/suibeXu_nester-1.4.0/suibeXu_setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'suibeXu_nester',
+        version     = '1.4.0',
+        py_modules  = ['suibeXu_nester'],
+        author      = 'hfpython',
+        author_email= 'hfpython@headfirstlabs.com',
+        url         = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+    )
