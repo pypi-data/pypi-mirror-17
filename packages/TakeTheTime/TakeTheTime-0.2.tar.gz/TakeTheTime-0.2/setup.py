fc59d62b84710a034d816b5fbd90da30d77e8d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:24 +0000

    Add package TakeTheTime/0.2/TakeTheTime-0.2.tar.gz

---
+++ packages/TakeTheTime/TakeTheTime-0.2.tar.gz/TakeTheTime-0.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='TakeTheTime',
+      version='0.2',
+      description='Take The Time, a time-taking library for Python',
+      author='Erik Bjäreholt',
+      author_email='erik@bjareho.lt',
+      url='https://github.com/ErikBjare/TakeTheTime',
+      packages=['TakeTheTime'],
+      install_requires=[
+          'typing'
+      ])
