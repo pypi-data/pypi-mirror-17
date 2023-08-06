da85ee97b1babcb64bdfbb0f5a26a8775d6ce32f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:00 +0000

    Add package riskiq/0.4.10/riskiq-0.4.10.tar.gz

---
+++ packages/riskiq/riskiq-0.4.10.tar.gz/riskiq-0.4.10/riskiq/render.py
@@ -0,0 +1,20 @@
+import os
+
+from jinja2 import Template, Environment, PackageLoader
+
+def renderer(data, template_file, custom_template=None,
+        verbose=False, oneline=False):
+    """
+    Render the template with supplied context.
+    Example template_file: "blacklist/lookup"
+
+    """
+    if custom_template:
+        with open(custom_template) as f:
+            template = Template(f.read())
+        return template.render(data=data, verbose=verbose).encode('utf-8').strip()
+    if oneline:
+        template_file += '_oneline'
+    env = Environment(loader=PackageLoader('riskiq', 'templates'))
+    template = env.get_template(template_file)
+    return template.render(data=data, verbose=verbose).encode('utf-8').strip()
