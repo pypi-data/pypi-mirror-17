fe1f8ddd5aac641c486317b0d8355d28c6bb1793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:37 +0000

    Add package QualiCSCLI/0.8.2/QualiCSCLI-0.8.2.tar.gz

---
+++ packages/QualiCSCLI/QualiCSCLI-0.8.2.tar.gz/QualiCSCLI-0.8.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='QualiCSCLI',
+	version='0.8.2',
+	py_modules=['QualiCSCLI'],
+	packages=find_packages(),
+	install_requires=['requests',],
+	author="graboskyc",
+	author_email="chris@grabosky.net",
+	entry_points='''
+		[console_scripts]
+		QualiCSCLI=QualiCSCLI:cli
+	''',
+)
