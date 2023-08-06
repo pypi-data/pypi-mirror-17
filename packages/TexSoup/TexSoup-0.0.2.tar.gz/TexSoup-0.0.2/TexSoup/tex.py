cc43bec2323a06a98d5955ab012317711d01d1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:12 +0000

    Add package TexSoup/0.0.2/TexSoup-0.0.2.tar.gz

---
+++ packages/TexSoup/TexSoup-0.0.2.tar.gz/TexSoup-0.0.2/TexSoup/tex.py
@@ -0,0 +1,20 @@
+from TexSoup.reader import *
+from TexSoup.data import *
+from TexSoup.utils import *
+import itertools
+
+
+def read(tex):
+    """Read and parse all LaTeX source
+
+    :param str tex: LaTeX source
+    :return TexEnv: the global environment
+    """
+    if isinstance(tex, str):
+        tex = tex.strip().splitlines()
+    buf, children = Buffer(itertools.chain(*tokenize_lines(tex))), []
+    while buf.hasNext():
+        content = read_tex(buf)
+        if content is not None:
+            children.append(content)
+    return TexEnv('[tex]', children)
