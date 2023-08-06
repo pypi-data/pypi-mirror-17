b26fda1947538c38bf0383d62a62dc7a190f7c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:32 +0000

    Add package okerrclient/2.0.9/okerrclient-2.0.9.tar.gz

---
+++ packages/okerrclient/okerrclient-2.0.9.tar.gz/okerrclient-2.0.9/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='okerrclient',
+    version='2.0.9',
+    description='client for okerr cloud monitoring system',
+    url='http://okerr.com/',
+    author='Yaroslav Polyakov',
+    author_email='xenon@sysattack.com',
+    license='MIT',
+    packages=['okerrclient'],
+    scripts=['scripts/okerrclient'],
+    data_files = [
+        ('okerrclient',['data/conf/okerrclient.conf']),
+        ('/etc/init.d', ['data/init.d/okerrclient']),
+        ('/etc/systemd/system',['data/systemd/okerrclient.service']),
+    ], 
+    install_requires=['six', 'requests', 'psutil', 'evalidate', 'python-daemon', 'configargparse'],
+    include_package_data = True,
+    zip_safe=False
+)    
+
