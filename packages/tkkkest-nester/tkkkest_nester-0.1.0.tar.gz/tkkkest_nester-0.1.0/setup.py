991df5fb07c876820602b98185f1a1f79b6799ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:24 +0000

    Add package tkkkest-nester/0.1.0/tkkkest_nester-0.1.0.tar.gz

---
+++ packages/tkkkest-nester/tkkkest_nester-0.1.0.tar.gz/tkkkest_nester-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tkkkest_nester',
+    version='0.1.0',
+    author='J. Random Hacker',
+    author_email='jrh@example.com',
+    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/TowelStuff/',
+    description='Useful towel-related stuff.',
+    
+)
