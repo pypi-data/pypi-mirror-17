8fdb4028ea6442ca51cd2c0c93229c0f5fe967c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:58 +0000

    Add package nexus_uploader/1.0.4/nexus_uploader-1.0.4.tar.gz

---
+++ packages/nexus_uploader/nexus_uploader-1.0.4.tar.gz/nexus_uploader-1.0.4/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import os, sys
+from setuptools import setup, find_packages
+
+version = '1.0.4'
+setup_requires = []
+
+if 'nexus_upload' in sys.argv:
+    sys.argv.remove('nexus_upload')
+    setup_requires.append('nexus_uploader')
+
+if '--snapshot' in sys.argv:
+    sys.argv.remove('--snapshot')
+    version += '-SNAPSHOT'
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')) as file_requirements:
+    requirements = file_requirements.readlines()
+
+setup(
+    name='nexus_uploader',
+    description='CLI tool to upload Python packages listed in a requirements.txt file into a Sonatype Nexus (from Pypi), and return the list of the artifact URLs',
+    author='Lucas Cimon',
+    url='http://github.com/voyages-sncf-technologies/nexus_uploader',
+    install_requires=requirements,
+    packages=find_packages(),
+    version=version,
+    setup_requires=setup_requires,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities',
+    ],
+    keywords='nexus artifacts packages upload requirements pip-compile',
+    license='Apache License 2.0',
+    include_package_data=True,  # Active la prise en compte du fichier MANIFEST.in
+    entry_points={
+        'console_scripts': ['pyRequirements2nexus = nexus_uploader.pyrequirements2nexus:main'],
+        'distutils.commands': ['nexus_upload = nexus_uploader.nexus_upload_cmd:NexusUploadCmd'],
+    },
+)
