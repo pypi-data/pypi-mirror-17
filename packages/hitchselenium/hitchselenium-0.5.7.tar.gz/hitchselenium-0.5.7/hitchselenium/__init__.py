875077e6aa2d0c7f1b41cc63320973922c632600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:05 +0000

    Add package hitchselenium/0.5.7/hitchselenium-0.5.7.tar.gz

---
+++ packages/hitchselenium/hitchselenium-0.5.7.tar.gz/hitchselenium-0.5.7/hitchselenium/__init__.py
@@ -0,0 +1,11 @@
+from hitchselenium.step_library import SeleniumStepLibrary
+from hitchselenium.selenium_service import SeleniumService
+from hitchselenium.firefox_service import FirefoxService
+from hitchselenium.firefox_package import FirefoxPackage
+from hitchselenium.profile import Profile
+from hitchselenium.server import run
+
+
+UNIXPACKAGES = [
+    "xvfb", "xauth", "xserver-xorg", "dbus-x11", "ca-certificates",
+]
