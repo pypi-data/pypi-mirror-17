c35cf854641e6e4cf5d7af22636cc1c0888ac99a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:26 +0000

    Add package PageTracker/0.1.0/PageTracker-0.1.0.tar.gz

---
+++ packages/PageTracker/PageTracker-0.1.0.tar.gz/PageTracker-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='PageTracker',
+    version='0.1.0',
+    author='rakesh sukumar',
+    author_email='rsukuma2@ncsu.edu',
+    packages=['PageTracker'],
+    url='http://pypi.python.org/pypi/PageTracker/',
+    license='LICENSE.txt',
+    description='Tracks pages and sends out email alerts when the page changes',
+    long_description=open('README.txt').read(),
+    install_requires=[
+       "beautifulsoup4>=4.5.1",
+       "bs4>=0.0.1",
+       "requests>=2.11.1",
+       "simplejson>=3.8.2"
+    ],
+)
