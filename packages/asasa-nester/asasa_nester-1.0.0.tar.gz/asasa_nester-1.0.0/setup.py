6f4aafa549ee9ddb404c020b2d0c65948c72683e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:24 +0000

    Add package asasa-nester/1.0.0/asasa_nester-1.0.0.tar.gz

---
+++ packages/asasa-nester/asasa_nester-1.0.0.tar.gz/asasa_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name            = 'asasa_nester',
+       version         = '1.0.0',
+       py_modules      = ['asasa_nester'],
+       author          = 'fengdanfeng',
+       author_email    = '15881023297m@sina.cn',
+       url             = 'http://www.headfirstlabs.com',
+       description     = 'A simple printer of nested lists',
+    )
