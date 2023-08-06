b9397e5570c1dc5e0960537c5ab197a056862cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:13 +0000

    Add package medved/2016.10.20.3/medved-2016.10.20.3.tar.gz

---
+++ packages/medved/medved-2016.10.20.3.tar.gz/medved-2016.10.20.3/medved/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5 import QtWidgets
+from medved.controller import MedvedController
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    app.setOrganizationName('SNBL')
+    app.setOrganizationDomain('snbl.eu')
+    app.setApplicationName('medved')
+    MedvedController()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
