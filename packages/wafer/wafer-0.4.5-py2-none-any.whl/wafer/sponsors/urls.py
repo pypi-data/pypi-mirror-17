818d53a85a48b725fb69190f8318db143eb4eab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:54 +0000

    Add package wafer/0.4.5/wafer-0.4.5-py2-none-any.whl

---
+++ packages/wafer/wafer-0.4.5-py2-none-any.whl/wafer/sponsors/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, url, include
+from rest_framework import routers
+
+from wafer.sponsors.views import (
+    ShowSponsors, SponsorView, ShowPackages, SponsorViewSet, PackageViewSet)
+
+
+router = routers.DefaultRouter()
+router.register(r'sponsors', SponsorViewSet)
+router.register(r'packages', PackageViewSet)
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', ShowSponsors.as_view(),
+        name='wafer_sponsors'),
+    url(r'^(?P<pk>\d+)/$', SponsorView.as_view(), name='wafer_sponsor'),
+    url(r'^packages/$', ShowPackages.as_view(),
+        name='wafer_sponsorship_packages'),
+    url(r'^api/', include(router.urls)),
+)
