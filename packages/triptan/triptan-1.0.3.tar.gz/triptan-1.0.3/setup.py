78170af4492e51ebfef5ac9ed0b1cf7305fb67d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:40 +0000

    Add package triptan/1.0.3/triptan-1.0.3.tar.gz

---
+++ packages/triptan/triptan-1.0.3.tar.gz/triptan-1.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='triptan',
+    version='1.0.3',
+    description='Datastore independent migration tool',
+    author='Alexander Jung-Loddenkemper',
+    author_email='alexander@julo.ch',
+    url='https://github.com/alexanderjulo/triptan',
+    packages=find_packages(),
+    install_requires=[
+        'Click>=6,<7',
+        'click-log<1',
+        'PyYAML>=3,<4',
+        'jinja2>=2,<4'
+    ],
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': ['triptan=triptan.cli:main']
+    }
+)
