d2bd101f33362da43130114352d1f2ae81388288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:25 +0000

    Add package coco-nester/1.0.0/coco_nester-1.0.0.tar.gz

---
+++ packages/coco-nester/coco_nester-1.0.0.tar.gz/coco_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name            = 'coco_nester',
+       version         = '1.0.0',
+       py_modules      = ['coco_nester'],
+       author          = 'fengdanfeng',
+       author_email    = '15881023297m@sina.cn',
+       url             = 'http://www.headfirstlabs.com',
+       description     = 'A simple printer of nested lists',
+    )
