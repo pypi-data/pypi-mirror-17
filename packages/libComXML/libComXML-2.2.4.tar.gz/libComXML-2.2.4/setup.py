76a9c046ca4ee977331b0126e5b2c4c7b16b1756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:31 +0000

    Add package libComXML/2.2.4/libComXML-2.2.4.tar.gz

---
+++ packages/libComXML/libComXML-2.2.4.tar.gz/libComXML-2.2.4/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='libComXML',
+    version='2.2.4',
+    url='https://github.com/gisce/libComXML',
+    author='GISCE Enginyeria, SL',
+    author_email='devel@gisce.net',
+    packages=find_packages(),
+    install_requires=['lxml'],
+    license='None',
+    description='This library permits XML generation from Python objects',
+    test_suite='tests'
+)
