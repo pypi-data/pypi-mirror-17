1d9c5ec80978729a6e499b1215b5e494862d8aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:07 +0000

    Add package badwolf/0.4.0/badwolf-0.4.0.tar.gz

---
+++ packages/badwolf/badwolf-0.4.0.tar.gz/badwolf-0.4.0/badwolf/tasks.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+import atexit
+import logging
+import multiprocessing
+from concurrent.futures import ProcessPoolExecutor
+
+from badwolf.extensions import sentry
+
+
+logger = logging.getLogger(__name__)
+
+executor = ProcessPoolExecutor(max_workers=10)
+# per repository lock
+_LOCKS = {}
+
+
+@atexit.register
+def _shutdown_executor():
+    try:
+        executor.shutdown()
+    except Exception:
+        pass
+
+
+def _run_task(_task_func, *args, **kwargs):
+    try:
+        _task_func(*args, **kwargs)
+    except Exception:
+        logger.exception('Error running task func: %s', _task_func)
+        sentry.captureException()
+
+
+def async_task(f):
+    def delay(*args, **kwargs):
+        # return _run_task(f, *args, **kwargs)
+        return executor.submit(_run_task, f, *args, **kwargs)
+    f.delay = delay
+    return f
+
+
+@async_task
+def run_test(context):
+    from badwolf.runner import TestRunner
+
+    lock = _LOCKS.setdefault(context.repository, multiprocessing.Lock())
+    runner = TestRunner(context, lock)
+    runner.run()
