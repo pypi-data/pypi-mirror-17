f13e606b9cffef70e0ea7baad09786aa3e648f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:04 +0000

    Add package techbubbleiotjumpwaymqtt/0.1.0/techbubbleiotjumpwaymqtt-0.1.0.tar.gz

---
+++ packages/techbubbleiotjumpwaymqtt/techbubbleiotjumpwaymqtt-0.1.0.tar.gz/techbubbleiotjumpwaymqtt-0.1.0/setup.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.insert(0, 'src')
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='techbubbleiotjumpwaymqtt',
+    version="0.1.0",
+    author='Adam Milton-Barker',
+    author_email='adammiltonbarker@gmail.com',
+    package_dir={'': 'src'},
+	package_data={'techbubbleiotjumpwaymqtt': ['*.pem']},
+    url='https://github.com/TechBubbleTechnologies/TechBubble-Iot-JumpWay-Python-MQTT',
+    license='MIT',
+    description='TechBubble Technologies IoT JumpWay MQTT Client for Python',
+    packages=['techbubbleiotjumpwaymqtt'],
+    install_requires=[
+        "paho-mqtt >= 1.1",
+    ],
+    classifiers=[]
+)
