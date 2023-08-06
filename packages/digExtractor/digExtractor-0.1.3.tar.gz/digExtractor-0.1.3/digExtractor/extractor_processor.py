3455f515921e885a2b369e5247945474cc28c26e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:09 +0000

    Add package digExtractor/0.1.3/digExtractor-0.1.3.tar.gz

---
+++ packages/digExtractor/digExtractor-0.1.3.tar.gz/digExtractor-0.1.3/digExtractor/extractor_processor.py
@@ -0,0 +1,47 @@
+import itertools
+import types
+
+class ExtractorProcessor:
+
+	def set_output_field(self, output_field):
+		self.output_field = output_field
+		return self
+
+	def set_input_fields(self, input_fields):
+		if not (isinstance(input_fields, basestring) or isinstance(input_fields, types.ListType)):
+			raise ValueError("input_fields must be a string or a list")
+		self.input_fields = input_fields
+		return self
+
+	def set_extractor(self, extractor):
+		self.extractor = extractor
+		return self
+
+	def extract(self, doc):
+		if isinstance(self.input_fields, basestring):
+			renamed_inputs = dict()
+			renamed_inputs[self.extractor.get_renamed_input_fields()] = doc[self.input_fields]
+			
+		elif isinstance(self.input_fields, types.ListType):
+			inputs = dict(filter(lambda i:i[0] in self.input_fields, doc.iteritems()))
+			renamed_inputs = dict()
+			for input, renamed_input in itertools.izip(iter(self.input_fields), iter(self.extractor.get_renamed_input_fields())):
+				renamed_inputs[renamed_input] = inputs[input]
+			
+		else :
+			raise ValueError("input_fields must be a string or a list")
+		metadata = self.extractor.get_metadata()
+		metadata['value'] = self.extractor.extract(renamed_inputs)
+		metadata['source'] = self.input_fields
+		if self.output_field in doc:
+			output = doc[self.output_field]
+			if isinstance(output, dict):
+				output = [output, metadata]
+			elif isinstance(output, types.ListType):
+				output.append(metadata) 
+
+		else:
+			output = metadata
+		doc[self.output_field] = output
+		return doc
+
