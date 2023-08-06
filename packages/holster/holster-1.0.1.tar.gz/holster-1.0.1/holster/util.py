63c8c0bd915db1ca73affa1a76198952ac513dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:40 +0000

    Add package holster/1.0.1/holster-1.0.1.tar.gz

---
+++ packages/holster/holster-1.0.1.tar.gz/holster-1.0.1/holster/util.py
@@ -0,0 +1,15 @@
+from flask import flash, redirect
+
+
+def flashy(msg, etype="danger", path="/"):
+    """
+    Flashes a message and redirects a user. Returns a flask Response that can
+    be returned to the user.
+    """
+    flash(msg, etype)
+    return redirect(path)
+
+
+class SimpleObject(object):
+    def __init__(self, data=None):
+        self.__dict__.update(data or {})
