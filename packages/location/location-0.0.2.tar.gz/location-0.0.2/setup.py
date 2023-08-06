63984014e07d05994d1ffc6e8b3ef0ce8f535b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:13 +0000

    Add package location/0.0.2/location-0.0.2.tar.gz

---
+++ packages/location/location-0.0.2.tar.gz/location-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup (
+    name             = 'location',
+    version          = '0.0.2',
+    keywords         = ('location'),
+    description      = 'just a python',
+    license         = 'MIT License',
+    install_requires = [],
+
+    author           = 'leo',
+    author_email     = 'leo19920823@gmail.com',
+
+    packages         = find_packages(),
+    platforms        = 'any'
+)
