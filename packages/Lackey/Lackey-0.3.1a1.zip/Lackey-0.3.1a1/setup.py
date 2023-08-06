9eb87abaf22b680578b801d65804059618edb5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:50 +0000

    Add package Lackey/0.3.1a1/Lackey-0.3.1a1.zip

---
+++ packages/Lackey/Lackey-0.3.1a1.zip/Lackey-0.3.1a1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from setuptools.dist import Distribution
+
+class BinaryDistribution(Distribution):
+    def is_pure(self):
+        return False
+
+setup(
+	name="Lackey",
+	version="0.3.1a1",
+	description="A graphical automation framework for Python",
+	long_description="Lackey is a flexible automation framework using image recognition to reliably control complex and non-OS-standard business applications. Potential applications include automating tedious workflows, routine user interface testing, etc. Lackey can also run Sikuli scripts with the help of a shim.",
+	url="https://github.com/glitchassassin/lackey",
+	author="Jon Winsley",
+	author_email="jon.winsley@gmail.com",
+	license="MIT",
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		"Environment :: Console",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Information Technology",
+		"Intended Audience :: System Administrators",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: Microsoft :: Windows",
+		"Operating System :: Microsoft :: Windows",
+		"Topic :: Software Development :: Testing",
+		"Topic :: Utilities",
+		"Topic :: Desktop Environment"
+	],
+	keywords="automation testing sikuli",
+	packages=find_packages(exclude=['docs', 'tests']),
+	install_requires=['pillow', 'numpy', 'opencv-python'],
+	include_package_data=True,
+	distclass=BinaryDistribution
+)
