192e3ca869504e91b4aa88ee238505cd96dd2c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:55 +0000

    Add package hpx-insightly/2.2.7/hpx-insightly-2.2.7.tar.gz

---
+++ packages/hpx-insightly/hpx-insightly-2.2.7.tar.gz/hpx-insightly-2.2.7/insightly/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+from insightly.insightly import *
+
+__version__ = '2.2'
+__doc__= "This is a python library for insightly api v2.1 and v2.2"
---
+++ packages/hpx-insightly/hpx-insightly-2.2.7.tar.gz/hpx-insightly-2.2.7/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='hpx-insightly',
+	  version='2.2.7',
+	  description='A thin python wrapper around insightly api. This is a fork of insightly-python which not very active',
+	  url='https://github.com/harvestport/hpx-insightly',
+	  packages=['insightly']
+	  )
