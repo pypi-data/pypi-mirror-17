fd01d9fade01a547efaeb7558838a44f88155e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:28 +0000

    Add package uclime/0.1.4/uclime-0.1.4.win32.zip

---
+++ packages/uclime/uclime-0.1.4.win32.zip/Python27/Scripts/uclime-script.py
@@ -0,0 +1,10 @@
+#!C:\Python27\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'uclime==0.1.4','console_scripts','uclime'
+__requires__ = 'uclime==0.1.4'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('uclime==0.1.4', 'console_scripts', 'uclime')()
+    )
