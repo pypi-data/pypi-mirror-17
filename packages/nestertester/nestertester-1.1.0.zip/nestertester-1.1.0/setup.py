e2ca82ee58787524bc9248d4275857274a838f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:12 +0000

    Add package nestertester/1.1.0/nestertester-1.1.0.zip

---
+++ packages/nestertester/nestertester-1.1.0.zip/nestertester-1.1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+        name         = 'nestertester',
+        version      = '1.1.0',
+        py_modules   = ['nester'],
+        description  = 'A simple printer of nested lists',
+    )
