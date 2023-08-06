109f5861b6c5ded8653c8b2cfea0d254ff9f5cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:46 +0000

    Add package sda/0.8/sda-0.8.tar.gz

---
+++ packages/sda/sda-0.8.tar.gz/sda-0.8/sda/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""sda
+
+.. codeauthor:: John Lane <jlane@fanthreesixty.com>
+
+"""
+
+import element
+import fixtures
+import locators
+import mixins
+import page
+import site
+import structures
+
+__author__ = 'jlane'
+__copyright__ = 'Copyright (c) 2016 FanThreeSixty'
+__license__ = "MIT"
+__version__ = '0.8'
+__contact__ = 'jlane@fanthreesixty.com'
+__status__ = 'Beta'
+__docformat__ = 'reStructuredText'
+
+__all__ = ['element', 'fixtures', 'locators', 'page', 'shortcuts', 'site', 'structures']
