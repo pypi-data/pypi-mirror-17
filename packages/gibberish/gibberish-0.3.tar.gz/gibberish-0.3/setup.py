8f74a10e38a1da0ecfe7edba12d6581fc666dc16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:35 +0000

    Add package gibberish/0.3/gibberish-0.3.tar.gz

---
+++ packages/gibberish/gibberish-0.3.tar.gz/gibberish-0.3/setup.py
@@ -0,0 +1,32 @@
+import os
+
+from setuptools import setup
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(base_dir, 'README.rst')) as fp:
+    long_description = fp.read()
+
+setup(
+    name='gibberish',
+    description="A pseudo-word generator",
+    version='0.3',
+    author='Gregory Haskins',
+    author_email='greg@greghaskins.com',
+    maintainer='YPlan',
+    maintainer_email='alex.damian@yplanapp.com',
+    url='https://github.com/YPlan/gibberish',
+    py_modules=('gibberish',),
+    license='MIT License',
+    long_description=long_description,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    entry_points={
+        'console_scripts': 'gibberish = gibberish:console_main',
+    }
+)
