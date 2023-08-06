2cc7cdd6f8114b6749b0512e14b082876e23eb5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:21 +0000

    Add package osm-hall-monitor/0.52.1/osm_hall_monitor-0.52.1.tar.gz

---
+++ packages/osm-hall-monitor/osm_hall_monitor-0.52.1.tar.gz/osm_hall_monitor-0.52.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='osm_hall_monitor',
+      version='0.52.1',
+      description='Passive changeset monitoring for OpenStreetMap.',
+      url='http://github.com/ethan-nelson/osm_hall_monitor',
+      author='Ethan Nelson',
+      license='MIT',
+      author_email='git@ethan-nelson.com',
+      install_requires = ['psycopg2','osm_diff_tool'],
+      packages=['osmhm'])
+      
