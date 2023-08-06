6e054294376dd4a3a452f003e1479d6a0bae4d72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:38 +0000

    Add package lhc-python/1.3.6/lhc_python-1.3.6-py3.5.egg

---
+++ packages/lhc-python/lhc_python-1.3.6-py3.5.egg/lhc/collections/inorder_access_interval_set.py
@@ -0,0 +1,30 @@
+import bisect
+
+
+class InOrderAccessIntervalSet(object):
+    def __init__(self, iterator):
+        self._iterator = iterator
+        self._stops = []
+        self._items = []
+        self._item = next(self._iterator)
+
+    def __getitem__(self, key):
+        stops = self._stops
+        items = self._items
+
+        while len(stops) > 0 and stops[0] <= key.start:
+            stops.pop(0)
+            items.pop(0)
+
+        item = self._item
+        try:
+            while item.start < key.stop and key.start < item.stop:
+                index = bisect.bisect_right(stops, item.stop)
+                stops.insert(index, item.stop)
+                items.insert(index, item)
+                item = next(self._iterator)
+        except StopIteration:
+            pass
+        self._item = item
+
+        return sorted(items)
