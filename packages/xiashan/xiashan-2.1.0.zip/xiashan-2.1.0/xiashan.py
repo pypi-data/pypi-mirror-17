713d7ed08c3d01cdf3040be3d0c4dba96b536386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:17 +0000

    Add package xiashan/2.1.0/xiashan-2.1.0.zip

---
+++ packages/xiashan/xiashan-2.1.0.zip/xiashan-2.1.0/xiashan.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+def print_lol(the_list,indent = False,leve = 0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,leve+1)
+        else:
+            if indent:
+                for tab_stop in range(leve):
+                    print "\t",' '
+            print each_item
