b81f31d2d21da898d2e680384ec2105e215bd521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:04 +0000

    Add package searchpossibleposition/1.0.0/searchpossibleposition-1.0.0.zip

---
+++ packages/searchpossibleposition/searchpossibleposition-1.0.0.zip/searchpossibleposition-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='searchpossibleposition',
+    version='1.0.0',
+    py_modules=['searchposition'],
+    author='lyf94',
+    author_email='youfanglai@pku.edu.cn',
+    url='http://www.phy.pku.edu.cn',
+    description='give a atom and find its possible position in a crystal',
+    )
