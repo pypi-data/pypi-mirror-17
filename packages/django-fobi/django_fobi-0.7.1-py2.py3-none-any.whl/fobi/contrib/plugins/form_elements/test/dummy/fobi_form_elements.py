9f05bb37126f03932e7848f25d033138ce903255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:16 +0000

    Add package django-fobi/0.7.1/django_fobi-0.7.1-py2.py3-none-any.whl

---
+++ packages/django-fobi/django_fobi-0.7.1-py2.py3-none-any.whl/fobi/contrib/plugins/form_elements/test/dummy/fobi_form_elements.py
@@ -0,0 +1,49 @@
+from uuid import uuid4
+
+from django.utils.translation import ugettext, ugettext_lazy as _
+
+from nonefield.fields import NoneField
+
+from fobi.base import FormElementPlugin, form_element_plugin_registry
+from fobi.helpers import safe_text
+
+from . import UID
+
+__title__ = 'fobi.contrib.plugins.form_elements.test.dummy.fobi_form_elements'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('DummyPlugin',)
+
+
+class DummyPlugin(FormElementPlugin):
+    """Dummy plugin."""
+
+    uid = UID
+    name = _("Dummy")
+    group = _("Testing")
+
+    def post_processor(self):
+        """Post process data.
+
+        Always the same.
+        """
+        self.data.name = "{0}_{1}".format(self.uid, uuid4())
+
+    def get_form_field_instances(self, request=None):
+        """Get form field instances."""
+        kwargs = {
+            'initial': "<p>{0}</p>".format(
+                safe_text(ugettext("Dummy content"))
+            ),
+            'required': False,
+            'label': '',
+        }
+
+        form_field_instances = []
+
+        form_field_instances.append((self.data.name, NoneField, kwargs))
+        return form_field_instances
+
+
+form_element_plugin_registry.register(DummyPlugin)
