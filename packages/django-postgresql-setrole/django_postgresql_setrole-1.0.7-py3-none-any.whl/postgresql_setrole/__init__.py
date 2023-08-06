7c0a833df2a85c75280de7de83b86883a9493a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:03 +0000

    Add package django-postgresql-setrole/1.0.7/django_postgresql_setrole-1.0.7-py3-none-any.whl

---
+++ packages/django-postgresql-setrole/django_postgresql_setrole-1.0.7-py3-none-any.whl/postgresql_setrole/__init__.py
@@ -0,0 +1,31 @@
+# -* encoding: utf-8 *-
+import warnings
+
+from django.apps import AppConfig
+from django.db.backends.postgresql.base import DatabaseWrapper as PostgreSQLDatabaseWrapper
+from django.db.backends.signals import connection_created
+from typing import Any, Type
+
+
+def setrole_connection(*, sender: Type[PostgreSQLDatabaseWrapper],
+                       connection: PostgreSQLDatabaseWrapper, **kwargs: Any) -> None:
+    role = None
+    if "set_role" in connection.settings_dict:
+        role = connection.settings_dict["set_role"]
+    elif "SET_ROLE" in connection.settings_dict:
+        role = connection.settings_dict["SET_ROLE"]
+
+    if role:
+        connection.cursor().execute("SET ROLE %s", (role,))
+    else:
+        warnings.warn("postgresql_setrole app is installed, but no SET_ROLE value is in settings.DATABASE")
+
+
+class DjangoPostgreSQLSetRoleApp(AppConfig):
+    name = "postgresql_setrole"
+
+    def ready(self) -> None:
+        connection_created.connect(setrole_connection, sender=PostgreSQLDatabaseWrapper)
+
+
+default_app_config = 'postgresql_setrole.DjangoPostgreSQLSetRoleApp'
