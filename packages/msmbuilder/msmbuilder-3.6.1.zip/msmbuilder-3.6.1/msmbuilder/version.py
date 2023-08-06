2ea712d0ab0e5dd464e54273013381b5d79d6dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:27 +0000

    Add package msmbuilder/3.6.1/msmbuilder-3.6.1.zip

---
+++ packages/msmbuilder/msmbuilder-3.6.1.zip/msmbuilder-3.6.1/msmbuilder/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '3.6.1'
+short_version = '3.6'
+full_version = '3.6.1'
+git_revision = '472339ae5683724ec48129ec01c8665edf3685bb'
+release = True
