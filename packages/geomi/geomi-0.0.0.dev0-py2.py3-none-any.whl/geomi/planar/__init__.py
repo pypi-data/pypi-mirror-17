97f86cad15b328ae21811ac6d8dc83c7df45a20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:27 +0000

    Add package geomi/0.0.0.dev0/geomi-0.0.0.dev0-py2.py3-none-any.whl

---
+++ packages/geomi/geomi-0.0.0.dev0-py2.py3-none-any.whl/geomi/planar/__init__.py
@@ -0,0 +1,5 @@
+"""
+This is the **planar** module that encapsulates a couple of submodules.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra mollis pharetra. Nam semper, lectus sit amet tincidunt maximus, nibh tellus accumsan neque, ac varius lacus ante nec augue. Maecenas volutpat sed ipsum ut lacinia. Ut sed consectetur libero. Mauris velit est, scelerisque quis libero eget, aliquet blandit est. Donec bibendum odio et velit convallis, et scelerisque ante fermentum. Duis congue sem ex, at semper turpis egestas nec. Aenean eu condimentum orci, ac efficitur nulla. Etiam facilisis varius quam, at ultrices lectus semper eget. Suspendisse ultrices eros eu porttitor ornare. Nunc vitae diam ultricies, interdum mi a, faucibus leo.
+"""
