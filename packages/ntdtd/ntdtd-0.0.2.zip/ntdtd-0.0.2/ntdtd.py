a8ca524d3cb8a3b77933f1500bea1ebf9863b718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:46 +0000

    Add package ntdtd/0.0.2/ntdtd-0.0.2.zip

---
+++ packages/ntdtd/ntdtd-0.0.2.zip/ntdtd-0.0.2/ntdtd.py
@@ -0,0 +1,7 @@
+import dacm
+import mcb_h
+import plot
+import plot_sub
+import mpl_config
+import hist
+import rename
---
+++ packages/ntdtd/ntdtd-0.0.2.zip/ntdtd-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'ntdtd',
+    version = '0.0.2',
+    py_modules = ['ntdtd'],
+    author = 'zbz',
+    author_email = 'zhaobozhen@hotmail.com',
+    description = 'Processing tools for Nuctech detector data, 2016-09-23',
+) 
