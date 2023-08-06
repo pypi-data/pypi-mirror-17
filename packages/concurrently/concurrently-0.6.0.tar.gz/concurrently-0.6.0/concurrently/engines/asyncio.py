b70cb826691c7efcc0a7e12c8efb0f9dc347cd1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:32 +0000

    Add package concurrently/0.6.0/concurrently-0.6.0.tar.gz

---
+++ packages/concurrently/concurrently-0.6.0.tar.gz/concurrently-0.6.0/concurrently/engines/asyncio.py
@@ -0,0 +1,73 @@
+import asyncio
+from concurrent.futures import ProcessPoolExecutor
+from functools import lru_cache
+from typing import Callable, List
+
+from . import AbstractEngine, AbstractWaiter, UnhandledExceptions
+
+
+class AsyncIOWaiter(AbstractWaiter):
+
+    def __init__(self, fs: List[asyncio.Future], loop: asyncio.BaseEventLoop):
+        self._fs = fs
+        self._loop = loop
+
+    async def __call__(self, *, suppress_exceptions=False, fail_hard=False):
+        when = asyncio.FIRST_EXCEPTION if fail_hard else asyncio.ALL_COMPLETED
+        done, pending = await asyncio.wait(
+            self._fs, loop=self._loop, return_when=when
+        )
+
+        if fail_hard:
+            f = next(filter(lambda x: x.exception(), done), None)
+            if f:
+                [p.cancel() for p in pending]
+                await asyncio.wait(pending, loop=self._loop)
+                raise f.exception()
+
+        if not suppress_exceptions and self.exceptions():
+            raise UnhandledExceptions(self.exceptions())
+
+    async def stop(self):
+        for f in self._fs:
+            f.cancel()
+        await self(suppress_exceptions=True)
+
+    @lru_cache()
+    def exceptions(self) -> List[Exception]:
+        exc_list = []
+        for f in self._fs:
+            if f.cancelled():
+                continue
+
+            exc = f.exception()
+            if exc:
+                exc_list.append(exc)
+
+        return exc_list
+
+
+class AsyncIOEngine(AbstractEngine):
+
+    def __init__(self, *, loop: asyncio.BaseEventLoop=None):
+        super().__init__()
+        self.loop = loop or asyncio.get_event_loop()
+
+    def create_task(self, fn: asyncio.coroutine) -> asyncio.Future:
+        return self.loop.create_task(fn())
+
+    def waiter_factory(self, fs: List[asyncio.Future]):
+        return AsyncIOWaiter(fs=fs, loop=self.loop)
+
+
+class AsyncIOExecutorEngine(AsyncIOEngine):
+
+    def __init__(self, *, loop=None, executor=None):
+        assert not isinstance(executor, ProcessPoolExecutor), \
+            'ProcessPoolExecutor is not supported'
+
+        super().__init__(loop=loop)
+        self.pool = executor
+
+    def create_task(self, fn: Callable[[], None]) -> asyncio.Future:
+        return self.loop.run_in_executor(self.pool, fn)
