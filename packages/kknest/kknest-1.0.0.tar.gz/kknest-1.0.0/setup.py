fdbe6cc29e4dba45187f0f088b177f10899e6335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:54 +0000

    Add package kknest/1.0.0/kknest-1.0.0.tar.gz

---
+++ packages/kknest/kknest-1.0.0.tar.gz/kknest-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'kknest',
+	version = '1.0.0',
+	py_modules = ['kknest'],
+	author = 'kiran',
+	author_email = 'kiranurs4ever@gmail.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of nested list',
+	)
+
