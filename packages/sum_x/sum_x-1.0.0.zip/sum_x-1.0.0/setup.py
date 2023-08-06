5bf24d8f9cc6beb15d782c4843dacbc6d16f14de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:25 +0000

    Add package sum_x/1.0.0/sum_x-1.0.0.zip

---
+++ packages/sum_x/sum_x-1.0.0.zip/sum_x-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name = 'sum_x',
+        version = '1.0.0',
+        py_modules = ['sum_x'],
+        author = 'Raimundo César Ribeiro Leite Filho',
+        author_email = 'cesarribeiro234@gmail.com',
+        description = 'printer of a sum of four numbers',
+        )
---
+++ packages/sum_x/sum_x-1.0.0.zip/sum_x-1.0.0/sum_x.py
@@ -0,0 +1,4 @@
+def sum_x(a,b,c,d):
+    soma = a + b + c + d
+    print(soma)
+        
