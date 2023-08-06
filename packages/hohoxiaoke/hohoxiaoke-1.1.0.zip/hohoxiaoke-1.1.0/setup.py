4386202f44bde0b2ba1b2f761781ebe626ef3e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:22 +0000

    Add package hohoxiaoke/1.1.0/hohoxiaoke-1.1.0.zip

---
+++ packages/hohoxiaoke/hohoxiaoke-1.1.0.zip/hohoxiaoke-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name           = 'hohoxiaoke',
+	version        = '1.1.0',
+	py_modules     = ['nester'],
+	author         = 'hfpython',
+	author_email   = 'mrzhaoke@yeah.net',
+	url            = 'http://zhaoke.me',
+	description    = 'A simple printer of nested lists',
+     )
