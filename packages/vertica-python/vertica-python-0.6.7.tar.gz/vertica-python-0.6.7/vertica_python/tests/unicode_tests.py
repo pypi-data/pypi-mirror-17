b477058b536cebbdc70c9fbe0e8d26a4a87e7a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:40 +0000

    Add package vertica-python/0.6.7/vertica-python-0.6.7.tar.gz

---
+++ packages/vertica-python/vertica-python-0.6.7.tar.gz/vertica-python-0.6.7/vertica_python/tests/unicode_tests.py
@@ -0,0 +1,52 @@
+from .test_commons import conn_info, VerticaTestCase
+from .. import connect
+
+
+class UnicodeTestCase(VerticaTestCase):
+    def test_unicode_query(self):
+        value = u'\u16a0'
+        query = u"SELECT '{}'".format(value)
+
+        with connect(**conn_info) as conn:
+            cur = conn.cursor()
+            cur.execute(query)
+            res = cur.fetchone()
+
+            assert res[0] == value
+
+    # this test is broken on python3: see issue #112
+    def test_unicode_named_parameter_binding(self):
+        key = u'\u16a0'
+        value = u'\u16b1'
+        query = u"SELECT :{}".format(key)
+
+        with connect(**conn_info) as conn:
+            cur = conn.cursor()
+            cur.execute(query, {key: value})
+            res = cur.fetchone()
+
+            assert res[0] == value
+
+    def test_string_query(self):
+        value = u'test'
+        query = u"SELECT '{}'".format(value)
+
+        with connect(**conn_info) as conn:
+            cur = conn.cursor()
+            cur.execute(query)
+            res = cur.fetchone()
+
+            assert res[0] == value
+
+    # this test is broken on python3: see issue #112
+    def test_string_named_parameter_binding(self):
+        key = u'test'
+        value = u'value'
+        query = u"SELECT :{}".format(key)
+
+        with connect(**conn_info) as conn:
+            cur = conn.cursor()
+            cur.execute(query, {key: value})
+            res = cur.fetchone()
+
+            assert res[0] == value
