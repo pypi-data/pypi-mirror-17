55ba384dc0183fc9c8a10175b90c4d750d0b32b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:28 +0000

    Add package validater/0.11.0/validater-0.11.0.tar.gz

---
+++ packages/validater/validater-0.11.0.tar.gz/validater-0.11.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+setup(
+    name="validater",
+    version="0.11.0",
+    description="a python tool to "
+                "validate json/dict/list and convert value by schema",
+    author="guyskk",
+    author_email='guyskk@qq.com',
+    url="https://github.com/guyskk/validater",
+    license="MIT",
+    packages=['validater'],
+    install_requires=[],
+    tests_require=[
+        'pytest',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
