a88e32da28e283fa691401db6aacf81ae713ddb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:25 +0000

    Add package slotter/0.0.2/slotter-0.0.2.tar.gz

---
+++ packages/slotter/slotter-0.0.2.tar.gz/slotter-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+  name='slotter',
+  license='MIT',
+  version='0.0.2',
+  url='https://github.com/saurabh-hirani/slotter',
+  description=('Python library to slot stuff'),
+  author='Saurabh Hirani',
+  author_email='saurabh.hirani@gmail.com',
+  packages=find_packages(),
+  install_requires=[
+    'blist',
+    'pytest'
+  ],
+  entry_points = {}
+)
