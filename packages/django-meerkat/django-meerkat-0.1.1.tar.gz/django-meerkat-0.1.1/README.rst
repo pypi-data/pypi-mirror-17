0cb191d418645982d2044034d7e1474bb50c4590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:28 +0000

    Add package django-meerkat/0.1.1/django-meerkat-0.1.1.tar.gz

---
+++ packages/django-meerkat/django-meerkat-0.1.1.tar.gz/django-meerkat-0.1.1/README.rst
@@ -0,0 +1,78 @@
+=======
+Meerkat
+=======
+
+.. start-badges
+
+|travis|
+|landscape|
+|version|
+|wheel|
+|codecov|
+|gitter|
+
+.. |supported-versions|
+.. |supported-implementations|
+
+.. |travis| image:: https://travis-ci.org/Pawamoy/django-meerkat.svg?branch=master
+    :alt: Travis-CI Build Status
+    :target: https://travis-ci.org/Pawamoy/django-meerkat/
+
+.. |codecov| image:: https://codecov.io/github/Pawamoy/django-meerkat/coverage.svg?branch=master
+    :alt: Coverage Status
+    :target: https://codecov.io/github/Pawamoy/django-meerkat/
+
+.. |landscape| image:: https://landscape.io/github/Pawamoy/django-meerkat/master/landscape.svg?style=flat
+    :target: https://landscape.io/github/Pawamoy/django-meerkat/
+    :alt: Code Quality Status
+
+.. |version| image:: https://img.shields.io/pypi/v/django-meerkat.svg?style=flat
+    :alt: PyPI Package latest release
+    :target: https://pypi.python.org/pypi/django-meerkat/
+
+.. |downloads| image:: https://img.shields.io/pypi/dm/django-meerkat.svg?style=flat
+    :alt: PyPI Package monthly downloads
+    :target: https://pypi.python.org/pypi/django-meerkat/
+
+.. |wheel| image:: https://img.shields.io/pypi/wheel/django-meerkat.svg?style=flat
+    :alt: PyPI Wheel
+    :target: https://pypi.python.org/pypi/django-meerkat/
+
+.. |supported-versions| image:: https://img.shields.io/pypi/pyversions/django-meerkat.svg?style=flat
+    :alt: Supported versions
+    :target: https://pypi.python.org/pypi/django-meerkat/
+
+.. |supported-implementations| image:: https://img.shields.io/pypi/implementation/django-meerkat.svg?style=flat
+    :alt: Supported implementations
+    :target: https://pypi.python.org/pypi/django-meerkat/
+
+.. |gitter| image:: https://badges.gitter.im/Pawamoy/django-meerkat.svg
+    :alt: Join the chat at https://gitter.im/Pawamoy/django-meerkat
+    :target: https://gitter.im/Pawamoy/django-meerkat?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge
+
+
+.. end-badges
+
+Security audit tool for Django sites.
+
+License
+=======
+
+Software licensed under `MPL 2.0`_ license.
+
+.. _MPL 2.0 : https://www.mozilla.org/en-US/MPL/2.0/
+
+Installation
+============
+
+No release on PyPi yet. Clone the repo.
+
+Documentation
+=============
+
+https://github.com/Pawamoy/django-meerkat/wiki
+
+Development
+===========
+
+To run all the tests: ``tox``
---
+++ packages/django-meerkat/django-meerkat-0.1.1.tar.gz/django-meerkat-0.1.1/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import, print_function
+
+import io
+import re
+from glob import glob
+from os.path import basename, dirname, join, splitext
+
+from setuptools import find_packages, setup
+
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+setup(
+    name='django-meerkat',
+    version='0.1.1',
+    license='MPL 2.0',
+    description='Security audit tool for Django sites',
+    long_description='%s\n%s' % (
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S)
+            .sub('', read('README.rst')),
+        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    ),
+    author='Timothee Mazzucotelli',
+    author_email='timothee.mazzucotelli@gmail.com',
+    url='https://github.com/pawamoy/django-meerkat',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        # uncomment if you test on these interpreters:
+        # 'Programming Language :: Python :: Implementation :: IronPython',
+        # 'Programming Language :: Python :: Implementation :: Jython',
+        # 'Programming Language :: Python :: Implementation :: Stackless',
+        'Topic :: Utilities',
+    ],
+    keywords=[
+        'security', 'audit', 'admin', 'dashboard', 'logs', 'analysis', 'django'
+    ],
+    install_requires=[
+        'django-suit-dashboard'
+    ],
+    extras_require={
+        # eg:
+        #   'rst': ['docutils>=0.11'],
+        #   ':python_version=="2.6"': ['argparse'],
+    },
+)
