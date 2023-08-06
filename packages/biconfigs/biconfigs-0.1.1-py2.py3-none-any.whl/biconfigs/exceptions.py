e24ace8c90b3902071067372afc90b148f142fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:22 +0000

    Add package biconfigs/0.1.1/biconfigs-0.1.1-py2.py3-none-any.whl

---
+++ packages/biconfigs/biconfigs-0.1.1-py2.py3-none-any.whl/biconfigs/exceptions.py
@@ -0,0 +1,20 @@
+class InvalidPaserError(Exception):
+    pass
+
+class InvalidStorageError(Exception):
+    pass
+
+class AlreadyCreatedError(Exception):
+    pass
+
+class InvaildFilePathError(Exception):
+    pass
+
+class OpenFileError(Exception):
+    pass
+
+class LoadError(Exception):
+    pass
+
+class DumpError(Exception):
+    pass
