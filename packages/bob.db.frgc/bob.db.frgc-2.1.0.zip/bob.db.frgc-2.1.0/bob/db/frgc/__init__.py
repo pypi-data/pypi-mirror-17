35eb5d3e2c8d40072cadcbbe2da60953d01d2165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:10 +0000

    Add package bob.db.frgc/2.1.0/bob.db.frgc-2.1.0.zip

---
+++ packages/bob.db.frgc/bob.db.frgc-2.1.0.zip/bob.db.frgc-2.1.0/bob/db/frgc/__init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# @author: Manuel Guenther <Manuel.Guenther@idiap.ch>
+# @date:   Thu Sep  6 09:01:32 CEST 2012
+#
+# Copyright (C) 2011-2012 Idiap Research Institute, Martigny, Switzerland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+  This is the Bob database entry for the Face Recognition Grand Challange (FRGC) database in version 2.0.
+"""
+
+from .query import Database
+from .models import File, FileSet
+from .driver import Interface
+
+def get_config():
+  """Returns a string containing the configuration information.
+  """
+  import bob.extension
+  return bob.extension.get_config(__name__)
+
+
+# gets sphinx autodoc done right - don't remove it
+__all__ = [_ for _ in dir() if not _.startswith('_')]
