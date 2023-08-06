701fc97521c6b207fdd5f19a7ac25ae989cead71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:55 +0000

    Add package aiogithubauth/0.3.0/aiogithubauth-0.3.0.tar.gz

---
+++ packages/aiogithubauth/aiogithubauth-0.3.0.tar.gz/aiogithubauth-0.3.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+requires = [
+    'aioauth-client',
+    'cryptography',
+    'aiohttp',
+    'aiohttp_session'
+]
+
+setup_options = dict(
+    name='aiogithubauth',
+    version='0.3.0',
+    description='github auth middleware for aiohttp',
+    long_description='See readme at https://github.com/CanopyTax/aiohttp-github-auth',
+    author='nhumrich',
+    author_email='nick@humrich.us',
+    packages=find_packages('.'),
+    package_dir={'aiogithubauth': 'aiogithubauth'},
+    url='https://github.com/CanopyTax/aiohttp-github-auth',
+    install_requires=requires,
+    license="Apache",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
+
+setup(**setup_options)
