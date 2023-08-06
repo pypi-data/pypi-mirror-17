f9d8b5a5c51f74a86c5da0170aa88f15331afdb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:25 +0000

    Add package nester_0926/1.3.0/nester_0926-1.3.0.tar.gz

---
+++ packages/nester_0926/nester_0926-1.3.0.tar.gz/nester_0926-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nester_0926',
+	version		= '1.3.0',
+	py_modules	= ['nester_0926'],
+	author		= 'richard.cl.chang',
+	author_email	= 'richard@mail.nsysu.edu.tw',
+	url		= 'http://www.google.com',
+	description	= 'A simple printer of nested lists',
+     )
