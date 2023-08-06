b3fde0e4cedd6be65aa9b24a4915bf2ec1ececc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:16 +0000

    Add package Orange-Textable-Prototypes/0.1.6/Orange_Textable_Prototypes-0.1.6-py2.7.egg

---
+++ packages/Orange-Textable-Prototypes/Orange_Textable_Prototypes-0.1.6-py2.7.egg/_textable_prototypes/widgets/__init__.py
@@ -0,0 +1,22 @@
+"""
+Textable Prototypes
+===================
+
+"""
+
+NAME = "Textable Prototypes"
+
+DESCRIPTION = """Extra text analysis widgets for Orange Textable"""
+
+LONG_DESCRIPTION = """
+Orange Textable Prototypes collects text-analytic widgets that couldn't be 
+included in the core Orange Textable distribution for various reasons (notably 
+because they introduce dependencies to third-party packages, or simply because 
+they're not yet production-ready), but that may still be found useful by Orange 
+Canvas/Textable users.
+"""
+
+ICON = "icons/Category-Textable-Prototypes.png"
+
+BACKGROUND = "#ddecf9"
+
