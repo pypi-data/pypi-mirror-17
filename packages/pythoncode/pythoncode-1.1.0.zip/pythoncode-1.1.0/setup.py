874284fdc592442bf32a1fcce17ceffddf21c692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:20 +0000

    Add package pythoncode/1.1.0/pythoncode-1.1.0.zip

---
+++ packages/pythoncode/pythoncode-1.1.0.zip/pythoncode-1.1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+          name = 'pythoncode',
+          version = '1.1.0',
+          py_modules = ['pythoncode'],
+          author = 'hfpython',
+          author_email ='1021918257@qq.com',
+          description = 'A simple printer of nested lists',
+
+
+
+
+
+
+
+     )
