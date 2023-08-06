ef373e839cdf3a9969fdfe4d527a8cf360ea4e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:59 +0000

    Add package meanGen/0.1.0/meanGen-0.1.0.tar.gz

---
+++ packages/meanGen/meanGen-0.1.0.tar.gz/meanGen-0.1.0/setup.py
@@ -0,0 +1,19 @@
+import re
+from setuptools import setup, find_packages
+
+with open("README.rst", "rb") as f:
+	long_descr = f.read().decode("utf-8")
+
+setup(
+	name = "meanGen",
+	packages = find_packages(),
+	entry_points = {
+ 			"console_scripts": ["meanGen = src.meanGen:main"]
+		},
+	version = "0.1.0",
+	description = "",
+	long_description = long_descr,
+	author = "Oscar Vazquez",
+	author_email = "ovazquez@codingdojo.com",
+	url = "https://github.com/oscarvazquez/angularSkeleton"
+)
