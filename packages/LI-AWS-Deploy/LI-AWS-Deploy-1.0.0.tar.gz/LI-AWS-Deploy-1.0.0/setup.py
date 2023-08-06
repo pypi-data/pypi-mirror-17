48db9297d21021e12900bd694b8b24b116935e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:50 +0000

    Add package LI-AWS-Deploy/1.0.0/LI-AWS-Deploy-1.0.0.tar.gz

---
+++ packages/LI-AWS-Deploy/LI-AWS-Deploy-1.0.0.tar.gz/LI-AWS-Deploy-1.0.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+install_requires = [
+    'nose',
+    'coverage'
+]
+
+setup(
+    name='LI-AWS-Deploy',
+    version='1.0.0',
+    description='Deploy tool for Loja Integrada Web Applications',
+    long_description=long_description,
+    author='Chris Maillefaud',
+    # Choose your license
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.5'
+    ],
+    keywords='aws deploy',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=install_requires,
+    entry_points={
+        'lideploy': [
+            'deploy=run:start',
+        ],
+    },
+)
