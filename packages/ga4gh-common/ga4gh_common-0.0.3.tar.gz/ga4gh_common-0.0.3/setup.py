92ae9d233765dc75c587a98c40dd06cb3849905b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:56 +0000

    Add package ga4gh-common/0.0.3/ga4gh_common-0.0.3.tar.gz

---
+++ packages/ga4gh-common/ga4gh_common-0.0.3.tar.gz/ga4gh_common-0.0.3/setup.py
@@ -0,0 +1,18 @@
+# Don't import __future__ packages here; they make setup fail
+
+import ga4gh_common.setup as setup
+
+
+packageDict = {
+    "name": "ga4gh_common",
+    "description": "Common utilities for GA4GH packages",
+    "packages": ["ga4gh_common"],
+    "url": "https://github.com/ga4gh/ga4gh-common",
+    "use_scm_version": {"write_to": "ga4gh_common/_version.py"},
+    "entry_points": {
+        "console_scripts": [
+            "ga4gh_run_tests=ga4gh_common.run_tests:run_tests_main",
+        ],
+    },
+}
+setup.doSetup(packageDict)
