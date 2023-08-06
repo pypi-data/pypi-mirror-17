f3cd2b8c529f8b9fb5edfb0e8a9111422a7042ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:25 +0000

    Add package elavonvtpv/1.5.4/elavonvtpv-1.5.4.tar.gz

---
+++ packages/elavonvtpv/elavonvtpv-1.5.4.tar.gz/elavonvtpv-1.5.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='elavonvtpv',
+  packages=['elavonvtpv'],
+  version='1.5.4',
+  description='Python 3 package to comunicate with the Elavon Virtual TPV Remote API',
+  author='Javier Ros Honduvilla',
+  author_email='javier.ros.honduvilla@gmail.com',
+  url='https://github.com/jros99/elavontpv',
+  download_url='https://github.com/jros99/elavontpv/tarball/0.3',
+  keywords=['elavon', 'tpv', 'virtual tpv'],
+  classifiers=[],
+)
