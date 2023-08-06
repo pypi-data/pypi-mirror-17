5fafeecbae6df2d549b5985bd08608c5fa6259f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:53 +0000

    Add package barracks_sdk/0.0.8/barracks_sdk-0.0.8.tar.gz

---
+++ packages/barracks_sdk/barracks_sdk-0.0.8.tar.gz/barracks_sdk-0.0.8/barracks_sdk/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    This module provides the Barracks API implementation
+"""
+
+from apiresponse import ApiResponse
+from barrackshelper import BarracksHelper
+from packagedownloadhelper import PackageDownloadHelper
+from packageinfo import PackageInfo
+from updatecheckhelper import UpdateCheckHelper
+from updatedetail import UpdateDetail
+from updatedetailrequest import UpdateDetailRequest
+
+__all__ = ["UpdateDetail", "UpdateDetailRequest", "PackageDownloadHelper", "BarracksHelper", "ApiResponse",
+           "PackageInfo",
+           "UpdateCheckHelper"]
+
+__version__ = "0.0.8"
