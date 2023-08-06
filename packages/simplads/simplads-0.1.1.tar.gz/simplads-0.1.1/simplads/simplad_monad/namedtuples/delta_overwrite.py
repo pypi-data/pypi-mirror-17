f462419a5966c18b9e84099489cff06d565a80dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:39 +0000

    Add package simplads/0.1.1/simplads-0.1.1.tar.gz

---
+++ packages/simplads/simplads-0.1.1.tar.gz/simplads-0.1.1/simplads/simplad_monad/namedtuples/delta_overwrite.py
@@ -0,0 +1,3 @@
+from named_tup import NamedTup
+
+DeltaOverwrite = NamedTup('DeltaOverwrite', 'overwrite new_value')
---
+++ packages/simplads/simplads-0.1.1.tar.gz/simplads-0.1.1/simplads/simplad_monad/namedtuples/named_tup.py
@@ -0,0 +1,11 @@
+import collections
+
+def NamedTup(typename, field_names, default_values=()):
+    T = collections.namedtuple(typename, field_names)
+    T.__new__.__defaults__ = (None,) * len(T._fields)
+    if isinstance(default_values, collections.Mapping):
+        prototype = T(**default_values)
+    else:
+        prototype = T(*default_values)
+    T.__new__.__defaults__ = tuple(prototype)
+    return T
