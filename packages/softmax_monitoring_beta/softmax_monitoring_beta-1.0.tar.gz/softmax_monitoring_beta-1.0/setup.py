14473ddcbb481eb84572fc4f383d93a20665e35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:25 +0000

    Add package softmax_monitoring_beta/1.0/softmax_monitoring_beta-1.0.tar.gz

---
+++ packages/softmax_monitoring_beta/softmax_monitoring_beta-1.0.tar.gz/softmax_monitoring_beta-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='softmax_monitoring_beta',
+	packages=['softmax_monitoring'],
+	version='1.0',
+	description='Beta release of softmax monitoring software',
+	author='softmax',
+	author_email='softmax.vision@gmail.com',
+	url='https://github.com/softmax-vision/softmax_monitoring',
+	download_url='https://github.com/softmax-vision/softmax_monitoring/archive/1.0.tar.gz',
+	keywords=[],
+	classifiers=[]
+)
