f015cf35a8de70c7a7ca55e1f089d3e85a99a4c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:14 +0000

    Add package halimun/0.1.1/halimun-0.1.1.tar.gz

---
+++ packages/halimun/halimun-0.1.1.tar.gz/halimun-0.1.1/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='halimun',
+        version='0.1.1',
+        description='Python package',
+        author='PT. Inteligensi Risiko',
+        py_modules=['halimun']
+        )
