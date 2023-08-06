ee9bb76ce097f90dfa1bb8ccea31a75962780ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:38 +0000

    Add package my_movies_list_1001/1.1.1/my_movies_list_1001-1.1.1.zip

---
+++ packages/my_movies_list_1001/my_movies_list_1001-1.1.1.zip/my_movies_list_1001-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+               name                               = 'my_movies_list_1001',
+               version                            = '1.1.1',
+               py_modules                    = ['my_movies_List_1001'],
+               author                              = 'SmartRoshan',
+               author_email                  ='smartroshan@163.com',
+               url                                     = 'http://headfirstlabs.com',
+               description                      = 'A simple printer of nested lists',
+            )
