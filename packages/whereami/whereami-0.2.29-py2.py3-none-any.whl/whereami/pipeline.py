3430e3a65f6633afddca62bc6929d29466cf12ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:45 +0000

    Add package whereami/0.2.29/whereami-0.2.29-py2.py3-none-any.whl

---
+++ packages/whereami/whereami-0.2.29-py2.py3-none-any.whl/whereami/pipeline.py
@@ -0,0 +1,47 @@
+import pickle
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.pipeline import make_pipeline
+from sklearn.cross_validation import train_test_split
+from whereami.get_data import get_train_data
+from whereami.utils import get_model_file
+
+
+def get_pipeline():
+    return make_pipeline(DictVectorizer(sparse=False),
+                         RandomForestClassifier(n_estimators=100))
+
+
+def cross_validate_model(pipeline, X, y, n=100):
+    means = []
+    for _ in range(n):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        pipeline.fit(X_train, y_train)
+        means.append(np.mean(pipeline.predict(X_test) == y_test))
+    return np.mean(means)
+
+
+def train_model():
+    model_file = get_model_file()
+    X, y = get_train_data()
+    # fantastic: because using "quality" rather than "rssi", we expect values 0-150
+    # 0 essentially indicates no connection
+    # 150 is something like best possible connection
+    # Not observing a wifi will mean a value of 0, which is the perfect default.
+    lp = get_pipeline()
+    lp.fit(X, y)
+    with open(model_file, "wb") as f:
+        pickle.dump(lp, f)
+    return lp
+
+
+def get_model():
+    model_file = get_model_file()
+    try:
+        with open(model_file, "rb") as f:
+            lp = pickle.load(f)
+    except FileNotFoundError:
+        msg = "First learn a location, e.g. with `metadate learn home 10`."
+        raise FileNotFoundError(msg)
+    return lp
