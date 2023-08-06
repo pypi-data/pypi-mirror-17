afdbe101396f7d702230e80d1c4d75fba944b08b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:14 +0000

    Add package nester_jp/1.1.1/nester_jp-1.1.1.tar.gz

---
+++ packages/nester_jp/nester_jp-1.1.1.tar.gz/nester_jp-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name       ='nester_jp',
+	version    ='1.1.1',
+	py_modules =['nester'],
+	author     ='david',
+	author_email ='ajkdghkasdhfg',
+	url        ='http://www.baidu.com',
+	description ='this is a test',
+      )
