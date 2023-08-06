54a716d76f659d8eda8db02266a54feacc035a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:10 +0000

    Add package demotest/0.1/demotest-0.1.tar.gz

---
+++ packages/demotest/demotest-0.1.tar.gz/demotest-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'demotest',
+    packages = ['demotest'], 
+    version = '0.1',
+    description = 'a simple funciton to address the sequence',
+    author = 'shenglele',
+    author_email = 'shengliao128@gmail.com',
+    url = 'https://github.com/shenglele',
+    download_url = 'https://github.com/shenglele/Hello-World-',
+    keywords = ['function'], 
+    classifiers = [],  
+)
