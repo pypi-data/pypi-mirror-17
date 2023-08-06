4f6ff70727ffd01c7c1c884f582c7184d7f03d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:56 +0000

    Add package esgprep/2.5.4/esgprep-2.5.4.tar.gz

---
+++ packages/esgprep/esgprep-2.5.4.tar.gz/esgprep-2.5.4/esgprep/utils/constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    :platform: Unix
+    :synopsis: Constants used in this package.
+
+"""
+
+# Facets ignored during checking
+IGNORED_FACETS = ['root', 'project', 'filename', 'variable', 'version']
