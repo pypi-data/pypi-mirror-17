6fc989a85aa161a7aa017b175cdb9cd05f87284a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:53 +0000

    Add package widgetastic.core/0.1.15/widgetastic.core-0.1.15.tar.gz

---
+++ packages/widgetastic.core/widgetastic.core-0.1.15.tar.gz/widgetastic.core-0.1.15/src/widgetastic/log.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+import time
+from six import wraps
+
+from .exceptions import DoNotReadThisWidget
+
+
+null_logger = logging.getLogger('widgetastic_null')
+null_logger.addHandler(logging.NullHandler())
+
+
+class PrependParentsAdapter(logging.LoggerAdapter):
+    """This class ensures the path to the widget is represented in the log records."""
+    def process(self, msg, kwargs):
+        return '[{}]: {}'.format(self.extra['widget_path'], msg), kwargs
+
+
+def create_widget_logger(widget_path, logger=None):
+    """Create a logger that prepends the ``widget_path`` to the log records."""
+    return PrependParentsAdapter(
+        logger or null_logger,
+        {'widget_path': widget_path})
+
+
+def logged(log_args=False, log_result=False):
+    def g(f):
+        @wraps(f)
+        def wrapped(self, *args, **kwargs):
+            start_time = time.time()
+            if log_args:
+                signature = '{}{!r}{!r}'.format(f.__name__, args, kwargs)
+            else:
+                signature = f.__name__
+            self.logger.debug('%s started', signature)
+            try:
+                result = f(self, *args, **kwargs)
+            except DoNotReadThisWidget:
+                elapsed_time = (time.time() - start_time) * 1000.0
+                self.logger.info(
+                    '%s not read on widget\'s request (elapsed %.0f ms)', signature, elapsed_time)
+                raise
+            else:
+                elapsed_time = (time.time() - start_time) * 1000.0
+                if log_result:
+                    self.logger.info('%s -> %r (elapsed %.0f ms)', signature, result, elapsed_time)
+                else:
+                    self.logger.info('%s (elapsed %.0f ms)', signature, elapsed_time)
+                return result
+
+        return wrapped
+
+    return g
