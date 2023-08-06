ee3de5f37e926fbcb315e5260aceff60078a5c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:58 +0000

    Add package class_a/1.1.0.0/class_a-1.1.0.0.zip

---
+++ packages/class_a/class_a-1.1.0.0.zip/class_a-1.1.0.0/nester.py
@@ -0,0 +1,10 @@
+"""this is a function that I made"""
+def print_lol(the_list,level):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(each_item)
+            
---
+++ packages/class_a/class_a-1.1.0.0.zip/class_a-1.1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='class_a',
+    version='1.1.0.0',
+    py_modules=['nester'],
+    author="kangshaoshun",    
+    )
