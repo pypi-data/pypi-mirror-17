2ce2cc1cf01242bab799a4a95c7576f67cd9a2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:33 +0000

    Add package test-h-project/0.0.1/test-h-project-0.0.1.tar.gz

---
+++ packages/test-h-project/test-h-project-0.0.1.tar.gz/test-h-project-0.0.1/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name="test-h-project",
+	version="0.0.1",
+	description="Learning to do pip",
+	url="",
+	author="Hemang Kumar",
+	author_email="hemangsk@gmail.com",
+	license='MIT',
+	packages=["test-h-project"],
+	scripts=["bin/test-h-project"],
+	zip_safe=False)
