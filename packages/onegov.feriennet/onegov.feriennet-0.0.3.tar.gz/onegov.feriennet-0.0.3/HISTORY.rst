c471dadaf9ad8e4c7dffe7f502cf4541032fbeb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:44 +0000

    Add package onegov.feriennet/0.0.3/onegov.feriennet-0.0.3.tar.gz

---
+++ packages/onegov.feriennet/onegov.feriennet-0.0.3.tar.gz/onegov.feriennet-0.0.3/HISTORY.rst
@@ -0,0 +1,17 @@
+Changelog
+---------
+
+0.0.3 (2016-09-22)
+~~~~~~~~~~~~~~~~~~~
+
+0.0.2 (2016-09-13)
+~~~~~~~~~~~~~~~~~~~
+
+- Adds login/registration buttons to default homepage.
+  [href]
+
+0.0.1 (2016-09-13)
+~~~~~~~~~~~~~~~~~~~
+
+- Initial Release.
+  [href]
---
+++ packages/onegov.feriennet/onegov.feriennet-0.0.3.tar.gz/onegov.feriennet-0.0.3/onegov/feriennet/collections/activity.py
@@ -0,0 +1,20 @@
+from onegov.activity import ActivityCollection
+from onegov.feriennet.security import ActivityQueryPolicy
+
+
+class VacationActivityCollection(ActivityCollection):
+
+    def __init__(self, session, identity=None, page=0):
+        super().__init__(session, type='vacation', page=page)
+        self.identity = identity
+
+    def subset(self):
+        """ Limits the pagination results to activites visible by the
+        current user.
+
+        """
+        policy = ActivityQueryPolicy.for_identity(self.identity)
+        return policy.granted_subset(self.query())
+
+    def page_by_index(self, index):
+        return self.__class__(self.session, self.identity, page=index)
