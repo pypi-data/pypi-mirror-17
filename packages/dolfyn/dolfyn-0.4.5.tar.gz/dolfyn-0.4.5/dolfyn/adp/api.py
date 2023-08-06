baaa96ec1f6b4e3c7d23c70f003b9ea2af6e952b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:08 +0000

    Add package dolfyn/0.4.5/dolfyn-0.4.5.tar.gz

---
+++ packages/dolfyn/dolfyn-0.4.5.tar.gz/dolfyn-0.4.5/dolfyn/adp/api.py
@@ -0,0 +1,18 @@
+from base import type_map, dio
+from _readbin import adcp_loader
+
+
+def load(fname, data_groups=None):
+    with dio.loader(fname, type_map) as ldr:
+        return ldr.load(data_groups)
+
+
+def mmload(fname, data_groups=None):
+    with dio.loader(fname, type_map) as ldr:
+        return ldr.mmload(data_groups)
+
+
+def read_rdi(fname):
+    with adcp_loader(fname) as ldr:
+        dat = ldr.load_data()
+    return dat
