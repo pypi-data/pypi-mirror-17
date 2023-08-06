42f89d1a46d1a5f42be90e9dfe52cfbb1645ee17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:47 +0000

    Add package kivyoav/0.4/kivyoav-0.4.zip

---
+++ packages/kivyoav/kivyoav-0.4.zip/kivyoav-0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'kivyoav',
+  packages = ['kivyoav'],
+  version = '0.4',
+  description = 'A set of kivy utils and widgets - more details on github ...',
+  author = 'Yoav Glazner',
+  author_email = 'yoavglazner@gmail.com',
+  url = 'https://github.com/yglazner/kivyoav', 
+  download_url = 'https://github.com/yglazner/kivyoav/tarball/0.4', 
+  keywords = ['kivy', 'widget', 'ui'], # arbitrary keywords
+  classifiers = [],
+)
