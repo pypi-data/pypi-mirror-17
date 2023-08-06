fc7faf4bd1beed836eeff905fc2dd54cfb7fa011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:46 +0000

    Add package Rtest_sanitize/1.0.0/Rtest_sanitize-1.0.0.zip

---
+++ packages/Rtest_sanitize/Rtest_sanitize-1.0.0.zip/Rtest_sanitize-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            =   'Rtest_sanitize',
+    version         =   '1.0.0',
+    py_modules      =   ['Rtest_sanitize'],
+    author          =   'Ryan',
+    author_email    =   'wurunxin91@gamil.com',
+    url             =   'http://wurunxin91@gamil.com',
+    description     =   'This is just for test',
+)
