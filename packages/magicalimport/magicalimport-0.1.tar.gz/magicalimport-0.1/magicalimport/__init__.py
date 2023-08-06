cfa2e4f050cddc43dad29abb4ac4dfe5a99141ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:55 +0000

    Add package magicalimport/0.1/magicalimport-0.1.tar.gz

---
+++ packages/magicalimport/magicalimport-0.1.tar.gz/magicalimport-0.1/magicalimport/__init__.py
@@ -0,0 +1,20 @@
+from importlib import machinery
+import sys
+
+
+def expose_all_members(module, globals_=None, _depth=2):
+    members = {k: v for k, v in module.__dict__.items() if not k.startswith("_")}
+    return expose_members(module, members, globals_=globals_, _depth=_depth)
+
+
+def expose_members(module, members, globals_=None, _depth=1):
+    if globals_ is None:
+        frame = sys._getframe(_depth)
+        globals_ = frame.f_globals
+    globals_.update({k: module.__dict__[k] for k in members})
+    return globals_
+
+
+def import_from_physical_path(path, as_=None):
+    module_id = as_ or path.replace("/", "_").rstrip(".py")
+    return machinery.SourceFileLoader(module_id, path).load_module()
