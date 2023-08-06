b0012c118340a63c145841f48f8f2de9fc11bb78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:33 +0000

    Add package toga-cocoa/0.2.2/toga_cocoa-0.2.2-py3-none-any.whl

---
+++ packages/toga-cocoa/toga_cocoa-0.2.2-py3-none-any.whl/toga_cocoa/widgets/container.py
@@ -0,0 +1,37 @@
+from rubicon.objc import *
+
+from toga.interface import Container as ContainerInterface
+
+from ..libs import *
+from .base import WidgetMixin
+
+
+class TogaContainer(NSView):
+    @objc_method
+    def isFlipped(self) -> bool:
+        # Default Cocoa coordinate frame is around the wrong way.
+        return True
+
+    @objc_method
+    def display(self) -> None:
+        self.layer.setNeedsDisplay_(True)
+        self.layer.displayIfNeeded()
+
+
+class Container(ContainerInterface, WidgetMixin):
+    def __init__(self, id=id, style=None, children=None):
+        super().__init__(id=id, style=style, children=None)
+        self.startup()
+
+        if children:
+            for child in children:
+                self.add(child)
+
+    def startup(self):
+        self._impl = TogaContainer.alloc().init()
+        self._impl._interface = self
+
+        # self._impl.setWantsLayer_(True)
+        # self._impl.setBackgroundColor_(NSColor.blueColor())
+
+        self._add_constraints()
