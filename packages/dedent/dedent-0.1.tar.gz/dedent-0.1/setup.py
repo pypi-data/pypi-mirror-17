211bf3560278a370d4731af6971b843703c3b787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:08 +0000

    Add package dedent/0.1/dedent-0.1.tar.gz

---
+++ packages/dedent/dedent-0.1.tar.gz/dedent-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dedent',
+      version='0.1',
+      description='Write easy inline code as it should be',
+      url='http://github.com/ctismer/dedent',
+      author='Christian Tismer',
+      author_email='tismer@stackless.com',
+      license='MIT',
+      packages=['dedent'],
+      zip_safe=False)
