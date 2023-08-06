e84b0aca9c366a9aadd399c8e77df04738dbeb05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:11 +0000

    Add package SPLAT-library/0.1.9/SPLAT-library-0.1.9.tar.gz

---
+++ packages/SPLAT-library/SPLAT-library-0.1.9.tar.gz/SPLAT-library-0.1.9/splat/complexity/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+########################################################################################################################
+##### INFORMATION ######################################################################################################
+### @PROJECT_NAME:		SPLAT: Speech Processing and Linguistic Analysis Tool										 ###
+### @VERSION_NUMBER:																								 ###
+### @PROJECT_SITE:		github.com/meyersbs/SPLAT																     ###
+### @AUTHOR_NAME:		Benjamin S. Meyers																			 ###
+### @CONTACT_EMAIL:		ben@splat-library.org																		 ###
+### @LICENSE_TYPE:		MIT																							 ###
+########################################################################################################################
+########################################################################################################################
+
+"""
+This package contains the following files:
+	[01] Util.py
+			A collection of functions for calculating various complexity metrics.
+"""
