d081d8379c63bea18c886ec49132d3980204f381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:53 +0000

    Add package medved/2016.10.20/medved-2016.10.20.tar.gz

---
+++ packages/medved/medved-2016.10.20.tar.gz/medved-2016.10.20/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='medved',
+    version='2016.10.20',
+    description='Modulation Enhanced Diffraction Viewer and EDitor',
+    author='Vadim Dyadkin',
+    author_email='dyadkin@gmail.com',
+    url='https://hg.3lp.cx/snbltb',
+    license='GPLv3',
+    long_description='Do not forget to cite: https://doi.org/10.1107/S2053273316008378',
+    install_requires=[
+        'numpy>=1.9',
+        'scipy>=0.10.0',
+        'fortranformat>=0.2.5',
+        'pyqtgraph-for-dubble-bubble>=2016.10.12',
+    ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'medved=medved.__init__:main',
+        ],
+    },
+    py_modules=[
+        'medved.ui.__init__',
+        'medved.ui.resources_rc',
+        'medved.ui.ui_sliders',
+        'medved.ui.ui_wmedeval',
+        'medved.__init__',
+        'medved.controller',
+        'medved.filereader',
+        'medved.imageview',
+        'medved.model',
+        'medved.plotwidget',
+        'medved.wmedeval',
+        'medved.wsliders',
+    ],
+)
