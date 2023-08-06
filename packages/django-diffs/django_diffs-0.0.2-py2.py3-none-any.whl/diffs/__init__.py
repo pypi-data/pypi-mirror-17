c6ceb6c739f268016340c51ad7fa4347ff946429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:51 +0000

    Add package django-diffs/0.0.2/django_diffs-0.0.2-py2.py3-none-any.whl

---
+++ packages/django-diffs/django_diffs-0.0.2-py2.py3-none-any.whl/diffs/__init__.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import, unicode_literals
+
+__version__ = '0.0.2'
+default_app_config = 'diffs.apps.DiffLogConfig'
+
+klasses_to_connect = []
+
+
+def register(klass):
+    """
+    Decorator function that registers a class to record diffs.
+
+    @register
+    class ExampleModel(models.Model):
+        ...
+    """
+    from django.apps import apps as django_apps
+    from dirtyfields import DirtyFieldsMixin
+
+    from .models import DiffLogEntryManager
+    from .signals import connect
+    # Hack to add dirtyfieldsmixin automatically
+    if DirtyFieldsMixin not in klass.__bases__:
+        klass.__bases__ = (DirtyFieldsMixin,) + klass.__bases__
+
+    klass.add_to_class('diffs', DiffLogEntryManager())
+
+    if not django_apps.ready:
+        klasses_to_connect.append(klass)
+    else:
+        connect(klass)
+
+    return klass
