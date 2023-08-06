2568d6eaf560318b719750005b6e769ee1c6271d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:56 +0000

    Add package bluetool/0.1.7/bluetool-0.1.7.tar.gz

---
+++ packages/bluetool/bluetool-0.1.7.tar.gz/bluetool-0.1.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='bluetool',
+    version='0.1.7',
+    license='GPL',
+    author='Aleksandr Aleksandrov',
+    author_email='aleksandr.aleksandrov@emlid.com',
+    url='https://github.com/emlid/bluetooth-dbus',
+    packages=['bluetool']
+)
