a1f9f0d714ffee223fb867061f7722b9d6d4b419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:17 +0000

    Add package umodules/0.5.10/umodules-0.5.10.win-amd64.zip

---
+++ packages/umodules/umodules-0.5.10.win-amd64.zip/Development/Workspace/Python/uModules/env/Scripts/umodules-script.py
@@ -0,0 +1,10 @@
+#!C:\Development\Workspace\Python\uModules\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'umodules==0.5.10','console_scripts','umodules'
+__requires__ = 'umodules==0.5.10'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('umodules==0.5.10', 'console_scripts', 'umodules')()
+    )
