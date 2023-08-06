d82fd9121032f18d6add8325792f2f787b998ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:10 +0000

    Add package pydgutils/0.0.11/pydgutils-0.0.11.tar.gz

---
+++ packages/pydgutils/pydgutils-0.0.11.tar.gz/pydgutils-0.0.11/pydgutils/version.py
@@ -0,0 +1,31 @@
+from collections import namedtuple
+
+# Release levels
+RL_ALPHA = 'alpha'
+RL_BETA = 'beta'
+RL_CANDIDATE = 'candidate'
+RL_FINAL = 'final'
+
+def version_info_to_str(version_info):
+    result = ".".join([str(a) for a in version_info[:3]])
+
+    # Release level abbreviations
+    # Reference to https://www.python.org/dev/peps/pep-0440/
+    rl_abbreviations = {RL_ALPHA:'a', RL_BETA:'b', RL_CANDIDATE:'rc', RL_FINAL:''}
+    result += rl_abbreviations[version_info.releaselevel]
+    if version_info.releaselevel != RL_FINAL:
+        result += str(version_info.serial)
+    return result
+
+def read_version_file(base_dir, package_name):
+    import os.path
+
+    package_name = package_name.replace("-", "_").replace(".", "/")
+    version_file_path = os.path.join(base_dir, package_name, "version.py")
+    with open(version_file_path) as f:
+        return f.read()
+
+# Version info structure just like python3's sys.version_info
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+version_info = VersionInfo(0, 0, 11, RL_FINAL, 0)
+__version__ = version_info_to_str(version_info)
