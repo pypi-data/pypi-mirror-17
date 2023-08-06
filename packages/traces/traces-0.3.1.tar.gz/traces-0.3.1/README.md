c8c80db2bf10d11628e4ca0695b22d9a518425f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:54 +0000

    Add package traces/0.3.1/traces-0.3.1.tar.gz

---
+++ packages/traces/traces-0.3.1.tar.gz/traces-0.3.1/README.md
@@ -0,0 +1,8 @@
+traces
+======
+
+[![Version](https://img.shields.io/pypi/v/traces.svg?)](https://pypi.python.org/pypi/traces) [![Build Status](https://circleci.com/gh/datascopeanalytics/traces/tree/master.svg?style=shield&circle-token=:circle-token)](https://circleci.com/gh/datascopeanalytics/traces) [![Documentation Status](https://readthedocs.org/projects/traces/badge/?version=latest)](https://traces.readthedocs.io/en/latest/?badge=latest) [![Updates](https://pyup.io/repos/github/datascopeanalytics/traces/shield.svg)](https://pyup.io/repos/github/datascopeanalytics/traces/)
+
+Traces is a Python library for unevenly-spaced time series
+analysis. To install it, run `pip install traces`. See the
+documentation [here](https://traces.readthedocs.io).
---
+++ packages/traces/traces-0.3.1.tar.gz/traces-0.3.1/tests/test_traces.py
@@ -0,0 +1,47 @@
+from traces import TimeSeries
+import nose
+
+
+def test_init_data():
+    ts = TimeSeries([(1, 2), (2, 3), (6, 1), (8, 4)])
+
+    assert ts[0] == 2
+    assert ts[1] == 2
+    assert ts[1.5] == 2
+    assert ts[6] == 1
+    assert ts[7] == 1
+    assert ts[8] == 4
+    assert ts[10] == 4
+
+    ts = TimeSeries([[1, 2], [2, 3], [6, 1], [8, 4]])
+
+    assert ts[0] == 2
+    assert ts[1] == 2
+    assert ts[1.5] == 2
+    assert ts[6] == 1
+    assert ts[7] == 1
+    assert ts[8] == 4
+    assert ts[10] == 4
+
+    ts = TimeSeries({1: 2, 2: 3, 6: 1, 8: 4})
+
+    assert ts[0] == 2
+    assert ts[1] == 2
+    assert ts[1.5] == 2
+    assert ts[6] == 1
+    assert ts[7] == 1
+    assert ts[8] == 4
+    assert ts[10] == 4
+
+
+def test_get():
+    ts = TimeSeries()
+    nose.tools.assert_raises(KeyError, ts.get, 0)
+
+    ts[1.2] = 1
+    ts[3] = 0
+    ts[6] = 2
+
+    assert ts[0] == 1
+    assert ts[5.5] == 0
+    assert ts[7] == 2
