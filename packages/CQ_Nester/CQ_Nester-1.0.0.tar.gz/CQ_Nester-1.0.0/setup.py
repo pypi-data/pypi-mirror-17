fe0411bba5e9f8209e4b498cd2d39f4e9c5a52cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:55 +0000

    Add package CQ_Nester/1.0.0/CQ_Nester-1.0.0.tar.gz

---
+++ packages/CQ_Nester/CQ_Nester-1.0.0.tar.gz/CQ_Nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name='CQ_Nester',
+version='1.0.0',
+py_modules=['CQ_Nester'],
+author='fumier',
+author_email='fumier@163.com',
+url='http://blog.csdn.net/fumier',
+description='A simple printer of nested lists',
+)
