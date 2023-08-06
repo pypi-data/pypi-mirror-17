d9cee3bce4457d78bf7cece60e848a2941e31a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:30 +0000

    Add package slotter/0.0.4/slotter-0.0.4.tar.gz

---
+++ packages/slotter/slotter-0.0.4.tar.gz/slotter-0.0.4/slotter/__init__.py
@@ -0,0 +1,15 @@
+"""
+Slotter
+
+Slotter is used to slot elements in buckets
+"""
+
+__title__ = 'slotter'
+__version__ = '0.0.1'
+__author__ = 'Saurabh Hirani'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Saurabh Hirani'
+
+from .slotter import Slotter
+
+create = Slotter
