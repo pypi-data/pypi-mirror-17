52c86477ef0527beb10a610e89df80955f4155b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:31 +0000

    Add package SPLAT-library/0.2.7/SPLAT-library-0.2.7.tar.gz

---
+++ packages/SPLAT-library/SPLAT-library-0.2.7.tar.gz/SPLAT-library-0.2.7/splat/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+########################################################################################################################
+##### INFORMATION ######################################################################################################
+### @PROJECT_NAME:		SPLAT: Speech Processing and Linguistic Analysis Tool										 ###
+### @VERSION_NUMBER:																								 ###
+### @PROJECT_SITE:		github.com/meyersbs/SPLAT																     ###
+### @AUTHOR_NAME:		Benjamin S. Meyers																			 ###
+### @CONTACT_EMAIL:		ben@splat-library.org																		 ###
+### @LICENSE_TYPE:		MIT																							 ###
+########################################################################################################################
+########################################################################################################################
+
+try:
+    import nltk
+    from splat import SPLAT
+    nltk.download("stopwords")
+    nltk.download("names")
+    nltk.download("punkt")
+    nltk.download("averaged_perceptron_tagger")
+except:
+    print(
+        "WARNING: Essential NLTK data could not be installed. Please run the following:\n python3 -m nltk.downloader stopwords names punkt averaged_perceptron_tagger")
