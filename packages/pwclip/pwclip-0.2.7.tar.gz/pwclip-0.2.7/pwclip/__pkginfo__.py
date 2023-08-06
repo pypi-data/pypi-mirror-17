694ab218f17ba6b485d02a763f74079f81d00bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:27 +0000

    Add package pwclip/0.2.7/pwclip-0.2.7.tar.gz

---
+++ packages/pwclip/pwclip-0.2.7.tar.gz/pwclip-0.2.7/pwclip/__pkginfo__.py
@@ -0,0 +1,43 @@
+"""pwclip packaging information"""
+import sys
+from os import getcwd
+from os.path import join, dirname
+
+modname = distname = 'pwclip'
+numversion = (0, 2, 7)
+version = '.'.join([str(num) for num in numversion])
+install_requires = ['pyusb', 'yubico', 'tkinter', 'gnupg']
+if sys.version_info[:2] > (2, 7):
+	install_requires = ['pyusb', 'yubico', 'gnupg']
+license = 'GPL'
+description = "gui to temporarily save passwords to clipboard (paste-buffer)"
+web = 'http://janeiskla.de'
+mailinglist = ""
+author = 'Leon Pelzer'
+author_email = 'mail@leonpelzer.de'
+classifiers = ['Development Status :: 4 - Beta',
+               'Environment :: Console',
+               'Environment :: MacOS X',
+               'Environment :: Win32 (MS Windows)',
+               'Environment :: X11 Applications',
+               'Intended Audience :: Developers',
+               'Intended Audience :: End Users/Desktop',
+               'Intended Audience :: System Administrators',
+               'Intended Audience :: Information Technology',
+               'License :: OSI Approved :: GNU General Public License (GPL)',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python',
+               'Programming Language :: Python :: 3',
+               'Topic :: Security',
+               'Topic :: Utilities',
+               'Topic :: Desktop Environment',
+               'Topic :: System :: Systems Administration']
+
+with open(join(getcwd(), 'README'), 'r') as rfh:
+	readme = rfh.read()
+
+long_desc = ( readme )
+
+scripts = [join('bin', 'pwclip')]
+
+entry_points = {'console_scripts': ['pwclip = pwclip.__init__:pwclipper']}
