22f74be46a7ed245bc368670a2e8b7e8708f6add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:18 +0000

    Add package cfpa/1.0.0/cfpa-1.0.0.zip

---
+++ packages/cfpa/cfpa-1.0.0.zip/cfpa-1.0.0/setup.py
@@ -0,0 +1,11 @@
+﻿from distutils.core import setup
+
+setup(
+         name = 'cfpa',
+         version = '1.0.0',
+         py_modules = ['cfpa'],
+         author = 'hfpython',
+         author_email = 'vip@vip.com',
+         url = 'http://www.python.com',
+         description = 'test',
+          )
