303f71d0013920ae309a0c4d11320e49ce23fd90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:32 +0000

    Add package boolrule/0.1.0/boolrule-0.1.0.tar.gz

---
+++ packages/boolrule/boolrule-0.1.0.tar.gz/boolrule-0.1.0/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'pyparsing'
+]
+
+test_requirements = [
+]
+
+setup(
+    name='boolrule',
+    version='0.1.0',
+    description="Simple boolean expression evaluation engine",
+    long_description=readme + '\n\n' + history,
+    author="Steve Webster",
+    author_email='spjwebster@gmail.com',
+    url='https://github.com/tailsdotcom/boolrule',
+    packages=[
+        'boolrule',
+    ],
+    package_dir={'boolrule':
+                 'boolrule'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='boolrule boolean expression',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='pytest',
+    tests_require=test_requirements
+)
