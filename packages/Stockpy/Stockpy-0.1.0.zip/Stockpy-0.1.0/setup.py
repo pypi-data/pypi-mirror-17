66fd6847cd4506058fca018e8b669c4fa707a6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:23 +0000

    Add package Stockpy/0.1.0/Stockpy-0.1.0.zip

---
+++ packages/Stockpy/Stockpy-0.1.0.zip/Stockpy-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name = 'Stockpy',
+    version = '0.1.0',
+    description = 'A Python package to retrieve stock data as well as Stock Symbols for the Indian Stock Market.',
+    author = 'Aadhar',
+    author_email = 'sachdevaaadhar@gmail.com',
+    url = 'https://github.com/Aadhar96/Stockpy', 
+    keywords= "market finance ticker stock stocks NSE BSE ".split(),
+    py_modules=['Stockpy'],
+    install_requires=[
+        # list of this package dependencies
+        "requests >= 2.2.1",
+        "pandas >= 0.16.1",
+    ],
+)
