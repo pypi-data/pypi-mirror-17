6c722e2cbf9cda21145fae704ec1883f34bb3d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:15 +0000

    Add package ATAX/1.1.3/ATAX-1.1.3-py2.py3-none-any.whl

---
+++ packages/ATAX/ATAX-1.1.3-py2.py3-none-any.whl/_ATAX/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2006-2016 Mag. Christian Tanzer. All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+# ****************************************************************************
+# This module is part of the package ATAX.
+#
+# This module is licensed under the terms of the BSD 3-Clause License
+# <http://www.c-tanzer.at/license/bsd_3c.html>.
+# ****************************************************************************
+#
+#++
+# Name
+#    ATAX.__init__
+#
+# Purpose
+#    Initialize package `ATAX`
+#
+# Revision Dates
+#    11-Feb-2006 (CT) Creation
+#    11-Oct-2016 (CT) Add `__version__`
+#    ««revision-date»»···
+#--
+
+from _TFL.Package_Namespace import Package_Namespace
+
+__version__ = "1.1.3"
+
+ATAX = Package_Namespace ()
+
+del Package_Namespace
+
+### __END__ ATAX.__init__
