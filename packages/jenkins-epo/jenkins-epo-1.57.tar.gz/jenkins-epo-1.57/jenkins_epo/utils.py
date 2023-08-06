5c327a61986eba1f59ca02abb98440a220f84be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:12 +0000

    Add package jenkins-epo/1.57/jenkins-epo-1.57.tar.gz

---
+++ packages/jenkins-epo/jenkins-epo-1.57.tar.gz/jenkins-epo-1.57/jenkins_epo/utils.py
@@ -0,0 +1,79 @@
+# This file is part of jenkins-epo
+#
+# jenkins-epo is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or any later version.
+#
+# jenkins-epo is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# jenkins-epo.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import
+
+import datetime
+import fnmatch
+import logging
+
+import retrying
+
+
+logger = logging.getLogger(__name__)
+
+
+def retry(*dargs, **dkw):
+    from .github import retry_filter
+    defaults = dict(
+        retry_on_exception=retry_filter,
+        wait_exponential_multiplier=500,
+        wait_exponential_max=15000,
+    )
+
+    if len(dargs) == 1 and callable(dargs[0]):
+        return retrying.retry(**defaults)(dargs[0])
+    else:
+        dkw = dict(defaults, **dkw)
+        return retrying.retry(*dargs, **dkw)
+
+
+def match(item, patterns):
+    matched = not patterns
+    for pattern in patterns:
+        negate = False
+        if pattern.startswith('-') or pattern.startswith('!'):
+            negate = True
+            pattern = pattern[1:]
+        if pattern.startswith('+'):
+            pattern = pattern[1:]
+
+        local_matched = fnmatch.fnmatch(item, pattern)
+        if negate:
+            matched = matched and not local_matched
+        else:
+            matched = matched or local_matched
+
+    return matched
+
+
+def parse_datetime(formatted):
+    return datetime.datetime.strptime(
+        formatted, '%Y-%m-%dT%H:%M:%SZ'
+    )
+
+
+def parse_patterns(raw):
+    return [p for p in str(raw).split(',') if p]
+
+
+class Bunch(dict):
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
