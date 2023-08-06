99d330b8359082283cc9d2febf3935a60561db73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:58 +0000

    Add package funniesttest/1.0/funniesttest-1.0.tar.gz

---
+++ packages/funniesttest/funniesttest-1.0.tar.gz/funniesttest-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='funniesttest',
+	packages=['funniest'],
+	version='1.0',
+	description='test',
+	author='softmax',
+	author_email='softmax.vision@gmail.com',
+	url='https://github.com/softmax-vision/funnytest',
+	download_url='https://github.com/softmax-vision/funnytest/archive/1.0.tar.gz',
+	keywords=['tag1','tag2'],
+	classifiers=[]
+)
