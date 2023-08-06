06ff875dc11dca40ce943035a1e9fb641ced396b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:38 +0000

    Add package nester_slayer/1.3.0/nester_slayer-1.3.0.tar.gz

---
+++ packages/nester_slayer/nester_slayer-1.3.0.tar.gz/nester_slayer-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'nester_slayer',
+        version     = '1.3.0',
+        py_modules  = ['nester'],
+        author      = 'slayer',
+        author_email= 'slayer@web.de',
+        url         = '',
+        description = 'A simple printer of nested lists',
+      )
