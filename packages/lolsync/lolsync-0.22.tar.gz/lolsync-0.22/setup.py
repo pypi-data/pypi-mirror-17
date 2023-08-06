34b96d2e05f1c0976b8eeaeea99230aa1ae2367e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:57 +0000

    Add package lolsync/0.22/lolsync-0.22.tar.gz

---
+++ packages/lolsync/lolsync-0.22.tar.gz/lolsync-0.22/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+setup(
+  name = 'lolsync',
+  packages = ['lol_sync'],
+  version = '0.22',
+  description = 'Keep in sync with friends on League of Legends!',
+  author = 'Jason Lin',
+  author_email = 'jason_lin2@yahoo.com',
+  license = 'MIT',
+  url = 'https://github.com/jason2249/LoLSync',
+  download_url = 'https://github.com/jason2249/lolsync/tarball/0.22',
+  keywords = ['League of Legends', 'Friend', 'Sync'],
+  classifiers = [
+    'Operating System :: MacOS',
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.4'
+  ],
+  py_modules=['lol_sync.py'],
+  scripts=['lolsync'],
+  install_requires = [
+    'blessings>=1.6',
+    'requests>=2.10.0'
+  ]
+)
