ac47904018d867c344218464ea2404fc86a89ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:29 +0000

    Add package django-slack-oauth/0.6/django_slack_oauth-0.6-py2-none-any.whl

---
+++ packages/django-slack-oauth/django_slack_oauth-0.6-py2-none-any.whl/django_slack_oauth/models.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
+from django.db import models
+
+from jsonfield import JSONField
+
+__all__ = (
+    'SlackUser',
+)
+
+
+class SlackUser(models.Model):
+    slacker = models.OneToOneField(settings.AUTH_USER_MODEL, primary_key=True, related_name='slack_user')
+    access_token = models.CharField(max_length=128, null=True)
+    extras = JSONField(null=True)
+
+    def is_slacked(self):
+        return self.access_token
+
+    def __unicode__(self):
+        return unicode(self.slacker)
+
+    class Meta:
+        db_table = 'slack_user'
