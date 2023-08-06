4d524b5cf4579846733e4b382e935593fa2443f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:43 +0000

    Add package Flutterwave/1.0.4/Flutterwave-1.0.4.tar.gz

---
+++ packages/Flutterwave/Flutterwave-1.0.4.tar.gz/Flutterwave-1.0.4/tests/test_accounts.py
@@ -0,0 +1,95 @@
+from flutterwave import Flutterwave
+import time
+import unittest
+import json
+import pprint
+
+class TestAccounts(unittest.TestCase):
+
+    global flw
+    global ref
+    global amount
+    global narration
+    global debitAccount
+    global transactionReference
+    global otp
+    global token
+    global country
+    global bankcode
+
+    flw = Flutterwave("tk_NabYp2XjZ6G9WwdFruzK", "tk_tdyrSMQo8a", {"debug": True})
+
+    ref = "{}{}".format("12345ref", time.time())[0:18]
+    amount = "100"
+    narration = "sample purchase"
+    debitAccount = "0690000000"
+    otp = "12345"
+    country = "NG"
+    bankcode = "044"
+
+
+    def test1AccountTokenize(self):
+        print "\n---------###-- Flutterwave Account Tokenize --###------------"
+        r = flw.account.tokenize(debitAccount, country)
+        d = json.loads(r.text)
+
+        global transactionReference
+        transactionReference = d["data"]["transactionReference"]
+
+        # self.assertEqual(d["data"]["responseCode"], "00")
+        print "{}".format(r.text)
+        
+
+    def test2AccountValidate(self):
+        print "\n---------###-- Flutterwave Account Tokenize Validate --###------------"
+        data = {
+            "amount": amount,
+            "ref": transactionReference,
+            "otp": otp,
+            "accountNumber": debitAccount,
+            "narration": narration,
+            "country": country
+        }
+        r = flw.account.validate(data)
+        d = json.loads(r.text)
+
+        global token
+        token = d["data"]["accountToken"]
+        
+        # self.assertEqual(d["data"]["responsecode"], "00")
+        print "{}".format(r.text)
+
+
+    def test3AccountCharge(self):
+        print "\n---------###-- Flutterwave Account Charge --###------------"
+        data = {
+            "amount": amount,
+            "token": token,
+            "narration": narration,
+            "country": country
+        }
+
+        r = flw.account.charge(data)
+        d = json.loads(r.text)
+        
+        # self.assertEqual(d["data"]["responseCode"], "00")
+        print "{}".format(r.text)
+
+
+    def test4AccountLookup(self):
+        print "\n---------###-- Flutterwave Account Lookup --###------------"
+        data = {
+            "destBankCode": bankcode,
+            "recipientAccount": debitAccount,
+            "country": country
+        }
+
+        r = flw.account.lookup(data)
+        d = json.loads(r.text)
+        
+        # self.assertEqual(d["data"]["responseCode"], "00")
+        print "{}".format(r.text)
+
+
+if __name__ == '__main__':
+    unittest.main()
