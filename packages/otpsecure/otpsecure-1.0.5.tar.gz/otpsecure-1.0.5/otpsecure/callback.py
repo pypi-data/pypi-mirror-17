989eebeb9b366e1f55dec6259365f5b453db6224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:40 +0000

    Add package otpsecure/1.0.5/otpsecure-1.0.5.tar.gz

---
+++ packages/otpsecure/otpsecure-1.0.5.tar.gz/otpsecure-1.0.5/otpsecure/callback.py
@@ -0,0 +1,9 @@
+from otpsecure.base import Base
+
+class Callback(Base):
+  def __init__(self):
+    self.id             = None
+    self.cid            = None
+    self.status 		= None
+    self.filename	    = None
+    self.filecontent    = None
