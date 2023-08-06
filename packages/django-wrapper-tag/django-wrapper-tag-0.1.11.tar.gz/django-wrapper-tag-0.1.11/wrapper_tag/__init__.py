2dbd993a384741a05d6241bb2438222a0a15c897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:37 +0000

    Add package django-wrapper-tag/0.1.11/django-wrapper-tag-0.1.11.tar.gz

---
+++ packages/django-wrapper-tag/django-wrapper-tag-0.1.11.tar.gz/django-wrapper-tag-0.1.11/wrapper_tag/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals, print_function, absolute_import
+from wrapper_tag.arguments import Keyword, KeywordGroup, Event, Method
+from wrapper_tag.rendered import RenderedTag
+from wrapper_tag.tag import Tag
+from wrapper_tag.utils import register_tag
+
+__version__ = "0.1.11"
+
+default_app_config = 'wrapper_tag.apps.WrapperTagConfig'
+
+__all__ = (
+    'Keyword', 'KeywordGroup', 'Event', 'Method',
+    'RenderedTag',
+    'Tag',
+    'register_tag',
+)
