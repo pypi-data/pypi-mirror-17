cb0d8d67e18d7eb99623e491924a6101e021d9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:32 +0000

    Add package nester12/1.1.1/nester12-1.1.1.zip

---
+++ packages/nester12/nester12-1.1.1.zip/nester12-1.1.1/nester12.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+def print_lol(klist,level=0):
+	for each_item in klist:
+		if isinstance(each_item,list):
+			print_lol(each_item,level+1)
+		else:
+			for tabs in range(0,level):
+				print("\t",end="")
+			print(each_item)
+
---
+++ packages/nester12/nester12-1.1.1.zip/nester12-1.1.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+		name="nester12",
+		version="1.1.1",
+		py_modules=["nester12"],
+		author="satyamkhare",
+		author_email="satyam.khare95@gmail.com",
+		description="simple printer of nested lists"
+	)
