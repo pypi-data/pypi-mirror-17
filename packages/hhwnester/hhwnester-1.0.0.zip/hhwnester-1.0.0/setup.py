79fca367f5d48e9de5b63f1fa0342bce2f6b3c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:47 +0000

    Add package hhwnester/1.0.0/hhwnester-1.0.0.zip

---
+++ packages/hhwnester/hhwnester-1.0.0.zip/hhwnester-1.0.0/setup.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-  
+from distutils.core import setup
+
+setup( 
+    name         = 'hhwnester', 
+    version      = '1.0.0', 
+    ## TODO: be sure to change these next few lines to match your details!
+    py_modules   = ['hhwnester'],
+    author       = 'wang',
+    author_email = 'muhan95@163.com',
+    url          = 'http://www.headfirstlabs.com',
+    description  = 'A simple printer of nested lists',
+ )
