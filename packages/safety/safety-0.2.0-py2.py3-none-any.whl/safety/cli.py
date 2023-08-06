25cc0409f038dbe8f175cf6caf2c11675ae30dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:16 +0000

    Add package safety/0.2.0/safety-0.2.0-py2.py3-none-any.whl

---
+++ packages/safety/safety-0.2.0-py2.py3-none-any.whl/safety/cli.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import sys
+import click
+from safety import __version__
+from safety import safety
+from safety.formatter import report
+
+
+
+@click.group()
+@click.version_option(version=__version__)
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("--full-report/--short-report", default=False)
+def check(full_report):
+    vulns = safety.check()
+    click.secho(report(vulns=vulns, full=full_report))
+    sys.exit(-1 if vulns else 0)
+
+
+if __name__ == "__main__":
+    cli()
