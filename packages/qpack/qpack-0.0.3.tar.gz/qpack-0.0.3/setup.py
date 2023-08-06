2e3606e40a07668d5b6f55745670fb4a428ee37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:07 +0000

    Add package qpack/0.0.3/qpack-0.0.3.tar.gz

---
+++ packages/qpack/qpack-0.0.3.tar.gz/qpack-0.0.3/setup.py
@@ -0,0 +1,57 @@
+#
+#  Got documentation from: http://peterdowns.com/posts/first-time-with-pypi.html
+#
+#   0. Update __init__.py
+#       changelog + version
+#
+#   1. Create tag:
+#       git tag 0.0.3 -m "Adds a tag so we can put this new version on PyPI."
+#
+#   2. Push tag:
+#       git push --tags origin master
+#
+#   3. Upload your package to PyPI Test:
+#       python setup.py register -r pypitest
+#       python setup.py sdist upload -r pypitest
+#
+#   4. Upload to PyPI Live
+#       python setup.py register -r pypi
+#       python setup.py sdist upload -r pypi
+#
+
+from distutils.core import setup, Extension
+
+module = Extension('qpack._qpack',
+                    define_macros = [],
+                    include_dirs = ['./qpack'],
+                    libraries = [],
+                    sources = ['./qpack/_qpack.c'])
+
+f = open('README.md')
+long_desc = f.read()
+f.close()
+del f
+
+setup(
+    name='qpack',
+    packages=['qpack'],
+    version='0.0.3',
+    description='QPack (de)serializer',
+    author='Jeroen van der Heijden',
+    author_email='jeroen@transceptor.technology',
+    url='https://github.com/transceptor-technology/qpack',
+    ext_modules = [module],
+    download_url='https://github.com/transceptor-technology/qpack/qpack/0.0.3',
+    keywords=['serializer', 'deserializer'],
+    long_description = long_desc,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Database',
+    ],
+)
