fc7d50189cdaaa872013f93a1c3f5647ff76a609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:41 +0000

    Add package lichengpnester/1.40/lichengpnester-1.40.zip

---
+++ packages/lichengpnester/lichengpnester-1.40.zip/lichengpnester-1.40/setup.py
@@ -0,0 +1,5 @@
+# setup.py
+# the command includes "python setup.py sdisk" "python setup.py install" and then you will have your own library;
+from distutils.core import setup
+
+setup(name='lichengpnester',version='1.40',py_modules=['linester'],author='sunTrail',author_email='2329408386@qq.com',url='http://cqu.edu.cn',description='A simple printer of nested lists')
