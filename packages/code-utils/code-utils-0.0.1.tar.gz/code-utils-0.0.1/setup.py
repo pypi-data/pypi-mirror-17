1ce7ddf191b4be353a485e9d015ca6f49442174a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:10 +0000

    Add package code-utils/0.0.1/code-utils-0.0.1.tar.gz

---
+++ packages/code-utils/code-utils-0.0.1.tar.gz/code-utils-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+	name = 'code-utils',
+	description=(
+		"this is some utils created by python or nodejs "
+		),
+	version = '0.0.1',
+	author_email='liudong@creditx.com',
+	license='MIT',
+	keywords='sample setuptools development',
+	url='https://github.com/HioryLiu/code-utils'
+
+	)
