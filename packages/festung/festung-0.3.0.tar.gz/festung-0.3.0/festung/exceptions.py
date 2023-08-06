435673df714fa3c7303b48048deac64efa5a427a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:47 +0000

    Add package festung/0.3.0/festung-0.3.0.tar.gz

---
+++ packages/festung/festung-0.3.0.tar.gz/festung-0.3.0/festung/exceptions.py
@@ -0,0 +1,43 @@
+from __future__ import absolute_import
+
+import exceptions
+
+
+class Error(exceptions.StandardError):
+    pass
+
+
+class Warning(exceptions.StandardError):
+    pass
+
+
+class InterfaceError(Error):
+    pass
+
+
+class DatabaseError(Error):
+    pass
+
+
+class InternalError(DatabaseError):
+    pass
+
+
+class OperationalError(DatabaseError):
+    pass
+
+
+class ProgrammingError(DatabaseError):
+    pass
+
+
+class IntegrityError(DatabaseError):
+    pass
+
+
+class DataError(DatabaseError):
+    pass
+
+
+class NotSupportedError(DatabaseError):
+    pass
