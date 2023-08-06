5d7adf0411ace022d13afb5b2a46d999b5021080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:00 +0000

    Add package ebenv/0.2.7/ebenv-0.2.7.tar.gz

---
+++ packages/ebenv/ebenv-0.2.7.tar.gz/ebenv-0.2.7/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+from pip.req import parse_requirements
+from pip.download import PipSession
+
+
+def get_requirements():
+    requirements = parse_requirements(
+        os.path.join(os.path.dirname(__file__), "requirements.txt"),
+        session=PipSession())
+    return [str(req.req) for req in requirements]
+
+
+setup(
+    name='ebenv',
+    version='0.2.7',
+    description='AWS Elastic Beanstalk environment dumper/extractor.',
+    url='https://github.com/steinnes/ebenv',
+    author='Steinn Eldjarn Sigurdarson',
+    author_email='steinnes@gmail.com',
+    keywords=['aws', 'elasticbeanstalk', 'environment'],
+    install_requires=get_requirements(),
+    packages=['ebenv'],
+    entry_points='''
+        [console_scripts]
+        ebenv=ebenv:cli
+    '''
+)
