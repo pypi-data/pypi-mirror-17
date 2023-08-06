245a57f7b0bf9647c30508b101fe420ff2f2fcdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:51 +0000

    Add package sqreen/0.8.7/sqreen-0.8.7.tar.gz

---
+++ packages/sqreen/sqreen-0.8.7.tar.gz/sqreen-0.8.7/sqreen/deliverer/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016 Sqreen. All Rights Reserved.
+# Please refer to our terms for more information: https://www.sqreen.io/terms.html
+""" Deliverer classes package
+"""
+from .simple import SimpleDeliverer
+from .batch import BatchDeliverer
+
+__all__ = ["SimpleDeliverer", "BatchDeliverer"]
