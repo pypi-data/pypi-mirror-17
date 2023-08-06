b75ed1c856046df642bf468aa3a7f8d2f32da8e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:50 +0000

    Add package recursivefunc/1.1.0/recursivefunc-1.1.0.tar.gz

---
+++ packages/recursivefunc/recursivefunc-1.1.0.tar.gz/recursivefunc-1.1.0/recursivefunc.py
@@ -0,0 +1,12 @@
+"""Este é o módulo "recursivefunc.py" que fornece uma função chamada print_lol()
+que imprime listas que podem ou não incluir listas aninhadas."""
+def print_lol(the_list, level):
+        """Esta função requer um argumento posicional chamado "the_list", que é qualquer lista Python (de possíveis listas aninhadas).
+        Cada item de dados na lista fornecida é (recursivamente) impressso na tela em sua própria linha."""
+        for each_item in the_list:
+                        if isinstance(each_item,list):
+                                print_lol(each_item,level+1)
+                        else:
+                                for tab_stop in range(level):
+                                        print("\t",end="")
+                                print(each_item)
