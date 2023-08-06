9767e2f0de2e7902e13dfd9ff9b2d5e4fd8d21a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:08 +0000

    Add package chris_nester/0.0.0/chris_nester-0.0.0.tar.gz

---
+++ packages/chris_nester/chris_nester-0.0.0.tar.gz/chris_nester-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'chris_nester',
+    fversion = '1.1.0',
+    py_modules = ['nester'],
+    author = 'hfpython',
+    author_email= 'joijow@sofijie.com',
+    url = 'http://www.baidu.com',
+    description = 'A simpel nested lists',
+)
