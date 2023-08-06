aa567d4d5f606f3280a2c57d8acef82a28ef443d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:15 +0000

    Add package wagtail/1.7/wagtail-1.7.tar.gz

---
+++ packages/wagtail/wagtail-1.7.tar.gz/wagtail-1.7/wagtail/utils/deprecation.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, unicode_literals
+
+
+class RemovedInWagtail18Warning(DeprecationWarning):
+    pass
+
+
+removed_in_next_version_warning = RemovedInWagtail18Warning
+
+
+class RemovedInWagtail19Warning(PendingDeprecationWarning):
+    pass
