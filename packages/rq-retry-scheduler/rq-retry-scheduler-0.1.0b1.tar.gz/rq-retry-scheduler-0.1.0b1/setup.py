4bed7b1108ee138159a8c7ebd0995d82051b8b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:08 +0000

    Add package rq-retry-scheduler/0.1.0b1/rq-retry-scheduler-0.1.0b1.tar.gz

---
+++ packages/rq-retry-scheduler/rq-retry-scheduler-0.1.0b1.tar.gz/rq-retry-scheduler-0.1.0b1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='rq-retry-scheduler',
+    version='0.1.0b1',
+    url='https://github.com/mikemill/rq_retry_scheduler',
+    description='RQ Retry and Scheduler',
+    long_description=open('README.rst').read(),
+    author='Michael Miller',
+    author_email='mikemill@gmail.com',
+    packages=find_packages(exclude=['*tests*']),
+    license='MIT',
+    install_requires=['rq>=0.6.0'],
+    zip_safe=False,
+    platforms='any',
+    entry_points={
+        'console_scripts': [
+            'rqscheduler = rq_retry_scheduler.cli:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
