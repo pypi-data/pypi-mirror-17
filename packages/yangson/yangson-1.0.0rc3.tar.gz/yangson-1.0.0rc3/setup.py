2b84f6b96a55014c92a4a349591b90da211997a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:02 +0000

    Add package yangson/1.0.0rc3/yangson-1.0.0rc3.tar.gz

---
+++ packages/yangson/yangson-1.0.0rc3.tar.gz/yangson-1.0.0rc3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme:
+    long_description = readme.read()
+
+setup(
+    name = "yangson",
+    packages = ["yangson"],
+    version = "1.0.0rc3",
+    description = "Library for working with data modelled in YANG",
+    long_description = long_description,
+    url = "https://github.com/CZ-NIC/yangson",
+    author = "Ladislav Lhotka",
+    author_email = "lhotka@nic.cz",
+    license = "LGPLv3",
+    install_requires = ['PyXB'],
+    tests_require = ["pytest"],
+    keywords = ["yang", "data model", "configuration", "json"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Systems Administration"]
+    )
