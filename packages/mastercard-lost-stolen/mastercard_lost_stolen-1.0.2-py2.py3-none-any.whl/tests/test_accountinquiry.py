d59e49b1b082db201b307198fcad50d5d8f99d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:45 +0000

    Add package mastercard-lost-stolen/1.0.2/mastercard_lost_stolen-1.0.2-py2.py3-none-any.whl

---
+++ packages/mastercard-lost-stolen/mastercard_lost_stolen-1.0.2-py2.py3-none-any.whl/tests/test_accountinquiry.py
@@ -0,0 +1,98 @@
+#
+# Copyright (c) 2016 MasterCard International Incorporated
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are
+# permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this list of
+# conditions and the following disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice, this list of
+# conditions and the following disclaimer in the documentation and/or other materials
+# provided with the distribution.
+# Neither the name of the MasterCard International Incorporated nor the names of its
+# contributors may be used to endorse or promote products derived from this software
+# without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+import unittest
+import time
+from mastercardapicore import RequestMap
+from base_test import BaseTest
+from mastercardloststolen import *
+
+
+class AccountInquiryTest(BaseTest):
+
+    def setUp(self):
+        Config.setDebug(True)
+        self.resetAuthentication()
+
+    
+        
+        
+                
+    def test_example_stolen(self):
+        
+
+        
+    
+        map = RequestMap()
+        map.set("AccountInquiry.AccountNumber", "5343434343434343")
+        
+        
+        response = AccountInquiry(map).update()
+
+        ignoreAsserts = []
+        
+        self.customAssertEqual(ignoreAsserts, "Account.Listed", response.get("Account.Listed"),"True")
+        self.customAssertEqual(ignoreAsserts, "Account.ReasonCode", response.get("Account.ReasonCode"),"S")
+        self.customAssertEqual(ignoreAsserts, "Account.Reason", response.get("Account.Reason"),"STOLEN")
+        
+
+        BaseTest.putResponse("example_stolen", response)
+        
+
+    
+    def test_example_lost(self):
+        
+
+        
+    
+        map = RequestMap()
+        map.set("AccountInquiry.AccountNumber", "5222222222222200")
+        
+        
+        response = AccountInquiry(map).update()
+
+        ignoreAsserts = []
+        
+        self.customAssertEqual(ignoreAsserts, "Account.Listed", response.get("Account.Listed"),"True")
+        self.customAssertEqual(ignoreAsserts, "Account.ReasonCode", response.get("Account.ReasonCode"),"L")
+        self.customAssertEqual(ignoreAsserts, "Account.Reason", response.get("Account.Reason"),"LOST")
+        
+
+        BaseTest.putResponse("example_lost", response)
+        
+
+    
+        
+        
+        
+        
+        
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
