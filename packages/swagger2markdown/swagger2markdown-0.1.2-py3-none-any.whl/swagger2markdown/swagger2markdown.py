38ddd21186b3a666ed97166fb5d39c80cc35da52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:24 +0000

    Add package swagger2markdown/0.1.2/swagger2markdown-0.1.2-py3-none-any.whl

---
+++ packages/swagger2markdown/swagger2markdown-0.1.2-py3-none-any.whl/swagger2markdown/swagger2markdown.py
@@ -0,0 +1,40 @@
+import argparse, json, os.path
+import jinja2, requests
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-i", "--input",
+        default="swagger.json",
+        help="path to or URL of the Swagger JSON file (default: swagger.json)",
+        metavar="SWAGGER_LOCATION"
+    )
+
+    parser.add_argument(
+        "-o", "--output",
+        default="swagger.md",
+        help="path to the output Markdown file (default: swagger.md)",
+        metavar="OUTPUT"
+    )
+
+    parser.add_argument(
+        "-t", "--template",
+        default=os.path.join(os.path.dirname(__file__), "swagger.md.j2"),
+        help="Jinja2 template used for conversion",
+        metavar="TEMPLATE"
+    )
+
+    args = parser.parse_args()
+
+    try:
+        swagger_data = json.load(open(args.input, encoding="utf8"))
+
+    except FileNotFoundError:
+        swagger_data = requests.get(args.input).json()
+
+    template = jinja2.Template(open(args.template, encoding="utf8").read())
+
+    with open(args.output, "w", encoding="utf8") as output:
+        output.write(template.render(swagger_data=swagger_data))
