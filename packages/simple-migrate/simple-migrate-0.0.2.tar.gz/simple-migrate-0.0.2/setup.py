485461358ed094fa0f7085bdde39d15ec8459a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:35 +0000

    Add package simple-migrate/0.0.2/simple-migrate-0.0.2.tar.gz

---
+++ packages/simple-migrate/simple-migrate-0.0.2.tar.gz/simple-migrate-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from os.path import join, dirname
+
+with open(join(dirname(__file__), 'requires.txt'), 'r') as f:
+    install_requires = f.read().split("\n")
+
+setup(
+    name="simple-migrate",
+    version="0.0.2",
+    description="A simple database migrate tool for flask",
+    author="ncuhome",
+    author_email="dev@ncuhome.cn",
+    url="https://github.com/ncuhome/simple-migrate",
+    license="MIT",
+    py_modules=['simple_migrate'],
+    install_requires=install_requires,
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
