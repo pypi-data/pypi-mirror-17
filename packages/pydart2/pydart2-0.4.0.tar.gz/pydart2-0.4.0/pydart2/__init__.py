0eff404f9c68aa7dc53c2d70efb0718e2c4fdb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:09 +0000

    Add package pydart2/0.4.0/pydart2-0.4.0.tar.gz

---
+++ packages/pydart2/pydart2-0.4.0.tar.gz/pydart2-0.4.0/pydart2/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from . import pydart2_api as papi
+from . import world
+from . import constraints
+import os.path
+import sys
+try:
+    from . import gui
+except Exception:
+    e = sys.exc_info()[1]
+    print("-" * 40)
+    print("Error while importing pydart2.gui")
+    print(e)
+    print("-" * 40)
+
+from . import utils
+from .world import World
+from .skel_vector import SkelVector
+
+
+def init(verbose=True):
+    papi.init(verbose)
