59536402fc80e4799469f60b4c6d2d6be48bb00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:12 +0000

    Add package jianghao/1.2/jianghao-1.2.zip

---
+++ packages/jianghao/jianghao-1.2.zip/jianghao-1.2/setup.py
@@ -0,0 +1,21 @@
+
+'''from distutils.core import setup
+
+setup(
+    name          ='nester',
+    bersion       ='1.0.1',
+    py_modules    =['jianghao'],
+    author        ='hfpython'
+    author_email  ='hfpython@headfirstlabs.com',
+    url           ='http://www.headfirstlabs.com',
+    description   ='A simple printer of nested lists',
+    )
+
+'''
+
+
+from distutils.core import setup
+setup(name='jianghao',
+      version='1.2',
+      py_modules='jianghao',
+      author='niyeye')
