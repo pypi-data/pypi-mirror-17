0860550ebce6052cecb6ced1ebfa6678b993f2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:53 +0000

    Add package SimpleSQLite/0.5.0/SimpleSQLite-0.5.0.tar.gz

---
+++ packages/SimpleSQLite/SimpleSQLite-0.5.0.tar.gz/SimpleSQLite-0.5.0/simplesqlite/loader/error.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+
+
+class ValidationError(Exception):
+    """
+    Raised data is not properly formatted.
+    """
+
+
+class InvalidDataError(ValueError):
+    """
+    Raised when data is invalid to load.
+    """
+
+
+class OpenError(IOError):
+    """
+    Raised when failed to open a file.
+    """
