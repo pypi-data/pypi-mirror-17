a13b6e4efc1f54c51f2f91b5660bd89e8e4f46c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:44 +0000

    Add package bottle-oop-rest/0.0.2/bottle-oop-rest-0.0.2.zip

---
+++ packages/bottle-oop-rest/bottle-oop-rest-0.0.2.zip/bottle-oop-rest-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='bottle-oop-rest',
+    version='0.0.2',
+    packages=['borest'],
+    url='https://github.com/jar3b/bottle-oop-rest',
+    license='MIT',
+    author='hello',
+    author_email='hellotan@live.ru',
+    description='Bottle.py OOP REST simple library',
+    install_requires=[
+       'bottle>=0.12.9'
+    ]
+)
