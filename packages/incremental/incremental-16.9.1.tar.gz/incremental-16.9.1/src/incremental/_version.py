e033348eb95e835fa7dea6b4c008988c752e9182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:21 +0000

    Add package incremental/16.9.1/incremental-16.9.1.tar.gz

---
+++ packages/incremental/incremental-16.9.1.tar.gz/incremental-16.9.1/src/incremental/_version.py
@@ -0,0 +1,7 @@
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update Incremental` to change this file.
+
+from incremental import Version
+
+__version__ = Version('Incremental', 16, 9, 1)
+__all__ = ["__version__"]
