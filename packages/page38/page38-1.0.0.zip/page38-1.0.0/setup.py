c0126310fc3ff8b28b9964aab9c883d114edaf3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:19 +0000

    Add package page38/1.0.0/page38-1.0.0.zip

---
+++ packages/page38/page38-1.0.0.zip/page38-1.0.0/setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+	name         = 'page38', 
+	version      = '1.0.0', 
+	## TODO: be sure to change these next few lines to match your details!
+	py_modules   = ['page38'],
+	author       = 'hfpython',
+	author_email = 'hfpython@headfirstlabs.com',
+	url          = 'http://www.headfirstlabs.com',
+	description  = 'A simple printer of nested lists',
+)
