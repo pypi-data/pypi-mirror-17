ca83f7291a2704eb271bd196743b7612629f47b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:55 +0000

    Add package python-usps2/0.2.0/python-usps2-0.2.0.tar.gz

---
+++ packages/python-usps2/python-usps2-0.2.0.tar.gz/python-usps2-0.2.0/usps/addressinformation/__init__.py
@@ -0,0 +1,5 @@
+from usps.addressinformation.base import USPSXMLError, Address
+
+USPS_CONNECTION = 'https://secure.shippingapis.com/ShippingAPI.dll'
+USPS_CONNECTION_TEST = 'https://secure.shippingapis.com/ShippingAPITest.dll'
+USPS_CONNECTION_TEST_SECURE = USPS_CONNECTION_TEST
