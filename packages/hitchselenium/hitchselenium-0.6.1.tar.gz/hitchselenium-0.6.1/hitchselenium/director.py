51bcdb7c9b5ed1d95bdb771af523eed8846434d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:30 +0000

    Add package hitchselenium/0.6.1/hitchselenium-0.6.1.tar.gz

---
+++ packages/hitchselenium/hitchselenium-0.6.1.tar.gz/hitchselenium-0.6.1/hitchselenium/director.py
@@ -0,0 +1,172 @@
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from hitchselenium import exceptions
+import time
+
+from selenium.common.exceptions import StaleElementReferenceException
+
+
+class text_to_be_equal_in_element_contents_or_value(object):
+    """ An expectation for checking if the given text is present and equal in the
+    specified element.
+    """
+    def __init__(self, locator, text_):
+        self.locator = locator
+        self.text = text_
+
+    def __call__(self, driver):
+        try:
+            element = expected_conditions._find_element(driver, self.locator)
+            #if element_text:
+            return self.text == element.text or self.text == element.get_attribute("value")
+            #else:
+                #return False
+        except StaleElementReferenceException:
+            return False
+
+
+class text_to_be_present_in_element_contents_or_value(object):
+    """
+    An expectation for checking if the given text is present in the element's
+    locator, text
+    """
+    def __init__(self, locator, text_):
+        self.locator = locator
+        self.text = text_
+
+    def __call__(self, driver):
+        try:
+            element = expected_conditions._find_element(driver, self.locator)
+            #if element_text:
+            return self.text in element.text or self.text in element.get_attribute("value")
+            #else:
+                #return False
+        except StaleElementReferenceException:
+                return False
+
+
+
+class IndividualElement(object):
+    def __init__(self, director, selector):
+        self._director = director
+        self._selector = selector
+
+    @property
+    def selector(self):
+        return self._selector
+
+    @property
+    def director(self):
+        return self._director
+
+    def fill_text(self, text):
+        """
+        Fill text in a text box.
+
+        Clears the text box of text first.
+        """
+        self.selector.find_element(self.director.driver).clear()
+        self.selector.find_element(self.director.driver).send_keys(text)
+
+    def send_keys(self, text):
+        """
+        Send specified keys to an element (dropdown, text box, etc.).
+
+        Unlike fill_text, this method does not attempt to clear the element (i.e. text box) first.
+        """
+        self.selector.find_element(self.director.driver).send_keys(text)
+
+    def click(self):
+        """
+        Click on element.
+        """
+        self.selector.find_element(self.director.driver).click()
+
+    def should_appear(self):
+        """
+        Check that element is visible.
+        """
+        WebDriverWait(self.director.driver, self.director.default_timeout).until(
+            expected_conditions.visibility_of_element_located(
+                self.selector.conditions()
+            )
+        )
+
+    def should_contain(self, text):
+        """
+        Check that text appears in element.
+        """
+        WebDriverWait(self.director.driver, self.director.default_timeout).until(
+            text_to_be_present_in_element_contents_or_value(
+                self.selector.conditions(), text
+            )
+        )
+
+    def should_only_contain(self, text):
+        """
+        Check that text appears in element.
+        """
+        WebDriverWait(self.director.driver, self.director.default_timeout).until(
+            text_to_be_equal_in_element_contents_or_value(
+                self.selector.conditions(), text
+            )
+        )
+
+    def contents(self):
+        """
+        Get text contents of any label.
+        """
+        return self.selector.find_element(self.director.driver).text
+
+
+
+class PageUrl(object):
+    def __init__(self, director):
+        self._director = director
+
+
+    def should_contain(self, text):
+        wait_for = self._director.default_timeout
+        for i in range(0, 10 * wait_for):
+            if text in self._director.driver.current_url:
+                return
+            time.sleep(0.1)
+
+        current_url = self._director.driver.current_url
+        if text in current_url:
+            return
+        raise exceptions.ShouldHaveButDidNot(
+            "URL '{}' did not contain '{}' after {} seconds.".format(
+                current_url,
+                text,
+                wait_for,
+            )
+        )
+
+
+class Director(object):
+    def __init__(self, driver, selector_translator, default_timeout=5):
+        self._driver = driver
+        self._selector_translator = selector_translator
+        self._default_timeout = default_timeout
+
+    @property
+    def default_timeout(self):
+        return self._default_timeout
+
+    @property
+    def driver(self):
+        return self._driver
+
+    def visit(self, url):
+        """
+        Load URL.
+        """
+        self.driver.get(url)
+
+    @property
+    def url(self):
+        return PageUrl(self)
+
+    def the(self, identifier):
+        return IndividualElement(self, self._selector_translator(identifier))
