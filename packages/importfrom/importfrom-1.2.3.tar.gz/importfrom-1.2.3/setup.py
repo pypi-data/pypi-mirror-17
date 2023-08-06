074dc21c3e7b54f31c8aba8e2e8f276e331da6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:36 +0000

    Add package importfrom/1.2.3/importfrom-1.2.3.tar.gz

---
+++ packages/importfrom/importfrom-1.2.3.tar.gz/importfrom-1.2.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='importfrom',
+      version='1.2.3',
+      description='Import python functions from anywhere!',
+      long_description='Disclaimer: This is not a serious project, just a bit of fun.',
+      url='https://github.com/libeclipse/import-from',
+      author='libeclipse',
+      author_email='libeclipse@gmail.com',
+      license='MIT',
+      packages=['importfrom'],
+      install_requires=["beautifulsoup4", "requests"],
+      keywords = ['importfrom', 'import', 'from', 'anywhere', 'host'],
+      zip_safe=False)
