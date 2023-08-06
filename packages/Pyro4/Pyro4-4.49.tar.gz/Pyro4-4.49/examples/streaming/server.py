09a4c25ac5c1e29d0064984d459df3666b1d465a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:41 +0000

    Add package Pyro4/4.49/Pyro4-4.49.tar.gz

---
+++ packages/Pyro4/Pyro4-4.49.tar.gz/Pyro4-4.49/examples/streaming/server.py
@@ -0,0 +1,36 @@
+from __future__ import print_function, division
+import time
+import Pyro4
+
+
+if Pyro4.config.ITER_STREAMING:
+    print("Note: iter-streaming has been enabled in the Pyro config.")
+else:
+    print("Note: iter-streaming has not been enabled in the Pyro config (PYRO_ITER_STREAMING).")
+
+
+@Pyro4.expose
+class Streamer(object):
+    def list(self):
+        return [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    def iterator(self):
+        return iter([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    def generator(self):
+        i = 1
+        while i < 10:
+            yield i
+            i += 1
+
+    def slow_generator(self):
+        i = 1
+        while i < 10:
+            time.sleep(0.5)
+            yield i
+            i += 1
+
+
+Pyro4.Daemon.serveSimple({
+        Streamer: "example.streamer"
+    }, ns=False)
