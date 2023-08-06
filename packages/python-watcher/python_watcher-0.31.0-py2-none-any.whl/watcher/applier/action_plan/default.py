f13c3280b7f16b0a9c029f9aa4326614efe1a287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:46 +0000

    Add package python-watcher/0.31.0/python_watcher-0.31.0-py2-none-any.whl

---
+++ packages/python-watcher/python_watcher-0.31.0-py2-none-any.whl/watcher/applier/action_plan/default.py
@@ -0,0 +1,65 @@
+# -*- encoding: utf-8 -*-
+# Copyright (c) 2015 b<>com
+#
+# Authors: Jean-Emile DARTOIS <jean-emile.dartois@b-com.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from oslo_log import log
+
+from watcher.applier.action_plan import base
+from watcher.applier import default
+from watcher.applier.messaging import event_types
+from watcher.common.messaging.events import event
+from watcher.objects import action_plan as ap_objects
+
+LOG = log.getLogger(__name__)
+
+
+class DefaultActionPlanHandler(base.BaseActionPlanHandler):
+    def __init__(self, context, service, action_plan_uuid):
+        super(DefaultActionPlanHandler, self).__init__()
+        self.ctx = context
+        self.service = service
+        self.action_plan_uuid = action_plan_uuid
+
+    def notify(self, uuid, event_type, state):
+        action_plan = ap_objects.ActionPlan.get_by_uuid(self.ctx, uuid)
+        action_plan.state = state
+        action_plan.save()
+        ev = event.Event()
+        ev.type = event_type
+        ev.data = {}
+        payload = {'action_plan__uuid': uuid,
+                   'action_plan_state': state}
+        self.service.publish_status_event(ev.type.name, payload)
+
+    def execute(self):
+        try:
+            # update state
+            self.notify(self.action_plan_uuid,
+                        event_types.EventTypes.LAUNCH_ACTION_PLAN,
+                        ap_objects.State.ONGOING)
+            applier = default.DefaultApplier(self.ctx, self.service)
+            applier.execute(self.action_plan_uuid)
+            state = ap_objects.State.SUCCEEDED
+        except Exception as e:
+            LOG.exception(e)
+            state = ap_objects.State.FAILED
+
+        finally:
+            # update state
+            self.notify(self.action_plan_uuid,
+                        event_types.EventTypes.LAUNCH_ACTION_PLAN,
+                        state)
---
+++ packages/python-watcher/python_watcher-0.31.0-py2-none-any.whl/watcher/tests/applier/action_plan/test_default_action_handler.py
@@ -0,0 +1,57 @@
+# -*- encoding: utf-8 -*-
+# Copyright (c) 2015 b<>com
+#
+# Authors: Jean-Emile DARTOIS <jean-emile.dartois@b-com.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import mock
+
+from watcher.applier.action_plan import default
+from watcher.applier.messaging import event_types as ev
+from watcher.objects import action_plan as ap_objects
+from watcher.tests.db import base
+from watcher.tests.objects import utils as obj_utils
+
+
+class TestDefaultActionPlanHandler(base.DbTestCase):
+    def setUp(self):
+        super(TestDefaultActionPlanHandler, self).setUp()
+        self.action_plan = obj_utils.create_test_action_plan(
+            self.context)
+
+    def test_launch_action_plan(self):
+        command = default.DefaultActionPlanHandler(
+            self.context, mock.MagicMock(), self.action_plan.uuid)
+        command.execute()
+        action_plan = ap_objects.ActionPlan.get_by_uuid(
+            self.context, self.action_plan.uuid)
+        self.assertEqual(ap_objects.State.SUCCEEDED, action_plan.state)
+
+    def test_trigger_audit_send_notification(self):
+        messaging = mock.MagicMock()
+        command = default.DefaultActionPlanHandler(
+            self.context, messaging, self.action_plan.uuid)
+        command.execute()
+
+        call_on_going = mock.call(ev.EventTypes.LAUNCH_ACTION_PLAN.name, {
+            'action_plan_state': ap_objects.State.ONGOING,
+            'action_plan__uuid': self.action_plan.uuid})
+        call_succeeded = mock.call(ev.EventTypes.LAUNCH_ACTION_PLAN.name, {
+            'action_plan_state': ap_objects.State.SUCCEEDED,
+            'action_plan__uuid': self.action_plan.uuid})
+
+        calls = [call_on_going, call_succeeded]
+        messaging.publish_status_event.assert_has_calls(calls)
+        self.assertEqual(2, messaging.publish_status_event.call_count)
