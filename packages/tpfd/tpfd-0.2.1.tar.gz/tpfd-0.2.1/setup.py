5df62de7ee92e4c5e10b449ae1900418dbc54782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:12 +0000

    Add package tpfd/0.2.1/tpfd-0.2.1.tar.gz

---
+++ packages/tpfd/tpfd-0.2.1.tar.gz/tpfd-0.2.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='tpfd',
+      version='0.2.1',
+      description='Text Parsing Function Dispatcher',
+      url='https://github.com/erinxocon/tpfd',
+      author="Erin O'Connell",
+      author_email='erinocon5@gmail.com',
+      license='MIT',
+      packages=['tpfd'],
+      zip_safe=False,
+      classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy'),
+       install_requires=['parse'],
+       data_files=['test/Test1.txt'])
