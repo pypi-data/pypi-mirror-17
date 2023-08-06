56aa38d214f0323793514024567bc25a5ec7c61a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:47 +0000

    Add package mastercard-personalized-loyalty-offers/1.0.2/mastercard_personalized_loyalty_offers-1.0.2.tar.gz

---
+++ packages/mastercard-personalized-loyalty-offers/mastercard_personalized_loyalty_offers-1.0.2.tar.gz/mastercard_personalized_loyalty_offers-1.0.2/tests/test_usersaving.py
@@ -0,0 +1,75 @@
+#
+# Copyright (c) 2016 MasterCard International Incorporated
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are
+# permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this list of
+# conditions and the following disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice, this list of
+# conditions and the following disclaimer in the documentation and/or other materials
+# provided with the distribution.
+# Neither the name of the MasterCard International Incorporated nor the names of its
+# contributors may be used to endorse or promote products derived from this software
+# without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+import unittest
+import time
+from mastercardapicore import RequestMap
+from base_test import BaseTest
+from mastercardpersonalizedloyaltyoffers import *
+
+
+class UserSavingTest(BaseTest):
+
+    def setUp(self):
+        Config.setDebug(True)
+        self.resetAuthentication()
+
+    
+        
+        
+        
+        
+        
+        
+                
+    def test_example_user_savings(self):
+        
+
+        
+    
+        map = RequestMap()
+        map.set("fid", "examplebank")
+        map.set("UserToken", "mh3WonUm5xmE")
+        map.set("ActivationId", "TRU_1000136")
+        
+        
+        response = UserSaving.userSaving(map)
+
+        ignoreAsserts = []
+        
+        
+
+        BaseTest.putResponse("example_user_savings", response)
+        
+
+    
+        
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
