0f82bfbca6cc9813c171ff62ca402b46dac496d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:43 +0000

    Add package django-ajax-redirect/0.1.0/django_ajax_redirect-0.1.0-py3-none-any.whl

---
+++ packages/django-ajax-redirect/django_ajax_redirect-0.1.0-py3-none-any.whl/ajax_redirect/__init__.py
@@ -0,0 +1,8 @@
+"""
+AJAX redirection middleware and decorator for Django
+"""
+
+from ajax_redirect.decorators import ajax_redirect
+from ajax_redirect.middleware import AjaxRedirectMiddleware
+
+__version__ = (0, 1, 0)
