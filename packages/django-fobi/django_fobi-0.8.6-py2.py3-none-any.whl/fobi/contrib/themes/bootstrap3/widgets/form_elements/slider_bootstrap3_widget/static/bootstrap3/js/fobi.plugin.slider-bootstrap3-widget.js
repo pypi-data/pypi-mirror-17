536797deb8c8c2f579fe661dfb8dfd1da441a622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:16 +0000

    Add package django-fobi/0.8.6/django_fobi-0.8.6-py2.py3-none-any.whl

---
+++ packages/django-fobi/django_fobi-0.8.6-py2.py3-none-any.whl/fobi/contrib/themes/bootstrap3/widgets/form_elements/slider_bootstrap3_widget/static/bootstrap3/js/fobi.plugin.slider-bootstrap3-widget.js
@@ -0,0 +1,13 @@
+/*
+    Document   : fobi.plugin.slider-bootstrap3-widget.js
+    Created on : Oct 19, 2016, 03:30:35 AM
+    Author     : Artur Barseghyan (artur.barseghyan@gmail.com)
+    Description:
+        `bootstrap3` theme slider plugin scripts.
+*/
+;
+
+$(document).ready(function() {
+    $('.slider').bootstrapSlider();
+    $('.slider-no-background').bootstrapSlider();
+});
