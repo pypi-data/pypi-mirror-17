332d9bffb31a719ac1a2184f838c7d53cd6eae81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:39 +0000

    Add package sagbescheid/0.3.0/sagbescheid-0.3.0.tar.gz

---
+++ packages/sagbescheid/sagbescheid-0.3.0.tar.gz/sagbescheid-0.3.0/docs/source/index.rst
@@ -0,0 +1,17 @@
+Welcome to sagbescheid's documentation!
+=======================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   setup
+   usage
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/sagbescheid/sagbescheid-0.3.0.tar.gz/sagbescheid-0.3.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+from setuptools import setup
+
+
+setup(name="sagbescheid",
+      author="Wieland Hoffmann",
+      author_email="themineo@gmail.com",
+      packages=["sagbescheid", "sagbescheid.notifiers"],
+      package_dir={"sagbescheid": "sagbescheid"},
+      download_url=["https://github.com/mineo/sagbescheid/tarball/master"],
+      url=["http://github.com/mineo/sagbescheid"],
+      license="MIT",
+      classifiers=["Development Status :: 4 - Beta",
+                   "License :: OSI Approved :: MIT License",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python :: 2.7"],
+      description="systemd notification daemon",
+      long_description=open("README.rst").read(),
+      install_requires=["Twisted[tls]>=15.2.0",
+                        "txdbus==1.0.11",
+                        "zope.interface==4.1.2",
+                        "enum34==1.0.4"],
+      setup_requires=["setuptools_scm"],
+      use_scm_version={"write_to": "sagbescheid/version.py"},
+      extras_require={
+          'docs': ['sphinx', 'sphinxcontrib-autoprogram']
+          }
+      )
