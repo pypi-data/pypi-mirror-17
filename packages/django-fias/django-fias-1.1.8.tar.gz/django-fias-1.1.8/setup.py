02019ac8149515a8acf411f30e1a1eae65590f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:20 +0000

    Add package django-fias/1.1.8/django-fias-1.1.8.tar.gz

---
+++ packages/django-fias/django-fias-1.1.8.tar.gz/django-fias-1.1.8/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import codecs
+import sys
+
+from fias.version import __version__
+PY3 = sys.version_info[0] == 3
+
+sys.path.insert(0, '..')
+
+extra_requirements = []
+if PY3:
+    extra_requirements = [
+        'suds-jurko>=0.6',
+    ]
+else:
+    extra_requirements = [
+        'suds>=0.4',
+    ]
+
+
+setup(
+    name='django-fias',
+    version=__version__,
+    author='Artem Vlasov',
+    author_email='root@proscript.ru',
+    url='https://github.com/Yuego/django-fias',
+    download_url='https://github.com/Yuego/django-fias/archive/{0}.tar.gz'.format(__version__),
+
+    description='FIAS Django integration',
+    long_description=codecs.open('README.rst', encoding='utf8').read(),
+
+    license='MIT license',
+    install_requires=[
+        'django >= 1.7, < 1.10',
+        'django_select2>=5.3.0',
+        #'zeep>=0.8.0',
+        'rarfile',
+        'six',
+        'lxml',
+        'unrar',
+        'dbfread>=2.0.5',
+        'progress',
+    ] + extra_requirements,
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Russian',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
