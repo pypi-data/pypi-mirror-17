8cc379e50775f3cea452dac8946a6da124195134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:46 +0000

    Add package bobot/0.1.1/bobot-0.1.1.tar.gz

---
+++ packages/bobot/bobot-0.1.1.tar.gz/bobot-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+  name = 'bobot',
+  packages = ['bobot'],
+  version = '0.1.1',
+  description = 'Telegram Bot API Wrapper',
+  author = 'Trdat Mkrtchyan',
+  author_email = 'wombtormb@gmail.com',
+  url = 'https://github.com/zefirka/bobot',
+  download_url = 'https://github.com/zefirka/bobot/archive/v0.1.1.tar.gz',
+  keywords = ['telegram', 'bot', 'api'],
+  classifiers = []
+)
+
+
+
+
+
