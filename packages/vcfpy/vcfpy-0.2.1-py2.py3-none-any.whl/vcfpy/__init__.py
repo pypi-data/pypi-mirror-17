fbb4d4bd22f0162502b1302168321e768d39cfe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:22 +0000

    Add package vcfpy/0.2.1/vcfpy-0.2.1-py2.py3-none-any.whl

---
+++ packages/vcfpy/vcfpy-0.2.1-py2.py3-none-any.whl/vcfpy/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from .exceptions import VCFPyException, InvalidHeaderException, \
+    InvalidRecordException, IncorrectVCFFormat, HeaderNotFound
+
+from .header import Header, HeaderLine, SimpleHeaderFile, \
+    SimpleHeaderFile, ContigHeaderLine, FilterHeaderLine, \
+    CompoundHeaderLine, InfoHeaderLine, FormatHeaderLine, \
+    SamplesInfos
+
+from .record import Record, Call, AltRecord, Substitution, SV, BreakEnd, \
+    SymbolicAllele
+
+from .reader import Reader
+
+from .writer import Writer
+
+__version__ = '0.2.1'
