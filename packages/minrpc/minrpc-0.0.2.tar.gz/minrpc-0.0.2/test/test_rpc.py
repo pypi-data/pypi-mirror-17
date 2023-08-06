d56797a48571437804bf10510abbfa08d50d7e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:28 +0000

    Add package minrpc/0.0.2/minrpc-0.0.2.tar.gz

---
+++ packages/minrpc/minrpc-0.0.2.tar.gz/minrpc-0.0.2/test/test_rpc.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import
+
+# standard library
+import os
+import subprocess
+import unittest
+
+# tested modules
+from minrpc.client import Client
+
+
+class TestRPC(unittest.TestCase):
+
+    # This test is currently only useful for windows. On linux open file
+    # descriptors don't prevent the file from being deleted.
+    # TODO: change this test such that it checks "directly" whether the file
+    # handle is still open.
+    def test_no_leaking_file_handles(self):
+        test_filename = 'foobar'
+        f = open(test_filename, 'w')
+        print(f.fileno())
+        svc, proc = Client.spawn_subprocess()
+        f.close()
+        try:
+            os.remove(test_filename)
+            file_can_be_deleted = True
+        except OSError:
+            file_can_be_deleted = False
+        svc.close()
+        proc.wait()
+        if not file_can_be_deleted:
+            os.remove(test_filename)
+        self.assertTrue(file_can_be_deleted)
+
+    # TODO: add tests to check that other resources get closed correctly.
+
+
+if __name__ == '__main__':
+    unittest.main()
