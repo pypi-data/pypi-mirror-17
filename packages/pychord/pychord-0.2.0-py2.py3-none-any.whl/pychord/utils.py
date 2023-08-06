748ca07e169fa3aef92729943f09a14cd52736eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:36 +0000

    Add package pychord/0.2.0/pychord-0.2.0-py2.py3-none-any.whl

---
+++ packages/pychord/pychord-0.2.0-py2.py3-none-any.whl/pychord/utils.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from .constants import NOTE_VAL_DICT, SCALE_VAL_DICT
+
+
+def note_to_val(note):
+    if note not in NOTE_VAL_DICT:
+        raise ValueError("Unknown note {}".format(note))
+    return NOTE_VAL_DICT[note]
+
+
+def val_to_note(val, scale="C"):
+    val %= 12
+    return SCALE_VAL_DICT[scale][val]
+
+
+def transpose_note(note, transpose):
+    val = note_to_val(note)
+    val += transpose
+    return val_to_note(val)
+
+
+def display_appended(appended):
+    # TODO: Implement this
+    return ""
+
+
+def display_on(on_note):
+    if on_note:
+        return "/{}".format(on_note)
+    return ""
