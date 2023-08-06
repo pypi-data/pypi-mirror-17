cd76a773f73922ecf50cf29ac62244fddb2ef4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:43 +0000

    Add package yadage/0.7.2/yadage-0.7.2.tar.gz

---
+++ packages/yadage/yadage-0.7.2.tar.gz/yadage-0.7.2/yadage/handlers/predicate_handlers.py
@@ -0,0 +1,81 @@
+import logging
+import utils
+import jsonpointer
+import jsonpath_rw
+
+log = logging.getLogger(__name__)
+
+handlers,scheduler = utils.handler_decorator()
+
+def convert(thing):
+    if type(thing)==jsonpath_rw.jsonpath.Index:
+        return thing.index
+    if type(thing)==jsonpath_rw.jsonpath.Fields:
+        fs = thing.fields
+        assert len(fs)==1
+        return fs[0]
+
+def unravelpath(path):
+    if type(path)==jsonpath_rw.jsonpath.Child:
+        for x in unravelpath(path.left):
+            yield x
+        yield convert(path.right)
+    else:
+        yield convert(path)
+
+def path2pointer(path):
+    return jsonpointer.JsonPointer.from_parts(x for x in unravelpath(path)).path
+
+def checkmeta(flowview,metainfo):
+    log.debug('checking meta %s on view with offset %s',metainfo,flowview.offset)
+    applied_ids = [rl.identifier for rl in flowview.applied_rules]
+    rulesok = all([x in applied_ids for x in metainfo['rules']])
+
+    # for x in metainfo['steps']:
+    #     print 'checking step',x,flowview.dag.getNode(x).has_result
+    stepsok = all([flowview.dag.getNode(x).has_result() for x in metainfo['steps']])
+    log.debug('all rules applied: %s, all steps have results: %s',rulesok,stepsok)
+    return (rulesok and stepsok)
+
+
+def scope_done(scope,flowview):
+    '''
+    walks recursively all scopes starting at some initial scope to determine if
+    all steps and rules under this scope have been executed / applied. Will indicate
+    that it's save to reference any result of the workflow within that scope.
+    '''
+    log.debug('checking scope %s on view with offset %s',scope,flowview.offset)
+    result = True
+
+    bookkeeper = jsonpointer.JsonPointer(scope).resolve(flowview.bookkeeper)
+    for k,v in bookkeeper.iteritems():
+        for k,v in bookkeeper.iteritems():
+            if k=='_meta':
+                result = result and checkmeta(flowview,v)
+            else:
+                childscope = scope+'/{}'.format(k)
+                result = result and scope_done(childscope,flowview)
+    return result
+
+@scheduler('jsonpath_ready')
+def jsonpath_ready(flowview,depspec):
+    '''
+    the main predicate for yadage. for a list of jsonpath expressions
+    determine whether the stage or workflow scope is ready (i.e. has a result)
+    '''
+    log.debug('checking jsonpath ready predicate\n%s',depspec)
+    dependencies = depspec['expressions']
+    for x in dependencies:
+        depmatches = flowview.query(x,flowview.steps)
+        if not depmatches:
+            return False
+        issubwork = '_nodeid' not in depmatches[0].value[0]
+        if issubwork:
+            log.debug('dependency is a subworkflow. determine if scope is done')
+            if not all([scope_done(scope['_offset'],flowview) for match in depmatches for scope in match.value]):
+                return False
+        else:
+            if not all([x.has_result() for x in flowview.getSteps(x)]):
+                return False
+    log.debug('all checks ok, predicate is True')
+    return True
