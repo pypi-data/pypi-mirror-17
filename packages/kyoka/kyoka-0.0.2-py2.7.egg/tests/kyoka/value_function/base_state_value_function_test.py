f83e3ac3af5524a3309b1ee12917c04667a67f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:48 +0000

    Add package kyoka/0.0.2/kyoka-0.0.2-py2.7.egg

---
+++ packages/kyoka/kyoka-0.0.2-py2.7.egg/tests/kyoka/value_function/base_state_value_function_test.py
@@ -0,0 +1,17 @@
+from tests.base_unittest import BaseUnitTest
+from kyoka.value_function.base_state_value_function import BaseStateValueFunction
+
+class BaseStateValueFunctionTest(BaseUnitTest):
+
+  def setUp(self):
+    self.func = BaseStateValueFunction()
+
+  def test_error_msg_when_not_implement_abstract_method(self):
+    try:
+      self.func.calculate_value("dummy")
+    except NotImplementedError as e:
+      self.include("BaseStateValueFunction", str(e))
+      self.include("calculate_value", str(e))
+    else:
+      self.fail("NotImplementedError does not occur")
+
