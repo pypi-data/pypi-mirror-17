a76497611382c00ac765dd3b11a3ae6bbcee0381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:57 +0000

    Add package class_a/1.0.0.0/class_a-1.0.0.0.zip

---
+++ packages/class_a/class_a-1.0.0.0.zip/class_a-1.0.0.0/nester.py
@@ -0,0 +1,8 @@
+"""this is a function that I made"""
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+            
