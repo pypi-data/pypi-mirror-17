7a132a20ec45c909b6b52060dee57421c6e98fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:23 +0000

    Add package knowit/0.1.1/knowit-0.1.1.tar.gz

---
+++ packages/knowit/knowit-0.1.1.tar.gz/knowit-0.1.1/knowit/providers/enzyme.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+import logging
+
+import enzyme
+from six import string_types
+
+from .. import OrderedDict
+from ..properties import (
+    AudioChannelsRule, AudioCodec, HearingImpairedRule, Integer,
+    Language, Property, ResolutionRule, VideoCodec, YesNo
+)
+from ..provider import MalformedFileError, Provider
+from ..utils import todict
+
+
+logger = logging.getLogger(__name__)
+
+
+class EnzymeProvider(Provider):
+    """Enzyme Provider."""
+
+    def __init__(self):
+        """Init method."""
+        super(EnzymeProvider, self).__init__({
+            'general': OrderedDict([
+                ('title', Property('title')),
+                ('duration', Property('duration')),
+            ]),
+            'video': OrderedDict([
+                ('number', Property('number', Integer('video track number'))),
+                ('name', Property('name')),
+                ('language', Property('language', Language())),
+                ('width', Property('width', Integer('width'))),
+                ('height', Property('height', Integer('height'))),
+                ('scan_type', Property('interlaced', YesNo('Interlaced', 'Progressive'), default='Progressive')),
+                ('resolution', Property(handler=ResolutionRule())),
+                # ('bit_depth', Property('bit_depth', Integer('video bit depth'))),
+                ('codec', Property('codec_id', VideoCodec())),
+                ('forced', Property('forced', YesNo(hide_value=False))),
+                ('default', Property('default', YesNo(hide_value=False))),
+                ('enabled', Property('enabled', YesNo(hide_value=True))),
+            ]),
+            'audio': OrderedDict([
+                ('number', Property('number', Integer('audio track number'))),
+                ('name', Property('name')),
+                ('language', Property('language', Language())),
+                ('codec', Property('codec_id', AudioCodec())),
+                ('channels_count', Property('channels', Integer('audio channels'))),
+                ('channels', Property(handler=AudioChannelsRule())),
+                ('forced', Property('forced', YesNo(hide_value=False))),
+                ('default', Property('default', YesNo(hide_value=False))),
+                ('enabled', Property('enabled', YesNo(hide_value=True))),
+            ]),
+            'subtitle': OrderedDict([
+                ('number', Property('number', Integer('subtitle track number'))),
+                ('name', Property('name')),
+                ('language', Property('language', Language())),
+                ('hearing_impaired', Property(handler=HearingImpairedRule())),
+                ('forced', Property('forced', YesNo(hide_value=False))),
+                ('default', Property('default', YesNo(hide_value=False))),
+                ('enabled', Property('enabled', YesNo(hide_value=True))),
+            ]),
+        })
+
+    def accepts(self, target):
+        """Accept only MKV files."""
+        return not isinstance(target, string_types) or target.lower().endswith('.mkv')
+
+    def describe(self, target, options):
+        """Return video metadata."""
+        if isinstance(target, string_types):
+            try:
+                with open(target, 'rb') as f:
+                    data = todict(enzyme.MKV(f))
+            except enzyme.MalformedMKVError:  # pragma: no cover
+                logger.warning("Invalid file '%s'", target)
+                if options['fail_on_error']:
+                    raise MalformedFileError
+                return dict()
+        else:
+            data = target
+
+        if options['raw']:
+            return data
+
+        return self._describe_tracks(data.get('info'), data.get('video_tracks'),
+                                     data.get('audio_tracks'), data.get('subtitle_tracks'))
