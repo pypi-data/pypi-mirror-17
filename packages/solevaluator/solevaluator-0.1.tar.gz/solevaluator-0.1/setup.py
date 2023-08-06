13f9be15e235d810ff5c08d1bbe72e69a3431beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:12 +0000

    Add package solevaluator/0.1/solevaluator-0.1.tar.gz

---
+++ packages/solevaluator/solevaluator-0.1.tar.gz/solevaluator-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='solevaluator',
+      version='0.1',
+      description='Solution evaluator for the Benchmark Set of the ORSP (by Leeftink and Hans 2016).',
+      author='Panagiotis Giannakoulias',
+      author_email='panosgia87@hotmail.com',
+      license='Freeware',
+      packages=['solevaluator'],
+      zip_safe=False)
