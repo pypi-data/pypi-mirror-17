5e3d3b8d30cb372de33fe880ef2529d6a854e996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:11 +0000

    Add package django-dboptions/0.2.0/django-dboptions-0.2.0.tar.gz

---
+++ packages/django-dboptions/django-dboptions-0.2.0.tar.gz/django-dboptions-0.2.0/dboptions/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+from dboptions.models import Option
+
+__version__ = "0.2.0"
+__author__ = 'Alexandr Emelin'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Alexandr Emelin'
+
+default_app_config = 'dboptions.apps.DBOptionsConfig'
