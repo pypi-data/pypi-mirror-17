b93934d13828aae7f47b49937c84760610998b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:29 +0000

    Add package qiu_nester/1.1.0/qiu_nester-1.1.0.zip

---
+++ packages/qiu_nester/qiu_nester-1.1.0.zip/qiu_nester-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+      name         ="qiu_nester",
+      version      ="1.1.0",
+      py_modules   =["qiu_nester"],
+      author       ="qiuzhishan1991",
+      author_email ="qiuzhishan1991@163.com",
+      url          ="http://www.headfirstlabs.com",
+      description  ="A simple printer of nested lists",
+      )
