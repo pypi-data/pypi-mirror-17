61719245ecd6cc482dda3565a69a58ee919b8253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:47 +0000

    Add package aiozk/0.1.6/aiozk-0.1.6.tar.gz

---
+++ packages/aiozk/aiozk-0.1.6.tar.gz/aiozk-0.1.6/aiozk/recipes/base_watcher.py
@@ -0,0 +1,46 @@
+import asyncio
+import collections
+import logging
+
+from aiozk import exc
+
+from .recipe import Recipe
+
+
+log = logging.getLogger(__name__)
+
+
+class BaseWatcher(Recipe):
+
+    watched_event = None
+
+    def __init__(self, *args, **kwargs):
+        super(BaseWatcher, self).__init__(*args, **kwargs)
+        self.callbacks = collections.defaultdict(set)
+
+    def add_callback(self, path, callback):
+        self.callbacks[path].add(callback)
+
+        if len(self.callbacks[path]) == 1:
+            asyncio.ensure_future(self.watch_loop(path))
+
+    def remove_callback(self, path, callback):
+        self.callbacks[path].discard(callback)
+
+    async def fetch(self, path):
+        raise NotImplementedError
+
+    async def watch_loop(self, path):
+        while self.callbacks[path]:
+            wait = self.client.wait_for_event(self.watched_event, path)
+
+            log.debug("Fetching data for %s", path)
+            try:
+                result = await self.fetch(path)
+            except exc.NoNode:
+                return
+
+            await wait
+
+            for callback in self.callbacks[path]:
+                asyncio.ensure_future(callback(result))
