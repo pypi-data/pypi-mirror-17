48729e864097f1ee6905571c2214f43bfd499a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:27 +0000

    Add package sugartensor/0.0.1.2/sugartensor-0.0.1.2.tar.gz

---
+++ packages/sugartensor/sugartensor-0.0.1.2.tar.gz/sugartensor-0.0.1.2/sugartensor/temp.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# load data
+x = np.genfromtxt('/home/mansour/Downloads/sample.csv', delimiter=',', dtype=np.float32)
+x = x[1:, 1:]
+
+window = 384  # window size
+max = 3000  # max value
+
+# delete zero pad data
+n = ((np.where(np.any(x, axis=1))[0][-1] + 1) // window) * window
+x = x[:n] / max
+
+# make to matrix
+X = np.asarray([x[i:i+window] for i in range(n-window)])
+np.random.shuffle(X)
+
+# plot result
+_, ax = plt.subplots(10, 10, sharex=True, sharey=True)
+for i in range(10):
+    for j in range(10):
+        ax[i][j].plot(X[i * 10 + j, :, 0])
+        ax[i][j].plot(X[i * 10 + j, :, 1])
+        ax[i][j].set_axis_off()
+plt.savefig('/home/mansour/prj/sugartensor/sugartensor/example/asset/train/hyundai/original.png', dpi=600)
+plt.show()
+
+##
+
+import numpy as np
+import joblib
+import matplotlib.pyplot as plt
+
+
+# load data
+data = joblib.load('/shared/asan/dild/data/patch.manufac.20x20.pkl')['siemens']
+X = []
+for klass in data:
+    for im in klass:
+        X.append(im)
+X = np.asarray(X)
+
+# shuffle and dimension matching
+np.random.shuffle(X)
+
+# plot result
+_, ax = plt.subplots(10, 10, sharex=True, sharey=True)
+for i in range(10):
+    for j in range(10):
+        ax[i][j].imshow(X[i * 10 + j], 'gray')
+        ax[i][j].set_axis_off()
+plt.savefig('/home/mansour/prj/sugartensor/sugartensor/example/asset/train/dild/original.png', dpi=600)
+plt.show()
+
+##
+
+import nltk
+nltk.download()
+
+##
