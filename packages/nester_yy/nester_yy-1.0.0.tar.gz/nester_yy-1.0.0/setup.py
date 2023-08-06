4475ee085fea25a0078bf82c7cb105708bcfaa5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:25 +0000

    Add package nester_yy/1.0.0/nester_yy-1.0.0.tar.gz

---
+++ packages/nester_yy/nester_yy-1.0.0.tar.gz/nester_yy-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup( 
+    name = 'nester_yy',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'yuanye',
+    author_email = 'wildfire8966@gmail.com',
+    url = 'http://github.com/wildfre8966',
+    description = 'A sample printer for nested lists',
+)
