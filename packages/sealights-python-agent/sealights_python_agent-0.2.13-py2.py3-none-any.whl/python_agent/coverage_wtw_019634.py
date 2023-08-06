946c2774ab57c9b68fc267be405b5f6db3d325a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:30 +0000

    Add package sealights-python-agent/0.2.13/sealights_python_agent-0.2.13-py2.py3-none-any.whl

---
+++ packages/sealights-python-agent/sealights_python_agent-0.2.13-py2.py3-none-any.whl/python_agent/coverage_wtw_019634.py
@@ -0,0 +1,3 @@
+{None: {},
+ <MagicMock name='post()._resultForDoCleanups.nodeid.split().__getitem__()' id='140526656289360'>: {},
+ <MagicMock name='post().raise_for_status._resultForDoCleanups.nodeid.split().__getitem__()' id='140526641550224'>: {}}
