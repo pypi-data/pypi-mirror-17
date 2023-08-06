c6673fa269d642c9cafe821ba3d4b430ccdab01e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:48 +0000

    Add package blitzr/1.1.1/blitzr-1.1.1.tar.gz

---
+++ packages/blitzr/blitzr-1.1.1.tar.gz/blitzr-1.1.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Blitzr Python Client',
+    'author': 'Blitzr',
+    'url': 'https://github.com/blitzr/blitzr-python',
+    'download_url': 'https://github.com/blitzr/blitzr-python/tarball/1.1.1',
+    'author_email': 'contact@blitzr.com',
+    'version': '1.1.1',
+    'install_requires': ['requests'],
+    'packages': ['blitzr'],
+    'scripts': [],
+    'name': 'blitzr'
+}
+
+setup(**config)
