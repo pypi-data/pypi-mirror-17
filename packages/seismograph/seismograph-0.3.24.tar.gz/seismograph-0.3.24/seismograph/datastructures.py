ffc1ac914b7a45ff0229643f40a738689739af56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:17 +0000

    Add package seismograph/0.3.24/seismograph-0.3.24.tar.gz

---
+++ packages/seismograph/seismograph-0.3.24.tar.gz/seismograph-0.3.24/seismograph/datastructures.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+
+
+class DictObject(dict):
+
+    def __getattr__(self, item):
+        try:
+            return self[item]
+        except KeyError:
+            raise AttributeError(
+                '"{}" does not have "{}" attribute.'.format(self.__class__.__name__, item),
+            )
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __delattr__(self, item):
+        try:
+            del self[item]
+        except KeyError:
+            raise AttributeError(
+                '"{}" does not have "{}" attribute.'.format(self.__class__.__name__, item),
+            )
+
+    def copy(self):
+        new_dct = self.__class__()
+        new_dct.update(
+            super(DictObject, self).copy(),
+        )
+        return new_dct
+
+
+class Context(DictObject):
+    pass
+
+
+class NamedContext(Context):
+
+    def __init__(self, name, *args, **kwargs):
+        super(NamedContext, self).__init__(*args, **kwargs)
+
+        self.__name = name
+
+    def __repr__(self):
+        return '<NamedContext: {}>'.format(self.__name)
+
+    @property
+    def name(self):
+        return self.__name
