bde09e82056c0d94eef672f8eee66b4df0766d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:05 +0000

    Add package qcache-client/0.4.0/qcache-client-0.4.0.tar.gz

---
+++ packages/qcache-client/qcache-client-0.4.0.tar.gz/qcache-client-0.4.0/setup.py
@@ -0,0 +1,57 @@
+# -*- encoding: utf-8 -*-
+import glob
+import io
+import re
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ).read()
+
+__version__ = "0.4.0"
+
+setup(
+    name="qcache-client",
+    version=__version__,
+    license="BSD",
+    description="Python client library for QCache",
+    long_description="%s\n%s" % (read("README.rst"), re.sub(":obj:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst"))),
+    author="Tobias Gustafsson",
+    author_email="tobias.l.gustafsson@gmail.com",
+    url="https://github.com/tobgu/qcache-client",
+    packages=["qclient"],
+    py_modules=[splitext(basename(i))[0] for i in glob.glob("src/*.py")],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Utilities",
+    ],
+    keywords=[
+        # eg: "keyword1", "keyword2", "keyword3",
+    ],
+    install_requires=[
+        "requests>=2.7.0"
+    ],
+    extras_require={
+        # eg: 'rst': ["docutils>=0.11"],
+    }
+)
