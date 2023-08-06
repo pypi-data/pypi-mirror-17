e008a45b838c968ffe071125e73a0f0dc0928479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:17 +0000

    Add package decent-tools/0.2.8/decent-tools-0.2.8.tar.gz

---
+++ packages/decent-tools/decent-tools-0.2.8.tar.gz/decent-tools-0.2.8/dn/tc65i/at.py
@@ -0,0 +1,66 @@
+"""handle at command transmission and receiving the response."""
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+E_OK = 'OK'
+E_ERROR = 'ERROR'
+LE = '\r\n'
+
+AT_CONFIG = 'AT^SCFG'
+AT_Q = '?'
+
+
+class ATException(Exception):
+
+    """raised when AT command related exception occurs."""
+
+    pass
+
+
+def send(serdev, cmd, term_char=LE, timeout=None):
+    logger.debug('Sending {} to {}'.format(cmd, serdev.name))
+    if timeout:
+        old = serdev.timeout
+        serdev.timeout = timeout
+    try:
+        serdev.flushInput()
+        serdev.flushOutput()
+        serdev.write(cmd + term_char)
+        serdev.flush()
+        response = []
+        i = 0
+        while True:
+            line = serdev.readline()
+            logger.debug('AT reply[%d]: %s', i, line)
+            i += 1
+            if not line:
+                raise ATException(
+                    response if response
+                    else (
+                        'No reply from %s within %.1f sec, make sure tc65i'
+                        ' is powered, attached, or not running MIDlet'
+                        % (serdev.name, serdev.timeout))
+                    )
+            elif line == LE:
+                continue
+            elif line == E_OK + LE:
+                success = True
+                break
+            elif line == E_ERROR + LE:
+                success = False
+                break
+            else:
+                response.append(line.strip())
+        logger.debug('Result: ({}) {}'.format(success, response))
+        return success, response
+    finally:
+        if timeout:
+            serdev.timeout = old
+
+
+def getconfig(serdev, ret_success=False):
+    """read configuration of the modem."""
+    res = send(serdev, AT_CONFIG + AT_Q)
+    return res if ret_success else res[1]
