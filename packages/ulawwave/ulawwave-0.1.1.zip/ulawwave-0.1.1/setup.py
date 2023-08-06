d246490a5e04f535c73370a8e2dee16980766932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:03 +0000

    Add package ulawwave/0.1.1/ulawwave-0.1.1.zip

---
+++ packages/ulawwave/ulawwave-0.1.1.zip/ulawwave-0.1.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name = 'ulawwave',
+    modules = ['ulawwave',], # this must be the same as the name above
+    version = '0.1.1',
+    description = 'extension to the standard wave module to deal with g711 (ulaw) and alaw',
+    author = 'Eric Woudenberg, Harald Singer',
+    author_email = 'haraldgsinger@gmail.com',
+    url = 'https://github.com/haraldsinger/ulawwave', # use the URL to the github repo
+    download_url = 'https://github.com/haraldsinger/ulawwave', # I'll explain this in a second
+    keywords = ['audio', 'ulaw', 'alaw', 'g711', 'ivr' ], # arbitrary keywords
+    install_requires = [],
+    classifiers = [],
+)
