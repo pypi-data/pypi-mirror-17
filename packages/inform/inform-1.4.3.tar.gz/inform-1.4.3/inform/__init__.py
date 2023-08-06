7995742e626981fc584810ae394129fbc0134c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:08 +0000

    Add package inform/1.4.3/inform-1.4.3.tar.gz

---
+++ packages/inform/inform-1.4.3.tar.gz/inform-1.4.3/inform/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '1.4.3'
+
+from .inform import (
+    indent, cull, is_str, is_iterable, is_collection,
+    Color, fmt, os_error, conjoin, plural, full_stop, InformantGenerator, Inform,
+    log, comment, codicil, narrate, display, output, notify, debug, warn, error,
+    fatal, panic, done, terminate, terminate_if_errors, errors_accrued, Error
+)
