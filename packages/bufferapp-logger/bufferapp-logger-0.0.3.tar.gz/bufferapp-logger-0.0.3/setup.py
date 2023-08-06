ccb48114acd0bcbec1eefa676b86a7ab6ad7416b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:43 +0000

    Add package bufferapp-logger/0.0.3/bufferapp-logger-0.0.3.tar.gz

---
+++ packages/bufferapp-logger/bufferapp-logger-0.0.3.tar.gz/bufferapp-logger-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bufferapp-logger',
+    version='0.0.3',
+    url='https://github.com/bufferapp/python-logger',
+    download_url='https://github.com/bufferapp/python-logger/archive/master.zip',
+    author='Steven Cheng',
+    author_email='stevenc81@gmail.com',
+    description='Python Logger library for Bufferapp.',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    license='MIT',
+)
