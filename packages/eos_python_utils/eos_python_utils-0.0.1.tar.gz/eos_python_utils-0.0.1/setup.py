32c5790ec88f531323e0afa46c6f8fc17e1b503e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:36 +0000

    Add package eos_python_utils/0.0.1/eos_python_utils-0.0.1.tar.gz

---
+++ packages/eos_python_utils/eos_python_utils-0.0.1.tar.gz/eos_python_utils-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='eos_python_utils',
+    version='0.0.1',
+    url='https://github.com/travisoneill/python-utils-eos-redux',
+    description='python route handling for eos-redux',
+    author='Travis Oneill',
+    author_email='travis.h.oneill@gmail.com',
+    zip_safe=False
+)
