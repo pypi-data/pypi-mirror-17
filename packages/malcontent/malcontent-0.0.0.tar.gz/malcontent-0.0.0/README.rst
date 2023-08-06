27017e548c3697caf6cacc73c76fa2768758132c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:09 +0000

    Add package malcontent/0.0.0/malcontent-0.0.0.tar.gz

---
+++ packages/malcontent/malcontent-0.0.0.tar.gz/malcontent-0.0.0/README.rst
@@ -0,0 +1,32 @@
+Malcontent
+==========
+
+A cross-platform content editor.
+
+**Malcontent requires Python 3**
+
+**THIS IS A PLACEHOLDER PROJECT**
+
+At present, it has no functionality - it exists purely to reserve the PyPI namespace.
+
+Community
+---------
+
+Malcontent is part of the `BeeWare suite`_. You can talk to the community through:
+
+* `@pybeeware on Twitter`_
+
+* The `pybee/general`_ channel on Gitter.
+
+Contributing
+------------
+
+If you experience problems with this backend, `log them on GitHub`_. If you
+want to contribute code, please `fork the code`_ and `submit a pull request`_.
+
+.. _BeeWare suite: http://pybee.org
+.. _@pybeeware on Twitter: https://twitter.com/pybeeware
+.. _pybee/general: https://gitter.im/pybee/general
+.. _log them on Github: https://github.com/pybee/toga-winrt/issues
+.. _fork the code: https://github.com/pybee/toga-winrt
+.. _submit a pull request: https://github.com/pybee/toga-winrt/pulls
---
+++ packages/malcontent/malcontent-0.0.0.tar.gz/malcontent-0.0.0/setup.py
@@ -0,0 +1,46 @@
+#/usr/bin/env python
+import io
+import re
+from setuptools import setup, find_packages
+import sys
+
+
+if sys.version_info[:3] < (3, 4):
+    raise SystemExit("Toga requires Python 3.4+.")
+
+
+with io.open('./malcontent/__init__.py', encoding='utf8') as version_file:
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
+    if version_match:
+        version = version_match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+with io.open('README.rst', encoding='utf8') as readme:
+    long_description = readme.read()
+
+
+setup(
+    name='malcontent',
+    version=version,
+    description='A cross-platform content editor.',
+    long_description=long_description,
+    author='Russell Keith-Magee',
+    author_email='russell@keith-magee.com',
+    url='http://pybee.org/',
+    packages=find_packages(exclude=['tests']),
+    license='New BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        # 'Operating System :: Microsoft :: Windows :: Windows 8',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development',
+    ],
+    test_suite='tests',
+)
