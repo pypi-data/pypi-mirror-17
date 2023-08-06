634cd8883782216b0d00aced989529169d8b449c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:29 +0000

    Add package python-otcclient/0.3.8/python-otcclient-0.3.8.zip

---
+++ packages/python-otcclient/python-otcclient-0.3.8.zip/python-otcclient-0.3.8/otcclient/tests/0.1_test-output.sh
@@ -0,0 +1,6 @@
+source ./otcclient/tests/otcfunc.sh 
+
+apitest otc ecs describe-instances --output text
+apitest otc ecs describe-instances --output table
+apitest otc ecs describe-instances --output Json
+
---
+++ packages/python-otcclient/python-otcclient-0.3.8.zip/python-otcclient-0.3.8/otcclient/tests/3.4_test-ecs-delete-instance.sh
@@ -0,0 +1,4 @@
+source ./otcclient/tests/otcfunc.sh 
+
+apitest otc ecs delete-instances --instance-name instancename-public
+apitest otc ecs delete-instances --instance-ids b6c602b1-06d0-4bdb-b764-5d43b47abc14
