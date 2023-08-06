63c7b4257dcdc59d9558e0911adbe51595b222e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:53 +0000

    Add package pypack/0.1/pypack-0.1.zip

---
+++ packages/pypack/pypack-0.1.zip/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='pypack',
+    version='0.1',
+    author='__author__',
+    url='__url__',
+
+    description='Wrap Python module into executable .zip package.',
+    license='__license__',
+
+    py_modules=['pypack'],
+
+    classifiers=[
+        'Classifier: Programming Language :: Python :: 2',
+        'Classifier: Programming Language :: Python :: 3',
+    ],
+)
