a1074e1acbb29bef936b26390dee73ff30ef7681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:21 +0000

    Add package django-slack-oauth/0.6.1/django-slack-oauth-0.6.1.tar.gz

---
+++ packages/django-slack-oauth/django-slack-oauth-0.6.1.tar.gz/django-slack-oauth-0.6.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-slack-oauth',
+    version='0.6.1',
+    packages=[
+        'django_slack_oauth',
+        'django_slack_oauth.migrations',
+    ],
+    include_package_data=True,
+    license='MIT License',
+    description='Handles OAuth and stores slack token',
+    long_description=README,
+    author='Sergey Keller',
+    author_email='izdieu@gmail.com',
+    url='https://github.com/izdi/django-slack-oauth',
+    download_url='https://github.com/izdi/django-slack-oauth/tarball/0.6.1',
+    install_requires=[
+        'Django>=1.8',
+        'requests',
+        'jsonfield==1.0.3',
+    ],
+)
