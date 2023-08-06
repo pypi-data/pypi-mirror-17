9753e02d2c643a4b1e1c3d4f8aba7bb9ba379eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:51 +0000

    Add package yip/0.1/yip-0.1.tar.gz

---
+++ packages/yip/yip-0.1.tar.gz/yip-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='yip',
+    version='0.1',
+    description='A sample Python project',
+    url='https://github.com/balzss/yip',
+    author='balzss',
+    author_email='balazs.saros@gmail.com',
+    license='GPLv3',
+    keywords='pip frontend user-friendly CLI command-line',
+    scripts=['yip']
+)
