72d65def5a3455f7017425b9a52b59f5f02df5d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:15 +0000

    Add package colony_print/0.1.0/colony_print-0.1.0.tar.gz

---
+++ packages/colony_print/colony_print-0.1.0.tar.gz/colony_print-0.1.0/src/colony_print/controllers/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from . import base
+from . import document
+from . import printer
+
+from .base import BaseController
+from .document import DocumentController
+from .printer import PrinterController
