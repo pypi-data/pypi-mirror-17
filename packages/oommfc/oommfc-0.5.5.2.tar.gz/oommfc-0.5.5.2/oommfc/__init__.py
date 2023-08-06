44bd45fe0220d0c7b7651f31012b4d8df77a643f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:34 +0000

    Add package oommfc/0.5.5.2/oommfc-0.5.5.2.tar.gz

---
+++ packages/oommfc/oommfc-0.5.5.2.tar.gz/oommfc-0.5.5.2/oommfc/__init__.py
@@ -0,0 +1,30 @@
+import oommfc.oommf
+from .hamiltonian import Exchange, UniaxialAnisotropy, \
+    Demag, Zeeman, Hamiltonian
+from .dynamics import Precession, Damping, STT, Dynamics
+from .drivers import Driver, MinDriver, TimeDriver, HysteresisDriver
+from .mesh import Mesh
+from .system import System
+from micromagneticmodel.consts import mu0, e, muB, kB
+
+
+def test():
+    """Runs all the tests"""
+    print(oommfc.oommf.status(raise_exception=True))  # pragma: no cover
+    import pytest  # pragma: no cover
+    pytest.main(["-v", "--pyargs", "oommfc"])  # pragma: no cover
+
+
+def test_non_oommf():
+    """ Runs tests that do not need an OOMMF installation"""
+    import pytest  # pragma: no cover
+    pytest.main(["-m", "not oommf", "-v", "--pyargs", "oommfc"])  # pragma: no cover
+
+
+def test_oommf():
+    """Runs all tests that require an OOMMF installation."""
+    oommfc.oommf.status(raise_exception=True)  # pragma: no cover
+    import pytest  # pragma: no cover
+    pytest.main(["-m", "oommf", "-v", "--pyargs", "oommfc"])  # pragma: no cover
+
+    
---
+++ packages/oommfc/oommfc-0.5.5.2.tar.gz/oommfc-0.5.5.2/oommfc/oommf/oommf.py
@@ -0,0 +1,99 @@
+import os
+import signal
+import subprocess
+
+
+def status(varname, dockername, raise_exception):
+    # OOMMF status on host
+    returncode = call_host(varname=varname, argstr="+version")
+    if not returncode:
+        print("OOMMF on host machine status: OK")
+        host = True
+    else:
+        host = False
+        # Investigate the reason why OOMMF does not run.
+        oommfpath = os.getenv(varname)
+        if oommfpath is None:
+            print("Cannot find {} path.".format(varname))
+        elif not os.path.isfile(oommfpath):
+            print("{} path {} set to a non-existing "
+                  "file.".format(varname, oommfpath))
+        else:
+            print("{} path {} set to an existing "
+                  "file.".format(varname, oommfpath))
+            print("Something wrong with OOMMF installation.")
+
+    # Docker status
+    try:
+        subprocess.check_call([dockername, "--version"])
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        docker = False
+        print("Docker not installed/active.")
+    else:
+        print("Docker status: OK")
+        docker = True
+
+    # Raise exception if required
+    if not (host or docker) and raise_exception:
+        raise EnvironmentError("OOMMF and docker not found.")
+
+    return {"host": host, "docker": docker}
+
+
+def call(argstr, varname, dockername, raise_exception, dockerimage, where=None):
+    where = where_to_run(where, varname, dockername, raise_exception)
+
+    if status(varname=varname, dockername=dockername,
+              raise_exception=raise_exception)[where]:
+        if where == "host":
+            return call_host(varname=varname, argstr=argstr)
+        elif where == "docker":
+            return call_docker(dockername=dockername, dockerimage=dockerimage,
+                               argstr=argstr)
+
+
+def version(where=None, varname="OOMMFTCL", dockerimage="joommf/oommf"):
+    where = where_to_run(where=where, varname=varname,
+                         dockername="docker",
+                         raise_exception=True)
+    if where == "host":
+        cmd = ("tclsh", os.getenv(varname), "+version")
+        phost = subprocess.Popen(cmd, stderr=subprocess.PIPE)
+        out, err = phost.communicate()
+    else:
+        returncode = subprocess.call(["docker", "pull", "joommf/oommf"])
+        cmd = ("docker run --privileged -v {}:/io {} "
+               "/bin/bash -c \"tclsh /usr/local/oommf/oommf/oommf.tcl "
+               "+version\"").format(os.getcwd(), dockerimage)
+        pdocker = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
+        out, err = pdocker.communicate()
+
+    return err.decode().split("oommf.tcl")[-1].strip()
+
+
+def where_to_run(where, varname, dockername, raise_exception):
+    oommf_status = status(varname=varname, dockername=dockername,
+                          raise_exception=raise_exception)
+    if where is None:
+        if oommf_status["host"]:
+            return "host"
+        else:
+            return "docker"
+    else:
+        return where
+
+
+def call_host(varname, argstr):
+    cmd = ("tclsh", os.getenv(varname, "None"),
+           "boxsi", "+fg", argstr, "-exitondone", "1")
+    return subprocess.call(cmd)
+
+
+def call_docker(dockername, dockerimage, argstr):
+    returncode = subprocess.call([dockername, "pull", dockerimage])
+    cmd = ("{} run -v {}:/io {} /bin/bash -c \"tclsh "
+           "/usr/local/oommf/oommf/oommf.tcl boxsi +fg {} "
+           "-exitondone 1\"").format(dockername, os.getcwd(),
+                                     dockerimage, argstr)
+    print(cmd)
+    return subprocess.call(cmd, shell=True)
