3c6c3ccd53635cf50b65c79bd852fc42164af18b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:38 +0000

    Add package location/0.0.5/location-0.0.5.tar.gz

---
+++ packages/location/location-0.0.5.tar.gz/location-0.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup (
+    name             = 'location',
+    version          = '0.0.5',
+    keywords         = ('location'),
+    description      = '完善修复baidu接口',
+    license         = 'MIT License',
+    install_requires = [],
+
+    author           = 'leo',
+    author_email     = 'leo19920823@gmail.com',
+
+    packages         = find_packages(),
+    platforms        = 'any'
+)
