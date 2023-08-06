c46e7b367658c9cc3652ade7c2ee32f62bbb4f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:42 +0000

    Add package imhotep_bandit/0.0.4/imhotep_bandit-0.0.4-py2-none-any.whl

---
+++ packages/imhotep_bandit/imhotep_bandit-0.0.4-py2-none-any.whl/imhotep_bandit/plugin.py
@@ -0,0 +1,42 @@
+import re
+import json
+import os.path
+import textwrap
+from collections import defaultdict
+
+from imhotep.tools import Tool
+
+
+class Bandit(Tool):
+    def invoke(self, dirname, filenames, linter_configs):
+        retval = defaultdict(lambda: defaultdict(list))
+
+        if filenames:
+            files = ' '.join(filenames)
+        else:
+            files = '-r %s' % dirname
+
+        cmd = 'bandit -f json %s' % files
+        output = self.executor(cmd)
+        data = json.loads(output.decode('utf8'))
+
+        for result in data['results']:
+            line = str(result['line_number'])
+            message = self.format_message(result)
+            retval[result['filename']][line].append(message)
+
+        return self.to_dict(retval)
+
+    def format_message(self, report):
+        return textwrap.dedent("""
+            **{test_id}**: {issue_text}
+            Severity: {issue_severity}, Confidence: {issue_confidence}
+        """.format(**report))
+
+    def to_dict(self, d):
+        if not isinstance(d, dict):
+            return d
+
+        return {
+            k: self.to_dict(v) for k, v in d.items()
+        }
