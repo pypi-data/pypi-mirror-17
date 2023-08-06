f5075f09292e8b2edd8f27f91beecb81f8f78479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:54 +0000

    Add package fernet/1.0.1/fernet-1.0.1.tar.gz

---
+++ packages/fernet/fernet-1.0.1.tar.gz/fernet-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='fernet',
+      version='1.0.1',
+      description='A simple python fernet implementation',
+      author='Oz N Tiram',
+      author_email='oz.tiram@gmail.com',
+      url='https://github.com/oz123/python-fernet',
+      py_modules=['fernet'],
+      install_requires=['pyaes'],
+      tests_require=['pytest', 'cryptography'],
+      license='MIT',
+      platforms='any',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Topic :: Security :: Cryptography',
+                   ],
+      )
