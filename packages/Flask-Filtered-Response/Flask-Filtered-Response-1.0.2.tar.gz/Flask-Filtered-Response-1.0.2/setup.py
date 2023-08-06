c5da9cf0c8281feca76aad8298ecfd0e61a81363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:10 +0000

    Add package Flask-Filtered-Response/1.0.2/Flask-Filtered-Response-1.0.2.tar.gz

---
+++ packages/Flask-Filtered-Response/Flask-Filtered-Response-1.0.2.tar.gz/Flask-Filtered-Response-1.0.2/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Flask-Filtered-Response',
+    version='1.0.2',
+    url='https://github.com/vrcmarcos/flask-filtered-response',
+    license='MIT',
+    author='Marcos Cardoso',
+    author_email='vrcmarcos@gmail.com',
+    description='Add filter capability to JSON responses',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask>=0.11.1',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
