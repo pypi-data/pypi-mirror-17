dfcc114c88882b7124b248df0bd0940a79bbfc28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:23 +0000

    Add package 4scanner/1.3.2/4scanner-1.3.2.tar.gz

---
+++ packages/4scanner/4scanner-1.3.2.tar.gz/4scanner-1.3.2/test/test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from scanner import scanner, download
+
+# Test on scanner.py
+
+# Try getting /a/ catalog as json
+catalog_json = scanner.get_catalog_json("a", "4chan")
+
+# Try getting a list of threads
+list_of_threads = scanner.scan_thread("anime", catalog_json)
+for i in list_of_threads:
+    print(i)
+
+# try writting a test log file
+scanner.add_to_downloaded("139294614", "log", ".")
+
+# test on download.py
+
+# Try getting a web page text (test broken)
+#download.load("http://www.4chan.org")
