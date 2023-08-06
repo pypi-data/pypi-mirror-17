68dc1467259da232881c63ef0bdaf13ebb5fe68c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:46 +0000

    Add package seed-services-client/0.9.0/seed_services_client-0.9.0-py2-none-any.whl

---
+++ packages/seed-services-client/seed_services_client-0.9.0-py2-none-any.whl/seed_services_client/__init__.py
@@ -0,0 +1,17 @@
+"""Seed Services client library."""
+
+from .identity_store import IdentityStoreApiClient
+from .stage_based_messaging import StageBasedMessagingApiClient
+from .auth import AuthApiClient
+from .control_interface import ControlInterfaceApiClient
+from .hub import HubApiClient
+from .message_sender import MessageSenderApiClient
+from .scheduler import SchedulerApiClient
+
+__version__ = "0.9.0"
+
+__all__ = [
+    'IdentityStoreApiClient', 'StageBasedMessagingApiClient', 'AuthApiClient',
+    'ControlInterfaceApiClient', 'HubApiClient', 'MessageSenderApiClient',
+    'SchedulerApiClient',
+]
