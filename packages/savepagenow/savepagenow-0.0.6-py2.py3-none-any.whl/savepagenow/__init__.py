eccc395916a3ff84cd3b3d882eb5c3fe0953c14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:25 +0000

    Add package savepagenow/0.0.6/savepagenow-0.0.6-py2.py3-none-any.whl

---
+++ packages/savepagenow/savepagenow-0.0.6-py2.py3-none-any.whl/savepagenow/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from api import capture, capture_or_cache, CachedPage
+
+
+__version__ = "0.0.6"
+__all__ = (
+    'capture',
+    'capture_or_cache',
+    'CachedPage',
+)
