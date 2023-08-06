fddbd3318805b9e54941b053055b29482165865b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:20 +0000

    Add package dml/0.0.12.0/dml-0.0.12.0.zip

---
+++ packages/dml/dml-0.0.12.0.zip/dml-0.0.12.0/dml/__init__.py
@@ -0,0 +1,9 @@
+# Make the extended version of PyMongo available when the
+# module is loaded.
+from dml.dml import pymongo
+
+# Check command line arguments and expose them when the
+# module is loaded.
+from dml.dml import options, auth
+
+##eof
