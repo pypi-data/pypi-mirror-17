b422c76cc331694f92597e2d242e7f6281c807ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:49 +0000

    Add package osnap/0.0.4/osnap-0.0.4.tar.gz

---
+++ packages/osnap/osnap-0.0.4.tar.gz/osnap-0.0.4/osnap/const.py
@@ -0,0 +1,21 @@
+
+import os
+
+package_name = 'osnap'
+
+# this is the same as the launcher uses
+python_folder = 'osnapy'
+
+#python_path = os.path.join(python_folder, 'bin', 'python3')
+
+default_python_version = '3.5.2'
+
+program = 'main.py'
+
+windows_app_dir = 'osnapp'
+
+dist_dir = 'dist'
+
+CACHE_FOLDER = 'cache'
+TEMP_FOLDER = 'temp'
+
