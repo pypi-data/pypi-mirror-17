14064f7401da73cc8f4323c7033a1fb8ea4f14f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:30 +0000

    Add package slashlock/0.1.1/slashlock-0.1.1.tar.gz

---
+++ packages/slashlock/slashlock-0.1.1.tar.gz/slashlock-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='slashlock',
+    version='0.1.1',
+    description='Slashlock - A Simple File and Folder Encryptor',
+    url='https://github.com/wookalar/slashlock/blob/master/slashlock.py',
+    author='Wookalar Software',
+    author_email='doc.tart@wookalar.io',
+    license='AGPLv3',
+    entry_points={'gui_scripts': [
+        'slashlock = slashlock:main',
+    ]},
+    install_requires=[
+        'argon2_cffi>=16.2.0',
+        'cffi>=1.8.3',
+        'pkg-resources>=0.0.0',
+        'pycparser>=2.14',
+        'PyNaCl>=1.0.1',
+        'six>=1.10.0',
+    ],
+    install_package_data=False,
+    py_modules=['slashlock'],
+)
