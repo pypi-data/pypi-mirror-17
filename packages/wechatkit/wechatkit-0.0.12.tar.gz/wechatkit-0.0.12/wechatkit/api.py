fe87ed5383b866bd2491744d1cb58d719a1ab21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:37 +0000

    Add package wechatkit/0.0.12/wechatkit-0.0.12.tar.gz

---
+++ packages/wechatkit/wechatkit-0.0.12.tar.gz/wechatkit-0.0.12/wechatkit/api.py
@@ -0,0 +1,51 @@
+"""Wechatkit api."""
+from .basic import WechatBasicAPI
+from .user import WechatUserAPI
+from .exceptions import WechatException
+
+
+class WechatAPI(object):
+    """WechatAPI."""
+
+    @staticmethod
+    def _check_exception(result, raise_exception=False):
+        if raise_exception and 'errmsg' in result:
+            raise WechatException(result['errmsg'])
+
+    @staticmethod
+    def get_callbackip(access_token, raise_exception=False):
+        """Get callbackip."""
+        result = WechatBasicAPI.get_callbackip(access_token)
+        WechatAPI._check_exception(result, raise_exception=raise_exception)
+        return result
+
+    @staticmethod
+    def get_web_access_token(appid, appsecret, code, raise_exception=False):
+        """Get web access token."""
+        result = WechatBasicAPI.get_web_access_token(appid, appsecret, code)
+        WechatAPI._check_exception(result, raise_exception=raise_exception)
+        return result
+
+    @staticmethod
+    def get_access_token(appid, appsecret, raise_exception=False):
+        """Get access token."""
+        result = WechatBasicAPI.get_access_token(appid, appsecret)
+        WechatAPI._check_exception(result, raise_exception=raise_exception)
+
+        return result
+
+    @staticmethod
+    def get_user_info(access_token, openid, raise_exception=False):
+        """Get user info."""
+        result = WechatUserAPI.get_user_basic_info(access_token, openid)
+        WechatAPI._check_exception(result, raise_exception=raise_exception)
+        return result
+
+    @staticmethod
+    def get_user_list(access_token, next_openid=None, raise_exception=False):
+        """Get user list."""
+        result = WechatUserAPI.get_user_list(access_token,
+                                             next_openid=next_openid)
+        WechatAPI._check_exception(result, raise_exception=raise_exception)
+
+        return result
