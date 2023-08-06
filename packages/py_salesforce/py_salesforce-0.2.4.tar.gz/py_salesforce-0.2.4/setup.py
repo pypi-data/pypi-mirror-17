a08ece59502085db5c74264229a710c422ecfd81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:24 +0000

    Add package py_salesforce/0.2.4/py_salesforce-0.2.4.tar.gz

---
+++ packages/py_salesforce/py_salesforce-0.2.4.tar.gz/py_salesforce-0.2.4/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+#from setuptools import setup
+setup(
+  name = 'py_salesforce',
+  packages = ['py_salesforce'],
+  package_data={
+    'py_salesforce': ['py_salesforce.conf'],
+  },
+  version = '0.2.4',
+  description = ' A Python wrapper for the Salesforce REST API and SOQL.',
+  author = 'Mohamed AboElKheir',
+  author_email = 'mohamed.osama.aboelkheir@gmail.com',
+  url = 'https://github.com/mohamed-osama-aboelkheir/py_salesforce', 
+  download_url = 'https://github.com/mohamed-osama-aboelkheir/py_salesforce/tarball/0.2.4', 
+  keywords = ['salesforce', 'REST', 'API','SOQL'], 
+  install_requires=['httplib2'],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7'],
+)
+
