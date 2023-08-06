ca84cc0e40cfefa78d38072c07048178a296eca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:01 +0000

    Add package pycatan/0.11/pycatan-0.11.tar.gz

---
+++ packages/pycatan/pycatan-0.11.tar.gz/pycatan-0.11/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='pycatan',
+      version='0.11',
+      description='A Python Module for playing The Settlers of Catan',
+      url='https://github.com/josefwaller/PyCatan',
+      long_description=open("readme.md").read(),
+      author='Josef Waller',
+      author_email='josef@siriusapplications.com',
+      license='MIT',
+      install_requires=[
+            "quotequail"
+      ],
+      packages=['PyCatan'],
+      zip_safe=False)
