daefcc88c4c19244783ee5c5e536a7dec408d562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:59 +0000

    Add package exifgps/1.014/exifgps-1.014.tar.gz

---
+++ packages/exifgps/exifgps-1.014.tar.gz/exifgps-1.014/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#---------------------------------------------
+# Script:   
+# Name:     Javier
+# Date:        Wed Sep 14 15:40:56 BST 2016
+# Description:  
+#---------------------------------------------
+
+from setuptools import setup
+
+setup(name="exifgps",
+      version='1.014',
+      description="EXIF GPS information to Google Maps url",
+      url="http://github.com/quincymd/exifgps",
+      author="Cambell Spong",
+      author_email="quincymd@mail.com",
+      license="CC",
+      packages=["exifgps"],
+      zip_safe=False)
+
+
