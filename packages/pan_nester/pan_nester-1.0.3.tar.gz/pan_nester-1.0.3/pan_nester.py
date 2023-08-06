c8addaf0fb96c434781ceeec50c52dc365455a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:26 +0000

    Add package pan_nester/1.0.3/pan_nester-1.0.3.tar.gz

---
+++ packages/pan_nester/pan_nester-1.0.3.tar.gz/pan_nester-1.0.3/pan_nester.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+"""打印嵌套列表-递归"""
+def print_lol(the_list,indent=False,level=0):
+        for i in the_list:
+                if isinstance(i,list):
+                    print_lol(i,indent,level+1)
+                else:
+                	if indent:
+                		for tab_stop in range(level):
+							print("\t",end='')
+                    print(i)
---
+++ packages/pan_nester/pan_nester-1.0.3.tar.gz/pan_nester-1.0.3/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+		name = 'pan_nester',
+		version='1.0.3',
+		py_modules=['pan_nester'],
+		author='panjie',
+		description='A simple printer of nested lists'
+	)
