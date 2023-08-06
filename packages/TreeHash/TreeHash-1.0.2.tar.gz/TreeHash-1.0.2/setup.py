8319c37c7168ff39c49adf0c0a053404eeee3816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:40 +0000

    Add package TreeHash/1.0.2/TreeHash-1.0.2.tar.gz

---
+++ packages/TreeHash/TreeHash-1.0.2.tar.gz/TreeHash-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="TreeHash",
+    version="1.0.2",
+    packages=['treehash'],
+    scripts=['bin/treehash'],
+    license="Simplified BSD",
+    description="Tree Hash Calculator",
+    long_description=open('README.rst').read(),
+    url="http://github.com/jdswinbank/treehash",
+    author="John Swinbank",
+    author_email="john@treehash.swinbank.org"
+)
