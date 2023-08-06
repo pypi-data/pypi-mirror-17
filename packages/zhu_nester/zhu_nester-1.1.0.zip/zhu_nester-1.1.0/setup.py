869d7f82827c00f48129903b21a3dab40b1ecc98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:47 +0000

    Add package zhu_nester/1.1.0/zhu_nester-1.1.0.zip

---
+++ packages/zhu_nester/zhu_nester-1.1.0.zip/zhu_nester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'zhu_nester',
+    version = '1.1.0',
+    py_modules = ['nester'],
+    author = 'hfpython',
+    author_email = 'hfpython@headfirstlabs.com',
+    url = 'http://www.headfirstlabs.com',
+    desription = 'A simple printer of nested lists',
+    )
