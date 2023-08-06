8a01a7b8fb718c1d62f094bb4911b488bcaa693a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:52 +0000

    Add package nesterNHJ/1.3.0/nesterNHJ-1.3.0.zip

---
+++ packages/nesterNHJ/nesterNHJ-1.3.0.zip/nesterNHJ-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nesterNHJ',
+    version = '1.3.0',
+    py_modules = ['nesterNHJ'],
+    author = 'Hajeong Noh',
+    author_email = 'sciance_2002@yahoo.co.kr',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
