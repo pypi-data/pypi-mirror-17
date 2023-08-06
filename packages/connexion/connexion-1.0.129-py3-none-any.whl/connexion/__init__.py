f351006ec3500aca5581aff6c911e2c138cf5d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:32 +0000

    Add package connexion/1.0.129/connexion-1.0.129-py3-none-any.whl

---
+++ packages/connexion/connexion-1.0.129-py3-none-any.whl/connexion/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+Copyright 2015 Zalando SE
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+ language governing permissions and limitations under the License.
+"""
+
+from flask import (abort, request, send_file, send_from_directory,  # NOQA
+                   render_template, render_template_string, url_for)
+import werkzeug.exceptions as exceptions  # NOQA
+from .app import App  # NOQA
+from .api import Api  # NOQA
+from .problem import problem  # NOQA
+from .decorators.produces import NoContent  # NOQA
+from .resolver import Resolution, Resolver, RestyResolver  # NOQA
+
+# This version is replaced during release process.
+__version__ = '1.0.129'
