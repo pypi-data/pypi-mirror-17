031ab786e1d1b0a48734a56aad492eaffe211e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:45 +0000

    Add package MyCapytain/1.0.4/MyCapytain-1.0.4.tar.gz

---
+++ packages/MyCapytain/MyCapytain-1.0.4.tar.gz/MyCapytain-1.0.4/MyCapytain/retrievers/cts5.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+"""
+.. module:: MyCapytain.retrievers.cts5
+   :synopsis: CTS5 endpoint implementation
+
+.. moduleauthor:: Thibault Clérice <leponteineptique@gmail.com>
+
+
+"""
+import MyCapytain.retrievers.proto
+import requests
+
+
+class CTS(MyCapytain.retrievers.proto.CTS):
+
+    """ 
+        Basic integration of the MyCapytain.retrievers.proto.CTS abstraction
+    """
+
+    def __init__(self, endpoint, inventory=None):
+        """ API Prototype object
+
+        :param self: Object
+        :type self: API
+        :param endpoint: URL of the API
+        :type endpoint: str
+        :param inventory: Inventory to use
+        :type inventory: str
+
+        :ivar endpoint: Url of the endpoint
+        :ivar inventory: Default Inventory
+        """
+        super(CTS, self).__init__(endpoint)
+        self.inventory = inventory
+
+    def call(self, parameters):
+        """ Call an endpoint given the parameters
+        
+        :param parameters: Dictionary of parameters
+        :type parameters: dict
+        :rtype: text
+        """
+        # DEV !
+        parameters = {
+            key: str(parameters[key]) for key in parameters if parameters[key] is not None
+        }
+        if self.inventory is not None and "inv" not in parameters:
+            parameters["inv"] = self.inventory
+
+        request = requests.get(self.endpoint, params=parameters)
+        return request.text
+
+    def getCapabilities(self, inventory=None):
+        """ Retrieve the inventory information of an API 
+        
+        :param inventory: Name of the inventory
+        :type inventory: text
+        :rtype: str
+        """
+        return self.call({
+            "inv": inventory,
+            "request": "GetCapabilities"
+        })
+
+    def getValidReff(self, urn, inventory=None, level=None):
+        """ Retrieve valid urn-references for a text
+        
+        :param urn: URN identifying the text
+        :type urn: text
+        :param inventory: Name of the inventory
+        :type inventory: text
+        :param level: Depth of references expected
+        :type level: int
+        :return: XML Response from the API as string
+        :rtype: str
+        """
+        return self.call({
+            "inv": inventory,
+            "urn": urn,
+            "level": level,
+            "request": "GetValidReff"
+        })
+
+    def getFirstUrn(self, urn, inventory=None):
+        """ Retrieve the first passage urn of a text
+        
+        :param urn: URN identifying the text
+        :type urn: text
+        :param inventory: Name of the inventory
+        :type inventory: text
+        :rtype: str
+        """
+        return self.call({
+            "inv": inventory,
+            "urn": urn,
+            "request": "GetFirstUrn"
+        })
+
+    def getPrevNextUrn(self, urn, inventory=None):
+        """ Retrieve the previous and next passage urn of one passage
+        
+        :param urn: URN identifying the text's passage (Minimum depth : 1)
+        :type urn: text
+        :param inventory: Name of the inventory
+        :type inventory: text
+        :rtype: str
+        """
+        return self.call({
+            "inv": inventory,
+            "urn": urn,
+            "request": "GetPrevNextUrn"
+        })
+
+    def getLabel(self, urn, inventory=None):
+        """ Retrieve informations about a CTS Urn
+        
+        :param urn: URN identifying the text's passage (Minimum depth : 1)
+        :type urn: text
+        :param inventory: Name of the inventory
+        :type inventory: text
+        :rtype: str
+        """
+        return self.call({
+            "inv": inventory,
+            "urn": urn,
+            "request": "GetLabel"
+        })
+
+    def getPassage(self, urn, inventory=None, context=None):
+        """ Retrieve a passage
+        
+        :param urn: URN identifying the text's passage (Minimum depth : 1)
+        :type urn: text
+        :param inventory: Name of the inventory
+        :type inventory: text
+        :param context: Number of citation units at the same level of the citation hierarchy as the requested urn, immediately preceding and immediately following the requested urn to include in the reply
+        :type context: int
+        :rtype: str
+        """
+        return self.call({
+            "inv": inventory,
+            "urn": urn,
+            "context": context,
+            "request": "GetPassage"
+        })
+
+    def getPassagePlus(self, urn, inventory=None, context=None):
+        """ Retrieve a passage and informations about it
+        
+        :param urn: URN identifying the text's passage (Minimum depth : 1)
+        :type urn: text
+        :param inventory: Name of the inventory
+        :type inventory: text
+        :param context: Number of citation units at the same level of the citation hierarchy as the requested urn, immediately preceding and immediately following the requested urn to include in the reply
+        :type context: int
+        :rtype: str
+        """
+        return self.call({
+            "inv": inventory,
+            "urn": urn,
+            "context": context,
+            "request": "GetPassagePlus"
+        })
---
+++ packages/MyCapytain/MyCapytain-1.0.4.tar.gz/MyCapytain-1.0.4/README.rst
@@ -0,0 +1,35 @@
+.. image:: https://travis-ci.org/Capitains/MyCapytain.svg?branch=master 
+   :target: https://travis-ci.org/Capitains/MyCapytain
+.. image:: https://coveralls.io/repos/Capitains/MyCapytain/badge.svg?branch=master 
+   :target: https://coveralls.io/r/Capitains/MyCapytain?branch=master
+.. image:: https://gemnasium.com/Capitains/MyCapytain.svg 
+   :target: https://gemnasium.com/Capitains/MyCapytain
+.. image:: https://badge.fury.io/py/MyCapytain.svg 
+   :target: http://badge.fury.io/py/MyCapytain
+.. image:: https://readthedocs.org/projects/mycapytain/badge/?version=latest
+   :target: http://mycapytain.readthedocs.org
+.. image:: https://zenodo.org/badge/3923/Capitains/MyCapytain.svg
+   :target: https://zenodo.org/badge/latestdoi/3923/Capitains/MyCapytain
+.. image:: https://img.shields.io/pypi/dm/MyCapytain.svg
+   :target: https://pypi.python.org/pypi/MyCapytain
+.. image:: https://api.codacy.com/project/badge/grade/8e63e69a94274422865e4f275dbf08ea
+   :target: https://www.codacy.com/app/leponteineptique/MyCapytain
+MyCapytain is a python package which provides a large set of tools to deal with CTS and Capitains Guidelines in Python.
+
+Installation and Requirements
+#############################
+
+The best way to install MyCapytain is to use pip. MyCapytain tries to support both Python 2.7 and Python 3.4.
+
+.. code-block:: shell
+
+   pip install MyCapytain
+
+If you prefer to use setup.py, you should clone and use the following
+
+.. code-block:: shell
+
+   git clone https://github.com/Capitains/MyCapytain.git
+   cd MyCapytain
+   python setup.py install
+
