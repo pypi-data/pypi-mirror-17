d4638398056d862097231954ad8b40162b26abd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:44 +0000

    Add package chipymap/0.1/chipymap-0.1.tar.gz

---
+++ packages/chipymap/chipymap-0.1.tar.gz/chipymap-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='chipymap',
+      version='0.1',
+      description='geocode Chicago addresses',
+      url=' ',
+      author='Lingwei Cheng',
+      license="",
+      packages=['chipymap'],
+      zip_safe=False)
