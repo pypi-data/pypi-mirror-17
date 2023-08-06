420b9ce93cd981f376f58535f33f48c8021a8da2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:07 +0000

    Add package gnip-analysis-pipeline/0.2/gnip_analysis_pipeline-0.2.tar.gz

---
+++ packages/gnip-analysis-pipeline/gnip_analysis_pipeline-0.2.tar.gz/gnip_analysis_pipeline-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='gnip_analysis_pipeline',
+        packages=find_packages(),
+        scripts=['tweet_enricher.py', 
+            'tweet_time_series_builder.py', 
+            'tweet_evaluator.py',
+            'user_id_evaluator.py',
+            'tweet_engagements.py'
+            ],
+        version='0.2',
+        license='MIT',
+        author='Jeff Kolb',
+        author_email='jeffakolb@gmail.com',
+        description='A processing pipeline for enriching and aggregating Tweet data from Twitter APIs',
+        url='https://github.com/jeffakolb/Gnip-Analysis-Pipeline',
+        install_requires=['sngrams','pyyaml','requests','requests_oauthlib','pyfarmhash']  ,
+        extras_require={'plotting':['matplotlib'],'nltk':['nltk']}
+        )
