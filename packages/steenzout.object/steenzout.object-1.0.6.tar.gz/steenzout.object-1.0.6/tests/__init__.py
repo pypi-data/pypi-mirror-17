608557b49af22d6784112cb7cca975b0733f68d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:41 +0000

    Add package steenzout.object/1.0.6/steenzout.object-1.0.6.tar.gz

---
+++ packages/steenzout.object/steenzout.object-1.0.6.tar.gz/steenzout.object-1.0.6/tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: steenzout.object.tests
+    :synopsis: Test cases for the steenzout.object package.
+
+.. moduleauthor:: Pedro Salgado <steenzout@ymail.com>
+"""
