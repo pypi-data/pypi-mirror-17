b03102d7cf83c3ef51a459764e0894da1123eaf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:22 +0000

    Add package IdopTest/1.3/IdopTest-1.3.zip

---
+++ packages/IdopTest/IdopTest-1.3.zip/IdopTest-1.3/IdopTest.py
@@ -0,0 +1,5 @@
+ver = "1.3"
+print "IdopTest Init " + ver
+
+def getVersion():
+    print "IdopTest get Version " + ver
---
+++ packages/IdopTest/IdopTest-1.3.zip/IdopTest-1.3/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='IdopTest',
+      version='1.3',
+      py_modules=['IdopTest'],
+      )
