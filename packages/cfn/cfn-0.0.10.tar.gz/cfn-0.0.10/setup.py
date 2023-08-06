e807e66b1e68e11e2fecb2e995367fd3279cb448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:46 +0000

    Add package cfn/0.0.10/cfn-0.0.10.tar.gz

---
+++ packages/cfn/cfn-0.0.10.tar.gz/cfn-0.0.10/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open("README.rst", encoding="utf-8") as f:
+  long_description = f.read()
+
+setup(
+	name="cfn",
+	version="0.0.10",
+	description="Small script to manipulate AWS CloudFormation stacks",
+	long_description=long_description,
+	url="https://github.com/hagbarddenstore/cfn",
+	author="Kim Johansson",
+	author_email="hagbarddenstore@gmail.com",
+	license="MIT",
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.7"
+	],
+	keywords="aws cfn cloudformation",
+	packages=find_packages(exclude=["contrib", "docs", "tests"]),
+	install_requires=["boto3", "PyYAML", "jinja2"],
+	extras_require={},
+	package_data={
+		"": [ "*.md", "*.txt", "*.rst" ]
+	},
+	data_files=[],
+	entry_points={
+		"console_scripts": [
+			"cfn=cfn:main"
+		]
+	}
+)
