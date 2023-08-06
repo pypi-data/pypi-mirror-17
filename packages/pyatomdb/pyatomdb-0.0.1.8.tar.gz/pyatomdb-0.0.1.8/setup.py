c380a6cd825d7189f15b5ce036f386bc1b5b80e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:58 +0000

    Add package pyatomdb/0.0.1.8/pyatomdb-0.0.1.8.tar.gz

---
+++ packages/pyatomdb/pyatomdb-0.0.1.8.tar.gz/pyatomdb-0.0.1.8/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, Extension
+
+readme = open('README.txt', 'r')
+README_TEXT = readme.read()
+readme.close()
+
+linapprox =  Extension('linear_approx',\
+                       define_macros = [('MAJOR_VERSION', '1'),\
+                                        ('MINOR_VERSION','0')],\
+                       sources=['linear_approx.c'])
+
+setup(name='pyatomdb',
+      version='0.0.1.8',
+      description='AtomDB python library. This is a very early development version.',
+      url='http://www.atomdb.org',
+      author='Adam Foster',
+      author_email='afoster@cfa.harvard.edu',
+      license='Smithsonian',
+      packages=['pyatomdb'],
+      classifiers=['Development Status :: 2 - Pre-Alpha',\
+                   'Environment :: Console',\
+                   'Intended Audience :: Developers',\
+                   'Intended Audience :: Education',\
+                   'Intended Audience :: End Users/Desktop',\
+                   'Intended Audience :: Science/Research',\
+                   'Topic :: Scientific/Engineering :: Astronomy',\
+                   'Topic :: Scientific/Engineering :: Physics',\
+                   'Programming Language :: Python',\
+                   'Operating System :: POSIX'],
+      zip_safe=False,
+      long_description = README_TEXT,\
+      install_requires=[
+      "requests",\
+      "wget",\
+      "numpy",\
+      "scipy"],
+      ext_modules = [linapprox])
+
