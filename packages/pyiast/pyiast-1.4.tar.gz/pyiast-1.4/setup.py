f17d6669cc9ccc7734fd5c29c30e094b2cf09eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:42 +0000

    Add package pyiast/1.4/pyiast-1.4.tar.gz

---
+++ packages/pyiast/pyiast-1.4.tar.gz/pyiast-1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='pyiast',
+    version='1.4',
+    description='Ideal Adsorbed Solution Theory',
+    url='https://github.com/CorySimon/pyIAST',
+    download_url='https://github.com/CorySimon/pyIAST/tarball/master',
+    install_requires=['numpy', 'scipy', 'pandas', 'matplotlib'],
+    keywords='chemistry isotherm iast',
+    author='Cory M. Simon',
+    author_email='CoryMSimon@gmail.com',
+    license='MIT',
+    package_dir={'':'src'},
+    packages=[''],
+    zip_safe=False)
