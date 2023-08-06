1a9fa49653c5c91258a704a98fccd9c3fb47552b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:12 +0000

    Add package smac/0.1.2/smac-0.1.2.tar.gz

---
+++ packages/smac/smac-0.1.2.tar.gz/smac-0.1.2/setup.py
@@ -0,0 +1,36 @@
+import os
+import setuptools
+
+
+requirements = ['setuptools',
+                'numpy>=1.6.1',
+                'scipy>=0.13.1',
+                'pyrfr',
+                'ConfigSpace',
+                'pynisher>=0.4.1']
+
+setuptools.setup(
+    name="smac",
+    version="0.1.2",
+    author="Marius Lindauer, Matthias Feurer, Katharina Eggensperger, Aaron Klein, Stefan Falkner and Frank Hutter",
+    author_email="fh@cs.uni-freiburg.de",
+    description=("SMAC3, a Python implementation of 'Sequential Model-based "
+                 "Algorithm Configuration'."),
+    license="3-clause BSD",
+    keywords="machine learning algorithm configuration hyperparameter "
+             "optimization tuning",
+    url="",
+    packages=setuptools.find_packages(exclude=['test', 'source']),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+    ],
+    platforms=['Linux'],
+    install_requires=requirements,
+    tests_require=['mock',
+                   'nose'],
+    test_suite='nose.collector'
+)
