f070c3617d1d54eb63d80b1a1bc9cdc23f4f54e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:41 +0000

    Add package strato-skipper/1.0.1/strato-skipper-1.0.1.tar.gz

---
+++ packages/strato-skipper/strato-skipper-1.0.1.tar.gz/strato-skipper-1.0.1/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import find_packages
+from distutils.core import setup
+
+
+
+data_files = []
+# Add bash completion script in case we are running as root
+if os.getuid() == 0:
+    data_files=[
+        ('/etc/bash_completion.d', ['data/skipper-complete.sh'])
+    ]
+
+setup(
+    name='strato-skipper',
+    version='1.0.1',
+    url='http://github.com/Stratoscale/skipper',
+    author='Adir Gabai',
+    author_email='adir@stratoscale.com',
+    description='Easily dockerize your Git repository',
+    packages=find_packages(include=['skipper*']),
+    data_files=data_files,
+    entry_points={
+          'console_scripts': [
+              'skipper = skipper.main:main',
+          ],
+      },
+    install_requires=[
+            'PyYAML>=3.11',
+            'click>=6.6',
+            'requests>=2.10.0',
+            'tabulate>=0.7.5',
+        ]
+)
---
+++ packages/strato-skipper/strato-skipper-1.0.1.tar.gz/strato-skipper-1.0.1/skipper/runner.py
@@ -0,0 +1,59 @@
+import grp
+import logging
+import os
+import subprocess
+
+
+def run(command, fqdn_image=None, environment=None):
+    if fqdn_image is not None:
+        cwd = os.getcwd()
+        workspace = os.path.dirname(cwd)
+        project = os.path.basename(cwd)
+
+        uid = os.getuid()
+        gid = grp.getgrnam('docker').gr_gid
+        return _run_nested(workspace, project, uid, gid, fqdn_image, environment, command)
+    else:
+        return _run(command)
+
+
+def _run(cmd):
+    logger = logging.getLogger('skipper')
+    logger.debug(' '.join(cmd))
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+    return_code = 0
+    while True:
+        line = proc.stdout.readline()
+        return_code = proc.poll()
+        if line == '' and return_code is not None:
+            break
+        print(line.rstrip())
+
+    return return_code
+
+
+def _run_nested(workspace, project, uid, gid, fqdn_image, environment, command):
+    docker_cmd = ['docker', 'run']
+    docker_cmd += ['-t']
+    docker_cmd += ['--rm']
+    docker_cmd += ['--net', 'host']
+
+    environment = environment or []
+    for env in environment:
+        docker_cmd += ['-e', env]
+
+    volumes = [
+        '%(workspace)s:/workspace:rw,Z' % dict(workspace=workspace),
+        '/var/lib/osmosis:/var/lib/osmosis:rw,Z' % dict(workspace=workspace),
+        '/var/run/docker.sock:/var/run/docker.sock:Z',
+    ]
+    for volume in volumes:
+        docker_cmd += ['-v', volume]
+
+    docker_cmd += ['-u', '%(uid)d:%(gid)d' % dict(uid=uid, gid=gid)]
+    docker_cmd += ['-w', '%(workdir)s' % dict(workdir=os.path.join('/workspace', project))]
+    docker_cmd += ['--entrypoint', command[0]]
+    docker_cmd += [fqdn_image]
+
+    return _run(docker_cmd + command[1:])
