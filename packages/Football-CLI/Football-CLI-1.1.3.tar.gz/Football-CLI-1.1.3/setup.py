39765204a60bdcb666ae63fea1acbd4a74f30eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:17 +0000

    Add package Football-CLI/1.1.3/Football-CLI-1.1.3.tar.gz

---
+++ packages/Football-CLI/Football-CLI-1.1.3.tar.gz/Football-CLI-1.1.3/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='Football-CLI',
+    version='1.1.3',
+    packages=find_packages(),
+    url='https://github.com/jctissier/Football-CLI',
+    license='MIT',
+    author='Jean-Claude Tissier',
+    author_email='jeanclaude.tca@gmail.com',
+    entry_points = {
+        'console_scripts':[
+            'Football-CLI = Football_CLI.__main__:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3 :: Only',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    description='Football Stats & Live Streams links CLI from your terminal',
+    install_requires=[
+    'beautifulsoup4',
+    'praw>=3.5.0',
+    'termcolor==1.1.0',
+    'requests==2.10.0',
+    'lxml>=3.6.0'
+    ],
+    keywords='Football stats web scraper'
+)
