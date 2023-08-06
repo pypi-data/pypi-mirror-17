2d7f1391cfc9b85221b9ab5c8e66db752e101e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:03 +0000

    Add package django-constance/1.3/django-constance-1.3.tar.gz

---
+++ packages/django-constance/django-constance-1.3.tar.gz/django-constance-1.3/constance/__init__.py
@@ -0,0 +1,17 @@
+import django.dispatch
+from django.utils.functional import LazyObject
+
+__version__ = '1.3'
+
+default_app_config = 'constance.apps.ConstanceConfig'
+
+
+class LazyConfig(LazyObject):
+    def _setup(self):
+        from .base import Config
+        self._wrapped = Config()
+
+
+config = LazyConfig()
+
+updated_signal = django.dispatch.Signal(providing_args=['key', 'value'])
