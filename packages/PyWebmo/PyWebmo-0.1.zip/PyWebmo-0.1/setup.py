e54f166899b7fd5ba4981dee4c983581d668a382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:49 +0000

    Add package PyWebmo/0.1/PyWebmo-0.1.zip

---
+++ packages/PyWebmo/PyWebmo-0.1.zip/PyWebmo-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+  name = 'PyWebmo',
+  packages = ['PyWebmo'],
+  version = '0.1',
+  description = 'Webmo client library for Python',
+  author = 'Cidre Interaction Design Inc.',
+  author_email = 'info@cidre.tokyo',
+  url = 'https://github.com/cidreixd/webmo-library-python',
+  download_url = 'https://github.com/cidreixd/webmo-library-python/tarball/0.1',
+  keywords = ['webmo', 'websocket'],
+  license = 'MIT',
+  install_requires = ['websocket-client'],
+  classifiers = [],
+)
