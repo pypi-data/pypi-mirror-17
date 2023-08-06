9e198d132708ac9d614c4319db576d85cce8f415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:08 +0000

    Add package demandlib/0.1/demandlib-0.1.tar.gz

---
+++ packages/demandlib/demandlib-0.1.tar.gz/demandlib-0.1/setup.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+"""TODO: Maybe add a docstring containing a long description
+
+  This would double as something we could put int the `long_description`
+  parameter for `setup` and it would squelch some complaints pylint has on
+  `setup.py`.
+
+"""
+
+from setuptools import setup
+
+setup(name='demandlib',
+      version='0.1',
+      author='oemof developing group',
+      url='http://github.com/oemof/demandlib',
+      license='GPL3',
+      author_email='oemof@rl-institut.de',
+      description='Demandlib of the open energy modelling framework',
+      packages=['demandlib'],
+      package_dir={'demandlib': 'demandlib'},
+      install_requires=['numpy >= 1.7.0',
+                        'pandas >= 0.17.0']
+      )
