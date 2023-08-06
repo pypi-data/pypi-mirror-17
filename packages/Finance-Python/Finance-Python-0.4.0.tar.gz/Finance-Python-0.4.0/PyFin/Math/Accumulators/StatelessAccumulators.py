4ca0330c15c4f6e37f3645725c6099d939b1574d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:56 +0000

    Add package Finance-Python/0.4.0/Finance-Python-0.4.0.tar.gz

---
+++ packages/Finance-Python/Finance-Python-0.4.0.tar.gz/Finance-Python-0.4.0/PyFin/Math/Accumulators/StatelessAccumulators.py
@@ -0,0 +1,353 @@
+# -*- coding: utf-8 -*-
+u"""
+Created on 2015-7-25
+
+@author: cheng.li
+"""
+
+import math
+import numpy as np
+from PyFin.Math.Accumulators.IAccumulators import Accumulator
+
+
+def _checkParameterList(dependency):
+    if not isinstance(dependency, Accumulator) and len(dependency) > 1 and not isinstance(dependency, str):
+        raise ValueError("This value holder (e.g. Max or Minimum) can't hold more than 2 parameter names ({0})"
+                         " provided".format(dependency))
+
+
+class StatelessAccumulator(Accumulator):
+    def __init__(self, dependency='x'):
+        super(StatelessAccumulator, self).__init__(dependency)
+        self._returnSize = 1
+        self._window = 1
+        self._containerSize = 1
+
+    def push(self, data):
+        value = super(StatelessAccumulator, self).push(data)
+
+        try:
+            bool_flag = np.all(np.isnan(value))
+            if bool_flag:
+                return np.nan
+        except TypeError:
+            pass
+
+        self._isFull = 1
+        return value
+
+
+class Latest(StatelessAccumulator):
+    def __init__(self, dependency='x'):
+        super(Latest, self).__init__(dependency)
+        _checkParameterList(dependency)
+        self._returnSize = 1
+        self._latest = np.nan
+
+    def push(self, data):
+        value = super(Latest, self).push(data)
+        if np.isnan(value):
+            return np.nan
+        self._latest = value
+
+    def result(self):
+        return self._latest
+
+
+class Diff(StatelessAccumulator):
+    def __init__(self, dependency='x'):
+        super(Diff, self).__init__(dependency)
+        _checkParameterList(dependency)
+        self._returnSize = 1
+        self._diff = np.nan
+        self._curr = np.nan
+        self._previous = np.nan
+
+    def push(self, data):
+        value = super(Diff, self).push(data)
+        if np.isnan(value):
+            return np.isnan
+        self._previous = self._curr
+        self._curr = value
+
+    def result(self):
+        return self._curr - self._previous
+
+
+class SimpleReturn(StatelessAccumulator):
+    def __init__(self, dependency='x'):
+        super(SimpleReturn, self).__init__(dependency)
+        _checkParameterList(dependency)
+        self._returnSize = 1
+        self._diff = np.nan
+        self._curr = np.nan
+        self._previous = np.nan
+
+    def push(self, data):
+        value = super(SimpleReturn, self).push(data)
+        if np.isnan(value):
+            return np.isnan
+        self._previous = self._curr
+        self._curr = value
+
+    def result(self):
+        return self._curr / self._previous - 1.
+
+
+class LogReturn(StatelessAccumulator):
+    def __init__(self, dependency='x'):
+        super(LogReturn, self).__init__(dependency)
+        _checkParameterList(dependency)
+        self._returnSize = 1
+        self._diff = np.nan
+        self._curr = np.nan
+        self._previous = np.nan
+
+    def push(self, data):
+        value = super(LogReturn, self).push(data)
+        if np.isnan(value):
+            return np.isnan
+        self._previous = self._curr
+        self._curr = value
+
+    def result(self):
+        return math.log(self._curr / self._previous)
+
+
+class Positive(StatelessAccumulator):
+    def __init__(self, dependency='x'):
+        super(Positive, self).__init__(dependency)
+        _checkParameterList(dependency)
+        self._returnSize = 1
+        self._pos = np.nan
+
+    def push(self, data):
+        value = super(Positive, self).push(data)
+        if np.isnan(value):
+            return np.nan
+
+        if value > 0.:
+            self._pos = value
+        elif value <= 0.:
+            self._pos = 0.
+        else:
+            self._pos = np.nan
+
+    def result(self):
+        return self._pos
+
+
+class Negative(StatelessAccumulator):
+    def __init__(self, dependency='x'):
+        super(Negative, self).__init__(dependency)
+        _checkParameterList(dependency)
+        self._returnSize = 1
+        self._neg = np.nan
+
+    def push(self, data):
+        value = super(Negative, self).push(data)
+        if np.isnan(value):
+            return np.nan
+
+        if value < 0.:
+            self._neg = value
+        elif value >= 0.:
+            self._neg = 0.
+        else:
+            self._neg = np.nan
+
+    def result(self):
+        return self._neg
+
+
+class Max(StatelessAccumulator):
+    def __init__(self, dependency='x'):
+        super(Max, self).__init__(dependency)
+        _checkParameterList(dependency)
+        self._currentMax = np.nan
+        self._returnSize = 1
+        self._first = True
+
+    def push(self, data):
+        value = super(Max, self).push(data)
+        if np.isnan(value):
+            return np.nan
+        if self._first:
+            self._currentMax = value
+            self._first = False
+        else:
+            if self._currentMax < value:
+                self._currentMax = value
+
+    def result(self):
+        return self._currentMax
+
+
+class Minimum(StatelessAccumulator):
+    def __init__(self, dependency='x'):
+        super(Minimum, self).__init__(dependency)
+        _checkParameterList(dependency)
+        self._currentMin = np.nan
+        self._returnSize = 1
+        self._first = True
+
+    def push(self, data):
+        value = super(Minimum, self).push(data)
+        if np.isnan(value):
+            return np.nan
+        if self._first:
+            self._currentMin = value
+            self._first = False
+        else:
+            if self._currentMin > value:
+                self._currentMin = value
+
+    def result(self):
+        return self._currentMin
+
+
+class Sum(StatelessAccumulator):
+    def __init__(self, dependency='x'):
+        super(Sum, self).__init__(dependency)
+        _checkParameterList(dependency)
+        self._currentSum = np.nan
+        self._returnSize = 1
+        self._first = True
+
+    def push(self, data):
+        value = super(Sum, self).push(data)
+        if np.isnan(value):
+            return np.nan
+        if self._first:
+            self._currentSum = value
+            self._first = False
+        else:
+            self._currentSum += value
+
+    def result(self):
+        return self._currentSum
+
+
+class Average(StatelessAccumulator):
+    def __init__(self, dependency='x'):
+        super(Average, self).__init__(dependency)
+        _checkParameterList(dependency)
+        self._currentSum = np.nan
+        self._currentCount = 0
+        self._returnSize = 1
+
+    def push(self, data):
+        value = super(Average, self).push(data)
+        if np.isnan(value):
+            return np.nan
+        if self._currentCount == 0:
+            self._currentSum = value
+        else:
+            self._currentSum += value
+        self._currentCount += 1
+
+    def result(self):
+        try:
+            return self._currentSum / self._currentCount
+        except ZeroDivisionError:
+            return np.nan
+
+
+class XAverage(StatelessAccumulator):
+    def __init__(self, window, dependency='x'):
+        super(XAverage, self).__init__(dependency)
+        self._average = 0.0
+        self._exp = 2.0 / (window + 1.)
+        self._count = 0
+
+    def push(self, data):
+        value = super(XAverage, self).push(data)
+        if np.isnan(value):
+            return np.nan
+        if self._count == 0:
+            self._average = value
+        else:
+            self._average += self._exp * (value - self._average)
+        self._count += 1
+
+    def result(self):
+        return self._average
+
+
+class MACD(StatelessAccumulator):
+    def __init__(self, short, long, dependency='x'):
+        super(MACD, self).__init__(dependency)
+        self._short_average = XAverage(window=short, dependency=dependency)
+        self._long_average = XAverage(window=long, dependency=dependency)
+
+    def push(self, data):
+        self._short_average.push(data)
+        self._long_average.push(data)
+        if self._isFull == 0 and self._short_average.isFull and self._long_average.isFull:
+            self._isFull = 1
+
+    def result(self):
+        return self._short_average.result() - self._long_average.result()
+
+
+class Variance(StatelessAccumulator):
+    def __init__(self, dependency='x', isPopulation=False):
+        super(Variance, self).__init__(dependency)
+        _checkParameterList(dependency)
+        self._currentSum = 0.0
+        self._currentSumSquare = 0.0
+        self._currentCount = 0
+        self._isPop = isPopulation
+        self._returnSize = 1
+
+    def push(self, data):
+        value = super(Variance, self).push(data)
+        if np.isnan(value):
+            return np.nan
+        self._currentSum += value
+        self._currentSumSquare += value * value
+        self._currentCount += 1
+
+    def result(self):
+        tmp = self._currentSumSquare - self._currentSum * self._currentSum / self._currentCount
+
+        pop_num = self._currentCount if self._isPop else self._currentCount - 1
+
+        try:
+            return tmp / pop_num
+        except ZeroDivisionError:
+            return np.nan
+
+
+class Correlation(StatelessAccumulator):
+    def __init__(self, dependency=('x', 'y')):
+        super(Correlation, self).__init__(dependency)
+        self._runningSumLeft = 0.0
+        self._runningSumRight = 0.0
+        self._runningSumSquareLeft = 0.0
+        self._runningSumSquareRight = 0.0
+        self._runningSumCrossSquare = 0.0
+        self._currentCount = 0
+        self._returnSize = 1
+
+    def push(self, data):
+        value = super(Correlation, self).push(data)
+        if np.any(np.isnan(value)):
+            return np.nan
+        self._runningSumLeft = self._runningSumLeft + value[0]
+        self._runningSumRight = self._runningSumRight + value[1]
+        self._runningSumSquareLeft = self._runningSumSquareLeft + value[0] * value[0]
+        self._runningSumSquareRight = self._runningSumSquareRight + value[1] * value[1]
+        self._runningSumCrossSquare = self._runningSumCrossSquare + value[0] * value[1]
+        self._currentCount += 1
+
+    def result(self):
+        try:
+            n = self._currentCount
+            nominator = n * self._runningSumCrossSquare - self._runningSumLeft * self._runningSumRight
+            denominator = (n * self._runningSumSquareLeft - self._runningSumLeft * self._runningSumLeft) \
+                          * (n * self._runningSumSquareRight - self._runningSumRight * self._runningSumRight)
+            denominator = math.sqrt(denominator)
+            return nominator / denominator
+        except ZeroDivisionError:
+                return np.nan
---
+++ packages/Finance-Python/Finance-Python-0.4.0.tar.gz/Finance-Python-0.4.0/PyFin/tests/Math/Accumulators/testStatelessAccumulators.py
@@ -0,0 +1,224 @@
+# -*- coding: utf-8 -*-
+u"""
+Created on 2015-7-29
+
+@author: cheng.li
+"""
+
+import unittest
+import math
+import numpy as np
+from PyFin.Math.Accumulators.StatelessAccumulators import Average
+from PyFin.Math.Accumulators.StatelessAccumulators import XAverage
+from PyFin.Math.Accumulators.StatelessAccumulators import MACD
+from PyFin.Math.Accumulators.StatelessAccumulators import Max
+from PyFin.Math.Accumulators.StatelessAccumulators import Minimum
+from PyFin.Math.Accumulators.StatelessAccumulators import Diff
+from PyFin.Math.Accumulators.StatelessAccumulators import SimpleReturn
+from PyFin.Math.Accumulators.StatelessAccumulators import LogReturn
+from PyFin.Math.Accumulators.StatelessAccumulators import Sum
+from PyFin.Math.Accumulators.StatelessAccumulators import Variance
+from PyFin.Math.Accumulators.StatelessAccumulators import Correlation
+
+
+class TestStatelessAccumulators(unittest.TestCase):
+    def setUp(self):
+        np.random.seed(0)
+        self.samplesOpen = np.random.randn(1000)
+        self.samplesClose = np.random.randn(1000)
+
+    def testAverage(self):
+        average = Average(dependency='close')
+
+        for i, value in enumerate(self.samplesClose):
+            average.push(dict(close=value))
+            expected = np.mean(self.samplesClose[:i + 1])
+
+            calculated = average.result()
+            self.assertAlmostEqual(expected, calculated, 10, "at index {0:d}\n"
+                                                             "expected average:   {1:f}\n"
+                                                             "calculated average: {2:f}".format(i,
+                                                                                                  expected,
+                                                                                                  calculated))
+
+    def testXAverage(self):
+        xaverage = XAverage(window=5, dependency='close')
+        exp_weight = 2.0 / 6.0
+
+        for i, value in enumerate(self.samplesClose):
+            xaverage.push(dict(close=value))
+            if i == 0:
+                expected = self.samplesClose[i]
+            else:
+                expected += exp_weight * (self.samplesClose[i] - expected)
+
+            calculated = xaverage.result()
+            self.assertAlmostEqual(expected, calculated, 10, "at index {0:d}\n"
+                                                             "expected x-average:   {1:f}\n"
+                                                             "calculated x-average: {2:f}".format(i,
+                                                                                                  expected,
+                                                                                                  calculated))
+
+    def testMACD(self):
+        macd = MACD(short=5, long=10, dependency='close')
+        short_average = XAverage(window=5, dependency='close')
+        long_average = XAverage(window=10, dependency='close')
+
+        for i, value in enumerate(self.samplesClose):
+            macd.push(dict(close=value))
+            short_average.push(dict(close=value))
+            long_average.push(dict(close=value))
+            expected = short_average.result() - long_average.result()
+
+            calculated = macd.result()
+            self.assertAlmostEqual(expected, calculated, 10, "at index {0:d}\n"
+                                                             "expected x-average:   {1:f}\n"
+                                                             "calculated x-average: {2:f}".format(i,
+                                                                                                  expected,
+                                                                                                  calculated))
+
+    def testEMAMACD(self):
+        fast = 5
+        slow = 10
+        ema_window = 10
+        macd = MACD(fast, slow, 'close')
+        ema_macd = XAverage(ema_window, macd)
+
+        macd_diff = macd - ema_macd
+
+        for i, value in enumerate(self.samplesClose):
+            macd.push(dict(close=value))
+            ema_macd.push(dict(close=value))
+            macd_diff.push(dict(close=value))
+            expected = macd.value - ema_macd.value
+            calculated = macd_diff.value
+            self.assertAlmostEqual(expected, calculated, 10, "at index {0:d}\n"
+                                                             "expected ema macd diff:   {1:f}\n"
+                                                             "calculated ema macd diff: {2:f}".format(i, expected, calculated))
+
+    def testMax(self):
+        mm = Max(dependency='close')
+
+        for i, value in enumerate(self.samplesClose):
+            mm.push(dict(close=value))
+            expected = max(self.samplesClose[:i + 1])
+            calculated = mm.result()
+
+            self.assertAlmostEqual(expected, calculated, 10, "at index {0:d}\n"
+                                                             "expected max:   {1:f}\n"
+                                                             "calculated max: {2:f}".format(i, expected, calculated))
+
+    def testMinimum(self):
+        mm = Minimum(dependency='close')
+
+        for i, value in enumerate(self.samplesClose):
+            mm.push(dict(close=value))
+            expected = min(self.samplesClose[:i + 1])
+            calculated = mm.result()
+
+            self.assertAlmostEqual(expected, calculated, 10, "at index {0:d}\n"
+                                                             "expected min:   {1:f}\n"
+                                                             "calculated min: {2:f}".format(i, expected, calculated))
+
+    def testDiff(self):
+        mm = Diff(dependency='close')
+        current = np.nan
+        for i, value in enumerate(self.samplesClose):
+            mm.push(dict(close=value))
+            previous = current
+            current = value
+
+            if not np.isnan(previous):
+                expected = current - previous
+                calculated = mm.result()
+
+                self.assertAlmostEqual(expected, calculated, 10, "at index {0:d}\n"
+                                                                 "expected min:   {1:f}\n"
+                                                                 "calculated diff: {2:f}".format(i, expected,
+                                                                                                 calculated))
+
+    def testSimpleReturn(self):
+        mm = SimpleReturn(dependency='close')
+        current = np.nan
+        for i, value in enumerate(self.samplesClose):
+            mm.push(dict(close=value))
+            previous = current
+            current = value
+
+            if not np.isnan(previous):
+                expected = current / previous - 1.
+                calculated = mm.result()
+
+                self.assertAlmostEqual(expected, calculated, 10, "at index {0:d}\n"
+                                                                 "expected min:   {1:f}\n"
+                                                                 "calculated simple return: {2:f}".format(i, expected,
+                                                                                                          calculated))
+
+    def testLogReturn(self):
+        mm = LogReturn(dependency='close')
+        current = np.nan
+        for i, value in enumerate(self.samplesClose):
+            previous = current
+            current = abs(value)
+            mm.push(dict(close=current))
+
+            if not np.isnan(previous):
+                expected = math.log(current / previous)
+                calculated = mm.result()
+
+                self.assertAlmostEqual(expected, calculated, 10, "at index {0:d}\n"
+                                                                 "expected min:   {1:f}\n"
+                                                                 "calculated log return: {2:f}".format(i, expected,
+                                                                                                       calculated))
+
+    def testSum(self):
+        mm = Sum(dependency='close')
+
+        for i, value in enumerate(self.samplesClose):
+            mm.push(dict(close=value))
+            expected = np.sum(self.samplesClose[:i + 1])
+            calculated = mm.result()
+
+            self.assertAlmostEqual(expected, calculated, 10, "at index {0:d}\n"
+                                                             "expected sum:   {1:f}\n"
+                                                             "calculated sum: {2:f}".format(i, expected, calculated))
+
+    def testVariance(self):
+        # np.var is population variance
+        mm = Variance(dependency='close', isPopulation=True)
+        mm2 = Variance(dependency='close', )
+
+        for i, value in enumerate(self.samplesClose):
+            mm.push(dict(close=value))
+            mm2.push(dict(close=value))
+            expected = np.var(self.samplesClose[:i + 1])
+            calculated = mm.result()
+
+            if i == 0:
+                self.assertTrue(np.isnan(mm2.result()))
+
+            self.assertAlmostEqual(expected, calculated, 10, "at index {0:d}\n"
+                                                             "expected var:   {1:f}\n"
+                                                             "calculated var: {2:f}".format(i, expected, calculated))
+
+            if i >= 1:
+                calculated = mm2.result() * i / (i + 1)  # transform sample variance to population variance
+                self.assertAlmostEqual(expected, calculated, 10, "at index {0:d}\n"
+                                                                 "expected var:   {1:f}\n"
+                                                                 "calculated var: {2:f}".format(i, expected,
+                                                                                                calculated))
+
+    def testCorrelation(self):
+        mm = Correlation(dependency=['close', 'open'])
+
+        for i, (openPrice, closePrice) in enumerate(zip(self.samplesOpen, self.samplesClose)):
+            mm.push(dict(open=openPrice, close=closePrice))
+            if i == 0:
+                self.assertTrue(np.isnan(mm.result()))
+            if i >= 1:
+                expected = np.corrcoef(self.samplesOpen[:i + 1], self.samplesClose[:i + 1])[0, 1]
+                calculated = mm.result()
+                self.assertAlmostEqual(expected, calculated, 10, "at index {0:d}\n"
+                                                                 "expected correlation:   {1:f}\n"
+                                                                 "calculated correlation: {2:f}".format(i, expected,
+                                                                                                        calculated))
