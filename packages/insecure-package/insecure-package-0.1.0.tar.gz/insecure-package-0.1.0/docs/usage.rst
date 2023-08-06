bb1dfa834d0fc4dab2b806e548e8e06865271f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:28 +0000

    Add package insecure-package/0.1.0/insecure-package-0.1.0.tar.gz

---
+++ packages/insecure-package/insecure-package-0.1.0.tar.gz/insecure-package-0.1.0/docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use insecure package in a project::
+
+    import insecure_package
---
+++ packages/insecure-package/insecure-package-0.1.0.tar.gz/insecure-package-0.1.0/tests/test_insecure_package.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_insecure_package
+----------------------------------
+
+Tests for `insecure_package` module.
+"""
+
+
+import sys
+import unittest
+
+from insecure_package import insecure_package
+
+
+
+class TestInsecure_package(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
