3ff5f478c7eccca5a2f7073c17d444426b85cdcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:31 +0000

    Add package constructor-io/0.0.4/constructor-io-0.0.4.tar.gz

---
+++ packages/constructor-io/constructor-io-0.0.4.tar.gz/constructor-io-0.0.4/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+        name="constructor-io",
+        version="0.0.4",
+        license="MIT",
+        description="Constructor.IO Python Client",
+        author="Constructor.io",
+        author_email="info@constructor.io",
+        url="https://www.constructor.io",
+        install_requires=[
+            "requests",
+            "vcrpy"
+        ],
+        packages=["constructor_io"]
+    )
