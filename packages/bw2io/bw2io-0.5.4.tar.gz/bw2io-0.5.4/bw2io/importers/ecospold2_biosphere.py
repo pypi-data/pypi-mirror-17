083fa3a9c0a7ffdf3f7b127ad9c570fa373df836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:17 +0000

    Add package bw2io/0.5.4/bw2io-0.5.4.tar.gz

---
+++ packages/bw2io/bw2io-0.5.4.tar.gz/bw2io-0.5.4/bw2io/importers/ecospold2_biosphere.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+from eight import *
+
+from .base_lci import LCIImporter
+from ..strategies import drop_unspecified_subcategories, normalize_units
+from bw2data.utils import recursive_str_to_unicode
+from lxml import objectify
+import os
+
+EMISSIONS_CATEGORIES = {
+    "air":   "emission",
+    "soil":  "emission",
+    "water": "emission",
+}
+
+
+class Ecospold2BiosphereImporter(LCIImporter):
+    db_name = u'biosphere3'
+    format = u'Ecoinvent XML'
+
+    def __init__(self):
+        self.data = self.extract()
+        self.strategies = [
+            normalize_units,
+            drop_unspecified_subcategories,
+        ]
+
+    def extract(self):
+        def extract_flow_data(o):
+            ds = {
+                'categories': (
+                    o.compartment.compartment.text,
+                    o.compartment.subcompartment.text
+                ),
+                'code': o.get('id'),
+                'name': o.name.text,
+                'database': 'biosphere3',
+                'exchanges': [],
+                'unit': o.unitName.text,
+            }
+            ds[u"type"] = EMISSIONS_CATEGORIES.get(
+                ds['categories'][0], ds['categories'][0]
+            )
+            return ds
+
+        fp = os.path.join(
+            os.path.dirname(__file__),
+            "..", "data", "lci",
+            "ecoinvent elementary flows 3.3.xml"
+        )
+        root = objectify.parse(open(fp, encoding='utf-8')).getroot()
+        return recursive_str_to_unicode([extract_flow_data(ds)
+                                         for ds in root.iterchildren()])
