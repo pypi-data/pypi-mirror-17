fba6392ae8b76b19bdf2c490019da12bd02f55a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:42 +0000

    Add package Superdesk-Core/1.3rc2/Superdesk_Core-1.3rc2-py3-none-any.whl

---
+++ packages/Superdesk-Core/Superdesk_Core-1.3rc2-py3-none-any.whl/superdesk/macros/currency_test.py
@@ -0,0 +1,20 @@
+
+import unittest
+from .currency import usd_to_aud
+from .currency_usd_to_cad import usd_to_cad
+
+
+class CurrencyTestCase(unittest.TestCase):
+
+    def test_usd_to_aud(self):
+        item = {'body_html': '$100'}
+        res, diff = usd_to_aud(item)
+        aud = float(res['body_html'][3:])
+        self.assertGreater(aud, 100)  # if this fails, check first the currency ;)
+        self.assertGreater(float(diff['$100'].split(' ')[1]), 100)
+
+    def test_usd_to_cad(self):
+        item = {'body_html': '$100'}
+        res, diff = usd_to_cad(item)
+        cad = float(res['body_html'][3:])
+        self.assertGreater(cad, 100)  # if this fails, check first the currency ;)
