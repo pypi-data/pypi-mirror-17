6d5a55fbe9edcb5ec5f4103a77348d1afe496128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:53 +0000

    Add package alchimia/0.7.0/alchimia-0.7.0-py2.py3-none-any.whl

---
+++ packages/alchimia/alchimia-0.7.0-py2.py3-none-any.whl/alchimia/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, division
+
+from alchimia.strategy import TwistedEngineStrategy, TWISTED_STRATEGY
+
+
+__all__ = [
+    "TWISTED_STRATEGY",
+]
+
+TwistedEngineStrategy()
