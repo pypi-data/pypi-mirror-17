a1ca0ed485728741eda2f1d6977ad092fac1edd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:39 +0000

    Add package looplistawy/1.2.0/looplistawy-1.2.0.tar.gz

---
+++ packages/looplistawy/looplistawy-1.2.0.tar.gz/looplistawy-1.2.0/looplistawy.py
@@ -0,0 +1,9 @@
+def print_lol(the_list,level=0):
+"""Esta função serve para imprimir na tela todos os itens de uma lista, até mesmo listas aninhadas, isto é, elementos-listas que podem conter dentro de listas"""
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item,level+1)
+		else:
+                        for tab_stop in range(level):
+                                print("\t",end='')
+                        print(each_item)
