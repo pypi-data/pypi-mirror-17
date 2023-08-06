6a015b00b3ee68dfb59f49811107148effa2e4f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:45 +0000

    Add package infi.django_postgresql_migrations/0.2/infi.django_postgresql_migrations-0.2-py2.7.egg

---
+++ packages/infi.django_postgresql_migrations/infi.django_postgresql_migrations-0.2-py2.7.egg/infi/django_postgresql_migrations/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2"
+__git_commiter_name__ = "Itai Shirav"
+__git_commiter_email__ = "itais@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.2)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'ec1b7734e381db41e885cf6df4764ecb555a609e'
+__git_head_subject__ = 'Finished Release v0.2'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2016-10-05 16:49:37'
