473ec6a638d0010fc95e5a224c9dbc9cf19189eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:37 +0000

    Add package perl/0.1/perl-0.1.tar.gz

---
+++ packages/perl/perl-0.1.tar.gz/perl-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'perl',
+  packages = ['perl'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Perl',
+  author = 'Tim Bart',
+  author_email = 'tim@fewagainstmany.com',
+  url = 'https://github.com/pims/perl', # use the URL to the github repo
+  download_url = 'https://github.com/pims/perl/tarball/0.1', # I'll explain this in a second
+  keywords = ['perl', 'example'], # arbitrary keywords
+  classifiers = [],
+)
