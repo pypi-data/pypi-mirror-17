19b44231807e3254cfee0c1ab3028dce47fa3ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:57 +0000

    Add package MIDIUtil/1.1.0/MIDIUtil-1.1.0.tar.gz

---
+++ packages/MIDIUtil/MIDIUtil-1.1.0.tar.gz/MIDIUtil-1.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(name='MIDIUtil',
+      version='1.1.0',
+      description='A pure python library for creating multi-track MIDI files',
+      author='Mark Conway Wirt',
+      author_email='markcwirt@gmail.com',
+      license='MIT',
+      url='https://github.com/MarkCWirt/MIDIUtil',
+      packages=find_packages(where="src"),
+      package_dir = {'': 'src'},
+      package_data={
+          '' : ['License.txt', 'README.rst', 'documentation/*'],
+          'examples' : ['single-note-example.py', 'c-major-scale.py']},
+      include_package_data = True,
+      platforms='Platform Independent',
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'License :: OSI Approved :: MIT License',
+          ],
+      keywords = 'Music MIDI',
+      long_description='''
+This package provides a simple interface to allow Python programs to write multi-track MIDI files.'''
+     )
