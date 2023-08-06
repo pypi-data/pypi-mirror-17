374ba46394a905edd9f79a3122627c805e3eedcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:51 +0000

    Add package juicebox-cli/1.0.0/juicebox-cli-1.0.0.tar.gz

---
+++ packages/juicebox-cli/juicebox-cli-1.0.0.tar.gz/juicebox-cli-1.0.0/juicebox_cli/config.py
@@ -0,0 +1,6 @@
+NETRC_HOST_NAME = 'api.juiceboxdata.com'
+PUBLIC_API_URLS = {
+    'prod': 'https://api.juiceboxdata.com',
+    'dev': 'https://api-dev.juiceboxdata.com',
+    'hipaadev': 'https://api-dev.juiceboxdata.com',
+}
