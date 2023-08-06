1f6d9ccd9d7af708d262e784dceffaf06e9f87b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:08 +0000

    Add package engineUtils/0.3/engineUtils-0.3.zip

---
+++ packages/engineUtils/engineUtils-0.3.zip/engineUtils-0.3/setup.py
@@ -0,0 +1,18 @@
+'''
+    Created by tracyda on 
+    Mail tracyliubai@gmail.com
+    Site http://liu-bai.com
+'''
+from distutils.core import setup
+
+setup(
+    name='engineUtils',
+    version='0.3',
+    packages=['engineUtils'],
+    url='https://github.com/tracyda/engineUtils',
+    license='',
+    author='Tracy',
+    author_email='tracyliubai@gmail.com',
+    download_url='https://github.com/tracyda/engineUtils/tarball/0.2',
+    description=''
+)
