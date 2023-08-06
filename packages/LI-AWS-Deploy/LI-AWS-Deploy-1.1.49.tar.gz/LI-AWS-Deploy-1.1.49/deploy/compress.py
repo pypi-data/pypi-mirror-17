766fc9ca2cb5b6b034b9ad4b4465c7d3c9e0896f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:00 +0000

    Add package LI-AWS-Deploy/1.1.49/LI-AWS-Deploy-1.1.49.tar.gz

---
+++ packages/LI-AWS-Deploy/LI-AWS-Deploy-1.1.49.tar.gz/LI-AWS-Deploy-1.1.49/deploy/compress.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+from jsmin import jsmin
+from rcssmin import cssmin
+
+baseDirStatic = "./static/loja/estrutura/v1/"
+
+jsSources = [
+    "js/jquery-1.10.1.min.js",
+    "js/jquery-ui.js",
+    "js/bootstrap.min.js",
+    "js/css3-mediaqueries.js",
+    "js/jquery.flexslider-min.js",
+    "js/jquery.mask.min.js",
+    "js/modernizr.custom.17475.js",
+    "js/jquery.cookie.min.js",
+    "js/jquery.rwdImageMaps.min.js",
+    "js/main.js"
+]
+
+cssSources = [
+    "css/bootstrap.css",
+    "css/font-awesome.css",
+    "css/font-awesome-ie7.css",
+    "css/font-awesome-v4.css",
+    "css/flexslider.css",
+    "css/prettify.css",
+    "css/es-cus.css",
+    "css/style.css",
+    "css/cores.css"
+]
+
+
+def _minifyCSS(text):
+    return cssmin(text, keep_bang_comments=True)
+
+
+def _minifyJS(text):
+    return jsmin(text)
+
+
+def saveFile(function, sourcePaths, destPath, minPath, baseDir, header=None):
+    print("Gerando arquivos {} e {}".format(destPath, minPath))
+    f = open(destPath, 'w')
+    mf = None
+    try:
+        mf = open(minPath, 'w')
+        if header:
+            mf.write(header)
+        for srcFile in sourcePaths:
+            print(srcFile)
+            with open("{}{}".format(baseDir, srcFile)) as inputFile:
+                srcText = inputFile.read()
+                minText = function(srcText)
+            f.write(srcText)
+            mf.write(minText)
+    except:
+        print("Ocorreu um erro ao gerar o Minify")
+        return False
+    finally:
+        f.close()
+        if mf and not mf.closed:
+            mf.close()
+        return True
+
+
+def minifyJS(baseDir=baseDirStatic, source=jsSources):
+    jsDestPath = "{}js/all.js".format(baseDir)
+    jsMinPath = "{}js/all.min.js".format(baseDir)
+
+    print("Gerando arquivos {} e {}".format(jsDestPath, jsMinPath))
+    try:
+        with open(jsDestPath, 'w') as f:
+            for srcFile in source:
+                print(srcFile)
+                with open("{}{}".format(baseDir, srcFile)) as inputFile:
+                    srcText = inputFile.read()
+                f.write(srcText)
+
+        with open(jsMinPath, 'w') as min_file:
+            with open(jsDestPath) as js_file:
+                minified = jsmin(js_file.read(), quote_chars="'\"`")
+            min_file.write(minified)
+
+        return True
+    except:
+        print("Ocorreu um erro ao gerar o Minify")
+        return False
+
+
+def minifyCSS(baseDir=baseDirStatic, source=cssSources):
+    cssDestPath = "{}css/all.css".format(baseDir)
+    cssMinPath = "{}css/all.min.css".format(baseDir)
+    return saveFile(_minifyCSS, source, cssDestPath, cssMinPath, baseDir)
