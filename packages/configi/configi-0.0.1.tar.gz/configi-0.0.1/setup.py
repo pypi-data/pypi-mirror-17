9680b286a61620de6c8339e0288cf39d0cb9cde2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:50 +0000

    Add package configi/0.0.1/configi-0.0.1.tar.gz

---
+++ packages/configi/configi-0.0.1.tar.gz/configi-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import configi
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    author='Josef Lange',
+    author_email='josef.d.lange@me.com',
+    name='configi',
+    description='Configi is a straightforward wrapper for configuration data. It provides a consistent interface to a number of back-ends, currently via arbitrary HTTP JSON loading, Redis K/V store, and S3 JSON file loading.',
+    version=configi.VERSION,
+    py_modules=['configi'],
+    license='MIT',
+    long_description=long_description,
+    install_requires=[
+        'requests'
+    ],
+    url='https://github.com/josefdlange/configi',
+    test_suite='nose.collector',
+    tests_require=['nose']
+)
+
+# End
+
