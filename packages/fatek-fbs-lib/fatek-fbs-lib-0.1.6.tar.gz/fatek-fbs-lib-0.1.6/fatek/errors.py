dcf424bc43217cea6394402def6d4da4a5b61ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:27 +0000

    Add package fatek-fbs-lib/0.1.6/fatek-fbs-lib-0.1.6.tar.gz

---
+++ packages/fatek-fbs-lib/fatek-fbs-lib-0.1.6.tar.gz/fatek-fbs-lib-0.1.6/fatek/errors.py
@@ -0,0 +1,3 @@
+
+class InvalidTargetError(Exception):
+    pass
---
+++ packages/fatek-fbs-lib/fatek-fbs-lib-0.1.6.tar.gz/fatek-fbs-lib-0.1.6/fatek/fatek.py
@@ -0,0 +1,31 @@
+from pymodbus3.client.sync import ModbusTcpClient
+from target import FatekTarget
+
+
+class Fatek(object):
+
+    def __init__(self, address, logger=None):
+        if not logger:
+            import logging
+            self.logger = logging.getLogger(__name__)
+            self.logger.setLevel(logging.ERROR)
+        else:
+            self.logger = logger
+
+        self.address = address
+        self.client = ModbusTcpClient(address)
+
+    def read(self, symbol):
+        t = FatekTarget(self.client, symbol)
+        return t.read()
+
+    def write(self, symbol, value=True):
+        t = FatekTarget(self.client, symbol)
+        return t.write(value)
+
+    def bulk_read(self, symbol, count, current_value=False):
+        """
+            current_value is for reading numeric values from T and C registers
+        """
+        t = FatekTarget(self.client, symbol, current_value)
+        return t.read_all(int(count))
