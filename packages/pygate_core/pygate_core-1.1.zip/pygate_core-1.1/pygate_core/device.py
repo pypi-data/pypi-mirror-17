3dcec750468e0c325c55914b4da912351b956964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:04 +0000

    Add package pygate_core/1.1/pygate_core-1.1.zip

---
+++ packages/pygate_core/pygate_core-1.1.zip/pygate_core-1.1/pygate_core/device.py
@@ -0,0 +1,35 @@
+﻿#allows a device to manage it's cloud presence.
+
+from pygate_core import cloud
+
+
+class Device(object):
+    '''allows a device to manage it's cloud presence'''
+
+    def __init__(self, moduleName, deviceId):
+        result = super(Device, self).__init__()
+        self._deviceId = deviceId
+        self._moduleName = moduleName
+        return result
+
+    def addAsset(self, id, name, description, assetType, profile, style = "Undefined"):
+        """add asset"""
+        cloud.addAsset(self._moduleName, self._deviceId, id, name, description, assetType, profile, style)
+
+    def deleteAsset(self, id):
+        cloud.deleteAsset(self._moduleName, self._deviceId, id)
+
+    def createDevice(self, name, description):
+        """add device"""
+        cloud.addDevice(self._moduleName, self._deviceId, name, description)
+
+    def send(self, value, actuator):
+        cloud.send(self._moduleName, self._deviceId, actuator, value)
+
+    def getValue(self, asset):
+        """
+        get the current value for the specified asset.
+        :param asset: name of asset
+        :return:
+        """
+        return cloud.getAssetState(self._moduleName, self._deviceId, asset)
---
+++ packages/pygate_core/pygate_core-1.1.zip/pygate_core-1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pygate_core',
+    version='1.1',
+    packages=['pygate_core'],
+    url='https://github.com/allthingstalk/pygate-core',
+    license='',
+    author='Jan Bogaerts',
+    author_email='jb@allthingstalk.com',
+    description="core functions for the pygate application. This package contains the shared functionality of the gateway"
+)
