abcd45eeda2979533a5787c692273db454bb6ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:56 +0000

    Add package wagtail-utphy-richdocument/0.1a0/wagtail-utphy-richdocument-0.1a0.tar.gz

---
+++ packages/wagtail-utphy-richdocument/wagtail-utphy-richdocument-0.1a0.tar.gz/wagtail-utphy-richdocument-0.1a0/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='wagtail-utphy-richdocument',
+    version='0.1a',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='A Wagtail richdocument content type build with stremfield.',
+    long_description=README,
+    author='Julian Comanean',
+    author_email='icom@physics.utoronto.ca',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
