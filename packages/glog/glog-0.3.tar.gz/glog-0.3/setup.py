9b21c9cddfda99e1697d27e8b148bc99de558566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:23 +0000

    Add package glog/0.3/glog-0.3.tar.gz

---
+++ packages/glog/glog-0.3.tar.gz/glog-0.3/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+import setuptools
+
+VERSION = '0.3'
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setuptools.setup(
+    name='glog',
+    author='Benjamin Staffin',
+    author_email='benley@gmail.com',
+    url='https://github.com/benley/python-glog',
+    install_requires=['python-gflags>=3.0'],
+    description='Simple Google-style logging wrapper for Python.',
+    long_description=README,
+    py_modules=['glog'],
+    license='BSD',
+    test_suite='tests',
+    version=VERSION,
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: System :: Logging',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    platforms='any',
+)
