b65360e96342697170518fbf9e254e243b448970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:14 +0000

    Add package JIS_Nester/1.0.0/JIS_Nester-1.0.0.tar.gz

---
+++ packages/JIS_Nester/JIS_Nester-1.0.0.tar.gz/JIS_Nester-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+        name = 'JIS_Nester',
+        version = '1.0.0',
+        py_modules = ['JIS_Nester'],
+        author = 'jis',
+        author_email = 'abc@gmail.com',
+        url = 'http://www.google.com',
+        description = 'A simple printer of nested lists',
+        )
+
+        
+        
