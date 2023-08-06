3397b1cd6e809cb29f794dcc96e94af0a0834f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:17 +0000

    Add package nester_astutulus/1.1.0/nester_astutulus-1.1.0.tar.gz

---
+++ packages/nester_astutulus/nester_astutulus-1.1.0.tar.gz/nester_astutulus-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name			= 'nester_astutulus',
+	version			= '1.1.0',
+	py_modules		= ['nester'],
+	author			= 'astutulus',
+	author_email	= 'robinwootton@gmail.com',
+	url				= "http://www.robinwootton.co.uk",
+	description		= 'a simple printer of nests',
+)
