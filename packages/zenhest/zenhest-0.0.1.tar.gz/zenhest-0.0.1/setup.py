b66496f302db4e59c3c91b5ae9edc3ce2a94d60f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:04 +0000

    Add package zenhest/0.0.1/zenhest-0.0.1.tar.gz

---
+++ packages/zenhest/zenhest-0.0.1.tar.gz/zenhest-0.0.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='zenhest',
+      version='0.0.1',
+      description='Text based event console for Zenoss',
+      url='https://github.com/KimNorgaard/ZenHest',
+      author=u'Kim Nørgaard',
+      keywords='zenoss monitoring console',
+      author_email='jasen@jasen.dk',
+      license="MIT",
+      install_requires=['clipboard >= 0.0.4', 'keyring >= 9.3.1', 'urwid'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: System :: Systems Administration',
+          'Topic :: System :: Monitoring',
+          'Topic :: Utilities',
+      ],
+      packages=[],
+      scripts=['zenhest'],
+)
