82157d6a1c552b45ebcfd268d9f6bb1e19ddb943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:06 +0000

    Add package pyonlinedict/1.0.3/pyonlinedict-1.0.3.tar.gz

---
+++ packages/pyonlinedict/pyonlinedict-1.0.3.tar.gz/pyonlinedict-1.0.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""Setup script for the 'pyonlinedict' distribution."""
+
+from distutils.core import setup, Extension
+from setuptools import setup,find_packages
+
+setup (name = "pyonlinedict",
+       version = "1.0.3",
+       description = "Python online command-line dictionary",
+       url = "https://github.com/4ido10n/pyonlinedict",
+       author = "4ido10n",
+       long_description = "Python online command-line dictionary",
+       keywords = ("Python,online,command-line,dictionary"),
+       license = "GPL V3 License",
+       author_email = "4ido10n@gmail.com",
+       packages = find_packages(),
+       scripts = ['scripts/pyonlinedict.py'],
+       platforms = "linux"
+      )
