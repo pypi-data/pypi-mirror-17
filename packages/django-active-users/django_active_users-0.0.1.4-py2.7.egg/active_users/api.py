03b2c2b2569ed4b6aab8dd16ff82ca6e35b34204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:14 +0000

    Add package django-active-users/0.0.1.4/django_active_users-0.0.1.4-py2.7.egg

---
+++ packages/django-active-users/django_active_users-0.0.1.4-py2.7.egg/active_users/api.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+from django.utils.encoding import force_unicode
+from django_redis import get_redis_connection
+
+from .settings import KEY_CLASS
+
+
+def get_active_users_keys(pattern='au:*'):
+    """ Get all keys from redis """
+    return map(force_unicode, get_redis_connection().keys(pattern))
+
+
+def get_active_users_count():
+    """ Get redis keys count """
+    return len(get_active_users_keys())
+
+
+def get_active_users():
+    """ Get list of all active users """
+    return map(KEY_CLASS.key_to_dict, get_active_users_keys())
