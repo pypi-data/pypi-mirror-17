5705e9be61bb444edc3733dd888f1da7a0b6d74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:53 +0000

    Add package qpack/0.0.1/qpack-0.0.1.tar.gz

---
+++ packages/qpack/qpack-0.0.1.tar.gz/qpack-0.0.1/setup.py
@@ -0,0 +1,39 @@
+#
+#  Got documentation from: http://peterdowns.com/posts/first-time-with-pypi.html
+#
+#   1. Create tag:
+#       git tag 0.0.1 -m "Adds a tag so that we can put this new version on PyPI."
+#
+#   2. Push tag:
+#       git push --tags origin master
+#
+#   3. Upload your package to PyPI Test:
+#       python setup.py register -r pypitest
+#       python setup.py sdist upload -r pypitest
+#
+#   4. Upload to PyPI Live
+#       python setup.py register -r pypi
+#       python setup.py sdist upload -r pypi
+#
+
+from distutils.core import setup
+setup(
+    name='qpack',
+    packages=['qpack'],
+    version='0.0.1',
+    description='QPack (de)serializer',
+    author='Jeroen van der Heijden',
+    author_email='jeroen@transceptor.technology',
+    url='https://github.com/transceptor-technology/qpack',
+    download_url='https://github.com/transceptor-technology/qpack/qpack/0.0.1',
+    keywords=['serializer', 'deserializer'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
