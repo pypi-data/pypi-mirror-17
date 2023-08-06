e89d1fa4dc879cd9835c60b89e1995a5e01d9a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:20 +0000

    Add package simplads/0.32/simplads-0.32.tar.gz

---
+++ packages/simplads/simplads-0.32.tar.gz/simplads-0.32/simplads/simplad_bundle/add_data.py
@@ -0,0 +1,8 @@
+def add_data(*args):
+    keys = [i for i in args][::2]
+    vals = [i for i in args][1::2]
+    def func(i):
+        for a in range(len(keys)):
+            i[keys[a]] = vals[a]
+        return i
+    return func
