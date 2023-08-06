84360d5591a7ce7364a49dc210d02ff83742063e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:52 +0000

    Add package scikit-sos/0.1.1/scikit-sos-0.1.1.tar.gz

---
+++ packages/scikit-sos/scikit-sos-0.1.1.tar.gz/scikit-sos-0.1.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import sys
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(name='scikit-sos',
+      version='0.1.1',
+      description='An sklearn-compatible Python implementation of Stochastic Outlier Selection (SOS) for detecting outliers in data.',
+      long_description=README,
+      author='Jeroen Janssens',
+      author_email='jeroen@jeroenjanssens.com',
+      url='https://github.com/jeroenjanssens/sos',
+      license='BSD',
+      package_dir={'sos': 'sksos'},
+      packages=['sos'],
+      install_requires=['numpy'],
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={'console_scripts':
+          ['sos=sos:main']
+    }
+)
+
