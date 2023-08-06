34b5d31e40932465721f82db6bc98a636eb87c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:42 +0000

    Add package csc4175_nester/1.3.0/csc4175_nester-1.3.0.zip

---
+++ packages/csc4175_nester/csc4175_nester-1.3.0.zip/csc4175_nester-1.3.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+       name         ="csc4175_nester",
+       version      ="1.3.0",
+       py_modules   =["csc4175_nester"],
+       author       ="hyuan",
+       author_email ="hyuan@126.com",
+       url          ="http://www.headfirstlabs.com",
+       description  ="A simple printer of nested lists",
+       )
+
+
