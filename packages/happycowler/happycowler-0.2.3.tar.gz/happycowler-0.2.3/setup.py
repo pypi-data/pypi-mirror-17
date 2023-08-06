4fa25c4aec256a45775ffcb535b09842f7f05fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:44 +0000

    Add package happycowler/0.2.3/happycowler-0.2.3.tar.gz

---
+++ packages/happycowler/happycowler-0.2.3.tar.gz/happycowler-0.2.3/setup.py
@@ -0,0 +1,33 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='happycowler',
+    version='0.2.3',
+    author='Peter Wittek',
+    author_email='peterwittek@users.noreply.github.com',
+    scripts=['scripts/happycowler'],
+    url='https://github.com/peterwittek/happycowler/',
+    keywords=[
+        'vegan',
+        'vegetarian',
+     'happycow.net',
+     'crawler',
+     'kml',
+     'gpx'],
+    license='LICENSE',
+    description='Crawl the HappyCow database to GPX or KML files for offline use.',
+    long_description=open('README.rst').read(),
+    classifiers=[
+         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+         'Operating System :: OS Independent',
+         'Intended Audience :: End Users/Desktop',
+         'Development Status :: 4 - Beta',
+         'Programming Language :: Python'
+    ],
+    install_requires=[
+        "beautifulsoup4 >= 4"
+    ],
+)
