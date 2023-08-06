15ad9d7e5ad2dd5526a6471eb5249638c45c540f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:12 +0000

    Add package checkPy/0.2.5/checkPy-0.2.5-py2-none-any.whl

---
+++ packages/checkPy/checkPy-0.2.5-py2-none-any.whl/checkpy/assertlib.py
@@ -0,0 +1,30 @@
+import lib
+import re
+import os
+
+def exact(actual, expected):
+	return actual == expected
+
+def between(actual, lower, upper):
+	return lower <= actual <= upper
+	
+def ignoreWhiteSpace(actual, expected):        
+	return exact(lib.removeWhiteSpace(actual), lib.removeWhiteSpace(expected))
+
+def contains(actual, expectedElement):
+	return expectedElement in actual
+
+def containsOnly(actual, expectedElements):
+	return len(filter(lambda e : e not in expectedElements, actual)) == 0
+	
+def sameType(actual, expected):
+	return type(actual) is type(expected)
+
+def match(actual, expectedRegEx):
+	return True if re.match(expectedRegEx, actual) else False
+
+def sameLength(actual, expected):
+	return len(actual) == len(expected)
+
+def fileExists(fileName):
+	return os.path.isfile(fileName) 
