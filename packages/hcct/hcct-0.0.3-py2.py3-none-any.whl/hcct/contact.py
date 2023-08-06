868ed437a7a42e5c437149205f5e292d80d6408b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:58 +0000

    Add package hcct/0.0.3/hcct-0.0.3-py2.py3-none-any.whl

---
+++ packages/hcct/hcct-0.0.3-py2.py3-none-any.whl/hcct/contact.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from cnprep import Extractor
+from Hachi_filter import HachiFilter
+
+class Contact(HachiFilter):
+    """
+    Detect contact information.
+    """
+    def __init__(self, args=[]):
+        if args == []:
+            args = ['wechat', 'url', 'email', 'telephone', 'blur', 'QQ']
+        self.filter = Extractor(args=args, limit=6)
+
+    def predict(self, message, level=0):
+        contact = self.filter.extract(message)
+        for item in contact.values():
+            if item != []:
+                return True
+        return False
