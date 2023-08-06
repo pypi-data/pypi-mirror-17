12ae04c26bbc00d9358a93d1394d6e22f65d0fa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:09 +0000

    Add package bootable-usb/0.4/bootable-usb-0.4.tar.gz

---
+++ packages/bootable-usb/bootable-usb-0.4.tar.gz/bootable-usb-0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bootable-usb',
+    version='0.4',
+    scripts=['bootable-usb'],
+    url='https://www.github.com/balzss/bootable-usb',
+    license='GPLv3',
+    description='Wrapper utility around *dd* to put iso images on usb-s fast and effortless',
+    author='balzss',
+    author_email='balazs.saros@gmail.com',
+    keywords='dd usb iso boot'
+)
