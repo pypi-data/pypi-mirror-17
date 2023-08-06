1e1396d0764e67d7c730c2313951d208baa2e514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:56 +0000

    Add package xicam/1.2.12/xicam-1.2.12.tar.gz

---
+++ packages/xicam/xicam-1.2.12.tar.gz/xicam-1.2.12/pipeline/msg.py
@@ -0,0 +1,91 @@
+import logging
+import inspect
+import sys
+import time
+
+statusbar = None # Must be registered to output to a ui status bar
+progressbar = None
+
+stdch = logging.StreamHandler(sys.stdout)
+
+guilogcallable = None
+
+DEBUG = logging.DEBUG           # 10
+INFO = logging.INFO             # 20
+WARNING = logging.WARNING       # 30
+ERROR = logging.ERROR           # 40
+CRITICAL = logging.CRITICAL     # 50
+
+logbacklog=[]
+
+
+def showProgress(value, min=0, max=100):
+    if progressbar:
+        progressbar.show()
+        progressbar.setRange(min, max)
+        progressbar.setValue(value)
+
+
+def showBusy():
+    if progressbar:
+        progressbar.show()
+        progressbar.setRange(0, 0)
+
+
+def hideBusy():
+    if progressbar:
+        progressbar.hide()
+        progressbar.setRange(0, 100)
+
+
+hideProgress = hideBusy
+
+
+def showMessage(s,timeout=0):
+    if statusbar is not None:
+        statusbar.showMessage(s, timeout * 1000)
+
+    logMessage(s)
+
+def logMessage(stuple,level=INFO,loggername=None,timestamp=None):
+
+    # ATTENTION: loggername is 'intelligently' determined with inspect. You probably want to leave it None.
+    if loggername is not None:
+        loggername = inspect.stack()[1][3]
+    logger = logging.getLogger(loggername)
+    try:
+        stdch.setLevel(level)
+    except ValueError:
+        print stuple,level
+    logger.addHandler(stdch)
+
+    if timestamp is None: timestamp = time.asctime()
+
+    if type(stuple) is not tuple:
+        stuple=[stuple]
+
+    stuple = (unicode(s) for s in stuple)
+
+    s = ' '.join(stuple)
+    m = timestamp +'\t'+unicode(s)
+
+    logger.log(level,m)
+    if guilogcallable:
+        guilogcallable(level,timestamp,s)
+    else:
+        global logbacklog
+        logbacklog.append({'stuple':s,'level':level,'timestamp':timestamp})
+    try:
+        print m
+    except UnicodeEncodeError:
+        print 'A unicode string could not be written to console. Some logging will not be displayed.'
+
+def flushbacklog():
+    for l in logbacklog:
+        logMessage(**l)
+    global logbacklog
+    logbacklog=[]
+
+def clearMessage():
+    statusbar.clearMessage()
+
---
+++ packages/xicam/xicam-1.2.12.tar.gz/xicam-1.2.12/xicam/plugins/hipgisaxs/latvec.py
@@ -0,0 +1,53 @@
+import numpy as np
+import itertools
+
+
+def reciprocalvectors(a, b, c, order=2):
+    V = abs(np.dot(a, np.cross(b, c)))
+    mra = 2 * np.pi / V * np.cross(b, c)
+    mrb = 2 * np.pi / V * np.cross(c, a)
+    mrc = 2 * np.pi / V * np.cross(a, b)
+    mi = np.vstack([mra, mrb, mrc])
+    combs = itertools.product(range(-order, order + 1), repeat=3)
+    vecs = [np.sum((mi.T * np.array(comb)).T, axis=0) for comb in combs]
+    return vecs
+
+
+def latticevectors(a, b, c, zoffset, order=2, maxr=100000, maxz=100000):
+    mi = np.vstack([a, b, c])
+    combs = itertools.product(range(-order, order + 1), repeat=3)
+    vecs = [np.sum((mi.T * np.array(comb)).T, axis=0) for comb in combs]
+    vecs = [zoffsetvec(vec,zoffset) for vec in vecs if vec[2] >= 0 and np.sqrt(vec[0]**2+vec[1]**2)<=maxr and vec[2]<=maxz]
+    return vecs
+
+def zoffsetvec(vec,zoffset=0):
+    vec[2]=vec[2]+zoffset
+    return vec
+
+
+def latticelines(a, b, c, zoffset, order=2, maxr=None, maxz=None):
+    vecs = latticevectors(a, b, c, zoffset, order, maxr, maxz)
+
+    lines = []
+    for vec in vecs:
+        lines.extend([(vec, vec + a), (vec, vec - a), (vec, vec + b), (vec, vec - b), (vec, vec + c), (vec, vec - c)])
+
+    return lines
+
+
+def vecs2angles(v):
+    v = np.array(v)
+    vproj = v.copy()
+    vproj[:, 2] = 0
+    return np.arctan2(vproj[:, 1], vproj[:, 0]) / np.pi * 180.
+
+
+if __name__ == '__main__':
+    a = [1, 0, 0]
+    b = [.5, np.sqrt(3) / 2, 0]
+    c = [0, 0, 1]
+    recipvecs = np.array(reciprocalvectors(a, b, c))
+
+    print(list(set(vecs2angles(recipvecs))))
+
+    exit(0)
