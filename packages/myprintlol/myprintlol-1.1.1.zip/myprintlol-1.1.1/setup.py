b22a340884637f2949bd13a6a5687b8be67223ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:20 +0000

    Add package myprintlol/1.1.1/myprintlol-1.1.1.zip

---
+++ packages/myprintlol/myprintlol-1.1.1.zip/myprintlol-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='myprintlol',
+    version='1.1.1',
+    py_modules=['myprintlol'],
+    author='Eugene Klub',
+    author_email='evgeny.klub@yandex.ru',
+    url='',
+    description='A simple printer of nested lists'
+)
