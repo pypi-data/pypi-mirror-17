92bfc0dea457e1f78f68bca46de1ce9e108cc286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:36 +0000

    Add package Flask-CDN/1.5.1/Flask-CDN-1.5.1.tar.gz

---
+++ packages/Flask-CDN/Flask-CDN-1.5.1.tar.gz/Flask-CDN-1.5.1/setup.py
@@ -0,0 +1,42 @@
+"""
+Flask-CDN
+---------
+
+Serve the static files in your Flask app from a CDN.
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-CDN',
+    version='1.5.1',
+    url='https://github.com/wichitacode/flask-cdn',
+    license='MIT',
+    author='William Fagan',
+    author_email='will@wichitacode.com',
+    description='Serve the static files in your Flask app from a CDN.',
+    long_description=__doc__,
+    py_modules=['flask_cdn'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    test_suite='tests',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
