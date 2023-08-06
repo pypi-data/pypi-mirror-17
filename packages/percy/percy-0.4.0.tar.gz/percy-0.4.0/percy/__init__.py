f1e9ba784c157654da04eb0de0d90af8fad39eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:29 +0000

    Add package percy/0.4.0/percy-0.4.0.tar.gz

---
+++ packages/percy/percy-0.4.0.tar.gz/percy-0.4.0/percy/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Perceptual Inc.'
+__email__ = 'team@percy.io'
+__version__ = '0.4.0'
+
+from client import *
+from config import *
+from environment import *
+from resource import *
+from resource_loader import *
+from runner import *
