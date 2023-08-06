37ad09b2977cc45644879f3f9d2f674da2d57c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:24 +0000

    Add package dynamallow/0.0.2/dynamallow-0.0.2-py2.py3-none-any.whl

---
+++ packages/dynamallow/dynamallow-0.0.2-py2.py3-none-any.whl/dynamallow/__init__.py
@@ -0,0 +1,8 @@
+"""The base module namespace simply imports ``MarshModel`` from ``dynamallow.model`` so that you can:
+
+.. code-block:: python
+
+    from dynamallow import MarshModel
+
+"""
+from .model import MarshModel
