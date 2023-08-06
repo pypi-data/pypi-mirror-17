06ba7a423725213ad85159ad5c61e3d703dbfc6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:43 +0000

    Add package onegov.feriennet/0.0.3/onegov.feriennet-0.0.3-py3-none-any.whl

---
+++ packages/onegov.feriennet/onegov.feriennet-0.0.3-py3-none-any.whl/onegov/feriennet/path.py
@@ -0,0 +1,17 @@
+from onegov.feriennet import FeriennetApp
+from onegov.feriennet.collections import VacationActivityCollection
+from onegov.feriennet.models import VacationActivity
+
+
+@FeriennetApp.path(model=VacationActivityCollection, path='/angebote')
+def get_vacation_activities(request, app, page=0):
+    return VacationActivityCollection(
+        session=app.session(),
+        page=page,
+        identity=request.identity
+    )
+
+
+@FeriennetApp.path(model=VacationActivity, path='/angebot/{name}')
+def get_vacation_activity(app, name):
+    return VacationActivityCollection(app.session()).by_name(name)
