c814c85f355bbf5b83d7945e4a780cf15cdd9678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:31 +0000

    Add package nbconvert_watch/0.1.1/nbconvert_watch-0.1.1.tar.gz

---
+++ packages/nbconvert_watch/nbconvert_watch-0.1.1.tar.gz/nbconvert_watch-0.1.1/nbconvert_watch/command_line.py
@@ -0,0 +1,12 @@
+import nbconvert_watch
+import sys
+
+def main():
+    if len(sys.argv) != 3:
+        print('Welcome to nbconvert-watch! Please run "nbconvert-watch <notebook folder> <results folder>"')
+        print('Press Enter to continue...')
+        raw_input()
+        return 0
+
+    notebook_dir, results_dir = sys.argv[1:3]
+    nbconvert_watch.main(notebook_dir, results_dir)
