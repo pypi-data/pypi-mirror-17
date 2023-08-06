fb50968329508e480243fb8266c784eb47fd5abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:46 +0000

    Add package dnsknife/0.10/dnsknife-0.10.tar.gz

---
+++ packages/dnsknife/dnsknife-0.10.tar.gz/dnsknife-0.10/dnsknife/monkeypatch.py
@@ -0,0 +1,22 @@
+import dns.name
+import dns.rdata
+import dns.rdataclass
+import dns.rdatatype
+import dns.rdtypes.ANY.DNSKEY
+
+dns.rdatatype.CDNSKEY = 60
+dns.rdatatype._by_text['CDNSKEY'] = dns.rdatatype.CDNSKEY
+dns.rdatatype._by_value[60] = 'CDNSKEY'
+
+dns.rdata._rdata_modules[(dns.rdataclass.IN,
+                          dns.rdatatype.CDNSKEY)] = dns.rdtypes.ANY.DNSKEY
+dns.rdtypes.ANY.DNSKEY.CDNSKEY = dns.rdtypes.ANY.DNSKEY.DNSKEY
+
+if not hasattr(dns.rdatatype, 'URI'):
+    dns.rdatatype.URI = 256
+    dns.rdatatype._by_text['URI'] = dns.rdatatype.URI
+    dns.rdata.GenericRdata.target = property(lambda x: x.data[4:])
+
+# For pickle/unpickle
+
+del dns.name.Name.__setattr__
