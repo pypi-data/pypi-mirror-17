db99221f31c1333330ed04125350836064ed0ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:10 +0000

    Add package att_iot_gateway/0.5.4/att_iot_gateway-0.5.4.zip

---
+++ packages/att_iot_gateway/att_iot_gateway-0.5.4.zip/att_iot_gateway-0.5.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='att_iot_gateway',
+    version='0.5.4',
+    packages=['att_iot_gateway'],      # pip and setup tools are loaded in the virtual environment for the IDE.
+    install_requires='paho-mqtt',
+    url='https://github.com/allthingstalk/rpi-python-gateway-client',
+    license='MIT',
+    author='Jan Bogaerts',
+    author_email='jb@allthingstalk.com',
+	keywords = ['ATT', 'iot', 'internet of things', 'AllThingsTalk'],
+    description='This package provides device & asset management + data feed features for the AllThingsTalk platform to your application.'
+)
