564fce3f73beaa49c49db7330a443629223e459e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:22 +0000

    Add package joncol-nester/1.5.0/joncol-nester-1.5.0.zip

---
+++ packages/joncol-nester/joncol-nester-1.5.0.zip/joncol-nester-1.5.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup (
+	name		= 'joncol-nester',
+	version		= '1.5.0',
+	py_modules	= ['nester'],
+	author		= 'joncol',
+	author_email	= 'japp89@gmail.com',
+	url		= 'http://www.headfirstlabs.com',
+	description	= 'A simple printer of nested lists',
+	
+      ) 
