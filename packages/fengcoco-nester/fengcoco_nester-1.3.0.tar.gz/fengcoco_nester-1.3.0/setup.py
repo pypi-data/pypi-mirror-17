56dd1cd0b7b1d35b48ff440229c5eacdfe7e8937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:07 +0000

    Add package fengcoco-nester/1.3.0/fengcoco_nester-1.3.0.tar.gz

---
+++ packages/fengcoco-nester/fengcoco_nester-1.3.0.tar.gz/fengcoco_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'fengcoco_nester',
+    version      = '1.3.0',
+    author       = 'fengdanfeng',
+    py_modules   = ['fengcoco_nester'],
+    author_email = '811850534@qq.com',
+    url          = 'http://www.headfirstlabs.com',
+    description  = 'A simple printer of nested lists',
+)
