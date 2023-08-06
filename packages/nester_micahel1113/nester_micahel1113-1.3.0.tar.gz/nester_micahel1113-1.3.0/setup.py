eb8e65771dbe31942d0ee1eb22a6a23af6aa3f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:20 +0000

    Add package nester_micahel1113/1.3.0/nester_micahel1113-1.3.0.tar.gz

---
+++ packages/nester_micahel1113/nester_micahel1113-1.3.0.tar.gz/nester_micahel1113-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester_micahel1113',
+        version = '1.3.0',
+        py_modules = ['nester_micahel1113'],
+        author = 'michael_you',
+        author_email = 'youjingmao@gmail.com',
+        url = 'http://www.michaelyou.me',
+        description = 'A simple printer of nested lists',
+        )
