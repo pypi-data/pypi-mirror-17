190fd09e2b43c6a677321112f4b383a6f4d4c691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:03 +0000

    Add package undebt/0.6.2/undebt-0.6.2.tar.gz

---
+++ packages/undebt/undebt-0.6.2.tar.gz/undebt-0.6.2/undebt/cmd/main.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+import traceback
+
+from undebt.cmd import logger
+from undebt.cmd import logic
+from undebt.cmd.logger import log
+from undebt.pattern.interface import patterns_from_modules
+
+
+def _exit_fail_upon_error(func):
+
+    def try_except(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as err:
+            log.exception(str(err))
+            sys.exit(1)
+
+    return try_except
+
+
+@_exit_fail_upon_error
+def _load_text(path):
+    if path is None:
+        file_obj = sys.stdin
+    else:
+        file_obj = open(path, 'r')
+
+    try:
+        return file_obj.read()
+    finally:
+        if path:
+            file_obj.close()
+
+
+@_exit_fail_upon_error
+def _write_result_text(result_text, path, dry_run):
+    if not dry_run and path:
+        with open(path, 'w') as file_obj:
+            file_obj.write(result_text)
+    else:
+        if path:
+            print('>>> {}'.format(path), file=sys.stderr)
+        sys.stdout.write(result_text)
+
+
+@_exit_fail_upon_error
+def _handle_arguments():
+    parser = argparse.ArgumentParser(prog='undebt')
+    parser.add_argument(
+        'files', nargs='*', metavar='FILE',
+        help='files to be modified; uses stdin if not passed',
+    )
+    parser.add_argument(
+        '--pattern', '-p', metavar='MODULE', action='append', required=True,
+        help='pattern definition modules',
+    )
+    parser.add_argument(
+        '--verbose', '-v', action='store_true', default=False,
+        help='verbose logging for troubleshooting',
+    )
+    parser.add_argument(
+        '--dry-run', '-d', action='store_true', default=False,
+        help='only print to stdout; do not overwrite files',
+    )
+    return parser.parse_args()
+
+
+@_exit_fail_upon_error
+def load_patterns(pattern_files):
+    return patterns_from_modules(pattern_files)
+
+
+def process(patterns, text_file, dry_run):
+    log.info('undebting {}'.format(text_file))
+
+    text = _load_text(text_file)
+
+    try:
+        result_text = logic.process(patterns, text)
+    except Exception:
+        log.exception(traceback.format_exc())
+        return False
+    else:
+        if result_text != text:
+            _write_result_text(result_text, text_file, dry_run)
+        return True
+
+
+def main():
+    """Handle and process arguments from sys.argv."""
+    args = _handle_arguments()
+
+    logger.setup(args.verbose)
+    patterns = load_patterns(args.pattern)
+    files = args.files
+
+    if not files:
+        log.info('running in stdin/stdout mode')
+        process(patterns, None, args.dry_run)
+        return
+
+    log.info('running across {} file(s)'.format(len(files)))
+    for f in files:
+        process(patterns, f, args.dry_run)
---
+++ packages/undebt/undebt-0.6.2.tar.gz/undebt-0.6.2/undebt/pattern/interface.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+
+from pyparsing import _trim_arity
+
+from undebt.cmd.logger import log
+from undebt.pattern.util import attach
+from undebt.pattern.util import tokens_as_list
+
+
+# required at the beginning of a string to be able to properly parse it
+#  " " is necessary to make START_OF_FILE work properly
+PARSING_PREFIX = " "
+
+
+def parse_grammar(grammar, text):
+    """Scan text and find matches for grammar."""
+    formatted_text = PARSING_PREFIX + text
+    results = grammar.parseWithTabs().scanString(formatted_text)
+    return [(tokens, _fix_index(start), _fix_index(stop)) for tokens, start, stop in results]
+
+
+def _fix_index(index):
+    index -= len(PARSING_PREFIX)
+    return index if index >= 0 else 0
+
+
+def get_patterns(*pattern_modules):
+    """Returns patterns for pattern modules."""
+    return [_get_patterns(p) for p in pattern_modules]
+
+
+def _get_patterns(pattern):
+    if hasattr(pattern, "patterns"):
+        return pattern.patterns
+
+    if hasattr(pattern, "grammar") and hasattr(pattern, "replace"):
+        patterns = [(pattern.grammar, pattern.replace)]
+        if hasattr(pattern, "extra"):
+            patterns.append(get_pattern_for_extra(pattern.extra))
+        return patterns
+
+    log.error(
+        'pattern file {} must define either ("patterns") or ("grammar" and "replace" and optionally "extra")'
+        .format(pattern.__name__)
+    )
+    sys.exit(1)
+
+
+def get_pattern_for_extra(extra):
+    """Returns a pattern object for an extra string."""
+    from undebt.pattern.lang.python import HEADER
+
+    @tokens_as_list(assert_len=1)
+    def extra_replace(tokens):
+        if extra not in tokens[0]:
+            return tokens[0] + extra + "\n"
+        else:
+            return None
+
+    return HEADER, extra_replace
+
+
+def patterns_from_modules(pattern_modules):
+    """Returns patterns for pattern files."""
+    return get_patterns(*(load_module(pattern_module) for pattern_module in pattern_modules))
+
+
+def load_module(module):
+    """Loads a module from its name."""
+    return __import__(module, fromlist=[''])
+
+
+def create_find_and_replace(grammar, replace):
+    """Creates a find-and-replace grammar."""
+    return attach(grammar, lambda s, l, t: [_trim_arity(replace)(s, l, t)])
