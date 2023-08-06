492028e0fd6a366ee21f72a2d6f6ed7cece5995f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:50 +0000

    Add package django-datawatch/0.1.10/django_datawatch-0.1.10-py2-none-any.whl

---
+++ packages/django-datawatch/django_datawatch-0.1.10-py2-none-any.whl/django_datawatch/settings.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+from django.conf import settings
+
+USER_SETTINGS = getattr(settings, 'DJANGO_DATAWATCH', {})
+
+DEFAULT_SETTINGS = dict(
+    QUEUE_NAME='django_datawatch',
+    BACKEND='django_datawatch.backends.synchronous')
+
+ddw_settings = DEFAULT_SETTINGS.copy()
+ddw_settings.update(USER_SETTINGS)
+ddw_settings = type('settings', (object, ), ddw_settings)
