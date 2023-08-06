b16691889fb18713eda632bd31b47fd6dbeecb00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:51 +0000

    Add package food_network_wrapper/0.5/food_network_wrapper-0.5.tar.gz

---
+++ packages/food_network_wrapper/food_network_wrapper-0.5.tar.gz/food_network_wrapper-0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name = 'food_network_wrapper',
+    packages = ['food_network_wrapper'], 
+    version = '0.5',
+    description = 'Allows you to easily search and scrape recipes from http://foodnetwork.com',
+    author = 'Ben Awad',
+    author_email = 'benawad97@gmail.com',
+    url = 'https://github.com/benawad/food_network_wrapper', 
+    download_url = 'https://github.com/benawad/food_network_wrapper/tarball/0.4',
+    keywords = ['cooking', 'recipes', 'recipe', 'python', 'harvest', 'foodnetwork', 'scrape', 'scraping'], 
+    install_requires=[
+        "beautifulsoup4",
+        "lxml",
+        "requests",
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
