f36d3f8abf14ff93757b8facafc8872bf9c59fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:10 +0000

    Add package trustedanalytics/0.7.2.dev20161005729/trustedanalytics-0.7.2.dev20161005729.tar.gz

---
+++ packages/trustedanalytics/trustedanalytics-0.7.2.dev20161005729.tar.gz/trustedanalytics-0.7.2.dev20161005729/trustedanalytics/core/admin.py
@@ -0,0 +1,52 @@
+# vim: set encoding=utf-8
+
+#
+#  Copyright (c) 2015 Intel Corporation 
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+"""
+Admin commands, not part of public API
+"""
+
+from trustedanalytics.rest.command import execute_command
+import atexit
+
+
+def drop_stale(stale_age=None):
+    """
+    Execute garbage collection out of cycle to drop stale entities
+    :param stale_age: Minimum age to qualify as "stale".  Defaults to server config.  Age specified using the typesafe config duration format.
+    :type stale_age: str
+    """
+    execute_command("_admin:/_drop_stale", None, stale_age=stale_age)
+
+
+def finalize_dropped():
+    """
+    Execute garbage collection out of cycle to finalize all dropped entities (i.e. erase their data)
+    """
+    execute_command("_admin:/_finalize_dropped", None, bogus=0)
+
+
+@atexit.register  # registers release to be call on normal python exit
+def release():
+    """
+    Release the current job context (shuts down Yarn application)
+    """
+    print "Releasing cluster resources..."
+    try:
+        execute_command("_admin:/_release", None, bogus=0)
+    except Exception as e:
+        print "Warning: release failed %s" % e   # print as warning and swallow exception
---
+++ packages/trustedanalytics/trustedanalytics-0.7.2.dev20161005729.tar.gz/trustedanalytics-0.7.2.dev20161005729/trustedanalytics/examples/frame.py
@@ -0,0 +1,138 @@
+# vim: set encoding=utf-8
+
+#
+#  Copyright (c) 2015 Intel Corporation 
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+from trustedanalytics import examples
+
+def run(path=r"datasets/cities.csv", ta=None):
+    """
+    Loads cities.csv into a frame and runs some simple frame operations. We will be dropping columns and adding new ones with python lambdas.
+    We are not required to use cities.csv but rather it's schema. Any other csv file with the correct schema and delimeter will work.
+
+    Parameters
+    ----------
+    path : str
+        The HDFS path to the cities.csv dataset. If a path is not given the default is datasets/cities.csv. The dataset is
+        available in the examples/datasets directory and in `github<https://github.com/trustedanalytics/atk/tree/master/python-client/trustedanalytics/examples/datasets>`__.
+        Must be a valid HDFS path either fully qualified hdfs://some/path or relative the ATK rest servers HDFS home directory.
+
+    ta : trusted analytics python import
+        Can be safely ignored when running examples. It is only used during integration testing to pass pre-configured
+        python client reference.
+
+
+    Returns
+    -------
+        A dictionary with the final built frame
+
+
+    Datasets
+    --------
+      All the datasets can be found in the examples/datasets directory of the python client or in `github<https://github.com/trustedanalytics/atk/tree/master/python-client/trustedanalytics/examples/datasets>`__.
+
+
+    Dataset
+    -------
+      Name : cities.csv
+
+      schema:
+        rank(int32) | city(str) | population_2013(int32) | population_2010(int32) | change(str) | county(str)
+
+        sample
+
+        .. code::
+
+          1|Portland|609456|583776|4.40%|Multnomah
+          2|Salem|160614|154637|3.87%|Marion
+          3|Eugene|159190|156185|1.92%|Lane
+
+      delimeter: |
+
+
+    Example
+    -------
+        To run the frame example first import the example.
+
+        .. code::
+
+          >>>import trustedanalytics.examples.frame as frame
+
+        After importing you can execute run method with the path to the dataset
+
+        .. code::
+
+          >>>frame.run("hdfs://FULL_HDFS_PATH")
+
+
+
+    """
+    NAME = "TEST"
+
+    if ta is None:
+        ta = examples.connect()
+
+    #csv schema definition
+    schema = [('rank', ta.int32),
+              ('city', str),
+              ('population_2013', ta.int32),
+              ('population_2010', ta.int32),
+              ('change', str),
+              ('county', str)]
+
+    #import csv file
+    csv = ta.CsvFile(path, schema, skip_header_lines=1, delimiter='|')
+
+    frames = ta.get_frame_names()
+    if NAME in frames:
+        print "Deleting old '{0}' frame.".format(NAME)
+        ta.drop_frames(NAME)
+
+    print "Building frame '{0}'.".format(NAME)
+
+    frame = ta.Frame(csv, NAME)
+
+    print "Inspecting frame '{0}'.".format(NAME)
+
+    print frame.inspect()
+
+    print "Drop Change column."
+
+    frame.drop_columns("change")
+
+    print frame.inspect()
+
+    print "Add Change column."
+    frame.add_columns(lambda row: ((row.population_2013 - row.population_2010)/float(row.population_2010)) * 100,
+                      ("change", ta.float32))
+
+    print frame.inspect()
+
+    print "Drop Change column."
+
+    frame.drop_columns("change")
+
+    print frame.inspect()
+
+    print "Add Change columns."
+
+    frame.add_columns(lambda row: [row.population_2013 - row.population_2010, ((row.population_2013 - row.population_2010)/float(row.population_2010)) * 100 ], [("difference", ta.int32 ), ("change", ta.float32 )])
+
+    print "Format inspection."
+    print frame.inspect(10, wrap=10, columns=["city", "population_2013", "population_2010", "change", "difference"], round=2)
+
+
+    return {"frame": frame}
