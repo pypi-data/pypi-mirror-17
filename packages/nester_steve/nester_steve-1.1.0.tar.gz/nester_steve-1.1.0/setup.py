ea73b8d671a75df91c536bddb0b56796d84ad229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:14 +0000

    Add package nester_steve/1.1.0/nester_steve-1.1.0.tar.gz

---
+++ packages/nester_steve/nester_steve-1.1.0.tar.gz/nester_steve-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='nester_steve',
+    version='1.1.0',
+    py_modules = ['nester_steve'],
+    author='steve',
+    author_email='a@a.com',
+    description = 'this is test',
+)
