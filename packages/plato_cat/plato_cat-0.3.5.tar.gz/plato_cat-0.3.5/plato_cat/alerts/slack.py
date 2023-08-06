82ba4d4e80cc48ae6ec1939fbe8d81aac0e50d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:06 +0000

    Add package plato_cat/0.3.5/plato_cat-0.3.5.tar.gz

---
+++ packages/plato_cat/plato_cat-0.3.5.tar.gz/plato_cat-0.3.5/plato_cat/alerts/slack.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import sys
+
+Incoming = 'https://hooks.slack.com/services/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'  # noqa
+
+
+class SlackAlert(object):
+
+    def call(self, exceptions):
+        endpoint = sys.argv[1]
+        access_key = sys.argv[2]
+        for exception in exceptions:
+            url = Incoming
+            payload = json.dumps({
+                'text': 'endpoint: %s\naccess key: %s\n'
+                'case: %s\nexception: %s\ntraceback: %s'
+                % (endpoint, access_key, exception['case'],
+                   exception['message'], exception['traceback']),
+            })
+            requests.post(url, payload)
+
+    def report(self, title, is_pass, stat):
+        url = Incoming
+
+        text = [title, 'Pass: %s' % is_pass]
+        text += ['%s: %s' % (k, v) for k, v in stat.items()]
+
+        text = '\n'.join(text)
+
+        payload = urllib.urlencode({'payload': json.dumps({'text': text})})
+        requests.post(url, payload)
