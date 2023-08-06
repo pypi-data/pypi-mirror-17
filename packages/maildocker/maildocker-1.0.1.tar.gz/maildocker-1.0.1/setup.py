4eea09a418d6b72ed8b19005cb2349a8483d91ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:02 +0000

    Add package maildocker/1.0.1/maildocker-1.0.1.tar.gz

---
+++ packages/maildocker/maildocker-1.0.1.tar.gz/maildocker-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+__version__ = '1.0.1'
+
+setup(
+    name='maildocker',
+    version=str(__version__),
+    packages=find_packages(),
+    description='Maildocker library for Python',
+    long_description=open('./README.rst').read(),
+)
