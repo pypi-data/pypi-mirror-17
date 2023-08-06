4bda0ee6c7be6a251546a0a32c2a0a22d7c47ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:40 +0000

    Add package ucf_options_base/1.4/ucf_options_base-1.4.tar.gz

---
+++ packages/ucf_options_base/ucf_options_base-1.4.tar.gz/ucf_options_base-1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ucf_options_base',
+      version='1.4',
+      description='Options data retrieval tool for weekly pitches',
+      url='https://github.com/Texas-UCF/OptionsBase',
+      author='Texas UCF',
+      author_email='texasucfmgmt@gmail.com',
+      license='',
+      packages=['OptionsBase'],
+      install_requires=['pandas'],
+      zip_safe=False)
