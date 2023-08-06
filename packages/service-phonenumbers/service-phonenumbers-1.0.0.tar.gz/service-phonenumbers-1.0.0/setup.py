040f118fb5cd0da4bd3a1411304547e06c67ea77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:13 +0000

    Add package service-phonenumbers/1.0.0/service-phonenumbers-1.0.0.tar.gz

---
+++ packages/service-phonenumbers/service-phonenumbers-1.0.0.tar.gz/service-phonenumbers-1.0.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+
+setup(
+    name='service-phonenumbers',
+    version='1.0.0',
+    description='Phonenumber Microservice client',
+    author='Cochise Ruhulessin',
+    author_email='cochiseruhulessin@gmail.com',
+    url='https://www.wizardsofindustry.net',
+    install_requires=[
+        'requests',
+        'PyCrypto'
+    ],
+    packages=[
+        'pvs'
+    ]
+)
