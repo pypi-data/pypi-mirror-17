4bae1febc5eac6e57102dfc1dd3b3060f55f55c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:42 +0000

    Add package ethereum-tester-client/1.2.4/ethereum-tester-client-1.2.4.tar.gz

---
+++ packages/ethereum-tester-client/ethereum-tester-client-1.2.4.tar.gz/ethereum-tester-client-1.2.4/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup
+
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+version = '1.2.4'
+
+readme = open(os.path.join(DIR, 'README.md')).read()
+
+
+setup(
+    name='ethereum-tester-client',
+    version=version,
+    description="""Ethereum JSON RPC Client for testing""",
+    long_description=readme,
+    author='Piper Merriam',
+    author_email='pipermerriam@gmail.com',
+    url='https://github.com/pipermerriam/ethereum-tester-client',
+    include_package_data=True,
+    py_modules=['eth_tester-client'],
+    install_requires=[
+        "ethereum>=1.5.2",
+        "gevent>=1.1.2",
+    ],
+    license="MIT",
+    zip_safe=False,
+    keywords='ethereum',
+    packages=[
+        "eth_tester_client",
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
