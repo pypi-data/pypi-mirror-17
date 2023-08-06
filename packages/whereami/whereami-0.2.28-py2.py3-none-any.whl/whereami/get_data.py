3f69f6e70977f4a7bbb8a127ff31902e9d7cd3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:42 +0000

    Add package whereami/0.2.28/whereami-0.2.28-py2.py3-none-any.whl

---
+++ packages/whereami/whereami-0.2.28-py2.py3-none-any.whl/whereami/get_data.py
@@ -0,0 +1,29 @@
+import json
+import os
+from access_points import get_scanner
+
+from whereami.utils import ensure_whereami_path
+
+
+def aps_to_dict(aps):
+    return {ap['ssid'] + " " + ap['bssid']: ap['quality'] for ap in aps}
+
+
+def sample():
+    wifi_scanner = get_scanner()
+    aps = wifi_scanner.get_access_points()
+    return aps_to_dict(aps)
+
+
+def get_train_data(folder=None):
+    if folder is None:
+        folder = ensure_whereami_path()
+    X = []
+    y = []
+    for fname in os.listdir(folder):
+        if fname.endswith(".txt"):
+            with open(os.path.join(folder, fname)) as f:
+                data = map(json.loads, f)
+            X.extend(data)
+            y.extend(fname.rstrip(".txt") for _ in data)
+    return X, y
