c759ce5ac6a2408beb97b5fbdc03f3e617e33306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:42 +0000

    Add package skywise-platform/0.0.1/skywise-platform-0.0.1.tar.gz

---
+++ packages/skywise-platform/skywise-platform-0.0.1.tar.gz/skywise-platform-0.0.1/skywiseplatform/datapoint.py
@@ -0,0 +1,57 @@
+from voluptuous import Schema
+
+from skywiserestclient import SkyWiseJSON
+from . import PlatformResource
+from .frame import SingleFrame
+
+
+class Datapoint(SkyWiseJSON, PlatformResource):
+
+    _path = "/frames/{frame_id}/datapoint/{latitude}/{longitude}"
+
+    _deserialize = Schema({
+        "tile": unicode,
+        "pixel": {
+            "row": int,
+            "column": int
+        },
+        "value": float,
+        "unit": {
+            "description": unicode,
+            "label": unicode
+        }
+    })
+
+    _serialize = Schema({
+        "tile": unicode,
+        "pixel": {
+            "row": int,
+            "column": int
+        },
+        "value": float,
+        "unit": {
+            "description": unicode,
+            "label": unicode
+        }
+    })
+
+    @classmethod
+    def find(cls, frame, latitude, longitude, **kwargs):
+        try:
+            frame.id
+        except AttributeError:
+            frame = SingleFrame.find(frame)
+        r = super(Datapoint, cls).find(frame_id=frame.id, latitude=latitude, longitude=longitude, **kwargs)
+        r.frame = frame
+        r.validTime = frame.validTime
+        return r
+
+    @classmethod
+    def find_async(cls, frame, latitude, longitude, **kwargs):
+        try:
+            frame.id
+        except AttributeError:
+            frame = SingleFrame.find(frame)
+        r = super(Datapoint, cls).find_async(frame_id=frame.id, latitude=latitude, longitude=longitude, **kwargs)
+        r.tag(frame=frame, validTime=frame.validTime)
+        return r
