46f162b61904173fbc9a2796a12b81ed2b65f698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:41 +0000

    Add package performline/0.1.0a2/performline-0.1.0a2.tar.gz

---
+++ packages/performline/performline-0.1.0a2.tar.gz/performline-0.1.0a2/performline/util.py
@@ -0,0 +1,56 @@
+from __future__ import unicode_literals
+import re
+
+CAMEL_OMIT = r'[\W\s\_\-]+'
+CAMEL_SPLIT = re.compile(r'(?:([A-Z][^A-Z]*)|'+CAMEL_OMIT+')')
+CAMEL_STRIP = re.compile(CAMEL_OMIT)
+DATA_ELEMENT_SEPARATOR = '/'
+
+
+class MissingKeyException(Exception):
+    pass
+
+
+def must_get(data, path):
+    if not isinstance(data, dict):
+        raise Exception("Response data is unavailable")
+
+    current = data
+    parts = path.split(DATA_ELEMENT_SEPARATOR)
+
+    for i in range(0, len(parts)):
+        if isinstance(current, dict) and parts[i] in current:
+            current = current[parts[i]]
+        else:
+            raise MissingKeyException("Cannot retrieve data element '%s'", path)
+
+    return current
+
+
+def get(data, path, fallback=None):
+    try:
+        return must_get(data, path)
+    except MissingKeyException:
+        return fallback
+
+
+def camelize(value, upperFirst=False):
+    # split on word-separating characters (and discard them), or on capital
+    # letters (preserving them)
+    value = CAMEL_SPLIT.split(value)
+
+    # filter out None and empty/only-whitespace strings from the list
+    value = [x for x in value if x is not None and not x.strip() == '']
+
+    # capitalize each word
+    value = ''.join(x.title() for x in value)
+
+    # do a final pass removing omitted characters from the value
+    value = re.sub(CAMEL_STRIP, '', value)
+
+    if upperFirst:
+        # return PascalCase
+        return value
+    else:
+        # return camelCase
+        return value[0].lower() + value[1:]
---
+++ packages/performline/performline-0.1.0a2.tar.gz/performline-0.1.0a2/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+import os
+
+setup(
+    name='performline',
+    description='Client library for using the public PerformMatch Compliance API',
+    version=('0.1.0a%s' % os.environ.get('REVISION', '1')),
+    author='PerformLine Engineering',
+    author_email='tech+api@performline.com',
+    py_modules=[
+        'performline'
+    ],
+    url='https://github.com/PerformLine/python-performline-client',
+    install_requires=[
+        'click==6.6',
+        'requests==2.11.1',
+    ],
+    entry_points={
+        'console_scripts': [
+            'performline=performline.cli',
+        ],
+    },
+    packages=[
+        'performline',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Office/Business',
+        'Environment :: Console',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Intended Audience :: Customer Service',
+        'Intended Audience :: Financial and Insurance Industry',
+    ],
+)
