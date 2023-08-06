9b9deaf9e6b53d89e98be94b82dbae300857928f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:29 +0000

    Add package bitcoinaverage/0.1.5/bitcoinaverage-0.1.5.tar.gz

---
+++ packages/bitcoinaverage/bitcoinaverage-0.1.5.tar.gz/bitcoinaverage-0.1.5/bitcoinaverage/__init__.py
@@ -0,0 +1,6 @@
+from bitcoinaverage.clients.websocket_client import WebsocketClient
+from bitcoinaverage.clients.restful_client import RestfulClient
+
+# __all__ = [
+#     'WebsocketClient', 'RestfulClient', 'exceptions'
+# ]
---
+++ packages/bitcoinaverage/bitcoinaverage-0.1.5.tar.gz/bitcoinaverage-0.1.5/setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='bitcoinaverage',
+    packages=['bitcoinaverage'],  # this must be the same as the name above
+    version='0.1.5',
+    description='Library to integrate with the BitcoinAverage API',
+    author='Blockchain Data LTD',
+    author_email='info@bitcoinaverage.com',
+    keywords=['bitcoinaverage', 'bitcoin', 'api'],  # arbitrary keywords
+    classifiers=[],
+    install_requires=[
+        "requests",
+        "six",
+        "itsdangerous",
+        "ws4py"
+    ]
+)
