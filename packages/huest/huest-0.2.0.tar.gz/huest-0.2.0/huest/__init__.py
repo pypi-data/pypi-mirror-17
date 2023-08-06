e7bbf900f6dc17b41f75dfd34652681225810b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:59 +0000

    Add package huest/0.2.0/huest-0.2.0.tar.gz

---
+++ packages/huest/huest-0.2.0.tar.gz/huest-0.2.0/huest/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+
+__all__ = ()
+
+
+from .hounsfield_estimate import *
+__all__ += hounsfield_estimate.__all__
---
+++ packages/huest/huest-0.2.0.tar.gz/huest-0.2.0/setup.py
@@ -0,0 +1,32 @@
+"""Setup script for huest.
+
+Installation command::
+
+    pip install [--user] [-e] .
+"""
+
+from __future__ import print_function, absolute_import
+
+from setuptools import setup, find_packages
+
+setup(
+    name='huest',
+
+    version='0.2.0',
+
+    description='HoUnsfield ESTimate',
+
+    url='https://github.com/adler-j/huest',
+
+    author='Jonas Adler',
+    author_email='jonasadl@kth.se',
+
+    license='GPLv3+',
+
+    keywords='research development emission prototyping imaging tomography',
+
+    packages=find_packages(exclude=['*test*']),
+    package_dir={'huest': 'huest'},
+
+    install_requires=['numpy']
+)
