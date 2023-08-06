612182840efb0c84c67126d669ca449e9014b257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:11 +0000

    Add package django-git-submodules/1.0.0/django-git-submodules-1.0.0.tar.gz

---
+++ packages/django-git-submodules/django-git-submodules-1.0.0.tar.gz/django-git-submodules-1.0.0/dj_git_submodule/submodule.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import __main__
+import glob
+
+def get_project_root():
+  return os.path.dirname(os.path.abspath(__main__.__file__))
+
+def add(submodules):
+  if not isinstance(submodules, (list, tuple, )):
+    submodules = [submodules]
+
+  PROJECT_ROOT = get_project_root()
+
+  for mod in submodules:
+    path = os.path.abspath(os.path.join(PROJECT_ROOT, mod))
+    sys.path.insert(0, path)
+
+def locate(string):
+  return glob.glob(os.path.join(get_project_root(), string))
