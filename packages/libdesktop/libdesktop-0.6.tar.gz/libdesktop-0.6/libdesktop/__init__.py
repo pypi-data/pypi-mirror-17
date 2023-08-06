105355ee2a2c8d85300a76683e5bee708f18f3a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:03 +0000

    Add package libdesktop/0.6/libdesktop-0.6.tar.gz

---
+++ packages/libdesktop/libdesktop-0.6.tar.gz/libdesktop-0.6/libdesktop/__init__.py
@@ -0,0 +1,10 @@
+import sys
+
+sys.path.insert(0, '.')
+
+from . import startup
+from . import desktopfile
+from . import system
+from . import applications
+from . import volume
+from . import dialog
---
+++ packages/libdesktop/libdesktop-0.6.tar.gz/libdesktop-0.6/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+_version = 0.6
+
+setup(
+  name = 'libdesktop',
+  packages = ['libdesktop', 'libdesktop.dialog'],
+  version = str(_version),
+  description = 'A cross-platform library for miscellaneous OS functions and conveniences.',
+  author = 'Bharadwaj Raju',
+  author_email = 'bharadwaj.raju@keemail.me',
+  url = 'https://github.com/bharadwaj-raju/libdesktop',
+  download_url = 'https://github.com/bharadwaj-raju/libdesktop/archive/master.tar.gz',
+  keywords = ['desktop', 'library', 'os', 'operating', 'system'],
+  classifiers = ['Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+	'Programming Language :: Python :: 2',
+	'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+	'Topic :: Utilities',
+	'Topic :: System',
+	'Topic :: Desktop Environment'],
+)
