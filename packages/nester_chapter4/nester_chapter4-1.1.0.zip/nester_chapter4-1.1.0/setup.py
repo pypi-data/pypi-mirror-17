9ceee3f1e7ca14ed4ad331eaa9fad7610daa5b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:24 +0000

    Add package nester_chapter4/1.1.0/nester_chapter4-1.1.0.zip

---
+++ packages/nester_chapter4/nester_chapter4-1.1.0.zip/nester_chapter4-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_chapter4',
+    version = '1.1.0',
+    py_modules = ['nester_chapter4'],
+    author = 'Leon',
+    author_email = 'leon_119@yahoo.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
+
+    
