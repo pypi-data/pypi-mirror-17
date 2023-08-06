268e1862e93c4e3d808985644912c1facea2affe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:13 +0000

    Add package csvparser/0.6.0/csvparser-0.6.0.tar.gz

---
+++ packages/csvparser/csvparser-0.6.0.tar.gz/csvparser-0.6.0/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+  name = 'csvparser',
+  packages = ['csvparser'],
+  version = '0.6.0',
+  description = 'Library for parsing and validating csv files using declared django-style parser classes.',
+  author = 'Bartłomiej Bęczkowski',
+  author_email = 'beczkowb@gmail.com',
+  url = 'https://github.com/beczkowb/csvparser',
+  download_url = 'https://github.com/beczkowb/csvparser.git',
+  keywords = ['csv', 'parsing', 'django'],
+  classifiers = [],
+  license = 'MIT',
+)
