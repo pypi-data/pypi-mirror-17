fd8027e87270d8adaf6fd7346752973d108d6c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:59 +0000

    Add package emojencode/1.1/emojencode-1.1.tar.gz

---
+++ packages/emojencode/emojencode-1.1.tar.gz/emojencode-1.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name='emojencode',
+    version='1.1',
+    description='Encode binary data as emoji! 😄',
+    author='Frederick "doctaphred" Wagner',
+    author_email='yo@doctaph.red',
+    url='https://github.com/doctaphred/emojencode',
+    py_modules=['emojencode'],
+)
