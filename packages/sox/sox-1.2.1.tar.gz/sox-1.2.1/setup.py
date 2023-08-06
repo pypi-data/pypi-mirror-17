8ccabf7f78a591890bf06f1669da8497229e79de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:34 +0000

    Add package sox/1.2.1/sox-1.2.1.tar.gz

---
+++ packages/sox/sox-1.2.1.tar.gz/sox-1.2.1/setup.py
@@ -0,0 +1,46 @@
+""" Setup script for sox. """
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name='sox',
+
+        version='1.2.1',
+
+        description='Python wrapper around SoX.',
+
+        author='Rachel Bittner',
+
+        author_email='rachel.bittner@nyu.edu',
+
+        url='https://github.com/rabitt/pysox',
+
+        download_url='http://github.com/rabitt/pysox/releases',
+
+        packages=['sox'],
+
+        package_data={'sox': []},
+
+        long_description="""Python wrapper around SoX.""",
+
+        keywords='audio effects SoX',
+
+        license='MIT',
+
+        install_requires=[
+        ],
+
+        extras_require={
+            'tests': [
+                'pytest',
+                'pytest-cov',
+                'pytest-pep8',
+            ],
+            'docs': [
+                'sphinx==1.2.3',  # autodoc was broken in 1.3.1
+                'sphinxcontrib-napoleon',
+                'sphinx_rtd_theme',
+                'numpydoc',
+            ],
+        }
+    )
