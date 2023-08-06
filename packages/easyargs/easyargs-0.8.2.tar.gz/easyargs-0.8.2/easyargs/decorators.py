b071a9117b4405b384b724ddf2323d66359abb9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:01 +0000

    Add package easyargs/0.8.2/easyargs-0.8.2.tar.gz

---
+++ packages/easyargs/easyargs-0.8.2.tar.gz/easyargs-0.8.2/easyargs/decorators.py
@@ -0,0 +1,20 @@
+from . import parsers
+import functools
+
+def make_easy_args(obj=None, auto_call=True):
+    def decorate(f):
+        @functools.wraps(f)
+        def decorated(*args, **kwargs):
+            parser = parsers.create_base_parser(f)
+            parsers.function_parser(f, parser)
+            if auto_call:
+                parsers.handle_parser(parser)
+
+
+            return parser
+        return decorated
+
+    if obj != None:
+        return decorate(obj)
+
+    return decorate
