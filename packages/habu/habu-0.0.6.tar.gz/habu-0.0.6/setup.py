3f53dce93a15383e4ba1dd8f3cc260639d8f57f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:51 +0000

    Add package habu/0.0.6/habu-0.0.6.tar.gz

---
+++ packages/habu/habu-0.0.6.tar.gz/habu-0.0.6/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='habu',
+    version='0.0.6',
+    description='Ethical Hacking Utils',
+    long_description=readme,
+    author='Fabian Martinez Portantier',
+    author_email='fportantier@securetia.com',
+    url='https://github.com/securetia/habu',
+    license='GNU General Public License v3 (GPLv3)',
+    install_requires=[
+        'Click',
+        'Requests',
+    ],
+    packages=find_packages(exclude=('tests', 'docs')),
+    include_package_data=True,
+    entry_points='''
+        [console_scripts]
+        habu.ip=habu.cli.cmd_ip:cmd_ip
+        habu.xor=habu.cli.cmd_xor:cmd_xor
+    ''',
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Security",
+        "Topic :: System :: Networking",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords = ['security'],
+    zip_safe=False,
+    test_suite='pyroma',
+)
+
