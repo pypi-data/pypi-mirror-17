b4cc35ce8c71ed2778568fe6a655be55aade67f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:16 +0000

    Add package pycode/0.1.2/pycode-0.1.2.zip

---
+++ packages/pycode/pycode-0.1.2.zip/pycode-0.1.2/docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use pycode in a project::
+
+    import pycode
---
+++ packages/pycode/pycode-0.1.2.zip/pycode-0.1.2/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Click>=6.0',
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='pycode',
+    version='0.1.2',
+    description="Python tool for research coding",
+    long_description=readme + '\n\n' + history,
+    author="Jerome Bergmann",
+    author_email='jerome.bergmann@rwth-aachen.de',
+    url='https://github.com/j340m3/pycode',
+    packages=find_packages(),
+    package_dir={'pycode':
+                     'pycode'},
+    entry_points={
+        'console_scripts': [
+            'pycode=pycode.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    zip_safe=False,
+    keywords='pycode',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    platforms=["any"]
+)
