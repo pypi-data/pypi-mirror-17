d2726b343a47aa7e01498a1cebcf6a0c047d9aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:45 +0000

    Add package apize/0.2.5/apize-0.2.5.tar.gz

---
+++ packages/apize/apize-0.2.5.tar.gz/apize-0.2.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+	name = 'apize',
+	version = '0.2.5',
+	author = 'herrersystem',
+	author_email = 'anton.millescamps@evhunter.fr',
+	url = 'http://github.com/herrersystem/apize',
+	keywords = 'apize api client',
+	description = 'Write quickly and easily to API clients',
+	license = 'GNU General Public License (GPL)',
+	packages = ['apize'],
+	install_requires = ['requests'],
+	
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: GNU General Public License (GPL)',
+		'Topic :: Software Development',
+		'Topic :: Software Development :: Libraries', 
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+	],
+)
