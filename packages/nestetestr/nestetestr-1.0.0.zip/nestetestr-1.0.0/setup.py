135874e536af21ca9b095ae8e4f5ee92d49b7910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:59 +0000

    Add package nestetestr/1.0.0/nestetestr-1.0.0.zip

---
+++ packages/nestetestr/nestetestr-1.0.0.zip/nestetestr-1.0.0/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+setup(
+      name          ='nestetestr',
+      version       ='1.0.0',
+      py_modules    =['nestetestr'],
+      author        ='hfpython',
+      author_email  ='hfpython@headfirstlabs.com',
+      url           ='http://www.headfirstlabs.com',
+      description   ='A simple printer of nested lists',
+      )
