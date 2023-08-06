bbe8b5f19e0d8d44064a090a949be4d82f0b46df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:46 +0000

    Add package nester01/1.3.0/nester01-1.3.0.zip

---
+++ packages/nester01/nester01-1.3.0.zip/nester01-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester01',
+    version='1.3.0',
+    py_modules=['nester01'],
+    author='dalijiang',
+    author_email='jiangco@163.com',
+    url='http://www.headfirstlabs.com',
+    description='A Simple printer of nester lists'
+    )
