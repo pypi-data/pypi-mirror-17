56ff78f38ba96edf38397ef3e4bb9754150c3e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:19 +0000

    Add package pydora/1.8.0/pydora-1.8.0.tar.gz

---
+++ packages/pydora/pydora-1.8.0.tar.gz/pydora-1.8.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pydora",
+    version="1.8.0",
+    description="Python wrapper for Pandora API",
+    long_description=open("README.rst", "r").read(),
+    author="Mike Crute",
+    author_email="mcrute@gmail.com",
+    url="https://github.com/mcrute/pydora",
+    test_suite="tests.discover_suite",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    setup_requires=[
+        "py_release_tools",
+        "wheel",
+    ],
+    install_requires=[
+        "pycrypto>=2.6.1",
+        "requests>=2",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pydora = pydora.player:main",
+            "pydora-configure = pydora.configure:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Multimedia :: Sound/Audio :: Players",
+    ]
+)
