6cfc42dd5a6de55cfda775dfa7459e27cbe085ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:56 +0000

    Add package leonardo-hijack/1.0.4/leonardo-hijack-1.0.4.tar.gz

---
+++ packages/leonardo-hijack/leonardo-hijack-1.0.4.tar.gz/leonardo-hijack-1.0.4/leonardo_hijack/settings.py
@@ -0,0 +1,5 @@
+
+HIJACK_LOGIN_REDIRECT_URL = "/admin/"  # where you want to be redirected to, after hijacking the user.
+HIJACK_LOGOUT_REDIRECT_URL = "/admin/"  # where you want to be redirected to, after releasing the user.
+
+HIJACK_ALLOW_GET_REQUESTS = True
