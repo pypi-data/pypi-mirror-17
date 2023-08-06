a10b46b298abbd09ae2c5ab3c9c1d4f602a704d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:56 +0000

    Add package wagtail-themes/0.1.2/wagtail-themes-0.1.2.tar.gz

---
+++ packages/wagtail-themes/wagtail-themes-0.1.2.tar.gz/wagtail-themes-0.1.2/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import find_packages, setup
+
+
+install_requires = [
+    'django>=1.8',
+    'wagtail>=1.2'
+]
+
+setup(
+    name='wagtail-themes',
+    version='0.1.2',
+    description='Site specific theme loader for Django Wagtail.',
+    author='Rob Moorman',
+    author_email='rob@moori.nl',
+    url='https://github.com/moorinteractive/wagtail-themes',
+    license='MIT',
+    install_requires=install_requires,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5'
+    ]
+)
