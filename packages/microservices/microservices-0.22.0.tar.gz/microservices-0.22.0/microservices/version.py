026cbc53909090d1346d5e5fa45ad7454b1e6669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:03 +0000

    Add package microservices/0.22.0/microservices-0.22.0.tar.gz

---
+++ packages/microservices/microservices-0.22.0.tar.gz/microservices-0.22.0/microservices/version.py
@@ -0,0 +1,11 @@
+MAJOR = 0
+MINOR = 22
+PATCH = 0
+
+
+def get_version(suffix=''):
+    return '.'.join([str(v) for v in (MAJOR, MINOR, PATCH)]) + suffix
+
+
+if __name__ == '__main__':
+    print(get_version())
