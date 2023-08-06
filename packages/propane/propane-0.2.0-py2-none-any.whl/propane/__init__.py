7a21a5f99789d1e91e3ceb54d0bd6951f0284c8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:08 +0000

    Add package propane/0.2.0/propane-0.2.0-py2-none-any.whl

---
+++ packages/propane/propane-0.2.0-py2-none-any.whl/propane/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from setuptools_scm import get_version
+
+
+__author__ = 'Tyler Butler <tyler@tylerbutler.com>'
+
+version = get_version(root='..', relative_to=__file__)
