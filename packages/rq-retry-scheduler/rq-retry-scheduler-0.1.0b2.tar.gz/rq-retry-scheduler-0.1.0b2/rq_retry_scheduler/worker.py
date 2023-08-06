ff1388816c2b1f795887cfa4e882f8dbb5269c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:46 +0000

    Add package rq-retry-scheduler/0.1.0b2/rq-retry-scheduler-0.1.0b2.tar.gz

---
+++ packages/rq-retry-scheduler/rq-retry-scheduler-0.1.0b2.tar.gz/rq-retry-scheduler-0.1.0b2/rq_retry_scheduler/worker.py
@@ -0,0 +1,42 @@
+from datetime import timedelta
+import logging
+from rq import Worker
+
+logger = logging.getLogger('rq:retryscheduler:worker')
+
+
+class Worker(Worker):
+
+    retry_delays = {
+        1: timedelta(minutes=1),
+        2: timedelta(minutes=5),
+        3: timedelta(minutes=10),
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.log = logger
+        self.push_exc_handler(self.exc_handler)
+
+    def exc_handler(self, job, exc_type, exc_value, traceback):
+        job.meta.setdefault('failures', 0)
+        job.meta['failures'] += 1
+
+        self.log.info("Job {}: Exception handler with {} failures".format(
+                        job.id, job.meta['failures']))
+
+        delay = self.retry_delays.get(job.meta['failures'])
+
+        if delay is None:
+            self.log.warn(
+                "Job {}: Failed too many times, not requeuing".format(job.id))
+            job.save()
+            return True
+
+        self.log.info("Job {}: Requeueing in {} into queue {}".format(
+                       job.id, delay, job.origin))
+
+        queue = self.queue_class(job.origin, connection=self.connection)
+        queue.enqueue_job_in(delay, job)
+        return False
