d0bec2b3bd442935a491bce3f9248b74b64265fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:14 +0000

    Add package cos_dumps_file_list/0.0.1/cos_dumps_file_list-0.0.1.tar.gz

---
+++ packages/cos_dumps_file_list/cos_dumps_file_list-0.0.1.tar.gz/cos_dumps_file_list-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='cos_dumps_file_list',
+    version='0.0.1',
+    packages=['dump_bucket'],
+    url='https://www.qcloud.com/',
+    license='MIT',
+    author='liuchang',
+    author_email='liuchang0812@gmail.com',
+    description='simple tool that could be used to dump file list within cos bucket',
+    entry_points={
+        'console_scripts': [
+            'cos_dumps = dump_bucket.main:main'
+        ]
+    },
+    install_requires=[
+        'qcloud-cos',
+        'requests'
+    ],
+    package=find_packages(),
+)
