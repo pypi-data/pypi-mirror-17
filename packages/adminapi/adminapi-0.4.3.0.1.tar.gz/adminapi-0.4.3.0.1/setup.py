5d310168ebf3ae652639955b98f1d71c5a79c249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:33 +0000

    Add package adminapi/0.4.3.0.1/adminapi-0.4.3.0.1.tar.gz

---
+++ packages/adminapi/adminapi-0.4.3.0.1.tar.gz/adminapi-0.4.3.0.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+__version__ = '0.4.3.0.1'
+
+setup(name="adminapi",
+      version=__version__,
+      description="Eucalyptus Cloud Services and General System Administrative Utilities",
+      long_description="Eucalyptus Cloud Services and General System Administrative Utilities",
+      url="https://github.com/bigschwan/adminapi",
+      dependency_links = ['https://github.com/nephomaniac/python-midonetclient/tarball/4_3_0_e#egg=midonetclient'],
+      install_requires=['paramiko >= 2.0.2',
+                        'boto >= 2.5.2',
+                        'argparse',
+                        'kazoo',
+                        'pywinrm',
+                        'requests >= 1',
+                        'prettytable',
+			'python-dateutil',
+                        'dnspython',
+                        'midonetclient'], 
+      packages=find_packages(),
+      license='BSD (Simplified)',
+      platforms='Posix; MacOS X;',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: System Administrators',
+                   'Operating System :: OS Independent',
+                   'Topic :: System :: Systems Administration'],
+      )
