4c9e68cd0d0061951187cc4ce04b6889ba84fdd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:54 +0000

    Add package rbcz/0.5/rbcz-0.5.tar.gz

---
+++ packages/rbcz/rbcz-0.5.tar.gz/rbcz-0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name = 'rbcz',
+    packages = [ 'rbcz', 'rbcz.test' ],
+    version = '0.5',
+    description = 'library for interacting with Czech Raiffeisen Bank\'s text bank statements',
+    author = 'Sean McLemon',
+    author_email = 'sean.mclemon@gmail.com',
+    url = 'https://github.com/smcl/rbcz.py',
+    download_url = 'https://github.com/smcl/rbcz.py/tarball/0.5',
+    keywords = ['banking', 'raiffeisen', 'czech', 'cz'],
+    classifiers = [],
+    test_suite='rbcz.test.all',
+    install_requires=[
+        'unittest2'
+    ],
+    setup_requires=[
+        'unittest2'
+    ],
+    
+)
