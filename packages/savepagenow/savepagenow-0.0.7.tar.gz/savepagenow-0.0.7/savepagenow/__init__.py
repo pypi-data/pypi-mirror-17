d0e5134b485b8b79e5c1bbd650ce4211805a848a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:26 +0000

    Add package savepagenow/0.0.7/savepagenow-0.0.7.tar.gz

---
+++ packages/savepagenow/savepagenow-0.0.7.tar.gz/savepagenow-0.0.7/savepagenow/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from .api import capture, capture_or_cache, CachedPage
+
+
+__version__ = "0.0.7"
+__all__ = (
+    'capture',
+    'capture_or_cache',
+    'CachedPage',
+)
