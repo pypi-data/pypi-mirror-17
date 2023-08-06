0c8614c878b68efa70ab7099e130efa0beeb65bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:08 +0000

    Add package malama-cores/0.2/malama_cores-0.2.tar.gz

---
+++ packages/malama-cores/malama_cores-0.2.tar.gz/malama_cores-0.2/demo/test.py
@@ -0,0 +1,7 @@
+
+
+def test():
+    print("Charles sb")
+
+if __name__=="__main__":
+    test()
---
+++ packages/malama-cores/malama_cores-0.2.tar.gz/malama_cores-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "malama_cores",
+    version = "0.2",
+    description = "Core libraries for malama",
+    author = "andy zhang",
+    license = "BSD",
+    packages = find_packages(),
+    classifiers = [ "Development Status :: 1 - Planning"]
+)
