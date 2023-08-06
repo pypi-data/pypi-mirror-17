8e3b88c5d54d8ba1e7833bfd55292d551bd9bf3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:25 +0000

    Add package problemo/0.2.0/problemo-0.2.0.tar.gz

---
+++ packages/problemo/problemo-0.2.0.tar.gz/problemo-0.2.0/problemo/__init__.py
@@ -0,0 +1,6 @@
+'''
+Problemo:   Tools for solving sparse linear systems using a
+            selection of external solver libraries.
+'''
+
+from problemo.solverwrap import MumpsSolver, MKLPardisoSolver, SuperLUSolver, BestSolver
