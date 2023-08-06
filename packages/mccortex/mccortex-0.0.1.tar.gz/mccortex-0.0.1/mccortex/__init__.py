4084dae1dd87d5b515eb626b9eaace52df6a4fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:10 +0000

    Add package mccortex/0.0.1/mccortex-0.0.1.tar.gz

---
+++ packages/mccortex/mccortex-0.0.1.tar.gz/mccortex-0.0.1/mccortex/__init__.py
@@ -0,0 +1,1 @@
+from cortex.cortex import GraphReader
---
+++ packages/mccortex/mccortex-0.0.1.tar.gz/mccortex-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#from distutils.core import setup
+from setuptools import setup
+
+
+setup(
+    name='mccortex',
+    version='0.0.1',
+    packages=[
+        'mccortex',
+    ],
+    license='MIT',
+    url='http://github.com/phelimb/cortex-py',
+    description='.',
+    author='Jerome Kelleher, Phelim Bradley',
+    author_email='jk@well.ox.ac.uk, wave@phel.im',
+    install_requires=[])
