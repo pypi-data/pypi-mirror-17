414ca2d3e615dbc18ef993d63c732f411964011c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:09 +0000

    Add package wagtailnews/0.11.2/wagtailnews-0.11.2-py3-none-any.whl

---
+++ packages/wagtailnews/wagtailnews-0.11.2-py3-none-any.whl/wagtailnews/urls.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.conf.urls import url
+
+from .views import chooser, editor
+
+urlpatterns = [
+    url(r'^$', chooser.choose,
+        name='wagtailnews_choose'),
+    url(r'^search/$', chooser.search,
+        name='wagtailnews_search'),
+    url(r'^(?P<pk>\d+)/$', chooser.index,
+        name='wagtailnews_index'),
+    url(r'^(?P<pk>\d+)/create/$', editor.create,
+        name='wagtailnews_create'),
+    url(r'^(?P<pk>\d+)/edit/(?P<newsitem_pk>.*)/$', editor.edit,
+        name='wagtailnews_edit'),
+    url(r'^(?P<pk>\d+)/unpublish/(?P<newsitem_pk>.*)/$', editor.unpublish,
+        name='wagtailnews_unpublish'),
+    url(r'^(?P<pk>\d+)/delete/(?P<newsitem_pk>.*)/$', editor.delete,
+        name='wagtailnews_delete'),
+    url(r'^(?P<pk>\d+)/view_draft/(?P<newsitem_pk>.*)/$', editor.view_draft,
+        name='wagtailnews_view_draft'),
+    # Choosers
+    url(r'^chooser/$', chooser.choose_modal, name='wagtailnews_chooser'),
+    url(r'^chooser/(?P<pk>\d+)/(?P<newsitem_pk>\d+)/$', chooser.chosen_modal, name='wagtailnews_chosen'),
+]
