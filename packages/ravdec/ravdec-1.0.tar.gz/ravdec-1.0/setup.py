57d781d54e12a5e5bef12379c28c74f39ae227d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:22 +0000

    Add package ravdec/1.0/ravdec-1.0.tar.gz

---
+++ packages/ravdec/ravdec-1.0.tar.gz/ravdec-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ravdec',
+      version='1.0',
+      description='Lossless Data Compression',
+      url='http://github.com/mr-ravin/ravdec',
+      author='Ravin Kumar',
+      author_email='mr.ravin_kumar@hotmail.com',
+      license='MIT',
+      packages=['ravdec'],
+      zip_safe=False)
