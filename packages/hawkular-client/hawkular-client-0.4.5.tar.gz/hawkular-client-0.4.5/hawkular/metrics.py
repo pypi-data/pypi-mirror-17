a0239f76d72459bea10147731a6f435759041bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:38 +0000

    Add package hawkular-client/0.4.5/hawkular-client-0.4.5.tar.gz

---
+++ packages/hawkular-client/hawkular-client-0.4.5.tar.gz/hawkular-client-0.4.5/hawkular/metrics.py
@@ -0,0 +1,435 @@
+"""
+   Copyright 2015-2016 Red Hat, Inc. and/or its affiliates
+   and other contributors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+from __future__ import unicode_literals
+
+import codecs
+import time
+import collections
+import base64
+import ssl
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+try:
+    # Python 3
+    from urllib.request import Request, urlopen, build_opener, install_opener, HTTPErrorProcessor
+    from urllib.error import HTTPError, URLError
+    from urllib.parse import quote, urlencode
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import Request, urlopen, URLError, HTTPError, HTTPErrorProcessor, build_opener, install_opener
+    from urllib import quote, urlencode
+
+class MetricType:
+    Gauge = 'gauges'
+    Availability = 'availability'
+    Counter = 'counters'
+    String = 'strings'
+    Rate = 'rate'
+    _Metrics = 'metrics'
+
+    @staticmethod
+    def short(metric_type):
+        if metric_type is MetricType.Gauge:
+            return 'gauge'
+        elif metric_type is MetricType.Counter:
+            return 'counter'
+        elif metric_type is MetricType.String:
+            return 'string'
+        else:
+            return 'availability'
+
+class Availability:
+    Down = 'down'
+    Up = 'up'
+    Unknown = 'unknown'
+
+class HawkularMetricsError(HTTPError):
+    pass
+        
+class HawkularMetricsConnectionError(URLError):
+    pass
+
+class HawkularHTTPErrorProcessor(HTTPErrorProcessor):
+    """
+    Hawkular-Metrics uses http codes 201, 204
+    """
+    def http_response(self, request, response):
+
+        if response.code in [200, 201, 204]:
+            return response
+        return HTTPErrorProcessor.http_response(self, request, response)
+  
+    https_response = http_response
+
+class HawkularMetricsClient:
+    """
+    Creates new client for Hawkular-Metrics. As tenant_id, give intended tenant_id, even if it's not
+    created yet. To change the instance's tenant_id, use tenant(tenant_id) method
+    """
+    def __init__(self,
+                 tenant_id,
+                 host='localhost',
+                 port=8080,
+                 path='hawkular/metrics',
+                 scheme='http',
+                 cafile=None,
+                 context=None,
+                 token=None,
+                 username=None,
+                 password=None):
+        """
+        A new instance of HawkularMetricsClient is created with the following defaults:
+
+        host = localhost
+        port = 8081
+        path = hawkular-metrics
+        scheme = http
+        cafile = None
+
+        The url that is called by the client is:
+
+        {scheme}://{host}:{port}/{2}/
+        """
+        self.tenant_id = tenant_id
+        self.host = host
+        self.port = port
+        self.path = path
+        self.cafile = cafile
+        self.scheme = scheme
+        self.context = context
+        self.token = token
+        self.username = username
+        self.password = password
+
+        opener = build_opener(HawkularHTTPErrorProcessor())
+        install_opener(opener)
+
+    """
+    Internal methods
+    """
+    @staticmethod
+    def _clean_metric_id(metric_id):
+        return quote(metric_id, '')
+
+    def _get_base_url(self):
+        return "{0}://{1}:{2}/{3}/".format(self.scheme, self.host, str(self.port), self.path)
+    
+    def _get_url(self, metric_type=None):
+        if metric_type is None:
+            metric_type = MetricType._Metrics
+
+        return self._get_base_url() + '{0}'.format(metric_type)
+
+    def _get_metrics_single_url(self, metric_type, metric_id):
+        return self._get_url(metric_type) + '/{0}'.format(self._clean_metric_id(metric_id))
+
+    def _get_metrics_raw_url(self, metrics_url):
+        return metrics_url + '/raw'
+
+    def _get_metrics_stats_url(self, metrics_url):
+        return metrics_url + '/stats'
+
+    def _get_metrics_tags_url(self, metrics_url):
+        return metrics_url + '/tags'
+
+    def _get_tenants_url(self):
+        return self._get_base_url() + 'tenants'
+
+    @staticmethod
+    def _transform_tags(**tags):
+        return ','.join("%s:%s" % (key,val) for (key,val) in tags.items())
+    
+    def _http(self, url, method, data=None):
+        res = None
+
+        try:
+            req = Request(url=url)
+            req.add_header('Content-Type', 'application/json')
+            req.add_header('Hawkular-Tenant', self.tenant_id)
+            req.add_header('Host', self.host)
+
+            if self.token is not None:
+                req.add_header('Authorization', 'Bearer {0}'.format(self.token))
+            elif self.username is not None:
+                req.add_header('Authorization', 'Basic {0}'.format(base64.b64encode(self.username + b':' + self.password)))
+
+            if not isinstance(data, str):
+                data = json.dumps(data, indent=2)
+
+            # writer = codecs.getencoder('utf-8')
+            reader = codecs.getreader('utf-8')
+
+            if data:
+                try:
+                    req.add_data(data)
+                except AttributeError:
+                    req.data = data.encode('utf-8')
+
+            req.get_method = lambda: method
+            res = urlopen(req, context = self.context)
+            if method == 'GET':
+                if res.getcode() == 200:
+                    data = json.load(reader(res))
+
+                elif res.getcode() == 204:
+                    data = {}
+
+                return data
+
+        except Exception as e:
+            self._handle_error(e)
+
+        finally:
+            if res:
+                res.close()        
+    
+    def _put(self, url, data):
+        self._http(url, 'PUT', data)
+
+    def _delete(self, url):
+        self._http(url, 'DELETE')    
+        
+    def _post(self, url, data):
+        self._http(url, 'POST', data)
+
+    def _get(self, url, **url_params):
+        params = urlencode(url_params)
+        if len(params) > 0:
+            url = '{0}?{1}'.format(url, params)
+
+        return self._http(url, 'GET')        
+        
+    def _handle_error(self, e):
+        if isinstance(e, HTTPError):
+            # Cast to HawkularMetricsError
+            e.__class__ = HawkularMetricsError
+            err_json = e.read()
+
+            try:
+                err_d = json.loads(err_json)
+                e.msg = err_d['errorMsg']
+            except:
+                # Keep the original payload, couldn't parse it
+                e.msg = err_json
+
+            raise e
+        elif isinstance(e, URLError):
+            # Cast to HawkularMetricsConnectionError
+            e.__class__ = HawkularMetricsConnectionError
+            e.msg = "Error, could not send event(s) to the Hawkular Metrics: " + str(e.reason)
+            raise e
+        else:
+            raise e
+        
+    def _isfloat(value):
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
+        
+    """
+    External methods
+    """    
+
+    def tenant(self, tenant_id):
+        self.tenant_id = tenant_id
+
+    """
+    Instance methods
+    """
+    
+    def put(self, data):
+        """
+        Send multiple different metric_ids to the server in a single batch. Metrics can be a mixture
+        of types.
+
+        data is a dict or a list of dicts created with create_metric(metric_type, metric_id, datapoints)
+        """
+        if not isinstance(data, list):
+            data = [data]
+
+        r = collections.defaultdict(list)
+
+        for d in data:
+            metric_type = d.pop('type', None)
+            if metric_type is None:
+                raise HawkularMetricsError('Undefined MetricType')
+            r[metric_type].append(d)
+
+        # This isn't transactional, but .. ouh well. One can always repost everything.
+        for l in r:
+            self._post(self._get_metrics_raw_url(self._get_url(l)), r[l])
+
+    def push(self, metric_type, metric_id, value, timestamp=None):
+        """
+        Pushes a single metric_id, datapoint combination to the server.
+
+        This method is an assistant method for the put method by removing the need to
+        create data structures first.
+        """
+        item = create_metric(metric_type, metric_id, create_datapoint(value, timestamp))
+        self.put(item)
+
+    def query_metric(self, metric_type, metric_id, **query_options):
+        """
+        Query for metrics from the server. For possible query_options, see the Hawkular-Metrics documentation.
+        """
+        return self._get(
+            self._get_metrics_raw_url(
+                self._get_metrics_single_url(metric_type, metric_id)),
+            **query_options)
+
+    def query_metric_stats(self, metric_type, metric_id, **query_options):
+        return self._get(
+            self._get_metrics_stats_url(
+                self._get_metrics_single_url(metric_type, metric_id)),
+            **query_options)
+    
+    def query_metric_definitions(self, metric_type=None, id_filter=None, **tags):
+        """
+        Query available metric definitions. Available query options are id_filter for id filtering and tags (dict) as tag based query.
+        Tags filtering is required for the id filtering to work.
+        """
+        if id is not None and tags is None:
+            raise HawkularMetricsError('Tags query is required when id filter is used')
+
+        params = {}
+
+        if metric_type is not None:
+            params = { 'type': MetricType.short(metric_type) }
+
+        if len(tags) > 0:
+            params['tags'] = self._transform_tags(**tags)
+
+        return self._get(self._get_url(), **params)
+
+    def query_tag_values(self, metric_type=None, **tags):
+        """
+        Query for possible tag values. **tags is a dict which has tagname as key and regexp tagvalue as value. See Hawkular-Metrics
+        tag query language for more detailed definition.
+        """
+        tagql = self._transform_tags(**tags)
+
+        return self._get(self._get_metrics_tags_url(self._get_url(metric_type)) + '/{}'.format(tagql))
+
+    def create_metric_definition(self, metric_type, metric_id, **tags):
+        """
+        Create metric definition with custom definition. **tags should be a set of tags, such as
+        units, env ..
+        """
+        item = { 'id': metric_id }
+        if len(tags) > 0:
+            # We have some arguments to pass..
+            data_retention = tags.pop('dataRetention',None)
+            if data_retention is not None:
+                item['dataRetention'] = data_retention
+
+            if len(tags) > 0:
+                item['tags'] = tags
+
+        json_data = json.dumps(item, indent=2)
+        try:
+            self._post(self._get_url(metric_type), json_data)
+        except HawkularMetricsError as e:
+            if e.code == 409:
+                return False
+            raise e
+
+        return True
+        
+    def query_metric_tags(self, metric_type, metric_id):
+        """
+        Returns a list of tags in the metric definition of metric_id
+        """
+        definition = self._get(self._get_metrics_tags_url(self._get_metrics_single_url(metric_type, metric_id)))
+        return definition
+
+    def update_metric_tags(self, metric_type, metric_id, **tags):
+        """
+        Replace the metric_id's tags with given **tags
+        """
+        self._put(self._get_metrics_tags_url(self._get_metrics_single_url(metric_type, metric_id)), tags)
+
+    def delete_metric_tags(self, metric_type, metric_id, **deleted_tags):
+        """
+        Delete one or more tags from the metric definition. 
+        """
+        tags = self._transform_tags(**deleted_tags)
+        tags_url = self._get_metrics_tags_url(self._get_metrics_single_url(metric_type, metric_id)) + '/{0}'.format(tags)
+
+        self._delete(tags_url)    
+        
+    """
+    Tenant related queries
+    """
+    
+    def query_tenants(self):
+        """
+        Query available tenants and their information.
+        """
+        return self._get(self._get_tenants_url())
+
+    def create_tenant(self, tenant_id, retentions=None):
+        """
+        Create a tenant. Currently nothing can be set (to be fixed after the master
+        version of Hawkular-Metrics has fixed implementation.
+        """        
+        item = { 'id': tenant_id }
+        if retentions is not None:
+            item['retentions'] = retentions
+
+        self._post(self._get_tenants_url(), json.dumps(item, indent=2))
+
+"""
+Static methods
+"""
+def time_millis():
+    """
+    Returns current milliseconds since epoch
+    """
+    return int(round(time.time() * 1000))
+
+def create_datapoint(value, timestamp=None, **tags):
+    """
+    Creates a single datapoint dict with a value, timestamp (optional - filled by the method if missing)
+    and tags (optional)
+    """
+    if timestamp is None:
+        timestamp = time_millis()
+
+    item = { 'timestamp': timestamp,
+             'value': value }
+
+    if tags is not None:
+        item['tags'] = tags
+
+    return item
+
+def create_metric(metric_type, metric_id, data):
+    """
+    Create Hawkular-Metrics' submittable structure, data is a datapoint or list of datapoints
+    """
+    if not isinstance(data, list):
+        data = [data]
+    
+    return { 'type': metric_type,'id': metric_id, 'data': data }
+        
---
+++ packages/hawkular-client/hawkular-client-0.4.5.tar.gz/hawkular-client-0.4.5/hawkular/metrics_test.py
@@ -0,0 +1,256 @@
+"""
+   Copyright 2015-2016 Red Hat, Inc. and/or its affiliates
+   and other contributors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+from __future__ import unicode_literals
+
+import unittest
+import uuid
+from metrics import *
+
+class TestMetricFunctionsBase(unittest.TestCase):
+
+    def setUp(self):
+        self.maxDiff = None
+        self.test_tenant = str(uuid.uuid4())
+        self.client = HawkularMetricsClient(tenant_id=self.test_tenant, port=8080)
+        
+class TenantTestCase(TestMetricFunctionsBase):
+    """
+    Test creating and fetching tenants. Each creation test should also
+    fetch the tenants to test that functionality also
+    """
+    
+    def test_tenant_creation(self):
+        tenant = str(uuid.uuid4())
+        self.client.create_tenant(tenant)
+        tenants = self.client.query_tenants()
+
+        expect = { 'id': tenant }
+        self.assertIn(expect, tenants)
+
+    def test_tenant_creation_with_retentions(self):
+        tenant = str(uuid.uuid4())
+        retentions = { 'gauge': 8, 'availability': 30 }
+        self.client.create_tenant(tenant, retentions)
+
+        expect = { 'id': tenant, 'retentions': { 'gauge': 8, 'availability': 30 } }
+        self.assertIn(expect, self.client.query_tenants())
+        
+class MetricsTestCase(TestMetricFunctionsBase):
+    """
+    Test metric functionality, both adding definition and querying for definition, 
+    as well as adding new gauge and availability metrics. 
+
+    Metric definition creation should also test fetching the definition, while
+    metric inserts should test also fetching the metric data.
+    """
+
+    def find_metric(self, name, definitions):
+        for defin in definitions:
+            if defin['id'] == name:
+                return defin
+    
+    def test_gauge_creation(self):
+        """
+        Test creating gauge metric definitions with different tags and definition.
+        """
+        # Create gauge metrics with empty details and added details
+        id_name = 'test.create.gauge.{}'
+
+        md1 = self.client.create_metric_definition(MetricType.Gauge, id_name.format('1'))
+        md2 = self.client.create_metric_definition(MetricType.Gauge, id_name.format('2'), dataRetention=90)
+        md3 = self.client.create_metric_definition(MetricType.Gauge, id_name.format('3'), dataRetention=90, units='bytes', env='qa')
+        self.assertTrue(md1)
+        self.assertTrue(md2)
+        self.assertTrue(md3)
+
+        # Fetch metrics definition and check that the ones we created appeared also
+        m = self.client.query_metric_definitions(MetricType.Gauge)
+        self.assertEqual(3, len(m))
+        self.assertEqual(self.test_tenant, m[0]['tenantId'])
+
+        md3_f = self.find_metric(id_name.format('3'), m)
+
+        self.assertEqual('bytes', md3_f['tags']['units'])
+
+        # This is what the returned dict should look like
+        expect = [
+            { 'dataRetention': 7, 'type': 'gauge', 'id': 'test.create.gauge.1',
+             'tenantId': self.test_tenant },
+            {'dataRetention': 90, 'type': 'gauge', 'id': 'test.create.gauge.2', 'tenantId': self.test_tenant},
+            {'tags': {'units': 'bytes', 'env': 'qa'},
+             'id': 'test.create.gauge.3', 'dataRetention': 90, 'type': 'gauge', 'tenantId': self.test_tenant}]
+
+        for e in expect:
+            self.assertIn(e, m)
+
+        # Lets try creating a duplicate metric
+        md4 = self.client.create_metric_definition(MetricType.Gauge, id_name.format('1'))
+        self.assertFalse(md4, 'Should have received an exception, metric with the same name was already created')
+
+    def test_availability_creation(self):
+        id_name = 'test.create.avail.{}'
+        # Create availability metric
+        # Fetch mterics and check that it did appear
+        self.client.create_metric_definition(MetricType.Availability, id_name.format('1'))
+        self.client.create_metric_definition(MetricType.Availability, id_name.format('2'), dataRetention=90)
+        self.client.create_metric_definition(MetricType.Availability, id_name.format('3'), dataRetention=94, env='qa')
+        # Fetch metrics and check that it did appear
+        m = self.client.query_metric_definitions(MetricType.Availability)        
+        self.assertEqual(3, len(m))
+
+        avail_3 = self.find_metric(id_name.format('3'), m)
+        self.assertEqual(94, avail_3['dataRetention'])
+
+    def test_tags_modifications(self):
+        m = 'test.create.tags.1'
+        # Create metric without tags
+        self.client.create_metric_definition(MetricType.Gauge, m)
+        e = self.client.query_metric_tags(MetricType.Gauge, m)
+        self.assertIsNotNone(e)
+        self.assertEqual({}, e)
+        # Add tags
+        self.client.update_metric_tags(MetricType.Gauge, m, hostname='machine1', a='b')
+        # Fetch metric - check for tags
+        tags = self.client.query_metric_tags(MetricType.Gauge, m)
+        self.assertEqual(2, len(tags))
+        self.assertEqual("b", tags['a'])
+        # Delete some metric tags
+        self.client.delete_metric_tags(MetricType.Gauge, m, a='b', hostname='machine1')
+        # Fetch metric - check that tags were deleted
+        tags_2 = self.client.query_metric_tags(MetricType.Gauge, m)
+        self.assertEqual(0, len(tags_2))
+
+    def test_tags_queries(self):
+        for i in range(1,9):
+            m_id = 'test.query.tags.{}'.format(i)
+            hostname = 'host{}'.format(i)
+            self.client.create_metric_definition(MetricType.Counter, m_id, hostname=hostname, env='qa')
+
+        mds = self.client.query_metric_definitions(hostname='host[123]')
+        self.assertEqual(3, len(mds))
+
+        values = self.client.query_tag_values(MetricType.Counter, hostname='host.*', env='qa')
+        self.assertEqual(2, len(values))
+
+    def test_add_gauge_single(self):
+        # Normal way
+        value = float(4.35)
+        datapoint = create_datapoint(value, time_millis())
+        metric = create_metric(MetricType.Gauge, 'test.gauge./', datapoint)
+        self.client.put(metric)
+
+        # Fetch results
+        data = self.client.query_metric(MetricType.Gauge, 'test.gauge./')
+        self.assertEqual(float(data[0]['value']), value)
+
+        # Shortcut method with tags
+        self.client.push(MetricType.Gauge, 'test.gauge.single.tags', value)
+
+        # Fetch results
+        data = self.client.query_metric(MetricType.Gauge, 'test.gauge.single.tags')
+        self.assertEqual(value, float(data[0]['value']))
+
+    def test_add_availability_single(self):
+        self.client.push(MetricType.Availability, 'test.avail.1', Availability.Up)
+        self.client.push(MetricType.Availability, 'test.avail.2', 'down')
+
+        up = self.client.query_metric(MetricType.Availability, 'test.avail.1')
+        self.assertEqual(up[0]['value'], 'up')
+        
+        down = self.client.query_metric(MetricType.Availability, 'test.avail.2')
+        self.assertEqual(down[0]['value'], Availability.Down)
+
+    def test_add_string_single(self):
+        self.client.push(MetricType.String, 'test.string.1', "foo")
+        data = self.client.query_metric(MetricType.String, 'test.string.1')
+        self.assertEqual(data, 'foo')
+        
+    def test_add_gauge_multi_datapoint(self):
+        metric_1v = create_datapoint(float(1.45))
+        metric_2v = create_datapoint(float(2.00), (time_millis() - 2000))
+
+        metric = create_metric(MetricType.Gauge, 'test.gauge.multi', [metric_1v, metric_2v])
+        self.client.put(metric)
+        
+        data = self.client.query_metric(MetricType.Gauge, 'test.gauge.multi')
+        self.assertEqual(len(data), 2)
+        self.assertEqual(data[0]['value'], float(1.45))
+        self.assertEqual(data[1]['value'], float(2.00))
+
+    def test_add_availability_multi_datapoint(self):
+        t = time_millis()
+        up = create_datapoint('up', (t - 2000))
+        down = create_datapoint('down', t)
+
+        m = create_metric(MetricType.Availability, 'test.avail.multi', [up, down])
+        
+        self.client.put(m)
+        data = self.client.query_metric(MetricType.Availability, 'test.avail.multi')
+
+        self.assertEqual(len(data), 2)
+        self.assertEqual(data[0]['value'], 'down')
+        self.assertEqual(data[1]['value'], 'up')
+
+    def test_add_mixed_metrics_and_datapoints(self):
+        metric1 = create_datapoint(float(1.45))
+        metric1_2 = create_datapoint(float(2.00), (time_millis() - 2000))
+
+        metric_multi = create_metric(MetricType.Gauge, 'test.multi.gauge.1', [metric1, metric1_2])
+
+        metric2 = create_datapoint(Availability.Up)
+        metric2_multi = create_metric(MetricType.Availability,'test.multi.gauge.2', [metric2])
+
+        self.client.put([metric_multi, metric2_multi])
+
+        # Check that both were added correctly..
+        metric1_data = self.client.query_metric(MetricType.Gauge, 'test.multi.gauge.1')
+        metric2_data = self.client.query_metric(MetricType.Availability, 'test.multi.gauge.2')
+
+        self.assertEqual(2, len(metric1_data))
+        self.assertEqual(1, len(metric2_data))
+
+    def test_query_options(self):
+        # Create metric with two values
+        t = time_millis()
+        v1 = create_datapoint(float(1.45), t)
+        v2 = create_datapoint(float(2.00), (t - 2000))
+
+        m = create_metric(MetricType.Gauge, 'test.query.gauge.1', [v1, v2])
+        self.client.put(m)
+
+        # Query first without limitations
+        d = self.client.query_metric(MetricType.Gauge, 'test.query.gauge.1')
+        self.assertEqual(2, len(d))
+
+        # Query for data which has start time limitation
+        d = self.client.query_metric(MetricType.Gauge, 'test.query.gauge.1', start=(t-1000))
+        self.assertEqual(1, len(d))
+
+    def test_tenant_changing(self):
+        self.client.create_metric_definition(MetricType.Availability, 'test.tenant.avail.1')
+        # Fetch metrics and check that it did appear
+        m = self.client.query_metric_definitions(MetricType.Availability)
+        self.assertEqual(1, len(m))
+
+        tenant_old = self.client.tenant_id
+        self.client.tenant(str(uuid.uuid4()))
+        m = self.client.query_metric_definitions(MetricType.Availability)
+        self.assertEqual(0, len(m))
+
+        
+if __name__ == '__main__':
+    unittest.main()
