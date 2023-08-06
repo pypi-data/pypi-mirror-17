12ebfa1eb4ea30002bab3cee7554268e9a36ea35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:32 +0000

    Add package wagtailsocialfeed/0.1.0/wagtailsocialfeed-0.1.0-py2.py3-none-any.whl

---
+++ packages/wagtailsocialfeed/wagtailsocialfeed-0.1.0-py2.py3-none-any.whl/wagtailsocialfeed/blocks.py
@@ -0,0 +1,40 @@
+from __future__ import unicode_literals
+
+from django import forms
+
+from wagtail.wagtailcore import blocks
+
+from .models import SocialFeedConfiguration
+from .utils import get_feed_items
+
+
+class FeedChooserBlock(blocks.ChooserBlock):
+    target_model = SocialFeedConfiguration
+    widget = forms.Select
+
+    def value_for_form(self, value):
+        if value:
+            if isinstance(value, int):
+                return value
+            return value.pk
+        return None
+
+    def to_python(self, value):
+        return self.target_model.objects.get(pk=value)
+
+
+class SocialFeedBlock(blocks.StructBlock):
+    feedconfig = FeedChooserBlock()
+    limit = blocks.IntegerBlock(required=False, min_value=0)
+
+    class Meta:
+        icon = 'icon icon-fa-rss'
+        template = 'wagtailsocialfeed/social_feed_block.html'
+
+    def get_context(self, value):
+        context = super(SocialFeedBlock, self).get_context(value)
+
+        feedconfig = value['feedconfig']
+        context['feed'] = get_feed_items(feedconfig, limit=value['limit'])
+
+        return context
