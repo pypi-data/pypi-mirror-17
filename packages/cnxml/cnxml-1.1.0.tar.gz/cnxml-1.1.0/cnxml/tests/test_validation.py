daf0ee8189309ebb42ef93e67f453b17050f78b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:34 +0000

    Add package cnxml/1.1.0/cnxml-1.1.0.tar.gz

---
+++ packages/cnxml/cnxml-1.1.0.tar.gz/cnxml-1.1.0/cnxml/tests/test_validation.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+def test_validate_cnxml(datadir):
+    from cnxml import validate_cnxml
+    errors = validate_cnxml(datadir / 'valid.cnxml')
+    assert errors == tuple()
+
+
+def test_cnxml_validation_messages(datadir):
+    from cnxml import validate_cnxml
+    expected = (
+        ['30', '17', 'error', 'unfinished element'],
+        ['55', '20', 'error', 'unfinished element'],
+        ['67', '11', 'error', 'required attributes missing'],
+    )
+    errors = validate_cnxml(datadir / 'invalid.cnxml')
+    assert tuple(list(l) for l in errors) == expected
