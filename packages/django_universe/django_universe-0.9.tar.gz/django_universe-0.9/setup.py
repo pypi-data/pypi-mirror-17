4e369a6777bc12ccc4e87abebf5dcdfd57b8596b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:44 +0000

    Add package django_universe/0.9/django_universe-0.9.tar.gz

---
+++ packages/django_universe/django_universe-0.9.tar.gz/django_universe-0.9/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name='django_universe',
+    version='0.9',
+    packages=['django_universe'],
+    description='A line of description for v0.8',
+    long_description=README,
+    author='jerin',
+    author_email='jerinzam@gmail.com',
+    url='https://github.com/jerinzam/django-universe/',
+    download_url='https://github.com/jerinzam/django-universe/tarball/0.8',
+    license='MIT',
+    install_requires=[
+        'Django>=1.6',
+    ]
+)
