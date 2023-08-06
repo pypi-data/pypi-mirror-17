063bed3fea22978cfe4fd76d385fdc777600c718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:18 +0000

    Add package liuxiaohui_nester/1.0.0/liuxiaohui_nester-1.0.0.zip

---
+++ packages/liuxiaohui_nester/liuxiaohui_nester-1.0.0.zip/liuxiaohui_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='liuxiaohui_nester',
+    version='1.0.0',
+    py_modules=['liuxiaohui_nester'],
+    author='heart4u',
+    author_email='heart4u@163.com',
+    url='http://www.2018studio.cn',
+    description='A simple printer of nested lists'
+)
