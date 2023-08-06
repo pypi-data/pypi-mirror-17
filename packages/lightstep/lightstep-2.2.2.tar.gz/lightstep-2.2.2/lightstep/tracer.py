503c7dbc25d79090d0ed3d288e00a8cf83f0b67d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:40 +0000

    Add package lightstep/2.2.2/lightstep-2.2.2.tar.gz

---
+++ packages/lightstep/lightstep-2.2.2.tar.gz/lightstep-2.2.2/lightstep/tracer.py
@@ -0,0 +1,63 @@
+"""
+LightStep's implementation of the python OpenTracing API.
+
+http://opentracing.io
+https://github.com/opentracing/api-python
+
+See the API definition for comments.
+"""
+from __future__ import absolute_import
+
+from basictracer import BasicTracer
+from basictracer.text_propagator import TextPropagator
+from opentracing import Format
+
+from .recorder import Recorder, LoggingRecorder
+
+
+def init_tracer(**kwargs):
+    """Instantiates LightStep's OpenTracing implementation.
+
+    :param str group_name: name identifying the type of service that is being tracked
+    :param str access_token: project's access token
+    :param bool secure: whether HTTP connection is secure
+    :param str service_host: Service host name
+    :param int service_port: Service port number
+    :param int max_span_records: Maximum number of spans records to buffer
+    :param int periodic_flush_seconds: seconds between periodic flushes, or 0
+        to disable
+    :param bool disable_binary_format: Whether to disable the binary
+        inject/extract format (which relies on protobufs and may cause problems
+        if other versions of protobufs are active in the same packaging
+        configuration). Defaults to False (i.e., binary format is enabled).
+    """
+    enable_binary_format = True
+    if kwargs.has_key('disable_binary_format'):
+        enable_binary_format = not kwargs['disable_binary_format']
+        del kwargs['disable_binary_format']
+    return _LightstepTracer(enable_binary_format, Recorder(**kwargs))
+
+
+def init_debug_tracer():
+    """Returns a tracer that logs to the console instead of reporting to
+    LightStep."""
+    tracer = BasicTracer(LoggingRecorder())
+    tracer.register_required_propagators()
+    return tracer
+
+
+class _LightstepTracer(BasicTracer):
+    def __init__(self, enable_binary_format, recorder):
+        """Initialize the LightStep Tracer, deferring to BasicTracer."""
+        super(_LightstepTracer, self).__init__(recorder)
+        self.register_propagator(Format.TEXT_MAP, TextPropagator())
+        self.register_propagator(Format.HTTP_HEADERS, TextPropagator())
+        if enable_binary_format:
+            # We do this import lazily because protobuf versioning issues
+            # can cause process-level failure at import time.
+            from basictracer.binary_propagator import BinaryPropagator
+            self.register_propagator(Format.BINARY, BinaryPropagator())
+
+    def flush(self):
+        """Force a flush of buffered Span data to the LightStep collector."""
+        self.recorder.flush()
