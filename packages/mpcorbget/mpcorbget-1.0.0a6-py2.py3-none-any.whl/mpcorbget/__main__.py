6c03db0fec0ff8685749df594ef7197ecffa7db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:42 +0000

    Add package mpcorbget/1.0.0a6/mpcorbget-1.0.0a6-py2.py3-none-any.whl

---
+++ packages/mpcorbget/mpcorbget-1.0.0a6-py2.py3-none-any.whl/mpcorbget/__main__.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import mpcorbget as mpc
+
+def main():
+    """Handy dandy quick ephemeris tool"""
+    print("QuickEphem v1.1 | Code by Alex Davenport\n----------------------------------------------")
+    asteroid = input("Asteroid Designation: ")
+    observatory = input("Observatory Code: ")
+    datetime = input("UTC (YYYY/MM/DD HH:MM:SS): ")
+    ast = MPCORB(asteroid)
+    observatory = Observatory(observatory)
+    geo = ast.geocentric(datetime)
+    topo = ast.topocentric(observatory.location, datetime)
+    print("----------------------------------------------")
+    print(geo)
+    print()
+    print(topo)
+
+if __name__ == "__main__":
+    main()
