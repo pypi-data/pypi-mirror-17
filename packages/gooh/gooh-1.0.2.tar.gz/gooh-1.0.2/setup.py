d0cdb960212a7f647650aa10ddb3043caaa44803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:49 +0000

    Add package gooh/1.0.2/gooh-1.0.2.tar.gz

---
+++ packages/gooh/gooh-1.0.2.tar.gz/gooh-1.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='gooh',
+      version='1.0.2',
+      description='Language agnostic semantic versioner using git tags',
+      url='http://github.com/lxhunter/gooh',
+      author='Alexander Jaeger',
+      author_email='alexander.jaeger@me.com',
+      license='MIT',
+      scripts=['bin/gooh'],
+      packages=['gooh'],
+      zip_safe=False,
+      install_requires=[
+          'semantic_version', 'sh', 'pyyaml']
+      )
