2d629747670ad530a65a3be0bb621fbc1d379325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:49 +0000

    Add package threeandthrees/0.2.0/threeandthrees-0.2.0.tar.gz

---
+++ packages/threeandthrees/threeandthrees-0.2.0.tar.gz/threeandthrees-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='threeandthrees',
+    version='0.2.0',
+    description='A CLI 3&3s game',
+    long_description='Play a word game against the words file.',
+    author='Ben Warren',
+    url='https://github.com/bwarren2/threeandthrees',
+    author_email='bwarren2@gmail.com',
+    license='MIT',
+    platforms='any',
+    py_modules=['threeandthrees'],
+    package_data={'': ['*.txt']},
+    install_requires=required,
+)
