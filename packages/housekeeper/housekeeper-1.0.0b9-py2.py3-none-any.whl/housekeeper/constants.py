24cf365e0636076a581cd952531c2b957bcbea64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:01 +0000

    Add package housekeeper/1.0.0b9/housekeeper-1.0.0b9-py2.py3-none-any.whl

---
+++ packages/housekeeper/housekeeper-1.0.0b9-py2.py3-none-any.whl/housekeeper/constants.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from datetime import timedelta
+
+PIPELINES = ('mip',)
+# three months before clean up by default
+TIME_TO_CLEANUP = timedelta(days=(30 * 3))
+ARCHIVE_TYPES = ('data', 'result', 'meta')
