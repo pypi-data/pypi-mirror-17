06ae960aa49d2801f46b2cf49b7f5804283b6cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:51 +0000

    Add package wafer/0.4.3/wafer-0.4.3-py3-none-any.whl

---
+++ packages/wafer/wafer-0.4.3-py3-none-any.whl/wafer/talks/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import patterns, url, include
+from rest_framework import routers
+
+from wafer.talks.views import (
+    Speakers, TalkCreate, TalkDelete, TalkUpdate, TalkView, UsersTalks,
+    TalksViewSet)
+
+router = routers.DefaultRouter()
+router.register(r'talks', TalksViewSet)
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', UsersTalks.as_view(), name='wafer_users_talks'),
+    url(r'^page/(?P<page>\d+)/$', UsersTalks.as_view(),
+        name='wafer_users_talks_page'),
+    url(r'^new/$', TalkCreate.as_view(), name='wafer_talk_submit'),
+    url(r'^(?P<pk>\d+)/$', TalkView.as_view(), name='wafer_talk'),
+    url(r'^(?P<pk>\d+)/edit/$', TalkUpdate.as_view(),
+        name='wafer_talk_edit'),
+    url(r'^(?P<pk>\d+)/delete/$', TalkDelete.as_view(),
+        name='wafer_talk_delete'),
+    url(r'^speakers/$', Speakers.as_view(), name='wafer_talks_speakers'),
+    url(r'^api/', include(router.urls)),
+)
