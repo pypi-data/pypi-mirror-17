5f4f3b6aeda51458223f0a20f49162d82feb1d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:33 +0000

    Add package vinayak123/0.1/vinayak123-0.1.zip

---
+++ packages/vinayak123/vinayak123-0.1.zip/vinayak123-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='vinayak123',    # This is the name of your PyPI-package.
+    author='vinayak b',
+    author_email='workvinayak95@gmail.com',
+    url = "http://chardet.feedparser.org/",
+    version='0.1',                          # Update the version number for new releases
+    scripts=['test']                  # The name of your scipt, and also the command you'll be using for calling it
+)
