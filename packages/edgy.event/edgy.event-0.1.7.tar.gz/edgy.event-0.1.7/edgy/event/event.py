e67ec4d23328be3aa484fa5a1d42b0f2133de20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:21 +0000

    Add package edgy.event/0.1.7/edgy.event-0.1.7.tar.gz

---
+++ packages/edgy.event/edgy.event-0.1.7.tar.gz/edgy.event-0.1.7/edgy/event/event.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+class Event(object):
+    propagation_stopped = False
+
+    def stop_propagation(self):
+        self.propagation_stopped = True
