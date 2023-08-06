25007ed2d3921c4e654eaeed6dc737a2db6f600f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:26 +0000

    Add package learn2map/0.1.0/learn2map-0.1.0.tar.gz

---
+++ packages/learn2map/learn2map-0.1.0.tar.gz/learn2map-0.1.0/setup.py
@@ -0,0 +1,43 @@
+"""
+Setup raster2stats package.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='learn2map',
+    version='0.1.0',
+    description='Spatial mapping from remote sensing data',
+    url='https://gitlab.com/alanxuliang/a1610_learn2map',
+
+    author='Alan Xu',
+    author_email='bireme@gmail.com',
+
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: GIS',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='DRC Forest Biomass',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src', exclude=['data', 'docs', 'tests']),
+
+    install_requires=[
+        'numpy',
+        'pandas',
+        'GDAL>=1.10',
+        'lxml',
+        'scikit-learn',
+        'xgboost',
+        'seaborn',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'rf_estimator=rf_estimator:main',
+        ],
+    },
+)
