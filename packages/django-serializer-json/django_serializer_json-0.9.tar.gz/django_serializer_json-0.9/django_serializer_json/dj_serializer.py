448d94862402febe5700f961181ad5b4596392e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:31 +0000

    Add package django-serializer-json/0.9/django_serializer_json-0.9.tar.gz

---
+++ packages/django-serializer-json/django_serializer_json-0.9.tar.gz/django_serializer_json-0.9/django_serializer_json/dj_serializer.py
@@ -0,0 +1,24 @@
+import json
+
+
+def json_loads(dict_results):
+    json_data = json.dumps(dict_results)
+    return json.loads(json_data)
+
+
+def dj_serializer(queryset, result_key='result'):
+    error = {"error": queryset}
+    if not(isinstance(queryset, str)):
+        dict_results = {}
+        result_list = []
+        for instance in queryset:
+            loop_dict = {}
+            for field in instance._meta.local_fields:
+                field_name = field.name
+                attribute = getattr(instance, field_name)
+                loop_dict[field_name] = str(attribute)
+            result_list.append(loop_dict)
+        dict_results[result_key] = result_list
+        return json_loads(dict_results)
+    else:
+        return json_loads(error)
