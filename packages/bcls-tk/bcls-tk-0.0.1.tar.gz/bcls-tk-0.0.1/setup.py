5cc344433e3c7443ec4b35c748f18c586ee45391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:14 +0000

    Add package bcls-tk/0.0.1/bcls-tk-0.0.1.tar.gz

---
+++ packages/bcls-tk/bcls-tk-0.0.1.tar.gz/bcls-tk-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'bcls-tk',
+    packages = ['pkgA', 'pkgB'],
+    version = '0.0.1',
+    description = 'Test for Binary Clustering Tool Kit',
+    author = 'echonesis',
+    author_email = 'echonesis@gmail.com',
+    url = 'https://github.com/echonesis/bcls-tk',
+    download_url = 'https://github.com/echonesis/bcls-tk/tarball/v0.0.1',
+    keywords = ['binary image', 'clustering algorithm'],
+    classifiers = [],
+)
