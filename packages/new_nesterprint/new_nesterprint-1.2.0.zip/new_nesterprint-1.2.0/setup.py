ce856835d3407395084e03b2819952ebb849686d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:47 +0000

    Add package new_nesterprint/1.2.0/new_nesterprint-1.2.0.zip

---
+++ packages/new_nesterprint/new_nesterprint-1.2.0.zip/new_nesterprint-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'new_nesterprint',
+        version = '1.2.0',
+        py_modules = ['new_nesterprint'],
+        author = 'xiaojianzheng',
+        author_email = '1272209235@qq.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+    )
