b8ee7f0b14c22d6e2da853fbbd89cd348b67786b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:50 +0000

    Add package static_tl/0.5.1/static_tl-0.5.1.tar.gz

---
+++ packages/static_tl/static_tl-0.5.1.tar.gz/static_tl-0.5.1/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+setup(name="static_tl",
+      version="0.5.1",
+      description="Generate a static HTML website from your twitter time line",
+      url="https://github.com/dmerejkowsy/static_tl",
+      author="Dimitri Merejkowsky",
+      author_email="d.merej@gmail.com",
+      packages=find_packages(),
+      package_data={
+          "static_tl" : [
+              "static/*",
+              "templates/*",
+          ],
+      },
+      install_requires=[
+          "arrow",
+          "feedgenerator",
+          "flask",
+          "jinja2",
+          "toml",
+          "twitter"
+      ],
+      license="BSD",
+      entry_points = {
+        "console_scripts" : [
+            "static-tl   = static_tl.main:main"
+        ]
+      },
+      classifiers=[
+          "Environment :: Console",
+          "License :: OSI Approved :: BSD License",
+          "Programming Language :: Python :: 3 :: Only",
+    ]
+)
