1c54b50b131658a69eee7775374cecd1a14aee6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:48 +0000

    Add package boxed/0.3.7/boxed-0.3.7.tar.gz

---
+++ packages/boxed/boxed-0.3.7.tar.gz/boxed-0.3.7/src/boxed/jsonbox/__init__.py
@@ -0,0 +1,49 @@
+import json
+import logging
+
+from boxed.core import return_from_status_data, execute_subprocess, indent, SerializationError
+logger = logging.getLogger('boxed.jsonbox')
+
+
+def run(target, args=(), kwargs=None, *, timeout=None, user='nobody',
+        imports=(), print_messages=False):
+    """Simple sandboxing based on the existence of the python executable
+    `python_boxed` with setuid capabilities.
+
+    This function communicates with the slave process with a JSON-based stream.
+
+    This sandboxing relies on changing the uid to the unprivileged user nobody.
+    """
+
+    # Creates and serializes input data
+    data = {
+        'header': 'jsonbox::0.1',
+        'target': '%s.%s' % (target.__module__, target.__qualname__),
+        'args': args,
+        'kwargs': kwargs or {},
+        'user': user,
+        'imports': imports,
+    }
+    try:
+        serialized = json.dumps(data)
+    except Exception as ex:
+        raise SerializationError(ex)
+
+    # Execute a subprocess by sending the input JSON structure. We should
+    # receive another JSON structure and interpret it
+    logger.info('called %s() on sandbox' % data['target'])
+    json_data, comments = execute_subprocess(
+        ['python_boxed', '-m', 'boxed.jsonbox'],
+        inputs=serialized,
+        timeout=timeout,
+        args=args,
+        kwargs=kwargs,
+        target=target,
+    )
+    if print_messages:
+        print(comments)
+
+    # The output should be JSON-encodable and contain the result of
+    # execution
+    logger.debug('child sub-process finished. Processing response.')
+    return return_from_status_data(json_data, comments, json.loads)
