ffb819a3b35a63aab8e8fe65d231062872511b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:04 +0000

    Add package kanjinetworks/0.1.6/kanjinetworks-0.1.6.tar.gz

---
+++ packages/kanjinetworks/kanjinetworks-0.1.6.tar.gz/kanjinetworks-0.1.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+def requires():
+    with open('requirements.txt') as f:
+        return f.read().split("\n")
+
+config = {
+    'name': 'kanjinetworks',
+    'version': '0.1.6',
+    'description': 'Kanji Networks interface',
+    'long_description': readme(),
+    'license': 'MIT',
+    'author': 'Arnaud Coomans',
+    'author_email': 'arnaud.coomans@gmail.com',
+    'url': 'https://github.com/acoomans/kanjinetworks',
+    'download_url': 'https://github.com/acoomans/kanjinetworks/archive/master.zip',
+    'install_requires': requires(),
+    'packages': ['kanjinetworks', 'kanjinetworks.extract', 'kanjinetworks.export'],
+    'scripts': ['scripts/kn_to_ja.py'],
+    'package_data': {'kanjinetworks': ['data/*']},
+    'include_package_data': True,
+    'zip_safe': False,
+    'test_suite': 'kanjinetworks',
+}
+
+setup(**config)
