1a5c722be0dfa0187722017d5e65ed910218aa10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:02 +0000

    Add package rwt/2.11/rwt-2.11.tar.gz

---
+++ packages/rwt/rwt-2.11.tar.gz/rwt-2.11/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+# Project skeleton maintained at https://github.com/jaraco/skeleton
+
+import io
+import sys
+
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+	long_description = readme.read()
+
+needs_pytest = {'pytest', 'test'}.intersection(sys.argv)
+pytest_runner = ['pytest_runner'] if needs_pytest else []
+needs_wheel = {'release', 'bdist_wheel', 'dists'}.intersection(sys.argv)
+wheel = ['wheel'] if needs_wheel else []
+
+name = 'rwt'
+description = 'run with this'
+
+setup_params = dict(
+	name=name,
+	use_scm_version=True,
+	author="Jason R. Coombs",
+	author_email="jaraco@jaraco.com",
+	description=description or name,
+	long_description=long_description,
+	url="https://github.com/jaraco/" + name,
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+	namespace_packages=name.split('.')[:-1],
+	install_requires=[
+	],
+	extras_require={
+	},
+	setup_requires=[
+		'setuptools_scm>=1.9',
+	] + pytest_runner + wheel,
+	tests_require=[
+		'pytest>=2.8',
+	],
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+	],
+	entry_points={
+		'console_scripts': [
+			'rwt = rwt:run',
+		],
+	},
+)
+if __name__ == '__main__':
+	setuptools.setup(**setup_params)
