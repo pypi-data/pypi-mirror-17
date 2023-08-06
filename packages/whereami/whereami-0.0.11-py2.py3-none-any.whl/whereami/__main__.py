64971f20d0e9b2aa4a63ed550e6be02648cc2a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:09 +0000

    Add package whereami/0.0.11/whereami-0.0.11-py2.py3-none-any.whl

---
+++ packages/whereami/whereami-0.0.11-py2.py3-none-any.whl/whereami/__main__.py
@@ -0,0 +1,15 @@
+import sys
+from whereami.predict import predict
+from whereami.predict import predict_proba
+from whereami.learn import learn
+
+
+def main():
+    if "predict_proba" == sys.argv[1]:
+        predict_proba()
+    elif "predict" == sys.argv[1]:
+        predict("-tts" in sys.argv)
+    elif "learn" in sys.argv[1] and len(sys.argv) == 4:
+        category = sys.argv[2]
+        n = int(sys.argv[3])
+        learn(category, n)
