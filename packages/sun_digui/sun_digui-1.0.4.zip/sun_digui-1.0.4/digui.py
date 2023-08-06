0a447ad0aba74e2471f71205eb57655ebd5eaf49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:36 +0000

    Add package sun_digui/1.0.4/sun_digui-1.0.4.zip

---
+++ packages/sun_digui/sun_digui-1.0.4.zip/sun_digui-1.0.4/digui.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+movices=["a","b",["c","d"],"e",["f"]]
+'''for index in movices:
+    if isinstance(index,list):
+      for index1 in index:
+            if isinstance(index1,list):
+                    for index2 in index1:
+                        print(index2)
+            else:
+                     print(index1)
+    else:
+from aifc import data
+       print(index)'''
+def print_list(_list,level=0):
+    for index in _list:
+        if isinstance(index,list):
+          print_list(index,level+1)
+        else:
+          for tab_stop in range(level):  
+            print('\t')
+        print(index)
+      
