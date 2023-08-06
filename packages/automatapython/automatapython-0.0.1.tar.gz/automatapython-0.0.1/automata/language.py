2375567d188084a15dd48c82126c65a0c875b7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:32 +0000

    Add package automatapython/0.0.1/automatapython-0.0.1.tar.gz

---
+++ packages/automatapython/automatapython-0.0.1.tar.gz/automatapython-0.0.1/automata/language.py
@@ -0,0 +1,7 @@
+
+
+def length(string):
+    return len(str(string))
+
+def reverse(string):
+    return string[::1]
---
+++ packages/automatapython/automatapython-0.0.1.tar.gz/automatapython-0.0.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""Setup logic for pip."""
+
+from setuptools import setup
+
+
+def get_long_description():
+    """Get long description used on PyPI project page."""
+    try:
+        # Use pandoc to create reStructuredText README if possible
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except:
+        return None
+
+
+setup(
+    name='automatapython',
+    version='0.0.1',
+    description='The only automaton library you will ever need',
+    long_description=get_long_description(),
+    url='https://github.com/hemangsk/automatapython',
+    author='Hemang Kumar',
+    author_email='hemangsk@gmail.com',
+    license='GPLv3',
+    keywords='language finite automata turing machine push down automata linear bound automata',
+    packages=[
+        'automata'
+    ],
+    install_requires=[],
+    entry_points={}
+)
