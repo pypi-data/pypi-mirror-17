4a80b83647820a3d3883b0aae5b9a6efe765238b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:13 +0000

    Add package tvlc/0.4/tvlc-0.4.tar.gz

---
+++ packages/tvlc/tvlc-0.4.tar.gz/tvlc-0.4/setup.py
@@ -0,0 +1,24 @@
+
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+setup(name="tvlc",
+	  version="0.4",
+	  description="Stream movies directly from torrent",
+	  url="https://github.com/RatulGhosh/movie-stream",
+	  author="Ratul Ghosh",
+	  author_email="ratulghoshr@gmail.com",
+	  license='MIT',
+	  packages=["tvlc"],
+	  scripts=["bin/tvlc.py"],
+	  long_description=open('README.rst').read(),
+	  install_requires=[
+		  'BeautifulSoup4',
+		  'requests'],
+)
