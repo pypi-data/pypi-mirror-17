15fe3a2ac69bad07761753525cdd63bad11f027c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:08 +0000

    Add package atomman/1.0.1/atomman-1.0.1-py2-none-any.whl

---
+++ packages/atomman/atomman-1.0.1-py2-none-any.whl/atomman/__init__.py
@@ -0,0 +1,8 @@
+from core import *
+import unitconvert
+import defect
+import lammps
+import tools
+import convert
+
+unitconvert.reset_units(length = 'angstrom', mass = 'amu', energy='eV', charge='e')
