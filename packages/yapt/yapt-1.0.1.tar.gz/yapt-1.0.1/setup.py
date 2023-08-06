5815b04da3283055a86fc396e79ae29ae11032e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:53 +0000

    Add package yapt/1.0.1/yapt-1.0.1.tar.gz

---
+++ packages/yapt/yapt-1.0.1.tar.gz/yapt-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup, Extension
+
+setup(name='yapt',
+      version='1.0.1',
+      py_modules=['yapt.confighandler', 'yapt.wrapper', 'yapt.__main__'],
+      scripts=['bin/yapt'],
+      url='https://github.com/AnderRasoVazquez/yapt',
+      download_url='https://github.com/AnderRasoVazquez/yapt/archive/v1.0.1.tar.gz',
+      author='Ander Raso',
+      author_email='anderraso@gmail.com',
+      license='gplv3',
+      long_description='Creates an installation menu around "apt search" command',
+      description='Creates an installation menu around "apt search" command',
+      summary='Creates an installation menu around "apt search" command',
+      data_files=[('/usr/share/yapt', ['examples/yaptrc'])],
+      )
