3001a62a25e695e9f7a552a79d6e0588191185c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:04 +0000

    Add package microdrop/2.0.post110.dev156674653/microdrop-2.0.post110.dev156674653.tar.gz

---
+++ packages/microdrop/microdrop-2.0.post110.dev156674653.tar.gz/microdrop-2.0.post110.dev156674653/microdrop/core_plugins/electrode_controller_plugin/__init__.py
@@ -0,0 +1,253 @@
+"""
+Copyright 2015 Christian Fobel
+
+This file is part of electrode_controller_plugin.
+
+electrode_controller_plugin is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+electrode_controller_plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License
+along with electrode_controller_plugin.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import logging
+
+from zmq_plugin.plugin import Plugin as ZmqPlugin
+from zmq_plugin.schema import decode_content_data
+import gobject
+import gtk
+import pandas as pd
+import zmq
+
+from ...app_context import get_app, get_hub_uri
+from ...plugin_helpers import StepOptionsController
+from ...plugin_manager import (PluginGlobals, SingletonPlugin, IPlugin,
+                               implements, emit_signal)
+
+logger = logging.getLogger(__name__)
+
+def drop_duplicates_by_index(series):
+    '''
+    Drop all but first entry for each set of entries with the same index value.
+
+    Args:
+
+        series (pandas.Series) : Input series.
+
+    Returns:
+
+        (pandas.Series) : Input series with *first* value in `series` for each
+            *distinct* index value (i.e., duplicate entries dropped for same
+            index value).
+    '''
+    return series[~series.index.duplicated()]
+
+
+class ElectrodeControllerZmqPlugin(ZmqPlugin):
+    '''
+    API for turning electrode(s) on/off.
+
+    Must handle:
+     - Updating state of hardware channels (if connected).
+     - Updating device user interface.
+    '''
+    def __init__(self, parent, *args, **kwargs):
+        self.parent = parent
+        self.control_board = None
+        super(ElectrodeControllerZmqPlugin, self).__init__(*args, **kwargs)
+
+    @property
+    def electrode_states(self):
+        # Set the state of DMF device channels.
+        step_options = self.parent.get_step_options()
+        return step_options.get('electrode_states', pd.Series())
+
+    @electrode_states.setter
+    def electrode_states(self, electrode_states):
+        # Set the state of DMF device channels.
+        step_options = self.parent.get_step_options()
+        step_options['electrode_states'] = electrode_states
+
+    def get_actuated_area(self, electrode_states):
+        '''
+        Get area of actuated electrodes.
+        '''
+        app = get_app()
+        actuated_electrodes = electrode_states[electrode_states > 0].index
+        return app.dmf_device.electrode_areas.ix[actuated_electrodes].sum()
+
+    def get_state(self, electrode_states):
+        app = get_app()
+
+        electrode_channels = (app.dmf_device
+                              .actuated_channels(electrode_states.index)
+                              .dropna().astype(int))
+
+        # Each channel should be represented *at most* once in
+        # `channel_states`.
+        channel_states = pd.Series(electrode_states
+                                   .ix[electrode_channels.index].values,
+                                   index=electrode_channels)
+        # Duplicate entries may result from multiple electrodes mapped to the
+        # same channel or vice versa.
+        channel_states = drop_duplicates_by_index(channel_states)
+
+        channel_electrodes = (app.dmf_device.electrodes_by_channel
+                              .ix[channel_states.index])
+        electrode_states = pd.Series(channel_states
+                                     .ix[channel_electrodes.index].values,
+                                     index=channel_electrodes.values)
+
+        # Each electrode should be represented *at most* once in
+        # `electrode_states`.
+        return {'electrode_states': drop_duplicates_by_index(electrode_states),
+                'channel_states': channel_states}
+
+    def get_channel_states(self):
+        '''
+        Returns:
+
+            (pandas.Series) : State of channels, indexed by channel.
+        '''
+        result = self.get_state(self.electrode_states)
+        result['actuated_area'] = self.get_actuated_area(result
+                                                         ['electrode_states'])
+        return result
+
+    def set_electrode_state(self, electrode_id, state):
+        '''
+        Set the state of a single electrode.
+
+        Args:
+
+            electrode_id (str) : Electrode identifier (e.g., `"electrode001"`)
+            state (int) : State of electrode
+        '''
+        return self.set_electrode_states(pd.Series([state],
+                                                   index=[electrode_id]))
+
+    def set_electrode_states(self, electrode_states, save=True):
+        '''
+        Set the state of multiple electrodes.
+
+        Args:
+
+            electrode_states (pandas.Series) : State of electrodes, indexed by
+                electrode identifier (e.g., `"electrode001"`).
+            save (bool) : Trigger save request for protocol step.
+
+        Returns:
+
+            (dict) : States of modified channels and electrodes, as well as the
+                total area of all actuated electrodes.
+        '''
+        app = get_app()
+
+        result = self.get_state(electrode_states)
+
+        # Set the state of DMF device channels.
+        self.electrode_states = (result['electrode_states']
+                                 .combine_first(self.electrode_states))
+
+        if save:
+            def notify(step_number):
+                emit_signal('on_step_options_changed', [self.name,
+                                                        step_number],
+                            interface=IPlugin)
+            gtk.idle_add(notify, app.protocol.current_step_number)
+
+        result['actuated_area'] = self.get_actuated_area(self.electrode_states)
+        return result
+
+    def on_execute__set_electrode_state(self, request):
+        data = decode_content_data(request)
+        return self.set_electrode_state(data['electrode_id'], data['state'])
+
+    def on_execute__set_electrode_states(self, request):
+        data = decode_content_data(request)
+        try:
+            return self.set_electrode_states(data['electrode_states'],
+                                             save=data.get('save', True))
+        except:
+            logger.error(str(data), exc_info=True)
+
+    def on_execute__get_channel_states(self, request):
+        return self.get_channel_states()
+
+PluginGlobals.push_env('microdrop')
+
+
+class ElectrodeControllerPlugin(SingletonPlugin, StepOptionsController):
+    """
+    This class is automatically registered with the PluginManager.
+    """
+    implements(IPlugin)
+    plugin_name = 'wheelerlab.electrode_controller_plugin'
+
+    def __init__(self):
+        self.name = self.plugin_name
+        self.plugin = None
+        self.command_timeout_id = None
+
+    def on_plugin_enable(self):
+        """
+        Handler called once the plugin instance is enabled.
+
+        Note: if you inherit your plugin from AppDataController and don't
+        implement this handler, by default, it will automatically load all
+        app options from the config file. If you decide to overide the
+        default handler, you should call:
+
+            AppDataController.on_plugin_enable(self)
+
+        to retain this functionality.
+        """
+        self.cleanup()
+        self.plugin = ElectrodeControllerZmqPlugin(self, self.name,
+                                                   get_hub_uri())
+        # Initialize sockets.
+        self.plugin.reset()
+
+        def check_command_socket():
+            try:
+                msg_frames = (self.plugin.command_socket
+                              .recv_multipart(zmq.NOBLOCK))
+            except zmq.Again:
+                pass
+            else:
+                self.plugin.on_command_recv(msg_frames)
+            return True
+
+        self.command_timeout_id = gobject.timeout_add(10, check_command_socket)
+
+    def cleanup(self):
+        if self.command_timeout_id is not None:
+            gobject.source_remove(self.command_timeout_id)
+        if self.plugin is not None:
+            self.plugin = None
+
+    def on_plugin_disable(self):
+        """
+        Handler called once the plugin instance is disabled.
+        """
+        self.cleanup()
+
+    def on_app_exit(self):
+        """
+        Handler called just before the Microdrop application exits.
+        """
+        self.cleanup()
+
+    def on_step_swapped(self, old_step_number, step_number):
+        if self.plugin is not None:
+            self.plugin.execute_async('wheelerlab.electrode_controller_plugin',
+                                      'get_channel_states')
+
+
+PluginGlobals.pop_env()
---
+++ packages/microdrop/microdrop-2.0.post110.dev156674653.tar.gz/microdrop-2.0.post110.dev156674653/microdrop/gui/dmf_device_controller.py
@@ -0,0 +1,442 @@
+"""
+Copyright 2011 Ryan Fobel
+
+This file is part of Microdrop.
+
+Microdrop is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Microdrop is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Microdrop.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import traceback
+import shutil
+import logging
+
+from flatland import Form
+from lxml import etree
+from microdrop_device_converter import convert_device_to_svg
+from microdrop_utility import copytree
+from microdrop_utility.gui import yesno
+from pygtkhelpers.ui.extra_dialogs import text_entry_dialog
+from pygtkhelpers.ui.extra_widgets import Directory
+from pygtkhelpers.ui.notebook import add_filters
+import gtk
+import path_helpers as ph
+import pygtkhelpers as pgh
+import pygtkhelpers.ui.dialogs
+import svg_model as sm
+
+from ..app_context import get_app
+from ..dmf_device import DmfDevice, ELECTRODES_XPATH
+from ..plugin_helpers import AppDataController
+from ..plugin_manager import (IPlugin, SingletonPlugin, implements,
+                              PluginGlobals, ScheduleRequest, emit_signal)
+from .. import base_path
+
+logger = logging.getLogger(__name__)
+
+PluginGlobals.push_env('microdrop')
+
+# Define name of device file.  Name of device is inferred from name of parent
+# directory when device is loaded.
+OLD_DEVICE_FILENAME = 'device'
+DEVICE_FILENAME = 'device.svg'
+
+
+class DmfDeviceController(SingletonPlugin, AppDataController):
+    implements(IPlugin)
+
+    AppFields = Form.of(Directory.named('device_directory')
+                        .using(default='', optional=True))
+
+    def __init__(self):
+        self.name = "microdrop.gui.dmf_device_controller"
+        self.previous_device_dir = None
+        self._modified = False
+
+    @property
+    def modified(self):
+        return self._modified
+
+    @modified.setter
+    def modified(self, value):
+        self._modified = value
+        if getattr(self, 'menu_rename_dmf_device', None):
+            self.menu_rename_dmf_device.set_sensitive(not value)
+            self.menu_save_dmf_device.set_sensitive(value)
+
+    def on_app_options_changed(self, plugin_name):
+        try:
+            if plugin_name == self.name:
+                values = self.get_app_values()
+                if 'device_directory' in values:
+                    self.apply_device_dir(values['device_directory'])
+        except (Exception,):
+            logger.info(''.join(traceback.format_exc()))
+            raise
+
+    def apply_device_dir(self, device_directory):
+        app = get_app()
+
+        # if the device directory is empty or None, set a default
+        if not device_directory:
+            device_directory = (ph.path(app.config.data['data_dir'])
+                                .joinpath('devices'))
+            self.set_app_values({'device_directory': device_directory})
+
+        if self.previous_device_dir and (device_directory ==
+                                         self.previous_device_dir):
+            # If the data directory hasn't changed, we do nothing
+            return False
+
+        device_directory = ph.path(device_directory)
+        if self.previous_device_dir:
+            device_directory.makedirs_p()
+            if device_directory.listdir():
+                result = yesno('Merge?', '''\
+Target directory [%s] is not empty.  Merge contents with
+current devices [%s] (overwriting common paths in the target
+directory)?''' % (device_directory, self.previous_device_dir))
+                if not result == gtk.RESPONSE_YES:
+                    return False
+
+            original_directory = ph.path(self.previous_device_dir)
+            for d in original_directory.dirs():
+                copytree(d, device_directory.joinpath(d.name))
+            for f in original_directory.files():
+                f.copyfile(device_directory.joinpath(f.name))
+            original_directory.rmtree()
+        elif not device_directory.isdir():
+            # if the device directory doesn't exist, copy the skeleton dir
+            if device_directory.parent:
+                device_directory.parent.makedirs_p()
+            base_path().joinpath('devices').copytree(device_directory)
+        self.previous_device_dir = device_directory
+        return True
+
+    def on_plugin_enable(self):
+        app = get_app()
+
+        self.menu_detect_connections = app.builder.get_object('menu_detect_connections')
+        self.menu_import_dmf_device = app.builder.get_object('menu_import_dmf_device')
+        self.menu_load_dmf_device = app.builder.get_object('menu_load_dmf_device')
+        self.menu_rename_dmf_device = app.builder.get_object('menu_rename_dmf_device')
+        self.menu_save_dmf_device = app.builder.get_object('menu_save_dmf_device')
+        self.menu_save_dmf_device_as = app.builder.get_object('menu_save_dmf_device_as')
+
+        app.signals["on_menu_detect_connections_activate"] = self.on_detect_connections
+        app.signals["on_menu_import_dmf_device_activate"] = self.on_import_dmf_device
+        app.signals["on_menu_load_dmf_device_activate"] = self.on_load_dmf_device
+        app.signals["on_menu_rename_dmf_device_activate"] = self.on_rename_dmf_device
+        app.signals["on_menu_save_dmf_device_activate"] = self.on_save_dmf_device
+        app.signals["on_menu_save_dmf_device_as_activate"] = self.on_save_dmf_device_as
+
+        app.dmf_device_controller = self
+        defaults = self.get_default_app_options()
+        data = app.get_data(self.name)
+        for k, v in defaults.items():
+            if k not in data:
+                data[k] = v
+        app.set_data(self.name, data)
+        emit_signal('on_app_options_changed', [self.name])
+
+        # disable menu items until a device is loaded
+        self.menu_detect_connections.set_sensitive(False)
+        self.menu_rename_dmf_device.set_sensitive(False)
+        self.menu_save_dmf_device.set_sensitive(False)
+        self.menu_save_dmf_device_as.set_sensitive(False)
+
+    def on_protocol_pause(self):
+        pass
+
+    def on_app_exit(self):
+        self.save_check()
+
+    def load_device(self, file_path, **kwargs):
+        '''
+        Load device file.
+
+        Parameters
+        ----------
+        file_path : str
+            A MicroDrop device `.svg` file or a (deprecated) MicroDrop 1.0
+            device.
+        '''
+        app = get_app()
+        self.modified = False
+        device = app.dmf_device
+        file_path = ph.path(file_path)
+
+        if not file_path.isfile():
+            old_version_file_path = (file_path.parent
+                                     .joinpath(OLD_DEVICE_FILENAME))
+            if old_version_file_path:
+                # SVG device file does not exist, but old-style (i.e., v0.3.0)
+                # device file found.
+                try:
+                    # Try to import old-style device to new SVG format.
+                    self.import_device(old_version_file_path)
+                    logger.warning('Auto-converted old-style device to new SVG'
+                                   ' device format.  Open in Inkscape to '
+                                   'verify scale and adjacent electrode '
+                                   'connections.')
+                except Exception, e:
+                    logger.error('Error importing device. %s' % e, exc_info=True)
+                return
+            else:
+                logger.error('Error opening device.  Please ensure file '
+                             'exists and is readable.', exc_info=True)
+                return
+
+        # SVG device file exists.  Load the device.
+        try:
+            logger.info('[DmfDeviceController].load_device: %s' % file_path)
+            if app.get_device_directory().realpath() == (file_path.realpath()
+                                                         .parent.parent):
+                # Selected device file is in MicroDrop devices directory.
+                new_device = False
+            else:
+                # Selected device file is not in MicroDrop devices directory.
+
+                # Copy file to devices directory under subdirectory with same
+                # name as file.
+                new_device_directory = (app.get_device_directory().realpath()
+                                        .joinpath(file_path.namebase)
+                                        .noconflict())
+                new_device_directory.makedirs_p()
+                new_file_path = new_device_directory.joinpath('device.svg')
+                file_path.copy(new_file_path)
+                file_path = new_file_path
+                new_device = True
+
+            # Load device from SVG file.
+            device = DmfDevice.load(file_path, name=file_path.parent.name,
+                                    **kwargs)
+            if new_device:
+                # Inform user that device was copied from original location
+                # into MicroDrop devices directory.
+                pgh.ui.dialogs.info('Device imported successfully',
+                                    long='New device copied into MicroDrop '
+                                    'devices directory:\n{}'.format(file_path),
+                                    parent=app.main_window_controller.view)
+                logger.info('[DmfDeviceController].load_device: Copied new '
+                            'device to: %s', file_path)
+            emit_signal("on_dmf_device_swapped", [app.dmf_device, device])
+        except:
+            logger.error('Error loading device.', exc_info=True)
+
+    def save_check(self):
+        app = get_app()
+        if self.modified:
+            result = yesno('Device %s has unsaved changes.  Save now?' %
+                           app.dmf_device.name)
+            if result == gtk.RESPONSE_YES:
+                self.save_dmf_device()
+
+    def save_dmf_device(self, save_as=False, rename=False):
+        '''
+        Save device configuration.
+
+        If `save_as=True`, we are saving a copy of the current device with a
+        new name.
+
+        If `rename=True`, we are saving the current device with a new name _(no
+        new copy is created)_.
+        '''
+        app = get_app()
+
+        name = app.dmf_device.name
+        # If the device has no name, try to get one.
+        if save_as or rename or name is None:
+            if name is None:
+                name = ""
+            name = text_entry_dialog('Device name', name, 'Save device')
+            if name is None:
+                name = ""
+        if name:
+            device_directory = ph.path(app.get_device_directory()).realpath()
+            # Construct the directory name for the current device.
+            if app.dmf_device.name:
+                src = device_directory.joinpath(app.dmf_device.name)
+            # Construct the directory name for the new device _(which is the
+            # same as the current device, if we are not renaming or "saving
+            # as")_.
+            dest = device_directory.joinpath(name)
+
+            # If we're renaming, move the old directory.
+            if rename and src.isdir():
+                if src == dest:
+                    return
+                if dest.isdir():
+                    logger.error("A device with that name already exists.")
+                    return
+                shutil.move(src, dest)
+
+            # Create the directory for the new device name, if it doesn't
+            # exist.
+            dest.makedirs_p()
+
+            # Convert device to SVG string.
+            svg_unicode = app.dmf_device.to_svg()
+
+            output_path = dest.joinpath(DEVICE_FILENAME)
+
+            # Save the device to the new target directory.
+            with output_path.open('wb') as output:
+                output.write(svg_unicode)
+
+            # Reset modified status, since save acts as a checkpoint.
+            self.modified = False
+
+            # Notify plugins that device has been saved.
+            emit_signal('on_dmf_device_saved', [app.dmf_device,
+                                                str(output_path)])
+
+            # If the device name has changed, update the application device
+            # state.
+            if name != app.dmf_device.name:
+                self.load_device(output_path)
+            return output_path
+
+    def get_schedule_requests(self, function_name):
+        """
+        Returns a list of scheduling requests (i.e., ScheduleRequest
+        instances) for the function specified by function_name.
+        """
+        if function_name == 'on_plugin_enable':
+            return [ScheduleRequest('microdrop.gui.config_controller',
+                                    self.name),
+                    ScheduleRequest('microdrop.gui.main_window_controller',
+                                    self.name)]
+        elif function_name == 'on_dmf_device_swapped':
+            return [ScheduleRequest('microdrop.app', self.name),
+                    ScheduleRequest('microdrop.gui.protocol_controller',
+                                    self.name)]
+        return []
+
+    # GUI callbacks
+
+    def on_load_dmf_device(self, widget, data=None):
+        self.save_check()
+        app = get_app()
+        directory = app.get_device_directory()
+        dialog = gtk.FileChooserDialog(title="Load device",
+                                       action=gtk.FILE_CHOOSER_ACTION_OPEN,
+                                       buttons=(gtk.STOCK_CANCEL,
+                                                gtk.RESPONSE_CANCEL,
+                                                gtk.STOCK_OPEN,
+                                                gtk.RESPONSE_OK))
+        dialog.set_default_response(gtk.RESPONSE_OK)
+        if directory:
+            dialog.set_current_folder(directory)
+        add_filters(dialog, [{'name': 'DMF device (*.svg)',
+                              'pattern': '*.svg'}])
+        response = dialog.run()
+        if response == gtk.RESPONSE_OK:
+            filename = dialog.get_filename()
+            self.load_device(filename)
+        dialog.destroy()
+
+    def on_import_dmf_device(self, widget, data=None):
+        self.save_check()
+        dialog = gtk.FileChooserDialog(title="Import device",
+                                       action=gtk.FILE_CHOOSER_ACTION_OPEN,
+                                       buttons=(gtk.STOCK_CANCEL,
+                                                gtk.RESPONSE_CANCEL,
+                                                gtk.STOCK_OPEN,
+                                                gtk.RESPONSE_OK))
+        dialog.set_default_response(gtk.RESPONSE_OK)
+        add_filters(dialog, [{'name': 'DMF device version 0.3.0 (device)',
+                              'pattern': 'device'}])
+        response = dialog.run()
+        filename = dialog.get_filename()
+        dialog.destroy()
+
+        if response == gtk.RESPONSE_OK:
+            try:
+                self.import_device(filename)
+            except Exception, e:
+                logger.error('Error importing device. %s' % e, exc_info=True)
+
+    def import_device(self, input_device_path):
+        input_device_path = ph.path(input_device_path).realpath()
+        output_device_path = (input_device_path.parent
+                              .joinpath(input_device_path.namebase + '.svg'))
+        overwrite = False
+        if output_device_path.isfile():
+            result = yesno('Output file exists.  Overwrite?')
+            if not result == gtk.RESPONSE_YES:
+                return
+            overwrite = True
+        convert_device_to_svg(input_device_path, output_device_path,
+                              use_svg_path=True, detect_connections=True,
+                              extend_mm=.5, overwrite=overwrite)
+        self.load_device(output_device_path)
+
+    def on_detect_connections(self, widget, data=None):
+        '''
+        Auto-detect adjacent electrodes in device and save updated SVG to
+        device file path.
+        '''
+        app = get_app()
+        svg_source = ph.path(app.dmf_device.svg_filepath)
+
+        # Check for existing `Connections` layer.
+        xml_root = etree.parse(svg_source)
+        connections_xpath = '//svg:g[@inkscape:label="Connections"]'
+        connections_groups = xml_root.xpath(connections_xpath,
+                                            namespaces=sm.INKSCAPE_NSMAP)
+
+        if connections_groups:
+            # Existing "Connections" layer found in source SVG.
+            # Prompt user to overwrite.
+            response = pgh.ui.dialogs.yesno('"Connections" layer already '
+                                            'exists in device file.\n\n'
+                                            'Overwrite?',
+                                            parent=
+                                            app.main_window_controller.view)
+            if response == gtk.RESPONSE_NO:
+                return
+
+        # Auto-detect adjacent electrodes from SVG paths and polygons from
+        # `Device` layer.
+        connections_svg = (sm.detect_connections
+                           .auto_detect_adjacent_shapes(svg_source,
+                                                        shapes_xpath=
+                                                        ELECTRODES_XPATH))
+
+        # Remove existing "Connections" layer and merge new "Connections" layer
+        # with original SVG.
+        output_svg = (sm.merge
+                      .merge_svg_layers([sm.remove_layer(svg_source,
+                                                         'Connections'),
+                                         connections_svg]))
+
+        with svg_source.open('w') as output:
+            output.write(output_svg.getvalue())
+
+    def on_rename_dmf_device(self, widget, data=None):
+        self.save_dmf_device(rename=True)
+
+    def on_save_dmf_device(self, widget, data=None):
+        self.save_dmf_device()
+
+    def on_save_dmf_device_as(self, widget, data=None):
+        self.save_dmf_device(save_as=True)
+
+    def on_dmf_device_changed(self, dmf_device):
+        self.modified = True
+
+    def on_dmf_device_swapped(self, old_dmf_device, dmf_device):
+        self.menu_detect_connections.set_sensitive(True)
+        self.menu_save_dmf_device_as.set_sensitive(True)
+
+PluginGlobals.pop_env()
