338718612b7f547ad657b8435aff203c2ef241f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:02 +0000

    Add package IndicoIo/0.16.3/IndicoIo-0.16.3.tar.gz

---
+++ packages/IndicoIo/IndicoIo-0.16.3.tar.gz/IndicoIo-0.16.3/setup.py
@@ -0,0 +1,60 @@
+"""
+Setup for indico apis
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="IndicoIo",
+    version="0.16.3",
+    packages=[
+        "indicoio",
+        "indicoio.text",
+        "indicoio.images",
+        "indicoio.utils",
+        "indicoio.custom",
+        "tests",
+    ],
+    description="""
+        A Python Wrapper for indico.
+        Use pre-built state of the art machine learning algorithms with a single line of code.
+    """,
+    license="MIT License (See LICENSE)",
+    long_description=open("README.rst").read(),
+    url="https://github.com/IndicoDataSolutions/indicoio-python",
+    author="Alec Radford, Slater Victoroff, Aidan McLaughlin, Madison May, Anne Carlson",
+    author_email="""
+        Alec Radford <alec@indico.io>,
+        Slater Victoroff <slater@indico.io>,
+        Aidan McLaughlin <aidan@indico.io>,
+        Madison May <madison@indico.io>,
+        Anne Carlson <annie@indico.io>
+    """,
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    setup_requires=[
+        "six >= 1.3.0",
+        "pillow >= 2.8.1"
+    ],
+    install_requires=[
+        "requests >= 1.2.3",
+        "six >= 1.3.0",
+        "pillow >= 2.8.1",
+        "mock >= 1.3.0, < 2.0.0"
+    ]
+)
---
+++ packages/IndicoIo/IndicoIo-0.16.3.tar.gz/IndicoIo-0.16.3/tests/test_places.py
@@ -0,0 +1,38 @@
+import unittest
+
+from indicoio import config
+from indicoio import places
+
+
+class Places(unittest.TestCase):
+    def setUp(self):
+        self.api_key = config.api_key
+
+        if not self.api_key:
+            raise SkipTest
+
+    def test_places_v2(self):
+        test_data = "Lets all go to Virginia beach before it gets too cold to wander outside."
+        response = places(test_data)
+        self.assertTrue(isinstance(response, list))
+        sorted_response = sorted(response, key=lambda x: x['confidence'], reverse=True)
+        self.assertTrue('Virginia' in sorted_response[0]['text'])
+
+        test_data = [test_data] * 2
+        response = places(test_data)
+        self.assertTrue(isinstance(response, list))
+        sorted_response = [sorted(arr, key=lambda x: x['confidence'], reverse=True) for arr in response]
+        self.assertTrue('Virginia' in sorted_response[0][0]['text'])
+
+    def test_places_v1(self):
+        test_data = "Lets all go to Virginia beach before it gets too cold to wander outside."
+        response = places(test_data, version=1)
+        self.assertTrue(isinstance(response, list))
+        sorted_response = sorted(response, key=lambda x: x['confidence'], reverse=True)
+        self.assertTrue('Virginia' in sorted_response[0]['text'])
+
+        test_data = [test_data] * 2
+        response = places(test_data, version = 1)
+        self.assertTrue(isinstance(response, list))
+        sorted_response = [sorted(arr, key=lambda x: x['confidence'], reverse=True) for arr in response]
+        self.assertTrue('Virginia' in sorted_response[0][0]['text'])
