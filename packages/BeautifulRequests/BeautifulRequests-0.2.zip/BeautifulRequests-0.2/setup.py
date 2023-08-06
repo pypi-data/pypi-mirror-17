3539b94ce46b555ed5f90abe98856e39dece1091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:17 +0000

    Add package BeautifulRequests/0.2/BeautifulRequests-0.2.zip

---
+++ packages/BeautifulRequests/BeautifulRequests-0.2.zip/BeautifulRequests-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='BeautifulRequests',
+      packages = ['BeautifulRequests'],
+      version='0.2',
+      description='Combine Beautiful Soup 4 and Requests for one super library',
+      url='https://github.com/shantnu/BeautifulRequests',
+      download_url = 'https://github.com/shantnu/BeautifulRequests/archive/0.2.zip',
+      author='Shantnu',
+      license='GPL',
+      install_requires=['requests', 'beautifulsoup4', 'vcrpy', 'pytest'],
+      )
