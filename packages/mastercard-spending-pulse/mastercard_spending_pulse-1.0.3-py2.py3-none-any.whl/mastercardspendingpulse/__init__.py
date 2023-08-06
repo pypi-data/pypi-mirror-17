bb08c7d41b12a907d7d20e9952aa9c4a83df45d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:24 +0000

    Add package mastercard-spending-pulse/1.0.3/mastercard_spending_pulse-1.0.3-py2.py3-none-any.whl

---
+++ packages/mastercard-spending-pulse/mastercard_spending_pulse-1.0.3-py2.py3-none-any.whl/mastercardspendingpulse/__init__.py
@@ -0,0 +1,6 @@
+from mastercardapicore import *
+from mastercardspendingpulse.parameters import *
+from mastercardspendingpulse.spendingpulsereport import *
+from mastercardspendingpulse.gasweekly import *
+from mastercardspendingpulse.subscription import *
+from mastercardspendingpulse.sdkconfig import *
