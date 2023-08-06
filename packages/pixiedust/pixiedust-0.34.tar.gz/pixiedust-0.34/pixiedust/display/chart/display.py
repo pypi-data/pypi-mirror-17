fb55fc024b224480faf581ca087eb9c4efadc909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:16 +0000

    Add package pixiedust/0.34/pixiedust-0.34.tar.gz

---
+++ packages/pixiedust/pixiedust-0.34.tar.gz/pixiedust-0.34/pixiedust/display/chart/display.py
@@ -0,0 +1,44 @@
+# -------------------------------------------------------------------------------
+# Copyright IBM Corp. 2016
+# 
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -------------------------------------------------------------------------------
+
+from ..display import Display
+from pyspark.sql import functions as F
+    
+class ChartDisplay(Display):
+    def doRender(self, handlerId):
+        self._addHTML("""
+            <p><b>Sorry, but this visualization is not yet implemented. Please check back often!</b></p>
+        """
+        )
+
+    # numerical used as a boolean flag for truth table
+    def sampleColumn(self, numerical):
+        default=None
+        for field in self.entity.schema.fields:
+            # Ignore unique ids
+            if field.name.lower() != 'id' and ( not numerical or self.isNumericType(field.dataType.__class__.__name__) ):
+            # Find a good column to display in pie ChartDisplay
+                default = default or field.name
+                count = self.entity.count()
+                sample = self.entity.sample(False, (float(200) / count)) if count > 200 else self.entity
+                orderedSample = sample.groupBy(field.name).agg(F.count(field.name).alias("agg")).orderBy(F.desc("agg")).select("agg")
+                if orderedSample.take(1)[0]["agg"] > 10:
+                    return [field.name]
+        # Otherwise, return first non-id column
+        return [default]
+
+    def isNumericType(self, type):
+        return (type =="LongType" or type == "IntegerType" or type == "DoubleType" or type == "DecimalType" or type == "FloatType")
