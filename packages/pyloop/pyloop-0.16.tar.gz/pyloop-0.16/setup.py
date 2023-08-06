195c991d358bbfd98fdd841498dd7254eea300fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:56 +0000

    Add package pyloop/0.16/pyloop-0.16.tar.gz

---
+++ packages/pyloop/pyloop-0.16.tar.gz/pyloop-0.16/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+		name="pyloop",
+		version="0.16",
+		py_modules = ['pyloop.app'],
+		packages = ['pyloop'],
+		description='An universal pacakge manager for python',
+		install_requires=[
+			'Click',
+			'colorama',
+			'termcolor',
+			'pyfiglet',
+			'jsonschema'
+		],
+		entry_points = '''
+			[console_scripts]
+			pyloop=pyloop.app:index
+		''',
+		url='https://github.com/Hedronium/pyloop',
+		download_url = 'https://github.com/Hedronium/pyloop/tarball/0.16',
+		author = 'Hedronium',
+		keywords = ['package manger','python 3'],
+		author_email= 'project.anik@gmail.com',
+		licence='MIT'
+	)
