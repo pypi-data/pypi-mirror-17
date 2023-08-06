a2d4554c24408b6912ead6b9f3b358f8ad4bf38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:06 +0000

    Add package of/0.13.6/of-0.13.6-py3.5.egg

---
+++ packages/of/of-0.13.6-py3.5.egg/of/__init__.py
@@ -0,0 +1,10 @@
+"""
+This package holds the Optimal Framework source package
+"""
+__author__ = 'Nicklas Borjesson'
+
+# Version and release information used by Sphinx for documentation and setuptools for package generation.
+__version__ = '0.13'
+__release__ = '0.13.6'
+__copyright__ = '2010-2016, Optimal BPM'
+
