664adf9730fc55bd5ef10e0234da0ca4081301d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:26 +0000

    Add package babel-vue-extractor/0.2.1/babel-vue-extractor-0.2.1.tar.gz

---
+++ packages/babel-vue-extractor/babel-vue-extractor-0.2.1.tar.gz/babel-vue-extractor-0.2.1/babelvueextractor/extract.py
@@ -0,0 +1,62 @@
+import re
+from ast import literal_eval
+
+from babelvueextractor.lexer import (
+    Lexer,
+    TOKEN_VAR,
+    TOKEN_CONST,
+    TOKEN_COMMENT,
+    TOKEN_RAW_HTML,
+    TOKEN_DOUBLE_WAY_BINDING,
+    TOKEN_DIRECTIVE
+)
+from babelvueextractor.utils import force_text
+
+TOKENS = [TOKEN_VAR, TOKEN_CONST, TOKEN_COMMENT, TOKEN_RAW_HTML, TOKEN_DOUBLE_WAY_BINDING, TOKEN_DIRECTIVE]
+
+func_re = re.compile('(?P<funcname>(\w+|_))\((?P<messages>.*?)\)+?')
+
+
+def _get_messages(raw_messages_string):
+    try:
+        messages = literal_eval(u'[%s]' % unicode(raw_messages_string))
+    except:
+        raise
+    messages = map(force_text, messages)
+
+    if len(messages) == 1:
+        return messages[0]
+
+    return messages
+
+
+def extract_vue(fileobj, keywords, comment_tags, options):
+    """Extract messages from Vue template files.
+
+    :param fileobj: the file-like the messages should be extracted from
+    :param keywords: a list of keywords (i.e. function names) that should be recognize as translation functions
+    :param comment_tags: a list of translator tags to search for and include in the results
+    :param options: a dictionary of additional options (optional)
+    :return: an iterator over ``(lineno, funcname, message, comments)``
+    :rtype: ``iterator``
+    """
+
+    intrans = False
+    inplural = False
+    incomment = False
+    singular = []
+    plural = []
+    lineno = 1
+    template = None
+
+    for t in Lexer(unicode(fileobj.read(), encoding=options.get('encoding', 'utf-8')), None).tokenize():
+        if t.token_type in TOKENS:
+            try:
+                matched = func_re.match(t.contents).groupdict()
+            except AttributeError:
+                pass
+            else:
+                messages = _get_messages(matched['messages'])
+                # messages = matched['messages']
+
+                yield t.lineno, matched['funcname'], messages, []
---
+++ packages/babel-vue-extractor/babel-vue-extractor-0.2.1.tar.gz/babel-vue-extractor-0.2.1/babelvueextractor/tests/test_extractor.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from babel.messages.extract import DEFAULT_KEYWORDS, extract
+
+from babelvueextractor.extract import extract_vue
+from babelvueextractor.tests.utils import FileMock
+
+
+class TestMessagesExtractor(unittest.TestCase):
+    def test_empty_template(self):
+        template = FileMock("")
+        result = extract_vue(template, DEFAULT_KEYWORDS.keys(), [], {})
+        self.assertEqual(list(), list(result))
+
+    def test_no_messages(self):
+        template = FileMock("""
+        <div>
+            <h1>Foo</h1>
+            <p>Lore ipsum delore ...</p>
+        </div>
+        """)
+        result = extract_vue(template, DEFAULT_KEYWORDS.keys(), [], {})
+        self.assertEqual(list(), list(result))
+
+    def test_gettext(self):
+        template = FileMock("""
+        <div>
+            {{ gettext('Foo') }}
+        </div>
+        """)
+        result = extract_vue(template, DEFAULT_KEYWORDS.keys(), [], {})
+        self.assertEqual(list(result), [
+            (3, u'gettext', u"Foo", [])
+        ])
+
+    def test_underscore(self):
+        template = FileMock("""
+        <div>
+            {{ _("Bar") }}
+        </div>
+        """)
+        result = extract_vue(template, DEFAULT_KEYWORDS.keys(), [], {})
+        self.assertEqual(list(result), [
+            (3, '_', u'Bar', [])
+        ])
+
+    def test_token_without_content(self):
+        template = FileMock("""
+            {{  }}
+        """)
+        result = extract_vue(template, DEFAULT_KEYWORDS.keys(), [], {})
+        self.assertEqual(list(result), [])
+
+    def test_commas(self):
+        template = FileMock("""
+            {{ gettext('Hello, User') }}
+            {{ gettext("You're") }}
+        """)
+
+        result = extract_vue(template, DEFAULT_KEYWORDS.keys(), [], {})
+        self.assertEqual(list(result), [
+            (2, 'gettext', u'Hello, User', []),
+            (3, 'gettext', u"You're", [])
+        ])
+
+    def test_babel(self):
+        method = 'babelvueextractor.extract.extract_vue'
+        fileobj = open('babelvueextractor/tests/templates/for_babel.vhtml')
+        result = extract(method, fileobj)
+
+        self.assertListEqual(list(result), [
+            (1, u'Привет, User', [], None),
+            (2, u'привет123', [], None)
+        ])
+
+    def test_gettext_with_parameter(self):
+        template = FileMock("""
+        <li class="select__option"
+            v-for="season in rankSeasons"
+            @click="toggleSeasonAction(season)">
+            {{ gettext('{number} season').replace("{number}", season) }}
+        </li>
+        """)
+        result = extract_vue(template, DEFAULT_KEYWORDS.keys(), [], {})
+        self.assertEqual(list(result), [
+            (5, u'gettext', u'{number} season', [])
+        ])
+
+    def test_directives(self):
+        template = FileMock("""
+        <div v-text="gettext('Sometext')"></div>
+        """)
+        result = extract_vue(template, DEFAULT_KEYWORDS.keys(), [], {})
+        self.assertListEqual(list(result), [(2, u'gettext', u'Sometext', [])])
+
+    def test_directives_with_inner_tag(self):
+        template = FileMock("""
+        <div v-text="gettext('Sometext')">
+        {{ gettext('Hello') }}
+        </div>
+        """)
+        result = extract_vue(template, DEFAULT_KEYWORDS.keys(), [], {})
+        self.assertListEqual(list(result), [
+            (2, u'gettext', u'Sometext', []),
+            (3, u'gettext', u'Hello', [])
+        ])
