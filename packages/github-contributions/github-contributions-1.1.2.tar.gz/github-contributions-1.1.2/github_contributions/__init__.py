8d7ab863d6d1d68d01771f58a9bd09c7a2c500c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:52 +0000

    Add package github-contributions/1.1.2/github-contributions-1.1.2.tar.gz

---
+++ packages/github-contributions/github-contributions-1.1.2.tar.gz/github-contributions-1.1.2/github_contributions/__init__.py
@@ -0,0 +1,31 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+GITHUB_URL = 'https://github.com/'
+
+
+def get_contributions(usernames):
+    """
+    Get a github user's public contributions.
+    :param usernames: A string or sequence of github usernames.
+    """
+    contributions = {'users': [], 'total': 0}
+
+    if isinstance(usernames, str) or isinstance(usernames, unicode):
+        usernames = [usernames]
+
+    for username in usernames:
+        response = requests.get('{0}{1}'.format(GITHUB_URL, username))
+
+        if not response.ok:
+            contributions['users'].append({username: dict(total=0)})
+            continue
+
+        bs = BeautifulSoup(response.content, "html.parser")
+        total = bs.find('div', {'class': 'js-contribution-graph'}).findNext('h2')
+        contributions['users'].append({username: dict(total=int(total.text.split()[0].replace(',', '')))})
+        contributions['total'] += int(total.text.split()[0].replace(',', ''))
+
+    return json.dumps(contributions, indent=4)
+
---
+++ packages/github-contributions/github-contributions-1.1.2.tar.gz/github-contributions-1.1.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='github-contributions',
+    version='1.1.2',
+    packages=['github_contributions'],
+    url='https://github.com/garnertb/github-contributions',
+    license='GNU General Public License',
+    author='Tyler Garner',
+    author_email='garnertb@prominentedge.com',
+    description='Scrape Github for a user\'s public contributions.',
+    install_requires=[
+        'requests==2.9.1',
+        'bs4==0.0.1',
+        'click==6.6'
+    ],
+    entry_points={
+        'console_scripts': [
+            'contributions = github_contributions.cli:get_contributions_cli',
+        ],
+    }
+)
